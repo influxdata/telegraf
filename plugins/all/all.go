bcb33d880e0f2e35bdae3855c3964892737f2f17
Evan Phoenix <evan@phx.io>
2015-05-18 11:54:27 -0700

    Enable pg and mysql by default

--- plugins/all/all.go
+++ plugins/all/all.go
@@ -1,6 +1,8 @@
 package all
 
 import (
+	_ "github.com/influxdb/tivan/plugins/mysql"
+	_ "github.com/influxdb/tivan/plugins/postgresql"
 	_ "github.com/influxdb/tivan/plugins/redis"
 	_ "github.com/influxdb/tivan/plugins/system"
 )
