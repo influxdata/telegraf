5390a8ea71ca3055507cd78bdf1b1db895626f37
Brian Brazil <brian.brazil@boxever.com>
2015-06-20 13:38:01 +0100

    Add Prometheus plugin.
    This allows pulling Prometheus metrics from
    any client library or exporter over HTTP.

--- plugins/all/all.go
+++ plugins/all/all.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/influxdb/telegraf/plugins/memcached"
 	_ "github.com/influxdb/telegraf/plugins/mysql"
 	_ "github.com/influxdb/telegraf/plugins/postgresql"
+	_ "github.com/influxdb/telegraf/plugins/prometheus"
 	_ "github.com/influxdb/telegraf/plugins/redis"
 	_ "github.com/influxdb/telegraf/plugins/system"
 )
