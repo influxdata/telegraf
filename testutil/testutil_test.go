9ea5a88f84109da953a8a7685e90eda00300cc74
Cameron Sparr <cameronsparr@gmail.com>
2015-08-03 17:16:02 -0600

    Fix GetLocalHost testutil function for mac users (boot2docker)

--- testutil/testutil_test.go
+++ testutil/testutil_test.go
@@ -23,4 +23,12 @@ func TestDockerHost(t *testing.T) {
 		t.Fatalf("Host should take DOCKER_HOST value when set. Current value is [%s] and DOCKER_HOST is [%s]", host, os.Getenv("DOCKER_HOST"))
 	}
 
+	os.Setenv("DOCKER_HOST", "tcp://1.1.1.1:8080")
+
+	host = GetLocalHost()
+
+	if host != "1.1.1.1" {
+		t.Fatalf("Host should take DOCKER_HOST value when set. Current value is [%s] and DOCKER_HOST is [%s]", host, os.Getenv("DOCKER_HOST"))
+	}
+
 }
