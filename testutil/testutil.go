03c520798e37b694d2e339d3919d202513ca471d
Cameron Sparr <cameronsparr@gmail.com>
2015-08-03 17:16:02 -0600

    Fix GetLocalHost testutil function for mac users (boot2docker)

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -1,12 +1,27 @@
 package testutil
 
-import "os"
+import (
+	"net"
+	"net/url"
+	"os"
+)
 
 var localhost = "localhost"
 
 func GetLocalHost() string {
 	if dockerHostVar := os.Getenv("DOCKER_HOST"); dockerHostVar != "" {
-		return dockerHostVar
+		u, err := url.Parse(dockerHostVar)
+		if err != nil {
+			return dockerHostVar
+		}
+
+		// split out the ip addr from the port
+		host, _, err := net.SplitHostPort(u.Host)
+		if err != nil {
+			return dockerHostVar
+		}
+
+		return host
 	}
 	return localhost
 }
