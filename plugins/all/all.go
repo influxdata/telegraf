3b588f05021d0e6656de65135fb6e09c90524060
Simon Fraser <simonfr@gmail.com>
2015-08-04 13:47:50 +0100

    Add Lustre 2 plugin

--- plugins/all/all.go
+++ plugins/all/all.go
@@ -3,6 +3,7 @@ package all
 import (
 	_ "github.com/influxdb/telegraf/plugins/elasticsearch"
 	_ "github.com/influxdb/telegraf/plugins/kafka_consumer"
+	_ "github.com/influxdb/telegraf/plugins/lustre2"
 	_ "github.com/influxdb/telegraf/plugins/memcached"
 	_ "github.com/influxdb/telegraf/plugins/mongodb"
 	_ "github.com/influxdb/telegraf/plugins/mysql"
