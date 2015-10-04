6f4a3816a5bdf3a6bea20dfa1646fda3b394b640
Shirou WAKAYAMA <shirou.faw@gmail.com>
2015-10-04 22:52:29 +0900

    Add MQTT output.

--- outputs/all/all.go
+++ outputs/all/all.go
@@ -5,5 +5,6 @@ import (
 	_ "github.com/influxdb/telegraf/outputs/datadog"
 	_ "github.com/influxdb/telegraf/outputs/influxdb"
 	_ "github.com/influxdb/telegraf/outputs/kafka"
+	_ "github.com/influxdb/telegraf/outputs/mqtt"
 	_ "github.com/influxdb/telegraf/outputs/opentsdb"
 )
