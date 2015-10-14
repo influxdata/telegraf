7121e1a3b09fc12f57ed461aedeb8a423a6a13e9
Jonathan Cross <jrxFive@gmail.com>
2015-10-13 22:01:07 -0400

    fixes based on comments

--- plugins/zookeeper/zookeeper_test.go
+++ plugins/zookeeper/zookeeper_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMemcachedGeneratesMetrics(t *testing.T) {
+func TestZookeeperGeneratesMetrics(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping integration test in short mode")
 	}
@@ -22,7 +22,19 @@ func TestMemcachedGeneratesMetrics(t *testing.T) {
 	err := z.Gather(&acc)
 	require.NoError(t, err)
 
-	intMetrics := []string{"zookeeper_avg_latency", "zookeeper_packets_sent", "zookeeper_znode_count", "zookeeper_open_file_descriptor_count", "zookeeper_max_file_descriptor_count"}
+	intMetrics := []string{"zookeeper_avg_latency",
+		"zookeeper_max_latency",
+		"zookeeper_min_latency",
+		"zookeeper_packets_received",
+		"zookeeper_packets_sent",
+		"zookeeper_outstanding_requests",
+		"zookeeper_znode_count",
+		"zookeeper_watch_count",
+		"zookeeper_ephemerals_count",
+		"zookeeper_approximate_data_size",
+		"zookeeper_pending_syncs",
+		"zookeeper_open_file_descriptor_count",
+		"zookeeper_max_file_descriptor_count"}
 
 	for _, metric := range intMetrics {
 		assert.True(t, acc.HasIntValue(metric), metric)
