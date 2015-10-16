43b4f16de350df5c7ebe38233e78022080dd5d77
Jonathan Cross <jrxFive@gmail.com>
2015-10-16 13:50:26 -0400

    removed old commented out tests

--- plugins/puppetagent/puppetagent_test.go
+++ plugins/puppetagent/puppetagent_test.go
@@ -14,35 +14,6 @@ func TestGather(t *testing.T) {
 	}
 	pa.Gather(&acc)
 
-	// assert.True(t, acc.HasIntValue("events_failure"))
-	// assert.True(t, acc.HasIntValue("events_total"))
-	// assert.True(t, acc.HasIntValue("events_success"))
-	// assert.True(t, acc.HasIntValue("resources_failed"))
-	// assert.True(t, acc.HasIntValue("resources_scheduled"))
-	// assert.True(t, acc.HasIntValue("resources_changed"))
-	// assert.True(t, acc.HasIntValue("resources_skipped"))
-	// assert.True(t, acc.HasIntValue("resources_total"))
-	// assert.True(t, acc.HasIntValue("resources_failedtorestart"))
-	// assert.True(t, acc.HasIntValue("resources_restarted"))
-	// assert.True(t, acc.HasIntValue("resources_outofsync"))
-	// assert.True(t, acc.HasIntValue("changes_total"))
-
-	// assert.True(t, acc.HasIntValue("time_lastrun"))
-	// assert.True(t, acc.HasIntValue("version_config"))
-
-	// assert.True(t, acc.HasFloatValue("time_user"))
-	// assert.True(t, acc.HasFloatValue("time_schedule"))
-	// assert.True(t, acc.HasFloatValue("time_filebucket"))
-	// assert.True(t, acc.HasFloatValue("time_file"))
-	// assert.True(t, acc.HasFloatValue("time_exec"))
-	// assert.True(t, acc.HasFloatValue("time_anchor"))
-	// assert.True(t, acc.HasFloatValue("time_sshauthorizedkey"))
-	// assert.True(t, acc.HasFloatValue("time_service"))
-	// assert.True(t, acc.HasFloatValue("time_package"))
-	// assert.True(t, acc.HasFloatValue("time_total"))
-	// assert.True(t, acc.HasFloatValue("time_configretrieval"))
-	// assert.True(t, acc.HasFloatValue("time_cron"))
-
 	checkInt := []struct {
 		name  string
 		value int64
