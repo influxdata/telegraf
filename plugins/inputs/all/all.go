0a127048e6393ed64d48029f1f2527ad52850934
Henry Hu <henry.pf.hu@gmail.com>
2016-02-03 17:30:03 +0800

    Updated the module dependencies.

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -39,6 +39,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/sqlserver"
 	_ "github.com/influxdata/telegraf/plugins/inputs/statsd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/system"
+	_ "github.com/influxdata/telegraf/plugins/inputs/tail"
 	_ "github.com/influxdata/telegraf/plugins/inputs/trig"
 	_ "github.com/influxdata/telegraf/plugins/inputs/twemproxy"
 	_ "github.com/influxdata/telegraf/plugins/inputs/win_perf_counters"
