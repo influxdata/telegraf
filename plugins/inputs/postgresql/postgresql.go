eac52f1929c01a3f290497a24665d38b90a811af
Thomas Menard <menardorama@gmail.com>
2016-02-11 01:34:17 +0100

    Update the postgresql plugin to add the pg_stat_bgwriter view

--- plugins/inputs/postgresql/postgresql.go
+++ plugins/inputs/postgresql/postgresql.go
@@ -21,22 +21,22 @@ type Postgresql struct {
 var ignoredColumns = map[string]bool{"datid": true, "datname": true, "stats_reset": true}
 
 var sampleConfig = `
-  # specify address via a url matching:
-  #   postgres://[pqgotest[:password]]@localhost[/dbname]?sslmode=[disable|verify-ca|verify-full]
-  # or a simple string:
-  #   host=localhost user=pqotest password=... sslmode=... dbname=app_production
-  #
-  # All connection parameters are optional.
-  #
-  # Without the dbname parameter, the driver will default to a database
-  # with the same name as the user. This dbname is just for instantiating a
-  # connection with the server and doesn't restrict the databases we are trying
-  # to grab metrics for.
-  #
+  ### specify address via a url matching:
+  ###   postgres://[pqgotest[:password]]@localhost[/dbname]?sslmode=[disable|verify-ca|verify-full]
+  ### or a simple string:
+  ###   host=localhost user=pqotest password=... sslmode=... dbname=app_production
+  ###
+  ### All connection parameters are optional.
+  ###
+  ### Without the dbname parameter, the driver will default to a database
+  ### with the same name as the user. This dbname is just for instantiating a
+  ### connection with the server and doesn't restrict the databases we are trying
+  ### to grab metrics for.
+  ###
   address = "host=localhost user=postgres sslmode=disable"
 
-  # A list of databases to pull metrics about. If not specified, metrics for all
-  # databases are gathered.
+  ### A list of databases to pull metrics about. If not specified, metrics for all
+  ### databases are gathered.
   # databases = ["app_production", "testing"]
 `
 
@@ -94,30 +94,29 @@ func (p *Postgresql) Gather(acc telegraf.Accumulator) error {
 			return err
 		}
 	}
+	//return rows.Err()
+        query = `SELECT * FROM pg_stat_bgwriter`
 
-	query = `SELECT * FROM pg_stat_bgwriter`
-	
-	rows, err := db.Query(query)
-	if err != nil {
-		return err
-	}
+        bg_writer_row, err := db.Query(query)
+        if err != nil {
+                return err
+        }
 
-	defer rows.Close()
+        defer bg_writer_row.Close()
 
-	// grab the column information from the result
-	p.OrderedColumns, err = rows.Columns()
-	if err != nil {
-		return err
-	}
+        // grab the column information from the result
+        p.OrderedColumns, err = bg_writer_row.Columns()
+        if err != nil {
+                return err
+        }
 
-	for rows.Next() {
-		err = p.accRow(rows, acc)
-		if err != nil {
-			return err
-		}
-	}
-
-	return rows.Err()
+        for bg_writer_row.Next() {
+                err = p.accRow(bg_writer_row, acc)
+                if err != nil {
+                        return err
+                }
+        }
+	return bg_writer_row.Err()
 }
 
 type scanner interface {
@@ -146,11 +145,14 @@ func (p *Postgresql) accRow(row scanner, acc telegraf.Accumulator) error {
 	if err != nil {
 		return err
 	}
-
-	// extract the database name from the column map
-	dbnameChars := (*columnMap["datname"]).([]uint8)
-	for i := 0; i < len(dbnameChars); i++ {
-		dbname.WriteString(string(dbnameChars[i]))
+	if  columnMap["datname"] != nil {
+		// extract the database name from the column map
+		dbnameChars := (*columnMap["datname"]).([]uint8)
+		for i := 0; i < len(dbnameChars); i++ {
+			dbname.WriteString(string(dbnameChars[i]))
+		}
+	} else {
+		dbname.WriteString("postgres")
 	}
 
 	tags := map[string]string{"server": p.Address, "db": dbname.String()}
