755332a458d557ed2346736a0471716c9338e9d5
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2016-04-11 16:56:37 +0200

    \#920 Log core metrics.

--- plugins/inputs/uwsgi/uwsgi.go
+++ plugins/inputs/uwsgi/uwsgi.go
@@ -65,6 +65,7 @@ func (u *Uwsgi) gatherURL(acc telegraf.Accumulator, url string) error {
 	u.gatherStatServer(acc, &s)
 	u.gatherWorkers(acc, &s)
 	u.gatherApps(acc, &s)
+	u.gatherCores(acc, &s)
 
 	return nil
 }
@@ -138,6 +139,30 @@ func (u *Uwsgi) gatherApps(acc telegraf.Accumulator, s *StatsServer) error {
 			}
 			acc.AddFields("uwsgi_apps", fields, tags)
 		}
+	}
+
+	return nil
+}
+
+func (u *Uwsgi) gatherCores(acc telegraf.Accumulator, s *StatsServer) error {
+	for _, w := range s.Workers {
+		for _, c := range w.Cores {
+			fields := map[string]interface{}{
+				"requests":           c.Requests,
+				"static_requests":    c.StaticRequests,
+				"routed_requests":    c.RoutedRequests,
+				"offloaded_requests": c.OffloadedRequests,
+				"write_errors":       c.WriteErrors,
+				"read_errors":        c.ReadErrors,
+				"in_request":         c.InRequest,
+			}
+			tags := map[string]string{
+				"core_id":   strconv.Itoa(c.CoreId),
+				"worker_id": strconv.Itoa(w.WorkerId),
+			}
+			acc.AddFields("uwsgi_cores", fields, tags)
+		}
+
 	}
 
 	return nil
