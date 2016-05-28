c31df3583e55ff3596c0408f1e7b79f62d1b5553
Rickard von Essen <rickard.von.essen@gmail.com>
2016-05-29 00:12:18 +0200

    Updated the README to confrom with the guidelines and added FreeBSD info

--- plugins/inputs/zfs/zfs.go
+++ plugins/inputs/zfs/zfs.go
@@ -14,14 +14,14 @@ type Zfs struct {
 var sampleConfig = `
   ## ZFS kstat path. Ignored on FreeBSD
   ## If not specified, then default is:
-  kstatPath = "/proc/spl/kstat/zfs"
+  # kstatPath = "/proc/spl/kstat/zfs"
 
   ## By default, telegraf gather all zfs stats
   ## If not specified, then default is:
-  kstatMetrics = ["arcstats", "zfetchstats", "vdev_cache_stats"]
+  # kstatMetrics = ["arcstats", "zfetchstats", "vdev_cache_stats"]
 
   ## By default, don't gather zpool stats
-  poolMetrics = false
+  # poolMetrics = false
 `
 
 func (z *Zfs) SampleConfig() string {
