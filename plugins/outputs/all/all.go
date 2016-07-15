84806cb7c9b9213c53f7c241245fc6688fdf815d
Zach Dexter <zach.dexter@gmail.com>
2016-07-15 11:26:58 -0400

    Add Pusher output to all.go

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -18,4 +18,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/outputs/opentsdb"
 	_ "github.com/influxdata/telegraf/plugins/outputs/prometheus_client"
 	_ "github.com/influxdata/telegraf/plugins/outputs/riemann"
+	_ "github.com/influxdata/telegraf/plugins/outputs/pusher"
 )
