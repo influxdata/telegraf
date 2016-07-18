101dbd22da730e8be686ce972807c7dcb3771334
Dennis Bellinger <dennis.bellinger@gmail.com>
2016-07-18 18:11:10 -0400

    Merged service.go into telegraf.go
    Due to compilation issues on Windows, moved the code from service.go
    into telegraf.go and removed service.go entirely.

--- cmd/telegraf/telegraf.go
+++ cmd/telegraf/telegraf.go
@@ -104,6 +104,15 @@ Examples:
   telegraf -config telegraf.conf -input-filter cpu:mem -output-filter influxdb
 `
 
+var logger service.Logger
+
+var stop chan struct{}
+
+var srvc service.Service
+
+type program struct{}
+
+
 func reloadLoop(stop chan struct{}, s service.Service) {
 	defer func() {
 		if service.Interactive() {
@@ -300,4 +309,41 @@ func reloadLoop(stop chan struct{}, s service.Service) {
 func usageExit(rc int) {
 	fmt.Println(usage)
 	os.Exit(rc)
+}
+
+func (p *program) Start(s service.Service) error {
+	srvc = s
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	stop = make(chan struct{})
+	reloadLoop(stop, srvc)
+}
+func (p *program) Stop(s service.Service) error {
+	close(stop)
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "telegraf",
+		DisplayName: "Telegraf Data Collector Service",
+		Description: "Collects data using a series of plugins and publishes it to" +
+			"another series of plugins.",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
 }
