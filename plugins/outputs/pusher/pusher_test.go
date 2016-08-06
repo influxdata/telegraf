654484756ca2edc7b4147b3ad5a6807ba23d0c8c
Zach Dexter <zach.dexter@gmail.com>
2016-08-06 14:08:28 -0400

    Add test for Pusher output.

---
+++ plugins/outputs/pusher/pusher_test.go
@@ -0,0 +1,40 @@
+package pusher
+
+import (
+    "fmt"
+    "testing"
+    "github.com/stretchr/testify/require"
+    "net/http"
+    "net/http/httptest"
+    "github.com/influxdata/telegraf/testutil"
+    "github.com/influxdata/telegraf/plugins/serializers"
+)
+
+func TestWrite(t *testing.T) {
+    // Set up test server
+    server := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+        res.WriteHeader(http.StatusOK)
+        res.Header().Set("Content-Type", "application/json")
+        fmt.Fprintln(res, `{"results":[{}]}`)
+    }))
+    defer server.Close()
+
+    s, _ := serializers.NewJsonSerializer()
+    p := &Pusher{
+        AppId: "test_app",
+        AppKey: "test_key",
+        AppSecret: "test_secret",
+        ChannelName: "test_channel",
+        Host: server.Listener.Addr().String(),
+        Secure: false,
+        serializer: s,
+    }
+    err := p.Connect()
+    require.NoError(t, err)
+
+    metrics := testutil.MockMetrics()
+
+    err = p.Write(metrics)
+    require.NoError(t, err)
+
+}
