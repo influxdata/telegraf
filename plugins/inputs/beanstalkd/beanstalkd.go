79e3aff4ababce3a432e4c20cc844bdf5f0ab6e0
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-10-18 15:04:41 -0200

    beanstalkd

---
+++ plugins/inputs/beanstalkd/beanstalkd.go
@@ -0,0 +1,193 @@
+package beanstalkd
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal/errchan"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+// Beanstalkd is a Beanstalkd plugin
+type Beanstalkd struct {
+	Servers     []string
+}
+
+var sampleConfig = `
+  ## An array of address to gather stats about. Specify an ip on hostname
+  ## with optional port. ie localhost, 10.0.0.1:11300, etc.
+  servers = ["localhost:11300"]
+`
+
+var defaultTimeout = 5 * time.Second
+
+// The list of metrics that should be sent
+var sendMetrics = []string{
+	"current-jobs-urgent",
+	"current-jobs-ready",
+	"current-jobs-reserved",
+	"current-jobs-delayed",
+	"current-jobs-buried",
+	"cmd-put",
+	"cmd-peek",
+	"cmd-peek-ready",
+	"cmd-peek-delayed",
+	"cmd-peek-buried",
+	"cmd-reserve",
+	"cmd-reserve-with-timeout",
+	"cmd-delete",
+	"cmd-release",
+	"cmd-use",
+	"cmd-watch",
+	"cmd-ignore",
+	"cmd-bury",
+	"cmd-kick",
+	"cmd-touch",
+	"cmd-stats",
+	"cmd-stats-job",
+	"cmd-stats-tube",
+	"cmd-list-tubes",
+	"cmd-list-tube-used",
+	"cmd-list-tubes-watched",
+	"cmd-pause-tube",
+	"job-timeouts",
+	"total-jobs",
+	"current-tubes",
+	"current-connections",
+	"current-producers",
+	"current-workers",
+	"current-waiting",
+	"total-connections",
+	"uptime",
+	"binlog-oldest-index",
+	"binlog-current-index",
+	"binlog-records-migrated",
+	"binlog-records-written",
+	"binlog-max-size",
+}
+
+// SampleConfig returns sample configuration message
+func (m *Beanstalkd) SampleConfig() string {
+	return sampleConfig
+}
+
+// Description returns description of Beanstalkd plugin
+func (m *Beanstalkd) Description() string {
+	return "Read metrics from one or many Beanstalkd servers"
+}
+
+// Gather reads stats from all configured servers accumulates stats
+func (m *Beanstalkd) Gather(acc telegraf.Accumulator) error {
+	if len(m.Servers) == 0 {
+		return m.gatherServer(":11300", acc)
+	}
+
+	errChan := errchan.New(len(m.Servers))
+	for _, serverAddress := range m.Servers {
+		errChan.C <- m.gatherServer(serverAddress, acc)
+	}
+
+	return errChan.Error()
+}
+
+func (m *Beanstalkd) gatherServer(
+	address string,
+	acc telegraf.Accumulator,
+) error {
+	var conn net.Conn
+	var err error
+	_, _, err = net.SplitHostPort(address)
+	if err != nil {
+		address = address + ":11300"
+	}
+
+	conn, err = net.DialTimeout("tcp", address, defaultTimeout)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if conn == nil {
+		return fmt.Errorf("Failed to create net connection")
+	}
+
+	// Extend connection
+	conn.SetDeadline(time.Now().Add(defaultTimeout))
+
+	// Read and write buffer
+	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+
+	// Send command
+	if _, err := fmt.Fprint(rw, "stats\r\n"); err != nil {
+		return err
+	}
+	if err := rw.Flush(); err != nil {
+		return err
+	}
+
+	values, err := parseResponse(rw.Reader)
+	if err != nil {
+		return err
+	}
+
+	// Add server address as a tag
+	tags := map[string]string{"server": address}
+
+	// Process values
+	fields := make(map[string]interface{})
+	for _, key := range sendMetrics {
+		if value, ok := values[key]; ok {
+			// Mostly it is the number
+			if iValue, errParse := strconv.ParseInt(value, 10, 64); errParse == nil {
+				fields[key] = iValue
+			} else {
+				fields[key] = value
+			}
+		}
+	}
+	acc.AddFields("beanstalkd", fields, tags)
+	return nil
+}
+
+func parseResponse(r *bufio.Reader) (map[string]string, error) {
+	values := make(map[string]string)
+
+	for {
+		// Read line
+		line, _, errRead := r.ReadLine()
+		if errRead != nil {
+			return values, errRead
+		}
+		// Done
+		if bytes.Equal(line, []byte("")) {
+			break
+		}
+
+		// Read values
+		s := bytes.SplitN(line, []byte(": "), 2)
+
+		if len(s) != 2 {
+			s := bytes.SplitN(line, []byte(" "), 2)
+			if bytes.Equal(s[0], []byte("---")) || bytes.Equal(s[0], []byte("OK")) {
+				continue
+			} else {
+				return values, fmt.Errorf("unexpected line in stats response: %q", line)
+			}
+		}
+
+		// Save values
+		values[string(s[0])] = string(s[1])
+	}
+	return values, nil
+}
+
+func init() {
+	inputs.Add("beanstalkd", func() telegraf.Input {
+		return &Beanstalkd{}
+	})
+}
---
+++ plugins/inputs/beanstalkd/beanstalkd_test.go
@@ -0,0 +1,162 @@
+package beanstalkd
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/influxdata/telegraf/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBeanstalkdGeneratesMetrics(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	m := &Beanstalkd{
+		Servers: []string{testutil.GetLocalHost()},
+	}
+
+	var acc testutil.Accumulator
+
+	err := m.Gather(&acc)
+	require.NoError(t, err)
+
+	intMetrics := []string{"current-jobs-urgent", "current-jobs-ready", "current-jobs-reserved",
+		"current-jobs-delayed", "current-jobs-buried", "cmd-put", "cmd-peek", "cmd-peek-ready",
+		"cmd-peek-delayed", "cmd-peek-buried", "cmd-reserve", "cmd-reserve-with-timeout",
+		"cmd-delete", "cmd-release", "cmd-use", "cmd-watch", "cmd-ignore",
+		"cmd-bury", "cmd-kick", "cmd-touch", "cmd-stats", "cmd-stats-job",
+		"cmd-stats-tube", "cmd-list-tubes", "cmd-list-tube-used", "cmd-list-tubes-watched",
+		"cmd-pause-tube", "job-timeouts", "total-jobs", "max-job-size",
+		"current-tubes", "current-connections", "current-producers",
+		"current-workers", "current-waiting", "total-connections", "pid", "uptime",
+		"binlog-oldest-index", "binlog-current-index", "binlog-records-migrated",
+		"binlog-records-written", "binlog-max-size"
+	}
+
+	for _, metric := range intMetrics {
+		assert.True(t, acc.HasIntField("beanstalkd", metric), metric)
+	}
+}
+
+func TestBeanstalkdParseMetrics(t *testing.T) {
+	r := bufio.NewReader(strings.NewReader(beanstalkdStats))
+	values, err := parseResponse(r)
+	require.NoError(t, err, "Error parsing beanstalkd response")
+
+	tests := []struct {
+		key   string
+		value string
+	}{
+		{"current-jobs-urgent", "0"},
+		{"current-jobs-ready", "0"},
+		{"current-jobs-reserved", "0"},
+		{"current-jobs-delayed", "0"},
+		{"current-jobs-buried", "0"},
+		{"cmd-put", "613"},
+		{"cmd-peek", "0"},
+		{"cmd-peek-ready", "0"},
+		{"cmd-peek-delayed", "0"},
+		{"cmd-peek-buried", "0"},
+		{"cmd-reserve", "805"},
+		{"cmd-reserve-with-timeout", "0"},
+		{"cmd-delete", "613"},
+		{"cmd-release", "0"},
+		{"cmd-use", "613"},
+		{"cmd-watch", "805"},
+		{"cmd-ignore", "805"},
+		{"cmd-bury", "0"},
+		{"cmd-kick", "0"},
+		{"cmd-touch", "0"},
+		{"cmd-stats", "5"},
+		{"cmd-stats-job", "0"},
+		{"cmd-stats-tube", "0"},
+		{"cmd-list-tubes", "0"},
+		{"cmd-list-tube-used", "0"},
+		{"cmd-list-tubes-watched", "0"},
+		{"cmd-pause-tube", "0"},
+		{"job-timeouts", "0"},
+		{"total-jobs", "613"},
+		{"max-job-size", "65535"},
+		{"current-tubes", "1"},
+		{"current-connections", "1"},
+		{"current-producers", "0"},
+		{"current-workers", "0"},
+		{"current-waiting", "0"},
+		{"total-connections", "809"},
+		{"pid", "1363"},
+		{"uptime", "8007"},
+		{"binlog-oldest-index", "10"},
+		{"binlog-current-index", "10"},
+		{"binlog-records-migrated", "0"},
+		{"binlog-records-written", "1226"},
+		{"binlog-max-size", "10485760"},
+	}
+
+	for _, test := range tests {
+		value, ok := values[test.key]
+		if !ok {
+			t.Errorf("Did not find key for metric %s in values", test.key)
+			continue
+		}
+		if value != test.value {
+			t.Errorf("Metric: %s, Expected: %s, actual: %s",
+				test.key, test.value, value)
+		}
+	}
+}
+
+var beanstalkdStats = `---
+current-jobs-urgent: 0
+current-jobs-ready: 0
+current-jobs-reserved: 0
+current-jobs-delayed: 0
+current-jobs-buried: 0
+cmd-put: 613
+cmd-peek: 0
+cmd-peek-ready: 0
+cmd-peek-delayed: 0
+cmd-peek-buried: 0
+cmd-reserve: 805
+cmd-reserve-with-timeout: 0
+cmd-delete: 613
+cmd-release: 0
+cmd-use: 613
+cmd-watch: 805
+cmd-ignore: 805
+cmd-bury: 0
+cmd-kick: 0
+cmd-touch: 0
+cmd-stats: 1
+cmd-stats-job: 0
+cmd-stats-tube: 0
+cmd-list-tubes: 0
+cmd-list-tube-used: 0
+cmd-list-tubes-watched: 0
+cmd-pause-tube: 0
+job-timeouts: 0
+total-jobs: 613
+max-job-size: 65535
+current-tubes: 1
+current-connections: 1
+current-producers: 0
+current-workers: 0
+current-waiting: 0
+total-connections: 806
+pid: 1363
+version: 1.10
+rusage-utime: 0.060000
+rusage-stime: 0.212000
+uptime: 7754
+binlog-oldest-index: 10
+binlog-current-index: 10
+binlog-records-migrated: 0
+binlog-records-written: 1226
+binlog-max-size: 10485760
+id: 95dd7f24ee126dd6
+hostname: irrlab
+
+`
