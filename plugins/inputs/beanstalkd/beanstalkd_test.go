9f1a2f4402fd96b9a4b3284b3a2d964f73892477
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-10-18 16:16:07 -0200

    max-job-size and pid removed

--- plugins/inputs/beanstalkd/beanstalkd_test.go
+++ plugins/inputs/beanstalkd/beanstalkd_test.go
@@ -30,9 +30,9 @@ func TestBeanstalkdGeneratesMetrics(t *testing.T) {
 		"cmd-delete", "cmd-release", "cmd-use", "cmd-watch", "cmd-ignore",
 		"cmd-bury", "cmd-kick", "cmd-touch", "cmd-stats", "cmd-stats-job",
 		"cmd-stats-tube", "cmd-list-tubes", "cmd-list-tube-used", "cmd-list-tubes-watched",
-		"cmd-pause-tube", "job-timeouts", "total-jobs", "max-job-size",
+		"cmd-pause-tube", "job-timeouts", "total-jobs",
 		"current-tubes", "current-connections", "current-producers",
-		"current-workers", "current-waiting", "total-connections", "pid", "uptime",
+		"current-workers", "current-waiting", "total-connections", "uptime",
 		"binlog-oldest-index", "binlog-current-index", "binlog-records-migrated",
 		"binlog-records-written", "binlog-max-size",
 	}
@@ -80,14 +80,12 @@ func TestBeanstalkdParseMetrics(t *testing.T) {
 		{"cmd-pause-tube", "0"},
 		{"job-timeouts", "0"},
 		{"total-jobs", "613"},
-		{"max-job-size", "65535"},
 		{"current-tubes", "1"},
 		{"current-connections", "1"},
 		{"current-producers", "0"},
 		{"current-workers", "0"},
 		{"current-waiting", "0"},
 		{"total-connections", "809"},
-		{"pid", "1363"},
 		{"uptime", "8007"},
 		{"binlog-oldest-index", "10"},
 		{"binlog-current-index", "10"},
@@ -139,14 +137,12 @@ cmd-list-tubes-watched: 0
 cmd-pause-tube: 0
 job-timeouts: 0
 total-jobs: 613
-max-job-size: 65535
 current-tubes: 1
 current-connections: 1
 current-producers: 0
 current-workers: 0
 current-waiting: 0
 total-connections: 806
-pid: 1363
 version: 1.10
 rusage-utime: 0.060000
 rusage-stime: 0.212000
