c658592bf8ab7bd79551d4f46c3b3e43dc05fadd
Krishnanand Thommandra <kthommandra@arista.com>
2016-10-31 12:12:26 -0700

    Make telegraf depend on IP/hostname availability
    If systemd-networkd is used then telegraf is enabled
    at boot up time and it relies on network-online.target for DHCP
    completion. If systemd-networkd is not being used then we assume
    dhclient is being used. On such systems telegraf is disabled upon
    boot and is started by dhclient-up-hooks
    
    This fix ensures that on systemd enabled systems, telegraf will
    always start with proper hostname and never 'localhost'.
    
    This fix does not handle hostname lost post-boot.

--- Arista/post_install_config.sh
+++ Arista/post_install_config.sh
@@ -1,4 +1,52 @@
 #!/usr/bin/bash
 
-systemctl enable telegraf
-systemctl restart telegraf
+# Issue:
+# Telegraf uses hostname for every point writtern to InfluxDB
+# If Telegraf is started before hostname is setup via DHCP and the 
+# network is UP then all points get written with host tag = 'localhost'
+# Telegraf isn't restarted when the hostname gets setup after DHCP interaction completes.
+# This issue happens only on systemd enabled systems.
+
+# On systems with dhclient, telegraf is disabled at boot up and started 
+#   only after dhclient has configured the hostname, IP etc using dhclient hooks
+#   NOTE: a4 workspaces also use dhclient so we can't do simple process check
+#   so we use lack of systemd-networkd to assume that dhclient is being used to manage DHCP
+#   properties
+# On systems with networkd, telegraf uses network-online systemd target
+
+TRUE="0"
+SYSTEMD_INUSE=`systemctl -a --no-pager > /dev/null;  echo $?`
+SYSTEMD_NETWORKD_INUSE=`systemctl status systemd-networkd | grep 'Active' | grep running  > /dev/null; echo $?`
+TELEGRAF_IN_DHCLIENT_HOOKS=`grep telegraf /etc/dhcp/dhclient-up-hooks > /dev/null 2>&1; echo $?`
+TELEGRAF_SERVICE=/usr/lib/systemd/system/telegraf.service
+
+if [[ "$SYSTEMD_INUSE" != "$TRUE" ]]; then
+	exit 0
+fi
+
+if [[ "$SYSTEMD_NETWORKD_INUSE" != "$TRUE" ]]; then
+
+  if [[ -f $TELEGRAF_SERVICE ]]; then
+  	systemctl disable telegraf
+  fi
+
+  cp /usr/lib/systemd/system/telegraf-dhclient.service /usr/lib/systemd/system/telegraf.service
+  systemctl daemon-reload
+  if [[ "$TELEGRAF_IN_DHCLIENT_HOOKS" != "$TRUE"  ]]
+  then
+  cat >> /etc/dhcp/dhclient-up-hooks <<EOF
+   echo "Starting telegraf from dhclient hook"; systemctl start telegraf
+EOF
+  fi
+else
+  cp /usr/lib/systemd/system/telegraf-networkd.service /usr/lib/systemd/system/telegraf.service
+  systemctl daemon-reload
+  systemctl enable telegraf
+fi
+
+# restart telegraf only if hostname is not localhost
+MYHOST=`hostname -s`
+if [[ "$MYHOST" != "localhost" ]]
+then
+  systemctl restart telegraf
+fi
