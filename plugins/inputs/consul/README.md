764b93c3b6c90b39a91e005808d5d1ae7763ed8b
Łukasz Harasimowicz <dev@harnash.eu>
2016-11-24 18:53:36 +0100

    plugin: consul: added more statistics for server and services
    As requested in #913 consul plugin now returns some basic stats about
    the server (perrs, nodes, services, leadership). It is also possible to
    query all service health registered in the catalog but since it is quite
    slow it is set as optional.

--- plugins/inputs/consul/README.md
+++ plugins/inputs/consul/README.md
@@ -1,7 +1,13 @@
 # Telegraf Input Plugin: Consul
 
-This plugin will collect statistics about all helath checks registered in the Consul. It uses [Consul API](https://www.consul.io/docs/agent/http/health.html#health_state)
-to query the data. It will not report the [telemetry](https://www.consul.io/docs/agent/telemetry.html) but Consul can report those stats already using StatsD protocol if needed.
+This plugin will collect statistics about all helath checks registered in the Consul and some basic information about Consul status.
+It uses [Consul Health API](https://www.consul.io/docs/agent/http/health.html#health_state) and [Consul Catalog API] (https://www.consul.io/docs/agent/http/catalog.html)
+to query the data.
+
+It **will not** report the [telemetry](https://www.consul.io/docs/agent/telemetry.html) but Consul can report those stats already using StatsD protocol if needed.
+
+Additionaly it can be configured to gather service health checks (parameter: `service_health`) but since it may be time
+consuming it is disabled by default.
 
 ## Configuration:
 
@@ -21,11 +27,13 @@ to query the data. It will not report the [telemetry](https://www.consul.io/docs
   # password = ""
   ## Optional data centre to query the health checks from (default: "")
   # datacentre = ""
+  ## Optional should we gather service health checks (default: false)
+  # service_health = false
 ```
 
 ## Measurements:
 
-### Consul:
+### consul_health_checks:
 Tags:
 - node: on which node check/service is registered on
 - service_name: name of the service (this is the service name not the service ID)
@@ -36,11 +44,37 @@ Fields:
 - service_id
 - status
 
-## Example output
+### consul_server_stats
+Tags:
+  *None*
 
+Fields:
+- leader (0/1)
+- nodes
+- peers
+- services
+
+### consul_service_health
+Tags:
+- node
+- service_name
+
+Fields:
+- healthy (0/1)
+
+## Example output
 ```
 $ telegraf --config ./telegraf.conf -input-filter consul -test
 * Plugin: consul, Collection 1
 > consul_health_checks,host=wolfpit,node=consul-server-node check_id="serfHealth",check_name="Serf Health Status",service_id="",status="passing" 1464698464486439902
 > consul_health_checks,host=wolfpit,node=consul-server-node,service_name=www.example.com check_id="service:www-example-com.test01",check_name="Service 'www.example.com' check",service_id="www-example-com.test01",status="critical" 1464698464486519036
+> consul_server_stats,host=wolfpit leader=0,nodes=130,peers=5,services=84 1480009729000000000
+```
+
+and with `serivce_health` set to `true` additional measurements are provided:
+
+```
+> consul_service_health,host=wolfpit,node=dev-db-f1,service_name=db-dev-db healthy=1 1480009729000000000
+> consul_service_health,host=wolfpit,node=dev-db-f2,service_name=db-dev-db healthy=1 1480009729000000000
+> consul_service_health,host=wolfpit,node=dev-mesos-1,service_name=example healthy=1 1480009730000000000
 ```
--- plugins/inputs/consul/consul.go
+++ plugins/inputs/consul/consul.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/hashicorp/consul/api"
+	consulStruct "github.com/hashicorp/consul/consul/structs"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/inputs"
@@ -16,6 +17,8 @@ type Consul struct {
 	Username   string
 	Password   string
 	Datacentre string
+
+	CollectServiceHealth bool `toml:"service_health"`
 
 	// Path to CA file
 	SSLCA string `toml:"ssl_ca"`
@@ -44,6 +47,8 @@ var sampleConfig = `
   # password = ""
   ## Optional data centre to query the health checks from (default: "")
   # datacentre = ""
+  ## Optional should we gather service health checks (default: false)
+  # service_health = false
 `
 
 func (c *Consul) Description() string {
@@ -107,6 +112,94 @@ func (c *Consul) GatherHealthCheck(acc telegraf.Accumulator, checks []*api.Healt
 	}
 }
 
+func (c *Consul) GatherServiceHealth(services map[string][]string, acc telegraf.Accumulator) error {
+	for s := range services {
+		service, _, err := c.client.Health().Service(s, "", false, &api.QueryOptions{})
+
+		if err != nil {
+			return err
+		}
+
+		for _, elem := range service {
+			if len(elem.Checks) == 0 {
+				continue
+			}
+
+			record := make(map[string]interface{})
+			tags := make(map[string]string)
+
+			record["healthy"] = 1.0
+			tags["service_name"] = s
+
+			for _, check := range elem.Checks {
+				if len(tags["node"]) == 0 {
+					tags["node"] = check.Node
+				}
+				if check.Status != consulStruct.HealthPassing {
+					record["healthy"] = 0.0
+					break
+				}
+			}
+
+			acc.AddFields("consul_service_health", record, tags)
+		}
+	}
+
+	return nil
+}
+
+func (c *Consul) GatherServerStats(acc telegraf.Accumulator) error {
+	peers, err := c.client.Status().Peers()
+
+	if err != nil {
+		return err
+	}
+
+	record := make(map[string]interface{})
+
+	record["peers"] = float64(len(peers))
+
+	leader, err := c.client.Status().Leader()
+
+	if err != nil {
+		return err
+	}
+
+	if len(leader) == 0 {
+		record["leader"] = 1.0
+	} else {
+		record["leader"] = 0.0
+	}
+
+	nodes, _, err := c.client.Catalog().Nodes(&api.QueryOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	record["nodes"] = float64(len(nodes))
+
+	services, _, err := c.client.Catalog().Services(&api.QueryOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	record["services"] = float64(len(services))
+
+	acc.AddFields("consul_server_stats", record, map[string]string{})
+
+	if c.CollectServiceHealth {
+		err = c.GatherServiceHealth(services, acc)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (c *Consul) Gather(acc telegraf.Accumulator) error {
 	if c.client == nil {
 		newClient, err := c.createAPIClient()
@@ -125,6 +218,12 @@ func (c *Consul) Gather(acc telegraf.Accumulator) error {
 	}
 
 	c.GatherHealthCheck(acc, checks)
+
+	err = c.GatherServerStats(acc)
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
