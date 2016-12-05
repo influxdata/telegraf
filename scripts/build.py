1d1afe6481d4e3dee83891bc8eedbd401d1a0110
Pierre Fersing <pierref@pierref.org>
2016-12-05 17:45:02 +0100

    Fix RPM architecture for armhf (#2003)
    Signed-off-by: Pierre Fersing <pierre.fersing@bleemeo.com>

--- scripts/build.py
+++ scripts/build.py
@@ -583,6 +583,8 @@ def package(build_output, pkg_name, version, nightly=False, iteration=1, static=
                     if "static_" in arch:
                         # Remove the "static_" from the displayed arch on the package
                         package_arch = arch.replace("static_", "")
+                    elif package_type == "rpm" and arch == 'armhf':
+                        package_arch = 'armv6hl'
                     else:
                         package_arch = arch
                     if not release and not nightly:
