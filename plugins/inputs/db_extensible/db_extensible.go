24b74dc5e0b6bbf005dd5883aec436c374559f90
Thomas  MENARD <thomas.menard@pasteur.fr>
2017-01-18 11:15:32 +0100

    Update import package
    add blank identifier for db drivers

--- plugins/inputs/db_extensible/db_extensible.go
+++ plugins/inputs/db_extensible/db_extensible.go
@@ -11,8 +11,8 @@ import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 
-	"github.com/lib/pq"
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 
