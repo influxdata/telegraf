c1e87a414be8fe4319cefe5953b8707781d28bc7
Patrick Hemmer <phemmer@chewy.com>
2017-03-20 19:04:27 -0400

    add postfix plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -61,6 +61,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/passenger"
 	_ "github.com/influxdata/telegraf/plugins/inputs/phpfpm"
 	_ "github.com/influxdata/telegraf/plugins/inputs/ping"
+	_ "github.com/influxdata/telegraf/plugins/inputs/postfix"
 	_ "github.com/influxdata/telegraf/plugins/inputs/postgresql"
 	_ "github.com/influxdata/telegraf/plugins/inputs/postgresql_extensible"
 	_ "github.com/influxdata/telegraf/plugins/inputs/powerdns"
