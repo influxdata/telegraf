ec41ae62f2bebb227e1074234b99bfb8bcd548c0
Manoj Kumar <manojm321@arista.com>
2017-03-24 16:17:51 -0700

    Reverting un-needed changes

--- Arista/post_install_config.sh
+++ Arista/post_install_config.sh
@@ -28,13 +28,12 @@ if [[ "$SYSTEMD_NETWORKD_INUSE" != "$TRUE" ]]; then
 
   if [[ -f $TELEGRAF_SERVICE ]]; then
   	systemctl disable telegraf
-
   fi
   cp /usr/lib/systemd/system/telegraf-dhclient.service /usr/lib/systemd/system/telegraf.service
   systemctl daemon-reload
   if [[ "$TELEGRAF_IN_DHCLIENT_HOOKS" != "$TRUE"  ]]
   then
-   cat >> /etc/dhcp/dhclient-up-hooks <<EOF
+  cat >> /etc/dhcp/dhclient-up-hooks <<EOF
    echo "Starting telegraf from dhclient hook"; systemctl start telegraf
 EOF
   chmod a+x /etc/dhcp/dhclient-up-hooks
