0c2f633209e445799e8ccd812c4c2aabbbc450e7
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-04-03 10:30:53 -0400

    Add input plugin for OpenLDAP

---
+++ plugins/inputs/openldap/README.md
@@ -0,0 +1,76 @@
+# Openldap Input Plugin
+
+This plugin gathers metrics from OpenLDAP's cn=Monitor backend.
+
+### Configuration:
+
+```toml
+# Description
+[[inputs.openldap]]
+  host = "localhost"
+  port = 389
+  # starttls. Default is false.
+  tls = false
+  # skip peer certificate verification. Default is false.
+  tls_skipverify = false
+
+  # dn/password to bind with. If bind_dn is empty, an anonymous bind is performed.
+  bind_dn = ""
+  bind_password = ""
+```
+
+### Measurements & Fields:
+
+All **monitorCounter**, **monitorOpInitiated**, and **monitorOpCompleted** attributes are gathered based on this LDAP query:
+
+```(|(objectClass=monitorCounterObject)(objectClass=monitorOperation))```
+
+Metric names are based on their entry DN.
+
+Metrics for the **monitorOp*** attributes have **_initiated** and **_completed** added to the base name.
+
+An OpenLDAP 2.4 server will provide these metrics:
+
+- openldap
+	- max_file_descriptors_connections
+	- current_connections
+	- total_connections
+	- abandon_operations_completed
+	- abandon_operations_initiated
+	- add_operations_completed
+	- add_operations_initiated
+	- bind_operations_completed
+	- bind_operations_initiated
+	- compare_operations_completed
+	- compare_operations_initiated
+	- delete_operations_completed
+	- delete_operations_initiated
+	- extended_operations_completed
+	- extended_operations_initiated
+	- modify_operations_completed
+	- modify_operations_initiated
+	- modrdn_operations_completed
+	- modrdn_operations_initiated
+	- search_operations_completed
+	- search_operations_initiated
+	- unbind_operations_completed
+	- unbind_operations_initiated
+	- bytes_statistics
+	- entries_statistics
+	- pdu_statistics
+	- referrals_statistics
+	- read_waiters
+	- write_waiters
+
+### Tags:
+
+- server= # value from config
+- port= # value from config
+    
+### Example Output:
+
+```
+$ telegraf -config telegraf.conf -input-filter openldap -test --debug
+* Plugin: inputs.openldap, Collection 1
+> openldap,port=389,host=localhost,server=localhost abandon_operations_initiated=4,extended_operations_completed=125963,bytes_statistics=595939321,pdu_statistics=17028251,modify_operations_initiated=0,delete_operations_completed=0,compare_operations_completed=0,max_file_descriptors_connections=4096,unbind_operations_completed=7981688,extended_operations_initiated=125963,referrals_statistics=0,modify_operations_completed=0,delete_operations_initiated=0,bind_operations_completed=8115329,search_operations_completed=4385841,add_operations_completed=0,abandon_operations_completed=4,write_waiters=0,bind_operations_initiated=8115329,modrdn_operations_initiated=0,compare_operations_initiated=0,entries_statistics=4401128,read_waiters=1,current_connections=3,search_operations_initiated=4385842,modrdn_operations_completed=0,add_operations_initiated=0,total_connections=8147531,unbind_operations_initiated=7981688 1491189665000000000
+```
---
+++ plugins/inputs/openldap/openldap.go
@@ -0,0 +1,143 @@
+package openldap
+
+import (
+	"fmt"
+	"gopkg.in/ldap.v2"
+	"strings"
+	"strconv"
+	"crypto/tls"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+type Openldap struct {
+	Host string
+	Port int
+	Tls bool
+	TlsSkipverify bool
+	BindDn string
+	BindPassword string
+}
+
+const sampleConfig string = `
+  host = "localhost"
+  port = 389
+  # starttls. Default is false.
+  tls = false
+  # skip peer certificate verification. Default is false.
+  tls_skipverify = false
+
+  # dn/password to bind with. If bind_dn is empty, an anonymous bind is performed.
+  bind_dn = ""
+  bind_password = ""
+`
+
+var searchBase = "cn=Monitor"
+var searchFilter = "(|(objectClass=monitorCounterObject)(objectClass=monitorOperation))"
+var searchAttrs = []string{"monitorCounter", "monitorOpInitiated", "monitorOpCompleted"}
+var attrTranslate = map[string]string{
+	"monitorCounter": "",
+	"monitorOpInitiated": "_initiated",
+	"monitorOpCompleted": "_completed",
+}
+
+func (o *Openldap) SampleConfig() string {
+	return sampleConfig
+}
+
+func (o *Openldap) Description() string {
+	return "OpenLDAP cn=Monitor plugin"
+}
+
+// return an initialized Openldap
+func NewOpenldap() *Openldap {
+	return &Openldap{
+		Host: "localhost",
+		Port: 389,
+		Tls: false,
+		TlsSkipverify: false,
+		BindDn: "",
+		BindPassword: "",
+	}
+}
+
+// gather metrics
+func (o *Openldap) Gather(acc telegraf.Accumulator) error {
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", o.Host, o.Port))
+	defer l.Close()
+	if err != nil {
+		acc.AddError(err)
+		return nil
+	}
+
+	// TLS
+	if o.Tls {
+		err = l.StartTLS(&tls.Config{InsecureSkipVerify: o.TlsSkipverify})
+		if err != nil {
+			acc.AddError(err)
+			return nil
+		}
+	}
+
+	// username/password bind
+	if o.BindDn != "" && o.BindPassword != "" {
+		err = l.Bind(o.BindDn, o.BindPassword)
+		if err != nil {
+			acc.AddError(err)
+			return nil
+		}
+	}
+
+	searchRequest := ldap.NewSearchRequest(
+		searchBase,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		searchFilter,
+		searchAttrs,
+		nil,
+	)
+
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		acc.AddError(err)
+		return nil
+	}
+
+	fields := map[string]interface{}{}
+	tags := map[string]string{
+		"server": o.Host,
+		"port": strconv.Itoa(o.Port),
+	}
+	for _, entry := range sr.Entries {
+		metricName := dnToMetric(entry.DN, searchBase)
+		for _, attr := range entry.Attributes {
+			if len(attr.Values[0]) >= 1 {
+				if v, err := strconv.ParseFloat(attr.Values[0], 64); err == nil {
+					fields[metricName + attrTranslate[attr.Name]] = v
+				}
+			}
+		}
+	}
+	acc.AddFields("openldap", fields, tags)
+
+	return nil
+}
+
+// Convert a DN to metric name, eg cn=Read,cn=Waiters,cn=Monitor to read_waiters
+func dnToMetric(dn, searchBase string) string {
+	metricName := strings.Trim(dn, " ")
+	metricName = strings.Replace(metricName, " ", "_", -1)
+	metricName = strings.ToLower(metricName)
+	metricName = strings.TrimPrefix(metricName, "cn=")
+	metricName = strings.Replace(metricName, strings.ToLower(searchBase), "", -1)
+	metricName = strings.Replace(metricName, "cn=", "_", -1)
+	return strings.Replace(metricName, ",", "", -1)
+}
+
+func init() {
+	inputs.Add("openldap", func() telegraf.Input { return NewOpenldap() })
+}
