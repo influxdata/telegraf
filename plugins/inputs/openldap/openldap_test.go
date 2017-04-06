3f78c5cf479f207dec1638b4e626804915e094bf
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-04-06 00:06:14 -0400

    openldap: add TestOpenldapMockResult(), put our common tests in runTests()

--- plugins/inputs/openldap/openldap_test.go
+++ plugins/inputs/openldap/openldap_test.go
@@ -2,11 +2,38 @@ package openldap
 
 import (
 	"testing"
+	"gopkg.in/ldap.v2"
 
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestOpenldapMockResult(t *testing.T) {
+	var acc testutil.Accumulator
+
+	mockSearchResult := ldap.SearchResult{
+		Entries: []*ldap.Entry{
+			{
+				DN: "cn=Total,cn=Connections,cn=Monitor",
+				Attributes: []*ldap.EntryAttribute{{Name: "monitorCounter", Values: []string{"1"}}},
+			},
+		},
+		Referrals: []string{},
+		Controls: []ldap.Control{},
+	}
+
+	o := &Openldap {
+		Host: "localhost",
+		Port: 389,
+	}
+
+	err := gatherSearchResult(&mockSearchResult, o, &acc)
+
+	require.NoError(t, err)
+	assert.NotZero(t, acc.NFields())
+	assert.True(t, acc.HasFloatField("openldap", "total_connections"))
+}
 
 func TestOpenldapNoConnection(t *testing.T) {
 	if testing.Short() {
@@ -14,14 +41,14 @@ func TestOpenldapNoConnection(t *testing.T) {
 	}
 
 	o := &Openldap {
-		Host: "127.0.0.1",
+		Host: testutil.GetLocalHost(),
 		Port: 389,
 	}
 
 	var acc testutil.Accumulator
 	err := o.Gather(&acc)
 	require.NoError(t, err) // test that we didn't return an error
-	assert.Zero(t, acc.NFields()) // test that we set an error
+	assert.Zero(t, acc.NFields()) // test that we didn't return any fields
 	assert.NotEmpty(t, acc.Errors) // test that we set an error
 }
 
@@ -36,8 +63,9 @@ func TestOpenldapGeneratesMetrics(t *testing.T) {
 
 	var acc testutil.Accumulator
 	err := o.Gather(&acc)
+
 	require.NoError(t, err)
-	// do more stuff
+	assert.Empty(t, acc.Errors)
 }
 
 func TestOpenldapStartTLS(t *testing.T) {
@@ -73,3 +101,9 @@ func TestOpenldapBind(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func runTests(t *testing.T, acc *testutil.Accumulator) {
+	assert.True(t, acc.HasMeasurement("openldap"))
+	assert.True(t, acc.HasTag("host", testutil.GetLocalHost()))
+	assert.True(t, acc.HasTag("port", "389"))
+	assert.NotZero(t, acc.NFields())
+}
