6c71de815b4304095c10b3d103654a654e6711be
DanKans <daniel@salbert.net>
2017-04-12 16:20:01 +0100

    Simplify error for slice comparision

--- plugins/inputs/fluentd/fluentd_test.go
+++ plugins/inputs/fluentd/fluentd_test.go
@@ -123,7 +123,7 @@ func Test_parse(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(pluginOutput, expectedOutput) {
-		t.Errorf("pluginOutput: expected\n %s, \nactual\n %s\n", pluginOutput, expectedOutput)
+        t.Errorf("pluginOutput is different from expectedOutput")
 	}
 
 }
