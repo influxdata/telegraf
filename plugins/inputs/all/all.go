41c1626ebc1593a00e045dc4cf6214d9fa2dc41f
mlabouardy <mohamed@labouardy.com>
2017-04-14 13:29:48 +0200

    traefik readme

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -76,6 +76,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/system"
 	_ "github.com/influxdata/telegraf/plugins/inputs/tail"
 	_ "github.com/influxdata/telegraf/plugins/inputs/tcp_listener"
+	_ "github.com/influxdata/telegraf/plugins/inputs/traefik"
 	_ "github.com/influxdata/telegraf/plugins/inputs/trig"
 	_ "github.com/influxdata/telegraf/plugins/inputs/twemproxy"
 	_ "github.com/influxdata/telegraf/plugins/inputs/udp_listener"
