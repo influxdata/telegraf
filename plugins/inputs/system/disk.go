776452e54f088d5b289625f67bcc7fcc1b1a81fb
Dark(김성진) <ekdxhrl@coupang.com>
2017-04-16 15:34:43 +0900

    Disk plugin expose inodes_used_percent.

--- plugins/inputs/system/disk.go
+++ plugins/inputs/system/disk.go
@@ -59,19 +59,24 @@ func (s *DiskStats) Gather(acc telegraf.Accumulator) error {
 			"fstype": du.Fstype,
 		}
 		var used_percent float64
-		if du.Used+du.Free > 0 {
-			used_percent = float64(du.Used) /
-				(float64(du.Used) + float64(du.Free)) * 100
+		if du.Total > 0 {
+			used_percent = float64(du.Used) / float64(du.Total) * 100
+		}
+
+		var inodes_used_percent float64
+		if du.InodesTotal > 0 {
+			inodes_used_percent = float64(du.InodesUsed) / float64(du.InodesTotal) * 100
 		}
 
 		fields := map[string]interface{}{
-			"total":        du.Total,
-			"free":         du.Free,
-			"used":         du.Used,
-			"used_percent": used_percent,
-			"inodes_total": du.InodesTotal,
-			"inodes_free":  du.InodesFree,
-			"inodes_used":  du.InodesUsed,
+			"total":               du.Total,
+			"free":                du.Free,
+			"used":                du.Used,
+			"used_percent":        used_percent,
+			"inodes_total":        du.InodesTotal,
+			"inodes_free":         du.InodesFree,
+			"inodes_used":         du.InodesUsed,
+			"inodes_used_percent": inodes_used_percent,
 		}
 		acc.AddGauge("disk", fields, tags)
 	}
