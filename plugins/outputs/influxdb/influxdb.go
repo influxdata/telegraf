bc474d3a53514d39f35242751b0b5dcf2cd08412
Daniel Nelson <daniel.nelson@influxdb.com>
2017-04-28 13:46:23 -0700

    Clarify retention policy option for influxdb output
    closes #2696

--- plugins/outputs/influxdb/influxdb.go
+++ plugins/outputs/influxdb/influxdb.go
@@ -55,7 +55,8 @@ var sampleConfig = `
   ## The target database for metrics (telegraf will create it if not exists).
   database = "telegraf" # required
 
-  ## Retention policy to write to. Empty string writes to the default rp.
+  ## Name of existing retention policy to write to.  Empty string writes to
+  ## the default retention policy.
   retention_policy = ""
   ## Write consistency (clusters only), can be: "any", "one", "quorum", "all"
   write_consistency = "any"
