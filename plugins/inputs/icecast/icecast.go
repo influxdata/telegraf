5c938557d9b2299847522f33a028a15c6d453b8b
Rogier van der Heide <r.vanderheide@wearetriple.com>
2017-05-08 12:06:35 +0200

    Fixed issues with bad URL's SEG faulting Added SSL support Added option to have multiple URL's using the same credentials

--- plugins/inputs/icecast/icecast.go
+++ plugins/inputs/icecast/icecast.go
@@ -3,27 +3,16 @@ package icecast
 import (
 	"fmt"
 	"net/http"
-	"encoding/xml"
-	"io/ioutil"
+	"net/url"
 	"strings"
 	"time"
+  "encoding/xml"
+	"io/ioutil"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
-	"github.com/influxdata/telegraf/internal/errchan"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
-
-// Icecast is an icecast plugin
-type Icecast struct {
-	Host						string
-	ResponseTimeout internal.Duration
-	Username				string
-	Password				string
-	Alias						string
-	Slash						bool
-}
-
 // SourceListmounts holds single listener elements from the Icecast XML
 type SourceListmounts struct {
 	Mount					string		`xml:"mount,attr"`
@@ -37,109 +26,167 @@ type Listmounts struct {
 	Sources []SourceListmounts `xml:"source"`
 }
 
-var sampleConfig = `
-  ## Specify the IP adress to where the 'admin/listmounts' can be found. You can include port if needed.
-  host = "localhost"
+type Icecast struct {
+	Urls            []string
+	Username        string
+	Password        string
+	ResponseTimeout internal.Duration
+	Slash						bool
+	// Path to CA file
+	SSLCA string `toml:"ssl_ca"`
+	// Path to host cert file
+	SSLCert string `toml:"ssl_cert"`
+	// Path to cert key file
+	SSLKey string `toml:"ssl_key"`
+	// Use SSL but skip chain & host verification
+	InsecureSkipVerify bool
+}
 
-	## Timeout to the complete conection and reponse time in seconds. Default (5 seconds)
-  response_timeout = "25s"
+var sampleConfig = `
+  ## Specify the IP adress to where the '/admin/listmounts' can be found. You can include port if needed.
+  ## If you'd like to report under an alias, use ; (e.g. https://localhost;Server 1)
+  ## You can use multiple hosts who use the same login credentials by dividing with , (e.g. "http://localhost","https://127.0.0.1")
+  urls = ["http://localhost"]
+
+  ## Timeout to the complete conection and reponse time in seconds. Default (5 seconds)
+  # response_timeout = "25s"
 
   ## The username/password combination needed to read the listmounts page.
   ## These must be equal to the admin login details specified in your Icecast configuration
   username = "admin"
   password = "hackme"
 
-  ## If you wish your host name to be different then the one specified under host, you can change it here
-  alias = ""
+  ## Include the slash in mountpoint names or not
+  slash = false
 
-	## Include the slash in mountpoint names or not
-	slash = false
+  ## Optional SSL Config
+  # ssl_ca = "/etc/telegraf/ca.pem"
+  # ssl_cert = "/etc/telegraf/cert.pem"
+  # ssl_key = "/etc/telegraf/key.pem"
+  ## Use SSL but skip chain & host verification
+  # insecure_skip_verify = false
 `
 
-// Description returns description of Icecast plugin
-func (ice *Icecast) Description() string {
-	return "Read listeners from an Icecast instance per mount"
-}
-
-// The list of metrics that should be sent
-var sendMetrics = []string{
-	"listeners",
-	"host",
-	"mount",
-}
-
-// SampleConfig returns sample configuration message
-func (ice *Icecast) SampleConfig() string {
+func (n *Icecast) SampleConfig() string {
 	return sampleConfig
 }
 
-// Gather reads stats from all configured servers mount stats
-func (ice *Icecast) Gather(acc telegraf.Accumulator) error {
-	if ice.ResponseTimeout.Duration < time.Second {
-		ice.ResponseTimeout.Duration = time.Second * 5
-	}
-
-  errChan := errchan.New(len(ice.Host))
-
-  // Check to see if the needed fields are filled in, and if so, connect.
-  if len(ice.Host) != 0 && len(ice.Username) != 0 && len(ice.Password) != 0 {
-    errChan.C <- ice.gatherServer(ice.Host, ice.Username , ice.Password, ice.Alias, ice.Slash, ice.ResponseTimeout, acc)
-  }
-
-	return errChan.Error()
+func (n *Icecast) Description() string {
+	return "Read listeners from an Icecast instance per mount"
 }
 
-// Main gather function
-func (ice *Icecast) gatherServer(
-	host string,
-	username string,
-	password string,
-	alias string,
-	slash bool,
-	timeout time.Duration,
-	acc telegraf.Accumulator,
-) error {
-	var err error
-	var listmounts Listmounts
+func (n *Icecast) Gather(acc telegraf.Accumulator) error {
+	if len(n.Urls) == 0 {
+		n.Urls = []string{"http://localhost"}
+	}
+	if n.ResponseTimeout.Duration < time.Second {
+		n.ResponseTimeout.Duration = time.Second * 5
+	}
+
+	var outerr error
+	var errch = make(chan error)
+
+	for _, u := range n.Urls {
+    // Default admin listmounts page of Icecast
+		adminPageUrl := "/admin/listmounts"
+
+		// Check to see if there is an alias
+		var alias string
+		if strings.Contains(u, ";") {
+			urlAlais := strings.Split(u, ";")
+			alias = urlAlais[1]
+			u = urlAlais [0]
+		}
+
+    // Check to see if the user isn't adding a / at the end
+    if u[len(u)-1:] == "/" {
+      if last := len(u) - 1; last >= 0 && u[last] == '/' {
+        u = u[:last]
+      }
+    }
+
+    // Parsing the URL to see if it's ok
+		hosturl := u + adminPageUrl
+		addr, err := url.Parse(hosturl)
+		if err != nil {
+			return fmt.Errorf("Unable to parse address '%s': %s", u, err)
+		}
+
+		go func(addr *url.URL) {
+			errch <- n.gatherUrl(addr, u, alias, acc)
+		}(addr)
+	}
+
+	// Drain channel, waiting for all requests to finish and save last error.
+	for range n.Urls {
+		if err := <-errch; err != nil {
+			outerr = err
+		}
+	}
+
+	return outerr
+}
+
+func (n *Icecast) gatherUrl(
+  addr 		*url.URL,
+  host		string,
+  alias 	string,
+  acc 		telegraf.Accumulator,
+  ) error {
+	var tr *http.Transport
+  var listmounts Listmounts
 	var total int32
 
-	// Checking URL
-	u := fmt.Sprintf("http://%s/admin/listmounts", host)
-	addr, err := url.Parse(u)
+	if addr.Scheme == "https" {
+		tlsCfg, err := internal.GetTLSConfig(
+			n.SSLCert, n.SSLKey, n.SSLCA, n.InsecureSkipVerify)
+		if err != nil {
+			return err
+		}
+		tr = &http.Transport{
+			ResponseHeaderTimeout: time.Duration(3 * time.Second),
+			TLSClientConfig:       tlsCfg,
+		}
+	} else {
+		tr = &http.Transport{
+			ResponseHeaderTimeout: time.Duration(3 * time.Second),
+		}
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   n.ResponseTimeout.Duration,
+	}
+
+	req, err := http.NewRequest("GET", addr.String(), nil)
 	if err != nil {
-		return fmt.Errorf("Unable to parse address '%s': %s", u, err)
+		return fmt.Errorf("error on new request to %s : %s\n", addr.String(), err)
 	}
 
-  // Create HTTP client to fetch the listmounts stats
-  httpClientIcecast := &http.Client{
-			Timeout:   timeout,
+	if len(n.Username) != 0 && len(n.Password) != 0 {
+		req.SetBasicAuth(n.Username, n.Password)
 	}
-  req, err := http.NewRequest("GET", u, nil)
-  if err != nil {
-    fmt.Errorf("HTTP request error: %s", err)
-  }
-  req.SetBasicAuth(username, password)
 
-  // Starting the HTTP request
-  icecastResponse, err := httpClientIcecast.Do(req)
-  if icecastResponse == nil {
-    fmt.Errorf("No response: %s", err)
-  }
-  if err != nil {
-    fmt.Errorf("HTTP request error: %s", err)
-  }
-  defer icecastResponse.Body.Close()
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error on request to %s : %s\n", addr.String(), err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%s returned HTTP status %s", addr.String(), resp.Status)
+	}
 
   // Processing XML response
-  if body, err := ioutil.ReadAll(icecastResponse.Body); err == nil {
+  if body, err := ioutil.ReadAll(resp.Body); err == nil {
     if err := xml.Unmarshal(body, &listmounts); err != nil {
-      fmt.Errorf("XML error: %s", err)
+      return fmt.Errorf("XML error: %s", err)
     }
   } else {
-    fmt.Errorf("Read error: %s", err)
+    return fmt.Errorf("Read error: %s", err)
   }
 
-	// Settings alias as host if one is given
+	// Setting alias if availible
 	if len(alias) != 0 {
 		host = alias
 	}
@@ -148,7 +195,7 @@ func (ice *Icecast) gatherServer(
   for _, sources := range listmounts.Sources {
 		var mountname string
 
-		if slash == false {
+		if n.Slash == false {
 			mountname = strings.Trim(sources.Mount,"/")
 		} else {
 			mountname = sources.Mount
@@ -159,7 +206,7 @@ func (ice *Icecast) gatherServer(
 			"mount": mountname,
     }
     fields := map[string]interface{}{
-      "listeners":   sources.Listeners,
+      "listeners": sources.Listeners,
     }
 		acc.AddFields("icecast", fields, tags)
     total += sources.Listeners
@@ -175,8 +222,10 @@ func (ice *Icecast) gatherServer(
   }
 	acc.AddFields("icecast", fields_total, tags_total)
 
+
 	return nil
 }
+
 
 func init() {
 	inputs.Add("icecast", func() telegraf.Input {
