825adf2f9a5f79a3332e686a167b8433c279d0fb
Gavin Zhou <gavin.zhou@gmail.com>
2017-06-18 14:09:08 +0900

    test05

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -2,8 +2,35 @@
 
 VERSION="1.3.2"
 BUILD_DIR=$HOME/telegraf-build
+
+# Executes the given statement, and exits if the command returns a non-zero code.
+function exit_if_fail {
+    command=$@
+    echo "Executing '$command'"
+    eval $command
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echo "'$command' returned $rc."
+        exit $rc
+    fi
+}
+
+# Check that go fmt has been run.
+function check_go_fmt {
+    fmtcount=`git ls-files | grep '.go$' | grep -v Godep | xargs gofmt -l 2>&1 | wc -l`
+    if [ $fmtcount -gt 0 ]; then
+        echo "run 'go fmt ./...' to format your source code."
+        exit 1
+    fi
+}
+
+# Set up the build directory, and then GOPATH.
+exit_if_fail mkdir $BUILD_DIR
 export GOPATH=$BUILD_DIR
+# Turning off GOGC speeds up build times
+export GOGC=off
 export PATH=$GOPATH/bin:$PATH
+exit_if_fail mkdir -p $GOPATH/src/github.com/influxdata
 
 # Dump some test config to the log.
 echo "Test configuration"
@@ -11,6 +38,10 @@ echo "========================================"
 echo "\$HOME: $HOME"
 echo "\$GOPATH: $GOPATH"
 echo "\$CIRCLE_BRANCH: $CIRCLE_BRANCH"
+
+# Move the checked-out source to a better location
+exit_if_fail mv $HOME/telegraf $GOPATH/src/github.com/influxdata
+exit_if_fail cd $GOPATH/src/github.com/influxdata/telegraf
 
 gem instal fpm
 
