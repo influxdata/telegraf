8fd5848436612a7e858f6f9d14935634009c1329
Ayrdrie Palmer <ayrdriepalmer@gmail.com>
2017-06-21 13:11:08 -0700

    Isolate username with regex
    Signed-off-by: Adam Perlin <adamperlin@seattleacademy.org>

--- plugins/inputs/minecraft/main.go
+++ plugins/inputs/minecraft/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
 )
 
 func main() {
@@ -18,4 +19,37 @@ func main() {
 	}
 
 	fmt.Println("Full results", results)
+
+	var newSlice []string
+	if len(results) > 1 {
+		newSlice = results[1:]
+	} else {
+		// TODO: newSlice
+		return
+	}
+
+	var re = regexp.MustCompile(`for\s(.*):-`)
+
+	for _, s := range newSlice {
+		fmt.Printf("original string: %s\n", s)
+		for i, match := range re.FindAllStringSubmatch(s, -1) {
+			fmt.Println(match[1], "found at index", i, "\n")
+		}
+	}
+
+	// NEXT STEPS
+	// use regex to isolate stat name
+	// use regex to isolate stats themselves
+	// structure all data into JSON output format
+	// use that data somehow
+
+	// Result of running command stored in packet.Body
+	/*
+			var re = regexp.MustCompile(`for\s(.*):-`)
+		    var str = `4 tracked objective(s) for mauxlaim:- total_kills: 2 (total_kills)- iron_pickaxe: 190 (iron_pickaxe)- jumps: 866 (jumps)- level: 35 (level),`
+
+		    for i, match := range re.FindAllString(str, -1) {
+		        fmt.Println(match, "found at index", i)
+		    }
+	*/
 }
