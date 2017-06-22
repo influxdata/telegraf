54b177873174a4c52df55e1503590658878d8f7f
Adam Perlin <adamperlin@seattleacademy.org>
2017-06-22 11:20:36 -0700

    Add method to parse scoreboard data from rcon for minecraft input plugin
    Signed-off-by: Ayrdrie Palmer <ayrdriepalmer@gmail.com>

--- plugins/inputs/minecraft/minecraft.go
+++ plugins/inputs/minecraft/minecraft.go
@@ -5,6 +5,7 @@ package minecraft
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
@@ -19,20 +20,25 @@ const sampleConfig = `
   password = "replace_me"
 `
 
+// Minecraft represents a connection to a minecraft server
 type Minecraft struct {
 	Server   string
 	Port     string
 	Password string
 }
 
+// Description gives a brief description.
 func (s *Minecraft) Description() string {
 	return "it collects stats from Minecraft servers"
 }
 
+// SampleConfig returns our sampleConfig.
 func (s *Minecraft) SampleConfig() string {
 	return sampleConfig
 }
 
+// Gather uses the RCON protocal to collect username and
+// scoreboard stats from a minecraft server.
 func (s *Minecraft) Gather(acc telegraf.Accumulator) error {
 	if s.Port == " " {
 		acc.AddFields("state", map[string]interface{}{"value": "pretty good"}, nil)
@@ -43,6 +49,8 @@ func (s *Minecraft) Gather(acc telegraf.Accumulator) error {
 	return nil
 }
 
+// ParseUsername takes an input string from rcon, to parse
+// the username.
 func ParseUsername(input string) (string, error) {
 	var re = regexp.MustCompile(`for\s(.*):-`)
 
@@ -51,6 +59,45 @@ func ParseUsername(input string) (string, error) {
 		return "", fmt.Errorf("no username was matched")
 	}
 	return usernameMatches[0][1], nil
+}
+
+// Score is an individual tracked scoreboard stat.
+type Score struct {
+	Name  string
+	Value int
+}
+
+// ParseScoreboard takes an input string from rcon, to parse
+// scoreboard stats.
+func ParseScoreboard(input string) ([]Score, error) {
+	var re = regexp.MustCompile(`(?U):\s(\d+)\s\((.*)\)`)
+	scoreMatches := re.FindAllStringSubmatch(input, -1)
+	if scoreMatches == nil {
+		return nil, fmt.Errorf("No scores found")
+	}
+
+	var scores []Score
+
+	for _, match := range scoreMatches {
+		//fmt.Println(match)
+		number := match[1]
+		name := match[2]
+		n, err := strconv.Atoi(number)
+		//Not necessary in current state, because regex can only match integers,
+		// maybe become necessary if regex is modified to match more types of
+		//numbers
+		if err != nil {
+			return nil, fmt.Errorf("Failed to parse statistic")
+		}
+		s := Score{
+			Name:  name,
+			Value: n,
+		}
+		//	fmt.Println(s)
+		scores = append(scores, s)
+	}
+	//fmt.Println(scores)
+	return scores, nil
 }
 
 func init() {
--- plugins/inputs/minecraft/minecraft_test.go
+++ plugins/inputs/minecraft/minecraft_test.go
@@ -1,6 +1,10 @@
 package minecraft
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 // TestParseUsername tests different Minecraft RCON inputs for usernames
 func TestParseUsername(t *testing.T) {
@@ -25,4 +29,141 @@ func TestParseUsername(t *testing.T) {
 	if got != want {
 		t.Errorf("got %s\n want %s\n", got, want)
 	}
+
+	// Test an invalid input string to ensure error is returned
+	input = "1 tracked objective(s) for 😂:- jumps: 178 (jumps)"
+	got, err = ParseUsername(input)
+	want = "😂"
+	if err != nil {
+		t.Fatalf("username returned error. Error: %s\n", err)
+	}
+	if got != want {
+		t.Errorf("got %s\n want %s\n", got, want)
+	}
+}
+
+// TestParseScoreboard tests different Minecraft RCON inputs for scoreboard stats.
+func TestParseScoreboard(t *testing.T) {
+	// test a valid input string to ensure stats are parsed correctly.
+	input := `1 tracked objective(s) for divislight:- jumps: 178 (jumps)- sword: 5 (sword)`
+	got, err := ParseScoreboard(input)
+
+	fmt.Println(got)
+
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+
+	want := []Score{
+		{
+			Name:  "jumps",
+			Value: 178,
+		},
+		{
+			Name:  "sword",
+			Value: 5,
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: \n%#v\nWant: %#v", got, want)
+	}
+
+	// Tests a partial input string.
+	input = `1 tracked objective(s) for divislight:- jumps: (jumps)- sword: 5 (sword)`
+	got, err = ParseScoreboard(input)
+
+	fmt.Println(got)
+
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+
+	want = []Score{
+		{
+			Name:  "sword",
+			Value: 5,
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: \n%#v\nWant:\n%#v", got, want)
+	}
+
+	// Tests an empty string.
+	input = ``
+	got, err = ParseScoreboard(input)
+
+	fmt.Println(got)
+
+	if err == nil {
+		t.Fatal("Expected input error, but error was nil")
+	}
+
+	// Tests when a number isn't an integer.
+	input = `1 tracked objective(s) for divislight:- jumps: 178.5 (jumps)- sword: 5 (sword)`
+	got, err = ParseScoreboard(input)
+
+	fmt.Println(got)
+
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+
+	want = []Score{
+		{
+			Name:  "sword",
+			Value: 5,
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: \n%#v\nWant: %#v", got, want)
+	}
+
+	//Testing a real life data scenario with unicode characters
+	input = `7 tracked objective(s) for mauxlaim:- total_kills: 39 (total_kills)- "howdy doody": 37 (dalevel)- howdy: 37 (lvl)- jumps: 1290 (jumps)- iron_pickaxe: 284 (iron_pickaxe)- cow_kills: 1 (cow_kills)- "asdf": 37 (😂)`
+	got, err = ParseScoreboard(input)
+
+	fmt.Println(got)
+
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+
+	want = []Score{
+		{
+			Name:  "total_kills",
+			Value: 39,
+		},
+		{
+			Name:  "dalevel",
+			Value: 37,
+		},
+		{
+			Name:  "lvl",
+			Value: 37,
+		},
+		{
+			Name:  "jumps",
+			Value: 1290,
+		},
+		{
+			Name:  "iron_pickaxe",
+			Value: 284,
+		},
+		{
+			Name:  "cow_kills",
+			Value: 1,
+		},
+		{
+			Name:  "😂",
+			Value: 37,
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: \n%#v\nWant: %#v", got, want)
+	}
+
 }
