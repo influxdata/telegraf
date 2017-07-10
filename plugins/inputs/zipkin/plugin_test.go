36198dedeb3bb0b2b22e7d40a37cbc81a3155c67
Adam Perlin <adamperlin@seattleacademy.org>
2017-07-10 15:48:51 -0700

    Add clarifying comments to zipkin server test, fix hardcoded path name bug

--- plugins/inputs/zipkin/plugin_test.go
+++ plugins/inputs/zipkin/plugin_test.go
@@ -13,13 +13,9 @@ import (
 	"github.com/influxdata/telegraf/testutil"
 )
 
-type UnitTest struct {
-	datfile        string
-	tags           []map[string]string
-	expectedFields []map[string]interface{}
-}
+var tests []func(*testing.T)
 
-func TestZipkinPlugin(t *testing.T) {
+func testBasicSpans(t *testing.T) {
 	var acc testutil.Accumulator
 	z := &Zipkin{
 		Path: "/api/v1/test",
@@ -34,10 +30,6 @@ func TestZipkinPlugin(t *testing.T) {
 	postTestData(t, "testdata/threespans.dat")
 	acc.Wait(6)
 
-	for _, m := range acc.Metrics {
-		log.Println(m)
-	}
-
 	if len(acc.Errors) != 0 {
 		for _, e := range acc.Errors {
 			fmt.Println(e)
@@ -45,6 +37,11 @@ func TestZipkinPlugin(t *testing.T) {
 		t.Fatal("Errors were added during request")
 	}
 
+	// Actual testing:
+
+	// The tags we will be querying by:
+
+	// Test for the first span in the trace:
 	tags := map[string]string{
 		"id":               "8090652509916334619",
 		"parent_id":        "22964302721410078",
@@ -57,11 +54,15 @@ func TestZipkinPlugin(t *testing.T) {
 		"type":             "STRING",
 	}
 
-	//map[endpoint_host:2130706433:0 name:Child service_name:trivial trace_id:2505404965370368069 annotation_value:trivial key:lc type:STRING id:8090652509916334619 parent_id:22964302721410078]
-
-	//map[trace_id:2505404965370368069 key:lc type:STRING id:103618986556047333 name:Child service_name:trivial annotation_value:trivial endpoint_host:2130706433:0 parent_id:22964302721410078]
+	// assertContainsTaggedDuration asserts that the specified field which corresponds to `tags` has
+	// the specified value. In this case, we are testing that the measurement zipkin with tags `tags` has a
+	// field called `duration` with the value 53106 microseconds
 
 	assertContainsTaggedDuration(t, &acc, "zipkin", "duration", time.Duration(53106)*time.Microsecond, tags)
+
+	// Test for the second span in the trace:
+
+	//Update tags in order to perform our next test
 
 	tags = map[string]string{
 		"id":               "103618986556047333",
@@ -74,7 +75,11 @@ func TestZipkinPlugin(t *testing.T) {
 		"key":              "lc",
 		"type":             "STRING",
 	}
+
+	//Similar test as above, but with different tags
 	assertContainsTaggedDuration(t, &acc, "zipkin", "duration", time.Duration(50410)*time.Microsecond, tags)
+
+	//test for the third span in the trace (with three annotations)
 
 	tags = map[string]string{
 		"id":               "22964302721410078",
@@ -85,9 +90,15 @@ func TestZipkinPlugin(t *testing.T) {
 		"annotation_value": "Starting child #0",
 		"endpoint_host":    "2130706433:0",
 	}
+
+	// test for existence of annotation specific fields
 	assertContainsTaggedInt64(t, &acc, "zipkin", "annotation_timestamp", 1498688360851325, tags)
+
+	// test for existence of annotation specific fields
 	tags["annotation_value"] = "Starting child #1"
 	assertContainsTaggedInt64(t, &acc, "zipkin", "annotation_timestamp", 1498688360904545, tags)
+
+	//test for existence of annotation specific fields
 	tags["annotation_value"] = "A Log"
 	assertContainsTaggedInt64(t, &acc, "zipkin", "annotation_timestamp", 1498688360954992, tags)
 
@@ -102,8 +113,17 @@ func TestZipkinPlugin(t *testing.T) {
 		"key":              "lc",
 		"type":             "STRING",
 	}
-
+	// test for existence of span time stamp in third span, using binary annotation specific values.
 	assertContainsTaggedDuration(t, &acc, "zipkin", "duration", time.Duration(103680)*time.Microsecond, tags)
+}
+
+func TestZipkinPlugin(t *testing.T) {
+	tests = append(tests, testBasicSpans)
+
+	// iterate through all tests to run each test in sequence
+	for _, test := range tests {
+		t.Run("Trivial Test", test)
+	}
 }
 
 func assertContainsTaggedDuration(
@@ -179,7 +199,7 @@ func assertContainsTaggedInt64(
 }
 
 func postTestData(t *testing.T, datafile string) {
-	dat, err := ioutil.ReadFile("testdata/threespans.dat")
+	dat, err := ioutil.ReadFile(datafile)
 	if err != nil {
 		t.Fatal("Could not read from data file")
 	}
