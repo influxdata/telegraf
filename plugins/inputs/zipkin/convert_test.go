9d378948f99dd81677e738b7349036eb1c7d9860
Ayrdrie Palmer <ayrdriepalmer@gmail.com>
2017-07-17 15:21:32 -0600

    Add more data for testing zipkin plugin

--- plugins/inputs/zipkin/convert_test.go
+++ plugins/inputs/zipkin/convert_test.go
@@ -67,6 +67,43 @@ func TestLineProtocolConverter_Record(t *testing.T) {
 							},
 						},
 					},
+					Span{
+						ID:        "22964302721410078",
+						TraceID:   "2505404965370368069",
+						Name:      "Parent",
+						ParentID:  "22964302721410078",
+						Timestamp: time.Unix(0, 1498688360851318000),
+						Duration:  time.Duration(103680) * time.Microsecond,
+						Annotations: []Annotation{
+							Annotation{
+								Timestamp:   time.Unix(0, 1498688360851325000),
+								Value:       "Starting child #0",
+								Host:        "2130706433:0",
+								ServiceName: "trivial",
+							},
+							Annotation{
+								Timestamp:   time.Unix(0, 1498688360904545000),
+								Value:       "Starting child #1",
+								Host:        "2130706433:0",
+								ServiceName: "trivial",
+							},
+							Annotation{
+								Timestamp:   time.Unix(0, 1498688360954992000),
+								Value:       "A Log",
+								Host:        "2130706433:0",
+								ServiceName: "trivial",
+							},
+						},
+						BinaryAnnotations: []BinaryAnnotation{
+							BinaryAnnotation{
+								Key:         "lc",
+								Value:       "dHJpdmlhbA==",
+								Host:        "2130706433:0",
+								ServiceName: "trivial",
+								Type:        "STRING",
+							},
+						},
+					},
 				},
 			},
 			want: []testutil.Metric{
@@ -106,15 +143,209 @@ func TestLineProtocolConverter_Record(t *testing.T) {
 					},
 					Time: time.Unix(0, 1498688360904552000),
 				},
+				testutil.Metric{
+					Measurement: "zipkin",
+					Tags: map[string]string{
+						"service_name":     "trivial",
+						"annotation_value": "Starting child #0",
+						"endpoint_host":    "2130706433:0",
+						"id":               "22964302721410078",
+						"parent_id":        "22964302721410078",
+						"trace_id":         "2505404965370368069",
+						"name":             "Parent",
+					},
+					Fields: map[string]interface{}{
+						"annotation_timestamp": int64(1498688360),
+						"duration":             time.Duration(103680) * time.Microsecond,
+					},
+					Time: time.Unix(0, 1498688360851318000),
+				},
+				testutil.Metric{
+					Measurement: "zipkin",
+					Tags: map[string]string{
+						"service_name":     "trivial",
+						"annotation_value": "Starting child #1",
+						"endpoint_host":    "2130706433:0",
+						"id":               "22964302721410078",
+						"parent_id":        "22964302721410078",
+						"trace_id":         "2505404965370368069",
+						"name":             "Parent",
+					},
+					Fields: map[string]interface{}{
+						"annotation_timestamp": int64(1498688360),
+						"duration":             time.Duration(103680) * time.Microsecond,
+					},
+					Time: time.Unix(0, 1498688360851318000),
+				},
+				testutil.Metric{
+					Measurement: "zipkin",
+					Tags: map[string]string{
+						"parent_id":        "22964302721410078",
+						"trace_id":         "2505404965370368069",
+						"name":             "Parent",
+						"service_name":     "trivial",
+						"annotation_value": "A Log",
+						"endpoint_host":    "2130706433:0",
+						"id":               "22964302721410078",
+					},
+					Fields: map[string]interface{}{
+						"annotation_timestamp": int64(1498688360),
+						"duration":             time.Duration(103680) * time.Microsecond,
+					},
+					Time: time.Unix(0, 1498688360851318000),
+				},
+				testutil.Metric{
+					Measurement: "zipkin",
+					Tags: map[string]string{
+						"trace_id":         "2505404965370368069",
+						"service_name":     "trivial",
+						"annotation_value": "dHJpdmlhbA==",
+						"key":              "lc",
+						"type":             "STRING",
+						"id":               "22964302721410078",
+						"parent_id":        "22964302721410078",
+						"name":             "Parent",
+						"endpoint_host":    "2130706433:0",
+					},
+					Fields: map[string]interface{}{
+						"duration": time.Duration(103680) * time.Microsecond,
+					},
+					Time: time.Unix(0, 1498688360851318000),
+				},
 			},
 			wantErr: false,
 		},
 
+		// Test data from zipkin cli app:
+		//https://github.com/openzipkin/zipkin-go-opentracing/tree/master/examples/cli_with_2_services
 		/*{
-			name:    "changeMe2",
-			fields:  fields{},
-			args:    args{},
+			name:    "cli",
+			fields:  fields{
+			acc: &mockAcc,
+		},
+			args:    args{
+			t: Trace{
+				Span{
+					ID:          "3383422996321511664",
+					TraceID:     "243463817635710260",
+					Name:        "Concat",
+					ParentID:    "4574092882326506380",
+					Timestamp:   time.Unix(0, 1499817952283903000),
+					Duration:    time.Duration(2888) * time.Microsecond,
+					Annotations: []Annotation{
+						Annotaitons{
+							Timestamp:   time.Unix(0, 1499817952283903000),
+							Value:       "cs",
+							Host:        "0:0",
+							ServiceName: "cli",
+						},
+						Annotaitons{
+							Timestamp:   time.Unix(0, 1499817952286792000),
+							Value:       "cr",
+							Host:        "0:0",
+							ServiceName: "cli",
+						},
+				},
+					BinaryAnnotations: []BinaryAnnotation{
+						BinaryAnnotation{
+							Key:         "http.path",
+							Value:       "L2NvbmNhdC8=",
+							Host:        "0:0",
+							ServiceName: "cli",
+							Type:        "STRING",
+						},
+						BinaryAnnotation{
+							Key:         "http.url",
+							Value:       "aHR0cDovL2xvY2FsaG9zdDo2MTAwMS9jb25jYXQv",
+							Host:        "0:0",
+							ServiceName: "cli",
+							Type:        "STRING",
+						},
+						BinaryAnnotation{
+							Key:         "peer.hostname",
+							Value:       "bG9jYWxob3N0",
+							Host:        "0:0",
+							ServiceName: "cli",
+							Type:        "STRING",
+						},
+						BinaryAnnotation{
+							Key:         "span.kind",
+							Value:       "Y2xpZW50",
+							Host:        "0:0",
+							ServiceName: "cli",
+							Type:        "STRING",
+						},
+						BinaryAnnotation{
+							Key:         "http.method",
+							Value:       "R0VU",
+							Host:        "0:0",
+							ServiceName: "cli",
+							Type:        "STRING",
+						},
+						BinaryAnnotation{
+							Key:         "http.host",
+							Value:       "bG9jYWxob3N0OjYxMDAx",
+							Host:        "0:0",
+							ServiceName: "cli",
+							Type:        "STRING",
+						},
+					},
+		},
+		want: []testutil.Metric{
+		testutil.Metric{
+			Measurement: "zipkin",
+			Tags: map[string]string{
+				"id":               "3383422996321511664",
+				"parent_id":        "4574092882326506380",
+				//TODO: Can trace_id be this long?
+				"trace_id":         "8269862291023777619243463817635710260",
+				"name":             "Concat",
+				"service_name":     "cli",
+				//TODO: Do we need annotation_timestamp?
+				"annotation_value": "cs",
+				//TODO: Multiple binary_annotations
+				//"endpoint_host":    "0:0",
+				//"key":              "lc",
+				//"type":             "STRING",
+			},
+			Fields: map[string]interface{}{
+				"duration": time.Duration(2888) * time.Microsecond,
+			},
+			Time: time.Unix(0, 1499817952283903000),
+		},
 			wantErr: false,
+		},*/
+
+		//// Test data from distributed trace repo sample json
+		// https://github.com/mattkanwisher/distributedtrace/blob/master/testclient/sample.json
+		/*{
+			name: "distributed_trace_sample"
+			fields: fields{
+				acc: &mockAcc,
+			},
+			args: args{
+				t: Trace{
+					Span{
+						ID: "6802735349851856000",
+						TraceID: "6802735349851856000",
+						Name:"main.dud",
+						//TODO: ParentID nil or itself
+						ParentID:
+						//TODO: No Timestamp? or duraiton?
+						Timestamp:
+						Duration:
+						Annotation: []Annotation{
+							Annotation{
+								Timestamp: "1433330263415871",
+								Value: "cs",
+								Host: "0:9410",
+								ServiceName: "go-zipkin-testclient",
+							}
+						}
+						BinaryAnnotation: []BinaryAnnotation{},
+					},
+				},
+			},
 		},*/
 	}
 	for _, tt := range tests {
