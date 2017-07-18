3b3106fdb9b49a1e5bd526a52359c2757ada0ba1
vlastahajek <vlastimil.hajek@bonitoo.io>
2017-07-18 11:31:07 +0200

    Reformated with go fmt

--- plugins/inputs/win_services/win_services_test.go
+++ plugins/inputs/win_services/win_services_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows/svc/mgr"
 	"testing"
-    "golang.org/x/sys/windows/svc/mgr"
 )
 
 var InvalidServices = []string{"XYZ1@", "ZYZ@", "SDF_@#"}
@@ -25,9 +25,9 @@ func TestList(t *testing.T) {
 }
 
 func TestEmptyList(t *testing.T) {
-    services, err := listServices([]string {})
-    require.NoError(t, err)
-    assert.Condition(t, func () bool { return len(services) > 20}, "Too few service")
+	services, err := listServices([]string{})
+	require.NoError(t, err)
+	assert.Condition(t, func() bool { return len(services) > 20 }, "Too few service")
 }
 
 func TestListEr(t *testing.T) {
@@ -41,22 +41,22 @@ func TestListEr(t *testing.T) {
 }
 
 func TestGather(t *testing.T) {
-    ws := &Win_Services{KnownServices}
-    assert.Len(t, ws.ServiceNames, 2, "Different number of services")
-    var acc testutil.Accumulator
-    require.NoError(t, ws.Gather(&acc))
-    assert.Len(t, acc.Errors, 0, "There should be no errors after gather")
+	ws := &Win_Services{KnownServices}
+	assert.Len(t, ws.ServiceNames, 2, "Different number of services")
+	var acc testutil.Accumulator
+	require.NoError(t, ws.Gather(&acc))
+	assert.Len(t, acc.Errors, 0, "There should be no errors after gather")
 
-    for i := 0; i < 2; i++ {
-        fields := make(map[string]interface{})
-        tags := make(map[string]string)
-        si := getServiceInfo(KnownServices[i])
-        fields["state"] = ServiceStatesMap[si.State]
-        fields["startup_mode"] = ServiceStartupModeMap[si.StartUpMode]
-        tags["service_name"] = si.ServiceName
-        tags["display_name"] = si.DisplayName
-        acc.AssertContainsTaggedFields(t, "win_services", fields, tags)
-    }
+	for i := 0; i < 2; i++ {
+		fields := make(map[string]interface{})
+		tags := make(map[string]string)
+		si := getServiceInfo(KnownServices[i])
+		fields["state"] = ServiceStatesMap[si.State]
+		fields["startup_mode"] = ServiceStartupModeMap[si.StartUpMode]
+		tags["service_name"] = si.ServiceName
+		tags["display_name"] = si.DisplayName
+		acc.AssertContainsTaggedFields(t, "win_services", fields, tags)
+	}
 
 }
 
@@ -68,34 +68,34 @@ func TestGatherErrors(t *testing.T) {
 	assert.Len(t, acc.Errors, 3, "There should be 3 errors after gather")
 }
 
-func getServiceInfo(srvName string) (*ServiceInfo) {
+func getServiceInfo(srvName string) *ServiceInfo {
 
-    scmgr, err := mgr.Connect()
-    if err != nil {
-        return nil
-    }
-    defer scmgr.Disconnect()
+	scmgr, err := mgr.Connect()
+	if err != nil {
+		return nil
+	}
+	defer scmgr.Disconnect()
 
-    srv, err := scmgr.OpenService(srvName)
-    if err != nil {
-        return nil
-    }
-    var si ServiceInfo
-    si.ServiceName = srvName
-    srvStatus, err := srv.Query()
-    if err == nil {
-        si.State = int(srvStatus.State)
-    } else {
-        si.Error = err
-    }
+	srv, err := scmgr.OpenService(srvName)
+	if err != nil {
+		return nil
+	}
+	var si ServiceInfo
+	si.ServiceName = srvName
+	srvStatus, err := srv.Query()
+	if err == nil {
+		si.State = int(srvStatus.State)
+	} else {
+		si.Error = err
+	}
 
-    srvCfg, err := srv.Config()
-    if err == nil {
-        si.DisplayName = srvCfg.DisplayName
-        si.StartUpMode = int(srvCfg.StartType)
-    } else {
-        si.Error = err
-    }
-    srv.Close()
-    return &si
+	srvCfg, err := srv.Config()
+	if err == nil {
+		si.DisplayName = srvCfg.DisplayName
+		si.StartUpMode = int(srvCfg.StartType)
+	} else {
+		si.Error = err
+	}
+	srv.Close()
+	return &si
 }
