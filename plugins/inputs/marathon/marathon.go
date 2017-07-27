850598c835579da957d034cf73687d04eec37698
Sergei Smolianinov <sergei.smolianinov@f-secure.com>
2017-07-27 15:13:20 +0300

    Store each Marathon metric in dedicated measurement

--- plugins/inputs/marathon/marathon.go
+++ plugins/inputs/marathon/marathon.go
@@ -6,35 +6,41 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/inputs"
-	jsonparser "github.com/influxdata/telegraf/plugins/parsers/json"
 )
 
+var sampleConfig = `
+  ## Specify Marathon servers via an address:port list
+  ## e.g.
+  ##   localhost:8080
+  ##
+  ## If host is specified but port is not specified, then 8080 is used as the port
+  ## If no servers are specified, then localhost:8080 is used as the server
+  servers = ["localhost:8080"]
+
+  ## Timeout for HTTP requests to the Marathon servers
+  http_timeout = "5s"
+`
+
+// Marathon is a plugin to read metrics from one or many Marathon servers
 type Marathon struct {
 	Servers     []string
-	MetricTypes []string `toml:"metric_types"`
+	HttpTimeout internal.Duration
+	client      *http.Client
 }
 
-var allMetricTypes = []string{
-	"gauges", "counters", "histograms", "meters", "timers",
+// NewMarathon returns a new instance of Marathon
+func NewMarathon() *Marathon {
+	return &Marathon{
+		HttpTimeout: internal.Duration{Duration: time.Second * 5},
+	}
 }
-
-var sampleConfig = `
-  ## A list of Marathon servers.
-  servers = ["localhost:8080"]
-  ## Metric types to be collected, by default all enabled.
-  metric_types = [
-    "gauges",
-    "counters",
-    "histograms",
-    "meters",
-    "timers",
-  ]
-`
 
 // SampleConfig returns a sample configuration block
 func (m *Marathon) SampleConfig() string {
@@ -46,17 +52,24 @@ func (m *Marathon) Description() string {
 	return "Telegraf plugin for gathering metrics from Marathon hosts"
 }
 
-func (m *Marathon) SetDefaults() {
-	if len(m.MetricTypes) == 0 {
-		m.MetricTypes = allMetricTypes
-	}
-}
-
 // Gather() metrics from given list of Marathon servers
 func (m *Marathon) Gather(acc telegraf.Accumulator) error {
-	var wg sync.WaitGroup
 
-	m.SetDefaults()
+	if m.client == nil {
+		client, err := m.createHttpClient()
+
+		if err != nil {
+			return err
+		}
+
+		m.client = client
+	}
+
+	if len(m.Servers) == 0 {
+		m.Servers = append(m.Servers, "127.0.0.1:8080")
+	}
+
+	var wg sync.WaitGroup
 
 	for _, v := range m.Servers {
 		wg.Add(1)
@@ -71,13 +84,15 @@ func (m *Marathon) Gather(acc telegraf.Accumulator) error {
 	return nil
 }
 
-var tr = &http.Transport{
-	ResponseHeaderTimeout: time.Duration(3 * time.Second),
-}
-
-var client = &http.Client{
-	Transport: tr,
-	Timeout:   time.Duration(4 * time.Second),
+func (m *Marathon) createHttpClient() (*http.Client, error) {
+	tr := &http.Transport{
+		ResponseHeaderTimeout: m.HttpTimeout.Duration,
+	}
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   m.HttpTimeout.Duration,
+	}
+	return client, nil
 }
 
 func contains(strSlice []string, searchStr string) bool {
@@ -89,16 +104,8 @@ func contains(strSlice []string, searchStr string) bool {
 	return false
 }
 
-func (m *Marathon) filterMetrics(metrics map[string]interface{}) {
-	for k, _ := range metrics {
-		if contains(m.MetricTypes, k) == false {
-			delete(metrics, k)
-		}
-	}
-}
-
 func (m *Marathon) gatherMetrics(addr string, defaultPort string, acc telegraf.Accumulator) error {
-	var jsonOut map[string]interface{}
+	var jsonOut map[string]map[string]map[string]interface{}
 
 	host, _, err := net.SplitHostPort(addr)
 	if err != nil {
@@ -110,7 +117,7 @@ func (m *Marathon) gatherMetrics(addr string, defaultPort string, acc telegraf.A
 		"server": host,
 	}
 
-	resp, err := client.Get("http://" + addr + "/metrics")
+	resp, err := m.client.Get("http://" + addr + "/metrics")
 	if err != nil {
 		return err
 	}
@@ -125,22 +132,17 @@ func (m *Marathon) gatherMetrics(addr string, defaultPort string, acc telegraf.A
 		return errors.New("Error decoding JSON response")
 	}
 
-	m.filterMetrics(jsonOut)
-
-	jf := jsonparser.JSONFlattener{}
-	err = jf.FlattenJSON("", jsonOut)
-
-	if err != nil {
-		return err
+	for _, metrics := range jsonOut {
+		for metric, value := range metrics {
+			acc.AddFields(strings.Replace(metric, ".", "_", -1), value, tags)
+		}
 	}
-
-	acc.AddFields("marathon", jf.Fields, tags)
 
 	return nil
 }
 
 func init() {
 	inputs.Add("marathon", func() telegraf.Input {
-		return &Marathon{}
+		return NewMarathon()
 	})
 }
--- plugins/inputs/marathon/marathon_test.go
+++ plugins/inputs/marathon/marathon_test.go
@@ -11,26 +11,64 @@ import (
 )
 
 var inputMetrics map[string]interface{}
-var outputMetrics map[string]interface{}
+var outputMetrics map[string]map[string]interface{}
 var testServer *httptest.Server
 
 func generateMetrics() {
 	inputMetrics = map[string]interface{}{
+		"counters": map[string]interface{}{
+			"org.eclipse.jetty.servlet.ServletContextHandler.active-dispatches": map[string]interface{}{
+				"count": 1,
+			},
+		},
 		"gauges": map[string]interface{}{
 			"jvm.buffers.direct.capacity": map[string]interface{}{
-				"count": 2,
+				"value": 1,
+			},
+		},
+		"histograms": map[string]interface{}{
+			"service.mesosphere.marathon.state.MarathonStore.FrameworkId.read-data-size": map[string]interface{}{
+				"count": 1,
+				"max":   1,
+				"mean":  1,
+				"min":   1,
+			},
+		},
+		"meters": map[string]interface{}{
+			"service.mesosphere.marathon.state.GroupRepository.read-requests": map[string]interface{}{
+				"count":     1,
+				"mean_rate": 0.1,
 			},
 		},
 		"timers": map[string]interface{}{
 			"service.mesosphere.marathon.MarathonSchedulerService.run": map[string]interface{}{
 				"m1_rate": 0.1,
+				"m5_rate": 0.01,
 			},
 		},
 	}
 
-	outputMetrics = map[string]interface{}{
-		"gauges_jvm.buffers.direct.capacity_count":                                2.0,
-		"timers_service.mesosphere.marathon.MarathonSchedulerService.run_m1_rate": 0.1,
+	outputMetrics = map[string]map[string]interface{}{
+		"org_eclipse_jetty_servlet_ServletContextHandler_active-dispatches": {
+			"count": 1.0,
+		},
+		"jvm_buffers_direct_capacity": {
+			"value": 1.0,
+		},
+		"service_mesosphere_marathon_state_MarathonStore_FrameworkId_read-data-size": {
+			"count": 1.0,
+			"max":   1.0,
+			"mean":  1.0,
+			"min":   1.0,
+		},
+		"service_mesosphere_marathon_state_GroupRepository_read-requests": {
+			"count":     1.0,
+			"mean_rate": 0.1,
+		},
+		"service_mesosphere_marathon_MarathonSchedulerService_run": {
+			"m1_rate": 0.1,
+			"m5_rate": 0.01,
+		},
 	}
 }
 
@@ -64,30 +102,7 @@ func TestMetricsGather(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 
-	acc.AssertContainsFields(t, "marathon", outputMetrics)
-}
-
-func TestMetricsFilter(t *testing.T) {
-	m := Marathon{
-		MetricTypes: []string{
-			"gauges",
-		},
-	}
-	b := []string{
-		"counters", "timers", "meters", "histrograms",
-	}
-
-	m.filterMetrics(inputMetrics)
-
-	for _, k := range b {
-		if _, ok := inputMetrics[k]; ok {
-			t.Errorf("Found key %s, it should be gone.", k)
-		}
-	}
-
-	for _, k := range m.MetricTypes {
-		if _, ok := inputMetrics[k]; !ok {
-			t.Errorf("Didn't find key %s, it should be present.", k)
-		}
+	for measurement, fields := range outputMetrics {
+		acc.AssertContainsFields(t, measurement, fields)
 	}
 }
