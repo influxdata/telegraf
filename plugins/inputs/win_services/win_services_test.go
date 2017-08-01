8c755624beb4d583b0bd6ce35a94bd5021cc0f7a
vlastahajek <vlastimil.hajek@bonitoo.io>
2017-08-01 16:03:50 +0200

    Added data driven unit tests with mock implementation

--- plugins/inputs/win_services/win_services_test.go
+++ plugins/inputs/win_services/win_services_test.go
@@ -4,98 +4,176 @@
 package win_services
 
 import (
+	"errors"
+	"fmt"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows/svc"
 	"golang.org/x/sys/windows/svc/mgr"
 	"testing"
 )
 
-var InvalidServices = []string{"XYZ1@", "ZYZ@", "SDF_@#"}
-var KnownServices = []string{"LanmanServer", "TermService"}
-
-func TestList(t *testing.T) {
-	services, err := listServices(KnownServices)
-	require.NoError(t, err)
-	assert.Len(t, services, 2, "Different number of services")
-	assert.Equal(t, services[0].ServiceName, KnownServices[0])
-	assert.Nil(t, services[0].Error)
-	assert.Equal(t, services[1].ServiceName, KnownServices[1])
-	assert.Nil(t, services[1].Error)
+type TestData struct {
+	//collection that will be returned in ListServices if service arrays passed into WinServices constructor is empty
+	queryServiceList     []string
+	mgrConnectError      error
+	mgrListServicesError error
+	services             []ServiceTestInfo
 }
 
-func TestEmptyList(t *testing.T) {
-	services, err := listServices([]string{})
-	require.NoError(t, err)
-	assert.Condition(t, func() bool { return len(services) > 20 }, "Too few service")
+type ServiceTestInfo struct {
+	serviceOpenError   error
+	serviceQueryError  error
+	serviceConfigError error
+	serviceName        string
+	displayName        string
+	state              int
+	startUpMode        int
 }
 
-func TestListEr(t *testing.T) {
-	services, err := listServices(InvalidServices)
-	require.NoError(t, err)
-	assert.Len(t, services, 3, "Different number of services")
-	for i := 0; i < 3; i++ {
-		assert.Equal(t, services[i].ServiceName, InvalidServices[i])
-		assert.NotNil(t, services[i].Error)
+type FakeSvcMgr struct {
+	testData TestData
+}
+
+func (m *FakeSvcMgr) Disconnect() error {
+	return nil
+}
+
+func (m *FakeSvcMgr) OpenService(name string) (WinService, error) {
+	for _, s := range m.testData.services {
+		if s.serviceName == name {
+			if s.serviceOpenError != nil {
+				return nil, s.serviceOpenError
+			} else {
+				return &FakeWinSvc{s}, nil
+			}
+		}
+	}
+	return nil, fmt.Errorf("Cannot find service %s", name)
+}
+
+func (m *FakeSvcMgr) ListServices() ([]string, error) {
+	if m.testData.mgrListServicesError != nil {
+		return nil, m.testData.mgrListServicesError
+	} else {
+		return m.testData.queryServiceList, nil
 	}
 }
 
-func TestGather(t *testing.T) {
-	ws := &Win_Services{KnownServices}
-	assert.Len(t, ws.ServiceNames, 2, "Different number of services")
-	var acc testutil.Accumulator
-	require.NoError(t, ws.Gather(&acc))
-	assert.Len(t, acc.Errors, 0, "There should be no errors after gather")
+type FakeMgProvider struct {
+	testData TestData
+}
 
-	for i := 0; i < 2; i++ {
+func (m *FakeMgProvider) Connect() (WinServiceManager, error) {
+	if m.testData.mgrConnectError != nil {
+		return nil, m.testData.mgrConnectError
+	} else {
+		return &FakeSvcMgr{m.testData}, nil
+	}
+}
+
+type FakeWinSvc struct {
+	testData ServiceTestInfo
+}
+
+func (m *FakeWinSvc) Close() error {
+	return nil
+}
+func (m *FakeWinSvc) Config() (mgr.Config, error) {
+	if m.testData.serviceConfigError != nil {
+		return mgr.Config{}, m.testData.serviceConfigError
+	} else {
+		return mgr.Config{0, uint32(m.testData.startUpMode), 0, "", "", 0, nil, m.testData.serviceName, m.testData.displayName, "", ""}, nil
+	}
+}
+func (m *FakeWinSvc) Query() (svc.Status, error) {
+	if m.testData.serviceQueryError != nil {
+		return svc.Status{}, m.testData.serviceQueryError
+	} else {
+		return svc.Status{svc.State(m.testData.state), 0, 0, 0}, nil
+	}
+}
+
+var testErrors = []TestData{
+	{nil, errors.New("Fake mgr connect error"), nil, nil},
+	{nil, nil, errors.New("Fake mgr list services error"), nil},
+	{[]string{"Fake service 1", "Fake service 2", "Fake service 3"}, nil, nil, []ServiceTestInfo{
+		{errors.New("Fake srv open error"), nil, nil, "Fake service 1", "", 0, 0},
+		{nil, errors.New("Fake srv query error"), nil, "Fake service 2", "", 0, 0},
+		{nil, nil, errors.New("Fake srv config error"), "Fake service 3", "", 0, 0},
+	}},
+	{nil, nil, nil, []ServiceTestInfo{
+		{errors.New("Fake srv open error"), nil, nil, "Fake service 1", "", 0, 0},
+	}},
+}
+
+func TestBasicInfo(t *testing.T) {
+
+	winServices := &WinServices{nil, &FakeMgProvider{testErrors[0]}}
+	assert.NotEmpty(t, winServices.SampleConfig())
+	assert.NotEmpty(t, winServices.SampleConfig())
+}
+
+func TestMgrErrors(t *testing.T) {
+	//mgr.connect error
+	winServices := &WinServices{nil, &FakeMgProvider{testErrors[0]}}
+	var acc1 testutil.Accumulator
+	err := winServices.Gather(&acc1)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), testErrors[0].mgrConnectError.Error())
+
+	////mgr.listServices error
+	winServices = &WinServices{nil, &FakeMgProvider{testErrors[1]}}
+	var acc2 testutil.Accumulator
+	err = winServices.Gather(&acc2)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), testErrors[1].mgrListServicesError.Error())
+
+	////mgr.listServices error 2
+	winServices = &WinServices{[]string{"Fake service 1"}, &FakeMgProvider{testErrors[3]}}
+	var acc3 testutil.Accumulator
+	err = winServices.Gather(&acc3)
+	require.NoError(t, err)
+	assert.Len(t, acc3.Errors, 1)
+
+}
+
+func TestServiceErrors(t *testing.T) {
+	winServices := &WinServices{nil, &FakeMgProvider{testErrors[2]}}
+	var acc1 testutil.Accumulator
+	require.NoError(t, winServices.Gather(&acc1))
+	assert.Len(t, acc1.Errors, 3)
+	//open service error
+	assert.Contains(t, acc1.Errors[0].Error(), testErrors[2].services[0].serviceOpenError.Error())
+	//query service error
+	assert.Contains(t, acc1.Errors[1].Error(), testErrors[2].services[1].serviceQueryError.Error())
+	//config service error
+	assert.Contains(t, acc1.Errors[2].Error(), testErrors[2].services[2].serviceConfigError.Error())
+
+}
+
+var testSimpleData = []TestData{
+	{[]string{"Service 1", "Service 2"}, nil, nil, []ServiceTestInfo{
+		{nil, nil, nil, "Service 1", "Fake service 1", 1, 2},
+		{nil, nil, nil, "Service 2", "Fake service 2", 1, 2},
+	}},
+}
+
+func TestGather2(t *testing.T) {
+	winServices := &WinServices{nil, &FakeMgProvider{testSimpleData[0]}}
+	var acc1 testutil.Accumulator
+	require.NoError(t, winServices.Gather(&acc1))
+	assert.Len(t, acc1.Errors, 0, "There should be no errors after gather")
+
+	for _, s := range testSimpleData[0].services {
 		fields := make(map[string]interface{})
 		tags := make(map[string]string)
-		si := getServiceInfo(KnownServices[i])
-		fields["state"] = int(si.State)
-		fields["startup_mode"] = int(si.StartUpMode)
-		tags["service_name"] = si.ServiceName
-		tags["display_name"] = si.DisplayName
-		acc.AssertContainsTaggedFields(t, "win_services", fields, tags)
+		fields["state"] = int(s.state)
+		fields["startup_mode"] = int(s.startUpMode)
+		tags["service_name"] = s.serviceName
+		tags["display_name"] = s.displayName
+		acc1.AssertContainsTaggedFields(t, "win_services", fields, tags)
 	}
 
-}
-
-func TestGatherErrors(t *testing.T) {
-	ws := &Win_Services{InvalidServices}
-	assert.Len(t, ws.ServiceNames, 3, "Different number of services")
-	var acc testutil.Accumulator
-	require.NoError(t, ws.Gather(&acc))
-	assert.Len(t, acc.Errors, 3, "There should be 3 errors after gather")
-}
-
-func getServiceInfo(srvName string) *ServiceInfo {
-
-	scmgr, err := mgr.Connect()
-	if err != nil {
-		return nil
-	}
-	defer scmgr.Disconnect()
-
-	srv, err := scmgr.OpenService(srvName)
-	if err != nil {
-		return nil
-	}
-	var si ServiceInfo
-	si.ServiceName = srvName
-	srvStatus, err := srv.Query()
-	if err == nil {
-		si.State = int(srvStatus.State)
-	} else {
-		si.Error = err
-	}
-
-	srvCfg, err := srv.Config()
-	if err == nil {
-		si.DisplayName = srvCfg.DisplayName
-		si.StartUpMode = int(srvCfg.StartType)
-	} else {
-		si.Error = err
-	}
-	srv.Close()
-	return &si
 }
