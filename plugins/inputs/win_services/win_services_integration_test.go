215828e1c89fc826970238dfe797eb48a8f134c1
vlastahajek <vlastimil.hajek@bonitoo.io>
2017-08-04 22:57:04 +0200

    Proper skipping of tests in short mode

--- plugins/inputs/win_services/win_services_integration_test.go
+++ plugins/inputs/win_services/win_services_integration_test.go
@@ -15,67 +15,71 @@ var InvalidServices = []string{"XYZ1@", "ZYZ@", "SDF_@#"}
 var KnownServices = []string{"LanmanServer", "TermService"}
 
 func TestList(t *testing.T) {
-	if !testing.Short() {
-		services, err := listServices(&MgProvider{}, KnownServices)
-		require.NoError(t, err)
-		assert.Len(t, services, 2, "Different number of services")
-		assert.Equal(t, services[0].ServiceName, KnownServices[0])
-		assert.Nil(t, services[0].Error)
-		assert.Equal(t, services[1].ServiceName, KnownServices[1])
-		assert.Nil(t, services[1].Error)
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
 	}
+	services, err := listServices(&MgProvider{}, KnownServices)
+	require.NoError(t, err)
+	assert.Len(t, services, 2, "Different number of services")
+	assert.Equal(t, services[0].ServiceName, KnownServices[0])
+	assert.Nil(t, services[0].Error)
+	assert.Equal(t, services[1].ServiceName, KnownServices[1])
+	assert.Nil(t, services[1].Error)
 }
 
 func TestEmptyList(t *testing.T) {
-	if !testing.Short() {
-		services, err := listServices(&MgProvider{}, []string{})
-		require.NoError(t, err)
-		assert.Condition(t, func() bool { return len(services) > 20 }, "Too few service")
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
 	}
+	services, err := listServices(&MgProvider{}, []string{})
+	require.NoError(t, err)
+	assert.Condition(t, func() bool { return len(services) > 20 }, "Too few service")
 }
 
 func TestListEr(t *testing.T) {
-	if !testing.Short() {
-		services, err := listServices(&MgProvider{}, InvalidServices)
-		require.NoError(t, err)
-		assert.Len(t, services, 3, "Different number of services")
-		for i := 0; i < 3; i++ {
-			assert.Equal(t, services[i].ServiceName, InvalidServices[i])
-			assert.NotNil(t, services[i].Error)
-		}
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+	services, err := listServices(&MgProvider{}, InvalidServices)
+	require.NoError(t, err)
+	assert.Len(t, services, 3, "Different number of services")
+	for i := 0; i < 3; i++ {
+		assert.Equal(t, services[i].ServiceName, InvalidServices[i])
+		assert.NotNil(t, services[i].Error)
 	}
 }
 
 func TestGather(t *testing.T) {
-	if !testing.Short() {
-		ws := &WinServices{KnownServices, &MgProvider{}}
-		assert.Len(t, ws.ServiceNames, 2, "Different number of services")
-		var acc testutil.Accumulator
-		require.NoError(t, ws.Gather(&acc))
-		assert.Len(t, acc.Errors, 0, "There should be no errors after gather")
-
-		for i := 0; i < 2; i++ {
-			fields := make(map[string]interface{})
-			tags := make(map[string]string)
-			si := getServiceInfo(KnownServices[i])
-			fields["state"] = int(si.State)
-			fields["startup_mode"] = int(si.StartUpMode)
-			tags["service_name"] = si.ServiceName
-			tags["display_name"] = si.DisplayName
-			acc.AssertContainsTaggedFields(t, "win_services", fields, tags)
-		}
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
 	}
+	ws := &WinServices{KnownServices, &MgProvider{}}
+	assert.Len(t, ws.ServiceNames, 2, "Different number of services")
+	var acc testutil.Accumulator
+	require.NoError(t, ws.Gather(&acc))
+	assert.Len(t, acc.Errors, 0, "There should be no errors after gather")
 
+	for i := 0; i < 2; i++ {
+		fields := make(map[string]interface{})
+		tags := make(map[string]string)
+		si := getServiceInfo(KnownServices[i])
+		fields["state"] = int(si.State)
+		fields["startup_mode"] = int(si.StartUpMode)
+		tags["service_name"] = si.ServiceName
+		tags["display_name"] = si.DisplayName
+		acc.AssertContainsTaggedFields(t, "win_services", fields, tags)
+	}
 }
 
 func TestGatherErrors(t *testing.T) {
-	if !testing.Short() {
-		ws := &WinServices{InvalidServices, &MgProvider{}}
-		assert.Len(t, ws.ServiceNames, 3, "Different number of services")
-		var acc testutil.Accumulator
-		require.NoError(t, ws.Gather(&acc))
-		assert.Len(t, acc.Errors, 3, "There should be 3 errors after gather")
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
 	}
+	ws := &WinServices{InvalidServices, &MgProvider{}}
+	assert.Len(t, ws.ServiceNames, 3, "Different number of services")
+	var acc testutil.Accumulator
+	require.NoError(t, ws.Gather(&acc))
+	assert.Len(t, acc.Errors, 3, "There should be 3 errors after gather")
 }
 
 func getServiceInfo(srvName string) *ServiceInfo {
