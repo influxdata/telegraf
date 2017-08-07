4cd2de19d8a9946eec6f1040743f71834621efd5
vlastahajek <vlastimil.hajek@bonitoo.io>
2017-08-07 22:55:06 +0200

    Collecting to accumulator

--- plugins/inputs/dcos/dcos.go
+++ plugins/inputs/dcos/dcos.go
@@ -1,18 +1,21 @@
 package dcos
 
 import (
-	"github.com/influxdata/telegraf"
-	"net/http"
-	"time"
-	"io/ioutil"
 	"encoding/json"
 	"fmt"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type Dcos struct {
 	ClusterURL string `toml: cluster_url`
-	AuthToken	string `toml: auth_token`
+	AuthToken  string `toml: auth_token`
 }
+
 var sampleConfig = `
 	# Base URL of DCOS cluster, e.g. http://dcos.example.com
 	cluster_url=
@@ -20,13 +23,15 @@ var sampleConfig = `
 	auth_token=
 `
 
-func (m* Dcos) Description() string {
+func (m *Dcos) Description() string {
 	return "Input plugin for gathering DCOS agent metrics"
 }
 
-func (m* Dcos) SampleConfig() string {
+func (m *Dcos) SampleConfig() string {
 	return sampleConfig
 }
+
+var nameSanitizer = strings.NewReplacer("/", "_", `\`, "_")
 
 var tr = &http.Transport{
 	ResponseHeaderTimeout: time.Duration(3 * time.Second),
@@ -38,8 +43,8 @@ var client = &http.Client{
 }
 
 type mesosMasterStateSummary struct {
-	Hostname string `json:"hostname"`
-	Slaves   []slave   `json:"slaves"`
+	Hostname string  `json:"hostname"`
+	Slaves   []slave `json:"slaves"`
 }
 
 type slave struct {
@@ -48,15 +53,15 @@ type slave struct {
 }
 
 type datapoint struct {
-	Name string `json:"name"`
-	Value interface{} `json:"value"`
-	Unit string `json:"unit"`
-	TimeStamp string `json:"timestamp"`
-	Tags map[string]string `json:"tags"`
+	Name      string            `json:"name"`
+	Value     interface{}       `json:"value"`
+	Unit      string            `json:"unit"`
+	TimeStamp string            `json:"timestamp"`
+	Tags      map[string]string `json:"tags"`
 }
 
 type metric struct {
-	Datapoints   []datapoint `json:"datapoints"`
+	Datapoints []datapoint       `json:"datapoints"`
 	Dimensions map[string]string `json:"dimensions"`
 }
 
@@ -65,11 +70,11 @@ const AgentContainersUrl = "/system/v1/agent/%s/metrics/v0/containers"
 const AgentContainerMetricsUrl = "/system/v1/agent/%s/metrics/v0/containers/%s"
 const AgentNodeMetricsUrl = "/system/v1/agent/%s/metrics/v0/node"
 
-func (m* Dcos) Gather(acc telegraf.Accumulator) error {
+func (m *Dcos) Gather(acc telegraf.Accumulator) error {
 	//get agents
 	url := m.ClusterURL + MesosMasterStateSummaryUrl
 	var state mesosMasterStateSummary
-	err :=  m.handleJsonRequest(url, &state)
+	err := m.handleJsonRequest(url, &state)
 	if err != nil {
 		return err
 	}
@@ -79,7 +84,7 @@ func (m* Dcos) Gather(acc telegraf.Accumulator) error {
 	}
 
 	for _, a := range state.Slaves {
-		fmt.Println("Agent: " + a.Id)
+		//fmt.Println("Agent: " + a.Id)
 
 		url = m.ClusterURL + fmt.Sprintf(AgentNodeMetricsUrl, a.Id)
 		var nodeMetric metric
@@ -88,24 +93,24 @@ func (m* Dcos) Gather(acc telegraf.Accumulator) error {
 			return err
 		}
 
-		dumpMetric(&nodeMetric, acc)
+		processMetric(&nodeMetric, acc, "agent")
 
 		url = m.ClusterURL + fmt.Sprintf(AgentContainersUrl, a.Id)
-		var containerIds   []string
+		var containerIds []string
 		err = m.handleJsonRequest(url, &containerIds)
 		if err != nil {
 			return err
 		}
 
 		for _, c := range containerIds {
-			fmt.Println("Container: " + c)
+			//fmt.Println("Container: " + c)
 			url = m.ClusterURL + fmt.Sprintf(AgentContainerMetricsUrl, a.Id, c)
 			var metric metric
 			err = m.handleJsonRequest(url, &metric)
 			if err != nil {
 				return err
 			}
-			dumpMetric(&metric, acc)
+			processMetric(&metric, acc, "container")
 		}
 	}
 
@@ -113,18 +118,18 @@ func (m* Dcos) Gather(acc telegraf.Accumulator) error {
 }
 
 //newRequest creates http request object to given url with common headers required by DCOS
-func (m* Dcos ) newRequest(url string) (*http.Request, error) {
+func (m *Dcos) newRequest(url string) (*http.Request, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("Authorization", "token=" + m.AuthToken)
+	req.Header.Add("Authorization", "token="+m.AuthToken)
 	req.Header.Add("Accept", "application/json")
 	return req, nil
 }
 
-//handleJsonRequest takes care of sending request to given url, returning JSON data and unmarshalling JSON payload into given object
-func (m* Dcos ) handleJsonRequest(url string, obj interface{}) ( error) {
+//handleJsonRequest takes care of sending request to given url, returning JSON data and un-marshalling JSON payload into given object
+func (m *Dcos) handleJsonRequest(url string, obj interface{}) error {
 	req, err := m.newRequest(url)
 	if err != nil {
 		return err
@@ -132,18 +137,17 @@ func (m* Dcos ) handleJsonRequest(url string, obj interface{}) ( error) {
 
 	resp, err := client.Do(req)
 
-	defer resp.Body.Close()
-
 	if err != nil {
 		return err
 	}
+
+	defer resp.Body.Close()
 
 	if resp.StatusCode/100 != 2 {
 		return fmt.Errorf("HTTP request has failed. HTTP status code: %d", resp.StatusCode)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 
 	if err != nil {
 		return err
@@ -156,12 +160,61 @@ func (m* Dcos ) handleJsonRequest(url string, obj interface{}) ( error) {
 	return nil
 }
 
-func dumpMetric(metric *metric, acc telegraf.Accumulator ) {
-	fmt.Println("Datapoits")
-	for _, d := range metric.Datapoints {
-		fmt.Printf(" - [ %s: %v%s\n", d.Name, d.Value, d.Unit)
-		fmt.Printf("      %v]\n", d.Tags)
+func processMetric(metric *metric, acc telegraf.Accumulator, metricType string) {
+	tags := make(map[string]string)
+	fields := make(map[string]interface{})
+
+	for k, v := range metric.Dimensions {
+		tags[k] = v
 	}
-	fmt.Println("Dimensions")
-	fmt.Printf("  %v\n", metric.Dimensions)
+	tags["Type"] = metricType
+
+	//fmt.Println("Datapoits")
+
+	var t time.Time
+	var err error
+	var timeCollected = false
+
+	for _, d := range metric.Datapoints {
+		//all datapoints have the same timestamp, take the first
+		if !timeCollected {
+			t, err = time.Parse(time.RFC3339, d.TimeStamp)
+			if err == nil {
+				timeCollected = true
+			}
+		}
+		preProcessDataPoint(&d)
+		fields[d.Name] = d.Unit
+		//fmt.Printf(" - [ %s: %v%s\n", d.Name, d.Value, d.Unit)
+		//fmt.Printf("      %v]\n", d.Tags)
+		for k, v := range d.Tags {
+			tags[k] = v
+		}
+	}
+	//fmt.Println("Dimensions")
+	//fmt.Printf("  %v\n", metric.Dimensions)
+	//fmt.Println("AllTags")
+	//fmt.Printf("  %v\n", tags)
+
+	if !timeCollected {
+		t = time.Now()
+	}
+	acc.AddFields("dcos", fields, tags, t)
+}
+
+func preProcessDataPoint(datapoint *datapoint) {
+	for k, v := range datapoint.Tags {
+		switch k {
+		case "interface":
+			datapoint.Name = fmt.Sprintf("%s.%s", datapoint.Name, nameSanitizer.Replace(v))
+			delete(datapoint.Tags, k)
+		case "path":
+			datapoint.Name = datapoint.Name + nameSanitizer.Replace(v)
+			delete(datapoint.Tags, k)
+		}
+	}
+}
+
+func init() {
+	inputs.Add("dcos", func() telegraf.Input { return &Dcos{} })
 }
