bbb45eba26dda2205d71e4702d93f99c015e4627
Felix Geisendörfer <felix@debuggable.com>
2017-08-08 16:58:56 +0200

    WIP: mostly worked on escaping stuff

--- plugins/outputs/cratedb/cratedb.go
+++ plugins/outputs/cratedb/cratedb.go
@@ -3,6 +3,8 @@ package cratedb
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/influxdata/telegraf"
@@ -57,22 +59,90 @@ func (c *CrateDB) Write(metrics []telegraf.Metric) error {
 INSERT INTO ` + c.Table + ` ("name", "timestamp", "tags", "tags_hash")
 VALUES ($1, $2, $3, $4);
 `
+
 	ctx, cancel := context.WithTimeout(context.Background(), c.Timeout.Duration)
 	defer cancel()
 
-	//stmt, err := c.DB.PrepareContext(ctx, sql)
-	//if err != nil {
-	//return err
-	//}
-	//defer stmt.Close()
+	stmt, err := c.DB.PrepareContext(ctx, sql)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
 	//_ = stmt
 	for _, m := range metrics {
-		if _, err := c.DB.ExecContext(ctx, sql, m.Name(), m.Time(), 1, 2); err != nil {
+		if _, err := stmt.ExecContext(ctx, m.Name(), m.Time(), 1, 2); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func insertSQL(table string, metrics []telegraf.Metric) (string, error) {
+	rows := make([]string, len(metrics))
+	for i, m := range metrics {
+		cols := []interface{}{
+			m.Name(),
+			m.Time(),
+			m.Tags(),
+		}
+		escapedCols := make([]string, len(cols))
+		for i, col := range cols {
+			escaped, err := escapeValue(col)
+			if err != nil {
+				return "", err
+			}
+			escapedCols[i] = escaped
+		}
+		rows[i] = `(` + strings.Join(escapedCols, ",") + `)`
+	}
+	sql := `INSERT INTO ` + table + ` ("name", "timestamp", "tags", "tags_hash", "value")
+VALUES
+` + strings.Join(rows, "  ,\n") + `;`
+	return sql, nil
+}
+
+// escapeValue returns a string version of val that is suitable for being used
+// inside of a VALUES expression or similar. Unsupported types return an error.
+//
+// Warning: This is not ideal from a security perspective, but unfortunately
+// CrateDB does not support enough of the PostgreSQL wire protocol to allow
+// using lib/pq with $1, $2 placeholders. Security conscious users of this
+// plugin should probably refrain from using it in combination with untrusted
+// inputs.
+func escapeValue(val interface{}) (string, error) {
+	switch t := val.(type) {
+	case string:
+		return escapeString(t, `'`), nil
+	case time.Time:
+		// see https://crate.io/docs/crate/reference/sql/data_types.html#timestamp
+		return escapeValue(t.Format("2006-01-02T15:04:05.999-0700"))
+	case map[string]string:
+		// There is a decent chance that the implementation below doesn't catch all
+		// edge cases, but it's hard to tell since the format seems to be a bit
+		// underspecified. Anyway, luckily we only have to deal with a
+		// map[string]string here, giving a higher chance that the code below is
+		// correct.
+		// See https://crate.io/docs/crate/reference/sql/data_types.html#object
+		pairs := make([]string, 0, len(t))
+		for k, v := range t {
+			val, err := escapeValue(v)
+			if err != nil {
+				return "", err
+			}
+			pairs = append(pairs, escapeString(k, `"`)+" = "+val)
+		}
+		return `{` + strings.Join(pairs, ", ") + `}`, nil
+	default:
+		// This might be panic worthy under normal circumstances, but it's probably
+		// better to not shut down the entire telegraf process because of one
+		// misbehaving plugin.
+		return "", fmt.Errorf("unexpected type: %#v", t)
+	}
+}
+
+func escapeString(s string, quote string) string {
+	return quote + strings.Replace(s, quote, quote+quote, -1) + quote
 }
 
 func (c *CrateDB) SampleConfig() string {
--- plugins/outputs/cratedb/cratedb_test.go
+++ plugins/outputs/cratedb/cratedb_test.go
@@ -5,12 +5,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestConnectAndWrite(t *testing.T) {
+	return
 	if testing.Short() {
 		t.Skip("Skipping integration test in short mode")
 	}
@@ -25,7 +27,47 @@ func TestConnectAndWrite(t *testing.T) {
 	require.NoError(t, c.Connect())
 	require.NoError(t, c.Write(testutil.MockMetrics()))
 	require.NoError(t, c.Close())
+}
 
+func Test_insertSQL(t *testing.T) {
+	return
+	tests := []struct {
+		Metrics []telegraf.Metric
+		Want    string
+	}{
+		{
+			Metrics: testutil.MockMetrics(),
+			Want:    "INSERT ...",
+		},
+	}
+
+	for _, test := range tests {
+		if got, err := insertSQL("my_table", test.Metrics); err != nil {
+			t.Error(err)
+		} else if got != test.Want {
+			t.Errorf("got:\n%s\n\nwant:\n%s", got, test.Want)
+		}
+	}
+}
+
+func Test_escapeValue(t *testing.T) {
+	tests := []struct {
+		Val  interface{}
+		Want string
+	}{
+		{`foo`, `'foo'`},
+		{`foo'bar 'yeah`, `'foo''bar ''yeah'`},
+		{time.Date(2017, 8, 7, 16, 44, 52, 123*1000*1000, time.FixedZone("Dreamland", 5400)), `'2017-08-07T16:44:52.123+0130'`},
+		{map[string]string{"foo": "bar"}, `{"foo" = 'bar'}`},
+	}
+
+	for _, test := range tests {
+		if got, err := escapeValue(test.Val); err != nil {
+			t.Errorf("val: %#v: %s", test.Val, err)
+		} else if got != test.Want {
+			t.Errorf("got:\n%s\n\nwant:\n%s", got, test.Want)
+		}
+	}
 }
 
 func testURL() string {
