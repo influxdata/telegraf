1c5f95e300919a7ec957ca87bda70264d345ef82
vlastahajek <vlastimil.hajek@bonitoo.io>
2017-08-11 16:04:22 +0200

    Basic functionality finished

--- plugins/inputs/dcos/dcos.go
+++ plugins/inputs/dcos/dcos.go
@@ -2,25 +2,33 @@ package dcos
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
 type Dcos struct {
-	ClusterURL string `toml: cluster_url`
-	AuthToken  string `toml: auth_token`
+	ClusterURL        string   `toml:"cluster_url"`
+	AuthToken         string   `toml:"auth_token"`
+	FileSystemMounts  []string `toml:"file_system_mounts"`
+	NetworkInterfaces []string `toml:"network_interfaces"`
 }
 
 var sampleConfig = `
-	# Base URL of DCOS cluster, e.g. http://dcos.example.com
+	# Base URL of DC/OS cluster, e.g. http://dcos.example.com
 	cluster_url=
 	# Authentication token, obtained by running: dcos config show core.dcos_acs_token
 	auth_token=
+	# DC/OS agent node file system mount for which related metrics should be gathered
+	file_system_mounts = []
+	# DC/OS agent node network interface names for which related metrics should be gathered
+	network_interfaces = []
 `
 
 func (m *Dcos) Description() string {
@@ -54,7 +62,7 @@ type slave struct {
 
 type datapoint struct {
 	Name      string            `json:"name"`
-	Value     interface{}       `json:"value"`
+	Value     float64           `json:"value"`
 	Unit      string            `json:"unit"`
 	TimeStamp string            `json:"timestamp"`
 	Tags      map[string]string `json:"tags"`
@@ -65,55 +73,118 @@ type metric struct {
 	Dimensions map[string]string `json:"dimensions"`
 }
 
+const (
+	Node      = "node"
+	Container = "container"
+)
+
 const MesosMasterStateSummaryUrl = "/mesos/master/state-summary"
 const AgentContainersUrl = "/system/v1/agent/%s/metrics/v0/containers"
 const AgentContainerMetricsUrl = "/system/v1/agent/%s/metrics/v0/containers/%s"
 const AgentNodeMetricsUrl = "/system/v1/agent/%s/metrics/v0/node"
 
+//validateConfiguration tests whether important configuration params are not empty
+func (m *Dcos) validateConfiguration() error {
+	errorStrings := []string{}
+
+	if len(m.ClusterURL) == 0 {
+		errorStrings = append(errorStrings, "Invalid configuration, cluster_url is empty")
+	}
+	if len(m.AuthToken) == 0 {
+		errorStrings = append(errorStrings, "Invalid configuration, auth_token is empty")
+	}
+
+	if len(errorStrings) > 0 {
+		return errors.New(strings.Join(errorStrings, "\n"))
+	} else {
+		return nil
+	}
+
+}
+
 func (m *Dcos) Gather(acc telegraf.Accumulator) error {
+	err := m.validateConfiguration()
+	if err != nil {
+		return err
+	}
 	//get agents
 	url := m.ClusterURL + MesosMasterStateSummaryUrl
 	var state mesosMasterStateSummary
-	err := m.handleJsonRequest(url, &state)
+	err = m.handleJsonRequest(url, &state)
 	if err != nil {
 		return err
 	}
 
 	if len(state.Slaves) == 0 {
-		return fmt.Errorf("DCOS cluster %s does not have any running slave", m.ClusterURL)
+		return fmt.Errorf("DC/OS cluster %s does not have any running slave", m.ClusterURL)
 	}
+
+	var wg sync.WaitGroup
 
 	for _, a := range state.Slaves {
-		//fmt.Println("Agent: " + a.Id)
-
-		url = m.ClusterURL + fmt.Sprintf(AgentNodeMetricsUrl, a.Id)
-		var nodeMetric metric
-		err = m.handleJsonRequest(url, &nodeMetric)
-		if err != nil {
-			return err
-		}
-
-		processMetric(&nodeMetric, acc, "agent")
-
-		url = m.ClusterURL + fmt.Sprintf(AgentContainersUrl, a.Id)
-		var containerIds []string
-		err = m.handleJsonRequest(url, &containerIds)
-		if err != nil {
-			return err
-		}
-
-		for _, c := range containerIds {
-			//fmt.Println("Container: " + c)
-			url = m.ClusterURL + fmt.Sprintf(AgentContainerMetricsUrl, a.Id, c)
-			var metric metric
-			err = m.handleJsonRequest(url, &metric)
-			if err != nil {
-				return err
-			}
-			processMetric(&metric, acc, "container")
-		}
+		wg.Add(1)
+		go func(agent slave) {
+			acc.AddError(m.gatherAgentMetrics(agent, acc))
+			wg.Done()
+		}(a)
+		wg.Add(1)
+		go func(agent slave) {
+			acc.AddError(m.gatherAgentContainers(agent, acc))
+			wg.Done()
+		}(a)
 	}
 
+	wg.Wait()
+	return nil
+}
+
+//gatherAgentMetrics collects metric for agent
+func (m *Dcos) gatherAgentMetrics(agent slave, acc telegraf.Accumulator) error {
+	url := m.ClusterURL + fmt.Sprintf(AgentNodeMetricsUrl, agent.Id)
+
+	var nodeMetric metric
+	err := m.handleJsonRequest(url, &nodeMetric)
+
+	if err != nil {
+		return err
+	}
+
+	m.processMetric(&nodeMetric, acc, Node)
+	return nil
+}
+
+//gatherAgentContainers manages gathering metrics for containers on agent.
+func (m *Dcos) gatherAgentContainers(agent slave, acc telegraf.Accumulator) error {
+	url := m.ClusterURL + fmt.Sprintf(AgentContainersUrl, agent.Id)
+	var containerIds []string
+	err := m.handleJsonRequest(url, &containerIds)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+
+	for _, c := range containerIds {
+		wg.Add(1)
+		go func(cid string) {
+			acc.AddError(m.gatherContainerMetrics(agent.Id, cid, acc))
+			wg.Done()
+		}(c)
+	}
+	wg.Wait()
+
+	return nil
+}
+
+//gatherContainerMetrics collects metric for given container
+func (m *Dcos) gatherContainerMetrics(agentId string, containerId string, acc telegraf.Accumulator) error {
+	url := m.ClusterURL + fmt.Sprintf(AgentContainerMetricsUrl, agentId, containerId)
+	var metric metric
+	err := m.handleJsonRequest(url, &metric)
+	if err != nil {
+		return err
+	}
+	m.processMetric(&metric, acc, Container)
 	return nil
 }
 
@@ -144,7 +215,13 @@ func (m *Dcos) handleJsonRequest(url string, obj interface{}) error {
 	defer resp.Body.Close()
 
 	if resp.StatusCode/100 != 2 {
-		return fmt.Errorf("HTTP request has failed. HTTP status code: %d", resp.StatusCode)
+		switch resp.StatusCode {
+		case 401:
+			return fmt.Errorf("Authentication error. Verify the auth_token is correct")
+		default:
+			return fmt.Errorf("HTTP request to %s has failed. HTTP status code: %d", url, resp.StatusCode)
+		}
+
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
@@ -160,31 +237,24 @@ func (m *Dcos) handleJsonRequest(url string, obj interface{}) error {
 	return nil
 }
 
-func processMetric(metric *metric, acc telegraf.Accumulator, metricType string) {
+//processMetric validated metric and fills accumulator with metric data
+func (m *Dcos) processMetric(metric *metric, acc telegraf.Accumulator, metricType string) {
 	tags := make(map[string]string)
 	fields := make(map[string]interface{})
 
 	for k, v := range metric.Dimensions {
 		tags[k] = v
 	}
-	tags["Type"] = metricType
+	tags["metric_scope"] = metricType
+	tags["cluster_url"] = m.ClusterURL
 
-	//fmt.Println("Datapoits")
-
-	var t time.Time
-	var err error
-	var timeCollected = false
+	//fmt.Println("Datapoints")
 
 	for _, d := range metric.Datapoints {
-		//all datapoints have the same timestamp, take the first
-		if !timeCollected {
-			t, err = time.Parse(time.RFC3339, d.TimeStamp)
-			if err == nil {
-				timeCollected = true
-			}
+		if m.preProcessDataPoint(&d, metricType) {
+			continue
 		}
-		preProcessDataPoint(&d)
-		fields[d.Name] = d.Unit
+		fields[d.Name] = d.Value
 		//fmt.Printf(" - [ %s: %v%s\n", d.Name, d.Value, d.Unit)
 		//fmt.Printf("      %v]\n", d.Tags)
 		for k, v := range d.Tags {
@@ -195,24 +265,47 @@ func processMetric(metric *metric, acc telegraf.Accumulator, metricType string) 
 	//fmt.Printf("  %v\n", metric.Dimensions)
 	//fmt.Println("AllTags")
 	//fmt.Printf("  %v\n", tags)
-
-	if !timeCollected {
-		t = time.Now()
-	}
-	acc.AddFields("dcos", fields, tags, t)
+	//fmt.Println("Fields")
+	//fmt.Printf("  %v\n",fields)
+	//fmt.Println("Tags")
+	//fmt.Printf("  %v\n", tags)
+	acc.AddFields("dcos", fields, tags)
 }
 
-func preProcessDataPoint(datapoint *datapoint) {
-	for k, v := range datapoint.Tags {
-		switch k {
-		case "interface":
-			datapoint.Name = fmt.Sprintf("%s.%s", datapoint.Name, nameSanitizer.Replace(v))
-			delete(datapoint.Tags, k)
-		case "path":
-			datapoint.Name = datapoint.Name + nameSanitizer.Replace(v)
-			delete(datapoint.Tags, k)
+//preProcessDataPoint checks and filters metric and modifies name in case of group metrics for file system and network. Return true if datapoint  should be added to measurement
+func (m *Dcos) preProcessDataPoint(datapoint *datapoint, metricType string) bool {
+	if metricType == Node {
+		for k, v := range datapoint.Tags {
+			switch k {
+			case "interface":
+				if isItemFiltered(m.NetworkInterfaces, v) {
+					return true
+				}
+				datapoint.Name = fmt.Sprintf("%s.%s", datapoint.Name, nameSanitizer.Replace(v))
+				delete(datapoint.Tags, k)
+			case "path":
+				if isItemFiltered(m.FileSystemMounts, v) {
+					return true
+				}
+				datapoint.Name = datapoint.Name + nameSanitizer.Replace(v)
+				delete(datapoint.Tags, k)
+			}
 		}
 	}
+	return false
+}
+
+//isItemFiltered tests whether item is part of non empty array, if not, returns true
+func isItemFiltered(array []string, item string) bool {
+	if len(array) > 0 {
+		for _, i := range array {
+			if i == item {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
 
 func init() {
