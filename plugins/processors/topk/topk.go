00b007f8d288d16f953fae679eca9c17f1eb5249
Germán Jaber <gjaber@talpor.com>
2017-09-19 22:04:16 -0500

    Add stub for topk processor

--- plugins/processors/topk/topk.go
+++ plugins/processors/topk/topk.go
@@ -1,35 +1,52 @@
 package topk
 
 import (
-	"fmt"
-
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/processors"
 )
 
 type TopK struct {
+	cache map[uint64][]telegraf.Metric
+}
+
+func NewTopK() telegraf.Processor{
+	topk := &TopK{}
+	topk.Reset()
+	return topk
 }
 
 var sampleConfig = `
 `
 
-func (p *TopK) SampleConfig() string {
+func (t *TopK) SampleConfig() string {
 	return sampleConfig
 }
 
-func (p *TopK) Description() string {
+func (t *TopK) Reset() {
+	t.cache = make(map[uint64][]telegraf.Metric)
+}
+
+func (t *TopK) Description() string {
 	return "Print all metrics that pass through this filter."
 }
 
-func (p *TopK) Apply(in ...telegraf.Metric) []telegraf.Metric {
-	for _, metric := range in {
-		fmt.Println(metric.String())
-	}
+func (t *TopK) Apply(in ...telegraf.Metric) []telegraf.Metric {
 	return in
+}
+
+func convert(in interface{}) (float64, bool) {
+	switch v := in.(type) {
+	case float64:
+		return v, true
+	case int64:
+		return float64(v), true
+	default:
+		return 0, false
+	}
 }
 
 func init() {
 	processors.Add("topk", func() telegraf.Processor {
-		return &TopK{}
+		return NewTopK()
 	})
 }
