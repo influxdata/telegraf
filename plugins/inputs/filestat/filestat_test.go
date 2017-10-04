a2bb0925ef746db2b8d9218795d2ecdb2ec614fb
Pierre Tessier <puckpuck.com@gmail.com>
2017-10-04 09:36:04 -0400

    added modification_time field

--- plugins/inputs/filestat/filestat_test.go
+++ plugins/inputs/filestat/filestat_test.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/influxdata/telegraf/internal/globpath"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,8 +26,9 @@ func TestGatherNoMd5(t *testing.T) {
 		"file": dir + "log1.log",
 	}
 	fields1 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags1["file"])),
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
@@ -34,8 +36,9 @@ func TestGatherNoMd5(t *testing.T) {
 		"file": dir + "log2.log",
 	}
 	fields2 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags2["file"])),
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
 
@@ -65,9 +68,10 @@ func TestGatherExplicitFiles(t *testing.T) {
 		"file": dir + "log1.log",
 	}
 	fields1 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
-		"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags1["file"])),
+		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
@@ -75,9 +79,10 @@ func TestGatherExplicitFiles(t *testing.T) {
 		"file": dir + "log2.log",
 	}
 	fields2 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
-		"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags2["file"])),
+		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
 
@@ -105,9 +110,10 @@ func TestGatherGlob(t *testing.T) {
 		"file": dir + "log1.log",
 	}
 	fields1 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
-		"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags1["file"])),
+		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
@@ -115,9 +121,10 @@ func TestGatherGlob(t *testing.T) {
 		"file": dir + "log2.log",
 	}
 	fields2 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
-		"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags2["file"])),
+		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
 }
@@ -137,9 +144,10 @@ func TestGatherSuperAsterisk(t *testing.T) {
 		"file": dir + "log1.log",
 	}
 	fields1 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
-		"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags1["file"])),
+		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
@@ -147,9 +155,10 @@ func TestGatherSuperAsterisk(t *testing.T) {
 		"file": dir + "log2.log",
 	}
 	fields2 := map[string]interface{}{
-		"size_bytes": int64(0),
-		"exists":     int64(1),
-		"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+		"size_bytes":        int64(0),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags2["file"])),
+		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
 
@@ -157,9 +166,10 @@ func TestGatherSuperAsterisk(t *testing.T) {
 		"file": dir + "test.conf",
 	}
 	fields3 := map[string]interface{}{
-		"size_bytes": int64(104),
-		"exists":     int64(1),
-		"md5_sum":    "5a7e9b77fa25e7bb411dbd17cf403c1f",
+		"size_bytes":        int64(104),
+		"exists":            int64(1),
+		"modification_time": int64(getModificationTime(fs, tags3["file"])),
+		"md5_sum":           "5a7e9b77fa25e7bb411dbd17cf403c1f",
 	}
 	acc.AssertContainsTaggedFields(t, "filestat", fields3, tags3)
 }
@@ -172,6 +182,36 @@ func TestGetMd5(t *testing.T) {
 
 	md5, err = getMd5("/tmp/foo/bar/fooooo")
 	assert.Error(t, err)
+}
+
+func getModificationTime(f *FileStat, filepath string) int64 {
+	// This function is a near copy from the main code,
+	// however since the modification time of test files will change based on code pull this seems the only way to solve unit tests
+	// All unit tests should call this function for expected value of modification_time
+
+	var err error
+	g, ok := f.globs[filepath]
+	if !ok {
+		if g, err = globpath.Compile(filepath); err != nil {
+			return 0
+		}
+		f.globs[filepath] = g
+	}
+
+	files := g.Match()
+	if len(files) == 0 {
+		return 0
+	}
+
+	for _, fileInfo := range files {
+		if fileInfo == nil {
+			return 0
+		} else {
+			return fileInfo.ModTime().Unix()
+		}
+	}
+
+	return 0
 }
 
 func getTestdataDir() string {
