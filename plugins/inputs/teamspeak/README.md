cf96153778bbf4b99eaa7099302ddb2047fe5cae
p4ddy1 <mail@lpnw.de>
2017-10-07 22:14:32 +0200

    Input plugin for Teamspeak 3 servers

---
+++ plugins/inputs/teamspeak/README.md
@@ -0,0 +1,42 @@
+# Teamspeak 3 Input Plugin
+
+This plugin uses the Teamspeak 3 ServerQuery interface of the Teamspeak server to collect statistics of one or more
+virtual servers.
+
+### Configuration:
+
+```
+[[inputs.teamspeak]]
+## Server address for Teamspeak 3 ServerQuery
+  server = "127.0.0.1:10011"
+## Username for ServerQuery
+  username = "serveradmin"
+## Password for ServerQuery
+  password = "secret"
+## Array of virtual servers
+  vservers = [1]
+```
+
+### Measurements:
+
+- teamspeak
+    - uptime
+    - clients_online
+    - total_ping
+    - total_packet_loss
+    - packets_sent_total
+    - packets_received_total
+    - bytes_sent_total
+    - bytes_received_total
+
+### Tags:
+
+- The following tags are used:
+    - v_server
+    - name
+
+### Example output:
+
+```
+teamspeak,v_server=1,name=LeopoldsServer,host=vm01 bytes_received_total=29638202639i,uptime=13567846i,total_ping=26.89,total_packet_loss=0,packets_sent_total=415821252i,packets_received_total=237069900i,bytes_sent_total=55309568252i,clients_online=11i 1507406561000000000
+```
---
+++ plugins/inputs/teamspeak/teamspeak.go
@@ -0,0 +1,90 @@
+package teamspeak
+
+import (
+	"github.com/multiplay/go-ts3"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"strconv"
+)
+
+type Teamspeak struct {
+	Server   string
+	Username string
+	Password string
+	Vservers []int
+}
+
+func (ts *Teamspeak) Description() string {
+	return "Reads metrics from a Teamspeak 3 Server via ServerQuery"
+}
+
+const sampleConfig = `
+  ## Server address for Teamspeak 3 ServerQuery
+  # server = "127.0.0.1:10011"
+  ## Username for ServerQuery
+  # username = "serveradmin"
+  ## Password for ServerQuery
+  # password = "secret"
+  ## Array of virtual servers
+  # vservers = [1]
+`
+
+func (ts *Teamspeak) SampleConfig() string {
+	return sampleConfig
+}
+
+func (ts *Teamspeak) Gather(acc telegraf.Accumulator) error {
+	var err error
+	client, err := ts3.NewClient(ts.Server)
+
+	if err != nil {
+		return err
+	}
+
+	err = client.Login(ts.Username, ts.Password)
+
+	if err != nil {
+		return err
+	}
+
+	for _, vserver := range ts.Vservers {
+		client.Use(vserver)
+
+		sm, err := client.Server.Info()
+		if err != nil {
+			return err
+		}
+
+		sc, err := client.Server.ServerConnectionInfo()
+		if err != nil {
+			return err
+		}
+
+		tags := map[string]string{
+			"v_server": strconv.Itoa(sm.ID),
+			"name":     sm.Name,
+		}
+
+		fields := map[string]interface{}{
+			"uptime":                 sm.Uptime,
+			"clients_online":         sm.ClientsOnline,
+			"total_ping":             sm.TotalPing,
+			"total_packet_loss":      sm.TotalPacketLossTotal,
+			"packets_sent_total":     sc.PacketsSentTotal,
+			"packets_received_total": sc.PacketsReceivedTotal,
+			"bytes_sent_total":       sc.BytesSentTotal,
+			"bytes_received_total":   sc.BytesReceivedTotal,
+		}
+
+		acc.AddFields("teamspeak", fields, tags)
+	}
+	client.Close()
+	return nil
+}
+
+func init() {
+	inputs.Add("teamspeak", func() telegraf.Input {
+		return &Teamspeak{}
+	})
+}
