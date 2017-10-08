8b25cad2d3a33c158c4aac173bb84b90ddd53a34
Pierre Tessier <puckpuck.com@gmail.com>
2017-10-07 20:59:23 -0400

    updated all tests
    Test each field individually and modification_time is an int64 when exists is 1

--- plugins/inputs/filestat/filestat_test.go
+++ plugins/inputs/filestat/filestat_test.go
@@ -1,11 +1,11 @@
 package filestat
 
 import (
+	"reflect"
 	"runtime"
 	"strings"
 	"testing"
 
-	"github.com/influxdata/telegraf/internal/globpath"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 )
@@ -22,33 +22,57 @@ func TestGatherNoMd5(t *testing.T) {
 	acc := testutil.Accumulator{}
 	acc.GatherError(fs.Gather)
 
-	tags1 := map[string]string{
-		"file": dir + "log1.log",
+	var testMetrics = []struct {
+		Tags   map[string]string
+		Fields map[string]interface{}
+	}{
+		{
+			map[string]string{"file": dir + "log1.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+			},
+		},
+		{
+			map[string]string{"file": dir + "log2.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+			},
+		},
+		{
+			map[string]string{"file": "/non/existant/file"},
+			map[string]interface{}{
+				"exists": int64(0),
+			},
+		},
 	}
-	fields1 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags1["file"])),
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
-	tags2 := map[string]string{
-		"file": dir + "log2.log",
-	}
-	fields2 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags2["file"])),
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
+	for _, m := range acc.Metrics {
+		foundTags := false
+		for _, tm := range testMetrics {
+			if reflect.DeepEqual(tm.Tags, m.Tags) {
 
-	tags3 := map[string]string{
-		"file": "/non/existant/file",
+				for k, v := range tm.Fields {
+					if m.Fields[k] != v {
+						t.Errorf("\nFailed on %s Field\n\texpected\t%+v\n\treceived\t%+v\n", k, v, m.Fields[k])
+					}
+				}
+
+				if m.Fields["exists"] == 1 {
+					if _, ok := m.Fields["modification_time"].(int64); !ok {
+						t.Error("modification_time Field is not set")
+					}
+				}
+
+				foundTags = true
+				break
+			}
+		}
+		if !foundTags {
+			t.Errorf("\nFailed, could not find matching tags\n\t%+v", m.Tags)
+		}
 	}
-	fields3 := map[string]interface{}{
-		"exists": int64(0),
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields3, tags3)
 }
 
 func TestGatherExplicitFiles(t *testing.T) {
@@ -64,35 +88,59 @@ func TestGatherExplicitFiles(t *testing.T) {
 	acc := testutil.Accumulator{}
 	acc.GatherError(fs.Gather)
 
-	tags1 := map[string]string{
-		"file": dir + "log1.log",
+	var testMetrics = []struct {
+		Tags   map[string]string
+		Fields map[string]interface{}
+	}{
+		{
+			map[string]string{"file": dir + "log1.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+				"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+			},
+		},
+		{
+			map[string]string{"file": dir + "log2.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+				"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+			},
+		},
+		{
+			map[string]string{"file": "/non/existant/file"},
+			map[string]interface{}{
+				"exists": int64(0),
+			},
+		},
 	}
-	fields1 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags1["file"])),
-		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
-	tags2 := map[string]string{
-		"file": dir + "log2.log",
-	}
-	fields2 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags2["file"])),
-		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
+	for _, m := range acc.Metrics {
+		foundTags := false
+		for _, tm := range testMetrics {
+			if reflect.DeepEqual(tm.Tags, m.Tags) {
 
-	tags3 := map[string]string{
-		"file": "/non/existant/file",
+				for k, v := range tm.Fields {
+					if m.Fields[k] != v {
+						t.Errorf("\nFailed on %s Field\n\texpected\t%+v\n\treceived\t%+v\n", k, v, m.Fields[k])
+					}
+				}
+
+				if m.Fields["exists"] == 1 {
+					if _, ok := m.Fields["modification_time"].(int64); !ok {
+						t.Error("modification_time Field is not set")
+					}
+				}
+
+				foundTags = true
+				break
+			}
+		}
+		if !foundTags {
+			t.Errorf("\nFailed, could not find matching tags\n\t%+v", m.Tags)
+		}
 	}
-	fields3 := map[string]interface{}{
-		"exists": int64(0),
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields3, tags3)
 }
 
 func TestGatherGlob(t *testing.T) {
@@ -106,27 +154,53 @@ func TestGatherGlob(t *testing.T) {
 	acc := testutil.Accumulator{}
 	acc.GatherError(fs.Gather)
 
-	tags1 := map[string]string{
-		"file": dir + "log1.log",
+	var testMetrics = []struct {
+		Tags   map[string]string
+		Fields map[string]interface{}
+	}{
+		{
+			map[string]string{"file": dir + "log1.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+				"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+			},
+		},
+		{
+			map[string]string{"file": dir + "log2.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+				"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+			},
+		},
 	}
-	fields1 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags1["file"])),
-		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
-	tags2 := map[string]string{
-		"file": dir + "log2.log",
+	for _, m := range acc.Metrics {
+		foundTags := false
+		for _, tm := range testMetrics {
+			if reflect.DeepEqual(tm.Tags, m.Tags) {
+
+				for k, v := range tm.Fields {
+					if m.Fields[k] != v {
+						t.Errorf("\nFailed on %s Field\n\texpected\t%+v\n\treceived\t%+v\n", k, v, m.Fields[k])
+					}
+				}
+
+				if m.Fields["exists"] == 1 {
+					if _, ok := m.Fields["modification_time"].(int64); !ok {
+						t.Error("modification_time Field is not set")
+					}
+				}
+
+				foundTags = true
+				break
+			}
+		}
+		if !foundTags {
+			t.Errorf("\nFailed, could not find matching tags\n\t%+v", m.Tags)
+		}
 	}
-	fields2 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags2["file"])),
-		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
 }
 
 func TestGatherSuperAsterisk(t *testing.T) {
@@ -140,38 +214,61 @@ func TestGatherSuperAsterisk(t *testing.T) {
 	acc := testutil.Accumulator{}
 	acc.GatherError(fs.Gather)
 
-	tags1 := map[string]string{
-		"file": dir + "log1.log",
+	var testMetrics = []struct {
+		Tags   map[string]string
+		Fields map[string]interface{}
+	}{
+		{
+			map[string]string{"file": dir + "log1.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+				"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+			},
+		},
+		{
+			map[string]string{"file": dir + "log2.log"},
+			map[string]interface{}{
+				"size_bytes": int64(0),
+				"exists":     int64(1),
+				"md5_sum":    "d41d8cd98f00b204e9800998ecf8427e",
+			},
+		},
+		{
+			map[string]string{"file": dir + "test.conf"},
+			map[string]interface{}{
+				"size_bytes": int64(104),
+				"exists":     int64(1),
+				"md5_sum":    "5a7e9b77fa25e7bb411dbd17cf403c1f",
+			},
+		},
 	}
-	fields1 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags1["file"])),
-		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields1, tags1)
 
-	tags2 := map[string]string{
-		"file": dir + "log2.log",
-	}
-	fields2 := map[string]interface{}{
-		"size_bytes":        int64(0),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags2["file"])),
-		"md5_sum":           "d41d8cd98f00b204e9800998ecf8427e",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields2, tags2)
+	for _, m := range acc.Metrics {
+		foundTags := false
+		for _, tm := range testMetrics {
+			if reflect.DeepEqual(tm.Tags, m.Tags) {
 
-	tags3 := map[string]string{
-		"file": dir + "test.conf",
+				for k, v := range tm.Fields {
+					if m.Fields[k] != v {
+						t.Errorf("\nFailed on %s Field\n\texpected\t%+v\n\treceived\t%+v\n", k, v, m.Fields[k])
+					}
+				}
+
+				if m.Fields["exists"] == 1 {
+					if _, ok := m.Fields["modification_time"].(int64); !ok {
+						t.Error("modification_time Field is not set")
+					}
+				}
+
+				foundTags = true
+				break
+			}
+		}
+		if !foundTags {
+			t.Errorf("\nFailed, could not find matching tags\n\t%+v", m.Tags)
+		}
 	}
-	fields3 := map[string]interface{}{
-		"size_bytes":        int64(104),
-		"exists":            int64(1),
-		"modification_time": int64(getModificationTime(fs, tags3["file"])),
-		"md5_sum":           "5a7e9b77fa25e7bb411dbd17cf403c1f",
-	}
-	acc.AssertContainsTaggedFields(t, "filestat", fields3, tags3)
 }
 
 func TestGetMd5(t *testing.T) {
@@ -182,36 +279,6 @@ func TestGetMd5(t *testing.T) {
 
 	md5, err = getMd5("/tmp/foo/bar/fooooo")
 	assert.Error(t, err)
-}
-
-func getModificationTime(f *FileStat, filepath string) int64 {
-	// This function is a near copy from the main code,
-	// however since the modification time of test files will change based on code pull this seems the only way to solve unit tests
-	// All unit tests should call this function for expected value of modification_time
-
-	var err error
-	g, ok := f.globs[filepath]
-	if !ok {
-		if g, err = globpath.Compile(filepath); err != nil {
-			return 0
-		}
-		f.globs[filepath] = g
-	}
-
-	files := g.Match()
-	if len(files) == 0 {
-		return 0
-	}
-
-	for _, fileInfo := range files {
-		if fileInfo == nil {
-			return 0
-		} else {
-			return fileInfo.ModTime().UnixNano()
-		}
-	}
-
-	return 0
 }
 
 func getTestdataDir() string {
