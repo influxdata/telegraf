8d9b9a2b19930ddf89d75e97f89f84040780a0a7
Sven Klemm <sven.klemm@sql-ag.de>
2017-11-05 20:29:49 +0100

    fix tests

--- plugins/outputs/postgresql/postgresql_test.go
+++ plugins/outputs/postgresql/postgresql_test.go
@@ -35,25 +35,18 @@ func TestPostgresqlInsertStatement(t *testing.T) {
 	p := Postgresql{}
 	timestamp := time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC)
 
-	var m telegraf.Metric
-	m, _ = metric.New("m", nil, map[string]interface{}{"f": float64(3.14)}, timestamp)
-	sql, values := p.generateInsert(m)
+	sql, _ := p.generateInsert("m", []string{"time", "f"}, []interface{}{timestamp, 3.1})
 	assert.Equal(t, "INSERT INTO m(time,f) VALUES($1,$2)", sql)
-	assert.EqualValues(t, []interface{}{timestamp, float64(3.14)}, values)
 
-	m, _ = metric.New("m", nil, map[string]interface{}{"i": int(3)}, timestamp)
-	sql, values = p.generateInsert(m)
+	sql, _ = p.generateInsert("m", []string{"time", "i"}, []interface{}{timestamp, 3})
 	assert.Equal(t, "INSERT INTO m(time,i) VALUES($1,$2)", sql)
 
-	m, _ = metric.New("m", nil, map[string]interface{}{"f": float64(3.14), "i": int(3)}, timestamp)
-	sql, values = p.generateInsert(m)
+	sql, _ = p.generateInsert("m", []string{"time", "f", "i"}, []interface{}{timestamp, 3.1, 3})
 	assert.Equal(t, "INSERT INTO m(time,f,i) VALUES($1,$2,$3)", sql)
 
-	m, _ = metric.New("m", map[string]string{"k": "v"}, map[string]interface{}{"i": int(3)}, timestamp)
-	sql, values = p.generateInsert(m)
+	sql, _ = p.generateInsert("m", []string{"time", "k", "i"}, []interface{}{timestamp, "v", 3})
 	assert.Equal(t, "INSERT INTO m(time,k,i) VALUES($1,$2,$3)", sql)
 
-	m, _ = metric.New("m", map[string]string{"k1": "v1", "k2": "v2"}, map[string]interface{}{"i": int(3)}, timestamp)
-	sql, values = p.generateInsert(m)
+	sql, _ = p.generateInsert("m", []string{"time", "k1", "k2", "i"}, []interface{}{timestamp, "v1", "v2", 3})
 	assert.Equal(t, "INSERT INTO m(time,k1,k2,i) VALUES($1,$2,$3,$4)", sql)
 }
