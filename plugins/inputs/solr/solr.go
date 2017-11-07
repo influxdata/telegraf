cbace99af1566bda80054a0c63046d3d49a32194
Lukasz Jagiello <lukasz@wikia-inc.com>
2017-11-07 15:51:02 -0800

    Shorten version of getFloat

--- plugins/inputs/solr/solr.go
+++ plugins/inputs/solr/solr.go
@@ -354,20 +354,6 @@ func getFloat(unk interface{}) (float64, error) {
 	switch i := unk.(type) {
 	case float64:
 		return i, nil
-	case float32:
-		return float64(i), nil
-	case int64:
-		return float64(i), nil
-	case int32:
-		return float64(i), nil
-	case int:
-		return float64(i), nil
-	case uint64:
-		return float64(i), nil
-	case uint32:
-		return float64(i), nil
-	case uint:
-		return float64(i), nil
 	case string:
 		return strconv.ParseFloat(i, 64)
 	default:
