01341c20ee0af9ccd38b6be8fefc3b52f0190979
dhendel <dustinhendel@gmail.com>
2017-11-27 13:57:52 -0600

    added rancher plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -72,6 +72,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/puppetagent"
 	_ "github.com/influxdata/telegraf/plugins/inputs/rabbitmq"
 	_ "github.com/influxdata/telegraf/plugins/inputs/raindrops"
+	_ "github.com/influxdata/telegraf/plugins/inputs/rancher"
 	_ "github.com/influxdata/telegraf/plugins/inputs/redis"
 	_ "github.com/influxdata/telegraf/plugins/inputs/rethinkdb"
 	_ "github.com/influxdata/telegraf/plugins/inputs/riak"
