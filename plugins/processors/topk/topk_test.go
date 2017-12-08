cb2e9bbaaa396db5d4ccbb40887a8e3f986f8fc0
Germán Jaber <gjaber@talpor.com>
2017-12-07 22:24:02 -0600

    TopK processor: Add smoke test for the sum, min and max aggregators, and skeleton for the rest of the tests

--- plugins/processors/topk/topk_test.go
+++ plugins/processors/topk/topk_test.go
@@ -32,12 +32,13 @@ func equalSets(l1 []telegraf.Metric, l2 []telegraf.Metric) bool {
 	return subSet(l1, l2) && subSet(l2, l1)
 }
 
+// Smoke avg
 func TestTopkAvgSmokeTest(t *testing.T) {
 	var topk TopK
 	topk = NewTopK()
 	topk.Period = 1
 	topk.Fields = []string{"a"}
-	topk.GroupBy = []string{"tag_value1"}
+	topk.GroupBy = []string{"tag_name"}
 
 	period := time.Second * time.Duration(topk.Period)
 	time.Sleep(period)
@@ -49,3 +50,76 @@ func TestTopkAvgSmokeTest(t *testing.T) {
 		t.Error("\nExpected metrics:\n", answer, "\nReturned metrics:\n", ret)
 	}
 }
+
+// Smoke sum
+func TestTopkSumSmokeTest(t *testing.T) {
+	var topk TopK
+	topk = NewTopK()
+	topk.Period = 1
+	topk.Aggregation = "sum"
+	topk.Fields = []string{"a"}
+	topk.GroupBy = []string{"tag_name"}
+
+	period := time.Second * time.Duration(topk.Period)
+	time.Sleep(period)
+
+	ret := topk.Apply(MetricsSet1...)
+	answer := MetricsSet1
+
+	if ! equalSets(ret, answer) {
+		t.Error("\nExpected metrics:\n", answer, "\nReturned metrics:\n", ret)
+	}
+}
+
+// Smoke max
+func TestTopkMaxSmokeTest(t *testing.T) {
+	var topk TopK
+	topk = NewTopK()
+	topk.Period = 1
+	topk.Aggregation = "max"
+	topk.Fields = []string{"a"}
+	topk.GroupBy = []string{"tag_name"}
+
+	period := time.Second * time.Duration(topk.Period)
+	time.Sleep(period)
+
+	ret := topk.Apply(MetricsSet1...)
+	answer := MetricsSet1
+
+	if ! equalSets(ret, answer) {
+		t.Error("\nExpected metrics:\n", answer, "\nReturned metrics:\n", ret)
+	}
+}
+
+// Smoke min
+func TestTopkMinSmokeTest(t *testing.T) {
+	var topk TopK
+	topk = NewTopK()
+	topk.Period = 1
+	topk.Aggregation = "min"
+	topk.Fields = []string{"a"}
+	topk.GroupBy = []string{"tag_name"}
+
+	period := time.Second * time.Duration(topk.Period)
+	time.Sleep(period)
+
+	ret := topk.Apply(MetricsSet1...)
+	answer := MetricsSet1
+
+	if ! equalSets(ret, answer) {
+		t.Error("\nExpected metrics:\n", answer, "\nReturned metrics:\n", ret)
+	}
+}
+
+// AggregationField + avg
+// AggregationField + sum
+// AggregationField + max
+// AggregationField + min
+
+// GroupBy
+// GroupBy + Fields
+// GroupBy metric name
+// GroupBy + GroupBy metric name
+// DropNoGroup
+// DropNonTop=false + PositionField
+// BottomK
