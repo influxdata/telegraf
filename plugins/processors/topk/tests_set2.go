773ebffb6e969989ede0860ae2856b17e033e3b2
Germán Jaber <gjaber@talpor.com>
2017-12-13 13:18:55 -0600

    Topk processor: Refactor the way in wich the test metrics are generated

--- plugins/processors/topk/tests_set2.go
+++ plugins/processors/topk/tests_set2.go
@@ -98,67 +98,16 @@ var metric26, _ = metric.New(
 
 var MetricsSet2 = []telegraf.Metric{metric21, metric22, metric23, metric24, metric25, metric26}
 
-var ans2groupby13, _ = metric.New(
-	"metric1",
-	map[string]string{
-		"tag1": "TWO",
-		"tag2": "FOUR",
-		"tag3": "SIX",
-	},
-	map[string]interface{}{
-		"value": float64(74.18),
-		"A": float64(77.42),
-		"B": float64(60.96),
-		"sumag_value": float64(74.18),
-	},
-	metric23.Time(),
-)
 
-var ans2groupby14, _ = metric.New(
-	"metric2",
-	map[string]string{
-		"tag1": "ONE",
-		"tag2": "FIVE",
-		"tag3": "THREE",
-	},
-	map[string]interface{}{
-		"value": float64(72),
-		"B": float64(22.1),
-		"C": float64(30.8),
-		"sumag_value": float64(72),
-	},
-	metric24.Time(),
-)
+var ans23groupby1 = metric23.Copy()
+var ans24groupby1 = metric24.Copy()
+var ans25groupby1 = metric25.Copy()
+var ans26groupby1 = metric26.Copy()
+var GroupBy0Ans = []telegraf.Metric{ans23groupby1, ans24groupby1, ans25groupby1, ans26groupby1}
 
-var ans2groupby15, _ = metric.New(
-	"metric2",
-	map[string]string{
-		"tag1": "TWO",
-		"tag2": "FOUR",
-		"tag3": "SEVEN",
-	},
-	map[string]interface{}{
-		"value": float64(87.92),
-		"B": float64(81.55),
-		"C": float64(45.1),
-		"sumag_value": float64(163.22),
-	},
-	metric25.Time(),
-)
-
-var ans2groupby16, _ = metric.New(
-	"metric2",
-	map[string]string{
-		"tag1": "TWO",
-		"tag2": "FIVE",
-		"tag3": "SEVEN",
-	},
-	map[string]interface{}{
-		"value": float64(75.3),
-		"A": float64(29.45),
-		"C": float64(4.86),
-		"sumag_value": float64(163.22),
-	},
-	metric26.Time(),
-)
-var GroupBy0Ans = []telegraf.Metric{ans2groupby13, ans2groupby14, ans2groupby15, ans2groupby16}
+func setupTestSet2(){
+	ans23groupby1.AddField("sumag_value", float64(74.18))
+	ans24groupby1.AddField("sumag_value", float64(72))
+	ans25groupby1.AddField("sumag_value", float64(163.22))
+	ans26groupby1.AddField("sumag_value", float64(163.22))
+}
