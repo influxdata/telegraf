979a0a6c6f5afc2d7cd2168635cdcfc81989ce78
Ildar Svetlov <svetlov.ildar@gmail.com>
2017-12-13 23:13:56 +0400

    Don't add system input uptime_format as a counter (#3578)
    (cherry picked from commit d935dfa9edd42c8ce5cbbffadf6ec23c815bb47d)

--- plugins/outputs/prometheus_client/prometheus_client.go
+++ plugins/outputs/prometheus_client/prometheus_client.go
@@ -96,7 +96,6 @@ var sampleConfig = `
   string_as_label = true
 `
 
-<<<<<<< HEAD
 func (p *PrometheusClient) basicAuth(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if p.BasicUsername != "" && p.BasicPassword != "" {
@@ -115,8 +114,6 @@ func (p *PrometheusClient) basicAuth(h http.Handler) http.Handler {
 	})
 }
 
-=======
->>>>>>> Fix separation of multiple prometheus_client outputs (#3570)
 func (p *PrometheusClient) Start() error {
 	defaultCollectors := map[string]bool{
 		"gocollector": true,
@@ -149,13 +146,8 @@ func (p *PrometheusClient) Start() error {
 	}
 
 	mux := http.NewServeMux()
-<<<<<<< HEAD
 	mux.Handle(p.Path, p.basicAuth(promhttp.HandlerFor(
 		registry, promhttp.HandlerOpts{ErrorHandling: promhttp.ContinueOnError})))
-=======
-	mux.Handle(p.Path, promhttp.HandlerFor(
-		registry, promhttp.HandlerOpts{ErrorHandling: promhttp.ContinueOnError}))
->>>>>>> Fix separation of multiple prometheus_client outputs (#3570)
 
 	p.server = &http.Server{
 		Addr:    p.Listen,
