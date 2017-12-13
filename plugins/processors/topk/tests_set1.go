f53355cac08d258b801d152f5b9d0bbc94d15f0c
Germán Jaber <gjaber@talpor.com>
2017-12-13 13:18:55 -0600

    Topk processor: Refactor the way in wich the test metrics are generated

--- plugins/processors/topk/tests_set1.go
+++ plugins/processors/topk/tests_set1.go
@@ -58,237 +58,60 @@ var metric15, _ = metric.New(
 
 var MetricsSet1 = []telegraf.Metric{metric11, metric12, metric13, metric14, metric15}
 
-// Expected answer for the TopkAvgAggretationField test
-var ans_avg_11, _ = metric.New(
-	"1one",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(15.3),
-		"b": float64(40),
-		"avgag_a": float64(28.044),
-	},
-	metric11.Time(),
-)
-
-var ans_avg_12, _ = metric.New(
-	"1two",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50),
-		"avgag_a": float64(28.044),
-	},
-	metric12.Time(),
-)
-
-var ans_avg_13, _ = metric.New(
-	"1three",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(0.3),
-		"c": float64(400),
-		"avgag_a": float64(28.044),
-	},
-	metric13.Time(),
-)
-
-var ans_avg_14, _ = metric.New(
-	"1four",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(24.12),
-		"b": float64(40),
-		"avgag_a": float64(28.044),
-	},
-	metric14.Time(),
-)
-
-var ans_avg_15, _ = metric.New(
-	"1five",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50.5),
-		"h": float64(1),
-		"u": float64(2.4),
-		"avgag_a": float64(28.044),
-	},
-	metric15.Time(),
-)
-
+var ans_avg_11 = metric11.Copy()
+var ans_avg_12 = metric12.Copy()
+var ans_avg_13 = metric13.Copy()
+var ans_avg_14 = metric14.Copy()
+var ans_avg_15 = metric15.Copy()
 var AvgAggregationFieldAns = []telegraf.Metric{ans_avg_11, ans_avg_12, ans_avg_13, ans_avg_14, ans_avg_15}
 
-
-// Expected answer for the TopkSumAggretationField test
-var ans_sum_11, _ = metric.New(
-	"1one",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(15.3),
-		"b": float64(40),
-		"sumag_a": float64(140.22),
-	},
-	metric11.Time(),
-)
-
-var ans_sum_12, _ = metric.New(
-	"1two",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50),
-		"sumag_a": float64(140.22),
-	},
-	metric12.Time(),
-)
-
-var ans_sum_13, _ = metric.New(
-	"1three",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(0.3),
-		"c": float64(400),
-		"sumag_a": float64(140.22),
-	},
-	metric13.Time(),
-)
-
-var ans_sum_14, _ = metric.New(
-	"1four",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(24.12),
-		"b": float64(40),
-		"sumag_a": float64(140.22),
-	},
-	metric14.Time(),
-)
-
-var ans_sum_15, _ = metric.New(
-	"1five",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50.5),
-		"h": float64(1),
-		"u": float64(2.4),
-		"sumag_a": float64(140.22),
-	},
-	metric15.Time(),
-)
-
+var ans_sum_11 = metric11.Copy()
+var ans_sum_12 = metric12.Copy()
+var ans_sum_13 = metric13.Copy()
+var ans_sum_14 = metric14.Copy()
+var ans_sum_15 = metric15.Copy()
 var SumAggregationFieldAns = []telegraf.Metric{ans_sum_11, ans_sum_12, ans_sum_13, ans_sum_14, ans_sum_15}
 
-
-// Expected answer for the TopkSumAggretationField test
-var ans_max_11, _ = metric.New(
-	"1one",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(15.3),
-		"b": float64(40),
-		"maxag_a": float64(50.5),
-	},
-	metric11.Time(),
-)
-
-var ans_max_12, _ = metric.New(
-	"1two",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50),
-		"maxag_a": float64(50.5),
-	},
-	metric12.Time(),
-)
-
-var ans_max_13, _ = metric.New(
-	"1three",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(0.3),
-		"c": float64(400),
-		"maxag_a": float64(50.5),
-	},
-	metric13.Time(),
-)
-
-var ans_max_14, _ = metric.New(
-	"1four",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(24.12),
-		"b": float64(40),
-		"maxag_a": float64(50.5),
-	},
-	metric14.Time(),
-)
-
-var ans_max_15, _ = metric.New(
-	"1five",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50.5),
-		"h": float64(1),
-		"u": float64(2.4),
-		"maxag_a": float64(50.5),
-	},
-	metric15.Time(),
-)
-
+var ans_max_11 = metric11.Copy()
+var ans_max_12 = metric12.Copy()
+var ans_max_13 = metric13.Copy()
+var ans_max_14 = metric14.Copy()
+var ans_max_15 = metric15.Copy()
 var MaxAggregationFieldAns = []telegraf.Metric{ans_max_11, ans_max_12, ans_max_13, ans_max_14, ans_max_15}
 
-
-// Expected answer for the TopkSumAggretationField test
-var ans_min_11, _ = metric.New(
-	"1one",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(15.3),
-		"b": float64(40),
-		"minag_a": float64(0.3),
-	},
-	metric11.Time(),
-)
-
-var ans_min_12, _ = metric.New(
-	"1two",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50),
-		"minag_a": float64(0.3),
-	},
-	metric12.Time(),
-)
-
-var ans_min_13, _ = metric.New(
-	"1three",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(0.3),
-		"c": float64(400),
-		"minag_a": float64(0.3),
-	},
-	metric13.Time(),
-)
-
-var ans_min_14, _ = metric.New(
-	"1four",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(24.12),
-		"b": float64(40),
-		"minag_a": float64(0.3),
-	},
-	metric14.Time(),
-)
-
-var ans_min_15, _ = metric.New(
-	"1five",
-	map[string]string{"tag_name": "tag_value1"},
-	map[string]interface{}{
-		"a": float64(50.5),
-		"h": float64(1),
-		"u": float64(2.4),
-		"minag_a": float64(0.3),
-	},
-	metric15.Time(),
-)
-
+var ans_min_11 = metric11.Copy()
+var ans_min_12 = metric12.Copy()
+var ans_min_13 = metric13.Copy()
+var ans_min_14 = metric14.Copy()
+var ans_min_15 = metric15.Copy()
 var MinAggregationFieldAns = []telegraf.Metric{ans_min_11, ans_min_12, ans_min_13, ans_min_14, ans_min_15}
+
+func setupTestSet1(){
+	// Expected answer for the TopkAvgAggretationField test
+	ans_avg_11.AddField("avgag_a", float64(28.044))
+	ans_avg_12.AddField("avgag_a", float64(28.044))
+	ans_avg_13.AddField("avgag_a", float64(28.044))
+	ans_avg_14.AddField("avgag_a", float64(28.044))
+	ans_avg_15.AddField("avgag_a", float64(28.044))
+
+	// Expected answer for the TopkSumAggretationField test
+	ans_sum_11.AddField("sumag_a", float64(140.22))
+	ans_sum_12.AddField("sumag_a", float64(140.22))
+	ans_sum_13.AddField("sumag_a", float64(140.22))
+	ans_sum_14.AddField("sumag_a", float64(140.22))
+	ans_sum_15.AddField("sumag_a", float64(140.22))
+
+	// Expected answer for the TopkMaxAggretationField test
+	ans_max_11.AddField("maxag_a", float64(50.5))
+	ans_max_12.AddField("maxag_a", float64(50.5))
+	ans_max_13.AddField("maxag_a", float64(50.5))
+	ans_max_14.AddField("maxag_a", float64(50.5))
+	ans_max_15.AddField("maxag_a", float64(50.5))
+
+	// Expected answer for the TopkMinAggretationField test
+	ans_min_11.AddField("minag_a", float64(0.3))
+	ans_min_12.AddField("minag_a", float64(0.3))
+	ans_min_13.AddField("minag_a", float64(0.3))
+	ans_min_14.AddField("minag_a", float64(0.3))
+	ans_min_15.AddField("minag_a", float64(0.3))
+}
