452f496fd7ef15834a431466f564c1111e526f27
Ben Aldrich <vrecan@gmail.com>
2017-12-14 22:22:01 -0700

    revert godeps to old version of gopsutil and implement it directly

--- plugins/inputs/procstat/pgrep_windows.go
+++ plugins/inputs/procstat/pgrep_windows.go
@@ -1,12 +1,19 @@
 package procstat
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"time"
 
+	"github.com/StackExchange/wmi"
 	"github.com/shirou/gopsutil/process"
 )
 
 var ErrorNotImplemented = errors.New("not implemented in windows")
+
+//Timeout is the timeout used when making wmi calls
+var Timeout = 5 * time.Second
 
 // Implemention of PIDGatherer that execs pgrep to find processes
 type Pgrep struct {
@@ -22,7 +29,7 @@ func (pg *Pgrep) PidFile(path string) ([]PID, error) {
 
 func (pg *Pgrep) Pattern(pattern string) ([]PID, error) {
 	pids := make([]PID, 0)
-	procs, err := process.GetWin32ProcsByName(pattern)
+	procs, err := GetWin32ProcsByName(pattern, Timeout)
 	if err != nil {
 		return pids, err
 	}
@@ -34,7 +41,7 @@ func (pg *Pgrep) Pattern(pattern string) ([]PID, error) {
 
 func (pg *Pgrep) FullPattern(pattern string) ([]PID, error) {
 	pids := make([]PID, 0)
-	procs, err := process.GetWin32ProcsByCmdLine(pattern)
+	procs, err := GetWin32ProcsByCmdLine(pattern, Timeout)
 	if err != nil {
 		return pids, err
 	}
@@ -46,4 +53,45 @@ func (pg *Pgrep) FullPattern(pattern string) ([]PID, error) {
 
 func (pg *Pgrep) Uid(user string) ([]PID, error) {
 	return nil, ErrorNotImplemented
+}
+
+func GetWin32ProcsByCmdLine(cmdLine string, timeout time.Duration) ([]process.Win32_Process, error) {
+	var dst []process.Win32_Process
+	query := fmt.Sprint("WHERE CommandLine LIKE \"%", cmdLine, "%\"")
+	q := wmi.CreateQuery(&dst, query)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	err := WMIQueryWithContext(ctx, q, &dst)
+	if err != nil {
+		return []process.Win32_Process{}, fmt.Errorf("could not get win32Proc: %s", err)
+	}
+	return dst, nil
+}
+
+func GetWin32ProcsByName(name string, timeout time.Duration) ([]process.Win32_Process, error) {
+	var dst []process.Win32_Process
+	query := fmt.Sprint("WHERE Name LIKE \"%", name, "%\"")
+	q := wmi.CreateQuery(&dst, query)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	err := WMIQueryWithContext(ctx, q, &dst)
+	if err != nil {
+		return []process.Win32_Process{}, fmt.Errorf("could not get win32Proc: %s", err)
+	}
+	return dst, nil
+}
+
+// WMIQueryWithContext - wraps wmi.Query with a timed-out context to avoid hanging
+func WMIQueryWithContext(ctx context.Context, query string, dst interface{}, connectServerArgs ...interface{}) error {
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- wmi.Query(query, dst, connectServerArgs...)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-errChan:
+		return err
+	}
 }
