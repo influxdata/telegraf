39e6e742fb3a6d5dd788a4fc516be464fef985cd
Laurent Sesques <laurent.sesques@canonical.com>
2018-01-01 18:07:56 +0100

    add unit tests, README updates and gofmt run

--- plugins/inputs/socketstat/socketstat.go
+++ plugins/inputs/socketstat/socketstat.go
@@ -5,8 +5,8 @@ package socketstat
 import (
 	"os/exec"
 	"regexp"
-	"strings"
 	"strconv"
+	"strings"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
@@ -67,7 +67,7 @@ func (ss *Socketstat) socketList(proto string) (string, error) {
 	cmdName := ssPath
 	var args []string
 	args = append(args, "-in")
-	args = append(args, "--" + proto)
+	args = append(args, "--"+proto)
 	c := exec.Command(cmdName, args...)
 	out, err := c.Output()
 	return string(out), err
@@ -84,68 +84,71 @@ func (ss *Socketstat) parseAndGather(data, proto string, acc telegraf.Accumulato
 	}
 	tags := map[string]string{}
 	fields := make(map[string]interface{})
-        flushData := false
+	flushData := false
 	for _, line := range lines[1:] {
 		words := strings.Fields(line)
-                if line == "" {
-                        continue
-                }
-                var err error
-		if ! beginsWithBlank.MatchString(line) {
-                        if flushData {
-                                acc.AddFields(measurement, fields, tags)
-                                flushData = false
-                        }
-                        tags, fields = getTagsAndState(proto, words)
-                        flushData = true
+		if line == "" {
+			continue
+		}
+		var err error
+		if !beginsWithBlank.MatchString(line) {
+			if flushData {
+				acc.AddFields(measurement, fields, tags)
+				flushData = false
+			}
+			tags, fields = getTagsAndState(proto, words)
+			flushData = true
 		} else {
 			for _, word := range words {
 				if validValues.MatchString(word) {
 					kv := strings.Split(word, ":")
 					fields[kv[0]], err = strconv.ParseUint(kv[1], 10, 64)
-                                        if err != nil { continue }
+					if err != nil {
+						continue
+					}
 				}
 			}
-                        acc.AddFields(measurement, fields, tags)
-                        flushData = false
+			acc.AddFields(measurement, fields, tags)
+			flushData = false
 		}
 	}
-        if flushData {
-                acc.AddFields(measurement, fields, tags)
-        }
+	if flushData {
+		acc.AddFields(measurement, fields, tags)
+	}
 	return nil
 }
 
 func getTagsAndState(proto string, words []string) (map[string]string, map[string]interface{}) {
-        tags := map[string]string{}
-        fields := make(map[string]interface{})
-        tags["proto"] = proto
-        switch proto {
-        case "udp", "raw":
-                words = append([]string{"dummy"}, words...)
-        case "tcp", "dccp", "sctp":
-                fields["state"] = words[0]
-        }
-        switch proto {
-        case "tcp", "udp", "raw", "dccp", "sctp":
-                local := strings.Split(words[3], ":")
-                remote := strings.Split(words[4], ":")
-                tags["local_addr"] = strings.Join(local[:len(local)-1], ":")
-                tags["local_port"] = local[len(local)-1]
-                tags["remote_addr"] = strings.Join(remote[:len(remote)-1], ":")
-                tags["remote_port"] = remote[len(remote)-1]
-        case "unix", "packet":
-                fields["netid"] = words[0]
-                tags["local_addr"] = words[4]
-                tags["local_port"] = words[5]
-                tags["remote_addr"] = words[6]
-                tags["remote_port"] = words[7]
-        }
-        var err error
-        fields["recv_q"], err = strconv.ParseUint(words[1], 10, 64)
-        fields["send_q"], err = strconv.ParseUint(words[2], 10, 64)
-        if err != nil { }
-        return tags, fields
+	tags := map[string]string{}
+	fields := make(map[string]interface{})
+	tags["proto"] = proto
+	switch proto {
+	case "udp", "raw":
+		words = append([]string{"dummy"}, words...)
+	case "tcp", "dccp", "sctp":
+		fields["state"] = words[0]
+	}
+	switch proto {
+	case "tcp", "udp", "raw", "dccp", "sctp":
+		local := strings.Split(words[3], ":")
+		remote := strings.Split(words[4], ":")
+		tags["local_addr"] = strings.Join(local[:len(local)-1], ":")
+		tags["local_port"] = local[len(local)-1]
+		tags["remote_addr"] = strings.Join(remote[:len(remote)-1], ":")
+		tags["remote_port"] = remote[len(remote)-1]
+	case "unix", "packet":
+		fields["netid"] = words[0]
+		tags["local_addr"] = words[4]
+		tags["local_port"] = words[5]
+		tags["remote_addr"] = words[6]
+		tags["remote_port"] = words[7]
+	}
+	var err error
+	fields["recv_q"], err = strconv.ParseUint(words[1], 10, 64)
+	fields["send_q"], err = strconv.ParseUint(words[2], 10, 64)
+	if err != nil {
+	}
+	return tags, fields
 }
 
 func init() {
