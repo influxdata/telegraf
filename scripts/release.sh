225c8288d82fcc08bc0b3523618e14a2e0875482
Paul Myjavec <pauly@myjavec.com>
2018-01-30 14:26:14 +1100

    Tidy up release script and Makefile
    Removes a bunch of redundant tests and debug statements from the release script.
    Also adjust the Circle build configuration.
    
    * Clean up the release script and rename it to something more approriate.
    * Add the `go fmt ./...` step to the default action in the Makefile.
    * Add a new step to the build, called "release"
    * Exlude artifacts from git using gitignore

---
+++ scripts/release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -u
+
+ARTIFACT_DIR='artifacts'
+run()
+{
+    "$@"
+    ret=$?
+    if [[ $ret -eq 0 ]]
+    then
+        echo "[INFO]  [ $@ ]"
+    else
+        echo "[ERROR] [ $@ ] returned $ret"
+        exit $ret
+    fi
+}
+
+run make
+run gzip telegraf -c > "$ARTIFACT_DIR/telegraf.gz"
+# RPM is used to build packages for Enterprise Linux hosts.
+# Boto is used to upload packages to S3.
+#
+run sudo apt-get install -y rpm python-boto ruby ruby-dev
+run sudo gem install fpm
+
+# If a release tag is found, perform a full release, else run nightlies.
+tag=$(git rev-parse $1 >/dev/null 2>&1)
+if [ ! -z ${tag} ]; then
+    run ./scripts/build.py --release --package --platform=all --arch=all --upload --bucket=dl.influxdata.com/telegraf/releases
+else
+    if [ "$(git rev-parse --abbrev-ref HEAD)" = master ]
+    then
+        run ./scripts/build.py --nightly --package --platform=all --arch=all --upload --bucket=dl.influxdata.com/telegraf/nightlies
+    else
+        run ./scripts/build.py --package --platform=all --arch=all
+    fi
+fi
+
+run mkdir -p ${ARTIFACT_DIR}
+run mv build $ARTIFACT_DIR
