1187c6825b5f490e1aba038b7b6a91fecf868ea4
Daniel Nelson <daniel.nelson@influxdb.com>
2018-02-01 14:47:42 -0800

    Always build packages, add workflow for nightly build

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,22 +17,18 @@ run()
 run make
 run mkdir -p ${ARTIFACT_DIR}
 run gzip telegraf -c > "$ARTIFACT_DIR/telegraf.gz"
+
 # RPM is used to build packages for Enterprise Linux hosts.
 # Boto is used to upload packages to S3.
-#
 run sudo apt-get install -y rpm python-boto ruby ruby-dev
 run sudo gem install fpm
 
-# If a release tag is found, perform a full release, else run nightlies.
 if git describe --exact-match HEAD 2>&1 >/dev/null; then
     run ./scripts/build.py --release --package --platform=all --arch=all --upload --bucket=dl.influxdata.com/telegraf/releases
-elif [ -n "${PACKAGE}" ]; then
-    if [ "$(git rev-parse --abbrev-ref HEAD)" = master ]
-    then
-        run ./scripts/build.py --nightly --package --platform=all --arch=all --upload --bucket=dl.influxdata.com/telegraf/nightlies
-    else
-        run ./scripts/build.py --package --platform=all --arch=all
-    fi
+elif [ "${CIRCLE_STAGE}" = nightly ]; then
+	run ./scripts/build.py --nightly --package --platform=all --arch=all --upload --bucket=dl.influxdata.com/telegraf/nightlies
+else
+	run ./scripts/build.py --package --platform=all --arch=all
 fi
 
 run mv build $ARTIFACT_DIR
