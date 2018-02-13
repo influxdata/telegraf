341a0f307c0dbfc375d770a2411d4b4402f49b8d
Germán Jaber <gjaber@talpor.com>
2018-02-12 23:54:50 -0600

    Topk processor: Update tests

--- plugins/processors/topk/tests_set1.go
+++ plugins/processors/topk/tests_set1.go
@@ -88,30 +88,30 @@ var MinAddAggregateFieldAns = []telegraf.Metric{ansMin11, ansMin12, ansMin13, an
 
 func setupTestSet1() {
 	// Expected answer for the TopkMeanAggretationField test
-	ansMean11.AddField("meanag_a", float64(28.044))
-	ansMean12.AddField("meanag_a", float64(28.044))
-	ansMean13.AddField("meanag_a", float64(28.044))
-	ansMean14.AddField("meanag_a", float64(28.044))
-	ansMean15.AddField("meanag_a", float64(28.044))
+	ansMean11.AddField("a_meanag", float64(28.044))
+	ansMean12.AddField("a_meanag", float64(28.044))
+	ansMean13.AddField("a_meanag", float64(28.044))
+	ansMean14.AddField("a_meanag", float64(28.044))
+	ansMean15.AddField("a_meanag", float64(28.044))
 
 	// Expected answer for the TopkSumAggretationField test
-	ansSum11.AddField("sumag_a", float64(140.22))
-	ansSum12.AddField("sumag_a", float64(140.22))
-	ansSum13.AddField("sumag_a", float64(140.22))
-	ansSum14.AddField("sumag_a", float64(140.22))
-	ansSum15.AddField("sumag_a", float64(140.22))
+	ansSum11.AddField("a_sumag", float64(140.22))
+	ansSum12.AddField("a_sumag", float64(140.22))
+	ansSum13.AddField("a_sumag", float64(140.22))
+	ansSum14.AddField("a_sumag", float64(140.22))
+	ansSum15.AddField("a_sumag", float64(140.22))
 
 	// Expected answer for the TopkMaxAggretationField test
-	ansMax11.AddField("maxag_a", float64(50.5))
-	ansMax12.AddField("maxag_a", float64(50.5))
-	ansMax13.AddField("maxag_a", float64(50.5))
-	ansMax14.AddField("maxag_a", float64(50.5))
-	ansMax15.AddField("maxag_a", float64(50.5))
+	ansMax11.AddField("a_maxag", float64(50.5))
+	ansMax12.AddField("a_maxag", float64(50.5))
+	ansMax13.AddField("a_maxag", float64(50.5))
+	ansMax14.AddField("a_maxag", float64(50.5))
+	ansMax15.AddField("a_maxag", float64(50.5))
 
 	// Expected answer for the TopkMinAggretationField test
-	ansMin11.AddField("minag_a", float64(0.3))
-	ansMin12.AddField("minag_a", float64(0.3))
-	ansMin13.AddField("minag_a", float64(0.3))
-	ansMin14.AddField("minag_a", float64(0.3))
-	ansMin15.AddField("minag_a", float64(0.3))
+	ansMin11.AddField("a_minag", float64(0.3))
+	ansMin12.AddField("a_minag", float64(0.3))
+	ansMin13.AddField("a_minag", float64(0.3))
+	ansMin14.AddField("a_minag", float64(0.3))
+	ansMin15.AddField("a_minag", float64(0.3))
 }
