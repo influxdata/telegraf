34445783952f81134339df139c4233a87adcdf3c
Daniel Nelson <daniel.nelson@influxdb.com>
2018-03-28 15:10:37 -0700

    Add int overflow tests

--- plugins/parsers/influx/parser_test.go
+++ plugins/parsers/influx/parser_test.go
@@ -16,12 +16,6 @@ func Metric(v telegraf.Metric, err error) telegraf.Metric {
 	return v
 }
 
-const (
-	Uint64Overflow uint64 = 9223372036854775808
-	Uint64Max      uint64 = 18446744073709551615
-	Uint64Test     uint64 = 42
-)
-
 var DefaultTime = func() time.Time {
 	return time.Unix(42, 0)
 }
@@ -263,6 +257,38 @@ var ptests = []struct {
 		err: nil,
 	},
 	{
+		name:  "field int overflow",
+		input: []byte("cpu value=9223372036854775808i"),
+		metrics: []telegraf.Metric{
+			Metric(
+				metric.New(
+					"cpu",
+					map[string]string{},
+					map[string]interface{}{},
+					time.Unix(42, 0),
+				),
+			),
+		},
+		err: nil,
+	},
+	{
+		name:  "field int maximum",
+		input: []byte("cpu value=9223372036854775807i"),
+		metrics: []telegraf.Metric{
+			Metric(
+				metric.New(
+					"cpu",
+					map[string]string{},
+					map[string]interface{}{
+						"value": 9223372036854775807,
+					},
+					time.Unix(42, 0),
+				),
+			),
+		},
+		err: nil,
+	},
+	{
 		name:  "field uint",
 		input: []byte("cpu value=42u"),
 		metrics: []telegraf.Metric{
@@ -271,7 +297,7 @@ var ptests = []struct {
 					"cpu",
 					map[string]string{},
 					map[string]interface{}{
-						"value": Uint64Test,
+						"value": uint64(42),
 					},
 					time.Unix(42, 0),
 				),
@@ -281,15 +307,13 @@ var ptests = []struct {
 	},
 	{
 		name:  "field uint int overflow",
-		input: []byte("cpu value=9223372036854775808u"),
+		input: []byte("cpu value=18446744073709551616u"),
 		metrics: []telegraf.Metric{
 			Metric(
 				metric.New(
 					"cpu",
 					map[string]string{},
-					map[string]interface{}{
-						"value": Uint64Overflow,
-					},
+					map[string]interface{}{},
 					time.Unix(42, 0),
 				),
 			),
@@ -305,7 +329,7 @@ var ptests = []struct {
 					"cpu",
 					map[string]string{},
 					map[string]interface{}{
-						"value": Uint64Max,
+						"value": uint64(18446744073709551615),
 					},
 					time.Unix(42, 0),
 				),
