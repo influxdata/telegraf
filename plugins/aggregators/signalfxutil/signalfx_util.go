4d2615a4ae10b9ec771bd64f5e00b94fc56cbc0a
Charlie Smith <charlie@signalfx.com>
2018-04-20 09:52:22 -0400

    comment in util plugin config indicating period must be 2x interval

--- plugins/aggregators/signalfxutil/signalfx_util.go
+++ plugins/aggregators/signalfxutil/signalfx_util.go
@@ -36,6 +36,8 @@ var sampleConfig = `
   ## Metrics will report with the plugin name "signalfx-metadata"
   ## General Aggregator Arguments:
   ## The period on which to flush & clear the aggregator.
+  ## The period must be at least double the collection interval
+  ## because this plugin aggregates metrics across two reporting intervals.
   period = "30s"
   ## If true, the original metric will be dropped by the
   ## aggregator and will not get sent to the output plugins.
