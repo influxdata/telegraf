8716903274204ef529da83a12815e2cfbbc406e6
Charlie Smith <charlie@signalfx.com>
2018-04-20 09:44:43 -0400

    rename signalfx utilization directory

--- plugins/aggregators/all/all.go
+++ plugins/aggregators/all/all.go
@@ -4,5 +4,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/aggregators/basicstats"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/histogram"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/minmax"
-	_ "github.com/influxdata/telegraf/plugins/aggregators/signalfx_util"
+	_ "github.com/influxdata/telegraf/plugins/aggregators/signalfxutil"
 )
