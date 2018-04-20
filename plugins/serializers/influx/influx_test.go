4b2e1a100c6b46429a1cd1f787b0d121ad942f33
Daniel Nelson <daniel.nelson@influxdb.com>
2018-04-20 14:29:07 -0700

    Fix ints being capped at 32-bits on 32-bit archs

--- plugins/serializers/influx/influx_test.go
+++ plugins/serializers/influx/influx_test.go
@@ -130,6 +130,20 @@ var tests = []struct {
 		output: []byte("cpu value=42i 0\n"),
 	},
 	{
+		name: "integer field 64-bit",
+		input: MustMetric(
+			metric.New(
+				"cpu",
+				map[string]string{},
+				map[string]interface{}{
+					"value": int64(123456789012345),
+				},
+				time.Unix(0, 0),
+			),
+		),
+		output: []byte("cpu value=123456789012345i 0\n"),
+	},
+	{
 		name: "uint field",
 		input: MustMetric(
 			metric.New(
