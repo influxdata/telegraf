4af13a4fc9ba5376b6171b8741e972901459fc5d
cthayer <cthayer@roblox.com>
2018-04-25 13:54:49 -0700

    Add input plugin for McRouter
    McRouter is a smart proxy for Memcached
    
    https://github.com/facebook/mcrouter

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -50,6 +50,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/logparser"
 	_ "github.com/influxdata/telegraf/plugins/inputs/lustre2"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mailchimp"
+	_ "github.com/influxdata/telegraf/plugins/inputs/mcrouter"
 	_ "github.com/influxdata/telegraf/plugins/inputs/memcached"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mesos"
 	_ "github.com/influxdata/telegraf/plugins/inputs/minecraft"
