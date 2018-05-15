dea1bb500e16300ca8ff014fd6d3e117f07cc765
Daniel Nelson <daniel.nelson@influxdb.com>
2018-05-14 17:11:44 -0700

    Add method, basic auth, and tls support to http output

--- plugins/outputs/http/http.go
+++ plugins/outputs/http/http.go
@@ -3,103 +3,119 @@ package http
 import (
 	"bytes"
 	"fmt"
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/plugins/outputs"
-	"github.com/influxdata/telegraf/plugins/serializers"
 	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/internal/tls"
+	"github.com/influxdata/telegraf/plugins/outputs"
+	"github.com/influxdata/telegraf/plugins/serializers"
 )
 
 var sampleConfig = `
-  ## It requires a url name.
-  ## Will be transmitted telegraf metrics to the HTTP Server using the below URL.
-  ## Note that not support the HTTPS.
+  ## URL is the address to send metrics to
   url = "http://127.0.0.1:8080/metric"
-  ## Configure dial timeout in seconds. Default : 3
-  timeout = 3
-  ## http_headers option can add a custom header to the request.
-  ## Content-Type is required http header in http plugin.
-  ## so content-type of HTTP specification (plain/text, application/json, etc...) must be filled out.
-  [outputs.http.headers]
-    Content-Type = "plain/text"
+
+  ## Timeout for HTTP message
+  # timeout = "5s"
+
+  ## HTTP method, one of: "POST" or "PUT"
+  # method = "POST"
+
+  ## HTTP Basic Auth credentials
+  # username = "username"
+  # password = "pa$$word"
+
+  ## Additional HTTP headers
+  # [outputs.http.headers]
+  #   # Should be set to "application/json" for json data_format
+  #   Content-Type = "text/plain; charset=utf-8"
+
+  ## Optional TLS Config
+  # tls_ca = "/etc/telegraf/ca.pem"
+  # tls_cert = "/etc/telegraf/cert.pem"
+  # tls_key = "/etc/telegraf/key.pem"
+  ## Use TLS but skip chain & host verification
+  # insecure_skip_verify = false
+
   ## Data format to output.
   ## Each data format has it's own unique set of configuration options, read
   ## more about them here:
   ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_OUTPUT.md
-  data_format = "influx"
+  # data_format = "influx"
 `
 
 const (
-	POST = "POST"
-
-	DEFAULT_TIME_OUT = 3
-
-	CONTENT_TYPE = "content-type"
+	defaultClientTimeout = 5 * time.Second
+	defaultContentType   = "text/plain; charset=utf-8"
+	defaultMethod        = http.MethodPost
 )
 
-type Http struct {
-	// http required option
-	URL     string `toml:"url"`
-	Headers map[string]string
+type HTTP struct {
+	URL      string            `toml:"url"`
+	Timeout  internal.Duration `toml:"timeout"`
+	Method   string            `toml:"method"`
+	Username string            `toml:"username"`
+	Password string            `toml:"password"`
+	Headers  map[string]string `toml:"headers"`
+	tls.ClientConfig
 
-	// Option with http default value
-	Timeout int `toml:"timeout"`
-
-	client     http.Client
+	client     *http.Client
 	serializer serializers.Serializer
 }
 
-func (h *Http) SetSerializer(serializer serializers.Serializer) {
+func (h *HTTP) SetSerializer(serializer serializers.Serializer) {
 	h.serializer = serializer
 }
 
-// Connect to the Output
-func (h *Http) Connect() error {
-	h.client = http.Client{
+func (h *HTTP) Connect() error {
+	if h.Method == "" {
+		h.Method = http.MethodPost
+	}
+	h.Method = strings.ToUpper(h.Method)
+	if h.Method != http.MethodPost && h.Method != http.MethodPut {
+		return fmt.Errorf("invalid method [%s] %s", h.URL, h.Method)
+	}
+
+	if h.Timeout.Duration == 0 {
+		h.Timeout.Duration = defaultClientTimeout
+	}
+
+	tlsCfg, err := h.ClientConfig.TLSConfig()
+	if err != nil {
+		return err
+	}
+
+	h.client = &http.Client{
 		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
+			TLSClientConfig: tlsCfg,
+			Proxy:           http.ProxyFromEnvironment,
 		},
-		Timeout: time.Duration(h.Timeout) * time.Second,
-	}
-
-	var isValid bool
-
-	for k := range h.Headers {
-		if strings.ToLower(k) == CONTENT_TYPE {
-			isValid = true
-		}
-	}
-
-	if !isValid {
-		return fmt.Errorf("E! httpHeader require content-type!")
+		Timeout: h.Timeout.Duration,
 	}
 
 	return nil
 }
 
-// Close is not implemented. Because http.Client not provided connection close policy. Instead, uses the response.Body.Close() pattern.
-func (h *Http) Close() error {
+func (h *HTTP) Close() error {
 	return nil
 }
 
-// Description A plugin that can transmit metrics over HTTP
-func (h *Http) Description() string {
+func (h *HTTP) Description() string {
 	return "A plugin that can transmit metrics over HTTP"
 }
 
-// SampleConfig provides sample example for developer
-func (h *Http) SampleConfig() string {
+func (h *HTTP) SampleConfig() string {
 	return sampleConfig
 }
 
-// Writes metrics over HTTP POST
-func (h *Http) Write(metrics []telegraf.Metric) error {
+func (h *HTTP) Write(metrics []telegraf.Metric) error {
 	reqBody, err := h.serializer.SerializeBatch(metrics)
-
 	if err != nil {
-		return fmt.Errorf("E! Error serializing some metrics: %s", err.Error())
+		return err
 	}
 
 	if err := h.write(reqBody); err != nil {
@@ -109,9 +125,10 @@ func (h *Http) Write(metrics []telegraf.Metric) error {
 	return nil
 }
 
-func (h *Http) write(reqBody []byte) error {
-	req, err := http.NewRequest(POST, h.URL, bytes.NewBuffer(reqBody))
+func (h *HTTP) write(reqBody []byte) error {
+	req, err := http.NewRequest(h.Method, h.URL, bytes.NewBuffer(reqBody))
 
+	req.Header.Set("Content-Type", defaultContentType)
 	for k, v := range h.Headers {
 		req.Header.Set(k, v)
 	}
@@ -123,20 +140,8 @@ func (h *Http) write(reqBody []byte) error {
 	defer resp.Body.Close()
 	_, err = ioutil.ReadAll(resp.Body)
 
-	if err := h.isOk(resp, err); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (h *Http) isOk(resp *http.Response, err error) error {
-	if resp == nil || err != nil {
-		return fmt.Errorf("E! %s request failed! %s.", h.URL, err.Error())
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode > 209 {
-		return fmt.Errorf("received bad status code, %d\n", resp.StatusCode)
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("when writing to [%s] received status code: %d", h.URL, resp.StatusCode)
 	}
 
 	return nil
@@ -144,8 +149,9 @@ func (h *Http) isOk(resp *http.Response, err error) error {
 
 func init() {
 	outputs.Add("http", func() telegraf.Output {
-		return &Http{
-			Timeout: DEFAULT_TIME_OUT,
+		return &HTTP{
+			Timeout: internal.Duration{Duration: defaultClientTimeout},
+			Method:  defaultMethod,
 		}
 	})
 }
