ccc4a85fd643ff8699b2d61b96fc20ad2addab92
Alexander Shepelin <Brightcor@gmail.com>
2018-05-22 01:46:10 +0300

    Add regex processor plugin (#3839)

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -3,5 +3,6 @@ package all
 import (
 	_ "github.com/influxdata/telegraf/plugins/processors/override"
 	_ "github.com/influxdata/telegraf/plugins/processors/printer"
+	_ "github.com/influxdata/telegraf/plugins/processors/regex"
 	_ "github.com/influxdata/telegraf/plugins/processors/topk"
 )
