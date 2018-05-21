c093b504d11b44f303174ea6378b532f88526f08
Tom Martin <tmartin14@gmail.com>
2018-05-21 13:22:56 -0400

    added nil check for race

--- plugins/outputs/splunk/splunk.go
+++ plugins/outputs/splunk/splunk.go
@@ -173,18 +173,19 @@ func (s *Splunk) Write(measures []telegraf.Metric) error {
 			}
 
 			req.Header.Add("Content-Type", "application/json")
-			req.Header.Add("Authorization", "Splunk "+s.AuthString)
+			// Check for existence of s.AuthString to prevent race ('panic: runtime error: invalid memory address or nil pointer dereference')
+			if s != nil {
+				req.Header.Add("Authorization", "Splunk " + s.AuthString)
+			}
 			resp, err := s.client.Do(req)
 			if err != nil {
 				return fmt.Errorf("error POST-ing metrics to Splunk[%s]  Sending Data:%s\n", err, payload)
 			}
+			defer resp.Body.Close()
 
 			if resp.StatusCode < 200 || resp.StatusCode > 209 {
 				return fmt.Errorf("received bad status code posting to %s, %d\n\n%s\n", s.SplunkUrl, resp.StatusCode, payload)
 			}
-
-			defer resp.Body.Close()
-
 		}
 	}
 	return nil
