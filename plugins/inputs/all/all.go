11335f5feec295960b585eaf0fea165153d1398d
Daniel Nelson <daniel.nelson@influxdb.com>
2018-06-07 12:35:02 -0700

    Restore tengine input plugin (#4160)
    This reverts commit 8826cdc423caa2555fb78d2f30f437579e02862c.

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -103,6 +103,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/tail"
 	_ "github.com/influxdata/telegraf/plugins/inputs/tcp_listener"
 	_ "github.com/influxdata/telegraf/plugins/inputs/teamspeak"
+	_ "github.com/influxdata/telegraf/plugins/inputs/tengine"
 	_ "github.com/influxdata/telegraf/plugins/inputs/tomcat"
 	_ "github.com/influxdata/telegraf/plugins/inputs/trig"
 	_ "github.com/influxdata/telegraf/plugins/inputs/twemproxy"
