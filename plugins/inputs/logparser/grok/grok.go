b13ef735296d05c0e3dfb52d2486b3d236f46444
Daniel Nelson <daniel.nelson@influxdb.com>
2018-06-21 21:53:14 -0700

    Fix syslog timestamp parsing with single digit day of month

--- plugins/inputs/logparser/grok/grok.go
+++ plugins/inputs/logparser/grok/grok.go
@@ -293,7 +293,7 @@ func (p *Parser) ParseLine(line string) (telegraf.Metric, error) {
 				timestamp = time.Unix(0, iv)
 			}
 		case SYSLOG_TIMESTAMP:
-			ts, err := time.ParseInLocation("Jan 02 15:04:05", v, p.loc)
+			ts, err := time.ParseInLocation(time.Stamp, v, p.loc)
 			if err == nil {
 				if ts.Year() == 0 {
 					ts = ts.AddDate(timestamp.Year(), 0, 0)
