5d7a7e856d2e4612cc9872e2135139becf6f95e9
Prashanth Joseph Babu <prashanth@aikaan.io>
2018-06-29 13:46:15 +0530

    docker_log plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -152,4 +152,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/zfs"
 	_ "github.com/influxdata/telegraf/plugins/inputs/zipkin"
 	_ "github.com/influxdata/telegraf/plugins/inputs/zookeeper"
+    _ "github.com/influxdata/telegraf/plugins/inputs/docker_logs"
 )
