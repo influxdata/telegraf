693b636a6b0eb0be804af5983b5c3c5eade3a3cc
userlabs <nayarlabs@gmail.com>
2018-07-04 11:23:20 -0400

    Correcting formatting errors

--- plugins/aggregators/basicstats/costats_test.go
+++ plugins/aggregators/basicstats/costats_test.go
@@ -19,12 +19,11 @@ var m1, _ = metric.New("m",
 var m2, _ = metric.New("m",
 	map[string]string{"foo": "bar"},
 	map[string]interface{}{
-		"a":        int64(2),
-		"b":        int64(3),
+		"a": int64(2),
+		"b": int64(3),
 	},
 	time.Now(),
 )
-
 
 var n1, _ = metric.New("n",
 	map[string]string{"foos": "ball"},
@@ -37,12 +36,11 @@ var n1, _ = metric.New("n",
 var n2, _ = metric.New("n",
 	map[string]string{"foos": "ball"},
 	map[string]interface{}{
-		"a":        int64(2),
-		"b":        int64(3),
+		"a": int64(2),
+		"b": int64(3),
 	},
 	time.Now(),
 )
-
 
 // Test only aggregating variance
 func TestCoStatsCovariance(t *testing.T) {
@@ -51,13 +49,13 @@ func TestCoStatsCovariance(t *testing.T) {
 
 	aggregator := NewBasicStats()
 	aggregator.Stats = []string{}
-	aggregator.CoStatsConfig = []costat {
-		{ Metrics: []metrics {
-				{Name:"n", Field:"a"}, {Name:"m", Field:"a"},
-			}},
-		{ Metrics: []metrics {
-				{Name:"m", Field:"b"}, {Name:"n", Field:"b"},
-			}},
+	aggregator.CoStatsConfig = []costat{
+		{Metrics: []metrics{
+			{Name: "n", Field: "a"}, {Name: "m", Field: "a"},
+		}},
+		{Metrics: []metrics{
+			{Name: "m", Field: "b"}, {Name: "n", Field: "b"},
+		}},
 	}
 
 	aggregator.Add(n1)
@@ -78,17 +76,14 @@ func TestCoStatsCovariance(t *testing.T) {
 	expectedFields := map[string]interface{}{
 		"correlation[m/a/foo:bar][n/a/foos:ball]": float64(2.202962962962964),
 		"correlation[m/b/foo:bar][n/b/foos:ball]": float64(2.2029629629629635),
-		"covariance[m/a/foo:bar][n/a/foos:ball]": float64(0.660888888888889),
-		"covariance[m/b/foo:bar][n/b/foos:ball]": float64(2.6435555555555554),
+		"covariance[m/a/foo:bar][n/a/foos:ball]":  float64(0.660888888888889),
+		"covariance[m/b/foo:bar][n/b/foos:ball]":  float64(2.6435555555555554),
 	}
 	expectedTags := map[string]string{
 		"foo": "bar",
 	}
 	acc.AssertContainsTaggedFields(t, "m", expectedFields, expectedTags)
 }
-
-
-
 
 // Test only aggregating variance
 func TestCoStatsAccuracyCostats(t *testing.T) {
@@ -97,21 +92,21 @@ func TestCoStatsAccuracyCostats(t *testing.T) {
 
 	aggregator := NewBasicStats()
 	aggregator.Stats = []string{}
-	aggregator.CoStatsConfig = []costat {
-		{ Metrics: []metrics {
-				{Name:"n", Field:"a"}, {Name:"m", Field:"a"},
-			}},
-		{ Metrics: []metrics {
-				{Name:"m", Field:"b"}, {Name:"n", Field:"b"},
-			}},
+	aggregator.CoStatsConfig = []costat{
+		{Metrics: []metrics{
+			{Name: "n", Field: "a"}, {Name: "m", Field: "a"},
+		}},
+		{Metrics: []metrics{
+			{Name: "m", Field: "b"}, {Name: "n", Field: "b"},
+		}},
 	}
 
-	for i:=1; i < 11; i++ {
+	for i := 1; i < 11; i++ {
 		m, _ := metric.New("m",
 			map[string]string{"foo": "bar"},
 			map[string]interface{}{
-				"a":        int64(i),
-				"b":        int64(i*2),
+				"a": int64(i),
+				"b": int64(i * 2),
 			},
 			time.Now(),
 		)
@@ -119,8 +114,8 @@ func TestCoStatsAccuracyCostats(t *testing.T) {
 		n, _ := metric.New("n",
 			map[string]string{"foos": "ball"},
 			map[string]interface{}{
-				"a":        int64(i),
-				"b":        int64(i*2),
+				"a": int64(i),
+				"b": int64(i * 2),
 			},
 			time.Now(),
 		)
@@ -132,8 +127,8 @@ func TestCoStatsAccuracyCostats(t *testing.T) {
 	expectedFields := map[string]interface{}{
 		"correlation[m/a/foo:bar][n/a/foos:ball]": float64(0.9939393939393938),
 		"correlation[m/b/foo:bar][n/b/foos:ball]": float64(0.9939393939393938),
-		"covariance[m/a/foo:bar][n/a/foos:ball]": float64(9.11111111111111),
-		"covariance[m/b/foo:bar][n/b/foos:ball]": float64(36.44444444444444),
+		"covariance[m/a/foo:bar][n/a/foos:ball]":  float64(9.11111111111111),
+		"covariance[m/b/foo:bar][n/b/foos:ball]":  float64(36.44444444444444),
 	}
 	expectedTags := map[string]string{
 		"foo": "bar",
