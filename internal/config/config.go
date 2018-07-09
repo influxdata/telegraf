f19e5a229321f3ff6d08f3a129cd18569bc23ab0
Max U <max.untrecht@gmail.com>
2018-07-09 13:15:33 -0700

    add parse_multivalue to collectd parser

--- internal/config/config.go
+++ internal/config/config.go
@@ -1285,6 +1285,14 @@ func buildParser(name string, tbl *ast.Table) (parsers.Parser, error) {
 		}
 	}
 
+	if node, ok := tbl.Fields["collectd_parse_multivalue"]; ok {
+		if kv, ok := node.(*ast.KeyValue); ok {
+			if str, ok := kv.Value.(*ast.String); ok {
+				c.CollectdSplit = str.Value
+			}
+		}
+	}
+
 	if node, ok := tbl.Fields["collectd_typesdb"]; ok {
 		if kv, ok := node.(*ast.KeyValue); ok {
 			if ary, ok := kv.Value.(*ast.Array); ok {
@@ -1348,6 +1356,7 @@ func buildParser(name string, tbl *ast.Table) (parsers.Parser, error) {
 	delete(tbl.Fields, "collectd_auth_file")
 	delete(tbl.Fields, "collectd_security_level")
 	delete(tbl.Fields, "collectd_typesdb")
+	delete(tbl.Fields, "collectd_parse_multivalue")
 	delete(tbl.Fields, "dropwizard_metric_registry_path")
 	delete(tbl.Fields, "dropwizard_time_path")
 	delete(tbl.Fields, "dropwizard_time_format")
