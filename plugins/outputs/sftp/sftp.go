c8593636389d926083be5cdad1320d943cd86661
Stephanie Closson <srclosson@gmail.com>
2018-07-18 15:03:58 -0600

    Sftp fix
    I should really do this in the sftp branch but... whatever...

--- plugins/outputs/sftp/sftp.go
+++ plugins/outputs/sftp/sftp.go
@@ -172,7 +172,7 @@ func (s *Sftp) NewSftpItem(relative_path string, tags map[string]string) *SftpIt
 
 	item.Data, err = ioutil.ReadFile(name)
 	if err != nil {
-		log.Println("ERROR [ReadFile]: ", err)
+		log.Println("ERROR [sftp.ReadFile]: ", err)
 	}
 
 	item.Source = name
