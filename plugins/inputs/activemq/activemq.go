6a0acbb584df85325e6508031b33ec024fddffe7
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2018-08-09 17:02:09 +0200

    fix package name

--- plugins/inputs/activemq/activemq.go
+++ plugins/inputs/activemq/activemq.go
@@ -10,9 +10,9 @@ import (
 	"strings"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/internal/tls"
 	"github.com/influxdata/telegraf/plugins/inputs"
-	"github.com/mlabouardy/telegraf/internal"
-	"github.com/mlabouardy/telegraf/internal/tls"
 )
 
 type ActiveMQ struct {
