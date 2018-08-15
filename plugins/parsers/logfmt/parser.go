192fbd805a7056a336282e55e72f6a4f3c405881
Ayrdrie Palmer <ayrdrie@influxdb.com>
2018-08-15 16:14:58 -0400

    Add updated logfmt parser code

--- plugins/parsers/logfmt/parser.go
+++ plugins/parsers/logfmt/parser.go
@@ -1,16 +1,19 @@
-// Package logfmt converts logfmt data into metrics.
 package logfmt
 
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"strconv"
-	"strings"
 	"time"
 
-	glogfmt "github.com/go-logfmt/logfmt"
+	"github.com/go-logfmt/logfmt"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
+)
+
+var (
+	ErrNoMetric = fmt.Errorf("no metric in line")
 )
 
 // Parser decodes logfmt formatted messages into metrics.
@@ -30,26 +33,21 @@ func NewParser(metricName string, defaultTags map[string]string) *Parser {
 }
 
 // Parse converts a slice of bytes in logfmt format to metrics.
-func (l *Parser) Parse(b []byte) ([]telegraf.Metric, error) {
+func (p *Parser) Parse(b []byte) ([]telegraf.Metric, error) {
 	reader := bytes.NewReader(b)
-	decoder := glogfmt.NewDecoder(reader)
+	decoder := logfmt.NewDecoder(reader)
 	metrics := make([]telegraf.Metric, 0)
 	for decoder.ScanRecord() {
 		tags := make(map[string]string)
 		fields := make(map[string]interface{})
-		//add default tags
-		for k, v := range l.DefaultTags {
-			tags[k] = v
-		}
-
 		for decoder.ScanKeyval() {
 			if string(decoder.Value()) == "" {
 				return metrics, fmt.Errorf("value could not be found for key: %v", string(decoder.Key()))
 			}
 
-			//attempt type conversions
+			//type conversions
 			value := string(decoder.Value())
-			if iValue, err := strconv.Atoi(value); err == nil {
+			if iValue, err := strconv.ParseInt(value, 10, 64); err == nil {
 				fields[string(decoder.Key())] = iValue
 			} else if fValue, err := strconv.ParseFloat(value, 64); err == nil {
 				fields[string(decoder.Key())] = fValue
@@ -59,52 +57,46 @@ func (l *Parser) Parse(b []byte) ([]telegraf.Metric, error) {
 				fields[string(decoder.Key())] = value
 			}
 		}
-		m, err := metric.New(l.MetricName, tags, fields, l.Now())
+		m, err := metric.New(p.MetricName, tags, fields, p.Now())
 		if err != nil {
+			log.Println("Error occurred")
 			return nil, err
 		}
+
 		metrics = append(metrics, m)
 	}
+	p.applyDefaultTags(metrics)
 	return metrics, nil
 }
 
-// ParseLine converts a single line of text in logfmt to metrics.
-func (l *Parser) ParseLine(s string) (telegraf.Metric, error) {
-	reader := strings.NewReader(s)
-	decoder := glogfmt.NewDecoder(reader)
-
-	decoder.ScanRecord()
-	tags := make(map[string]string)
-	fields := make(map[string]interface{})
-	//add default tags
-	for k, v := range l.DefaultTags {
-		tags[k] = v
-	}
-
-	for decoder.ScanKeyval() {
-		if string(decoder.Value()) == "" {
-			return nil, fmt.Errorf("value could not be found for key: %v", string(decoder.Key()))
-		}
-		//attempt type conversions
-		value := string(decoder.Value())
-		if iValue, err := strconv.ParseInt(value, 10, 64); err == nil {
-			fields[string(decoder.Key())] = iValue
-		} else if fValue, err := strconv.ParseFloat(value, 64); err == nil {
-			fields[string(decoder.Key())] = fValue
-		} else if bValue, err := strconv.ParseBool(value); err == nil {
-			fields[string(decoder.Key())] = bValue
-		} else {
-			fields[string(decoder.Key())] = value
-		}
-	}
-	m, err := metric.New(l.MetricName, tags, fields, l.Now())
+// ParseLine converts a single line of text in logfmt format to metrics.
+func (p *Parser) ParseLine(s string) (telegraf.Metric, error) {
+	metrics, err := p.Parse([]byte(s))
 	if err != nil {
 		return nil, err
 	}
-	return m, nil
+
+	if len(metrics) < 1 {
+		return nil, ErrNoMetric
+	}
+	return metrics[0], nil
 }
 
 // SetDefaultTags adds tags to the metrics outputs of Parse and ParseLine.
-func (l *Parser) SetDefaultTags(tags map[string]string) {
-	l.DefaultTags = tags
+func (p *Parser) SetDefaultTags(tags map[string]string) {
+	p.DefaultTags = tags
+}
+
+func (p *Parser) applyDefaultTags(metrics []telegraf.Metric) {
+	if len(p.DefaultTags) == 0 {
+		return
+	}
+
+	for _, m := range metrics {
+		for k, v := range p.DefaultTags {
+			if !m.HasTag(k) {
+				m.AddTag(k, v)
+			}
+		}
+	}
 }
