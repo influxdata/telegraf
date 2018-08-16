4b0c00b49a9c0ece43d8fafcb112ecde5ae3c49a
Greg Linton <g2linton@gmail.com>
2018-08-16 13:43:42 -0600

    Add printer back to processors/all

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/processors/enum"
 	_ "github.com/influxdata/telegraf/plugins/processors/override"
 	_ "github.com/influxdata/telegraf/plugins/processors/parser"
+	_ "github.com/influxdata/telegraf/plugins/processors/printer"
 	_ "github.com/influxdata/telegraf/plugins/processors/regex"
 	_ "github.com/influxdata/telegraf/plugins/processors/topk"
 )
