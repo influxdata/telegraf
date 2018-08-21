c9a00d52c2229c52f81a71982b37bafc9ba2c355
Vikrant <vikrant.dessai@gmail.com>
2018-08-21 16:02:48 -0700

    fmt check done

--- plugins/outputs/http/http.go
+++ plugins/outputs/http/http.go
@@ -2,20 +2,20 @@ package http
 
 import (
 	"bytes"
-		"fmt"
+	"fmt"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/internal/tls"
 	"github.com/influxdata/telegraf/plugins/outputs"
 	"github.com/influxdata/telegraf/plugins/serializers"
-		 	"io/ioutil"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
 
+	"context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
-		"context"
 )
 
 var sampleConfig = `
@@ -25,7 +25,8 @@ var sampleConfig = `
   ## Timeout for HTTP message
   # timeout = "5s"
 
-  ## HTTP method, one of: "POST" or "PUT"
+  ## HTTP method,
+ one of: "POST" or "PUT"
   # method = "POST"
 
   ## HTTP Basic Auth credentials
@@ -40,7 +41,8 @@ var sampleConfig = `
   # insecure_skip_verify = false
 
   ## Data format to output.
-  ## Each data format has it's own unique set of configuration options, read
+  ## Each data format has it's own unique set of configuration options,
+ read
   ## more about them here:
   ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_OUTPUT.md
   # data_format = "influx"
@@ -53,9 +55,10 @@ var sampleConfig = `
 
 const (
 	defaultClientTimeout = 5 * time.Second
-		defaultContentType   = "text/plain; charset=utf-8"
+	defaultContentType   = "text/plain; charset=utf-8"
 	defaultMethod        = http.MethodPost
 )
+
 type HTTP struct {
 	URL          string            `toml:"url"`
 	Timeout      internal.Duration `toml:"timeout"`
@@ -73,7 +76,7 @@ type HTTP struct {
 	serializer serializers.Serializer
 }
 
-var  tokensource oauth2.TokenSource
+var tokensource oauth2.TokenSource
 
 func (h *HTTP) SetSerializer(serializer serializers.Serializer) {
 	h.serializer = serializer
@@ -85,14 +88,17 @@ func (h *HTTP) Connect() error {
 	}
 	h.Method = strings.ToUpper(h.Method)
 	if h.Method != http.MethodPost && h.Method != http.MethodPut {
-		return fmt.Errorf("invalid method [%s] %s", h.URL, h.Method)
+		return fmt.Errorf("invalid method [%s] %s",
+ h.URL,
+ h.Method)
 	}
 
 	if h.Timeout.Duration == 0 {
 		h.Timeout.Duration = defaultClientTimeout
 	}
 
-	tlsCfg, err := h.ClientConfig.TLSConfig()
+	tlsCfg,
+ err := h.ClientConfig.TLSConfig()
 	if err != nil {
 		return err
 	}
@@ -100,18 +106,25 @@ func (h *HTTP) Connect() error {
 	h.client = &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: tlsCfg,
+
 			Proxy:           http.ProxyFromEnvironment,
+
 		},
+
 		Timeout: h.Timeout.Duration,
+
 	}
 	var appClientConf = clientcredentials.Config{
 		ClientID:     h.ClientID,
-		ClientSecret: h.ClientSecret,
-		TokenURL:     h.TokenURL,
-		Scopes:       []string{h.Scopes},
-	}
-	tokensource =appClientConf.TokenSource(context.Background())
 
+		ClientSecret: h.ClientSecret,
+
+		TokenURL:     h.TokenURL,
+
+		Scopes:       []string{h.Scopes},
+
+	}
+	tokensource = appClientConf.TokenSource(context.Background())
 
 	return nil
 }
@@ -129,45 +142,60 @@ func (h *HTTP) SampleConfig() string {
 }
 
 func (h *HTTP) Write(metrics []telegraf.Metric) error {
-	reqBody, err := h.serializer.SerializeBatch(metrics)
+	reqBody,
+ err := h.serializer.SerializeBatch(metrics)
 	if err != nil {
 		return err
 	}
 
-	token,err :=	tokensource.Token()
+	token,
+ err := tokensource.Token()
 
 	if err != nil {
 		return err
 	}
-	if err := h.write(reqBody,token.AccessToken); err != nil {
+	if err := h.write(reqBody,
+ token.AccessToken); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (h *HTTP) write(reqBody []byte, accessToken string) error {
-	req, err := http.NewRequest(h.Method, h.URL, bytes.NewBuffer(reqBody))
+func (h *HTTP) write(reqBody []byte,
+ accessToken string) error {
+	req,
+ err := http.NewRequest(h.Method,
+ h.URL,
+ bytes.NewBuffer(reqBody))
 	if err != nil {
 		return err
 	}
 
-	req.Header.Set("Content-Type", defaultContentType)
-	req.Header.Set("Authorization", "Bearer "+accessToken)
+	req.Header.Set("Content-Type",
+ defaultContentType)
+	req.Header.Set("Authorization",
+ "Bearer "+accessToken)
 
-	for k, v := range h.Headers {
-		req.Header.Set(k, v)
+	for k,
+ v := range h.Headers {
+		req.Header.Set(k,
+ v)
 	}
 
-	resp, err := h.client.Do(req)
+	resp,
+ err := h.client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	_, err = ioutil.ReadAll(resp.Body)
+	_,
+ err = ioutil.ReadAll(resp.Body)
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return fmt.Errorf("when writing to [%s] received status code: %d", h.URL, resp.StatusCode)
+		return fmt.Errorf("when writing to [%s] received status code: %d",
+ h.URL,
+ resp.StatusCode)
 	}
 
 	return nil
@@ -175,12 +203,13 @@ func (h *HTTP) write(reqBody []byte, accessToken string) error {
 
 func init() {
 
-
-
-	outputs.Add("http", func() telegraf.Output {
+	outputs.Add("http",
+ func() telegraf.Output {
 		return &HTTP{
 			Timeout: internal.Duration{Duration: defaultClientTimeout},
+
 			Method:  defaultMethod,
+
 		}
 	})
 }
