2e8a9b35ee380af8107d8203334714a6cd031305
Max U <max.untrecht@gmail.com>
2018-08-23 14:10:49 -0700

    update to threshold

--- plugins/processors/deviation/deviation.go
+++ plugins/processors/deviation/deviation.go
@@ -1,4 +1,4 @@
-package deviation
+package threshold
 
 import (
 	"log"
@@ -9,43 +9,48 @@ import (
 	"github.com/influxdata/telegraf/plugins/processors"
 )
 
-type Deviation struct {
-	DeviationField string `toml:"deviation_field"`
-	Count          int
-	Sum            float64
+type Threshold struct {
+	FieldName         string  `toml:"field_name"`
+	DeviationDistance float64 `toml:"outlier_distance"`
+	Count             int
+	Sum               float64
 }
 
-func (d *Deviation) SampleConfig() string {
+func (d *Threshold) SampleConfig() string {
 	return `
 ## must run metrics through average processor before this processor
-[[processors.deviation]]
+[[processors.threshold]]
 
 ## field to compile a standard deviation of
 ## the processor will assume the average of the field
 ## can be found in the field deviation_field"_mean"
-deviation_field = "trace_id"`
+field_name = "trace_id"
+
+## Determine the number of standard deviations
+## away you want your outlier to be
+outlier_distance = "2"`
 }
 
-func (d *Deviation) Description() string {
-	return "will append a field to each metric indicating the running average of the specified field"
+func (d *Threshold) Description() string {
+	return "will append a field to each metric indicating whether it is a outlier or not"
 }
 
-func (d *Deviation) Apply(in ...telegraf.Metric) []telegraf.Metric {
+func (d *Threshold) Apply(in ...telegraf.Metric) []telegraf.Metric {
 	nMetrics := make([]telegraf.Metric, 0)
 	for _, metric := range in {
-		if metric.Fields()[d.DeviationField] != nil {
-			fVal, err := strconv.ParseFloat(metric.Fields()[d.DeviationField].(string), 64)
+		if metric.Fields()[d.FieldName] != nil {
+			fVal, err := strconv.ParseFloat(metric.Fields()[d.FieldName].(string), 64)
 			if err != nil {
-				log.Printf("E! %v must be a float or integer value, %v", d.DeviationField, err)
+				log.Printf("E! %v must be a float or integer value, %v", d.FieldName, err)
 				continue
 			}
-			if metric.Fields()[d.DeviationField+"_mean"] != nil {
-				log.Printf("E! missing field %v from [processor.average]", d.DeviationField+"_mean")
+			if metric.Fields()[d.FieldName+"_mean"] != nil {
+				log.Printf("E! missing field %v from [processor.average]", d.FieldName+"_mean")
 				continue
 			}
-			aveVal, err := strconv.ParseFloat(metric.Fields()[d.DeviationField+"_mean"].(string), 64)
+			aveVal, err := strconv.ParseFloat(metric.Fields()[d.FieldName+"_mean"].(string), 64)
 			if err != nil {
-				log.Printf("E! %v must be a float or integer value, %v", d.DeviationField+"_mean", err)
+				log.Printf("E! %v must be a float or integer value, %v", d.FieldName+"_mean", err)
 				continue
 			}
 			diff := fVal - aveVal
@@ -64,7 +69,7 @@ func (d *Deviation) Apply(in ...telegraf.Metric) []telegraf.Metric {
 }
 
 func init() {
-	processors.Add("deviation", func() telegraf.Processor {
-		return &Deviation{}
+	processors.Add("threshold", func() telegraf.Processor {
+		return &Threshold{}
 	})
 }
