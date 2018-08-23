0523b961fb5c7bf798a8d05e12d1250afbd2401f
Vikrant <vikrant.dessai@gmail.com>
2018-08-22 23:10:54 -0700

    Update http.go

--- plugins/outputs/http/http.go
+++ plugins/outputs/http/http.go
@@ -14,8 +14,8 @@ import (
 	"time"
 
 	"context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
+	"github.com/golang.org/x/oauth2"
+	"github.com/golang.org/x/oauth2/clientcredentials"
 )
 
 var sampleConfig = `
