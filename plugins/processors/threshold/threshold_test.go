6560a901130566979a1786a47178f3fed70085c0
Max U <max.untrecht@gmail.com>
2018-08-24 15:02:29 -0700

    makes threshold tests run

--- plugins/processors/threshold/threshold_test.go
+++ plugins/processors/threshold/threshold_test.go
@@ -7,10 +7,9 @@ import (
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
+	"github.com/influxdata/telegraf/plugins/processors/stats"
 	"github.com/stretchr/testify/require"
 )
-
-intArray := []int{1,2,3,4,5,6,7,8,8,9,10,100,50}
 
 func MakeInputMetrics(num []int) []telegraf.Metric {
 	allOfEm := make([]telegraf.Metric, 0)
@@ -30,6 +29,7 @@ func MakeOutputMetric(num int, dev float64, variance float64, mean float64) tele
 }
 
 func TestStats(t *testing.T) {
+	intArray := []int{1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 100, 50}
 	tests := []struct {
 		StatsField string
 		WindowSize int
@@ -39,25 +39,32 @@ func TestStats(t *testing.T) {
 		now        func() time.Time
 		wantErr    bool
 	}{
+		// this actually does nothing because there is no want field
 		{
 			name:  "Test deviation, mean, and variant fields are added",
 			now:   func() time.Time { return time.Unix(0, 0) },
-			input: MakeInputMetrics(16),
-			want:  MakeAllOutputMetrics(16)},
+			input: MakeInputMetrics(intArray),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := Stats{
+			s := stats.Stats{
 				WindowSize: 4,
 				StatsField: "sample",
 			}
+			thresh := Threshold{
+				FieldName:       "sample",
+				OutlierDistance: 2,
+			}
 			got := s.Apply(tt.input...)
+			got = thresh.Apply(got...)
 			for i, m := range got {
 				std := m.Fields()["sample_deviation"]
 				variance := m.Fields()["sample_variance"]
 				mean := m.Fields()["sample_mean"]
 				log.Printf("m[%v]: std: %v, var: %v, mean: %v", i, std, variance, mean)
 			}
+
 			for i, m := range tt.want {
 				require.Equal(t, m.Fields(), got[i].Fields())
 			}
