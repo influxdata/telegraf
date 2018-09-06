2940e47917381d972c7542169c2560fe7eb4c4ef
Daniel Nelson <daniel.nelson@influxdb.com>
2018-09-05 18:35:54 -0700

    Add support for fetching config over https

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"math"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -589,7 +591,12 @@ func (c *Config) LoadConfig(path string) error {
 			return err
 		}
 	}
-	tbl, err := parseFile(path)
+	data, err := loadConfig(path)
+	if err != nil {
+		return fmt.Errorf("Error loading %s, %s", path, err)
+	}
+
+	tbl, err := parseConfig(data)
 	if err != nil {
 		return fmt.Errorf("Error parsing %s, %s", path, err)
 	}
@@ -736,15 +743,35 @@ func escapeEnv(value string) string {
 	return envVarEscaper.Replace(value)
 }
 
-// parseFile loads a TOML configuration from a provided path and
-// returns the AST produced from the TOML parser. When loading the file, it
-// will find environment variables and replace them.
-func parseFile(fpath string) (*ast.Table, error) {
-	contents, err := ioutil.ReadFile(fpath)
+func loadConfig(config string) ([]byte, error) {
+	u, err := url.Parse(config)
 	if err != nil {
 		return nil, err
 	}
-	// ugh windows why
+
+	switch u.Scheme {
+	case "https": // http not permitted
+		return fetchConfig(u)
+	default:
+		// If it isn't a http scheme, try it as a file.
+	}
+	return ioutil.ReadFile(config)
+
+}
+
+func fetchConfig(u *url.URL) ([]byte, error) {
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+// parseFile loads a TOML configuration from a provided path and
+// returns the AST produced from the TOML parser. When loading the file, it
+// will find environment variables and replace them.
+func parseConfig(contents []byte) (*ast.Table, error) {
 	contents = trimBOM(contents)
 
 	env_vars := envVarRe.FindAll(contents, -1)
