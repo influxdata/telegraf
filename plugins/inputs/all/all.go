69100f60b8676048e30572cac00db16aaf1c27b3
Alexander Shepelin <Brightcor@gmail.com>
2018-09-10 21:51:03 +0300

    Add Beanstalkd input plugin (#4272)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/apache"
 	_ "github.com/influxdata/telegraf/plugins/inputs/aurora"
 	_ "github.com/influxdata/telegraf/plugins/inputs/bcache"
+	_ "github.com/influxdata/telegraf/plugins/inputs/beanstalkd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/bond"
 	_ "github.com/influxdata/telegraf/plugins/inputs/burrow"
 	_ "github.com/influxdata/telegraf/plugins/inputs/cassandra"
