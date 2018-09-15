f2e74b2389dfb80df409a360ea8a52f65fd9e7ed
이재용 <jaeyo.developer@gmail.com>
2018-09-16 03:28:41 +0900

    re-order functions

--- plugins/inputs/ping/ping_windows.go
+++ plugins/inputs/ping/ping_windows.go
@@ -55,98 +55,6 @@ func (s *Ping) SampleConfig() string {
 	return sampleConfig
 }
 
-func hostPinger(timeout float64, args ...string) (string, error) {
-	bin, err := exec.LookPath("ping")
-	if err != nil {
-		return "", err
-	}
-	c := exec.Command(bin, args...)
-	out, err := internal.CombinedOutputTimeout(c,
-		time.Second*time.Duration(timeout+1))
-	return string(out), err
-}
-
-// processPingOutput takes in a string output from the ping command
-// based on linux implementation but using regex ( multilanguage support )
-// It returns (<transmitted packets>, <received reply>, <received packet>, <average response>, <min response>, <max response>)
-func processPingOutput(out string) (int, int, int, int, int, int, error) {
-	// So find a line contain 3 numbers except reply lines
-	var stats, aproxs []string = nil, nil
-	err := errors.New("Fatal error processing ping output")
-	stat := regexp.MustCompile(`=\W*(\d+)\D*=\W*(\d+)\D*=\W*(\d+)`)
-	aprox := regexp.MustCompile(`=\W*(\d+)\D*ms\D*=\W*(\d+)\D*ms\D*=\W*(\d+)\D*ms`)
-	tttLine := regexp.MustCompile(`TTL=\d+`)
-	lines := strings.Split(out, "\n")
-	var receivedReply int = 0
-	for _, line := range lines {
-		if tttLine.MatchString(line) {
-			receivedReply++
-		} else {
-			if stats == nil {
-				stats = stat.FindStringSubmatch(line)
-			}
-			if stats != nil && aproxs == nil {
-				aproxs = aprox.FindStringSubmatch(line)
-			}
-		}
-	}
-
-	// stats data should contain 4 members: entireExpression + ( Send, Receive, Lost )
-	if len(stats) != 4 {
-		return 0, 0, 0, -1, -1, -1, err
-	}
-	trans, err := strconv.Atoi(stats[1])
-	if err != nil {
-		return 0, 0, 0, -1, -1, -1, err
-	}
-	receivedPacket, err := strconv.Atoi(stats[2])
-	if err != nil {
-		return 0, 0, 0, -1, -1, -1, err
-	}
-
-	// aproxs data should contain 4 members: entireExpression + ( min, max, avg )
-	if len(aproxs) != 4 {
-		return trans, receivedReply, receivedPacket, -1, -1, -1, err
-	}
-	min, err := strconv.Atoi(aproxs[1])
-	if err != nil {
-		return trans, receivedReply, receivedPacket, -1, -1, -1, err
-	}
-	max, err := strconv.Atoi(aproxs[2])
-	if err != nil {
-		return trans, receivedReply, receivedPacket, -1, -1, -1, err
-	}
-	avg, err := strconv.Atoi(aproxs[3])
-	if err != nil {
-		return 0, 0, 0, -1, -1, -1, err
-	}
-
-	return trans, receivedReply, receivedPacket, avg, min, max, err
-}
-
-func (p *Ping) timeout() float64 {
-	// According to MSDN, default ping timeout for windows is 4 second
-	// Add also one second interval
-
-	if p.Timeout > 0 {
-		return p.Timeout + 1
-	}
-	return 4 + 1
-}
-
-// args returns the arguments for the 'ping' executable
-func (p *Ping) args(url string) []string {
-	args := []string{"-n", strconv.Itoa(p.Count)}
-
-	if p.Timeout > 0 {
-		args = append(args, "-w", strconv.FormatFloat(p.Timeout*1000, 'f', 0, 64))
-	}
-
-	args = append(args, url)
-
-	return args
-}
-
 func (p *Ping) Gather(acc telegraf.Accumulator) error {
 	if p.Count < 1 {
 		p.Count = 1
@@ -227,6 +135,98 @@ func (p *Ping) Gather(acc telegraf.Accumulator) error {
 		return nil
 	}
 	return errors.New(strings.Join(errorStrings, "\n"))
+}
+
+func hostPinger(timeout float64, args ...string) (string, error) {
+	bin, err := exec.LookPath("ping")
+	if err != nil {
+		return "", err
+	}
+	c := exec.Command(bin, args...)
+	out, err := internal.CombinedOutputTimeout(c,
+		time.Second*time.Duration(timeout+1))
+	return string(out), err
+}
+
+// args returns the arguments for the 'ping' executable
+func (p *Ping) args(url string) []string {
+	args := []string{"-n", strconv.Itoa(p.Count)}
+
+	if p.Timeout > 0 {
+		args = append(args, "-w", strconv.FormatFloat(p.Timeout*1000, 'f', 0, 64))
+	}
+
+	args = append(args, url)
+
+	return args
+}
+
+// processPingOutput takes in a string output from the ping command
+// based on linux implementation but using regex ( multilanguage support )
+// It returns (<transmitted packets>, <received reply>, <received packet>, <average response>, <min response>, <max response>)
+func processPingOutput(out string) (int, int, int, int, int, int, error) {
+	// So find a line contain 3 numbers except reply lines
+	var stats, aproxs []string = nil, nil
+	err := errors.New("Fatal error processing ping output")
+	stat := regexp.MustCompile(`=\W*(\d+)\D*=\W*(\d+)\D*=\W*(\d+)`)
+	aprox := regexp.MustCompile(`=\W*(\d+)\D*ms\D*=\W*(\d+)\D*ms\D*=\W*(\d+)\D*ms`)
+	tttLine := regexp.MustCompile(`TTL=\d+`)
+	lines := strings.Split(out, "\n")
+	var receivedReply int = 0
+	for _, line := range lines {
+		if tttLine.MatchString(line) {
+			receivedReply++
+		} else {
+			if stats == nil {
+				stats = stat.FindStringSubmatch(line)
+			}
+			if stats != nil && aproxs == nil {
+				aproxs = aprox.FindStringSubmatch(line)
+			}
+		}
+	}
+
+	// stats data should contain 4 members: entireExpression + ( Send, Receive, Lost )
+	if len(stats) != 4 {
+		return 0, 0, 0, -1, -1, -1, err
+	}
+	trans, err := strconv.Atoi(stats[1])
+	if err != nil {
+		return 0, 0, 0, -1, -1, -1, err
+	}
+	receivedPacket, err := strconv.Atoi(stats[2])
+	if err != nil {
+		return 0, 0, 0, -1, -1, -1, err
+	}
+
+	// aproxs data should contain 4 members: entireExpression + ( min, max, avg )
+	if len(aproxs) != 4 {
+		return trans, receivedReply, receivedPacket, -1, -1, -1, err
+	}
+	min, err := strconv.Atoi(aproxs[1])
+	if err != nil {
+		return trans, receivedReply, receivedPacket, -1, -1, -1, err
+	}
+	max, err := strconv.Atoi(aproxs[2])
+	if err != nil {
+		return trans, receivedReply, receivedPacket, -1, -1, -1, err
+	}
+	avg, err := strconv.Atoi(aproxs[3])
+	if err != nil {
+		return 0, 0, 0, -1, -1, -1, err
+	}
+
+	return trans, receivedReply, receivedPacket, avg, min, max, err
+}
+
+func (p *Ping) timeout() float64 {
+	// According to MSDN, default ping timeout for windows is 4 second
+	// Add also one second interval
+
+	if p.Timeout > 0 {
+		return p.Timeout + 1
+	}
+	return 4 + 1
 }
 
 func init() {
