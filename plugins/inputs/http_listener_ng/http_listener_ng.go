eb0e55f430100d8b36189112ed3b5b67e552f1a9
Julius Marozas <marozas.julius@gmail.com>
2018-09-26 21:14:22 +0300

    Uncommented sample config options

--- plugins/inputs/http_listener_ng/http_listener_ng.go
+++ plugins/inputs/http_listener_ng/http_listener_ng.go
@@ -75,48 +75,48 @@ type HTTPListenerNG struct {
 
 const sampleConfig = `
   ## Address and port to host HTTP listener on
-  # service_address = ":8186"
+  service_address = ":8186"
 
   ## Paths to listen to.
   ## "/query" and "/ping" paths are already taken.
   ## "/query" delivers dummy response.
   ## "/ping" responds to ping requests.
-  # paths = "/telegraf"
+  paths = "/telegraf"
 
   ## HTTP methods to accept.
-  # methods = ["POST", "PUT"]
+  methods = ["POST", "PUT"]
 
   ## maximum duration before timing out read of the request
-  # read_timeout = "10s"
+  read_timeout = "10s"
   ## maximum duration before timing out write of the response
-  # write_timeout = "10s"
+  write_timeout = "10s"
 
   ## Maximum allowed http request body size in bytes.
   ## 0 means to use the default of 536,870,912 bytes (500 mebibytes)
-  # max_body_size = 0
+  max_body_size = 0
 
   ## Maximum line size allowed to be sent in bytes.
   ## 0 means to use the default of 65536 bytes (64 kibibytes)
-  # max_line_size = 0
+  max_line_size = 0
 
   ## Set one or more allowed client CA certificate file names to 
   ## enable mutually authenticated TLS connections
-  # tls_allowed_cacerts = ["/etc/telegraf/clientca.pem"]
+  tls_allowed_cacerts = ["/etc/telegraf/clientca.pem"]
 
   ## Add service certificate and key
-  # tls_cert = "/etc/telegraf/cert.pem"
-  # tls_key = "/etc/telegraf/key.pem"
+  tls_cert = "/etc/telegraf/cert.pem"
+  tls_key = "/etc/telegraf/key.pem"
 
   ## Optional username and password to accept for HTTP basic authentication.
   ## You probably want to make sure you have TLS configured above for this.
-  # basic_username = "foobar"
-  # basic_password = "barfoo"
+  basic_username = "foobar"
+  basic_password = "barfoo"
 
   ## Data format to consume.
   ## Each data format has its own unique set of configuration options, read
   ## more about them here:
   ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md
-  # data_format = "influx"
+  data_format = "influx"
 `
 
 func (h *HTTPListenerNG) SampleConfig() string {
