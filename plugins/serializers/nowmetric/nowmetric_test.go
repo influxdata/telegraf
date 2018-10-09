7c7bfdbb20d3157b2108ff9b4ec53325bcafd002
JefMuller <37331926+JefMuller@users.noreply.github.com>
2018-10-09 18:58:57 +0200

    Tests section rewrite
    Corrected test issues

--- plugins/serializers/nowmetric/nowmetric_test.go
+++ plugins/serializers/nowmetric/nowmetric_test.go
@@ -34,7 +34,8 @@ func TestSerializeMetricFloat(t *testing.T) {
 	var buf []byte
 	buf, err = s.Serialize(m)
 	assert.NoError(t, err)
-	expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":91.5},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	//expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":91.5},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	expS := []byte(fmt.Sprintf(`[ { "metric_type": "usage_idle", "resource": "", "node": "", "value": 91.5, "timestamp": %d, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`, now.Unix()*1000) + "\n")
 	assert.Equal(t, string(expS), string(buf))
 }
 
@@ -45,34 +46,28 @@ func TestSerialize_TimestampUnits(t *testing.T) {
 		expected       string
 	}{
 		{
-			name:           "default of 1s",
-			timestampUnits: 0,
-			expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":1525478795}`,
-		},
-		{
-			name:           "1ns",
-			timestampUnits: 1 * time.Nanosecond,
-			expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":1525478795123456789}`,
-		},
-		{
 			name:           "1ms",
 			timestampUnits: 1 * time.Millisecond,
-			expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":1525478795123}`,
+			//expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":1525478795123}`,
+			expected: `[ { "metric_type": "value", "resource": "", "node": "", "value": 42, "timestamp": 1525478795123000, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`,
 		},
 		{
 			name:           "10ms",
 			timestampUnits: 10 * time.Millisecond,
-			expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":152547879512}`,
+			//expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":152547879512}`,
+			expected: `[ { "metric_type": "value", "resource": "", "node": "", "value": 42, "timestamp": 152547879512000, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`,
 		},
 		{
 			name:           "15ms is reduced to 10ms",
 			timestampUnits: 15 * time.Millisecond,
-			expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":152547879512}`,
+			//expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":152547879512}`,
+			expected: `[ { "metric_type": "value", "resource": "", "node": "", "value": 42, "timestamp": 152547879512000, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`,
 		},
 		{
 			name:           "65ms is reduced to 10ms",
 			timestampUnits: 65 * time.Millisecond,
-			expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":152547879512}`,
+			//expected:       `{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":152547879512}`,
+			expected: `[ { "metric_type": "value", "resource": "", "node": "", "value": 42, "timestamp": 152547879512000, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`,
 		},
 	}
 	for _, tt := range tests {
@@ -111,7 +106,8 @@ func TestSerializeMetricInt(t *testing.T) {
 	buf, err = s.Serialize(m)
 	assert.NoError(t, err)
 
-	expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":90},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	//expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":90},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	expS := []byte(fmt.Sprintf(`[ { "metric_type": "usage_idle", "resource": "", "node": "", "value": 90, "timestamp": %d, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`, now.Unix()*1000) + "\n")
 	assert.Equal(t, string(expS), string(buf))
 }
 
@@ -131,7 +127,8 @@ func TestSerializeMetricString(t *testing.T) {
 	buf, err = s.Serialize(m)
 	assert.NoError(t, err)
 
-	expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":"foobar"},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	//expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":"foobar"},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	expS := []byte(fmt.Sprintf(`[  ]`) + "\n")
 	assert.Equal(t, string(expS), string(buf))
 }
 
@@ -152,7 +149,8 @@ func TestSerializeMultiFields(t *testing.T) {
 	buf, err = s.Serialize(m)
 	assert.NoError(t, err)
 
-	expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":90,"usage_total":8559615},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	//expS := []byte(fmt.Sprintf(`{"fields":{"usage_idle":90,"usage_total":8559615},"name":"cpu","tags":{"cpu":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	expS := []byte(fmt.Sprintf(`[ { "metric_type": "usage_idle", "resource": "", "node": "", "value": 90, "timestamp": %d, "ci2metric_id": { "node": "" }, "source": "Telegraf" },`+"\n"+`{ "metric_type": "usage_total", "resource": "", "node": "", "value": 8559615, "timestamp": %d, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`, now.Unix()*1000, now.Unix()*1000) + "\n")
 	assert.Equal(t, string(expS), string(buf))
 }
 
@@ -171,7 +169,8 @@ func TestSerializeMetricWithEscapes(t *testing.T) {
 	buf, err := s.Serialize(m)
 	assert.NoError(t, err)
 
-	expS := []byte(fmt.Sprintf(`{"fields":{"U,age=Idle":90},"name":"My CPU","tags":{"cpu tag":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	//expS := []byte(fmt.Sprintf(`{"fields":{"U,age=Idle":90},"name":"My CPU","tags":{"cpu tag":"cpu0"},"timestamp":%d}`, now.Unix()) + "\n")
+	expS := []byte(fmt.Sprintf(`[ { "metric_type": "U,age=Idle", "resource": "", "node": "", "value": 90, "timestamp": %d, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`, now.Unix()*1000) + "\n")
 	assert.Equal(t, string(expS), string(buf))
 }
 
@@ -191,5 +190,6 @@ func TestSerializeBatch(t *testing.T) {
 	s, _ := NewSerializer(0)
 	buf, err := s.SerializeBatch(metrics)
 	require.NoError(t, err)
-	require.Equal(t, []byte(`{"metrics":[{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":0},{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":0}]}`), buf)
+	//require.Equal(t, []byte(`{"metrics":[{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":0},{"fields":{"value":42},"name":"cpu","tags":{},"timestamp":0}]}`), buf)
+	require.Equal(t, []byte(`[ { "metric_type": "value", "resource": "", "node": "", "value": 42, "timestamp": 0, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`+"\n"+`[ { "metric_type": "value", "resource": "", "node": "", "value": 42, "timestamp": 0, "ci2metric_id": { "node": "" }, "source": "Telegraf" } ]`+"\n"), buf)
 }
