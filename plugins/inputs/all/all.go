9a864d11d2bfb3639a9ea5592ec7b66387ff9a7a
Aleksejs Sinicins <monder@monder.cc>
2018-11-03 03:18:40 +0200

    Add nginx-module-vts input plugin. (#3782)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -82,6 +82,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_plus"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_plus_api"
+	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_vts"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nsq"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nsq_consumer"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nstat"
