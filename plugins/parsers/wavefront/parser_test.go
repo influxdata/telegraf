0384bddb8e3dc4e4cec9a1967de65e6b86ee9f6d
Robert Fisher <services@id264.net>
2018-12-05 19:04:02 +0000

    swap broken test for another good one

--- plugins/parsers/wavefront/parser_test.go
+++ plugins/parsers/wavefront/parser_test.go
@@ -19,12 +19,6 @@ func TestParse(t *testing.T) {
 	assert.Equal(t, parsedMetrics[0].Name(), testMetric.Name())
 	assert.Equal(t, parsedMetrics[0].Fields(), testMetric.Fields())
 
-	parsedMetrics, err = parser.Parse([]byte("∆dev.rack.request 1 1543271541 source=tornado-www site=\"sysdef-0.0.8\" page=\"/post/2014-10-22-solaris-upnp-linn-minidlna\""))
-	assert.NoError(t, err)
-	testMetric, err = metric.New("∆dev.rack.request 1 1543271541 source=tornado-www site=\"sysdef-0.0.8\" page=\"/post/2014-10-22-solaris-upnp-linn-minidlna\"", map[string]string{},
-		map[string]interface{}{"value": 1.}, time.Unix(1530939936, 0))
-	assert.NoError(t, err)
-	assert.EqualValues(t, parsedMetrics[0], testMetric)
 	parsedMetrics, err = parser.Parse([]byte("\u2206test.delta 1 1530939936"))
 	assert.NoError(t, err)
 	testMetric, err = metric.New("\u2206test.delta", map[string]string{},
@@ -39,7 +33,11 @@ func TestParse(t *testing.T) {
 	assert.NoError(t, err)
 	assert.EqualValues(t, parsedMetrics[0], testMetric)
 
-	parsedMetrics, err = parser.Parse([]byte("test.metric 1 1530939936"))
+	parsedMetrics, err = parser.Parse([]byte("\u0394test.delta 1.234 1530939936 source=\"mysource\" tag2=value2"))
+	assert.NoError(t, err)
+	testMetric, err = metric.New("\u0394test.delta", map[string]string{"source": "mysource", "tag2": "value2"}, map[string]interface{}{"value": 1.234}, time.Unix(1530939936, 0))
+	assert.NoError(t, err)
+	assert.EqualValues(t, parsedMetrics[0], testMetric)
 
 	parsedMetrics, err = parser.Parse([]byte("test.metric 1 1530939936"))
 	assert.NoError(t, err)
