91cd2bc4f3bda4a107f09d3f6976e1b5aa096a95
Greg Linton <greg@influxdata.com>
2019-01-04 17:58:29 -0700

    Convert client to use protobuf when gathering

--- plugins/inputs/kube_lite/client.go
+++ plugins/inputs/kube_lite/client.go
@@ -3,187 +3,89 @@ package kube_lite
 import (
 	"context"
 	"crypto/tls"
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"time"
 
+	"github.com/ericchiang/k8s"
 	"github.com/ericchiang/k8s/apis/apps/v1beta1"
 	"github.com/ericchiang/k8s/apis/apps/v1beta2"
 	"github.com/ericchiang/k8s/apis/core/v1"
-	metav1 "github.com/ericchiang/k8s/apis/meta/v1"
 )
 
 type client struct {
-	baseURL     string
-	httpClient  *http.Client
-	bearerToken string
-	semaphore   chan struct{}
+	namespace string
+	timeout   time.Duration
+	*k8s.Client
 }
 
-func newClient(baseURL string, timeout time.Duration, maxConns int, bearerToken string, tlsConfig *tls.Config) *client {
+func newClient(baseURL, namespace, bearerToken string, timeout time.Duration, tlsConfig *tls.Config) (*client, error) {
+	c, err := k8s.NewClient(&k8s.Config{
+		Clusters:  []k8s.NamedCluster{{Name: "cluster", Cluster: k8s.Cluster{Server: baseURL, InsecureSkipTLSVerify: tlsConfig.InsecureSkipVerify}}},
+		Contexts:  []k8s.NamedContext{{Name: "context", Context: k8s.Context{Cluster: "cluster", AuthInfo: "auth", Namespace: namespace}}},
+		AuthInfos: []k8s.NamedAuthInfo{{Name: "auth", AuthInfo: k8s.AuthInfo{Token: bearerToken}}},
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	return &client{
-		baseURL: baseURL,
-		httpClient: &http.Client{
-			Transport: &http.Transport{
-				MaxIdleConns:    maxConns,
-				TLSClientConfig: tlsConfig,
-			},
-			Timeout: timeout,
-		},
-		bearerToken: bearerToken,
-		semaphore:   make(chan struct{}, maxConns),
-	}
+		Client:    c,
+		timeout:   timeout,
+		namespace: namespace,
+	}, nil
 }
 
-func (c *client) getAPIResourceList(ctx context.Context) (rList *metav1.APIResourceList, err error) {
-	rList = new(metav1.APIResourceList)
-	if err = c.doGet(ctx, "", rList); err != nil {
-		return nil, err
-	}
-	if rList.GroupVersion == nil {
-		return nil, &APIError{
-			URL:        c.baseURL,
-			StatusCode: http.StatusOK,
-			Title:      "empty group version",
-		}
-	}
-	return rList, nil
+func (c *client) getConfigMaps(ctx context.Context) (*v1.ConfigMapList, error) {
+	list := new(v1.ConfigMapList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, c.namespace, list)
 }
 
-func (c *client) getConfigMaps(ctx context.Context) (list *v1.ConfigMapList, err error) {
-	list = new(v1.ConfigMapList)
-	if err = c.doGet(ctx, "/configmaps/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
+func (c *client) getDaemonSets(ctx context.Context) (*v1beta2.DaemonSetList, error) {
+	list := new(v1beta2.DaemonSetList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, c.namespace, list)
 }
 
-func (c *client) getDaemonSets(ctx context.Context) (list *v1beta2.DaemonSetList, err error) {
-	list = new(v1beta2.DaemonSetList)
-	if err = c.doGet(ctx, "/daemonsets/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
+func (c *client) getDeployments(ctx context.Context) (*v1beta1.DeploymentList, error) {
+	list := &v1beta1.DeploymentList{}
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, c.namespace, list)
 }
 
-func (c *client) getDeployments(ctx context.Context) (list *v1beta1.DeploymentList, err error) {
-	list = new(v1beta1.DeploymentList)
-	if err = c.doGet(ctx, "/deployments/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
+func (c *client) getNodes(ctx context.Context) (*v1.NodeList, error) {
+	list := new(v1.NodeList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, "", list)
 }
 
-func (c *client) getNodes(ctx context.Context) (list *v1.NodeList, err error) {
-	list = new(v1.NodeList)
-	if err = c.doGet(ctx, "/nodes/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
+func (c *client) getPersistentVolumes(ctx context.Context) (*v1.PersistentVolumeList, error) {
+	list := new(v1.PersistentVolumeList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, "", list)
 }
 
-func (c *client) getPersistentVolumes(ctx context.Context) (list *v1.PersistentVolumeList, err error) {
-	list = new(v1.PersistentVolumeList)
-	if err = c.doGet(ctx, "/persistentvolumes/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
+func (c *client) getPersistentVolumeClaims(ctx context.Context) (*v1.PersistentVolumeClaimList, error) {
+	list := new(v1.PersistentVolumeClaimList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, c.namespace, list)
 }
 
-func (c *client) getPersistentVolumeClaims(ctx context.Context) (list *v1.PersistentVolumeClaimList, err error) {
-	list = new(v1.PersistentVolumeClaimList)
-	if err = c.doGet(ctx, "/persistentvolumeclaims/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
+func (c *client) getPods(ctx context.Context) (*v1.PodList, error) {
+	list := new(v1.PodList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, c.namespace, list)
 }
 
-func (c *client) getPods(ctx context.Context) (list *v1.PodList, err error) {
-	list = new(v1.PodList)
-	if err = c.doGet(ctx, "/pods/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
-}
-
-func (c *client) getStatefulSets(ctx context.Context) (list *v1beta1.StatefulSetList, err error) {
-	list = new(v1beta1.StatefulSetList)
-	if err = c.doGet(ctx, "/statefulsets/", list); err != nil {
-		return nil, err
-	}
-	return list, nil
-}
-
-func (c *client) doGet(ctx context.Context, url string, v interface{}) error {
-	req, err := createGetRequest(c.baseURL+url, c.bearerToken)
-	if err != nil {
-		return err
-	}
-	select {
-	case c.semaphore <- struct{}{}:
-		break
-	case <-ctx.Done():
-		return ctx.Err()
-	}
-
-	resp, err := c.httpClient.Do(req.WithContext(ctx))
-	if err != nil {
-		<-c.semaphore
-		return err
-	}
-	defer func() {
-		resp.Body.Close()
-		<-c.semaphore
-	}()
-
-	// Clear invalid token if unauthorized
-	if resp.StatusCode == http.StatusUnauthorized {
-		c.bearerToken = ""
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return APIError{
-			URL:        url,
-			StatusCode: resp.StatusCode,
-			Title:      resp.Status,
-		}
-	}
-
-	if resp.StatusCode == http.StatusNoContent {
-		return nil
-	}
-
-	if err = json.NewDecoder(resp.Body).Decode(v); err != nil {
-		return err
-	}
-	return nil
-}
-
-func createGetRequest(url string, token string) (*http.Request, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	if token != "" {
-		req.Header.Set("Authorization", "Bearer "+token)
-	}
-	req.Header.Add("Accept", "application/json")
-
-	return req, nil
-}
-
-type APIError struct {
-	URL         string
-	StatusCode  int
-	Title       string
-	Description string
-}
-
-func (e APIError) Error() string {
-	if e.Description != "" {
-		return fmt.Sprintf("[%s] %s: %s", e.URL, e.Title, e.Description)
-	}
-	return fmt.Sprintf("[%s] %s", e.URL, e.Title)
+func (c *client) getStatefulSets(ctx context.Context) (*v1beta1.StatefulSetList, error) {
+	list := new(v1beta1.StatefulSetList)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	return list, c.List(ctx, c.namespace, list)
 }
