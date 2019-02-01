8e46414da2901a4d25e00de14ebd2ac0bc91d6fb
Daniel Nelson <daniel.nelson@influxdb.com>
2019-02-01 12:26:00 -0800

    Clean pubsub sample config

--- plugins/inputs/cloud_pubsub/pubsub.go
+++ plugins/inputs/cloud_pubsub/pubsub.go
@@ -1,16 +1,17 @@
 package cloud_pubsub
 
 import (
-	"cloud.google.com/go/pubsub"
 	"context"
 	"fmt"
+	"sync"
+
+	"cloud.google.com/go/pubsub"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/influxdata/telegraf/plugins/parsers"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
-	"sync"
 )
 
 type empty struct{}
@@ -256,22 +257,22 @@ const sampleConfig = `
   ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md
   data_format = "influx"
 
-  ## Optional. Filepath for GCP credentials JSON file to authorize calls to 
-  ## PubSub APIs. If not set explicitly, Telegraf will attempt to use 
-  ## Application Default Credentials, which is preferred. 
+  ## Optional. Filepath for GCP credentials JSON file to authorize calls to
+  ## PubSub APIs. If not set explicitly, Telegraf will attempt to use
+  ## Application Default Credentials, which is preferred.
   # credentials_file = "path/to/my/creds.json"
 
-  ## Optional. Maximum byte length of a message to consume. 
-  ## Larger messages are dropped with an error. If less than 0 or unspecified, 
+  ## Optional. Maximum byte length of a message to consume.
+  ## Larger messages are dropped with an error. If less than 0 or unspecified,
   ## treated as no limit.
   # max_message_len = 1000000
 
-  ## Optional. Maximum messages to read from PubSub that have not been written 
+  ## Optional. Maximum messages to read from PubSub that have not been written
   ## to an output. Defaults to %d.
   ## For best throughput set based on the number of metrics within
   ## each message and the size of the output's metric_batch_size.
   ##
-  ## For example, if each message contains 10 metrics and the output 
+  ## For example, if each message contains 10 metrics and the output
   ## metric_batch_size is 1000, setting this to 100 will ensure that a
   ## full batch is collected and the write is triggered immediately without
   ## waiting until the next flush_interval.
@@ -280,28 +281,28 @@ const sampleConfig = `
   ## The following are optional Subscription ReceiveSettings in PubSub.
   ## Read more about these values:
   ## https://godoc.org/cloud.google.com/go/pubsub#ReceiveSettings
-  
+
   ## Optional. Maximum number of seconds for which a PubSub subscription
   ## should auto-extend the PubSub ACK deadline for each message. If less than
   ## 0, auto-extension is disabled.
   # max_extension = 0
 
-  ## Optional. Maximum number of unprocessed messages in PubSub 
-  ## (unacknowledged but not yet expired in PubSub). 
-  ## A value of 0 is treated as the default PubSub value. 
+  ## Optional. Maximum number of unprocessed messages in PubSub
+  ## (unacknowledged but not yet expired in PubSub).
+  ## A value of 0 is treated as the default PubSub value.
   ## Negative values will be treated as unlimited.
   # max_outstanding_messages = 0
 
-  ## Optional. Maximum size in bytes of unprocessed messages in PubSub 
-  ## (unacknowledged but not yet expired in PubSub). 
-  ## A value of 0 is treated as the default PubSub value. 
+  ## Optional. Maximum size in bytes of unprocessed messages in PubSub
+  ## (unacknowledged but not yet expired in PubSub).
+  ## A value of 0 is treated as the default PubSub value.
   ## Negative values will be treated as unlimited.
   # max_outstanding_bytes = 0
 
-  ## Optional. Max number of goroutines a PubSub Subscription receiver can spawn 
-  ## to pull messages from PubSub concurrently. This limit applies to each 
-  ## subscription separately and is treated as the PubSub default if less than 
-  ## 1. Note this setting does not limit the number of messages that can be 
+  ## Optional. Max number of goroutines a PubSub Subscription receiver can spawn
+  ## to pull messages from PubSub concurrently. This limit applies to each
+  ## subscription separately and is treated as the PubSub default if less than
+  ## 1. Note this setting does not limit the number of messages that can be
   ## processed concurrently (use "max_outstanding_messages" instead).
   # max_receiver_go_routines = 0
 `
