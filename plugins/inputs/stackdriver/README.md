07b266d233f0255a959cad9fbe72f8b4c7347029
Daniel Nelson <daniel.nelson@influxdb.com>
2019-02-06 15:08:11 -0800

    Use snake_case in the config

--- plugins/inputs/stackdriver/README.md
+++ plugins/inputs/stackdriver/README.md
@@ -12,16 +12,16 @@ This plugin uses a [Google Service Account](https://cloud.google.com/docs/authen
 [[inputs.stackdriver]]
   ## GCP Project (required - must be prefixed with "projects/")
   project = "projects/{project_id_or_number}"
-  
+
   ## API rate limit. On a default project, it seems that a single user can make
   ## ~14 requests per second. This might be configurable. Each API request can
   ## fetch every time series for a single metric type, though -- this is plenty
   ## fast for scraping all the builtin metric types (and even a handful of
   ## custom ones) every 60s.
-  # rateLimit = 14
-  
+  # rate_limit = 14
+
   ## Collection Delay Seconds (required - must account for metrics availability via Stackdriver Monitoring API)
-  # delaySeconds = 60
+  # delay_seconds = 60
 
   ## The first query to stackdriver queries for data points that have timestamp t
   ## such that: (now() - delaySeconds - lookbackSeconds) <= t <= (now() - delaySeconds).
@@ -30,70 +30,70 @@ This plugin uses a [Google Service Account](https://cloud.google.com/docs/authen
   ## Note that influx will de-dedupe points that are pulled twice,
   ## so it's best to be safe here, just in case it takes GCP awhile
   ## to get around to recording the data you seek.
-  # lookbackSeconds = 120
+  # lookback_seconds = 120
 
   ## Metric collection period
   interval = "1m"
-  
+
   ## Configure the TTL for the internal cache of timeseries requests.
   ## Defaults to 1 hr if not specified
-  # cacheTTLSeconds = 3600
-  
+  # cache_ttl_seconds = 3600
+
   ## Sets whether or not to scrape all bucket counts for metrics whose value
   ## type is "distribution". If those ~70 fields per metric
   ## type are annoying to you, try out the distributionAggregationAligners
   ## configuration option, wherein you may specifiy a list of aggregate functions
   ## (e.g., ALIGN_PERCENTILE_99) that might be more useful to you.
-  # scrapeDistributionBuckets = true
-  
+  # scrape_distribution_buckets = true
+
   ## Excluded GCP metric types. Any string prefix works.
   ## Only declare either this or includeMetricTypePrefixes
-  excludeMetricTypePrefixes = [
-    "agent",
-    "aws",
-    "custom"
+  exclude_metric_type_prefixes = [
+        "agent",
+        "aws",
+        "custom"
   ]
-  
+
   ## *Only* include these GCP metric types. Any string prefix works
   ## Only declare either this or excludeMetricTypePrefixes
-  # includeMetricTypePrefixes = nil
-  
+  # include_metric_type_prefixes = []
+
   ## Excluded GCP metric and resource tags. Any string prefix works.
   ## Only declare either this or includeTagPrefixes
-  excludeTagPrefixes = [
-    "pod_id",
+  exclude_tag_prefixes = [
+        "pod_id",
   ]
-  
+
   ## *Only* include these GCP metric and resource tags. Any string prefix works
   ## Only declare either this or excludeTagPrefixes
-  # includeTagPrefixes = nil
-  
+  # include_tag_prefixes = nil
+
   ## Declares a list of aggregate functions to be used for metric types whose
   ## value type is "distribution". These aggregate values are recorded in the
   ## distribution's measurement *in addition* to the bucket counts. That is to
   ## say: setting this option is not mutually exclusive with
   ## scrapeDistributionBuckets.
-  distributionAggregationAligners = [
-    "ALIGN_PERCENTILE_99",
-    "ALIGN_PERCENTILE_95",
-    "ALIGN_PERCENTILE_50",
+  distribution_aggregation_aligners = [
+        "ALIGN_PERCENTILE_99",
+        "ALIGN_PERCENTILE_95",
+        "ALIGN_PERCENTILE_50",
   ]
-  
-  ## The filter string consists of logical AND of the 
+
+  ## The filter string consists of logical AND of the
   ## resource labels and metric labels if both of them
   ## are specified. (optional)
   ## See: https://cloud.google.com/monitoring/api/v3/filters
   ## Declares resource labels to filter GCP metrics
   ## that match any of them.
-  # [[inputs.stackdriver.filter.resourceLabels]]
+  # [[inputs.stackdriver.filter.resource_labels]]
   #   key = "instance_name"
   #   value = 'starts_with("localhost")'
-  
+
   ## Declares metric labels to filter GCP metrics
   ## that match any of them.
-  # [[inputs.stackdriver.filter.metricLabels]]
-  #   key = "device_name"
-  #   value = 'one_of("sda", "sdb")'
+  #  [[inputs.stackdriver.filter.metric_labels]]
+  #      key = "device_name"
+  #      value = 'one_of("sda", "sdb")'
 ```
 
 ### Tips
--- plugins/inputs/stackdriver/stackdriver.go
+++ plugins/inputs/stackdriver/stackdriver.go
@@ -9,16 +9,13 @@ import (
 	"sync"
 	"time"
 
-	"google.golang.org/api/iterator"
-
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/internal/limiter"
-	"github.com/influxdata/telegraf/plugins/inputs"
-
-	// Imports the Stackdriver Monitoring client package.
 	monitoring "cloud.google.com/go/monitoring/apiv3"
 	googlepbduration "github.com/golang/protobuf/ptypes/duration"
 	googlepbts "github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal/limiter"
+	"github.com/influxdata/telegraf/plugins/inputs" // Imports the Stackdriver Monitoring client package.
+	"google.golang.org/api/iterator"
 	distributionpb "google.golang.org/genproto/googleapis/api/distribution"
 	metricpb "google.golang.org/genproto/googleapis/api/metric"
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
@@ -27,108 +24,108 @@ import (
 const (
 	description  = "Plugin that scrapes Google's v3 monitoring API."
 	sampleConfig = `
-## GCP Project (required - must be prefixed with "projects/")
-project = "projects/{project_id_or_number}"
+  ## GCP Project (required - must be prefixed with "projects/")
+  project = "projects/{project_id_or_number}"
 
-## API rate limit. On a default project, it seems that a single user can make
-## ~14 requests per second. This might be configurable. Each API request can
-## fetch every time series for a single metric type, though -- this is plenty
-## fast for scraping all the builtin metric types (and even a handful of
-## custom ones) every 60s.
-# rateLimit = 14
+  ## API rate limit. On a default project, it seems that a single user can make
+  ## ~14 requests per second. This might be configurable. Each API request can
+  ## fetch every time series for a single metric type, though -- this is plenty
+  ## fast for scraping all the builtin metric types (and even a handful of
+  ## custom ones) every 60s.
+  # rate_limit = 14
 
-## Collection Delay Seconds (required - must account for metrics availability via Stackdriver Monitoring API)
-# delaySeconds = 60
+  ## Collection Delay Seconds (required - must account for metrics availability via Stackdriver Monitoring API)
+  # delay_seconds = 60
 
-## The first query to stackdriver queries for data points that have timestamp t
-## such that: (now() - delaySeconds - lookbackSeconds) <= t <= (now() - delaySeconds).
-## The subsequence queries to stackdriver query for data points that have timestamp t
-## such that: lastQueryEndTime <= t <= (now() - delaySeconds).
-## Note that influx will de-dedupe points that are pulled twice,
-## so it's best to be safe here, just in case it takes GCP awhile
-## to get around to recording the data you seek.
-# lookbackSeconds = 120
+  ## The first query to stackdriver queries for data points that have timestamp t
+  ## such that: (now() - delaySeconds - lookbackSeconds) <= t <= (now() - delaySeconds).
+  ## The subsequence queries to stackdriver query for data points that have timestamp t
+  ## such that: lastQueryEndTime <= t <= (now() - delaySeconds).
+  ## Note that influx will de-dedupe points that are pulled twice,
+  ## so it's best to be safe here, just in case it takes GCP awhile
+  ## to get around to recording the data you seek.
+  # lookback_seconds = 120
 
-## Metric collection period
-interval = "1m"
+  ## Metric collection period
+  interval = "1m"
 
-## Configure the TTL for the internal cache of timeseries requests.
-## Defaults to 1 hr if not specified
-# cacheTTLSeconds = 3600
+  ## Configure the TTL for the internal cache of timeseries requests.
+  ## Defaults to 1 hr if not specified
+  # cache_ttl_seconds = 3600
 
-## Sets whether or not to scrape all bucket counts for metrics whose value
-## type is "distribution". If those ~70 fields per metric
-## type are annoying to you, try out the distributionAggregationAligners
-## configuration option, wherein you may specifiy a list of aggregate functions
-## (e.g., ALIGN_PERCENTILE_99) that might be more useful to you.
-# scrapeDistributionBuckets = true
+  ## Sets whether or not to scrape all bucket counts for metrics whose value
+  ## type is "distribution". If those ~70 fields per metric
+  ## type are annoying to you, try out the distributionAggregationAligners
+  ## configuration option, wherein you may specifiy a list of aggregate functions
+  ## (e.g., ALIGN_PERCENTILE_99) that might be more useful to you.
+  # scrape_distribution_buckets = true
 
-## Excluded GCP metric types. Any string prefix works.
-## Only declare either this or includeMetricTypePrefixes
-excludeMetricTypePrefixes = [
-	"agent",
-	"aws",
-	"custom"
-]
+  ## Excluded GCP metric types. Any string prefix works.
+  ## Only declare either this or includeMetricTypePrefixes
+  exclude_metric_type_prefixes = [
+  	"agent",
+  	"aws",
+  	"custom"
+  ]
 
-## *Only* include these GCP metric types. Any string prefix works
-## Only declare either this or excludeMetricTypePrefixes
-# includeMetricTypePrefixes = nil
+  ## *Only* include these GCP metric types. Any string prefix works
+  ## Only declare either this or excludeMetricTypePrefixes
+  # include_metric_type_prefixes = []
 
-## Excluded GCP metric and resource tags. Any string prefix works.
-## Only declare either this or includeTagPrefixes
-excludeTagPrefixes = [
-	"pod_id",
-]
+  ## Excluded GCP metric and resource tags. Any string prefix works.
+  ## Only declare either this or includeTagPrefixes
+  exclude_tag_prefixes = [
+  	"pod_id",
+  ]
 
-## *Only* include these GCP metric and resource tags. Any string prefix works
-## Only declare either this or excludeTagPrefixes
-# includeTagPrefixes = nil
+  ## *Only* include these GCP metric and resource tags. Any string prefix works
+  ## Only declare either this or excludeTagPrefixes
+  # include_tag_prefixes = nil
 
-## Declares a list of aggregate functions to be used for metric types whose
-## value type is "distribution". These aggregate values are recorded in the
-## distribution's measurement *in addition* to the bucket counts. That is to
-## say: setting this option is not mutually exclusive with
-## scrapeDistributionBuckets.
-distributionAggregationAligners = [
-	"ALIGN_PERCENTILE_99",
-	"ALIGN_PERCENTILE_95",
-	"ALIGN_PERCENTILE_50",
-]
+  ## Declares a list of aggregate functions to be used for metric types whose
+  ## value type is "distribution". These aggregate values are recorded in the
+  ## distribution's measurement *in addition* to the bucket counts. That is to
+  ## say: setting this option is not mutually exclusive with
+  ## scrapeDistributionBuckets.
+  distribution_aggregation_aligners = [
+  	"ALIGN_PERCENTILE_99",
+  	"ALIGN_PERCENTILE_95",
+  	"ALIGN_PERCENTILE_50",
+  ]
 
-## The filter string consists of logical AND of the 
-## resource labels and metric labels if both of them
-## are specified. (optional)
-## See: https://cloud.google.com/monitoring/api/v3/filters
-## Declares resource labels to filter GCP metrics
-## that match any of them.
-# [[inputs.stackdriver.filter.resourceLabels]]
-#   key = "instance_name"
-#   value = 'starts_with("localhost")'
+  ## The filter string consists of logical AND of the
+  ## resource labels and metric labels if both of them
+  ## are specified. (optional)
+  ## See: https://cloud.google.com/monitoring/api/v3/filters
+  ## Declares resource labels to filter GCP metrics
+  ## that match any of them.
+  # [[inputs.stackdriver.filter.resource_labels]]
+  #   key = "instance_name"
+  #   value = 'starts_with("localhost")'
 
-## Declares metric labels to filter GCP metrics
-## that match any of them.
-#  [[inputs.stackdriver.filter.metricLabels]]
-#  	 key = "device_name"
-#  	 value = 'one_of("sda", "sdb")'
+  ## Declares metric labels to filter GCP metrics
+  ## that match any of them.
+  #  [[inputs.stackdriver.filter.metric_labels]]
+  #  	 key = "device_name"
+  #  	 value = 'one_of("sda", "sdb")'
 `
 )
 
 type (
 	// Stackdriver is the Google Stackdriver config info.
 	Stackdriver struct {
-		Project                         string
-		RateLimit                       int
-		LookbackSeconds                 int64
-		DelaySeconds                    int64
-		CacheTTLSeconds                 int64
-		IncludeMetricTypePrefixes       []string
-		ExcludeMetricTypePrefixes       []string
-		IncludeTagPrefixes              []string
-		ExcludeTagPrefixes              []string
-		ScrapeDistributionBuckets       bool
-		DistributionAggregationAligners []string
-		Filter                          *ListTimeSeriesFilter
+		Project                         string                `toml:"project"`
+		RateLimit                       int                   `toml:"rate_limit"`
+		LookbackSeconds                 int64                 `toml:"lookback_seconds"`
+		DelaySeconds                    int64                 `toml:"delay_seconds"`
+		CacheTTLSeconds                 int64                 `toml:"cache_ttl_seconds"`
+		IncludeMetricTypePrefixes       []string              `toml:"include_metric_type_prefixes"`
+		ExcludeMetricTypePrefixes       []string              `toml:"exclude_metric_type_prefixes"`
+		IncludeTagPrefixes              []string              `toml:"include_tag_prefixes"`
+		ExcludeTagPrefixes              []string              `toml:"exclude_tag_prefixes"`
+		ScrapeDistributionBuckets       bool                  `toml:"scrape_distribution_buckets"`
+		DistributionAggregationAligners []string              `toml:"distribution_aggregation_aligners"`
+		Filter                          *ListTimeSeriesFilter `toml:"filter"`
 
 		client              metricClient
 		timeSeriesConfCache *TimeSeriesConfCache
@@ -139,14 +136,14 @@ type (
 
 	// ListTimeSeriesFilter contains resource labels and metric labels
 	ListTimeSeriesFilter struct {
-		ResourceLabels []*Label
-		MetricLabels   []*Label
+		ResourceLabels []*Label `json:"resource_labels"`
+		MetricLabels   []*Label `json:"metric_labels"`
 	}
 
 	// Label contains key and value
 	Label struct {
-		Key   string
-		Value string
+		Key   string `toml:"key"`
+		Value string `toml:"value"`
 	}
 
 	// TimeSeriesConfCache caches generated timeseries configurations
