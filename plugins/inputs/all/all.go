9d8a574ac77e42fbfafb3375d0c15c7a8eba44e4
Greg <2653109+glinton@users.noreply.github.com>
2019-02-25 13:02:57 -0700

    Add kinesis input plugin (#5341)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -63,6 +63,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/kernel"
 	_ "github.com/influxdata/telegraf/plugins/inputs/kernel_vmstat"
 	_ "github.com/influxdata/telegraf/plugins/inputs/kibana"
+	_ "github.com/influxdata/telegraf/plugins/inputs/kinesis_consumer"
 	_ "github.com/influxdata/telegraf/plugins/inputs/kube_inventory"
 	_ "github.com/influxdata/telegraf/plugins/inputs/kubernetes"
 	_ "github.com/influxdata/telegraf/plugins/inputs/leofs"
