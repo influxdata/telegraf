7d7f19a76241d720cc8b8ece93f68deffc312c41
prydin <prydin@vmware.com>
2019-03-09 11:33:20 -0500

    Removed call to math.Round for compatibility with go 1.9

--- plugins/inputs/vsphere/endpoint.go
+++ plugins/inputs/vsphere/endpoint.go
@@ -1087,3 +1087,11 @@ func cleanDiskTag(disk string) string {
 	// Remove enclosing "<>"
 	return strings.TrimSuffix(strings.TrimPrefix(disk, "<"), ">")
 }
+
+func Round(x float64) float64 {
+	t := math.Trunc(x)
+	if math.Abs(x-t) >= 0.5 {
+		return t + math.Copysign(1, x)
+	}
+	return t
+}
