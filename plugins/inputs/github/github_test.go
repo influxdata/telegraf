a00aca85646b185619eb21b43d81ff986605d69f
David McKay <david@rawkode.dev>
2019-03-15 16:32:17 +0000

    Add Tests for splitRepositoryName

--- plugins/inputs/github/github_test.go
+++ plugins/inputs/github/github_test.go
@@ -1,1 +1,23 @@
 package github
+
+import "testing"
+
+func TestSplitRepositoryNameWithWorkingExample(t *testing.T) {
+	owner, repository, _ := splitRepositoryName("influxdata/influxdb")
+
+	if owner != "influxdata" {
+		t.Errorf("Owner from 'influxdata/influxdb' should return 'influxdata'")
+	}
+
+	if repository != "influxdb" {
+		t.Errorf("Repository from 'influxdata/influxdb' should return 'influxdb'")
+	}
+}
+
+func TestSplitRepositoryNameWithNoSlash(t *testing.T) {
+	_, _, error := splitRepositoryName("influxdata-influxdb")
+
+	if error == nil {
+		t.Errorf("Repository name without a slash should return an error")
+	}
+}
