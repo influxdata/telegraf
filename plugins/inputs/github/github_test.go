b916a7b5749315e2b5c42074c7c52ddd3f3d146d
David McKay <david@rawkode.dev>
2019-03-15 16:43:54 +0000

    Add tests for GetLicense

--- plugins/inputs/github/github_test.go
+++ plugins/inputs/github/github_test.go
@@ -1,6 +1,10 @@
 package github
 
-import "testing"
+import (
+	"testing"
+
+	gh "github.com/google/go-github/github"
+)
 
 func TestSplitRepositoryNameWithWorkingExample(t *testing.T) {
 	owner, repository, _ := splitRepositoryName("influxdata/influxdb")
@@ -19,5 +23,27 @@ func TestSplitRepositoryNameWithNoSlash(t *testing.T) {
 
 	if error == nil {
 		t.Errorf("Repository name without a slash should return an error")
+	}
+}
+
+func TestGetLicenseWhenExists(t *testing.T) {
+	licenseName := "MIT"
+	license := gh.License{Name: &licenseName}
+	repository := gh.Repository{License: &license}
+
+	getLicenseReturn := getLicense(&repository)
+
+	if getLicenseReturn != "MIT" {
+		t.Errorf("GetLicense doesn't return the correct variable")
+	}
+}
+
+func TestGetLicenseWhenMissing(t *testing.T) {
+	repository := gh.Repository{}
+
+	getLicenseReturn := getLicense(&repository)
+
+	if getLicenseReturn != "None" {
+		t.Errorf("GetLicense doesn't return 'None' when no license exists")
 	}
 }
