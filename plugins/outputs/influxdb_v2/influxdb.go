12e1b317d9cd0a2e79558140e706a5054eb68f40
Daniel Nelson <daniel@wavesofdawn.com>
2019-03-26 18:09:35 -0700

    Fix plugin name in influxdb_v2 output logging (#5627)

--- plugins/outputs/influxdb_v2/influxdb.go
+++ plugins/outputs/influxdb_v2/influxdb.go
@@ -162,7 +162,7 @@ func (i *InfluxDB) Write(metrics []telegraf.Metric) error {
 			return nil
 		}
 
-		log.Printf("E! [outputs.influxdb] when writing to [%s]: %v", client.URL(), err)
+		log.Printf("E! [outputs.influxdb_v2] when writing to [%s]: %v", client.URL(), err)
 	}
 
 	return errors.New("could not write any address")
