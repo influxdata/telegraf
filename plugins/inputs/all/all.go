0a68c8468b1cc285316979c9c24fcb0368af0400
David McKay <david@rawkode.dev>
2019-04-03 00:06:15 +0100

    Add github input plugin (#5587)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -38,6 +38,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/filecount"
 	_ "github.com/influxdata/telegraf/plugins/inputs/filestat"
 	_ "github.com/influxdata/telegraf/plugins/inputs/fluentd"
+	_ "github.com/influxdata/telegraf/plugins/inputs/github"
 	_ "github.com/influxdata/telegraf/plugins/inputs/graylog"
 	_ "github.com/influxdata/telegraf/plugins/inputs/haproxy"
 	_ "github.com/influxdata/telegraf/plugins/inputs/hddtemp"
