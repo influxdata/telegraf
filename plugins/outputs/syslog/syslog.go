15acd63755c0d93a2e8232aafa5fdf3bc685d261
javicrespo <javiercrespoalvez@gmail.com>
2019-05-01 03:42:45 +0200

    syslog output plugin: addressing PR remarks (#5759)

--- plugins/outputs/syslog/syslog.go
+++ plugins/outputs/syslog/syslog.go
@@ -4,101 +4,111 @@ import (
 	"crypto/tls"
 	"fmt"
 	"log"
-	"math"
 	"net"
-	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/influxdata/go-syslog/nontransparent"
 	"github.com/influxdata/go-syslog/rfc5424"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
+	framing "github.com/influxdata/telegraf/internal/syslog"
 	tlsint "github.com/influxdata/telegraf/internal/tls"
 	"github.com/influxdata/telegraf/plugins/outputs"
 )
 
 type Syslog struct {
-	Address         string
-	KeepAlivePeriod *internal.Duration
-	DefaultSdid     string
-	DefaultPriority uint64
-	DefaultAppname  string
-	Sdids           []string
-	Framing         Framing
-	Trailer         nontransparent.TrailerType
-	Separator       string `toml:"sdparam_separator"`
+	Address             string
+	KeepAlivePeriod     *internal.Duration
+	DefaultSdid         string
+	DefaultSeverityCode uint8
+	DefaultFacilityCode uint8
+	DefaultAppname      string
+	Sdids               []string
+	Separator           string `toml:"sdparam_separator"`
+	Framing             framing.Framing
+	Trailer             nontransparent.TrailerType
 	net.Conn
 	tlsint.ClientConfig
-	reservedFields map[string]bool
+	mapper *SyslogMapper
 }
 
 var sampleConfig = `
-## URL to connect to
-# address = "tcp://127.0.0.1:8094"
-# address = "tcp://example.com:http"
-# address = "tcp4://127.0.0.1:8094"
-# address = "tcp6://127.0.0.1:8094"
-# address = "tcp6://[2001:db8::1]:8094"
-# address = "udp://127.0.0.1:8094"
-# address = "udp4://127.0.0.1:8094"
-# address = "udp6://127.0.0.1:8094"
+  ## URL to connect to
+  ## ex: address = "tcp://127.0.0.1:8094"
+  ## ex: address = "tcp4://127.0.0.1:8094"
+  ## ex: address = "tcp6://127.0.0.1:8094"
+  ## ex: address = "tcp6://[2001:db8::1]:8094"
+  ## ex: address = "udp://127.0.0.1:8094"
+  ## ex: address = "udp4://127.0.0.1:8094"
+  ## ex: address = "udp6://127.0.0.1:8094"
+  address = "tcp://127.0.0.1:8094"
 
-## Optional TLS Config
-# tls_ca = "/etc/telegraf/ca.pem"
-# tls_cert = "/etc/telegraf/cert.pem"
-# tls_key = "/etc/telegraf/key.pem"
-## Use TLS but skip chain & host verification
-# insecure_skip_verify = false
+  ## Optional TLS Config
+  # tls_ca = "/etc/telegraf/ca.pem"
+  # tls_cert = "/etc/telegraf/cert.pem"
+  # tls_key = "/etc/telegraf/key.pem"
+  ## Use TLS but skip chain & host verification
+  # insecure_skip_verify = false
 
-## Period between keep alive probes.
-## Only applies to TCP sockets.
-## 0 disables keep alive probes.
-## Defaults to the OS configuration.
-# keep_alive_period = "5m"
+  ## Period between keep alive probes.
+  ## Only applies to TCP sockets.
+  ## 0 disables keep alive probes.
+  ## Defaults to the OS configuration.
+  # keep_alive_period = "5m"
 
-## The framing technique with which it is expected that messages are transported (default = "octet-counting").
-## Whether the messages come using the octect-counting (RFC5425#section-4.3.1, RFC6587#section-3.4.1),
-## or the non-transparent framing technique (RFC6587#section-3.4.2).
-## Must be one of "octect-counting", "non-transparent".
-# framing = "octet-counting"
+  ## The framing technique with which it is expected that messages are transported (default = "octet-counting").
+  ## Whether the messages come using the octect-counting (RFC5425#section-4.3.1, RFC6587#section-3.4.1),
+  ## or the non-transparent framing technique (RFC6587#section-3.4.2).
+  ## Must be one of "octect-counting", "non-transparent".
+  # framing = "octet-counting"
 
-## The trailer to be expected in case of non-trasparent framing (default = "LF").
-## Must be one of "LF", or "NUL".
-# trailer = "LF"
+  ## The trailer to be expected in case of non-trasparent framing (default = "LF").
+  ## Must be one of "LF", or "NUL".
+  # trailer = "LF"
 
-### SD-PARAMs settings
-### A syslog message can contain multiple parameters and multiple identifiers within structured data section
-### A syslog message can contain multiple structured data sections.
-### For each unrecognised metric field a SD-PARAMS can be created. 
-### Example
-### Configuration =>
-### sdparam_separator = "_"
-### default_sdid = "default@32473"
-### sdids = ["foo@123", "bar@456"]
-### input => xyzzy,x=y foo@123_value=42,bar@456_value2=84,something_else=1
-### output (structured data only) => [foo@123 value=42][bar@456 value2=84][default@32473 something_else=1 x=y]
+  ### SD-PARAMs settings
+  ### A syslog message can contain multiple parameters and multiple identifiers within structured data section
+  ### A syslog message can contain multiple structured data sections.
+  ### For each unrecognised metric field a SD-PARAMS can be created. 
+  ### Example
+  ### Configuration =>
+  ### sdparam_separator = "_"
+  ### default_sdid = "default@32473"
+  ### sdids = ["foo@123", "bar@456"]
+  ### input => xyzzy,x=y foo@123_value=42,bar@456_value2=84,something_else=1
+  ### output (structured data only) => [foo@123 value=42][bar@456 value2=84][default@32473 something_else=1 x=y]
 
-## SD-PARAMs separator between the sdid and field key (default = "_") 
-sdparam_separator = "_"
+  ## SD-PARAMs separator between the sdid and field key (default = "_") 
+  # sdparam_separator = "_"
 
-## Default sdid used for for fields that don't contain a prefix defined in the explict sdids setting below
-## If no default is specified, no SD-PARAMs will be used for unrecognised field.
-# default_sdid = "default@32473"
+  ## Default sdid used for for fields that don't contain a prefix defined in the explict sdids setting below
+  ## If no default is specified, no SD-PARAMs will be used for unrecognised field.
+  # default_sdid = "default@32473"
 
-##List of explicit prefixes to extract from fields and use as the SDID, if they match (see above example for more details):
-# sdids = ["foo@123", "bar@456"]
-###
+  ##List of explicit prefixes to extract from fields and use as the SDID, if they match (see above example for more details):
+  # sdids = ["foo@123", "bar@456"]
+  ###
 
-# Default PRI value (RFC5424#section-6.2.1) If no metric Field with key "PRI" is defined, this default value is used.
-default_priority = 0
+  ## Default severity value. Severity and Facility are used to calculate the message PRI value (RFC5424#section-6.2.1) 
+  ## Used when no metric field with key "severity_code" is defined.
+  ## If unset, 5 (notice) is the default
+  # default_severity_code = 5
 
-# Default APP-NAME value (RFC5424#section-6.2.5) If no metric Field with key "APP-NAME" is defined, this default value is used.
-default_appname = "Telegraf"
+  ## Default facility value. Facility and Severity are used to calculate the message PRI value (RFC5424#section-6.2.1) 
+  ## Used when no metric field with key "facility_code" is defined.
+  ## If unset, 1 (user-level) is the default
+  # default_facility_code = 1
+
+  ## Default APP-NAME value (RFC5424#section-6.2.5) 
+  ## Used when no metric tag with key "appname" is defined.
+  ## If unset, "Telegraf" is the default
+  # default_appname = "Telegraf"
 `
 
 func (s *Syslog) Connect() error {
+	s.initializeSyslogMapper()
+
 	spl := strings.SplitN(s.Address, "://", 2)
 	if len(spl) != 2 {
 		return fmt.Errorf("invalid address: %s", s.Address)
@@ -170,7 +180,7 @@ func (s *Syslog) Write(metrics []telegraf.Metric) error {
 	}
 
 	for _, metric := range metrics {
-		if msg, err := s.mapMetricToSyslogMessage(metric); err == nil {
+		if msg, err := s.mapper.MapMetricToSyslogMessage(metric); err == nil {
 			msgBytesWithObjectCounting := s.getSyslogMessageBytesWithFraming(msg)
 			if _, err := s.Conn.Write(msgBytesWithObjectCounting); err != nil {
 				if err, ok := err.(net.Error); !ok || !err.Temporary() {
@@ -180,123 +190,45 @@ func (s *Syslog) Write(metrics []telegraf.Metric) error {
 				}
 				return err
 			}
+		} else {
+			log.Printf("E! [outputs.syslog] Failed to create syslog message: %v", err)
 		}
 	}
 	return nil
-}
-
-func formatValue(value interface{}) string {
-	switch v := value.(type) {
-	case string:
-		return value.(string)
-	case bool:
-		if v {
-			return "1"
-		}
-		return "0"
-	case uint64:
-		return strconv.FormatUint(v, 10)
-	case int64:
-		return strconv.FormatInt(v, 10)
-	case float64:
-		if math.IsNaN(v) {
-			return ""
-		}
-
-		if math.IsInf(v, 0) {
-			return ""
-		}
-		return strconv.FormatFloat(v, 'f', -1, 64)
-	}
-
-	return ""
-}
-
-func (s *Syslog) mapMetricToSyslogMessage(metric telegraf.Metric) (*rfc5424.SyslogMessage, error) {
-	msg := &rfc5424.SyslogMessage{}
-	msg.SetVersion(1)
-	msg.SetTimestamp(metric.Time().Format(time.RFC3339))
-
-	if value, ok := metric.GetField("PRI"); ok {
-		if v, err := strconv.ParseUint(formatValue(value), 10, 8); err == nil {
-			msg.SetPriority(uint8(v))
-		}
-	} else {
-		//Use default PRI
-		msg.SetPriority(uint8(s.DefaultPriority))
-	}
-	if value, ok := metric.GetField("APP-NAME"); ok {
-		msg.SetAppname(formatValue(value))
-	} else {
-		//Use default APP-NAME
-		msg.SetAppname(s.DefaultAppname)
-	}
-	if value, ok := metric.GetField("MSGID"); ok {
-		msg.SetMsgID(formatValue(value))
-	} else {
-		// We default to metric name
-		msg.SetMsgID(metric.Name())
-	}
-	// Try with HOSTNAME, then with SOURCE, then take OS Hostname
-	if value, ok := metric.GetField("HOSTNAME"); ok {
-		msg.SetHostname(formatValue(value))
-	} else if value, ok := metric.GetField("SOURCE"); ok {
-		msg.SetHostname(formatValue(value))
-	} else if value, err := os.Hostname(); err == nil {
-		msg.SetHostname(value)
-	}
-	if value, ok := metric.GetField("PROCID"); ok {
-		msg.SetProcID(formatValue(value))
-	}
-	if value, ok := metric.GetField("MSG"); ok {
-		msg.SetMessage(formatValue(value))
-	}
-
-	for key, value := range metric.Fields() {
-		if s.reservedFields[key] {
-			continue
-		}
-		isExplicitSdid := false
-		for _, sdid := range s.Sdids {
-			k := strings.TrimLeft(key, sdid+s.Separator)
-			if len(key) > len(k) {
-				isExplicitSdid = true
-				msg.SetParameter(sdid, k, formatValue(value))
-				break
-			}
-		}
-		if !isExplicitSdid && len(s.DefaultSdid) > 0 {
-			k := strings.TrimLeft(key, s.DefaultSdid+s.Separator)
-			msg.SetParameter(s.DefaultSdid, k, formatValue(value))
-		}
-	}
-	if !msg.Valid() {
-		return msg, fmt.Errorf("Not enough information in the metric to create a valid Syslog message")
-	}
-	return msg, nil
 }
 
 func (s *Syslog) getSyslogMessageBytesWithFraming(msg *rfc5424.SyslogMessage) []byte {
 	msgString, _ := msg.String()
 	msgBytes := []byte(msgString)
 
-	if s.Framing == OctetCounting {
+	if s.Framing == framing.OctetCounting {
 		return append([]byte(strconv.Itoa(len(msgBytes))+" "), msgBytes...)
 	}
 	// Non-transparent framing
 	return append(msgBytes, byte(s.Trailer))
 }
 
+func (s *Syslog) initializeSyslogMapper() {
+	if s.mapper != nil {
+		return
+	}
+	s.mapper = newSyslogMapper()
+	s.mapper.DefaultFacilityCode = s.DefaultFacilityCode
+	s.mapper.DefaultSeverityCode = s.DefaultSeverityCode
+	s.mapper.DefaultAppname = s.DefaultAppname
+	s.mapper.Separator = s.Separator
+	s.mapper.DefaultSdid = s.DefaultSdid
+	s.mapper.Sdids = s.Sdids
+}
+
 func newSyslog() *Syslog {
 	return &Syslog{
-		reservedFields: map[string]bool{
-			"PRI": true, "HOSTNAME": true, "APP-NAME": true,
-			"PROCID": true, "MSGID": true, "MSG": true},
-		Framing:         OctetCounting,
-		Trailer:         nontransparent.LF,
-		Separator:       "_",
-		DefaultPriority: uint64(0),
-		DefaultAppname:  "Telegraf",
+		Framing:             framing.OctetCounting,
+		Trailer:             nontransparent.LF,
+		Separator:           "_",
+		DefaultSeverityCode: uint8(5), // notice
+		DefaultFacilityCode: uint8(1), // user-level
+		DefaultAppname:      "Telegraf",
 	}
 }
 
--- plugins/outputs/syslog/syslog_test.go
+++ plugins/outputs/syslog/syslog_test.go
@@ -2,169 +2,61 @@ package syslog
 
 import (
 	"net"
-	"os"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/influxdata/telegraf"
+	framing "github.com/influxdata/telegraf/internal/syslog"
 	"github.com/influxdata/telegraf/metric"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSyslogMapperWithDefaults(t *testing.T) {
-	// Init plugin
-	s := newSyslog()
-
-	// Init metrics
-	m1, _ := metric.New(
-		"testmetric",
-		map[string]string{},
-		map[string]interface{}{},
-		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC),
-	)
-	hostname, err := os.Hostname()
-	assert.NoError(t, err)
-	syslogMessage, err := s.mapMetricToSyslogMessage(m1)
-	require.NoError(t, err)
-	str, _ := syslogMessage.String()
-	assert.Equal(t, "<0>1 2010-11-10T23:00:00Z "+hostname+" Telegraf - testmetric -", str, "Wrong syslog message")
-}
-
-func TestSyslogMapperWithDefaultSdid(t *testing.T) {
-	// Init plugin
-	s := newSyslog()
-	s.DefaultSdid = "default@32473"
-
-	// Init metrics
-	m1, _ := metric.New(
-		"testmetric",
-		map[string]string{},
-		map[string]interface{}{
-			"PRI":                  uint64(0),
-			"MSG":                  "Test message",
-			"HOSTNAME":             "testhost",
-			"APP-NAME":             "testapp",
-			"PROCID":               uint64(25),
-			"MSGID":                int64(555),
-			"value1":               int64(2),
-			"default@32473_value2": "foo",
-			"value3":               float64(1.2),
-		},
-		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC),
-	)
-
-	syslogMessage, err := s.mapMetricToSyslogMessage(m1)
-	require.NoError(t, err)
-	str, _ := syslogMessage.String()
-	assert.Equal(t, "<0>1 2010-11-10T23:00:00Z testhost testapp 25 555 [default@32473 value1=\"2\" value2=\"foo\" value3=\"1.2\"] Test message", str, "Wrong syslog message")
-}
-
-func TestSyslogMapperWithDefaultSdidAndOtherSdids(t *testing.T) {
-	// Init plugin
-	s := newSyslog()
-	s.DefaultSdid = "default@32473"
-	s.Sdids = []string{"bar@123", "foo@456"}
-
-	// Init metrics
-	m1, _ := metric.New(
-		"testmetric",
-		map[string]string{},
-		map[string]interface{}{
-			"PRI":                  uint64(0),
-			"MSG":                  "Test message",
-			"HOSTNAME":             "testhost",
-			"APP-NAME":             "testapp",
-			"PROCID":               uint64(25),
-			"MSGID":                int64(555),
-			"value1":               int64(2),
-			"default@32473_value2": "default",
-			"bar@123_value3":       int64(2),
-			"foo@456_value4":       "foo",
-		},
-		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC),
-	)
-
-	syslogMessage, err := s.mapMetricToSyslogMessage(m1)
-	require.NoError(t, err)
-	str, _ := syslogMessage.String()
-	assert.Equal(t, "<0>1 2010-11-10T23:00:00Z testhost testapp 25 555 [bar@123 value3=\"2\"][default@32473 value1=\"2\" value2=\"default\"][foo@456 value4=\"foo\"] Test message", str, "Wrong syslog message")
-}
-
-func TestSyslogMapperWithNoSdids(t *testing.T) {
-	// Init plugin
-	s := newSyslog()
-
-	// Init metrics
-	m1, _ := metric.New(
-		"testmetric",
-		map[string]string{},
-		map[string]interface{}{
-			"PRI":                  uint64(0),
-			"MSG":                  "Test message",
-			"HOSTNAME":             "testhost",
-			"APP-NAME":             "testapp",
-			"PROCID":               uint64(25),
-			"MSGID":                int64(555),
-			"value1":               int64(2),
-			"default@32473_value2": "default",
-			"bar@123_value3":       int64(2),
-			"foo@456_value4":       "foo",
-		},
-		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC),
-	)
-
-	syslogMessage, err := s.mapMetricToSyslogMessage(m1)
-	require.NoError(t, err)
-	str, _ := syslogMessage.String()
-	assert.Equal(t, "<0>1 2010-11-10T23:00:00Z testhost testapp 25 555 - Test message", str, "Wrong syslog message")
-}
-
 func TestGetSyslogMessageWithFramingOctectCounting(t *testing.T) {
 	// Init plugin
 	s := newSyslog()
+	s.initializeSyslogMapper()
 
 	// Init metrics
 	m1, _ := metric.New(
 		"testmetric",
-		map[string]string{},
-		map[string]interface{}{
-			"PRI":    uint64(0),
-			"SOURCE": "testhost",
+		map[string]string{
+			"hostname": "testhost",
 		},
+		map[string]interface{}{},
 		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC),
 	)
 
-	syslogMessage, err := s.mapMetricToSyslogMessage(m1)
+	syslogMessage, err := s.mapper.MapMetricToSyslogMessage(m1)
 	require.NoError(t, err)
 	messageBytesWithFraming := s.getSyslogMessageBytesWithFraming(syslogMessage)
 
-	assert.Equal(t, "58 <0>1 2010-11-10T23:00:00Z testhost Telegraf - testmetric -", string(messageBytesWithFraming), "Incorrect Octect counting framing")
+	assert.Equal(t, "59 <13>1 2010-11-10T23:00:00Z testhost Telegraf - testmetric -", string(messageBytesWithFraming), "Incorrect Octect counting framing")
 }
 
 func TestGetSyslogMessageWithFramingNonTransparent(t *testing.T) {
 	// Init plugin
 	s := newSyslog()
-	s.Framing = NonTransparent
+	s.initializeSyslogMapper()
+	s.Framing = framing.NonTransparent
 
 	// Init metrics
 	m1, _ := metric.New(
 		"testmetric",
-		map[string]string{},
-		map[string]interface{}{
-			"PRI":    uint64(0),
-			"SOURCE": "testhost",
+		map[string]string{
+			"hostname": "testhost",
 		},
+		map[string]interface{}{},
 		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC),
 	)
 
-	syslogMessage, err := s.mapMetricToSyslogMessage(m1)
+	syslogMessage, err := s.mapper.MapMetricToSyslogMessage(m1)
 	require.NoError(t, err)
 	messageBytesWithFraming := s.getSyslogMessageBytesWithFraming(syslogMessage)
 
-	assert.Equal(t, "<0>1 2010-11-10T23:00:00Z testhost Telegraf - testmetric -\x00", string(messageBytesWithFraming), "Incorrect Octect counting framing")
+	assert.Equal(t, "<13>1 2010-11-10T23:00:00Z testhost Telegraf - testmetric -\x00", string(messageBytesWithFraming), "Incorrect Octect counting framing")
 }
 
 func TestSyslogWriteWithTcp(t *testing.T) {
@@ -205,7 +97,7 @@ func testSyslogWriteWithStream(t *testing.T, s *Syslog, lconn net.Conn) {
 		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC))
 
 	metrics = append(metrics, m1)
-	syslogMessage, err := s.mapMetricToSyslogMessage(metrics[0])
+	syslogMessage, err := s.mapper.MapMetricToSyslogMessage(metrics[0])
 	require.NoError(t, err)
 	messageBytesWithFraming := s.getSyslogMessageBytesWithFraming(syslogMessage)
 
@@ -219,7 +111,7 @@ func testSyslogWriteWithStream(t *testing.T, s *Syslog, lconn net.Conn) {
 }
 
 func testSyslogWriteWithPacket(t *testing.T, s *Syslog, lconn net.PacketConn) {
-	s.Framing = NonTransparent
+	s.Framing = framing.NonTransparent
 	metrics := []telegraf.Metric{}
 	m1, _ := metric.New(
 		"testmetric",
@@ -228,7 +120,7 @@ func testSyslogWriteWithPacket(t *testing.T, s *Syslog, lconn net.PacketConn) {
 		time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC))
 
 	metrics = append(metrics, m1)
-	syslogMessage, err := s.mapMetricToSyslogMessage(metrics[0])
+	syslogMessage, err := s.mapper.MapMetricToSyslogMessage(metrics[0])
 	require.NoError(t, err)
 	messageBytesWithFraming := s.getSyslogMessageBytesWithFraming(syslogMessage)
 
@@ -298,7 +190,7 @@ func TestSyslogWriteReconnect(t *testing.T) {
 	wg.Wait()
 	assert.NoError(t, lerr)
 
-	syslogMessage, err := s.mapMetricToSyslogMessage(metrics[0])
+	syslogMessage, err := s.mapper.MapMetricToSyslogMessage(metrics[0])
 	require.NoError(t, err)
 	messageBytesWithFraming := s.getSyslogMessageBytesWithFraming(syslogMessage)
 	buf := make([]byte, 256)
