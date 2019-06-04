a0449887a38da6004a2f989e34d8fd9066a8cef5
William Sutton <will@sutton-family.org>
2019-06-04 15:01:59 -0400

    Update hddtemp.go

--- plugins/inputs/hddtemp/hddtemp.go
+++ plugins/inputs/hddtemp/hddtemp.go
@@ -56,6 +56,7 @@ func (h *HDDTemp) Gather(acc telegraf.Accumulator) error {
 					"model":  disk.Model,
 					"unit":   disk.Unit,
 					"status": disk.Status,
+					"diskHost" : disk.diskHost,
 				}
 
 				fields := map[string]interface{}{
