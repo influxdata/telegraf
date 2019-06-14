1ea7863b9b041f282a19c49c5d24bb8d9dca61fe
Daniel Nelson <daniel.nelson@influxdb.com>
2019-06-14 15:26:56 -0700

    Add pivot and unpivot processors (#5991)

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -6,9 +6,11 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/processors/enum"
 	_ "github.com/influxdata/telegraf/plugins/processors/override"
 	_ "github.com/influxdata/telegraf/plugins/processors/parser"
+	_ "github.com/influxdata/telegraf/plugins/processors/pivot"
 	_ "github.com/influxdata/telegraf/plugins/processors/printer"
 	_ "github.com/influxdata/telegraf/plugins/processors/regex"
 	_ "github.com/influxdata/telegraf/plugins/processors/rename"
 	_ "github.com/influxdata/telegraf/plugins/processors/strings"
 	_ "github.com/influxdata/telegraf/plugins/processors/topk"
+	_ "github.com/influxdata/telegraf/plugins/processors/unpivot"
 )
