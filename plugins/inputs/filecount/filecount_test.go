e117a5c1a74f0388a9b38840b98e3c34e13730a7
Nic Grobler <nic.grobler2011@gmail.com>
2019-06-24 20:03:05 +0200

    Fix filecount plugin size tests (#6038)

--- plugins/inputs/filecount/filecount_test.go
+++ plugins/inputs/filecount/filecount_test.go
@@ -2,7 +2,6 @@ package filecount
 
 import (
 	"os"
-	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
@@ -18,7 +17,7 @@ func TestNoFilters(t *testing.T) {
 	matches := []string{"foo", "bar", "baz", "qux",
 		"subdir/", "subdir/quux", "subdir/quuz",
 		"subdir/nested2", "subdir/nested2/qux"}
-	fileCountEquals(t, fc, len(matches), 9084)
+	fileCountEquals(t, fc, len(matches), 5096)
 }
 
 func TestNoFiltersOnChildDir(t *testing.T) {
@@ -30,9 +29,8 @@ func TestNoFiltersOnChildDir(t *testing.T) {
 	tags := map[string]string{"directory": getTestdataDir() + "/subdir"}
 	acc := testutil.Accumulator{}
 	acc.GatherError(fc.Gather)
-
 	require.True(t, acc.HasPoint("filecount", tags, "count", int64(len(matches))))
-	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(4542)))
+	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(600)))
 }
 
 func TestNoRecursiveButSuperMeta(t *testing.T) {
@@ -46,7 +44,7 @@ func TestNoRecursiveButSuperMeta(t *testing.T) {
 	acc.GatherError(fc.Gather)
 
 	require.True(t, acc.HasPoint("filecount", tags, "count", int64(len(matches))))
-	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(4096)))
+	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(200)))
 }
 
 func TestNameFilter(t *testing.T) {
@@ -60,20 +58,22 @@ func TestNonRecursive(t *testing.T) {
 	fc := getNoFilterFileCount()
 	fc.Recursive = false
 	matches := []string{"foo", "bar", "baz", "qux", "subdir"}
-	fileCountEquals(t, fc, len(matches), 4542)
+
+	fileCountEquals(t, fc, len(matches), 4496)
 }
 
 func TestDoubleAndSimpleStar(t *testing.T) {
 	fc := getNoFilterFileCount()
 	fc.Directories = []string{getTestdataDir() + "/**/*"}
 	matches := []string{"qux"}
+
 	tags := map[string]string{"directory": getTestdataDir() + "/subdir/nested2"}
 
 	acc := testutil.Accumulator{}
 	acc.GatherError(fc.Gather)
 
 	require.True(t, acc.HasPoint("filecount", tags, "count", int64(len(matches))))
-	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(446)))
+	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(400)))
 }
 
 func TestRegularOnlyFilter(t *testing.T) {
@@ -82,7 +82,8 @@ func TestRegularOnlyFilter(t *testing.T) {
 	matches := []string{
 		"foo", "bar", "baz", "qux", "subdir/quux", "subdir/quuz",
 		"subdir/nested2/qux"}
-	fileCountEquals(t, fc, len(matches), 892)
+
+	fileCountEquals(t, fc, len(matches), 800)
 }
 
 func TestSizeFilter(t *testing.T) {
@@ -94,23 +95,22 @@ func TestSizeFilter(t *testing.T) {
 
 	fc.Size = internal.Size{Size: 100}
 	matches = []string{"qux", "subdir/nested2//qux"}
-	fileCountEquals(t, fc, len(matches), 892)
+
+	fileCountEquals(t, fc, len(matches), 800)
 }
 
 func TestMTimeFilter(t *testing.T) {
-	oldFile := filepath.Join(getTestdataDir(), "baz")
-	mtime := time.Date(1979, time.December, 14, 18, 25, 5, 0, time.UTC)
-	if err := os.Chtimes(oldFile, mtime, mtime); err != nil {
-		t.Skip("skipping mtime filter test.")
-	}
+
+	mtime := time.Date(2011, time.December, 14, 18, 25, 5, 0, time.UTC)
 	fileAge := time.Since(mtime) - (60 * time.Second)
 
 	fc := getNoFilterFileCount()
 	fc.MTime = internal.Duration{Duration: -fileAge}
 	matches := []string{"foo", "bar", "qux",
 		"subdir/", "subdir/quux", "subdir/quuz",
-		"sbudir/nested2", "subdir/nested2/qux"}
-	fileCountEquals(t, fc, len(matches), 9084)
+		"subdir/nested2", "subdir/nested2/qux"}
+
+	fileCountEquals(t, fc, len(matches), 5096)
 
 	fc.MTime = internal.Duration{Duration: fileAge}
 	matches = []string{"baz"}
@@ -126,12 +126,60 @@ func getNoFilterFileCount() FileCount {
 		Size:        internal.Size{Size: 0},
 		MTime:       internal.Duration{Duration: 0},
 		fileFilters: nil,
+		Fs:          getFakeFileSystem(getTestdataDir()),
 	}
 }
 
 func getTestdataDir() string {
-	_, filename, _, _ := runtime.Caller(1)
-	return strings.Replace(filename, "filecount_test.go", "testdata", 1)
+	dir, err := os.Getwd()
+	if err != nil {
+		// if we cannot even establish the test directory, further progress is meaningless
+		panic(err)
+	}
+
+	var chunks []string
+	var testDirectory string
+
+	if runtime.GOOS == "windows" {
+		chunks = strings.Split(dir, "\\")
+		testDirectory = strings.Join(chunks[:], "\\") + "\\testdata"
+	} else {
+		chunks = strings.Split(dir, "/")
+		testDirectory = strings.Join(chunks[:], "/") + "/testdata"
+	}
+	return testDirectory
+}
+
+func getFakeFileSystem(basePath string) fakeFileSystem {
+	// create our desired "filesystem" object, complete with an internal map allowing our funcs to return meta data as requested
+
+	mtime := time.Date(2015, time.December, 14, 18, 25, 5, 0, time.UTC)
+	olderMtime := time.Date(2010, time.December, 14, 18, 25, 5, 0, time.UTC)
+
+	// set file permisions
+	var fmask uint32 = 0666
+	var dmask uint32 = 0666
+
+	// set directory bit
+	dmask |= (1 << uint(32-1))
+
+	// create a lookup map for getting "files" from the "filesystem"
+	fileList := map[string]fakeFileInfo{
+		basePath:                         {name: "testdata", size: int64(4096), filemode: uint32(dmask), modtime: mtime, isdir: true},
+		basePath + "/foo":                {name: "foo", filemode: uint32(fmask), modtime: mtime},
+		basePath + "/bar":                {name: "bar", filemode: uint32(fmask), modtime: mtime},
+		basePath + "/baz":                {name: "baz", filemode: uint32(fmask), modtime: olderMtime},
+		basePath + "/qux":                {name: "qux", size: int64(400), filemode: uint32(fmask), modtime: mtime},
+		basePath + "/subdir":             {name: "subdir", size: int64(4096), filemode: uint32(dmask), modtime: mtime, isdir: true},
+		basePath + "/subdir/quux":        {name: "quux", filemode: uint32(fmask), modtime: mtime},
+		basePath + "/subdir/quuz":        {name: "quuz", filemode: uint32(fmask), modtime: mtime},
+		basePath + "/subdir/nested2":     {name: "nested2", size: int64(200), filemode: uint32(dmask), modtime: mtime, isdir: true},
+		basePath + "/subdir/nested2/qux": {name: "qux", filemode: uint32(fmask), modtime: mtime, size: int64(400)},
+	}
+
+	fs := fakeFileSystem{files: fileList}
+	return fs
+
 }
 
 func fileCountEquals(t *testing.T, fc FileCount, expectedCount int, expectedSize int) {
