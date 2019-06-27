b882064db15364a2e8128a8adda2d921c9f45794
Luís Picciochi Oliveira <luis.oliveira@feedzai.com>
2019-06-27 12:28:49 +0100

    input/statsd: Fix merge by re-adding missing import

--- plugins/inputs/statsd/statsd_test.go
+++ plugins/inputs/statsd/statsd_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
