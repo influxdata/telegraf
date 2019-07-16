61d3c464a0dd0e5c062dc0ad68027c446cd70636
greg linton <greg@influxdata.com>
2019-07-16 12:23:47 -0600

    Test ingress

--- plugins/inputs/kube_inventory/ingress.go
+++ plugins/inputs/kube_inventory/ingress.go
@@ -16,7 +16,7 @@ func collectIngress(ctx context.Context, acc telegraf.Accumulator, ki *Kubernete
 		return
 	}
 	for _, i := range list.Items {
-		if err = ki.gatherIngressWithIps(*i, acc); err != nil {
+		if err = ki.gatherIngress(*i, acc); err != nil {
 			acc.AddError(err)
 			return
 		}
@@ -24,38 +24,6 @@ func collectIngress(ctx context.Context, acc telegraf.Accumulator, ki *Kubernete
 }
 
 func (ki *KubernetesInventory) gatherIngress(i v1beta1EXT.Ingress, acc telegraf.Accumulator) error {
-	if i.Metadata.CreationTimestamp.GetSeconds() == 0 && i.Metadata.CreationTimestamp.GetNanos() == 0 {
-		return nil
-	}
-
-	fields := map[string]interface{}{
-		"created":    time.Unix(i.Metadata.CreationTimestamp.GetSeconds(), int64(i.Metadata.CreationTimestamp.GetNanos())).UnixNano(),
-		"generation": i.Metadata.GetGeneration(),
-	}
-
-	tags := map[string]string{
-		"ingress_name": i.Metadata.GetName(),
-		"namespace":    i.Metadata.GetNamespace(),
-	}
-
-	for _, rule := range i.GetSpec().GetRules() {
-		for _, path := range rule.GetIngressRuleValue().GetHttp().GetPaths() {
-			fields["backend_service_port"] = path.GetBackend().GetServicePort().GetIntVal()
-			fields["tls"] = i.GetSpec().GetTls() != nil
-
-			tags["backend_service_name"] = path.GetBackend().GetServiceName()
-			tags["path"] = path.GetPath()
-			tags["host"] = rule.GetHost()
-
-			acc.AddFields(ingressMeasurement, fields, tags)
-		}
-	}
-
-	return nil
-}
-
-// todo: do we want to add cardinality and collect values from `i.GetStatus().GetLoadBalancer().GetIngress()`
-func (ki *KubernetesInventory) gatherIngressWithIps(i v1beta1EXT.Ingress, acc telegraf.Accumulator) error {
 	if i.Metadata.CreationTimestamp.GetSeconds() == 0 && i.Metadata.CreationTimestamp.GetNanos() == 0 {
 		return nil
 	}
