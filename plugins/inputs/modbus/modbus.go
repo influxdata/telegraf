ddbda66c8f37a7e040052800078bb285e86bfbfd
olais <olais@gmail.com>
2019-07-25 22:34:33 -0500

    run make fmt

--- plugins/inputs/modbus/modbus.go
+++ plugins/inputs/modbus/modbus.go
@@ -1,41 +1,41 @@
 package modbus
 
 import (
+	"encoding/binary"
 	"errors"
+	"math"
 	"sort"
 	"strconv"
 	"time"
-	"math"
-	"encoding/binary"	
-	
+
 	mb "github.com/goburrow/modbus"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
 type Modbus struct {
-	Type           string
-	Controller     string
-	Port           int
-	BaudRate       int
-	DataBits       int
-	Parity         string
-	StopBits       int
-	SlaveId        int
-	Timeout        int
-	Registers      registers
-	isConnected    bool
-	isInitialized  bool
+	Type          string
+	Controller    string
+	Port          int
+	BaudRate      int
+	DataBits      int
+	Parity        string
+	StopBits      int
+	SlaveId       int
+	Timeout       int
+	Registers     registers
+	isConnected   bool
+	isInitialized bool
 	HandlerTcp    *mb.TCPClientHandler
 	HandlerSerial *mb.RTUClientHandler
-	Client         mb.Client
+	Client        mb.Client
 }
 
 type registers struct {
-	DiscreteInputs 	 	register
-	Coils          	 	register
-	HoldingRegisters 	register
-	InputRegisters  	register
+	DiscreteInputs   register
+	Coils            register
+	HoldingRegisters register
+	InputRegisters   register
 }
 
 type register struct {
@@ -59,8 +59,8 @@ type chunk struct {
 }
 
 const (
-    C_DIGITAL	= "digital"
-    C_ANALOG	= "analog"    
+	C_DIGITAL = "digital"
+	C_ANALOG  = "analog"
 )
 
 var ModbusConfig = `
@@ -146,30 +146,30 @@ func (s *Modbus) Description() string {
 	return "Modbus client"
 }
 
-func removeDuplicates(elements []int) []int {	
+func removeDuplicates(elements []int) []int {
 	encountered := map[int]bool{}
 	result := []int{}
 
 	for v := range elements {
-		if encountered[elements[v]] == true {			
-		} else {			
-			encountered[elements[v]] = true			
+		if encountered[elements[v]] == true {
+		} else {
+			encountered[elements[v]] = true
 			result = append(result, elements[v])
 		}
 	}
-	
+
 	return result
 }
 
 func createRawValueMap(r []tag, rawValue map[int]uint16) {
 	addr := []int{}
-	
+
 	for _, element := range r {
 		for _, a := range element.Address {
 			addr = append(addr, a)
 		}
 	}
-	
+
 	addr = removeDuplicates(addr)
 	sort.Ints(addr)
 
@@ -186,12 +186,12 @@ func createChunks(ch *[]chunk, rawValue map[int]uint16) {
 	for k := range rawValue {
 		r = append(r, k)
 	}
-	
+
 	sort.Ints(r)
 
 	for i, element := range r {
 		n := 1
-		if i + 1 == len(r) {
+		if i+1 == len(r) {
 			n = -1
 			if len(r) == 1 {
 				n = 0
@@ -199,7 +199,7 @@ func createChunks(ch *[]chunk, rawValue map[int]uint16) {
 		}
 		if element+n == r[i+n] {
 			chunk_t = append(chunk_t, element)
-			if i + 1 == len(r) {
+			if i+1 == len(r) {
 				chunks = append(chunks, chunk_t)
 			}
 		} else {
@@ -208,10 +208,10 @@ func createChunks(ch *[]chunk, rawValue map[int]uint16) {
 			chunk_t = []int{}
 		}
 	}
-	
+
 	for _, element := range chunks {
 		*ch = append(*ch, chunk{element[0], len(element)})
-	}	
+	}
 }
 
 func initialization(m *Modbus) {
@@ -228,12 +228,12 @@ func initialization(m *Modbus) {
 	createChunks(&m.Registers.DiscreteInputs.Chunks, m.Registers.DiscreteInputs.RawValues)
 	createChunks(&m.Registers.Coils.Chunks, m.Registers.Coils.RawValues)
 	createChunks(&m.Registers.HoldingRegisters.Chunks, m.Registers.HoldingRegisters.RawValues)
-	createChunks(&m.Registers.InputRegisters.Chunks, m.Registers.InputRegisters.RawValues)	
+	createChunks(&m.Registers.InputRegisters.Chunks, m.Registers.InputRegisters.RawValues)
 }
 
 func connect(m *Modbus) error {
 	switch m.Type {
-	case "TCP":	
+	case "TCP":
 		m.HandlerTcp = mb.NewTCPClientHandler(m.Controller + ":" + strconv.Itoa(m.Port))
 		m.HandlerTcp.Timeout = time.Duration(m.Timeout) * time.Second
 		m.HandlerTcp.SlaveId = byte(m.SlaveId)
@@ -245,7 +245,7 @@ func connect(m *Modbus) error {
 		}
 		m.isConnected = true
 		return nil
-	case "RTU":		
+	case "RTU":
 		m.HandlerSerial = mb.NewRTUClientHandler(m.Controller)
 		m.HandlerSerial.Timeout = time.Duration(m.Timeout) * time.Second
 		m.HandlerSerial.SlaveId = byte(m.SlaveId)
@@ -267,17 +267,18 @@ func connect(m *Modbus) error {
 }
 
 type fn func(uint16, uint16) ([]byte, error)
+
 func getRawValue(t string, f fn, ch []chunk, r map[int]uint16) error {
-	for _, chunk_t := range ch {		
+	for _, chunk_t := range ch {
 		results, err := f(uint16(chunk_t.Address), uint16(chunk_t.Length))
 		if err != nil {
 			return err
 		}
-		
+
 		if t == C_DIGITAL {
-			for i := 0; i < len(results); i++ {				
-				for b := 0; b < chunk_t.Length; b++ {				
-					r[chunk_t.Address + b] = uint16(results[i] >> uint(b) & 0x01)
+			for i := 0; i < len(results); i++ {
+				for b := 0; b < chunk_t.Length; b++ {
+					r[chunk_t.Address+b] = uint16(results[i] >> uint(b) & 0x01)
 				}
 
 			}
@@ -285,8 +286,8 @@ func getRawValue(t string, f fn, ch []chunk, r map[int]uint16) error {
 
 		if t == C_ANALOG {
 			for i := 0; i < len(results); i += 2 {
-				register := uint16(results[i]) << 8 | uint16(results[i + 1])
-				r[chunk_t.Address + i / 2] = uint16(register)				
+				register := uint16(results[i])<<8 | uint16(results[i+1])
+				r[chunk_t.Address+i/2] = uint16(register)
 			}
 		}
 	}
@@ -296,10 +297,10 @@ func getRawValue(t string, f fn, ch []chunk, r map[int]uint16) error {
 
 func convertEndianness16(o string, r []byte) interface{} {
 	switch o {
-	case "AB":		
+	case "AB":
 		return binary.BigEndian.Uint16(r)
 	case "BA":
-		return binary.LittleEndian.Uint16(r)	
+		return binary.LittleEndian.Uint16(r)
 	default:
 		return r
 	}
@@ -311,17 +312,17 @@ func convertEndianness32(o string, r []byte) interface{} {
 		return binary.BigEndian.Uint32(r)
 	case "DCBA":
 		return binary.LittleEndian.Uint32(r)
-	case "BADC":			
-		return uint32(binary.LittleEndian.Uint16(r[0:])) << 16 | uint32(binary.LittleEndian.Uint16(r[2:]))
+	case "BADC":
+		return uint32(binary.LittleEndian.Uint16(r[0:]))<<16 | uint32(binary.LittleEndian.Uint16(r[2:]))
 	case "CDAB":
-		return uint32(binary.BigEndian.Uint16(r[2:])) << 16 | uint32(binary.BigEndian.Uint16(r[0:]))
+		return uint32(binary.BigEndian.Uint16(r[2:]))<<16 | uint32(binary.BigEndian.Uint16(r[0:]))
 	default:
 		return r
 	}
 }
 
-func format16(f string, r uint16)  interface{}{
-	switch f{
+func format16(f string, r uint16) interface{} {
+	switch f {
 	case "UINT16":
 		return r
 	case "INT16":
@@ -331,8 +332,8 @@ func format16(f string, r uint16)  interface{}{
 	}
 }
 
-func format32(f string, r uint32)  interface{}{
-	switch f{
+func format32(f string, r uint32) interface{} {
+	switch f {
 	case "UINT32":
 		return r
 	case "INT32":
@@ -351,14 +352,14 @@ func scale16(s string, v uint16) interface{} {
 	operator := s[0]
 	switch operator {
 	case '/':
-		div, err := strconv.Atoi(s[1:len(s)])
-		if err == nil {		
+		div, err := strconv.Atoi(s[1:])
+		if err == nil {
 			return float32(v) / float32(div)
 		}
 		return 0
 	case '*':
-		div, err := strconv.Atoi(s[1:len(s)])
-		if err == nil {			
+		div, err := strconv.Atoi(s[1:])
+		if err == nil {
 			return v * uint16(div)
 		}
 		return 0
@@ -374,14 +375,14 @@ func scale32(s string, v uint32) interface{} {
 	operator := s[0]
 	switch operator {
 	case '/':
-		div, err := strconv.Atoi(s[1:len(s)])
-		if err == nil {		
+		div, err := strconv.Atoi(s[1:])
+		if err == nil {
 			return float32(v) / float32(div)
 		}
 		return 0
 	case '*':
-		div, err := strconv.Atoi(s[1:len(s)])
-		if err == nil {			
+		div, err := strconv.Atoi(s[1:])
+		if err == nil {
 			return v * uint32(div)
 		}
 		return 0
@@ -394,64 +395,64 @@ func setTag(s string, t []tag, r map[int]uint16) {
 	if s == C_DIGITAL {
 		for i := 0; i < len(t); i++ {
 			t[i].Value = r[t[i].Address[0]]
-		}		
+		}
 	}
 
 	if s == C_ANALOG {
-		for i := 0; i < len(t); i++ {		
+		for i := 0; i < len(t); i++ {
 			rawValues := []byte{}
-			for _, rv := range t[i].Address {								
-				rawValues = append(rawValues, byte(r[rv] >> 8) )
-				rawValues = append(rawValues, byte(r[rv] & 0x00FF) )
+			for _, rv := range t[i].Address {
+				rawValues = append(rawValues, byte(r[rv]>>8))
+				rawValues = append(rawValues, byte(r[rv]&0x00FF))
 			}
-			
+
 			switch t[i].DataType {
-			case "UINT16":							
+			case "UINT16":
 				e := convertEndianness16(t[i].Order, rawValues)
 				e16, _ := e.(uint16)
-				f := format16(t[i].DataType, e16)	
-				f16 := f.(uint16)			
+				f := format16(t[i].DataType, e16)
+				f16 := f.(uint16)
 				s := scale16(t[i].Scale, f16)
 				t[i].Value = s
-			case "INT16":							
+			case "INT16":
 				e := convertEndianness16(t[i].Order, rawValues)
 				e16, _ := e.(uint16)
-				f := format16(t[i].DataType, e16)					
+				f := format16(t[i].DataType, e16)
 				t[i].Value = f
-			case "UINT32":							
+			case "UINT32":
 				e := convertEndianness32(t[i].Order, rawValues)
 				e32, _ := e.(uint32)
 				f := format32(t[i].DataType, e32)
 				f32 := f.(uint32)
 				s := scale32(t[i].Scale, f32)
-				t[i].Value = s	
-			case "INT32":							
+				t[i].Value = s
+			case "INT32":
 				e := convertEndianness32(t[i].Order, rawValues)
 				e32, _ := e.(uint32)
-				f := format32(t[i].DataType, e32)				
+				f := format32(t[i].DataType, e32)
 				t[i].Value = f
 			case "FLOAT32-IEEE":
 				e := convertEndianness32(t[i].Order, rawValues)
 				e32, _ := e.(uint32)
-				f := format32(t[i].DataType, e32)				
+				f := format32(t[i].DataType, e32)
 				t[i].Value = f
 			case "FLOAT32":
-				if len(rawValues) == 2 {					
+				if len(rawValues) == 2 {
 					e := convertEndianness16(t[i].Order, rawValues)
 					e16, _ := e.(uint16)
-					f := format16(t[i].DataType, e16)	
-					f16 := f.(uint16)			
+					f := format16(t[i].DataType, e16)
+					f16 := f.(uint16)
 					s := scale16(t[i].Scale, f16)
 					t[i].Value = s
-				} else {					
+				} else {
 					e := convertEndianness32(t[i].Order, rawValues)
-					e32, _ := e.(uint32)										
+					e32, _ := e.(uint32)
 					s := scale32(t[i].Scale, e32)
-					t[i].Value = s	
+					t[i].Value = s
 				}
-				
+
 			default:
-				t[i].Value = 0				
+				t[i].Value = 0
 			}
 		}
 	}
@@ -459,7 +460,7 @@ func setTag(s string, t []tag, r map[int]uint16) {
 
 func addFields(t []tag) map[string]interface{} {
 	fields := make(map[string]interface{})
-	for i:=0 ; i < len(t); i++ {
+	for i := 0; i < len(t); i++ {
 		if len(t[i].Name) > 0 {
 			fields[t[i].Name] = t[i].Value
 		} else {
@@ -467,7 +468,7 @@ func addFields(t []tag) map[string]interface{} {
 			for _, e := range t[i].Address {
 				name = name + "-" + strconv.Itoa(e)
 			}
-			name = name[:len(name) - 1]
+			name = name[:len(name)-1]
 			fields[name] = t[i]
 		}
 	}
@@ -475,7 +476,7 @@ func addFields(t []tag) map[string]interface{} {
 	return fields
 }
 
-func (m *Modbus) Gather(acc telegraf.Accumulator) error {	
+func (m *Modbus) Gather(acc telegraf.Accumulator) error {
 	fields := make(map[string]interface{})
 	tags := make(map[string]string)
 
@@ -495,37 +496,37 @@ func (m *Modbus) Gather(acc telegraf.Accumulator) error {
 	}
 
 	// Get Raw Values
-	err := getRawValue(C_DIGITAL, 
-			   m.Client.ReadDiscreteInputs, 
-			   m.Registers.DiscreteInputs.Chunks, 
-			   m.Registers.DiscreteInputs.RawValues)
+	err := getRawValue(C_DIGITAL,
+		m.Client.ReadDiscreteInputs,
+		m.Registers.DiscreteInputs.Chunks,
+		m.Registers.DiscreteInputs.RawValues)
 	if err != nil {
 		m.isConnected = false
 		return err
 	}
 
-	err = getRawValue(C_DIGITAL, 
-			  m.Client.ReadCoils, 
-			  m.Registers.Coils.Chunks, 
-			  m.Registers.Coils.RawValues)
+	err = getRawValue(C_DIGITAL,
+		m.Client.ReadCoils,
+		m.Registers.Coils.Chunks,
+		m.Registers.Coils.RawValues)
 	if err != nil {
 		m.isConnected = false
 		return err
 	}
 
-	err = getRawValue(C_ANALOG, 
-			  m.Client.ReadHoldingRegisters, 
-			  m.Registers.HoldingRegisters.Chunks, 
-			  m.Registers.HoldingRegisters.RawValues)
+	err = getRawValue(C_ANALOG,
+		m.Client.ReadHoldingRegisters,
+		m.Registers.HoldingRegisters.Chunks,
+		m.Registers.HoldingRegisters.RawValues)
 	if err != nil {
 		m.isConnected = false
 		return err
 	}
 
-	err = getRawValue(C_ANALOG, 
-			  m.Client.ReadInputRegisters, 
-			  m.Registers.InputRegisters.Chunks, 
-			  m.Registers.InputRegisters.RawValues)
+	err = getRawValue(C_ANALOG,
+		m.Client.ReadInputRegisters,
+		m.Registers.InputRegisters.Chunks,
+		m.Registers.InputRegisters.RawValues)
 	if err != nil {
 		m.isConnected = false
 		return err
@@ -546,7 +547,7 @@ func (m *Modbus) Gather(acc telegraf.Accumulator) error {
 	acc.AddFields("modbus.HoldingRegister", fields, tags)
 	fields = addFields(m.Registers.InputRegisters.Tags)
 	acc.AddFields("modbus.InputRegisters", fields, tags)
-	
+
 	return nil
 }
 
--- plugins/inputs/modbus/modbus_test.go
+++ plugins/inputs/modbus/modbus_test.go
@@ -1,261 +1,261 @@
 package modbus
 
-import (	
+import (
 	"testing"
 
 	m "github.com/goburrow/modbus"
+	"github.com/stretchr/testify/assert"
 	"github.com/tbrandon/mbserver"
-	"github.com/stretchr/testify/assert"	
-	
+
 	"github.com/influxdata/telegraf/testutil"
 )
 
-type mbTest struct{
-	Name 		   string
-	Address        uint16
-	Quantity       uint16							
-	WriteValue   []byte
-	ExpectValue    interface{}	
+type mbTest struct {
+	Name        string
+	Address     uint16
+	Quantity    uint16
+	WriteValue  []byte
+	ExpectValue interface{}
 }
 
-func TestCoils(t *testing.T){		
+func TestCoils(t *testing.T) {
 	serv := mbserver.NewServer()
 	err := serv.ListenTCP("localhost:1502")
 	defer serv.Close()
 	assert.NoError(t, err)
 
-	handler := m.NewTCPClientHandler("localhost:1502")	
+	handler := m.NewTCPClientHandler("localhost:1502")
 	err = handler.Connect()
 	assert.NoError(t, err)
 	defer handler.Close()
 	client := m.NewClient(handler)
-					
+
 	modbus := Modbus{
-		Type : "TCP",
-		Controller : "localhost",   
-		Port : 1502,         								
-		SlaveId : 1,        
-		Timeout : 0,        
-		Registers : registers{
-			Coils : register{
+		Type:       "TCP",
+		Controller: "localhost",
+		Port:       1502,
+		SlaveId:    1,
+		Timeout:    0,
+		Registers: registers{
+			Coils: register{
 				Tags: []tag{
-					{Name : "Coil0", Address : []int{0},},
-					{Name : "Coil1", Address : []int{1},},
-				},				
+					{Name: "Coil0", Address: []int{0}},
+					{Name: "Coil1", Address: []int{1}},
+				},
 			},
 		},
 	}
-	
+
 	coilTests := []mbTest{}
-	coilTest := mbTest{Address : 0, Quantity : 2, WriteValue: []byte{0x03}, ExpectValue : []byte{0x03},}	
+	coilTest := mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x03}, ExpectValue: []byte{0x03}}
 	coilTests = append(coilTests, coilTest)
-	coilTest = mbTest{Address : 0, Quantity : 2, WriteValue: []byte{0x02}, ExpectValue : []byte{0x02},}	
+	coilTest = mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x02}, ExpectValue: []byte{0x02}}
 	coilTests = append(coilTests, coilTest)
-	coilTest = mbTest{Address : 0, Quantity : 2, WriteValue: []byte{0x01}, ExpectValue : []byte{0x01},}	
+	coilTest = mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x01}, ExpectValue: []byte{0x01}}
 	coilTests = append(coilTests, coilTest)
-	coilTest = mbTest{Address : 0, Quantity : 2, WriteValue: []byte{0x00}, ExpectValue : []byte{0x00},}	
+	coilTest = mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x00}, ExpectValue: []byte{0x00}}
 	coilTests = append(coilTests, coilTest)
-	
-	for _, coilTest := range coilTests {				
+
+	for _, coilTest := range coilTests {
 		_, err = client.WriteMultipleCoils(coilTest.Address, coilTest.Quantity, coilTest.WriteValue)
-		assert.NoError(t, err)	
+		assert.NoError(t, err)
 
 		var acc testutil.Accumulator
 		modbus.Gather(&acc)
-		
+
 		res := []byte{0x00}
-		for i, t := range modbus.Registers.Coils.Tags {						
-			v, _ := t.Value.(uint16)			
-			res[0] = res[0] | byte(v << uint(i))			
+		for i, t := range modbus.Registers.Coils.Tags {
+			v, _ := t.Value.(uint16)
+			res[0] = res[0] | byte(v<<uint(i))
 		}
-		
-		assert.Equal(t, coilTest.ExpectValue, res)		
-	}				
+
+		assert.Equal(t, coilTest.ExpectValue, res)
+	}
 }
 
-func TestRegisters(t *testing.T){	
+func TestRegisters(t *testing.T) {
 	serv := mbserver.NewServer()
 	err := serv.ListenTCP("localhost:1502")
 	defer serv.Close()
 	assert.NoError(t, err)
 
-	handler := m.NewTCPClientHandler("localhost:1502")	
+	handler := m.NewTCPClientHandler("localhost:1502")
 	err = handler.Connect()
 	assert.NoError(t, err)
 	defer handler.Close()
 	client := m.NewClient(handler)
 
-	modbus := Modbus {
-		Type : "TCP",
-		Controller : "localhost",   
-		Port : 1502,         								
-		SlaveId : 1,        
-		Timeout : 0,        
-		Registers : registers { 
-			HoldingRegisters : register {
+	modbus := Modbus{
+		Type:       "TCP",
+		Controller: "localhost",
+		Port:       1502,
+		SlaveId:    1,
+		Timeout:    0,
+		Registers: registers{
+			HoldingRegisters: register{
 				Tags: []tag{
-					{ 
-						Name : "Register0",    
-					        Order : "AB",   
-					        DataType : "FLOAT32",      
-					        Address : []int{0},    
-					        Scale : "/10", 
+					{
+						Name:     "Register0",
+						Order:    "AB",
+						DataType: "FLOAT32",
+						Address:  []int{0},
+						Scale:    "/10",
 					},
-					{ 
-						Name : "Register1-2",  
-					        Order : "ABCD", 
-					        DataType : "FLOAT32",       
-					        Address : []int{1, 2}, 
-					        Scale : "/1000",
+					{
+						Name:     "Register1-2",
+						Order:    "ABCD",
+						DataType: "FLOAT32",
+						Address:  []int{1, 2},
+						Scale:    "/1000",
 					},
-					{ 
-						Name : "Register3-4",  
-					  	Order : "ABCD", 
-					  	DataType : "FLOAT32",      
-						Address : []int{3, 4}, 
-						Scale : "/10",
+					{
+						Name:     "Register3-4",
+						Order:    "ABCD",
+						DataType: "FLOAT32",
+						Address:  []int{3, 4},
+						Scale:    "/10",
 					},
-					{ 
-						Name : "Register7",    
-						Order : "AB",   
-						DataType : "FLOAT32",      
-						Address : []int{7},    
-						Scale : "/10", 
+					{
+						Name:     "Register7",
+						Order:    "AB",
+						DataType: "FLOAT32",
+						Address:  []int{7},
+						Scale:    "/10",
 					},
-					{ 
-						Name : "Uint16AB",     
-						Order : "AB",   
-						DataType : "UINT16",	    
-						Address : []int{10},
+					{
+						Name:     "Uint16AB",
+						Order:    "AB",
+						DataType: "UINT16",
+						Address:  []int{10},
 					},
-					{ 
-						Name : "Uint16BA",     
-						Order : "BA",   
-						DataType : "UINT16",       
-						Address : []int{10},
+					{
+						Name:     "Uint16BA",
+						Order:    "BA",
+						DataType: "UINT16",
+						Address:  []int{10},
 					},
-					{ 
-						Name : "Int16AB",      
-					 	Order : "AB",   
-						DataType : "INT16",        
-						Address : []int{10}, 
+					{
+						Name:     "Int16AB",
+						Order:    "AB",
+						DataType: "INT16",
+						Address:  []int{10},
 					},
-					{ 
-						Name : "Int16BA",      
-						Order : "BA",   
-						DataType : "INT16",        
-						Address : []int{10}, 
-					},	
-					{ 
-						Name : "Int32ABCD",    
-						Order : "ABCD", 
-						DataType : "INT32",        
-						Address : []int{10, 11},
+					{
+						Name:     "Int16BA",
+						Order:    "BA",
+						DataType: "INT16",
+						Address:  []int{10},
 					},
-					{ 
-						Name : "Int32DCBA",    
-						Order : "DCBA", 
-						DataType : "INT32",        
-						Address : []int{10, 11},
+					{
+						Name:     "Int32ABCD",
+						Order:    "ABCD",
+						DataType: "INT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "Int32BADC",    
-						Order : "BADC", 
-						DataType : "INT32",        
-						Address : []int{10, 11},
+					{
+						Name:     "Int32DCBA",
+						Order:    "DCBA",
+						DataType: "INT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "Int32CDAB",    
-						Order : "CDAB", 
-						DataType : "INT32",        
-						Address : []int{10, 11},
+					{
+						Name:     "Int32BADC",
+						Order:    "BADC",
+						DataType: "INT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "Uint32ABCD",   
-						Order : "ABCD", 
-						DataType : "UINT32",       
-						Address : []int{10, 11},
+					{
+						Name:     "Int32CDAB",
+						Order:    "CDAB",
+						DataType: "INT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "Uint32DCBA",   
-						Order : "DCBA", 
-						DataType : "UINT32",       
-						Address : []int{10, 11},
+					{
+						Name:     "Uint32ABCD",
+						Order:    "ABCD",
+						DataType: "UINT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "Uint32BADC",   
-						Order : "BADC", 
-						DataType : "UINT32",	    
-						Address : []int{10, 11},
+					{
+						Name:     "Uint32DCBA",
+						Order:    "DCBA",
+						DataType: "UINT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "Uint32CDAB",   
-						Order : "CDAB", 
-						DataType : "UINT32",       
-						Address : []int{10, 11},
+					{
+						Name:     "Uint32BADC",
+						Order:    "BADC",
+						DataType: "UINT32",
+						Address:  []int{10, 11},
 					},
-					{ 
-						Name : "FLOAT32-IEEE", 
-						Order : "ABCD", 
-						DataType : "FLOAT32-IEEE",	
-						Address : []int{10, 11},
-					},										
-				},				
+					{
+						Name:     "Uint32CDAB",
+						Order:    "CDAB",
+						DataType: "UINT32",
+						Address:  []int{10, 11},
+					},
+					{
+						Name:     "FLOAT32-IEEE",
+						Order:    "ABCD",
+						DataType: "FLOAT32-IEEE",
+						Address:  []int{10, 11},
+					},
+				},
 			},
 		},
 	}
 
 	regTests := []mbTest{}
-	regTest := mbTest { Name : "Register0", Address : 0, Quantity : 1, WriteValue: []byte{0x08, 0x98,}, ExpectValue : float32(220) }	
+	regTest := mbTest{Name: "Register0", Address: 0, Quantity: 1, WriteValue: []byte{0x08, 0x98}, ExpectValue: float32(220)}
 	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Register1-2", Address : 1, Quantity : 2, WriteValue: []byte{0x00, 0x00, 0x03, 0xE8,}, ExpectValue : float32(1)}	
+	regTest = mbTest{Name: "Register1-2", Address: 1, Quantity: 2, WriteValue: []byte{0x00, 0x00, 0x03, 0xE8}, ExpectValue: float32(1)}
 	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Register3-4", Address : 3, Quantity : 2, WriteValue: []byte{0x00, 0x00, 0x08, 0x98,}, ExpectValue : float32(220)}	
+	regTest = mbTest{Name: "Register3-4", Address: 3, Quantity: 2, WriteValue: []byte{0x00, 0x00, 0x08, 0x98}, ExpectValue: float32(220)}
 	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Register7", Address : 7, Quantity : 1, WriteValue: []byte{0x01, 0xF4}, ExpectValue : float32(50)}	
-	regTests = append(regTests, regTest)
-	
-	regTest = mbTest {Name : "Uint16AB", Address : 10, Quantity : 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue : uint16(43981)}	
-	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Uint16BA", Address : 10, Quantity : 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue : uint16(52651)}	
-	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Int16AB", Address : 10, Quantity : 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue : int16(-21555)}	
-	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Int16BA", Address : 10, Quantity : 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue : int16(-12885)}	
+	regTest = mbTest{Name: "Register7", Address: 7, Quantity: 1, WriteValue: []byte{0x01, 0xF4}, ExpectValue: float32(50)}
 	regTests = append(regTests, regTest)
 
-	regTest = mbTest {Name : "Int32ABCD", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : int32(-1430532899)}	
+	regTest = mbTest{Name: "Uint16AB", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: uint16(43981)}
 	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Int32DCBA", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : int32(-573785174)}	
+	regTest = mbTest{Name: "Uint16BA", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: uint16(52651)}
 	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Int32BADC", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : int32(-1146430004)}	
+	regTest = mbTest{Name: "Int16AB", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: int16(-21555)}
 	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Int32CDAB", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : int32(-857888069)}	
-	regTests = append(regTests, regTest)
-
-	regTest = mbTest {Name : "Uint32ABCD", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : uint32(2864434397)}	
-	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Uint32DCBA", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : uint32(3721182122)}	
-	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Uint32BADC", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : uint32(3148537292)}	
-	regTests = append(regTests, regTest)
-	regTest = mbTest {Name : "Uint32CDAB", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : uint32(3437079227)}	
+	regTest = mbTest{Name: "Int16BA", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: int16(-12885)}
 	regTests = append(regTests, regTest)
 
-	regTest = mbTest {Name : "FLOAT32-IEEE", Address : 10, Quantity : 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue : float32(-3.3360025e-13)}	
+	regTest = mbTest{Name: "Int32ABCD", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-1430532899)}
+	regTests = append(regTests, regTest)
+	regTest = mbTest{Name: "Int32DCBA", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-573785174)}
+	regTests = append(regTests, regTest)
+	regTest = mbTest{Name: "Int32BADC", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-1146430004)}
+	regTests = append(regTests, regTest)
+	regTest = mbTest{Name: "Int32CDAB", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-857888069)}
 	regTests = append(regTests, regTest)
 
-	for _, regTest := range regTests {										
-		_, err = client.WriteMultipleRegisters(regTest.Address, regTest.Quantity, regTest.WriteValue)		
-		assert.NoError(t, err)	
+	regTest = mbTest{Name: "Uint32ABCD", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(2864434397)}
+	regTests = append(regTests, regTest)
+	regTest = mbTest{Name: "Uint32DCBA", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(3721182122)}
+	regTests = append(regTests, regTest)
+	regTest = mbTest{Name: "Uint32BADC", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(3148537292)}
+	regTests = append(regTests, regTest)
+	regTest = mbTest{Name: "Uint32CDAB", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(3437079227)}
+	regTests = append(regTests, regTest)
+
+	regTest = mbTest{Name: "FLOAT32-IEEE", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: float32(-3.3360025e-13)}
+	regTests = append(regTests, regTest)
+
+	for _, regTest := range regTests {
+		_, err = client.WriteMultipleRegisters(regTest.Address, regTest.Quantity, regTest.WriteValue)
+		assert.NoError(t, err)
 
 		var acc testutil.Accumulator
 		modbus.Gather(&acc)
 
-		for _, tags := range modbus.Registers.HoldingRegisters.Tags {			
-			if (tags.Name == regTest.Name) {				
+		for _, tags := range modbus.Registers.HoldingRegisters.Tags {
+			if tags.Name == regTest.Name {
 				assert.Equal(t, regTest.ExpectValue, tags.Value)
 			}
-		}					
+		}
 	}
 }
