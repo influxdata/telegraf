a9d6ac928c5fe8605869ce7197487410442651fa
greg linton <greg@influxdata.com>
2019-08-07 15:44:55 -0600

    Address feedback

--- plugins/inputs/apcupsd/apcupsd.go
+++ plugins/inputs/apcupsd/apcupsd.go
@@ -20,22 +20,26 @@ type ApcUpsd struct {
 	Timeout internal.Duration
 }
 
-func (_ *ApcUpsd) Description() string {
+func (*ApcUpsd) Description() string {
 	return "Monitor APC UPSes connected to apcupsd"
 }
 
 var sampleConfig = `
-  # a list of running apcupsd server to connect to. 
+  # A list of running apcupsd server to connect to.
   # If not provided will default to tcp://127.0.0.1:3551
   servers = ["tcp://127.0.0.1:3551"]
+
+  ## Timeout for dialing server.
   timeout = "5s"
 `
 
-func (_ *ApcUpsd) SampleConfig() string {
+func (*ApcUpsd) SampleConfig() string {
 	return sampleConfig
 }
 
 func (h *ApcUpsd) Gather(acc telegraf.Accumulator) error {
+	ctx := context.Background()
+
 	for _, addr := range h.Servers {
 		addrBits, err := url.Parse(addr)
 		if err != nil {
@@ -45,7 +49,10 @@ func (h *ApcUpsd) Gather(acc telegraf.Accumulator) error {
 			addrBits.Scheme = "tcp"
 		}
 
-		status, err := fetchStatus(addrBits, h.Timeout.Duration)
+		ctx, cancel := context.WithTimeout(ctx, h.Timeout.Duration)
+		defer cancel()
+
+		status, err := fetchStatus(ctx, addrBits)
 		if err != nil {
 			return err
 		}
@@ -53,20 +60,20 @@ func (h *ApcUpsd) Gather(acc telegraf.Accumulator) error {
 		tags := map[string]string{
 			"serial":   status.SerialNumber,
 			"ups_name": status.UPSName,
-			"online":   status.Status,
+			"status":   status.Status,
 		}
 
 		fields := map[string]interface{}{
-			"online":                 boolToInt(status.Status == "ONLINE"),
+			"status_online":          boolToInt(status.Status == "ONLINE"), // todo: revisit
 			"input_voltage":          status.LineVoltage,
 			"load_percent":           status.LoadPercent,
 			"battery_charge_percent": status.BatteryChargePercent,
-			"time_left_minutes":      status.TimeLeft.Minutes(),
+			"time_left_ns":           status.TimeLeft.Nanoseconds(),
 			"output_voltage":         status.OutputVoltage,
 			"internal_temp":          status.InternalTemp,
 			"battery_voltage":        status.BatteryVoltage,
 			"input_frequency":        status.LineFrequency,
-			"time_on_battery":        status.TimeOnBattery.Minutes(),
+			"time_on_battery_ns":     status.TimeOnBattery.Nanoseconds(),
 		}
 
 		acc.AddFields("apcupsd", fields, tags)
@@ -81,9 +88,7 @@ func boolToInt(b bool) int {
 	return 0
 }
 
-func fetchStatus(addr *url.URL, timeout time.Duration) (*apcupsd.Status, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
+func fetchStatus(ctx context.Context, addr *url.URL) (*apcupsd.Status, error) {
 	client, err := apcupsd.DialContext(ctx, addr.Scheme, addr.Host)
 	if err != nil {
 		return nil, err
--- plugins/inputs/apcupsd/apcupsd_test.go
+++ plugins/inputs/apcupsd/apcupsd_test.go
@@ -1,98 +1,117 @@
 package apcupsd
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
-	"sync"
 	"testing"
 
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/influxdata/telegraf/testutil"
+	"github.com/stretchr/testify/require"
 )
 
-func TestApcupsdGather(t *testing.T) {
+func TestApcupsdDocs(t *testing.T) {
+	apc := &ApcUpsd{}
+	apc.Description()
+	apc.SampleConfig()
+}
+
+func TestApcupsdInit(t *testing.T) {
 	input, ok := inputs.Inputs["apcupsd"]
 	if !ok {
 		t.Fatal("Input not defined")
 	}
-	apc := input().(*ApcUpsd)
 
-	apc.Description()
-	apc.SampleConfig()
+	_ = input().(*ApcUpsd)
+}
 
+func TestBoolToInt(t *testing.T) {
 	one := boolToInt(true)
 	if one != 1 {
 		t.Errorf("boolToInt failed")
 	}
+}
 
-	if testing.Short() {
-		t.Skip("Skipping network dependent tests in short mode")
-	}
-
-	ln, err := net.Listen("tcp", ":0")
+func listen(t *testing.T) (string, error) {
+	ln, err := net.Listen("tcp4", "127.0.0.1:0")
 	if err != nil {
-		t.Fatal(err)
+		return "", err
 	}
-	defer ln.Close()
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
 
 	go func() {
-		defer wg.Done()
-		conn, err := ln.Accept()
-		if err != nil {
-			return
-		}
+		for {
+			defer ln.Close()
 
-		in := make([]byte, 128)
-		n, err := conn.Read(in)
-		if err != nil {
-			t.Fatal(fmt.Sprintf("failed to read from connection - %s", err.Error()))
-		}
+			conn, err := ln.Accept()
+			if err != nil {
+				continue
+			}
+			defer conn.Close()
 
-		status := []byte{0, 6, 's', 't', 'a', 't', 'u', 's'}
-		if want, got := status, in[:n]; !bytes.Equal(want, got) {
-			t.Fatal(fmt.Sprintf("unexpected request from Client:\n- want: %v\n - got: %v",
-				want, got))
-		}
+			in := make([]byte, 128)
+			n, err := conn.Read(in)
+			require.NoError(t, err, "failed to read from connection")
 
-		// Run against test function and append EOF to end of output bytes
-		out := genOutput()
-		out = append(out, []byte{0, 0})
+			status := []byte{0, 6, 's', 't', 'a', 't', 'u', 's'}
+			want, got := status, in[:n]
+			require.Equal(t, want, got)
 
-		for _, o := range out {
-			if _, err := conn.Write(o); err != nil {
-				t.Fatal(fmt.Sprintf("failed to write to connection - %s", err.Error()))
+			// Run against test function and append EOF to end of output bytes
+			out := genOutput()
+			out = append(out, []byte{0, 0})
+
+			for _, o := range out {
+				_, err := conn.Write(o)
+				require.NoError(t, err, "failed to write to connection")
 			}
 		}
 	}()
 
-	var acc testutil.Accumulator
+	return ln.Addr().String(), nil
+}
 
-	apc.Servers = []string{ln.Addr().String()}
+func TestApcupsdGather(t *testing.T) {
+	apc := &ApcUpsd{Timeout: defaultTimeout}
 
-	err = apc.Gather(&acc)
-	if err == nil {
-		t.Fatal("Should have failed but didn't")
-	}
-
-	apc.Servers = []string{"127.0.0.3"}
-
-	err = apc.Gather(&acc)
-	if err == nil {
-		t.Fatal("Should have failed but didn't")
-	}
-
-	apc.Servers = []string{"tcp://" + ln.Addr().String()}
-
-	err = apc.Gather(&acc)
+	lAddr, err := listen(t)
 	if err != nil {
-		t.Fatal(fmt.Sprintf("Failed gathering when shouldn't - %s", err.Error()))
+		t.Fatal(err)
 	}
-	wg.Wait()
+
+	var (
+		tests = []struct {
+			servers []string
+			err     bool
+		}{
+			{
+				servers: []string{lAddr},
+				err:     true,
+			},
+			{
+				servers: []string{"127.0.0.3"},
+				err:     true,
+			},
+			{
+				servers: []string{"tcp://" + lAddr},
+				err:     false,
+			},
+		}
+
+		acc testutil.Accumulator
+	)
+
+	for _, test := range tests {
+		fmt.Println("running test", test.servers)
+		apc.Servers = test.servers
+
+		err = apc.Gather(&acc)
+		if test.err {
+			require.Error(t, err)
+		} else {
+			require.NoError(t, err)
+		}
+	}
 }
 
 // The following functionality is straight from apcupsd tests.
