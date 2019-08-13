bc4e9ece8ec3d0fd58869ed6617ad109b0117eef
Sascha Steinbiss <satta@debian.org>
2019-08-13 13:44:46 +0200

    tighten test coverage

--- plugins/inputs/suricata/suricata_test.go
+++ plugins/inputs/suricata/suricata_test.go
@@ -20,6 +20,15 @@ import (
 var ex2 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"capture":{"kernel_packets":905344474,"kernel_drops":78355440,"kernel_packets_delta":2376742,"kernel_drops_delta":82049}}}`
 var ex3 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": { "foo": { "capture":{"kernel_packets":905344474,"kernel_drops":78355440}}}}}`
 var ex4 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": { "W1#en..bar1": { "capture":{"kernel_packets":905344474,"kernel_drops":78355440}}}}}`
+var brokenType1 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": { "W1#en..bar1": { "capture":{"kernel_packets":905344474,"kernel_drops": true}}}}}`
+var brokenType2 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": { "W1#en..bar1": { "capture":{"kernel_packets":905344474,"kernel_drops": ["foo"]}}}}}`
+var brokenType3 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": { "W1#en..bar1": { "capture":{"kernel_packets":905344474,"kernel_drops":"none this time"}}}}}`
+var brokenType4 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": { "W1#en..bar1": { "capture":{"kernel_packets":905344474,"kernel_drops":null}}}}}`
+var brokenType5 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"foo": null}}`
+var brokenStruct1 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats":{"threads": ["foo"]}}`
+var brokenStruct2 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats"}`
+var brokenStruct3 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats": "foobar"}`
+var brokenStruct4 = `{"timestamp":"2017-03-06T07:43:39.000397+0000","event_type":"stats","stats": null}`
 var singleDotRegexp = regexp.MustCompilePOSIX(`[^.]\.[^.]`)
 
 func TestSuricataLarge(t *testing.T) {
@@ -273,6 +282,48 @@ func TestSuricataEmptyJSON(t *testing.T) {
 	acc.WaitError(1)
 
 	s.Stop()
+}
+
+func TestSuricataInvalidInputs(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	tmpfn := filepath.Join(dir, fmt.Sprintf("t%d", rand.Int63()))
+
+	s := Suricata{
+		Source:    tmpfn,
+		Delimiter: ".",
+	}
+
+	for input, errmsg := range map[string]string{
+		brokenType1:   `unsupported type bool encountered`,
+		brokenType2:   `unsupported type []interface {} encountered`,
+		brokenType3:   `unsupported type string encountered`,
+		brokenType4:   `unsupported type <nil> encountered`,
+		brokenType5:   `unsupported type <nil> encountered`,
+		brokenStruct1: `'threads' sub-object does not have required structure`,
+		brokenStruct2: `input does not contain necessary 'stats' sub-object`,
+		brokenStruct3: `'stats' sub-object does not have required structure`,
+		brokenStruct4: `'stats' sub-object does not have required structure`,
+	} {
+		acc := testutil.Accumulator{}
+		acc.SetDebug(true)
+		assert.NoError(t, s.Start(&acc))
+
+		c, err := net.Dial("unix", tmpfn)
+		if err != nil {
+			log.Println(err)
+		}
+		c.Write([]byte(input))
+		c.Write([]byte("\n"))
+		c.Close()
+
+		acc.WaitError(1)
+		assert.Contains(t, acc.FirstError().Error(), errmsg)
+		s.Stop()
+	}
 }
 
 func TestSuricataDisconnectSocket(t *testing.T) {
