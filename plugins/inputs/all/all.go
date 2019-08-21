8bdfa59e776336b737930da64dcaf498e938d1bc
Dmitry Ilyin <idv1985@gmail.com>
2019-08-22 02:34:28 +0300

    Add logstash input plugin (#4910)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -78,6 +78,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/leofs"
 	_ "github.com/influxdata/telegraf/plugins/inputs/linux_sysctl_fs"
 	_ "github.com/influxdata/telegraf/plugins/inputs/logparser"
+	_ "github.com/influxdata/telegraf/plugins/inputs/logstash"
 	_ "github.com/influxdata/telegraf/plugins/inputs/lustre2"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mailchimp"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mcrouter"
