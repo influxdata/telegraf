462bd3479787a610682f52d92a537e72dd44bd29
Mattias Jiderhamn <mattias@jiderhamn.se>
2019-08-30 01:29:25 +0200

    Add azure_storage_queue input plugin (#5323)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/apache"
 	_ "github.com/influxdata/telegraf/plugins/inputs/apcupsd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/aurora"
+	_ "github.com/influxdata/telegraf/plugins/inputs/azure_storage_queue"
 	_ "github.com/influxdata/telegraf/plugins/inputs/bcache"
 	_ "github.com/influxdata/telegraf/plugins/inputs/beanstalkd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/bind"
