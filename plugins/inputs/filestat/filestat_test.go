7812e694e873e8d01384a1ab5eda5301de81da59
vlastahajek <vlastimil.hajek@bonitoo.io>
2019-09-05 15:23:17 +0200

    Fix #3747: Fixing filestat tests on Windows by counting also CR char and comparing different MD5 sum

--- plugins/inputs/filestat/filestat_test.go
+++ plugins/inputs/filestat/filestat_test.go
@@ -1,6 +1,8 @@
 package filestat
 
 import (
+	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
@@ -128,9 +130,16 @@ func TestGatherSuperAsterisk(t *testing.T) {
 	tags3 := map[string]string{
 		"file": dir + "test.conf",
 	}
-	require.True(t, acc.HasPoint("filestat", tags3, "size_bytes", int64(104)))
+	reqSize := int64(104)
+	reqMD5Sum := "5a7e9b77fa25e7bb411dbd17cf403c1f"
+	if runtime.GOOS == "windows" {
+		//5 lines, add 5 x '\r'
+		reqSize += 5
+		reqMD5Sum = "1d4d1cd31d9d6721c0fc2c0abb9ea996"
+	}
+	require.True(t, acc.HasPoint("filestat", tags3, "size_bytes", reqSize))
 	require.True(t, acc.HasPoint("filestat", tags3, "exists", int64(1)))
-	require.True(t, acc.HasPoint("filestat", tags3, "md5_sum", "5a7e9b77fa25e7bb411dbd17cf403c1f"))
+	require.True(t, acc.HasPoint("filestat", tags3, "md5_sum", reqMD5Sum))
 }
 
 func TestModificationTime(t *testing.T) {
@@ -171,7 +180,12 @@ func TestGetMd5(t *testing.T) {
 	dir := getTestdataDir()
 	md5, err := getMd5(dir + "test.conf")
 	assert.NoError(t, err)
-	assert.Equal(t, "5a7e9b77fa25e7bb411dbd17cf403c1f", md5)
+
+	reqMD5Sum := "5a7e9b77fa25e7bb411dbd17cf403c1f"
+	if runtime.GOOS == "windows" {
+		reqMD5Sum = "1d4d1cd31d9d6721c0fc2c0abb9ea996"
+	}
+	assert.Equal(t, reqMD5Sum, md5)
 
 	md5, err = getMd5("/tmp/foo/bar/fooooo")
 	assert.Error(t, err)
@@ -179,5 +193,8 @@ func TestGetMd5(t *testing.T) {
 
 func getTestdataDir() string {
 	_, filename, _, _ := runtime.Caller(1)
-	return strings.Replace(filename, "filestat_test.go", "testdata/", 1)
+	if runtime.GOOS == "windows" {
+		filename = filepath.FromSlash(filename)
+	}
+	return strings.Replace(filename, "filestat_test.go", "testdata"+string(os.PathSeparator), 1)
 }
