171d0229ea5b4f41677ea722dedf7572f222d646
olais <olais@gmail.com>
2019-09-15 20:40:33 -0500

    implement table driven test

--- plugins/inputs/modbus/modbus_test.go
+++ plugins/inputs/modbus/modbus_test.go
@@ -10,15 +10,72 @@ import (
 	"github.com/influxdata/telegraf/testutil"
 )
 
-type mbTest struct {
-	Name        string
-	Address     uint16
-	Quantity    uint16
-	WriteValue  []byte
-	ExpectValue interface{}
-}
+func TestCoils(t *testing.T) {	
+	var coilTests = []struct {
+		name string		
+		address uint16
+		quantity uint16
+		write  []byte
+		read uint16
+	}{
+		{			
+			name : "Coil0-TurnOff",			
+			address : 0,
+			quantity : 1,
+			write : []byte{0x00}, 
+			read: 0,
+		},
+		{			
+			name : "Coil0-TurnOn",			
+			address : 0,
+			quantity : 1,
+			write : []byte{0x01}, 
+			read: 1,			
+		},
+		{			
+			name : "Coil1-TurnOn",			
+			address : 1,
+			quantity : 1,
+			write : []byte{0x01}, 
+			read: 1,			
+		},
+		{			
+			name : "Coil2-TurnOn",			
+			address : 2,
+			quantity : 1,
+			write : []byte{0x01}, 
+			read: 1,			
+		},
+		{			
+			name : "Coil3-TurnOn",			
+			address : 3,
+			quantity : 1,
+			write : []byte{0x01}, 
+			read: 1,			
+		},
+		{			
+			name : "Coil1-TurnOff",			
+			address : 1,
+			quantity : 1,
+			write : []byte{0x00}, 
+			read: 0,			
+		},
+		{			
+			name : "Coil2-TurnOff",			
+			address : 2,
+			quantity : 1,
+			write : []byte{0x00}, 
+			read: 0,			
+		},
+		{			
+			name : "Coil3-TurnOff",			
+			address : 3,
+			quantity : 1,
+			write : []byte{0x00}, 
+			read: 0,			
+		},
+	}
 
-func TestCoils(t *testing.T) {
 	serv := mbserver.NewServer()
 	err := serv.ListenTCP("localhost:1502")
 	defer serv.Close()
@@ -30,232 +87,246 @@ func TestCoils(t *testing.T) {
 	defer handler.Close()
 	client := m.NewClient(handler)
 
-	modbus := Modbus{
-		Type:       "TCP",
-		Controller: "localhost",
-		Port:       1502,
-		SlaveId:    1,
-		//Timeout:    0,
-		Registers: registers{
-			Coils: register{
-				Tags: []tag{
-					{Name: "Coil0", Address: []int{0}},
-					{Name: "Coil1", Address: []int{1}},
+	for _, ct := range coilTests {
+		t.Run(ct.name,func(t *testing.T) {
+			_, err = client.WriteMultipleCoils(ct.address, ct.quantity, ct.write)
+			assert.NoError(t, err)
+
+			modbus := Modbus {				
+				Controller: "tcp://localhost:1502",				
+				Slave_Id:    1,		
+				Coils: []tag {
+					{						 
+						Address: []uint16{ct.address},
+					},					
 				},
-			},
-		},
-	}
-
-	coilTests := []mbTest{}
-	coilTest := mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x03}, ExpectValue: []byte{0x03}}
-	coilTests = append(coilTests, coilTest)
-	coilTest = mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x02}, ExpectValue: []byte{0x02}}
-	coilTests = append(coilTests, coilTest)
-	coilTest = mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x01}, ExpectValue: []byte{0x01}}
-	coilTests = append(coilTests, coilTest)
-	coilTest = mbTest{Address: 0, Quantity: 2, WriteValue: []byte{0x00}, ExpectValue: []byte{0x00}}
-	coilTests = append(coilTests, coilTest)
-
-	for _, coilTest := range coilTests {
-		_, err = client.WriteMultipleCoils(coilTest.Address, coilTest.Quantity, coilTest.WriteValue)
-		assert.NoError(t, err)
-
-		var acc testutil.Accumulator
-		modbus.Gather(&acc)
-
-		res := []byte{0x00}
-		for i, t := range modbus.Registers.Coils.Tags {
-			v, _ := t.value.(uint16)
-			res[0] = res[0] | byte(v<<uint(i))
-		}
-
-		assert.Equal(t, coilTest.ExpectValue, res)
-	}
-}
-
-func TestRegisters(t *testing.T) {
-	serv := mbserver.NewServer()
-	err := serv.ListenTCP("localhost:1502")
-	defer serv.Close()
-	assert.NoError(t, err)
-
-	handler := m.NewTCPClientHandler("localhost:1502")
-	err = handler.Connect()
-	assert.NoError(t, err)
-	defer handler.Close()
-	client := m.NewClient(handler)
-
-	modbus := Modbus{
-		Type:       "TCP",
-		Controller: "localhost",
-		Port:       1502,
-		SlaveId:    1,
-		//Timeout:    0,
-		Registers: registers{
-			HoldingRegisters: register{
-				Tags: []tag{
-					{
-						Name:     "Register0",
-						Order:    "AB",
-						DataType: "FLOAT32",
-						Address:  []int{0},
-						Scale:    "/10",
-					},
-					{
-						Name:     "Register1-2",
-						Order:    "ABCD",
-						DataType: "FLOAT32",
-						Address:  []int{1, 2},
-						Scale:    "/1000",
-					},
-					{
-						Name:     "Register3-4",
-						Order:    "ABCD",
-						DataType: "FLOAT32",
-						Address:  []int{3, 4},
-						Scale:    "/10",
-					},
-					{
-						Name:     "Register7",
-						Order:    "AB",
-						DataType: "FLOAT32",
-						Address:  []int{7},
-						Scale:    "/10",
-					},
-					{
-						Name:     "Uint16AB",
-						Order:    "AB",
-						DataType: "UINT16",
-						Address:  []int{10},
-					},
-					{
-						Name:     "Uint16BA",
-						Order:    "BA",
-						DataType: "UINT16",
-						Address:  []int{10},
-					},
-					{
-						Name:     "Int16AB",
-						Order:    "AB",
-						DataType: "INT16",
-						Address:  []int{10},
-					},
-					{
-						Name:     "Int16BA",
-						Order:    "BA",
-						DataType: "INT16",
-						Address:  []int{10},
-					},
-					{
-						Name:     "Int32ABCD",
-						Order:    "ABCD",
-						DataType: "INT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Int32DCBA",
-						Order:    "DCBA",
-						DataType: "INT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Int32BADC",
-						Order:    "BADC",
-						DataType: "INT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Int32CDAB",
-						Order:    "CDAB",
-						DataType: "INT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Uint32ABCD",
-						Order:    "ABCD",
-						DataType: "UINT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Uint32DCBA",
-						Order:    "DCBA",
-						DataType: "UINT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Uint32BADC",
-						Order:    "BADC",
-						DataType: "UINT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "Uint32CDAB",
-						Order:    "CDAB",
-						DataType: "UINT32",
-						Address:  []int{10, 11},
-					},
-					{
-						Name:     "FLOAT32-IEEE",
-						Order:    "ABCD",
-						DataType: "FLOAT32-IEEE",
-						Address:  []int{10, 11},
-					},
-				},
-			},
-		},
-	}
-
-	regTests := []mbTest{}
-	regTest := mbTest{Name: "Register0", Address: 0, Quantity: 1, WriteValue: []byte{0x08, 0x98}, ExpectValue: float32(220)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Register1-2", Address: 1, Quantity: 2, WriteValue: []byte{0x00, 0x00, 0x03, 0xE8}, ExpectValue: float32(1)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Register3-4", Address: 3, Quantity: 2, WriteValue: []byte{0x00, 0x00, 0x08, 0x98}, ExpectValue: float32(220)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Register7", Address: 7, Quantity: 1, WriteValue: []byte{0x01, 0xF4}, ExpectValue: float32(50)}
-	regTests = append(regTests, regTest)
-
-	regTest = mbTest{Name: "Uint16AB", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: uint16(43981)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Uint16BA", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: uint16(52651)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Int16AB", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: int16(-21555)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Int16BA", Address: 10, Quantity: 1, WriteValue: []byte{0xAB, 0xCD}, ExpectValue: int16(-12885)}
-	regTests = append(regTests, regTest)
-
-	regTest = mbTest{Name: "Int32ABCD", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-1430532899)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Int32DCBA", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-573785174)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Int32BADC", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-1146430004)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Int32CDAB", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: int32(-857888069)}
-	regTests = append(regTests, regTest)
-
-	regTest = mbTest{Name: "Uint32ABCD", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(2864434397)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Uint32DCBA", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(3721182122)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Uint32BADC", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(3148537292)}
-	regTests = append(regTests, regTest)
-	regTest = mbTest{Name: "Uint32CDAB", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: uint32(3437079227)}
-	regTests = append(regTests, regTest)
-
-	regTest = mbTest{Name: "FLOAT32-IEEE", Address: 10, Quantity: 2, WriteValue: []byte{0xAA, 0xBB, 0xCC, 0xDD}, ExpectValue: float32(-3.3360025e-13)}
-	regTests = append(regTests, regTest)
-
-	for _, regTest := range regTests {
-		_, err = client.WriteMultipleRegisters(regTest.Address, regTest.Quantity, regTest.WriteValue)
-		assert.NoError(t, err)
-
-		var acc testutil.Accumulator
-		modbus.Gather(&acc)
-
-		for _, tags := range modbus.Registers.HoldingRegisters.Tags {
-			if tags.Name == regTest.Name {
-				assert.Equal(t, regTest.ExpectValue, tags.value)
 			}
-		}
+
+			var acc testutil.Accumulator
+			modbus.Gather(&acc)
+
+			for _, coil := range modbus.registers {
+				assert.Equal(t, ct.read, coil.Tags[0].value)						
+			}			
+		})
+	}		
+}
+
+func TestHoldingRegisters(t *testing.T) {	
+	var coilTests = []struct {
+		name string		
+		address []uint16
+		quantity uint16
+		byte_order string
+		data_type string
+		scale string
+		write  []byte
+		read interface{}
+	}{
+		{			
+			name : "Register0-AB-FLOAT32",			
+			address : []uint16{0},
+			quantity : 1,
+			byte_order : "AB",
+			data_type : "FLOAT32",
+			scale :"0.1",
+			write : []byte{0x08, 0x98}, 
+			read : float32(220),
+		},
+		{			
+			name : "Register0-Register1-AB-FLOAT32",			
+			address : []uint16{0, 1},
+			quantity : 2,
+			byte_order : "ABCD",
+			data_type : "FLOAT32",
+			scale :"0.001",
+			write : []byte{0x00, 0x00, 0x03, 0xE8}, 
+			read : float32(1),
+		},
+		{			
+			name : "Register1-Register2-ABCD-FLOAT32",			
+			address : []uint16{1, 2},
+			quantity : 2,
+			byte_order : "ABCD",
+			data_type : "FLOAT32",
+			scale :"0.1",
+			write : []byte{0x00, 0x00, 0x08, 0x98}, 
+			read : float32(220),
+		},
+		{			
+			name : "Register3-Register4-ABCD-FLOAT32",			
+			address : []uint16{3, 4},
+			quantity : 2,
+			byte_order : "ABCD",
+			data_type : "FLOAT32",
+			scale :"0.1",
+			write : []byte{0x00, 0x00, 0x08, 0x98}, 
+			read : float32(220),
+		},
+		{			
+			name : "Register7-AB-FLOAT32",			
+			address : []uint16{7},
+			quantity : 1,
+			byte_order : "AB",
+			data_type : "FLOAT32",
+			scale :"0.1",
+			write : []byte{0x01, 0xF4}, 
+			read : float32(50),
+		},
+		{			
+			name : "Register10-AB-UINT16",			
+			address : []uint16{10},
+			quantity : 1,
+			byte_order : "AB",
+			data_type : "UINT16",			
+			write : []byte{0xAB, 0xCD}, 
+			read : uint16(43981),
+		},
+		{			
+			name : "Register20-BA-UINT16",			
+			address : []uint16{20},
+			quantity : 1,
+			byte_order : "BA",
+			data_type : "UINT16",			
+			write : []byte{0xAB, 0xCD}, 
+			read : uint16(52651),
+		},
+		{			
+			name : "Register30-AB-INT16",			
+			address : []uint16{20},
+			quantity : 1,
+			byte_order : "AB",
+			data_type : "INT16",			
+			write : []byte{0xAB, 0xCD}, 
+			read : int16(-21555),
+		},
+		{			
+			name : "Register40-BA-INT16",			
+			address : []uint16{40},
+			quantity : 1,
+			byte_order : "BA",
+			data_type : "INT16",			
+			write : []byte{0xAB, 0xCD}, 
+			read : int16(-12885),
+		},
+		{			
+			name : "Register50-Register51-ABCD-INT32",			
+			address : []uint16{50, 51},
+			quantity : 2,
+			byte_order : "ABCD",
+			data_type : "INT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : int32(-1430532899),
+		},
+		{			
+			name : "Register60-Register61-DCBA-INT32",			
+			address : []uint16{60, 61},
+			quantity : 2,
+			byte_order : "DCBA",
+			data_type : "INT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : int32(-573785174),
+		},
+		{			
+			name : "Register70-Register71-BADC-INT32",			
+			address : []uint16{70, 71},
+			quantity : 2,
+			byte_order : "BADC",
+			data_type : "INT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : int32(-1146430004),
+		},
+		{			
+			name : "Register80-Register81-CDAB-INT32",			
+			address : []uint16{80, 81},
+			quantity : 2,
+			byte_order : "CDAB",
+			data_type : "INT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : int32(-857888069),
+		},
+		{			
+			name : "Register90-Register91-ABCD-UINT32",			
+			address : []uint16{90, 91},
+			quantity : 2,
+			byte_order : "ABCD",
+			data_type : "UINT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : uint32(2864434397),
+		},
+		{			
+			name : "Register100-Register101-DCBA-UINT32",			
+			address : []uint16{100, 101},
+			quantity : 2,
+			byte_order : "DCBA",
+			data_type : "UINT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : uint32(3721182122),
+		},
+		{			
+			name : "Register110-Register111-BADC-UINT32",			
+			address : []uint16{110, 111},
+			quantity : 2,
+			byte_order : "BADC",
+			data_type : "UINT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : uint32(3148537292),
+		},
+		{			
+			name : "Register120-Register121-CDAB-UINT32",			
+			address : []uint16{120, 121},
+			quantity : 2,
+			byte_order : "CDAB",
+			data_type : "UINT32",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : uint32(3437079227),
+		},
+		{			
+			name : "Register130-Register131-ABCD-FLOAT32-IEEE",			
+			address : []uint16{130, 131},
+			quantity : 2,
+			byte_order : "ABCD",
+			data_type : "FLOAT32-IEEE",			
+			write : []byte{0xAA, 0xBB, 0xCC, 0xDD}, 
+			read : float32(-3.3360025e-13),
+		},
+	}
+
+	serv := mbserver.NewServer()
+	err := serv.ListenTCP("localhost:1502")
+	defer serv.Close()
+	assert.NoError(t, err)
+
+	handler := m.NewTCPClientHandler("localhost:1502")
+	err = handler.Connect()
+	assert.NoError(t, err)
+	defer handler.Close()
+	client := m.NewClient(handler)
+
+	for _, ct := range coilTests {
+		t.Run(ct.name,func(t *testing.T) {
+			_, err = client.WriteMultipleRegisters(ct.address[0], ct.quantity, ct.write)
+			assert.NoError(t, err)
+
+			modbus := Modbus {				
+				Controller: "tcp://localhost:1502",				
+				Slave_Id:    1,		
+				Holding_Registers: []tag {
+					{			
+						Byte_Order : ct.byte_order,	
+						Data_Type : ct.data_type,
+						Scale : ct.scale,		 
+						Address: ct.address,
+					},					
+				},
+			}
+
+			var acc testutil.Accumulator
+			modbus.Gather(&acc)
+
+			for _, coil := range modbus.registers {
+				assert.Equal(t, ct.read, coil.Tags[0].value)						
+			}			
+		})
 	}
 }
