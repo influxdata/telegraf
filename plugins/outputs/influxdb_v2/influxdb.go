11ae54402006963f1fd899ff1e8aced331b9b5d5
Daniel Nelson <daniel.nelson@influxdb.com>
2019-09-23 10:02:19 -0700

    Add example URL for cloud2 to influxdb_v2 output

--- plugins/outputs/influxdb_v2/influxdb.go
+++ plugins/outputs/influxdb_v2/influxdb.go
@@ -27,6 +27,7 @@ var sampleConfig = `
   ##
   ## Multiple URLs can be specified for a single cluster, only ONE of the
   ## urls will be written to each interval.
+  ##   ex: urls = ["https://us-west-2-1.aws.cloud2.influxdata.com"]
   urls = ["http://127.0.0.1:9999"]
 
   ## Token for authentication.
