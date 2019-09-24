ff44680bbe9bb112e608962cd68c4ace5719aeb5
Ryan Ascheman <rascheman@groupon.com>
2019-09-23 17:17:46 -0700

    add plugin to all.go

--- plugins/aggregators/all/all.go
+++ plugins/aggregators/all/all.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/aggregators/histogram"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/minmax"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/valuecounter"
+	_ "github.com/influxdata/telegraf/plugins/aggregators/tdigestagg"
 )
