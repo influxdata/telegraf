fc6fb330678af46a778b73b9f018fda8872ce280
Daniel Nelson <daniel.nelson@influxdb.com>
2019-09-26 17:09:44 -0700

    Add merge aggregator (#6410)

--- plugins/aggregators/all/all.go
+++ plugins/aggregators/all/all.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/aggregators/basicstats"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/final"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/histogram"
+	_ "github.com/influxdata/telegraf/plugins/aggregators/merge"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/minmax"
 	_ "github.com/influxdata/telegraf/plugins/aggregators/valuecounter"
 )
