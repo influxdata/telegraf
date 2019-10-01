265893d944fa1a8ff36543dfa44c7d77d5856a17
Ankit Goel <ankit.goel@go-jek.com>
2019-10-01 10:48:16 +0530

    Adds test for histogram support in remote write

--- plugins/outputs/prometheus_remote_write/prometheus_remote_write_test.go
+++ plugins/outputs/prometheus_remote_write/prometheus_remote_write_test.go
@@ -194,3 +194,57 @@ func TestWrite_WithStringFieldWithoutAnyNumericField(t *testing.T) {
 		})
 	}
 }
+
+func TestWriteWithHistogram(t *testing.T) {
+	for i, tc := range []struct {
+		metrics  []telegraf.Metric
+		expected prompb.WriteRequest
+	}{
+		{
+			metrics:  []telegraf.Metric{},
+			expected: prompb.WriteRequest{},
+		},
+
+		{
+			metrics: []telegraf.Metric{
+				mustNew(t, "foo", map[string]string{"bar": "baz"},
+					map[string]interface{}{"99": 1.0}, time.Unix(0, 0), telegraf.Histogram),
+			},
+			expected: prompb.WriteRequest{
+				Timeseries: []prompb.TimeSeries{{
+					Labels: []prompb.Label{
+						{Name: "__name__", Value: "foo"},
+						{Name: "bar", Value: "baz"},
+						{Name: "le", Value: "99"},
+					},
+					Samples: []prompb.Sample{
+						{Timestamp: 0, Value: 1.0},
+					},
+				}},
+			},
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			var actual prompb.WriteRequest
+
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				buf, err := ioutil.ReadAll(r.Body)
+				require.NoError(t, err)
+
+				buf, err = snappy.Decode(nil, buf)
+				require.NoError(t, err)
+
+				err = proto.Unmarshal(buf, &actual)
+				require.NoError(t, err)
+			}))
+			defer server.Close()
+
+			remote := PrometheusRemoteWrite{
+				URL: server.URL,
+			}
+			err := remote.Write(tc.metrics)
+			require.NoError(t, err)
+			assert.Equal(t, actual, tc.expected)
+		})
+	}
+}
