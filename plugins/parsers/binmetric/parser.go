746e956a17f383d91d8a95582d06766c789fb111
yo <yuriy.olkhovyy@gmail.com>
2019-10-17 20:44:13 +0200

    Re-factored and tests

--- plugins/parsers/binmetric/parser.go
+++ plugins/parsers/binmetric/parser.go
@@ -12,7 +12,6 @@ import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/metric"
-	"github.com/influxdata/toml"
 )
 
 // Field is ...
@@ -27,40 +26,76 @@ type Field struct {
 type BinMetric struct {
 	MetricName string
 	// TagKeys    []string
-	TimeFormat string
-	Timezone   string
-	Endiannes  string
-	Fields     []Field
-}
-
-// Config is ...
-type Config struct {
-	BinMetric BinMetric
+	TimeFormat  string
+	Endiannes   string
+	Fields      []Field
+	DefaultTags map[string]string
 }
 
 const timeKey = "time"
 const timezone = "UTC"
 
-var data string = `
-[binmetric]
-	metric_name = "drone_status"
-	endiannes = "be"
-	time_format = "unix"
-	fields = [
-		{name="version",type="uint16",offset=0,size=2},
-		{name="time",type="int32",offset=2,size=4},
-		{name="location_latitude",type="float64",offset=6,size=8},
-		{name="location_longitude",type="float64",offset=14,size=8},
-		{name="location_altitude",type="float32",offset=22,size=4},
-		{name="orientation_heading",type="float32",offset=26,size=4},
-		{name="orientation_elevation",type="float32",offset=30,size=4},
-		{name="orientation_bank",type="float32",offset=34,size=4},
-		{name="speed_ground",type="float32",offset=38,size=4},
-		{name="speed_air",type="float32",offset=42,size=4},
-		{name="is_healthy",type="bool",offset=46,size=1},
-		{name="state",type="string",offset=47,size=8},
-	]
-`
+// Parse is ...
+func (config *BinMetric) Parse(binMetric []byte) ([]telegraf.Metric, error) {
+
+	endiannes, err := config.getEndiannes()
+	if err != nil {
+		return nil, err
+	}
+
+	fields := make(map[string]interface{})
+	reader := io.NewSectionReader(bytes.NewReader(binMetric), 0, int64(len(binMetric)))
+
+	for _, field := range config.Fields {
+		fieldBuffer := make([]byte, field.Size)
+		_, err := reader.ReadAt(fieldBuffer, int64(field.Offset))
+		if err != nil {
+			return nil, err
+		}
+
+		fieldType := fieldTypes[field.Type]
+		if fieldType == nil {
+			return nil, fmt.Errorf(`invalid field type "%s""`, field.Type)
+		}
+
+		if fieldType.Name() == "string" {
+			fields[field.Name] = string(fieldBuffer)
+		} else {
+			fieldValue := reflect.New(fieldType)
+			byteReader := bytes.NewReader(fieldBuffer)
+			binary.Read(byteReader, endiannes, fieldValue.Interface())
+			fields[field.Name] = fieldValue.Elem().Interface()
+		}
+	}
+
+	metricTime, err := config.getTime(fields)
+	if err != nil {
+		return nil, err
+	}
+
+	metric, err := metric.New(config.MetricName, nil, fields, metricTime)
+	if err != nil {
+		return nil, err
+	}
+
+	// metricTags := make(map[string]string)
+	// for _, tagKey := range config.BinMetric.TagKeys {
+	// 	metricTags[tagKey] = ""
+	// }
+
+	return []telegraf.Metric{metric}, err
+}
+
+// ParseLine is ...
+func (config *BinMetric) ParseLine(line string) (telegraf.Metric, error) {
+	fmt.Println("BinMetric.ParseLine() not supported")
+	return nil, nil
+}
+
+// SetDefaultTags is ...
+func (config *BinMetric) SetDefaultTags(tags map[string]string) {
+	fmt.Println("BinMetric.SetDefaultTags() not supported")
+}
 
 var fieldTypes = map[string]reflect.Type{
 	"bool":    reflect.TypeOf((*bool)(nil)).Elem(),
@@ -77,72 +112,37 @@ var fieldTypes = map[string]reflect.Type{
 	"string":  reflect.TypeOf((*string)(nil)).Elem(),
 }
 
-// Parse is ...
-func (p *BinMetric) Parse(binMetric []byte) ([]telegraf.Metric, error) {
-	var config Config
-	var err = toml.Unmarshal([]byte(data), &config)
-	if err != nil {
-		return nil, err
+func (config BinMetric) getEndiannes() (binary.ByteOrder, error) {
+	var endiannes binary.ByteOrder
+	cfgEndiannes := strings.ToLower(config.Endiannes)
+	if cfgEndiannes == "" || cfgEndiannes == "be" {
+		endiannes = binary.BigEndian
+	} else if cfgEndiannes == "le" {
+		endiannes = binary.LittleEndian
+	} else {
+		return nil, fmt.Errorf("invalid binmetric_endiannes %s", cfgEndiannes)
 	}
+	return endiannes, nil
+}
 
-	endiannes := strings.ToLower(config.BinMetric.Endiannes)
-	if endiannes != "be" && endiannes != "le" {
-		return nil, fmt.Errorf(`invalid endiannes "%s""`, endiannes)
-	}
-
-	fields := make(map[string]interface{})
-	s := io.NewSectionReader(bytes.NewReader(binMetric), 0, int64(len(binMetric)))
-
-	for _, field := range config.BinMetric.Fields {
-		fieldBuffer := make([]byte, field.Size)
-		_, err := s.ReadAt(fieldBuffer, int64(field.Offset))
-		if err != nil {
-			return nil, err
-		}
-
-		fieldType := fieldTypes[field.Type]
-		if fieldType == nil {
-			return nil, fmt.Errorf(`invalid field type "%s""`, field.Type)
-		}
-
-		if fieldType.Name() == "string" {
-			fields[field.Name] = string(fieldBuffer)
-		} else {
-			fieldValue := reflect.New(fieldType)
-			byteReader := bytes.NewReader(fieldBuffer)
-			if endiannes == "be" {
-				binary.Read(byteReader, binary.BigEndian, fieldValue.Interface())
-			} else {
-				binary.Read(byteReader, binary.LittleEndian, fieldValue.Interface())
-			}
-			fields[field.Name] = fieldValue.Elem().Interface()
-		}
-	}
-
-	metricTime := time.Now().UTC()
+func (config BinMetric) getTime(fields map[string]interface{}) (time.Time, error) {
+	nilTime := new(time.Time)
+	metricTime := time.Now()
 	timeValue := fields[timeKey]
 	if timeValue != nil {
-		if config.BinMetric.TimeFormat == "" {
-			return nil, fmt.Errorf(`use of "%s" field requires "binmetric_time_format"`, timeKey)
+		var err error
+		switch config.TimeFormat {
+		case "unix":
+			metricTime, err = internal.ParseTimestamp(config.TimeFormat, int64(timeValue.(int32)), timezone)
+		case "unix_ms", "unix_ns":
+			metricTime, err = internal.ParseTimestamp(config.TimeFormat, int64(timeValue.(int64)), timezone)
+		default:
+			return *nilTime, fmt.Errorf("invalid time format %s", config.TimeFormat)
 		}
-
-		var timeValueInt64 int64 = int64(timeValue.(int32))
-		metricTime, err = internal.ParseTimestamp(config.BinMetric.TimeFormat, timeValueInt64, timezone)
 		if err != nil {
-			return nil, err
+			return *nilTime, err
 		}
-
 		delete(fields, timeKey)
 	}
-
-	// metricTags := make(map[string]string)
-	// for _, tagKey := range config.BinMetric.TagKeys {
-	// 	metricTags[tagKey] = ""
-	// }
-
-	metric, err := metric.New(config.BinMetric.MetricName, nil, fields, metricTime)
-	if err != nil {
-		return nil, err
-	}
-	return []telegraf.Metric{metric}, err
+	return metricTime, nil
 }
--- plugins/parsers/binmetric/parser_test.go
+++ plugins/parsers/binmetric/parser_test.go
@@ -1,27 +1,270 @@
 package binmetric
 
 import (
-	"fmt"
-	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestParser(t *testing.T) {
+var binaryDataBigEndian = []byte{
+	0x00, 0x01, 0x02, 0x03, 0x00, 0x04, 0x00, 0x05, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x07,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09,
+	0x41, 0x29, 0x00, 0x00, 0x40, 0x26, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x41, 0x42, 0x43,
+	0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53,
+	0x5D, 0xA8, 0x6C, 0x4C,
+}
 
-	binMetric, err := ioutil.ReadFile("metric.bin")
-	require.NoError(t, err)
+var binaryDataLittleEndian = []byte{
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x00, 0x05, 0x00, 0x06, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00,
+	0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x29, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x40, 0x40, 0x41, 0x42, 0x43,
+	0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53,
+	0x4C, 0x6C, 0xA8, 0x5D,
+}
 
-	parser := BinMetric{}
-	metrics, err := parser.Parse(binMetric)
+var binaryDataBigEndianTimeMs = []byte{
+	0x00,
+	0x00, 0x00, 0x01, 0x6D, 0xD9, 0xE7, 0x0B, 0x17,
+}
+
+func TestAllFieldsOrderedBigEndian(t *testing.T) {
+
+	var allTypes = BinMetric{
+		MetricName: "all_types_be",
+		Endiannes:  "be",
+		TimeFormat: "unix",
+		Fields: []Field{
+			Field{Name: "fieldBool0", Type: "bool", Offset: 0, Size: 1},
+			Field{Name: "fieldBool1", Type: "bool", Offset: 1, Size: 1},
+			Field{Name: "fieldUint8", Type: "uint8", Offset: 2, Size: 1},
+			Field{Name: "fieldInt8", Type: "int8", Offset: 3, Size: 1},
+			Field{Name: "fieldUint16", Type: "uint16", Offset: 4, Size: 2},
+			Field{Name: "fieldInt16", Type: "int16", Offset: 6, Size: 2},
+			Field{Name: "fieldUint32", Type: "uint32", Offset: 8, Size: 4},
+			Field{Name: "fieldInt32", Type: "int32", Offset: 12, Size: 4},
+			Field{Name: "fieldUint64", Type: "uint64", Offset: 16, Size: 8},
+			Field{Name: "fieldInt64", Type: "int64", Offset: 24, Size: 8},
+			Field{Name: "fieldFloat32", Type: "float32", Offset: 32, Size: 4},
+			Field{Name: "fieldFloat64", Type: "float64", Offset: 36, Size: 8},
+			Field{Name: "fieldString", Type: "string", Offset: 44, Size: 20},
+			Field{Name: "time", Type: "int32", Offset: 64, Size: 4},
+		},
+	}
+
+	metrics, err := allTypes.Parse(binaryDataBigEndian)
 	require.NoError(t, err)
 	assert.Len(t, metrics, 1)
-	require.Equal(t, "drone_status", metrics[0].Name())
+	require.Equal(t, allTypes.MetricName, metrics[0].Name())
+	assert.Equal(t, int64(0x5DA86C4C), metrics[0].Time().Unix())
+	assert.Equal(t, map[string]interface{}{
+		"fieldBool0":   false,
+		"fieldBool1":   true,
+		"fieldUint8":   uint64(2),
+		"fieldInt8":    int64(3),
+		"fieldUint16":  uint64(4),
+		"fieldInt16":   int64(5),
+		"fieldUint32":  uint64(6),
+		"fieldInt32":   int64(7),
+		"fieldUint64":  uint64(8),
+		"fieldInt64":   int64(9),
+		"fieldFloat32": 10.5625,
+		"fieldFloat64": 11.0,
+		"fieldString":  "@ABCDEFGHIJKLMNOPQRS",
+	}, metrics[0].Fields())
 
-	fmt.Println("time", metrics[0].Time())
-	for key, value := range metrics[0].Fields() {
-		fmt.Println(key, value)
+	// fmt.Println("time", metrics[0].Time())
+	// for key, value := range metrics[0].Fields() {
+	// 	fmt.Println(key, value)
+	// }
+}
+
+func TestAllFieldsLittleEndian(t *testing.T) {
+
+	var allTypes = BinMetric{
+		MetricName: "all_types_le",
+		Endiannes:  "le",
+		TimeFormat: "unix",
+		Fields: []Field{
+			Field{Name: "fieldBool0", Type: "bool", Offset: 0, Size: 1},
+			Field{Name: "fieldBool1", Type: "bool", Offset: 1, Size: 1},
+			Field{Name: "fieldUint8", Type: "uint8", Offset: 2, Size: 1},
+			Field{Name: "fieldInt8", Type: "int8", Offset: 3, Size: 1},
+			Field{Name: "fieldUint16", Type: "uint16", Offset: 4, Size: 2},
+			Field{Name: "fieldInt16", Type: "int16", Offset: 6, Size: 2},
+			Field{Name: "fieldUint32", Type: "uint32", Offset: 8, Size: 4},
+			Field{Name: "fieldInt32", Type: "int32", Offset: 12, Size: 4},
+			Field{Name: "fieldUint64", Type: "uint64", Offset: 16, Size: 8},
+			Field{Name: "fieldInt64", Type: "int64", Offset: 24, Size: 8},
+			Field{Name: "fieldFloat32", Type: "float32", Offset: 32, Size: 4},
+			Field{Name: "fieldFloat64", Type: "float64", Offset: 36, Size: 8},
+			Field{Name: "fieldString", Type: "string", Offset: 44, Size: 20},
+			Field{Name: "time", Type: "int32", Offset: 64, Size: 4},
+		},
 	}
+
+	metrics, err := allTypes.Parse(binaryDataLittleEndian)
+	require.NoError(t, err)
+	assert.Len(t, metrics, 1)
+	require.Equal(t, allTypes.MetricName, metrics[0].Name())
+	assert.Equal(t, int64(0x5DA86C4C), metrics[0].Time().Unix())
+	assert.Equal(t, map[string]interface{}{
+		"fieldBool0":   false,
+		"fieldBool1":   true,
+		"fieldUint8":   uint64(2),
+		"fieldInt8":    int64(3),
+		"fieldUint16":  uint64(4),
+		"fieldInt16":   int64(5),
+		"fieldUint32":  uint64(6),
+		"fieldInt32":   int64(7),
+		"fieldUint64":  uint64(8),
+		"fieldInt64":   int64(9),
+		"fieldFloat32": 10.5625,
+		"fieldFloat64": 11.0,
+		"fieldString":  "@ABCDEFGHIJKLMNOPQRS",
+	}, metrics[0].Fields())
+}
+
+func TestFieldsOutOfOrder(t *testing.T) {
+
+	var outOfOrder = BinMetric{
+		MetricName: "out_of_order",
+		Endiannes:  "be",
+		TimeFormat: "unix",
+		Fields: []Field{
+			Field{Name: "fieldString", Type: "string", Offset: 44, Size: 20},
+			Field{Name: "fieldBool1", Type: "bool", Offset: 1, Size: 1},
+			Field{Name: "fieldBool0", Type: "bool", Offset: 0, Size: 1},
+			Field{Name: "fieldInt8", Type: "int8", Offset: 3, Size: 1},
+			Field{Name: "fieldUint8", Type: "uint8", Offset: 2, Size: 1},
+			Field{Name: "fieldInt16", Type: "int16", Offset: 6, Size: 2},
+			Field{Name: "fieldUint16", Type: "uint16", Offset: 4, Size: 2},
+			Field{Name: "time", Type: "int32", Offset: 64, Size: 4},
+			Field{Name: "fieldInt32", Type: "int32", Offset: 12, Size: 4},
+			Field{Name: "fieldUint32", Type: "uint32", Offset: 8, Size: 4},
+			Field{Name: "fieldInt64", Type: "int64", Offset: 24, Size: 8},
+			Field{Name: "fieldUint64", Type: "uint64", Offset: 16, Size: 8},
+			Field{Name: "fieldFloat64", Type: "float64", Offset: 36, Size: 8},
+			Field{Name: "fieldFloat32", Type: "float32", Offset: 32, Size: 4},
+		},
+	}
+
+	metrics, err := outOfOrder.Parse(binaryDataBigEndian)
+	require.NoError(t, err)
+	assert.Len(t, metrics, 1)
+	require.Equal(t, outOfOrder.MetricName, metrics[0].Name())
+	assert.Equal(t, int64(0x5DA86C4C), metrics[0].Time().Unix())
+	assert.Equal(t, map[string]interface{}{
+		"fieldBool0":   false,
+		"fieldBool1":   true,
+		"fieldUint8":   uint64(2),
+		"fieldInt8":    int64(3),
+		"fieldUint16":  uint64(4),
+		"fieldInt16":   int64(5),
+		"fieldUint32":  uint64(6),
+		"fieldInt32":   int64(7),
+		"fieldUint64":  uint64(8),
+		"fieldInt64":   int64(9),
+		"fieldFloat32": 10.5625,
+		"fieldFloat64": 11.0,
+		"fieldString":  "@ABCDEFGHIJKLMNOPQRS",
+	}, metrics[0].Fields())
+}
+
+func TestWithGaps(t *testing.T) {
+
+	var withGaps = BinMetric{
+		MetricName: "with_gaps",
+		Endiannes:  "be",
+		TimeFormat: "unix",
+		Fields: []Field{
+			Field{Name: "fieldString", Type: "string", Offset: 44, Size: 20},
+			Field{Name: "fieldBool1", Type: "bool", Offset: 1, Size: 1},
+			Field{Name: "fieldInt8", Type: "int8", Offset: 3, Size: 1},
+			Field{Name: "fieldUint8", Type: "uint8", Offset: 2, Size: 1},
+			Field{Name: "time", Type: "int32", Offset: 64, Size: 4},
+			Field{Name: "fieldInt32", Type: "int32", Offset: 12, Size: 4},
+			Field{Name: "fieldUint32", Type: "uint32", Offset: 8, Size: 4},
+			Field{Name: "fieldInt64", Type: "int64", Offset: 24, Size: 8},
+			Field{Name: "fieldUint64", Type: "uint64", Offset: 16, Size: 8},
+			Field{Name: "fieldFloat32", Type: "float32", Offset: 32, Size: 4},
+		},
+	}
+
+	metrics, err := withGaps.Parse(binaryDataBigEndian)
+	require.NoError(t, err)
+	assert.Len(t, metrics, 1)
+	require.Equal(t, withGaps.MetricName, metrics[0].Name())
+	assert.Equal(t, int64(0x5DA86C4C), metrics[0].Time().Unix())
+	assert.Equal(t, map[string]interface{}{
+		"fieldBool1":   true,
+		"fieldUint8":   uint64(2),
+		"fieldInt8":    int64(3),
+		"fieldUint32":  uint64(6),
+		"fieldInt32":   int64(7),
+		"fieldUint64":  uint64(8),
+		"fieldInt64":   int64(9),
+		"fieldFloat32": 10.5625,
+		"fieldString":  "@ABCDEFGHIJKLMNOPQRS",
+	}, metrics[0].Fields())
+}
+
+func TestTimeAddedByParser(t *testing.T) {
+
+	var noTimeHere = BinMetric{
+		MetricName: "no_time_here",
+		Endiannes:  "be",
+		TimeFormat: "unix",
+		Fields: []Field{
+			Field{Name: "fieldBool0", Type: "bool", Offset: 0, Size: 1},
+		},
+	}
+
+	metrics, err := noTimeHere.Parse(binaryDataBigEndian)
+	require.NoError(t, err)
+	assert.Len(t, metrics, 1)
+	require.Equal(t, noTimeHere.MetricName, metrics[0].Name())
+	assert.True(t, int64(0x5DA86C4C) < metrics[0].Time().Unix())
+	assert.Equal(t, map[string]interface{}{
+		"fieldBool0": false,
+	}, metrics[0].Fields())
+}
+
+func TestInvalidType(t *testing.T) {
+
+	var invalidType = BinMetric{
+		MetricName: "invalid_type",
+		Endiannes:  "be",
+		TimeFormat: "unix",
+		Fields: []Field{
+			Field{Name: "fieldBool0", Type: "boo", Offset: 0, Size: 1},
+		},
+	}
+
+	metrics, err := invalidType.Parse(binaryDataBigEndian)
+	require.Error(t, err)
+	assert.Len(t, metrics, 0)
+}
+
+func TestTimeMs(t *testing.T) {
+
+	var timeMs = BinMetric{
+		MetricName: "time_ms",
+		Endiannes:  "be",
+		TimeFormat: "unix_ms",
+		Fields: []Field{
+			Field{Name: "fieldBool0", Type: "bool", Offset: 0, Size: 1},
+			Field{Name: "time", Type: "int64", Offset: 1, Size: 8},
+		},
+	}
+
+	metrics, err := timeMs.Parse(binaryDataBigEndianTimeMs)
+	require.NoError(t, err)
+	assert.Len(t, metrics, 1)
+	require.Equal(t, timeMs.MetricName, metrics[0].Name())
+	assert.Equal(t, int64(0x0000016DD9E70B17), metrics[0].Time().UnixNano()/1000000)
+	assert.Equal(t, map[string]interface{}{
+		"fieldBool0": false,
+	}, metrics[0].Fields())
+
 }
