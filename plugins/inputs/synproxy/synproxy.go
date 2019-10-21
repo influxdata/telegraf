3bbc37a9796e463b3141b1967511a3da12a22553
rf@worldstream.nl <rf@worldstream.nl>
2019-10-21 15:40:39 +0200

    Removed license text en fixed comments

--- plugins/inputs/synproxy/synproxy.go
+++ plugins/inputs/synproxy/synproxy.go
@@ -1,29 +1,3 @@
-// Copyright (c) 2019 WorldStream B.V. All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. The names of the authors may not be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
-// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Remi Frenay <rf@worldstream.nl>
-
 // +build linux
 
 package synproxy
@@ -97,12 +71,10 @@ func (k *Synproxy) getSynproxyStat() (map[string]interface{}, error) {
 			// If field is not a valid hexstring
 			if err != nil {
 				return nil, fmt.Errorf("synproxy: Invalid value '%s' found!", val)
-				//	hname = append(hname, val)
-				//	fields[val] = 0
-				// If index is out of boundary
+			// If index is out of boundary
 			} else if i >= len(fields) {
 				return nil, fmt.Errorf("synproxy: Value '%s' out of column boundary!", val)
-				// If field is a valid hexstring and index not out of boundary
+			// If field is a valid hexstring and index not out of boundary
 			} else {
 				fields[hname[i]] = fields[hname[i]].(uint32) + uint32(x)
 			}
