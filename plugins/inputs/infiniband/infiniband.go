2f5facf18e42175043ae9723c5350bd32753820c
Will Furnell <willfurnell@users.noreply.github.com>
2019-11-07 11:34:09 +0000

    Cleanup infiniband code and start working on tests

--- plugins/inputs/infiniband/infiniband.go
+++ plugins/inputs/infiniband/infiniband.go
@@ -2,14 +2,19 @@ package infiniband
 
 import (
 	"strconv"
-	"github.com/willfurnell/rdmamap"
+	"fmt"
+	"github.com/Mellanox/rdmamap"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
+// Stores the configuration values for the infiniband plugin - as there are no
+// config values, this is intentionally empty
 type Infiniband struct {
+
 }
 
+// Sample configuration for plugin
 var InfinibandConfig = `
   ## no config required
 `
@@ -19,35 +24,53 @@ func (s *Infiniband) SampleConfig() string {
 }
 
 func (s *Infiniband) Description() string {
-	return "Gets counters from all Infiniband cards and ports installed, seperately"
+	return "Gets counters from all InfiniBand cards and ports installed"
 }
 
+// Gather statistics from our infiniband cards
 func (s *Infiniband) Gather(acc telegraf.Accumulator) error {
 
 	rdmaDevices := rdmamap.GetRdmaDeviceList()
+
+	if len(rdmaDevices) == 0 {
+		return fmt.Errorf("No InfiniBand devices found on this system! Check /sys/class/infiniband/ exists")
+	}
 
 	for _, dev := range rdmaDevices {
 		devicePorts := rdmamap.GetPorts(dev)
 		for _, port := range devicePorts {
 			portInt, err := strconv.Atoi(port)
-			stats, err2 := rdmamap.GetRdmaSysfsStats(dev, portInt)
-			if err == nil && err2 == nil {
-
-				tags := map[string]string{"card": dev, "port": port}
-				fields := make(map[string]interface{})
-
-				for _, entry := range stats {
-					fields[entry.Name] = entry.Value
-				}
-
-				acc.AddFields("infiniband", fields, tags)
+			if err != nil {
+				return err
 			}
+
+			stats, err := rdmamap.GetRdmaSysfsStats(dev, portInt)
+			if err != nil {
+				return err
+			}
+			
+			addStats(dev, port, stats, acc)
 		}
 	}
 
 	return nil
 }
 
+// Add the statistics to the accumulator
+func addStats(dev string, port string, stats[] rdmamap.RdmaStatEntry, acc telegraf.Accumulator) {
+	
+	// Allow users to filter by card and port
+	tags := map[string]string{"device": dev, "port": port}
+	fields := make(map[string]interface{})
+
+	for _, entry := range stats {
+		fields[entry.Name] = entry.Value
+	}
+
+	acc.AddFields("infiniband", fields, tags)
+}
+
+// Initialise plugin
 func init() {
 	inputs.Add("infiniband", func() telegraf.Input { return &Infiniband{} })
 }
