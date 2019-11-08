5d74dc5d31166da39ef04e003b7b8ee70ba4fa70
Will Furnell <willfurnell@users.noreply.github.com>
2019-11-08 12:10:35 +0000

    Remove init from base infiniband.go file

--- plugins/inputs/infiniband/infiniband.go
+++ plugins/inputs/infiniband/infiniband.go
@@ -1,10 +1,5 @@
 package infiniband
 
-import (
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/plugins/inputs"
-)
-
 // Stores the configuration values for the infiniband plugin - as there are no
 // config values, this is intentionally empty
 type Infiniband struct {
@@ -21,9 +16,4 @@ func (s *Infiniband) SampleConfig() string {
 
 func (s *Infiniband) Description() string {
 	return "Gets counters from all InfiniBand cards and ports installed"
-}
-
-// Initialise plugin
-func init() {
-	inputs.Add("infiniband", func() telegraf.Input { return &Infiniband{} })
 }
