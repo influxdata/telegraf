de0026b287278056ebdd82d658a91e3cb354e547
cfz <fangzhouchen@outlook.com>
2019-11-09 12:33:15 +0800

    close write side corrently if server fast reject

--- plugins/outputs/influxdb_v2/http.go
+++ plugins/outputs/influxdb_v2/http.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -214,7 +215,12 @@ func (c *httpClient) writeBatch(ctx context.Context, bucket string, metrics []te
 		return err
 	}
 
-	reader := influx.NewReader(metrics, c.serializer)
+	reader, err := c.requestBodyReader(metrics)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
 	req, err := c.makeWriteRequest(url, reader)
 	if err != nil {
 		return err
@@ -282,12 +288,6 @@ func (c *httpClient) writeBatch(ctx context.Context, bucket string, metrics []te
 
 func (c *httpClient) makeWriteRequest(url string, body io.Reader) (*http.Request, error) {
 	var err error
-	if c.ContentEncoding == "gzip" {
-		body, err = internal.CompressWithGzip(body)
-		if err != nil {
-			return nil, err
-		}
-	}
 
 	req, err := http.NewRequest("POST", url, body)
 	if err != nil {
@@ -302,6 +302,23 @@ func (c *httpClient) makeWriteRequest(url string, body io.Reader) (*http.Request
 	}
 
 	return req, nil
+}
+
+// requestBodyReader warp io.Reader from influx.NewReader to io.ReadCloser, which is usefully to fast close the write
+// side of the connection in case of error
+func (c *httpClient) requestBodyReader(metrics []telegraf.Metric) (io.ReadCloser, error) {
+	reader := influx.NewReader(metrics, c.serializer)
+
+	if c.ContentEncoding == "gzip" {
+		rc, err := internal.CompressWithGzip(reader)
+		if err != nil {
+			return nil, err
+		}
+
+		return rc, nil
+	}
+
+	return ioutil.NopCloser(reader), nil
 }
 
 func (c *httpClient) addHeaders(req *http.Request) {
