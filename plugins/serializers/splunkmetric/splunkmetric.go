f151b299f14161d8ed74313c925f0205350d34e2
Lance O'Connor <lance@ronnoco.net>
2019-11-14 21:05:26 -0800

    Break metric generation into functions

--- plugins/serializers/splunkmetric/splunkmetric.go
+++ plugins/serializers/splunkmetric/splunkmetric.go
@@ -13,6 +13,23 @@ type serializer struct {
 	SplunkmetricMultiMetric bool
 }
 
+type CommonTags struct {
+	Time   float64
+	Host   string
+	Index  string
+	Source string
+	Fields map[string]interface{}
+}
+
+type HECTimeSeries struct {
+	Time   float64                `json:"time"`
+	Event  string                 `json:"event"`
+	Host   string                 `json:"host,omitempty"`
+	Index  string                 `json:"index,omitempty"`
+	Source string                 `json:"source,omitempty"`
+	Fields map[string]interface{} `json:"fields"`
+}
+
 // NewSerializer Setup our new serializer
 func NewSerializer(splunkmetric_hec_routing bool, splunkmetric_multimetric bool) (*serializer, error) {
 	/*	Define output params */
@@ -49,6 +66,104 @@ func (s *serializer) SerializeBatch(metrics []telegraf.Metric) ([]byte, error) {
 	return serialized, nil
 }
 
+func (s *serializer) createMulti(metric telegraf.Metric, dataGroup HECTimeSeries, commonTags CommonTags) (metricGroup []byte, err error) {
+	/* When splunkmetric_multimetric is true, then we can write out multiple name=value pairs as part of the same
+	** event payload. This only works when the time, host, and dimensions are the same for every name=value pair
+	** in the timeseries data.
+	**
+	** The format for multimetric data is 'metric_name:nameOfMetric = valueOfMetric'
+	 */
+	var metricJSON []byte
+
+	// Set the event data from the commonTags above.
+	dataGroup.Event = "metric"
+	dataGroup.Time = commonTags.Time
+	dataGroup.Host = commonTags.Host
+	dataGroup.Index = commonTags.Index
+	dataGroup.Source = commonTags.Source
+	dataGroup.Fields = commonTags.Fields
+
+	// Stuff the metrid data into the structure.
+	for _, field := range metric.FieldList() {
+		value, valid := verifyValue(field.Value)
+
+		if !valid {
+			log.Printf("D! Can not parse value: %v for key: %v", field.Value, field.Key)
+			continue
+		}
+
+		dataGroup.Fields["metric_name:"+metric.Name()+"."+field.Key] = value
+	}
+
+	// Manage the rest of the event details based upon HEC routing rules
+	switch s.HecRouting {
+	case true:
+		// Output the data as a fields array and host,index,time,source overrides for the HEC.
+		metricJSON, err = json.Marshal(dataGroup)
+	default:
+		// Just output the data and the time, useful for file based outuputs
+		dataGroup.Fields["time"] = dataGroup.Time
+		metricJSON, err = json.Marshal(dataGroup.Fields)
+	}
+	if err != nil {
+		return nil, err
+	}
+	// Let the JSON fall through to the return below
+	metricGroup = metricJSON
+
+	return metricGroup, nil
+}
+
+func (s *serializer) createSingle(metric telegraf.Metric, dataGroup HECTimeSeries, commonTags CommonTags) (metricGroup []byte, err error) {
+	/* The default mode is to generate one JSON entitiy per metric (required for pre-8.0 Splunks)
+	**
+	** The format for single metric is 'nameOfMetric = valueOfMetric'
+	 */
+
+	var metricJSON []byte
+
+	for _, field := range metric.FieldList() {
+
+		value, valid := verifyValue(field.Value)
+
+		if !valid {
+			log.Printf("D! Can not parse value: %v for key: %v", field.Value, field.Key)
+			continue
+		}
+
+		dataGroup.Event = "metric"
+
+		dataGroup.Time = commonTags.Time
+
+		// Apply the common tags from above to every record.
+		dataGroup.Host = commonTags.Host
+		dataGroup.Index = commonTags.Index
+		dataGroup.Source = commonTags.Source
+		dataGroup.Fields = commonTags.Fields
+
+		dataGroup.Fields["metric_name"] = metric.Name() + "." + field.Key
+		dataGroup.Fields["_value"] = value
+
+		switch s.HecRouting {
+		case true:
+			// Output the data as a fields array and host,index,time,source overrides for the HEC.
+			metricJSON, err = json.Marshal(dataGroup)
+		default:
+			// Just output the data and the time, useful for file based outuputs
+			dataGroup.Fields["time"] = dataGroup.Time
+			metricJSON, err = json.Marshal(dataGroup.Fields)
+		}
+
+		metricGroup = append(metricGroup, metricJSON...)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return metricGroup, nil
+}
+
 func (s *serializer) createObject(metric telegraf.Metric) (metricGroup []byte, err error) {
 
 	/*  Splunk supports one metric json object, and does _not_ support an array of JSON objects.
@@ -58,25 +173,8 @@ func (s *serializer) createObject(metric telegraf.Metric) (metricGroup []byte, e
 		 ** time:       The timestamp for the metric
 		 ** All other index fields become dimensions.
 	*/
-	type HECTimeSeries struct {
-		Time   float64                `json:"time"`
-		Event  string                 `json:"event"`
-		Host   string                 `json:"host,omitempty"`
-		Index  string                 `json:"index,omitempty"`
-		Source string                 `json:"source,omitempty"`
-		Fields map[string]interface{} `json:"fields"`
-	}
 
 	dataGroup := HECTimeSeries{}
-	var metricJson []byte
-
-	type CommonTags struct {
-		Time   float64
-		Host   string
-		Index  string
-		Source string
-		Fields map[string]interface{}
-	}
 
 	// The tags are common to all events in this timeseries
 	commonTags := CommonTags{}
@@ -103,88 +201,9 @@ func (s *serializer) createObject(metric telegraf.Metric) (metricGroup []byte, e
 	commonTags.Time = float64(metric.Time().UnixNano()) / float64(1000000000)
 	switch s.SplunkmetricMultiMetric {
 	case true:
-		/* When splunkmetric_multimetric is true, then we can write out multiple name=value pairs as part of the same
-		** event payload. This only works when the time, host, and dimensions are the same for every name=value pair
-		** in the timeseries data.
-		**
-		** The format for multimetric data is 'metric_name:nameOfMetric = valueOfMetric'
-		 */
-
-		// Set the event data from the commonTags above.
-		dataGroup.Event = "metric"
-		dataGroup.Time = commonTags.Time
-		dataGroup.Host = commonTags.Host
-		dataGroup.Index = commonTags.Index
-		dataGroup.Source = commonTags.Source
-		dataGroup.Fields = commonTags.Fields
-
-		// Stuff the metrid data into the structure.
-		for _, field := range metric.FieldList() {
-			value, valid := verifyValue(field.Value)
-
-			if !valid {
-				log.Printf("D! Can not parse value: %v for key: %v", field.Value, field.Key)
-				continue
-			}
-
-			dataGroup.Fields["metric_name:"+metric.Name()+"."+field.Key] = value
-		}
-
-		// Manage the rest of the event details based upon HEC routing rules
-		switch s.HecRouting {
-		case true:
-			// Output the data as a fields array and host,index,time,source overrides for the HEC.
-			metricJson, err = json.Marshal(dataGroup)
-		default:
-			// Just output the data and the time, useful for file based outuputs
-			dataGroup.Fields["time"] = dataGroup.Time
-			metricJson, err = json.Marshal(dataGroup.Fields)
-		}
-		// Let the JSON fall through to the return below
-		metricGroup = metricJson
+		metricGroup, _ = s.createMulti(metric, dataGroup, commonTags)
 	default:
-		/* The default mode is to generate one JSON entitiy per metric (required for pre-8.0 Splunks)
-		**
-		** The format for single metric is 'nameOfMetric = valueOfMetric'
-		 */
-		for _, field := range metric.FieldList() {
-
-			value, valid := verifyValue(field.Value)
-
-			if !valid {
-				log.Printf("D! Can not parse value: %v for key: %v", field.Value, field.Key)
-				continue
-			}
-
-			dataGroup.Event = "metric"
-
-			dataGroup.Time = commonTags.Time
-
-			// Apply the common tags from above to every record.
-			dataGroup.Host = commonTags.Host
-			dataGroup.Index = commonTags.Index
-			dataGroup.Source = commonTags.Source
-			dataGroup.Fields = commonTags.Fields
-
-			dataGroup.Fields["metric_name"] = metric.Name() + "." + field.Key
-			dataGroup.Fields["_value"] = value
-
-			switch s.HecRouting {
-			case true:
-				// Output the data as a fields array and host,index,time,source overrides for the HEC.
-				metricJson, err = json.Marshal(dataGroup)
-			default:
-				// Just output the data and the time, useful for file based outuputs
-				dataGroup.Fields["time"] = dataGroup.Time
-				metricJson, err = json.Marshal(dataGroup.Fields)
-			}
-
-			metricGroup = append(metricGroup, metricJson...)
-
-			if err != nil {
-				return nil, err
-			}
-		}
+		metricGroup, _ = s.createSingle(metric, dataGroup, commonTags)
 	}
 
 	// Return the metric group regardless of if it's multimetric or single metric.
