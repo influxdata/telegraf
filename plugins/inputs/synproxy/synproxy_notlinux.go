8831651799974038c272c10e20fbafe541ac3345
Daniel Nelson <daniel.nelson@influxdb.com>
2020-01-02 16:15:48 -0800

    Show platform not supported warning only on plugin creation (#6801)

--- plugins/inputs/synproxy/synproxy_notlinux.go
+++ plugins/inputs/synproxy/synproxy_notlinux.go
@@ -3,29 +3,21 @@
 package synproxy
 
 import (
-	"log"
-
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
-type Synproxy struct{}
+func (k *Synproxy) Init() error {
+	k.Log.Warn("Current platform is not supported")
+	return nil
+}
 
 func (k *Synproxy) Gather(acc telegraf.Accumulator) error {
 	return nil
 }
 
-func (k *Synproxy) Description() string {
-	return ""
-}
-
-func (k *Synproxy) SampleConfig() string {
-	return ""
-}
-
 func init() {
 	inputs.Add("synproxy", func() telegraf.Input {
-		log.Print("W! [inputs.synproxy] Current platform is not supported")
 		return &Synproxy{}
 	})
 }
--- plugins/inputs/wireless/wireless_notlinux.go
+++ plugins/inputs/wireless/wireless_notlinux.go
@@ -3,11 +3,14 @@
 package wireless
 
 import (
-	"log"
-
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
+
+func (w *Wireless) Init() error {
+	w.Log.Warn("Current platform is not supported")
+	return nil
+}
 
 func (w *Wireless) Gather(acc telegraf.Accumulator) error {
 	return nil
@@ -15,7 +18,6 @@ func (w *Wireless) Gather(acc telegraf.Accumulator) error {
 
 func init() {
 	inputs.Add("wireless", func() telegraf.Input {
-		log.Print("W! [inputs.wireless] Current platform is not supported")
 		return &Wireless{}
 	})
 }
