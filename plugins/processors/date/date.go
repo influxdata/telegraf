04c38d55385aa93186f29c01e68468eebf48ba54
Daniel Nelson <daniel.nelson@influxdb.com>
2020-01-13 17:06:51 -0800

    Add timezone support to date processor

--- plugins/processors/date/date.go
+++ plugins/processors/date/date.go
@@ -1,6 +1,8 @@
 package date
 
 import (
+	"time"
+
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/processors"
@@ -16,12 +18,22 @@ const sampleConfig = `
 
   ## Offset duration added to the date string when writing the new tag.
   # date_offset = "0s"
+
+  ## Timezone to use when creating the tag.  This can be set to one of
+  ## "UTC", "Local", or to a location name in the IANA Time Zone database.
+  ##   example: timezone = "America/Los_Angeles"
+  # timezone = "UTC"
 `
+
+const defaultTimezone = "UTC"
 
 type Date struct {
 	TagKey     string            `toml:"tag_key"`
 	DateFormat string            `toml:"date_format"`
 	DateOffset internal.Duration `toml:"date_offset"`
+	Timezone   string            `toml:"timezone"`
+
+	location *time.Location
 }
 
 func (d *Date) SampleConfig() string {
@@ -32,9 +44,16 @@ func (d *Date) Description() string {
 	return "Dates measurements, tags, and fields that pass through this filter."
 }
 
+func (d *Date) Init() error {
+	var err error
+	// LoadLocation returns UTC if timezone is the empty string.
+	d.location, err = time.LoadLocation(d.Timezone)
+	return err
+}
+
 func (d *Date) Apply(in ...telegraf.Metric) []telegraf.Metric {
 	for _, point := range in {
-		tm := point.Time().UTC().Add(d.DateOffset.Duration)
+		tm := point.Time().In(d.location).Add(d.DateOffset.Duration)
 		point.AddTag(d.TagKey, tm.Format(d.DateFormat))
 	}
 
@@ -43,6 +62,8 @@ func (d *Date) Apply(in ...telegraf.Metric) []telegraf.Metric {
 
 func init() {
 	processors.Add("date", func() telegraf.Processor {
-		return &Date{}
+		return &Date{
+			Timezone: defaultTimezone,
+		}
 	})
 }
