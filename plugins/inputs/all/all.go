72c78e626a1af50a7b0f73997e2a4d70aef3d407
Dean Sheehan <dean.sheehan@me.com>
2020-01-14 21:22:56 +0000

    Removed Netflow parser and input plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -97,7 +97,6 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/neptune_apex"
 	_ "github.com/influxdata/telegraf/plugins/inputs/net"
 	_ "github.com/influxdata/telegraf/plugins/inputs/net_response"
-	_ "github.com/influxdata/telegraf/plugins/inputs/network_flow/netflow"
 	_ "github.com/influxdata/telegraf/plugins/inputs/network_flow/sflow"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_plus"
