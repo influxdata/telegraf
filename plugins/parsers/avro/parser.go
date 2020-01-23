c7879af861d0dabf406629831adb26541df9b5f2
Emanuele Falzone <emafalzo@gmail.com>
2020-01-23 23:28:09 +0100

    Decode avro message given a fixed schema

--- plugins/parsers/avro/parser.go
+++ plugins/parsers/avro/parser.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
+
+	"fmt"
+
+    "github.com/linkedin/goavro"
 )
 
 type Parser struct {
@@ -13,18 +17,43 @@ type Parser struct {
 }
 
 func (p *Parser) Parse(buf []byte) ([]telegraf.Metric, error) {
-	return p.createMeasures()
+	codec, err := goavro.NewCodec(`
+        {"namespace": "com.example.plant", "type": "record", "name": "Value", "fields": [{"name": "value", "type": "long"}]}`)
+    
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	fmt.Println(buf)
+
+    // Convert binary Avro data back to native Go form
+    native, _, err := codec.NativeFromBinary(buf[5:])
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println(native)
+
+    // Convert native Go form to textual Avro data
+    textual, err := codec.TextualFromNative(nil, native)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println(textual)
+
+	return p.createMeasures(0)
 }
 
 func (p *Parser) ParseLine(line string) (telegraf.Metric, error) {
-	return p.createMeasure()
+	return p.createMeasure(0)
 }
 
 func (p *Parser) SetDefaultTags(tags map[string]string) {
 	p.DefaultTags = tags
 }
 
-func (p *Parser) createMeasure() (telegraf.Metric, error) {
+func (p *Parser) createMeasure(value int) (telegraf.Metric, error) {
 	recordFields := make(map[string]interface{})
 	tags := make(map[string]string)
 
@@ -34,7 +63,7 @@ func (p *Parser) createMeasure() (telegraf.Metric, error) {
 
 	tags["tagName"] = "tagValue"
 
-	recordFields["fieldName"] = "fieldValue"
+	recordFields["value"] = value
 
 	measurementName := "measurementName"
 
@@ -47,10 +76,10 @@ func (p *Parser) createMeasure() (telegraf.Metric, error) {
 	return m, nil
 }
 
-func (p *Parser) createMeasures() ([]telegraf.Metric, error) {
+func (p *Parser) createMeasures(value int) ([]telegraf.Metric, error) {
 	metrics := make([]telegraf.Metric, 0)
 
-	m, err := p.createMeasure()
+	m, err := p.createMeasure(value)
 	if err != nil {
 		return metrics, err
 	}
