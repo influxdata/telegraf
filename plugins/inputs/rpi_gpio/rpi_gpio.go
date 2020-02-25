ee89985ccc7cec52e7430532de841c23b521fe6b
Michael Hall <mhall119@gmail.com>
2020-02-25 15:29:38 -0500

    First attempt at GPIO plugin for Telegraf

---
+++ plugins/inputs/rpi_gpio/rpi_gpio.go
@@ -0,0 +1,53 @@
+package rpi_gpio
+
+import (
+	"fmt"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+type RPiGPIO struct {
+	Pins map[string]int `toml:pins`
+}
+
+func (s *RPiGPIO) SampleConfig() string {
+	return `
+  ## Map data fields to pin numbers
+  pins = {
+	"motion_sensor" = 1
+	"button" = 2
+	"light_sensor" = 3
+  }
+	`
+}
+
+func (s *RPiGPIO) Description() string {
+	return "Reads binary values from the GPIO pins of a RaspberryPi"
+}
+
+func (s *RPiGPIO) Gather(acc telegraf.Accumulator) error {
+
+	fields := make(map[string]interface{})
+	tags := make(map[string]string)
+
+	err := rpio.Open()
+	if err != nil {
+		return fmt.Errorf("error opening GPIO connection: %s", err)
+	}
+	for field, pin_num := range s.Pins {
+		pin := rpio.Pin(pin_num)
+		pin.Input()
+		val := pin.Read()
+		fields[field] = val
+	}
+	rpio.Close()
+
+	acc.AddFields("GPIO", fields, tags)
+	return nil
+}
+
+func init() {
+	inputs.Add("rpi_gpio", func() telegraf.Input { return &RPiGPIO{} })
+}
