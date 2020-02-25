73b2df4478e4cb69b45e32cb9bce1b0fa8dc71f6
Michael Hall <mhall119@gmail.com>
2020-02-25 17:26:29 -0500

    Add debug logging

--- plugins/inputs/rpi_gpio/rpi_gpio.go
+++ plugins/inputs/rpi_gpio/rpi_gpio.go
@@ -9,17 +9,14 @@ import (
 )
 
 type RPiGPIO struct {
-	Pins map[string]int `toml:pins`
+	Pins map[string]int  `toml:pins`
+	Log  telegraf.Logger `toml:"-"`
 }
 
 func (s *RPiGPIO) SampleConfig() string {
 	return `
   ## Map data fields to pin numbers
-  pins = {
-	"motion_sensor" = 1
-	"button" = 2
-	"light_sensor" = 3
-  }
+  pins = {"motion_sensor"=1, "button"=2, "light_sensor"=3}
 	`
 }
 
@@ -32,16 +29,20 @@ func (s *RPiGPIO) Gather(acc telegraf.Accumulator) error {
 	fields := make(map[string]interface{})
 	tags := make(map[string]string)
 
+	s.Log.Debugf("Opening GPIO connections")
 	err := rpio.Open()
 	if err != nil {
 		return fmt.Errorf("error opening GPIO connection: %s", err)
 	}
 	for field, pin_num := range s.Pins {
+		s.Log.Debugf("Reading %s from pin %d", field, pin_num)
 		pin := rpio.Pin(pin_num)
 		pin.Input()
 		val := pin.Read()
+		s.Log.Debugf("%s=%d", field, val)
 		fields[field] = val
 	}
+	s.Log.Debugf("Closing GPIO connections")
 	rpio.Close()
 
 	acc.AddFields("GPIO", fields, tags)
