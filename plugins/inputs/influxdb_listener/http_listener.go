834290fed67a7dbdd4139049d5054183a4598251
John Green <nhojjohn@users.noreply.github.com>
2020-03-03 14:52:41 +0000

    Set headers correctly on influxdb_listener
    The ping endpoint in influx returns the version in a HTTP header (eg X-Influxdb-Version: 1.0).  The presence of this header is checked by Springboot influxdb ping() implementation (at quite possibly other clients).  This PR adds this header to ping responses and additionally sets the Content-Type correctly when used with the verbose parameter.

--- plugins/inputs/influxdb_listener/http_listener.go
+++ plugins/inputs/influxdb_listener/http_listener.go
@@ -244,7 +244,9 @@ func (h *HTTPListener) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 		verbose := req.URL.Query().Get("verbose")
 
 		// respond to ping requests
+		res.Header().Set("X-Influxdb-Version", "1.0")
 		if verbose != "" && verbose != "0" && verbose != "false" {
+			res.Header().Set("Content-Type", "application/json")
 			res.WriteHeader(http.StatusOK)
 			b, _ := json.Marshal(map[string]string{"version": "1.0"}) // based on header set above
 			res.Write(b)
