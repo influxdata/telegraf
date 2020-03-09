d778bbb0a42f57ee3945736b0395d60814a4fa49
Dmitry Kalenik <dmitry.kalenik@oxagile.com>
2020-03-09 11:48:17 +0300

    Added the ability to set the endpoint url in the configuration file

--- plugins/outputs/application_insights/transmitter.go
+++ plugins/outputs/application_insights/transmitter.go
@@ -1,13 +1,22 @@
 package application_insights
 
-import "github.com/Microsoft/ApplicationInsights-Go/appinsights"
+import (
+	"github.com/Microsoft/ApplicationInsights-Go/appinsights"
+)
 
 type Transmitter struct {
 	client appinsights.TelemetryClient
 }
 
-func NewTransmitter(ikey string) *Transmitter {
-	return &Transmitter{client: appinsights.NewTelemetryClient(ikey)}
+func NewTransmitter(ikey string, EndpointUrl string) *Transmitter {
+
+	if len(EndpointUrl) == 0 {
+		return &Transmitter{client: appinsights.NewTelemetryClient(ikey)}
+	}
+
+	telemetryConfig := appinsights.NewTelemetryConfiguration(ikey)
+	telemetryConfig.EndpointUrl = EndpointUrl
+	return &Transmitter{client: appinsights.NewTelemetryClientFromConfig(telemetryConfig)}
 }
 
 func (t *Transmitter) Track(telemetry appinsights.Telemetry) {
