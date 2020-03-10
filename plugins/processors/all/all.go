4c090515c090af586d679c9a0e0f6b174b80d070
alespour <42931850+alespour@users.noreply.github.com>
2020-03-10 23:39:06 +0100

    Add s2_geo processor plugin (#7087)

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/processors/converter"
 	_ "github.com/influxdata/telegraf/plugins/processors/date"
 	_ "github.com/influxdata/telegraf/plugins/processors/enum"
+	_ "github.com/influxdata/telegraf/plugins/processors/geo"
 	_ "github.com/influxdata/telegraf/plugins/processors/override"
 	_ "github.com/influxdata/telegraf/plugins/processors/parser"
 	_ "github.com/influxdata/telegraf/plugins/processors/pivot"
