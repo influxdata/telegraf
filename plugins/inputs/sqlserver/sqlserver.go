abd373f7b0bae8706885f3667d24053a39eb116a
trovalo <giovanni.luisotto@gmail.com>
2020-04-16 17:59:12 +0200

    Added CPU query

--- plugins/inputs/sqlserver/sqlserver.go
+++ plugins/inputs/sqlserver/sqlserver.go
@@ -117,6 +117,7 @@ func initQueries(s *SQLServer) error {
 		queries["Schedulers"] = Query{Script: sqlServerSchedulersV2, ResultByRow: false}
 		queries["SqlRequests"] = Query{Script: sqlServerRequestsV2, ResultByRow: false}
 		queries["VolumeSpace"] = Query{Script: sqlServerVolumeSpaceV2, ResultByRow: false}
+		queries["Cpu"] = Query{Script: sqlServerCPUV2, ResultByRow: false}
 	} else {
 		queries["PerformanceCounters"] = Query{Script: sqlPerformanceCounters, ResultByRow: true}
 		queries["WaitStatsCategorized"] = Query{Script: sqlWaitStatsCategorized, ResultByRow: false}
@@ -1591,6 +1592,41 @@ IF SERVERPROPERTY('EngineEdition') NOT IN (5,8)
 		sys.master_files as mf
 		CROSS APPLY sys.dm_os_volume_stats(mf.database_id, mf.file_id) as vs
 	END
+`
+
+const sqlServerCPUV2 string = `
+DECLARE @ts_now BIGINT
+
+SELECT @ts_now = [ms_ticks]
+FROM sys.dm_os_sys_info
+
+SELECT 
+	 'sqlserver_cpu' AS [measurement]
+	,REPLACE(@@SERVERNAME,'\',':') AS [sql_instance]
+	,[SQLProcessUtilization] AS [sqlserver_process_cpu]
+	,[SystemIdle] AS [system_idle_cpu]
+	,100 - [SystemIdle] - [SQLProcessUtilization] AS [other_process_cpu]
+FROM (
+	SELECT TOP 1
+		 [record_id]
+		,dateadd(ms, (y.[timestamp] - @ts_now), GETDATE()) AS [EventTime]
+		,[SQLProcessUtilization]
+		,[SystemIdle]
+	FROM (
+		SELECT record.value('(./Record/@id)[1]', 'int') AS [record_id]
+			,record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int') AS [SystemIdle]
+			,record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]', 'int') AS [SQLProcessUtilization]
+			,[TIMESTAMP]
+		FROM (
+			SELECT [TIMESTAMP]
+				,convert(XML, [record]) AS [record]
+			FROM sys.dm_os_ring_buffers
+			WHERE [ring_buffer_type] = N'RING_BUFFER_SCHEDULER_MONITOR'
+				AND [record] LIKE '%<SystemHealth>%'
+			) AS x
+		) AS y
+	ORDER BY record_id DESC
+) as z
 `
 
 // Queries V1
