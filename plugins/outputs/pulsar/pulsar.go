c42846f2a031deedf649421f0eb2ad32947b44ac
Yuvaraj Loganathan <uvaraj6@gmail.com>
2020-04-19 16:22:30 +0530

    Initial support for pulsar output

--- plugins/outputs/pulsar/pulsar.go
+++ plugins/outputs/pulsar/pulsar.go
@@ -50,7 +50,8 @@ type (
 		BatchingMaxPublishDelay string `toml:"batching_max_publish_delay"`
 		BatchingMaxMessages     uint   `toml:"batching_max_messages"`
 
-		producerCache map[string]pulsar.Producer
+		tenantNameSpace string
+		producerCache   map[string]pulsar.Producer
 
 		Log telegraf.Logger `toml:"-"`
 
@@ -66,117 +67,123 @@ type (
 )
 
 var sampleConfig = `
-  ## URLs of pulsar url
-  url = ["localhost:6650"]
-  ## Pulsar topic for producer messages
-  topic = "telegraf"
+  [[outputs.pulsar]]
+ ## URLs of pulsar url
+  url = "pulsar://localhost:6650"
+ ## Pulsar topic for producer messages
+  topic = "persistent://public/default/telegraf"
 
-  ## The value of this tag will be used as the topic.  If not set the 'topic'
-  ## option is used.
-  # topic_tag = ""
+ ## The value of this tag will be used as the topic.  If not set the 'topic'
+ ## option is used.
+ # topic_tag = "foo"
 
-  ## If true, the 'topic_tag' will be removed from to the metric.
-  # exclude_topic_tag = false
+ ## If true, the 'topic_tag' will be removed from to the metric.
+ # exclude_topic_tag = false
 
-  ## Optional topic suffix configuration.
-  ## If the section is omitted, no suffix is used.
-  ## Following topic suffix methods are supported:
-  ##   measurement - suffix equals to separator + measurement's name
-  ##   tags        - suffix equals to separator + specified tags' values
-  ##                 interleaved with separator
-
-  ## Suffix equals to "_" + measurement name
-  # [outputs.pulsar.topic_suffix]
-  #   method = "measurement"
-  #   separator = "_"
-
-  ## Suffix equals to "__" + measurement's "foo" tag value.
-  ##   If there's no such a tag, suffix equals to an empty string
-  # [outputs.pulsar.topic_suffix]
-  #   method = "tags"
-  #   keys = ["foo"]
-  #   separator = "__"
-
-  ## Suffix equals to "_" + measurement's "foo" and "bar"
-  ##   tag values, separated by "_". If there is no such tags,
-  ##   their values treated as empty strings.
-  # [outputs.pulsar.topic_suffix]
-  #   method = "tags"
-  #   keys = ["foo", "bar"]
-  #   separator = "_"
-
-  ## The routing tag specifies a tagkey on the metric whose value is used as
-  ## the message key.  The message key is used to determine which partition to
-  ## send the message to.  This tag is prefered over the routing_key option.
   routing_tag = "host"
 
-  ## The routing key is set as the message key and used to determine which
-  ## partition to send the message to.  This value is only used when no
-  ## routing_tag is set or as a fallback when the tag specified in routing tag
-  ## is not found.
-  ##
-  ## If set to "random", a random value will be generated for each message.
-  ##
-  ## When unset, no message key is added and each message is routed to a random
-  ## partition.
-  ##
-  ##   ex: routing_key = "random"
-  ##       routing_key = "telegraf"
-  # routing_key = ""
+ ## The routing key is set as the message key and used to determine which
+ ## partition to send the message to.  This value is only used when no
+ ## routing_tag is set or as a fallback when the tag specified in routing tag
+ ## is not found.
+ ##
+ ## If set to "random", a random value will be generated for each message.
+ ##
+ ## When unset, no message key is added and each message is routed to a random
+ ## partition.
+ ##
+ ##   ex: routing_key = "random"
+ ##       routing_key = "telegraf"
+ # routing_key = ""
 
-  ## Optional Authentication Provider Config Defaults to empty "" NoAuthentication
-  ## if set to "token" provide the JWT token
-  ## if set to "tls" the please mention tls_certificate_path and tls_private_key_path
-  # auth_provider = ""
-  # auth_token = ""
-  # tls_allow_insecure_connection = false
-  # tls_trust_certs_file_path = ""
-  # tls_validate_host_name = true
-  # tls_certificate_path = ""
-  # tls_private_key_path = ""
+ ## Optional Authentication Provider Config Defaults to empty "" NoAuthentication
+ ## if set to "token" provide the JWT token
+ ## if set to "tls" the please mention tls_certificate_path and tls_private_key_path
+ # auth_provider = ""
 
-  ## Optional timeout Config
-  # connection_timeout = 30s
-  # operation_timeout = 30s
+ # For token auth provider
+ # auth_token = ""
+
+ # Set the following values for tls auth provider
+ # tls_allow_insecure_connection = false
+ # tls_trust_certs_file_path = ""
+ # tls_validate_host_name = true
+ # tls_certificate_path = ""
+ # tls_private_key_path = ""
+
+ ## Optional timeout Config
+ # connection_timeout = "30s"
+ # operation_timeout = "30s"
 
  ## Optional Producer Config
 
-  ## CompressionType represents the various compression codecs recognized by
-  ## Pulsar in messages.
-  ##  0 : No compression
-  ##  1 : LZ4 compression
-  ##  2 : ZLib compression
-  ##  3 : ZSTD compression
-  # compression_type = 0
+ ## CompressionType represents the various compression codecs recognized by
+ ## Pulsar in messages.
+ ##  0 : No compression
+ ##  1 : LZ4 compression
+ ##  2 : ZLib compression
+ ##  3 : ZSTD compression
+ # compression_type = 0
 
-  ## MaxPendingMessages set the max size of the queue holding the messages pending to receive an
-  ## acknowledgment from the broker.
-  # max_pending_messages = 1000
+ ## MaxPendingMessages set the max size of the queue holding the messages pending to receive an
+ ## acknowledgment from the broker.
+ # max_pending_messages = 1000
 
-  ## BatchingMaxPublishDelay set the time period within which the messages sent will be batched (default: 10ms)
-  ## if batch messages are enabled. If set to a non zero value, messages will be queued until this time
-  ## interval or until
-  # batching_max_publish_delay = 10ms
+ ## BatchingMaxPublishDelay set the time period within which the messages sent will be batched (default: 10ms)
+ ## if batch messages are enabled. If set to a non zero value, messages will be queued until this time
+ ## interval or until
+ # batching_max_publish_delay = "10ms"
 
-  ## BatchingMaxMessages set the maximum number of messages permitted in a batch. (default: 1000)
-  ## If set to a value greater than 1, messages will be queued until this threshold is reached or
-  ## batch interval has elapsed.
-  # batching_max_messages = 1000
+ ## BatchingMaxMessages set the maximum number of messages permitted in a batch. (default: 1000)
+ ## If set to a value greater than 1, messages will be queued until this threshold is reached or
+ ## batch interval has elapsed.
+ # batching_max_messages = 1000
 
-  ## HashingScheme change the "HashingScheme" used to chose the partition on where to publish a particular message.
-  ##  JavaStringHash : JavaStringHash Hshing
-  ##  Murmur3_32Hash : Murmur3_32Hash Hashing
-  # hashing_scheme = JavaStringHash
+ ## HashingScheme change the "HashingScheme" used to chose the partition on where to publish a particular message.
+ ##  JavaStringHash : JavaStringHash Hshing
+ ##  Murmur3_32Hash : Murmur3_32Hash Hashing
+ # hashing_scheme = "JavaStringHash"
 
-  ## Disable batching will reduce the throughput
-  # disable_batching = false
+ ## Disable batching will reduce the throughput
+ # disable_batching = false
 
 
-  ## Data format to output.
-  ## Each data format has its own unique set of configuration options, read
-  ## more about them here:
-  ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_OUTPUT.md
-  # data_format = "influx"
+ ## Data format to output.
+ ## Each data format has its own unique set of configuration options, read
+ ## more about them here:
+ ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_OUTPUT.md
+ # data_format = "influx"
+
+ ## Optional topic suffix configuration.
+ ## If the section is omitted, no suffix is used.
+ ## Following topic suffix methods are supported:
+ ##   measurement - suffix equals to separator + measurement's name
+ ##   tags        - suffix equals to separator + specified tags' values
+ ##                 interleaved with separator
+ ## The routing tag specifies a tagkey on the metric whose value is used as
+ ## the message key.  The message key is used to determine which partition to
+ ## send the message to.  This tag is prefered over the routing_key option.
+
+ ## Suffix equals to "_" + measurement name
+ #  [outputs.pulsar.topic_suffix]
+ #    method = "measurement"
+ #    separator = "-"
+
+ ## Suffix equals to "__" + measurement's "foo" tag value.
+ ##   If there's no such a tag, suffix equals to an empty string
+ #  [outputs.pulsar.topic_suffix]
+ #    method = "tags"
+ #    keys = ["foo"]
+ #    separator = "-"
+
+ ## Suffix equals to "_" + measurement's "foo" and "bar"
+ ##   tag values, separated by "_". If there is no such tags,
+ ##   their values treated as empty strings.
+ #  [outputs.pulsar.topic_suffix]
+ #    method = "tags"
+ #    keys = ["foo","bar"]
+ #    separator = "-"
+
 `
 
 func ValidateTopicSuffixMethod(method string) error {
@@ -192,8 +199,7 @@ func (p *Pulsar) GetTopicName(metric telegraf.Metric) (telegraf.Metric, string) 
 	topic := p.Topic
 	if p.TopicTag != "" {
 		if t, ok := metric.GetTag(p.TopicTag); ok {
-			topic = t
-
+			topic = p.tenantNameSpace + "/" + t
 			// If excluding the topic tag, a copy is required to avoid modifying
 			// the metric buffer.
 			if p.ExcludeTopicTag {
@@ -250,7 +256,7 @@ func (p *Pulsar) GetProducer(topic string) (pulsar.Producer, error) {
 		default:
 			producerOptions.HashingScheme = pulsar.JavaStringHash
 		}
-		if p.MaxPendingMessages != 0 {
+		if p.MaxPendingMessages > 0 {
 			producerOptions.MaxPendingMessages = p.MaxPendingMessages
 		}
 		if producerOptions.DisableBatching == true {
@@ -268,12 +274,13 @@ func (p *Pulsar) GetProducer(topic string) (pulsar.Producer, error) {
 		if err != nil {
 			return nil, err
 		} else {
-			p.Log.Debugf("Created producer for topic %s", topic)
-			p.producerCache = map[string]pulsar.Producer{topic: producer}
+			p.Log.Infof("Created producer for topic %s", topic)
+			p.producerCache[topic] = producer
 			return producer, nil
 		}
 
 	} else {
+		p.Log.Debugf("Returning cached producer for topic %s", topic)
 		return producer, nil
 	}
 
@@ -284,6 +291,8 @@ func (p *Pulsar) SetSerializer(serializer serializers.Serializer) {
 
 func (p *Pulsar) Connect() error {
 	p.producerCache = make(map[string]pulsar.Producer)
+	splits := strings.Split(p.Topic, "/")
+	p.tenantNameSpace = strings.Join(splits[0:4], "/")
 	err := ValidateTopicSuffixMethod(p.TopicSuffix.Method)
 	if err != nil {
 		return err
@@ -294,18 +303,26 @@ func (p *Pulsar) Connect() error {
 		TLSTrustCertsFilePath:      p.TLSTrustCertsFilePath,
 		TLSValidateHostname:        p.TLSValidateHostname,
 	}
-	connectionTimeout, err := time.ParseDuration(p.ConnectionTimeout)
-	if err != nil {
-		p.Log.Error("Invalid ConnectionTimeout. Setting it to 30s ", err.Error())
-		clientOptions.ConnectionTimeout = 30 * time.Second
+	if p.ConnectionTimeout != "" {
+		connectionTimeout, err := time.ParseDuration(p.ConnectionTimeout)
+		if err != nil {
+			p.Log.Error("Invalid ConnectionTimeout. Setting it to 30s ", err.Error())
+			clientOptions.ConnectionTimeout = 30 * time.Second
+		} else {
+			clientOptions.ConnectionTimeout = connectionTimeout
+		}
 	}
-	operationTimeout, err := time.ParseDuration(p.OperationTimeout)
-	if err != nil {
-		p.Log.Error("Invalid OperationTimeout. Setting it to 30s", err.Error())
-		clientOptions.OperationTimeout = 30 * time.Second
+	if p.OperationTimeout != "" {
+		operationTimeout, err := time.ParseDuration(p.OperationTimeout)
+		if err != nil {
+			p.Log.Error("Invalid OperationTimeout. Setting it to 30s", err.Error())
+			clientOptions.OperationTimeout = 30 * time.Second
+		} else {
+			clientOptions.OperationTimeout = operationTimeout
+		}
+
 	}
-	clientOptions.ConnectionTimeout = connectionTimeout
-	clientOptions.OperationTimeout = operationTimeout
+
 	if p.AuthProvider == "tls" {
 		if p.TLSCertificatePath != "" && p.TLSPrivateKeyPath != "" {
 			clientOptions.Authentication = pulsar.NewAuthenticationTLS(p.TLSCertificatePath, p.TLSPrivateKeyPath)
@@ -342,7 +359,7 @@ func (p *Pulsar) SampleConfig() string {
 }
 
 func (p *Pulsar) Description() string {
-	return "Configuration for the Pulsar server to send metrics to"
+	return "Configuration to send metrics to pulsar broker"
 }
 
 func (p *Pulsar) routingKey(metric telegraf.Metric) string {
@@ -392,7 +409,7 @@ func (p *Pulsar) Write(metrics []telegraf.Metric) error {
 			}
 
 		} else {
-			p.Log.Error("Unable to create producer for topic %s : ", topic, err.Error())
+			p.Log.Errorf("Unable to create producer for topic %s : %s", topic, err.Error())
 		}
 
 	}
