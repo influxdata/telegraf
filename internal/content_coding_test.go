f0b97a6a59a2c99dc12aa7d90b260e6fae0fb5cf
Daniel Nelson <daniel.nelson@influxdb.com>
2020-04-30 18:06:41 -0700

    Fix gzip support in socket_listener with tcp sockets

--- internal/content_coding_test.go
+++ internal/content_coding_test.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"bytes"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -55,4 +57,69 @@ func TestIdentityEncodeDecode(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, "howdy", string(actual))
+}
+
+func TestStreamIdentityEncode(t *testing.T) {
+	var w bytes.Buffer
+
+	enc, err := NewStreamContentEncoder("identity", &w)
+	require.NoError(t, err)
+
+	n, err := enc.Write([]byte("howdy"))
+	require.NoError(t, err)
+	require.Equal(t, 5, n)
+
+	require.Equal(t, []byte("howdy"), w.Bytes())
+}
+
+func TestStreamIdentityDecode(t *testing.T) {
+	var r bytes.Buffer
+	n, err := r.Write([]byte("howdy"))
+	require.NoError(t, err)
+	require.Equal(t, 5, n)
+
+	dec, err := NewStreamContentDecoder("identity", &r)
+	require.NoError(t, err)
+
+	data, err := ioutil.ReadAll(dec)
+	require.NoError(t, err)
+
+	require.Equal(t, []byte("howdy"), data)
+}
+
+func TestStreamGzipEncode(t *testing.T) {
+	var w bytes.Buffer
+
+	enc, err := NewStreamContentEncoder("gzip", &w)
+	require.NoError(t, err)
+
+	n, err := enc.Write([]byte("howdy"))
+	require.NoError(t, err)
+	require.Equal(t, 5, n)
+
+	dec, err := NewGzipDecoder()
+	require.NoError(t, err)
+
+	actual, err := dec.Decode(w.Bytes())
+	require.NoError(t, err)
+	require.Equal(t, []byte("howdy"), actual)
+}
+
+func TestStreamGzipDecode(t *testing.T) {
+	enc, err := NewGzipEncoder()
+	require.NoError(t, err)
+	written, err := enc.Encode([]byte("howdy"))
+	require.NoError(t, err)
+
+	w := bytes.NewBuffer(written)
+
+	dec, err := NewStreamContentDecoder("gzip", w)
+	require.NoError(t, err)
+
+	b := make([]byte, 10)
+	n, err := dec.Read(b)
+	require.NoError(t, err)
+	require.Equal(t, 5, n)
+
+	require.Equal(t, []byte("howdy"), b[:n])
 }
