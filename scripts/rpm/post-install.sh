b6560c8b01a79a5c16526db24e1ecd7f050ba289
Daniel Nelson <daniel.nelson@influxdb.com>
2020-06-30 00:15:28 -0700

    Build packages in makefile (#7759)

--- scripts/rpm/post-install.sh
+++ scripts/rpm/post-install.sh
@@ -43,11 +43,6 @@ if [[ ! -d /etc/telegraf/telegraf.d ]]; then
     mkdir -p /etc/telegraf/telegraf.d
 fi
 
-# If 'telegraf.conf' is not present use package's sample (fresh install)
-if [[ ! -f /etc/telegraf/telegraf.conf ]] && [[ -f /etc/telegraf/telegraf.conf.sample ]]; then
-   cp /etc/telegraf/telegraf.conf.sample /etc/telegraf/telegraf.conf
-fi
-
 # Distribution-specific logic
 if [[ -f /etc/redhat-release ]] || [[ -f /etc/SuSE-release ]]; then
     # RHEL-variant logic
@@ -62,30 +57,6 @@ if [[ -f /etc/redhat-release ]] || [[ -f /etc/SuSE-release ]]; then
         else
             install_chkconfig
         fi
-    fi
-elif [[ -f /etc/debian_version ]]; then
-    # Debian/Ubuntu logic
-
-    # Ownership for RH-based platforms is set in build.py via the `rmp-attr` option.
-    # We perform ownership change only for Debian-based systems.
-    # Moving these lines out of this if statement would make `rmp -V` fail after installation.
-    test -d $LOG_DIR || mkdir -p $LOG_DIR
-    chown -R -L telegraf:telegraf $LOG_DIR
-    chmod 755 $LOG_DIR
-
-    if [[ "$(readlink /proc/1/exe)" == */systemd ]]; then
-        install_systemd /lib/systemd/system/telegraf.service
-        deb-systemd-invoke restart telegraf.service || echo "WARNING: systemd not running."
-    else
-        # Assuming SysVinit
-        install_init
-        # Run update-rc.d or fallback to chkconfig if not available
-        if which update-rc.d &>/dev/null; then
-            install_update_rcd
-        else
-            install_chkconfig
-        fi
-        invoke-rc.d telegraf restart
     fi
 elif [[ -f /etc/os-release ]]; then
     source /etc/os-release
