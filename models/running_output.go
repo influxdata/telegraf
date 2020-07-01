f2893db9ee33f2a5428f86ec472bc48fea09da80
Daniel Nelson <daniel.nelson@influxdb.com>
2020-06-30 23:15:11 -0700

    Allow overriding the collection_jitter and precision per input (#7762)

--- models/running_output.go
+++ models/running_output.go
@@ -24,7 +24,7 @@ type OutputConfig struct {
 	Filter Filter
 
 	FlushInterval     time.Duration
-	FlushJitter       *time.Duration
+	FlushJitter       time.Duration
 	MetricBufferLimit int
 	MetricBatchSize   int
 
