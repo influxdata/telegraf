6741640e6eda2f134ceebf1cb9b53d351bab2f93
Daniel Nelson <daniel.nelson@influxdb.com>
2020-07-06 22:17:25 -0700

    Remove flakey tests

--- plugins/inputs/tail/tail_test.go
+++ plugins/inputs/tail/tail_test.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"runtime"
 	"testing"
 	"time"
 
@@ -18,89 +17,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestTailFromBeginning(t *testing.T) {
-	if os.Getenv("CIRCLE_PROJECT_REPONAME") != "" {
-		t.Skip("Skipping CI testing due to race conditions")
-	}
-
-	tmpfile, err := ioutil.TempFile("", "")
-	require.NoError(t, err)
-	defer os.Remove(tmpfile.Name())
-	defer tmpfile.Close()
-	_, err = tmpfile.WriteString("cpu,mytag=foo usage_idle=100\n")
-	require.NoError(t, err)
-
-	tt := NewTail()
-	tt.Log = testutil.Logger{}
-	tt.FromBeginning = true
-	tt.Files = []string{tmpfile.Name()}
-	tt.SetParserFunc(parsers.NewInfluxParser)
-
-	err = tt.Init()
-	require.NoError(t, err)
-
-	acc := testutil.Accumulator{}
-	require.NoError(t, tt.Start(&acc))
-	defer tt.Stop()
-	require.NoError(t, acc.GatherError(tt.Gather))
-
-	acc.Wait(1)
-	acc.AssertContainsTaggedFields(t, "cpu",
-		map[string]interface{}{
-			"usage_idle": float64(100),
-		},
-		map[string]string{
-			"mytag": "foo",
-			"path":  tmpfile.Name(),
-		})
-}
-
-func TestTailFromEnd(t *testing.T) {
-	if os.Getenv("CIRCLE_PROJECT_REPONAME") != "" {
-		t.Skip("Skipping CI testing due to race conditions")
-	}
-
-	tmpfile, err := ioutil.TempFile("", "")
-	require.NoError(t, err)
-	defer os.Remove(tmpfile.Name())
-	defer tmpfile.Close()
-	_, err = tmpfile.WriteString("cpu,mytag=foo usage_idle=100\n")
-	require.NoError(t, err)
-
-	tt := NewTail()
-	tt.Log = testutil.Logger{}
-	tt.Files = []string{tmpfile.Name()}
-	tt.SetParserFunc(parsers.NewInfluxParser)
-
-	err = tt.Init()
-	require.NoError(t, err)
-
-	acc := testutil.Accumulator{}
-	require.NoError(t, tt.Start(&acc))
-	defer tt.Stop()
-	for _, tailer := range tt.tailers {
-		for n, err := tailer.Tell(); err == nil && n == 0; n, err = tailer.Tell() {
-			// wait for tailer to jump to end
-			runtime.Gosched()
-		}
-	}
-
-	_, err = tmpfile.WriteString("cpu,othertag=foo usage_idle=100\n")
-	require.NoError(t, err)
-	require.NoError(t, acc.GatherError(tt.Gather))
-
-	acc.Wait(1)
-	acc.AssertContainsTaggedFields(t, "cpu",
-		map[string]interface{}{
-			"usage_idle": float64(100),
-		},
-		map[string]string{
-			"othertag": "foo",
-			"path":     tmpfile.Name(),
-		})
-	assert.Len(t, acc.Metrics, 1)
-}
 
 func TestTailBadLine(t *testing.T) {
 	tmpfile, err := ioutil.TempFile("", "")
