71280e7df04c244bb8dca55ae6afc2614179dc31
Ales Pour <ales.pour@bonitoo.io>
2020-08-26 16:54:40 +0200

    feat: add build number field to jenkins_job measurement

--- plugins/inputs/jenkins/jenkins_test.go
+++ plugins/inputs/jenkins/jenkins_test.go
@@ -530,12 +530,14 @@ func TestGatherJobs(t *testing.T) {
 						Building:  false,
 						Result:    "SUCCESS",
 						Duration:  25558,
+						Number:    3,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 					"/job/job2/1/api/json": &buildResponse{
 						Building:  false,
 						Result:    "FAILURE",
 						Duration:  1558,
+						Number:    1,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 				},
@@ -549,6 +551,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(25558),
+							"number":      int64(3),
 							"result_code": 0,
 						},
 					},
@@ -559,6 +562,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(1558),
+							"number":      int64(1),
 							"result_code": 1,
 						},
 					},
@@ -583,6 +587,7 @@ func TestGatherJobs(t *testing.T) {
 						Building:  false,
 						Result:    "SUCCESS",
 						Duration:  25558,
+						Number:    3,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 				},
@@ -596,6 +601,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(25558),
+							"number":      int64(3),
 							"result_code": 0,
 						},
 					},
@@ -658,24 +664,28 @@ func TestGatherJobs(t *testing.T) {
 						Building:  false,
 						Result:    "FAILURE",
 						Duration:  1558,
+						Number:    1,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 					"/job/apps/job/k8s-cloud/job/PR-101/4/api/json": &buildResponse{
 						Building:  false,
 						Result:    "SUCCESS",
 						Duration:  76558,
+						Number:    4,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 					"/job/apps/job/k8s-cloud/job/PR-100/1/api/json": &buildResponse{
 						Building:  false,
 						Result:    "SUCCESS",
 						Duration:  91558,
+						Number:    1,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 					"/job/apps/job/k8s-cloud/job/PR%201/1/api/json": &buildResponse{
 						Building:  false,
 						Result:    "SUCCESS",
 						Duration:  87832,
+						Number:    1,
 						Timestamp: (time.Now().Unix() - int64(time.Minute.Seconds())) * 1000,
 					},
 				},
@@ -690,6 +700,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(87832),
+							"number":      int64(1),
 							"result_code": 0,
 						},
 					},
@@ -701,6 +712,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(91558),
+							"number":      int64(1),
 							"result_code": 0,
 						},
 					},
@@ -712,6 +724,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(76558),
+							"number":      int64(4),
 							"result_code": 0,
 						},
 					},
@@ -723,6 +736,7 @@ func TestGatherJobs(t *testing.T) {
 						},
 						Fields: map[string]interface{}{
 							"duration":    int64(1558),
+							"number":      int64(1),
 							"result_code": 1,
 						},
 					},
