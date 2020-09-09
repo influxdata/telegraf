544915ca63f59296d1d7c9019b90df4340fe3295
Patryk Małek <pmalek@sumologic.com>
2020-09-09 13:22:13 +0200

    Do not explicitly close process' stdin on close

--- internal/process/process.go
+++ internal/process/process.go
@@ -78,8 +78,6 @@ func (p *Process) Stop() {
 		// signal our intent to shutdown and not restart the process
 		p.cancel()
 	}
-	// close stdin so the app can shut down gracefully.
-	p.Stdin.Close()
 	p.mainLoopWg.Wait()
 }
 
