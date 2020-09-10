5534b9955ce10f337ce2c3b2d6f620cbe5285fe0
Patryk Małek <69143962+pmalek-sumo@users.noreply.github.com>
2020-09-10 18:14:11 +0200

    Carbon2 configuration option - include field in metric name (#8094)

--- plugins/serializers/carbon2/carbon2.go
+++ plugins/serializers/carbon2/carbon2.go
@@ -9,12 +9,39 @@ import (
 	"github.com/influxdata/telegraf"
 )
 
-type Serializer struct {
+type format string
+
+const (
+	Carbon2FormatFieldSeparate       string = "field_separate"
+	Carbon2FormatMetricIncludesField string = "metric_includes_field"
+
+	formatFieldSeparate       = format(Carbon2FormatFieldSeparate)
+	formatMetricIncludesField = format(Carbon2FormatMetricIncludesField)
+)
+
+var formats = map[string]format{
+	// Field separate is the default when no format specified.
+	"":                               formatFieldSeparate,
+	Carbon2FormatFieldSeparate:       formatFieldSeparate,
+	Carbon2FormatMetricIncludesField: formatMetricIncludesField,
 }
 
-func NewSerializer() (*Serializer, error) {
-	s := &Serializer{}
-	return s, nil
+type Serializer struct {
+	metricsFormat format
+}
+
+func NewSerializer(f string) (*Serializer, error) {
+	var (
+		ok            bool
+		metricsFormat format
+	)
+	if metricsFormat, ok = formats[f]; !ok {
+		return nil, fmt.Errorf("unknown carbon2 format: %s", f)
+	}
+
+	return &Serializer{
+		metricsFormat: metricsFormat,
+	}, nil
 }
 
 func (s *Serializer) Serialize(metric telegraf.Metric) ([]byte, error) {
@@ -32,30 +59,52 @@ func (s *Serializer) SerializeBatch(metrics []telegraf.Metric) ([]byte, error) {
 func (s *Serializer) createObject(metric telegraf.Metric) []byte {
 	var m bytes.Buffer
 	for fieldName, fieldValue := range metric.Fields() {
-		if isNumeric(fieldValue) {
-			m.WriteString("metric=")
-			m.WriteString(strings.Replace(metric.Name(), " ", "_", -1))
-			m.WriteString(" field=")
-			m.WriteString(strings.Replace(fieldName, " ", "_", -1))
-			m.WriteString(" ")
-			for _, tag := range metric.TagList() {
-				m.WriteString(strings.Replace(tag.Key, " ", "_", -1))
-				m.WriteString("=")
-				value := tag.Value
-				if len(value) == 0 {
-					value = "null"
-				}
-				m.WriteString(strings.Replace(value, " ", "_", -1))
-				m.WriteString(" ")
-			}
-			m.WriteString(" ")
-			m.WriteString(fmt.Sprintf("%v", fieldValue))
-			m.WriteString(" ")
-			m.WriteString(strconv.FormatInt(metric.Time().Unix(), 10))
-			m.WriteString("\n")
+		if !isNumeric(fieldValue) {
+			continue
 		}
+
+		switch s.metricsFormat {
+		case formatFieldSeparate:
+			m.WriteString(serializeMetricFieldSeparate(
+				metric.Name(), fieldName,
+			))
+		case formatMetricIncludesField:
+			m.WriteString(serializeMetricIncludeField(
+				metric.Name(), fieldName,
+			))
+		}
+
+		for _, tag := range metric.TagList() {
+			m.WriteString(strings.Replace(tag.Key, " ", "_", -1))
+			m.WriteString("=")
+			value := tag.Value
+			if len(value) == 0 {
+				value = "null"
+			}
+			m.WriteString(strings.Replace(value, " ", "_", -1))
+			m.WriteString(" ")
+		}
+		m.WriteString(" ")
+		m.WriteString(fmt.Sprintf("%v", fieldValue))
+		m.WriteString(" ")
+		m.WriteString(strconv.FormatInt(metric.Time().Unix(), 10))
+		m.WriteString("\n")
 	}
 	return m.Bytes()
+}
+
+func serializeMetricFieldSeparate(name, fieldName string) string {
+	return fmt.Sprintf("metric=%s field=%s ",
+		strings.Replace(name, " ", "_", -1),
+		strings.Replace(fieldName, " ", "_", -1),
+	)
+}
+
+func serializeMetricIncludeField(name, fieldName string) string {
+	return fmt.Sprintf("metric=%s_%s ",
+		strings.Replace(name, " ", "_", -1),
+		strings.Replace(fieldName, " ", "_", -1),
+	)
 }
 
 func isNumeric(v interface{}) bool {
