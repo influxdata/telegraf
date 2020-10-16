51bb76177beef5c6de69606c2d6f484fc5a876b4
Steven Soroka <ssoroka@influxdata.com>
2020-10-16 14:44:15 -0400

    temporarily remove RAS plugin
    (cherry picked from commit 933925e8e3f1594d32f16d79799c66ef265d165c)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -141,7 +141,6 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/puppetagent"
 	_ "github.com/influxdata/telegraf/plugins/inputs/rabbitmq"
 	_ "github.com/influxdata/telegraf/plugins/inputs/raindrops"
-	_ "github.com/influxdata/telegraf/plugins/inputs/ras"
 	_ "github.com/influxdata/telegraf/plugins/inputs/redfish"
 	_ "github.com/influxdata/telegraf/plugins/inputs/redis"
 	_ "github.com/influxdata/telegraf/plugins/inputs/rethinkdb"
