342c7052a6e3dfbbf78686d7349d3f3b92ce4985
Marcin Grzesiak <marcin.grzesiak@nauto.com>
2020-11-02 18:17:19 +0100

    Use access token as authorization method

--- plugins/outputs/proto/proto.go
+++ plugins/outputs/proto/proto.go
@@ -16,7 +16,6 @@ import (
 	"github.com/influxdata/telegraf/plugins/outputs"
 	"github.com/influxdata/telegraf/plugins/serializers"
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 type Proto struct {
@@ -32,51 +31,31 @@ type Proto struct {
 	doOnce     sync.Once
 }
 
-var sampleConfig = `
-  ## Files to write to, "stdout" is a specially handled file.
-  files = ["stdout", "/tmp/metrics.out"]
-
-  ## Use batch serialization format instead of line based delimiting.  The
-  ## batch format allows for the production of non line based output formats and
-  ## may more efficiently encode metric groups.
-  # use_batch_format = false
-
-  ## The file will be rotated after the time interval specified.  When set
-  ## to 0 no time based rotation is performed.
-  # rotation_interval = "0d"
-
-  ## The logfile will be rotated when it becomes larger than the specified
-  ## size.  When set to 0 no size based rotation is performed.
-  # rotation_max_size = "0MB"
-
-  ## Maximum number of rotated archives to keep, any older logs are deleted.
-  ## If set to -1, no archives are removed.
-  # rotation_max_archives = 5
-
-  ## Data format to output.
-  ## Each data format has its own unique set of configuration options, read
-  ## more about them here:
-  ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_OUTPUT.md
-  data_format = "influx"
-`
+var sampleConfig = ``
 
 func (f *Proto) SetSerializer(serializer serializers.Serializer) {
 	f.serializer = serializer
 }
 
 func (f *Proto) Connect() error {
+	var err error
 	f.doOnce.Do(func() {
 		config := aws.NewConfig().
 			WithRegion(f.AWSRegion).
 			WithCredentials(credentials.AnonymousCredentials)
-		sess, err := session.NewSession(config)
+		var sess *session.Session
+		sess, err = session.NewSession(config)
 		if err != nil {
-			log.Fatal(err)
+			f.Log.Error(err)
 		}
 
-		f.cip = NewCognitoIdentityProvider(sess, aws.String(f.User), aws.String(f.Password), aws.String(f.CognitoAppClientID))
+		f.cip = NewCognitoIdentityProvider(sess,
+			aws.String(f.User),
+			aws.String(f.Password),
+			aws.String(f.CognitoAppClientID))
 	})
-	return nil
+
+	return err
 }
 
 func (f *Proto) Close() error {
@@ -170,6 +149,11 @@ func (f *Proto) Write(metrics []telegraf.Metric) error {
 		}
 	}
 
+	accessToken, err := f.cip.GetAccessToken()
+	if err != nil {
+		return errors.Wrapf(err, "[outputs.proto] unable to get access token")
+	}
+	influx.AccessToken = *accessToken
 	b, err := protobuf.Marshal(&influx)
 	if err != nil {
 		return errors.Wrap(err, "[outputs.proto]")
@@ -189,10 +173,6 @@ func (f *Proto) Write(metrics []telegraf.Metric) error {
 		return errors.Wrap(err, "[outputs.proto]")
 	}
 	req.Header.Set("Content-Encoding", "gzip")
-	accessToken, err := f.cip.GetAccessToken()
-	if err != nil {
-		return errors.Wrap(err, "[outputs.proto] unable to get access token")
-	}
 	req.Header.Set("authorization", fmt.Sprintf("Bearer %s", *accessToken))
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
