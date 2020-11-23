3339c0100cc4100430073db2ec623d8ac845dead
Steven Soroka <ssoroka@influxdata.com>
2020-11-23 14:41:06 -0500

    log warning if enable_tls is used

--- plugins/common/kafka/config.go
+++ plugins/common/kafka/config.go
@@ -1,20 +1,25 @@
 package kafka
 
 import (
+	"log"
+
 	"github.com/Shopify/sarama"
 	"github.com/influxdata/telegraf/plugins/common/tls"
 )
 
+// ReadConfig for kafka clients meaning to read from Kafka.
 type ReadConfig struct {
 	Config
 }
 
+// SetConfig on the sarama.Config object from the ReadConfig struct.
 func (k *ReadConfig) SetConfig(config *sarama.Config) error {
 	config.Consumer.Return.Errors = true
 
 	return k.Config.SetConfig(config)
 }
 
+// WriteConfig for kafka clients meaning to write to kafka
 type WriteConfig struct {
 	Config
 
@@ -24,6 +29,7 @@ type WriteConfig struct {
 	IdempotentWrites bool `toml:"idempotent_writes"`
 }
 
+// SetConfig on the sarama.Config object from the WriteConfig struct.
 func (k *WriteConfig) SetConfig(config *sarama.Config) error {
 	config.Producer.Return.Successes = true
 	config.Producer.Idempotent = k.IdempotentWrites
@@ -35,6 +41,7 @@ func (k *WriteConfig) SetConfig(config *sarama.Config) error {
 	return k.Config.SetConfig(config)
 }
 
+// Config common to all Kafka clients.
 type Config struct {
 	SASLAuth
 	tls.ClientConfig
@@ -44,10 +51,14 @@ type Config struct {
 	CompressionCodec int    `toml:"compression_codec"`
 
 	// EnableTLS deprecated
-	_EnableTLS *bool `toml:"enable_tls"`
+	EnableTLS *bool `toml:"enable_tls"`
 }
 
+// SetConfig on the sarama.Config object from the Config struct.
 func (k *Config) SetConfig(config *sarama.Config) error {
+	if k.EnableTLS != nil {
+		log.Printf("W! [kafka] enable_tls is deprecated, and the setting does nothing, you can safely remove it from the config")
+	}
 	if k.Version != "" {
 		version, err := sarama.ParseKafkaVersion(k.Version)
 		if err != nil {
