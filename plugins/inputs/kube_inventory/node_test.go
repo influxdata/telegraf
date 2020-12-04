bf0c9cda9fd46f2bf07b6d03ff2b477ac41aed46
Joe Wang <me@joewang.net>
2020-12-04 11:51:36 -0800

    fmt

--- plugins/inputs/kube_inventory/node_test.go
+++ plugins/inputs/kube_inventory/node_test.go
@@ -103,12 +103,12 @@ func TestNode(t *testing.T) {
 					{
 						Measurement: nodeMeasurement,
 						Fields: map[string]interface{}{
-							"capacity_millicpu_cores":      int64(16000),
-							"capacity_memory_bytes":        int64(1.28837533696e+11),
-							"capacity_pods":                int64(110),
-							"allocatable_millicpu_cores":   int64(1000),
-							"allocatable_memory_bytes":     int64(1.28732676096e+11),
-							"allocatable_pods":             int64(110),
+							"capacity_millicpu_cores":    int64(16000),
+							"capacity_memory_bytes":      int64(1.28837533696e+11),
+							"capacity_pods":              int64(110),
+							"allocatable_millicpu_cores": int64(1000),
+							"allocatable_memory_bytes":   int64(1.28732676096e+11),
+							"allocatable_pods":           int64(110),
 						},
 						Tags: map[string]string{
 							"node_name": "node1",
