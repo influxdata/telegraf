3c36e96357b604c27acfc870b6cdabf4eb341ecb
Sven Rebhan <sven.rebhan@googlemail.com>
2020-12-07 18:20:25 +0100

    Rework test-cases, simplify and extend them.

--- plugins/inputs/knx_listener/knx_listener_test.go
+++ plugins/inputs/knx_listener/knx_listener_test.go
@@ -1,6 +1,8 @@
 package knx_listener
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
@@ -14,243 +16,119 @@ import (
 	"github.com/vapourismo/knx-go/knx/dpt"
 )
 
-const epsilon = 1e-6
+const epsilon = 1e-3
 
-// Check if we can receive data
-func TestRegularReceives_DPT1(t *testing.T) {
-	acc := &testutil.Accumulator{}
-
-	// Setup the unit-under-test
-	listener := KNXListener{
-		ServiceType:    "dummy",
-		ServiceAddress: "manual",
-		Measurements: []Measurement{
-			{"test_1001", "1.001", []string{"1/0/1"}},
-			{"test_1002", "1.002", []string{"1/0/2"}},
-			{"test_1003", "1.003", []string{"1/0/3"}},
-		},
+func setValue(data dpt.DatapointValue, value interface{}) error {
+	d := reflect.Indirect(reflect.ValueOf(data))
+	if !d.CanSet() {
+		return fmt.Errorf("cannot set datapoint %v", data)
 	}
-
-	err := listener.Start(acc)
-	require.NoError(t, err)
-
-	// Get the dummy interface to send data to the unit-under-test
-	client := listener.client.(*KNXDummyInterface)
-
-	tstart := time.Now()
-
-	// Send some test data we expect in the config
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(1, 0, 1),
-		Data:        dpt.DPT_1001(true).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(1, 0, 2),
-		Data:        dpt.DPT_1002(false).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(1, 0, 3),
-		Data:        dpt.DPT_1003(true).Pack(),
-	})
-
-	// Give the accumulator some time to collect the data
-	acc.Wait(3)
-
-	// Stop the listener
-	listener.Stop()
-	tstop := time.Now()
-
-	// Check if we got what we expected
-	require.Len(t, acc.Metrics, 3)
-
-	m := acc.Metrics[0]
-	assert.Equal(t, "test_1001", m.Measurement)
-	assert.Equal(t, "1/0/1", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.Equal(t, true, m.Fields["value"])
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[1]
-	assert.Equal(t, "test_1002", m.Measurement)
-	assert.Equal(t, "1/0/2", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.Equal(t, false, m.Fields["value"])
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[2]
-	assert.Equal(t, "test_1003", m.Measurement)
-	assert.Equal(t, "1/0/3", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.Equal(t, true, m.Fields["value"])
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
+	switch v := value.(type) {
+	case bool:
+		d.SetBool(v)
+	case float64:
+		d.SetFloat(v)
+	case int64:
+		d.SetInt(v)
+	case uint64:
+		d.SetUint(v)
+	default:
+		return fmt.Errorf("unknown type '%T' when setting value for DPT", value)
+	}
+	return nil
 }
 
-// Check if we can receive data
-func TestRegularReceives_DPT5(t *testing.T) {
+func TestRegularReceives_DPT(t *testing.T) {
+	// Define the test-cases
+	var testcases = []struct {
+		address  string
+		dpt      string
+		value    interface{}
+	}{
+		{"1/0/1", "1.001", true},
+		{"1/0/2", "1.002", false},
+		{"1/0/3", "1.003", true},
+		{"1/0/9", "1.009", false},
+		{"1/1/0", "1.010", true},
+		{"5/0/1", "5.001", 12.157},
+		{"5/0/3", "5.003", 121.412},
+		{"5/0/4", "5.004", uint64(25)},
+		{"9/0/1", "9.001", 18.56},
+		{"9/0/4", "9.004", 243.84},
+		{"9/0/5", "9.005", 12.01},
+		{"9/0/7", "9.007", 59.32},
+		{"13/0/1", "13.001", int64(-15)},
+		{"13/0/2", "13.002", int64(183)},
+		{"13/1/0", "13.010", int64(-141)},
+		{"13/1/1", "13.011", int64(277)},
+		{"13/1/2", "13.012", int64(-4096)},
+		{"13/1/3", "13.013", int64(8192)},
+		{"13/1/4", "13.014", int64(-65536)},
+		{"13/1/5", "13.015", int64(2147483647)},
+		{"14/0/0", "14.000", -1.31},
+		{"14/0/1", "14.001", 0.44},
+		{"14/0/2", "14.002", 32.08},
+		// {"14/0/3", "14.003", 92.69},
+		// {"14/0/4", "14.004", 1.00794},
+		{"14/1/0", "14.010", 5963.78},
+		{"14/1/1", "14.011", 150.95},
+	}
 	acc := &testutil.Accumulator{}
 
 	// Setup the unit-under-test
+	measurements := make([]Measurement, 0, len(testcases))
+	for _, testcase := range testcases {
+		measurements = append(measurements, Measurement{"test", testcase.dpt, []string{testcase.address}})
+	}
 	listener := KNXListener{
-		ServiceType:    "dummy",
-		ServiceAddress: "manual",
-		Measurements: []Measurement{
-			{"test_5001", "5.001", []string{"5/0/1"}},
-			{"test_5003", "5.003", []string{"5/0/3"}},
-			{"test_5004", "5.004", []string{"5/0/4"}},
-		},
+		ServiceType:  "dummy",
+		Measurements: measurements,
 	}
 
+	// Setup the listener to test
 	err := listener.Start(acc)
 	require.NoError(t, err)
-
-	// Get the dummy interface to send data to the unit-under-test
 	client := listener.client.(*KNXDummyInterface)
 
 	tstart := time.Now()
 
-	// Send some test data we expect in the config
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(5, 0, 1),
-		Data:        dpt.DPT_5001(12.12).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(5, 0, 3),
-		Data:        dpt.DPT_5003(120.1).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(5, 0, 4),
-		Data:        dpt.DPT_5004(25).Pack(),
-	})
+	// Send the defined test data
+	for _, testcase := range testcases {
+		addr, err := cemi.NewGroupAddrString(testcase.address)
+		require.NoError(t, err)
+
+		data, ok := dpt.Produce(testcase.dpt)
+		require.True(t, ok)
+		err = setValue(data, testcase.value)
+		require.NoError(t, err)
+
+		client.Send(knx.GroupEvent{
+			Command:     knx.GroupWrite,
+			Destination: addr,
+			Data:        data.Pack(),
+		})
+	}
 
 	// Give the accumulator some time to collect the data
-	acc.Wait(3)
+	acc.Wait(len(testcases))
 
 	// Stop the listener
 	listener.Stop()
 	tstop := time.Now()
 
 	// Check if we got what we expected
-	require.Len(t, acc.Metrics, 3)
-
-	m := acc.Metrics[0]
-	assert.Equal(t, "test_5001", m.Measurement)
-	assert.Equal(t, "5/0/1", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.InDelta(t, 12.12, m.Fields["value"], 100.0/255.0+epsilon)
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[1]
-	assert.Equal(t, "test_5003", m.Measurement)
-	assert.Equal(t, "5/0/3", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.InDelta(t, 120.1, m.Fields["value"], 360.0/255.0+epsilon)
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[2]
-	assert.Equal(t, "test_5004", m.Measurement)
-	assert.Equal(t, "5/0/4", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.Equal(t, uint64(25), m.Fields["value"])
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-}
-
-// Check if we can receive data
-func TestRegularReceives_DPT9(t *testing.T) {
-	acc := &testutil.Accumulator{}
-
-	// Setup the unit-under-test
-	listener := KNXListener{
-		ServiceType:    "dummy",
-		ServiceAddress: "manual",
-		Measurements: []Measurement{
-			{"test_9001", "9.001", []string{"9/0/1"}},
-			{"test_9004", "9.004", []string{"9/0/4"}},
-			{"test_9005", "9.005", []string{"9/0/5"}},
-			{"test_9007", "9.007", []string{"9/0/7"}},
-		},
+	require.Len(t, acc.Metrics, len(testcases))
+	for i, m := range acc.Metrics {
+		assert.Equal(t, "test", m.Measurement)
+		assert.Equal(t, testcases[i].address, m.Tags["groupaddress"])
+		assert.Len(t, m.Fields, 1)
+		switch v := testcases[i].value.(type) {
+		case bool,int64,uint64:
+			assert.Equal(t, v, m.Fields["value"])
+		case float64:
+			assert.InDelta(t, v, m.Fields["value"], epsilon)
+		}
+		assert.True(t, tstart.Before(m.Time))
+		assert.True(t, tstop.After(m.Time))
 	}
-
-	err := listener.Start(acc)
-	require.NoError(t, err)
-
-	// Get the dummy interface to send data to the unit-under-test
-	client := listener.client.(*KNXDummyInterface)
-
-	tstart := time.Now()
-
-	// Send some test data we expect in the config
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(9, 0, 1),
-		Data:        dpt.DPT_9001(18.56).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(9, 0, 4),
-		Data:        dpt.DPT_9004(243.9).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(9, 0, 5),
-		Data:        dpt.DPT_9005(12.01).Pack(),
-	})
-	client.Send(knx.GroupEvent{
-		Command:     knx.GroupWrite,
-		Destination: cemi.NewGroupAddr3(9, 0, 7),
-		Data:        dpt.DPT_9007(59.32).Pack(),
-	})
-
-	// Give the accumulator some time to collect the data
-	acc.Wait(4)
-
-	// Stop the listener
-	listener.Stop()
-	tstop := time.Now()
-
-	// Check if we got what we expected
-	require.Len(t, acc.Metrics, 4)
-
-	m := acc.Metrics[0]
-	assert.Equal(t, "test_9001", m.Measurement)
-	assert.Equal(t, "9/0/1", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.InDelta(t, 18.56, m.Fields["value"], 1e-3)
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[1]
-	assert.Equal(t, "test_9004", m.Measurement)
-	assert.Equal(t, "9/0/4", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.InDelta(t, 243.9, m.Fields["value"], 0.1)
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[2]
-	assert.Equal(t, "test_9005", m.Measurement)
-	assert.Equal(t, "9/0/5", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.InDelta(t, 12.01, m.Fields["value"], 1e-3)
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
-
-	m = acc.Metrics[3]
-	assert.Equal(t, "test_9007", m.Measurement)
-	assert.Equal(t, "9/0/7", m.Tags["groupaddress"])
-	assert.Len(t, m.Fields, 1)
-	assert.InDelta(t, 59.32, m.Fields["value"], 1e-3)
-	assert.True(t, tstart.Before(m.Time))
-	assert.True(t, tstop.After(m.Time))
 }
