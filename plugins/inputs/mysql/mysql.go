016c166df83b1065d129bf49d2ce676dac290555
Alex Kapustin <alexvkapustin@gmail.com>
2020-12-12 07:05:30 +1100

    commented settings with default falues

--- plugins/inputs/mysql/mysql.go
+++ plugins/inputs/mysql/mysql.go
@@ -124,11 +124,11 @@ const sampleConfig = `
   # perf_events_statements_time_limit = 86400
 
   ## gather metrics from PERFORMANCE_SCHEMA.EVENTS_STATEMENTS_SUMMARY_BY_ACCOUNT_BY_EVENT_NAME
-  gather_perf_sum_per_acc_per_event         = false
+  # gather_perf_sum_per_acc_per_event         = false
 
   ## list of events to be gathered for gather_perf_sum_per_acc_per_event
   ## in case of empty list all events will be gathered
-  perf_summary_events                       = []
+  # perf_summary_events                       = []
 
   ## Some queries we may want to run less often (such as SHOW GLOBAL VARIABLES)
   ##   example: interval_slow = "30m"
