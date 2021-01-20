7af20b87ca325a61cda57975c9f6003bce473951
David Reimschussel <dreimschussel@influxdata.com>
2021-01-19 17:37:24 -0700

    Add snmp_trap integration test with docker-compose

---
+++ plugins/inputs/snmp_trap/integration_test/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose -f docker-compose.test.yml up --build
--- plugins/inputs/snmp_trap/snmp_trap_test.go
+++ plugins/inputs/snmp_trap/snmp_trap_test.go
@@ -1320,3 +1320,164 @@ func TestReceiveTrap(t *testing.T) {
 	}
 
 }
+
+func TestIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	var now uint32
+	now = 1234567
+
+	var fakeTime time.Time
+	fakeTime = time.Unix(456456456, 456)
+
+	type entry struct {
+		oid string
+		e   mibEntry
+	}
+
+	var tests = []struct {
+		name string
+
+		// send
+		version gosnmp.SnmpVersion
+		trap    gosnmp.SnmpTrap // include pdus
+		// V3 auth and priv parameters
+		secName   string // v3 username
+		secLevel  string // v3 security level
+		authProto string // Auth protocol: "", MD5 or SHA
+		authPass  string // Auth passphrase
+		privProto string // Priv protocol: "", DES or AES
+		privPass  string // Priv passphrase
+
+		// V3 sender context
+		contextName string
+		engineID    string
+
+		// receive
+		entries []entry
+		metrics []telegraf.Metric
+	}{
+		//ordinary v2c coldStart trap
+		{
+			name:    "v2c coldStart",
+			version: gosnmp.Version2c,
+			trap: gosnmp.SnmpTrap{
+				Variables: []gosnmp.SnmpPDU{
+					{
+						Name:  ".1.3.6.1.2.1.1.3.0",
+						Type:  gosnmp.TimeTicks,
+						Value: now,
+					},
+					{
+						Name:  ".1.3.6.1.6.3.1.1.4.1.0", // SNMPv2-MIB::snmpTrapOID.0
+						Type:  gosnmp.ObjectIdentifier,
+						Value: ".1.3.6.1.6.3.1.1.5.1", // coldStart
+					},
+				},
+			},
+			entries: []entry{
+				{
+					oid: ".0.0",
+					e: mibEntry{
+						"SNMPv2-MIB",
+						"snmpTrapOID.0",
+					},
+				},
+				{
+					oid: ".1.3.6.1.6.3.1.1.5.3.0",
+					e: mibEntry{
+						"SNMPv2-MIB",
+						"linkDown.0",
+					},
+				},
+				{
+					oid: ".1.3.6.1.2.1.1.3.0",
+					e: mibEntry{
+						"UNUSED_MIB_NAME",
+						"sysUpTimeInstance",
+					},
+				},
+			},
+			metrics: []telegraf.Metric{
+				testutil.MustMetric(
+					"snmp_trap", // name
+					map[string]string{ // tags
+						"oid":       ".1.3.6.1.6.3.1.1.5.3.0",
+						"name":      "linkDown.0",
+						"mib":       "SNMPv2-MIB",
+						"version":   "2c",
+						"source":    "10.0.0.3",
+						"community": "community",
+					},
+					map[string]interface{}{ // fields
+						"snmpTrapOID.0":     "This is a test linkDown trap from v2c",
+						"sysUpTimeInstance": uint64(1234567),
+					},
+					fakeTime,
+				),
+			},
+		},
+	}
+
+	tt := tests[0]
+
+	// We would prefer to specify port 0 and let the network
+	// stack choose an unused port for us but TrapListener
+	// doesn't have a way to return the autoselected port.
+	// Instead, we'll use an unusual port and hope it's
+	// unused.
+	const port = 12399
+
+	// Hook into the trap handler so the test knows when the
+	// trap has been received
+	received := make(chan int)
+	wrap := func(f handler) handler {
+		return func(p *gosnmp.SnmpPacket, a *net.UDPAddr) {
+			f(p, a)
+			received <- 0
+		}
+	}
+
+	// Set up the service input plugin
+	s := &SnmpTrap{
+		ServiceAddress:     "udp://:" + strconv.Itoa(port),
+		makeHandlerWrapper: wrap,
+		timeFunc: func() time.Time {
+			return fakeTime
+		},
+		Log:          testutil.Logger{},
+		Version:      tt.version.String(),
+		SecName:      tt.secName,
+		SecLevel:     tt.secLevel,
+		AuthProtocol: tt.authProto,
+		AuthPassword: tt.authPass,
+		PrivProtocol: tt.privProto,
+		PrivPassword: tt.privPass,
+	}
+	require.Nil(t, s.Init())
+	// Don't look up oid with snmptranslate.
+	s.execCmd = fakeExecCmd
+	var acc testutil.Accumulator
+	require.Nil(t, s.Start(&acc))
+	defer s.Stop()
+
+	// Preload the cache with the oids we'll use in this test
+	// so snmptranslate and mibs don't need to be installed.
+	for _, entry := range tt.entries {
+		s.load(entry.oid, entry.e)
+	}
+
+	// Wait for trap to be received
+	select {
+	case <-received:
+	case <-time.After(10 * time.Second):
+		t.Fatal("timed out waiting for trap to be received")
+	}
+
+	// Verify plugin output
+	testutil.RequireMetricsEqual(t,
+		tt.metrics, acc.GetTelegrafMetrics(),
+		testutil.SortMetrics())
+}
