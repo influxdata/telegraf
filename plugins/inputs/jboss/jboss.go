433eacfa340b104201c7a468f40fef706f891cd5
Stefan Fagerstrom <stefan.faerstrom@johedkonsult.se>
2021-01-21 08:36:37 +0100

    Update for 1.17

--- plugins/inputs/jboss/jboss.go
+++ plugins/inputs/jboss/jboss.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
-	"github.com/influxdata/telegraf/internal/tls"
+	"github.com/influxdata/telegraf/plugins/common/tls"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	dac "github.com/stefa975/go-http-digest-auth-client"
 )
