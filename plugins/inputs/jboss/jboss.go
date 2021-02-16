b4ac13b0fc02dd987287f13398edb572b0c7b1c5
Stefan Fagerström <stefan.fagerstrom@migrationsverket.se>
2021-02-16 16:52:40 +0100

    Fixed some errors

--- plugins/inputs/jboss/jboss.go
+++ plugins/inputs/jboss/jboss.go
@@ -43,34 +43,6 @@ type KeyVal struct {
 // OrderedMap Define an ordered map
 type OrderedMap []KeyVal
 
-// MarshalJSON Implement the json.Marshaler interface
-func (omap OrderedMap) MarshalJSON() ([]byte, error) {
-	var buf bytes.Buffer
-
-	buf.WriteString("{")
-	for i, kv := range omap {
-		if i != 0 {
-			buf.WriteString(",")
-		}
-		// marshal key
-		key, err := json.Marshal(kv.Key)
-		if err != nil {
-			return nil, err
-		}
-		buf.Write(key)
-		buf.WriteString(":")
-		// marshal value
-		val, err := json.Marshal(kv.Val)
-		if err != nil {
-			return nil, err
-		}
-		buf.Write(val)
-	}
-
-	buf.WriteString("}")
-	return buf.Bytes(), nil
-}
-
 // HostResponse expected GetHost response type
 type ExecTypeResponse struct {
 	Outcome string `json:"outcome"`
