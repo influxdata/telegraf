e3bc188ce496ce9c1a519a49a737d9586b39c4ab
David Bennett <71459415+Jagularr@users.noreply.github.com>
2021-02-18 10:27:23 -0500

    Update mac-signing.sh

--- scripts/mac-signing.sh
+++ scripts/mac-signing.sh
@@ -1,8 +1,10 @@
+# Acquire the necessary certificates.
 base64 -D -o MacCertificate.p12 <<< $MacCertificate
 sudo security import MacCertificate.p12 -k /Library/Keychains/System.keychain -P $MacCertificatePassword -A
 base64 -D -o AppleSigningAuthorityCertificate.cer <<< $AppleSigningAuthorityCertificate
 sudo security import AppleSigningAuthorityCertificate.cer -k '/Library/Keychains/System.keychain' -A
 
+# Extract the built mac binary and sign it.
 cd dist
 tar -xzvf $(find . -name "*darwin_amd64.tar*")
 rm $(find . -name "*darwin_amd64.tar*")
@@ -11,13 +13,16 @@ cd usr/bin
 codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime telegraf
 codesign -v telegraf
 
+# Reset back out to the main directory.
 cd
 cd project/dist
 extractedFolder=$(find . -name "*telegraf-*" -type d)
 
+# Sign the 'telegraf entry' script, which is required to open Telegraf upon opening the .app bundle.
 codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime ../scripts/telegraf_entry_mac
 codesign -v ../scripts/telegraf_entry_mac
 
+# Create the .app bundle.
 mkdir Telegraf
 cd Telegraf
 mkdir Contents
@@ -29,12 +34,15 @@ cp ../scripts/info.plist Telegraf/Contents
 cp -R "$extractedFolder"/ Telegraf/Contents/Resources
 cp ../scripts/telegraf_entry_mac Telegraf/Contents/MacOS
 cp ../assets/icon.icns Telegraf/Contents/Resources
+chmod +x Telegraf/Contents/MacOS/telegraf_entry_mac
 mv Telegraf Telegraf.app
 
+# Sign the entire .app bundle, and wrap it in a DMG.
 codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime --deep --force Telegraf.app
 hdiutil create -size 500m -volname Telegraf -srcfolder Telegraf.app telegraf.dmg
 codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime telegraf.dmg
 
+# Send the DMG to be notarized.
 uuid=$(xcrun altool --notarize-app --primary-bundle-id "com.influxdata.telegraf" --username "$AppleUsername" --password "$ApplePassword" --file telegraf.dmg | awk '/RequestUUID/ { print $NF; }')
 echo $uuid
 if [[ $uuid == "" ]]; then 
@@ -42,12 +50,11 @@ if [[ $uuid == "" ]]; then
   exit 1
 fi
 
-# wait for status to be not "in progress" any more
+# Wait until the status returns something other than 'in progress'.
 request_status="in progress"
 while [[ "$request_status" == "in progress" ]]; do
   sleep 10
   request_status=$(xcrun altool --notarization-info $uuid --username "$AppleUsername" --password "$ApplePassword" 2>&1 | awk -F ': ' '/Status:/ { print $2; }' )
-  echo $(xcrun altool --notarization-info $uuid --username "$AppleUsername" --password "$ApplePassword")
 done
 
 if [[ $request_status != "success" ]]; then
@@ -55,9 +62,10 @@ if [[ $request_status != "success" ]]; then
   exit 1
 fi
 
-echo "Signed and notarized!"
-
+# Attach the notarization to the DMG.
 xcrun stapler staple telegraf.dmg
-rm Telegraf.app
+rm -rf Telegraf.app
 rm -rf $extractedFolder
 ls
+
+echo "Signed and notarized!"
