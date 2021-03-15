a018801c5d1f110056f693b794fc8a0f1dad915b
Sebastian Spaink <sspaink@influxdata.com>
2021-03-15 12:12:50 -0500

    bug fixes

--- scripts/mac_installgo.sh
+++ scripts/mac_installgo.sh
@@ -1,11 +1,12 @@
+# Install Go directly from tar, the reason we aren't using brew: it is slow to update and we can't pull specific minor versions
 install_go() {
     echo "installing go"
-    curl https://golang.org/dl/go1.16.2.darwin-amd64.tar.gz
+    curl https://golang.org/dl/go1.16.2.darwin-amd64.tar.gz --output go1.16.2.darwin-amd64.tar.gz
     rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.2.linux-amd64.tar.gz
     brew link --force --overwrite go@16
 }
 
-if [-d /usr/local/go]; then
+if [ -d /usr/local/go ]; then
     echo "Go is already installed"
     v=`go version | { read _ _ v _; echo ${v#go}; }`
     echo "$v is installed"
