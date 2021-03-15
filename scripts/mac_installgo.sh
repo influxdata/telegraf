aff9e441caa2a17199c7f7f9f853e6ce62e7824b
Sebastian Spaink <sspaink@influxdata.com>
2021-03-15 17:48:02 -0500

    trying linking and unlinking

--- scripts/mac_installgo.sh
+++ scripts/mac_installgo.sh
@@ -6,8 +6,7 @@ version="1.16.2"
 install_go () {
     echo "installing go"
     curl -OL https://golang.org/dl/go${version}.darwin-amd64.tar.gz --output go${version}.darwin-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /tmp/ -xzf go${version}.darwin-amd64.tar.gz
-    ln -sf /tmp/go/bin/go /usr/local/bin
+    rm -rf /usr/local/go && tar -C /tmp/ -xzf go${version}.darwin-amd64.tar.g
 }
 
 if command -v go &> /dev/null; then
