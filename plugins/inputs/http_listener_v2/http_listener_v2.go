2eba0b783719329780dce5b2bc7158c300ae0e4c
helenosheaa <38860767+helenosheaa@users.noreply.github.com>
2021-03-17 14:16:02 -0400

    defer linter err

--- plugins/inputs/http_listener_v2/http_listener_v2.go
+++ plugins/inputs/http_listener_v2/http_listener_v2.go
@@ -267,8 +267,8 @@ func (h *HTTPListenerV2) collectBody(res http.ResponseWriter, req *http.Request)
 		}
 		return bytes, true
 	case "snappy":
+		defer req.Body.Close()
 		bytes, err := ioutil.ReadAll(req.Body)
-		req.Body.Close()
 		if err != nil {
 			h.Log.Debug(err.Error())
 			badRequest(res)
@@ -283,8 +283,8 @@ func (h *HTTPListenerV2) collectBody(res http.ResponseWriter, req *http.Request)
 		}
 		return bytes, true
 	default:
+		defer req.Body.Close()
 		bytes, err := ioutil.ReadAll(req.Body)
-		req.Body.Close()
 		if err != nil {
 			h.Log.Debug(err.Error())
 			badRequest(res)
