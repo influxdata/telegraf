2fb25fb81c8d07c903c11f39ace425c765653982
John Seekins <jseekins@datto.com>
2021-04-07 13:18:20 -0600

    don't restrain build

--- plugins/inputs/mdstat/mdstat.go
+++ plugins/inputs/mdstat/mdstat.go
@@ -1,5 +1,3 @@
-// +build linux
-
 // Copyright 2018 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
