6711648544aa5d7e5b18cfefd40f720129e7e9b2
John Seekins <jseekins@datto.com>
2021-04-07 10:44:31 -0600

    add mdadm to plugin registry

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -99,6 +99,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/mailchimp"
 	_ "github.com/influxdata/telegraf/plugins/inputs/marklogic"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mcrouter"
+	_ "github.com/influxdata/telegraf/plugins/inputs/mdadm"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mem"
 	_ "github.com/influxdata/telegraf/plugins/inputs/memcached"
 	_ "github.com/influxdata/telegraf/plugins/inputs/mesos"
