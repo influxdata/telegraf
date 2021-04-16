57c998afb12c85025c3128bac198921296e4593d
Sebastian Spaink <sspaink@influxdata.com>
2021-04-16 14:30:12 -0500

    Move to separate file

---
+++ scripts/check-file-changes.sh
@@ -0,0 +1,5 @@
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+echo $BRANCH
+if [[ "$BRANCH" != "master" ]]; then
+    git diff master --name-only --no-color | egrep -e "^(\.circleci\/.*)|(.*\.(go|mod|sum))|Makefile$" || circleci step halt;
+fi
