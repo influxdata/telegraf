dd08bc2b351def48854994092e7266c52b226d64
David Bennett <david.bennett17@gmail.com>
2021-04-20 12:49:30 -0400

    reset not included changes

--- plugins/inputs/http/http.go
+++ plugins/inputs/http/http.go
@@ -52,33 +52,45 @@ var sampleConfig = `
   urls = [
     "http://localhost/metrics"
   ]
+
   ## HTTP method
   # method = "GET"
+
   ## Optional HTTP headers
   # headers = {"X-Special-Header" = "Special-Value"}
+
   ## Optional file with Bearer token
   ## file content is added as an Authorization header
   # bearer_token = "/path/to/file"
+
   ## Optional HTTP Basic Auth Credentials
   # username = "username"
   # password = "pa$$word"
+
   ## HTTP entity-body to send with POST/PUT requests.
   # body = ""
+
   ## HTTP Content-Encoding for write request body, can be set to "gzip" to
   ## compress body or "identity" to apply no encoding.
   # content_encoding = "identity"
+
   ## HTTP Proxy support
   # http_proxy_url = ""
+
   ## Optional TLS Config
   # tls_ca = "/etc/telegraf/ca.pem"
   # tls_cert = "/etc/telegraf/cert.pem"
   # tls_key = "/etc/telegraf/key.pem"
+
   ## Use TLS but skip chain & host verification
   # insecure_skip_verify = false
+
   ## Amount of time allowed to complete the HTTP request
   # timeout = "5s"
+
   ## List of success status codes
   # success_status_codes = [200]
+  
   ## Data format to consume.
   ## Each data format has its own unique set of configuration options, read
   ## more about them here:
