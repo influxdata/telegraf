ac3a638135916df0a1a92d1066ea2a9050a726fe
Greg Linton <greg.linton@gmail.com>
2021-05-13 17:26:50 +0000

    Updated the value parser to remove any starting tildes.

--- plugins/serializers/graphite/graphite.go
+++ plugins/serializers/graphite/graphite.go
@@ -326,9 +326,8 @@ func strictSanitize(value string) string {
 }
 
 func compatibleSanitize(name string, value string) string {
-	if strings.HasPrefix(value, "~") {
-		value = value[1:]
-	}
+	value = regexp.MustCompile(`^[~]+(.*)`).FindStringSubmatch(value)[1]
+
 	name = compatibleAllowedCharsName.ReplaceAllLiteralString(name, "_")
 	value = compatibleAllowedCharsValue.ReplaceAllLiteralString(value, "_")
 	return name + "=" + value
