2f4acdc4b86b477d5ac26d643d8cd8f61c9ead64
Alex Krantz <alex@krantz.dev>
2021-05-18 14:17:37 -0700

    Remove unnecessary parsing

--- config/config.go
+++ config/config.go
@@ -50,10 +50,6 @@ var (
 		`\`, `\\`,
 	)
 	httpLoadConfigRetryInterval = 10 * time.Second
-
-	// fetchURLRe is a regex to determine whether the requested file should
-	// be fetched from a remote or read from the filesystem.
-	fetchURLRe = regexp.MustCompile(`^\w+://`)
 )
 
 // Config specifies the URL/user/password for the database that telegraf
@@ -906,18 +902,20 @@ func escapeEnv(value string) string {
 }
 
 func loadConfig(config string) ([]byte, error) {
-	if fetchURLRe.MatchString(config) {
-		u, err := url.Parse(config)
-		if err != nil {
-			return nil, err
-		}
+	u, err := url.Parse(config)
+	if err != nil {
+		return nil, err
+	}
 
-		switch u.Scheme {
-		case "https", "http":
-			return fetchConfig(u)
-		default:
-			// If it isn't a https or file scheme, try it as a file
-		}
+	if u.Host == "" {
+		return ioutil.ReadFile(config)
+	}
+
+	switch u.Scheme {
+	case "https", "http":
+		return fetchConfig(u)
+	default:
+		// If it isn't a https or file scheme, try it as a file
 	}
 
 	return ioutil.ReadFile(config)
