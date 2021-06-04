46f3b3623bd44c12904b13e797e23f931cf234cb
Alex Krantz <alex@krantz.dev>
2021-06-04 12:06:28 -0700

    Add negative test case

--- plugins/common/auth/basic_auth_test.go
+++ plugins/common/auth/basic_auth_test.go
@@ -22,3 +22,12 @@ func TestBasicAuth_VerifyWithoutCredentials(t *testing.T) {
 
 	require.True(t, auth.Verify(r))
 }
+
+func TestBasicAuth_VerifyWithInvalidCredentials(t *testing.T) {
+	auth := BasicAuth{"username", "password"}
+
+	r := httptest.NewRequest("GET", "/github", nil)
+	r.SetBasicAuth("wrong-username", "wrong-password")
+
+	require.False(t, auth.Verify(r))
+}
