4c79964f896c481d2083b909af50180a245d51d1
Sebastian Spaink <3441183+sspaink@users.noreply.github.com>
2021-06-09 17:00:44 -0500

    Update scripts/check-file-changes.sh
    Co-authored-by: reimda <reimda@users.noreply.github.com>

--- scripts/check-file-changes.sh
+++ scripts/check-file-changes.sh
@@ -5,7 +5,7 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 echo $BRANCH
 if [[ "$BRANCH" != "master" ]] && [[ "$BRANCH" != release* ]]; then # This should never skip for master and release branches
     # Ask git for all the differences between this branch and master
-    # Then use grep to look for changes in the .circleci/ directory, anything named *.go* or *.mod* or *.sum* or *.sh* or *Makefile
+    # Then use grep to look for changes in the .circleci/ directory, anything named *.go or *.mod or *.sum or *.sh or Makefile
     # If no match is found, then circleci step halt will stop the CI job but mark it successful
     git diff master --name-only --no-color | egrep -e "^(\.circleci\/.*)$|^(.*\.(go|mod|sum|sh))$|^Makefile$" || circleci step halt;
 fi
