64b7ccfd1b9cd3945c41d837fee12797aff6ad7a
Sebastian Spaink <sspaink@influxdata.com>
2021-06-08 20:03:15 -0500

    Missing space

--- scripts/check-file-changes.sh
+++ scripts/check-file-changes.sh
@@ -2,6 +2,6 @@
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 echo $BRANCH
-if [[ "$BRANCH" != "master" ]] && [[ "$BRANCH"!= release* ]]; then
+if [[ "$BRANCH" != "master" ]] && [[ "$BRANCH" != release* ]]; then
     git diff master --name-only --no-color | egrep -e "^(\.circleci\/.*)|(.*\.(go|mod|sum|sh))|Makefile$" || circleci step halt;
 fi
