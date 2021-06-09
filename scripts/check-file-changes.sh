04cd729f627f1986670f9e918f603dba331c8765
Sebastian Spaink <sspaink@influxdata.com>
2021-06-08 19:58:37 -0500

    Fix mac install go script

--- scripts/check-file-changes.sh
+++ scripts/check-file-changes.sh
@@ -2,6 +2,6 @@
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 echo $BRANCH
-if [[ "$BRANCH" != "master" ]]; then
-    git diff master --name-only --no-color | egrep -e "^(\.circleci\/.*)|(.*\.(go|mod|sum))|Makefile$" || circleci step halt;
+if [[ "$BRANCH" != "master" ]] || [[ "$BRANCH" = release* ]]; then
+    git diff master --name-only --no-color | egrep -e "^(\.circleci\/.*)|(.*\.(go|mod|sum|sh))|Makefile$" || circleci step halt;
 fi
