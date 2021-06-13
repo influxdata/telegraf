932bf125a17d430fc3ea28faadaf6253a3b37494
Sven Rebhan <sven.rebhan@honda-ri.de>
2021-06-13 21:28:40 +0200

    Bump postgres version v4 to use what others use.

--- plugins/inputs/sql/drivers.go
+++ plugins/inputs/sql/drivers.go
@@ -4,5 +4,5 @@ import (
 	// Blank imports to register the drivers
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/jackc/pgx"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
