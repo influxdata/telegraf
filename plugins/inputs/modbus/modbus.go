77137efbcc6376c51f0c162043ecd03bb8e52151
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2021-07-12 14:38:16 +0200

    Fix whitespaces in sample-config.
    Co-authored-by: Thomas Casteleyn <thomas.casteleyn@me.com>

--- plugins/inputs/modbus/modbus.go
+++ plugins/inputs/modbus/modbus.go
@@ -116,11 +116,11 @@ const sampleConfig = `
   # workarounds = {
       ## Pause between read requests sent to the device. This might be necessary for (slow) serial devices.
       # pause_between_requests = "0ms"
-			## Close the connection after every gather cycle. Usually the plugin closes the connection after a certain
-			## idle-timeout, however, if you query a device with limited simultaneous connectivity (e.g. serial devices)
+      ## Close the connection after every gather cycle. Usually the plugin closes the connection after a certain
+      ## idle-timeout, however, if you query a device with limited simultaneous connectivity (e.g. serial devices)
       ## from multiple instances you might want to only stay connected during gather and disconnect afterwards.
-			# close_connection_after_gather = false
-	# }
+      # close_connection_after_gather = false
+    # }
 
 
   ## For Modbus over TCP you can choose between "TCP", "RTUoverTCP" and "ASCIIoverTCP"
