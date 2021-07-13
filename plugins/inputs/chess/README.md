641189e3958894ead17c5595fbccb0800117eab6
MyaLongmire <myalongmire05@gmail.com>
2021-07-13 11:52:49 -0600

    added readme

---
+++ plugins/inputs/chess/README.md
@@ -0,0 +1,25 @@
+# Chess Input Plugin
+
+The `chess` plugin gathers metrics from chess.com on specific players, 
+such as: online status, general stats, match data, and regional 
+distribution of players and clubs.
+
+
+### Configuration
+
+```toml
+[[inputs.chess]]
+# A list of profiles for monotoring 
+  profiles = ["username1", "username2"]
+```
+
+### Troubleshooting
+
+Check that the username is spelt correctly. 
+
+### Example Output
+
+```
+measurement1,tag1=foo,tag2=bar field1=1i,field2=2.1 1453831884664956455
+measurement2,tag1=foo,tag2=bar,tag3=baz field3=1i 1453831884664956455
+```
--- plugins/inputs/chess/chess.go
+++ plugins/inputs/chess/chess.go
@@ -1,6 +1,8 @@
 package chess
 
 import (
+	"fmt"
+
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
@@ -9,32 +11,37 @@ import (
 
 // chess is the plugin type
 type Chess struct {
-	Ok  bool            `toml:"ok"`
-	Log telegraf.Logger `toml:"-"`
+	Profiles []string        `toml:"profiles"`
+	Log      telegraf.Logger `toml:"-"`
 }
 
+const SampleConfig = `
+  # A list of profiles for monotoring 
+  profiles = ["username1", "username2"]
+`
+
 func (c *Chess) Description() string {
-	return "A description of the chess plugin..."
+	return "Monitor profiles from chess.com"
 }
 
 func (c *Chess) SampleConfig() string {
-	return `
-	## Return a sample configuration for plugin...
-	ok = true
-`
+	return SampleConfig
 }
 
 // Init is a method that sets up and validates the config
 func (c *Chess) Init() error {
+	if c.Profiles == nil {
+		return fmt.Errorf("no profiles listed in the config")
+	}
 	return nil
 }
 
 func (c *Chess) Gather(acc telegraf.Accumulator) error {
-	if c.Ok {
-		acc.AddFields("state", map[string]interface{}{"value": "pretty good"}, nil)
-	} else {
-		acc.AddFields("state", map[string]interface{}{"value": "not great"}, nil)
-	}
+	// if c.Ok {
+	// 	acc.AddFields("state", map[string]interface{}{"value": "pretty good"}, nil)
+	// } else {
+	// 	acc.AddFields("state", map[string]interface{}{"value": "not great"}, nil)
+	// }
 	return nil
 }
 
