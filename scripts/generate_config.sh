19404bd34672491389be93b14cbe90adc93e6d0e
Sebastian Spaink <sspaink@influxdata.com>
2021-09-14 13:21:34 -0500

    fix: path issues persist

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -8,14 +8,14 @@ config_name="telegraf.conf"
 
 if [ "$os" = "windows" ]; then
     zip=$(/bin/find ./build/dist -maxdepth 1 -name "*windows_amd64.zip" -print)
-    exe_path="./build/extracted"
-    unzip "$zip" -d $exe_path
+    exe_path="$PWD/build/extracted"
+    unzip "$zip" -d "$exe_path"
     telegraf="telegraf.exe"
     config_name="telegraf_windows.conf"
     exe_path="$exe_path/telegraf*"
 else
     tar=$(find /build/dist -maxdepth 1 -name "*linux_amd64.tar.gz" -print)
-    tar -xf "$tar" -c "$exe_path"
+    tar -xfc "$tar" "$exe_path"
     exe_path="$exe_path./telegraf*/usr/bin"
 fi
 
