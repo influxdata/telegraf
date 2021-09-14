e7147e6937883f8012be8e5837fbab24e8b94682
Sebastian Spaink <sspaink@influxdata.com>
2021-09-14 11:37:51 -0500

    fix: typo

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 os=$1 # windows or linux
-extracted_path="/build/extracted" # Path will contain telegraf binary
+exe_path="/build/extracted" # Path will contain telegraf binary
 
 telegraf="telegraf"
 config_name="telegraf.conf"
 
 if [ "$os" = "windows" ]; then
     zip=$(/bin/find ./build/dist -maxdepth 1 -name "*windows_amd64.zip" -print)
-    extracted_path="./build/extracted"
-    unzip "$zip" -d $extracted_path
+    exe_path="./build/extracted"
+    unzip "$zip" -d $exe_path
     telegraf="telegraf.exe"
     config_name="telegraf_windows.conf"
-    extracted_path="$extracted_path/telegraf*"
+    exe_path="$exe_path/telegraf*"
 else
     tar=$(find /build/dist -maxdepth 1 -name "*linux_amd64.tar.gz" -print)
-    tar -xf "$tar" -c "$extracted_path"
-    extracted_path="$extracted_path./telegraf*/usr/bin"
+    tar -xf "$tar" -c "$exe_path"
+    exe_path="$exe_path./telegraf*/usr/bin"
 fi
 
-cd "$extracted_path" || exit
+cd "$exe_path" || exit
 ls
 ./exe_path/$telegraf config > $config_name
 
