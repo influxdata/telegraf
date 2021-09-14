ad175d6d58fc8c8aff00b1036f5ded372f5d242d
Sebastian Spaink <sspaink@influxdata.com>
2021-09-14 10:41:26 -0500

    fix: need to use /bin/find in win

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -2,13 +2,13 @@
 
 os=$1 # windows or linux
 artifact_dir="./build/dist" # Path is from .circleci/config.yml
-extracted_path=$artifact_dir+"/extracted" # Path will contain telegraf binary
+extracted_path=$artifact_dir"/extracted" # Path will contain telegraf binary
 
 telegraf="telegraf"
 config_name="telegraf.conf"
 
 if [ "$os" = "windows" ]; then
-    zip=$(find $artifact_dir -maxdepth 1 -name "*windows_amd64.zip" -print)
+    zip=$(/bin/find $artifact_dir -maxdepth 1 -name "*windows_amd64.zip" -print)
     unzip "$zip" -d $extracted_path
     telegraf="telegraf.exe"
     config_name="telegraf_windows.conf"
--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -6,9 +6,15 @@ config_path="/new-config"
 
 if [ ! -f "$config_path/telegraf.conf" ]; then
     echo "$config_path/telegraf.conf does not exist"
+    exit
 fi
 if [ ! -f "$config_path/telegraf_windows.conf" ]; then
-    echo "$config_path/telegraf.conf does not exist"
+    echo "$config_path/telegraf_windows.conf does not exist"
+    exit
+fi
+
+if cmp -s "$config_path/telegraf.conf" "etc/telegraf.conf" && cmp -s "$config_path/telegraf_windows.conf" "etc/telegraf_windows.conf"; then
+    echo "Both telegraf.conf and telegraf_windows.conf haven't changed"
 fi
 
 # curl -H "Authorization: Bearer $token" -X POST "https://182c7jdgog.execute-api.us-east-1.amazonaws.com/prod/updateconfig"
