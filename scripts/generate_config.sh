f4f6af873dacf5aff91ec1cccea187085d1f4088
Sebastian Spaink <sspaink@influxdata.com>
2021-09-14 11:35:23 -0500

    fix: path issues

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 os=$1 # windows or linux
-artifact_dir="./build/dist" # Path is from .circleci/config.yml
-extracted_path=$artifact_dir"/extracted" # Path will contain telegraf binary
+extracted_path="/build/extracted" # Path will contain telegraf binary
 
 telegraf="telegraf"
 config_name="telegraf.conf"
 
 if [ "$os" = "windows" ]; then
-    zip=$(/bin/find $artifact_dir -maxdepth 1 -name "*windows_amd64.zip" -print)
+    zip=$(/bin/find ./build/dist -maxdepth 1 -name "*windows_amd64.zip" -print)
+    extracted_path="./build/extracted"
     unzip "$zip" -d $extracted_path
     telegraf="telegraf.exe"
     config_name="telegraf_windows.conf"
+    extracted_path="$extracted_path/telegraf*"
 else
-    tar=$(find $artifact_dir -maxdepth 1 -name "*linux_amd64.tar.gz" -print)
-    tar -xf "$tar" -c $extracted_path
+    tar=$(find /build/dist -maxdepth 1 -name "*linux_amd64.tar.gz" -print)
+    tar -xf "$tar" -c "$extracted_path"
+    extracted_path="$extracted_path./telegraf*/usr/bin"
 fi
 
-cd $extracted_path || exit
-./$telegraf config > $config_name
+cd "$extracted_path" || exit
+ls
+./exe_path/$telegraf config > $config_name
 
 mkdir ./new-config
 mv $config_name ./new-config
