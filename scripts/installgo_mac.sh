b959af8a66e45f66f8f18fde11ec245a50283438
Alan Pope <alan@popey.com>
2021-10-22 12:35:02 +0100

    Allow for non x86 macs
    New macs have arm64 processors. This PR simply modifies the helper script to install the correct architecture build of go on an arm64 (so-called M1/M1 Max/M1 Pro) Apple computer.

--- scripts/installgo_mac.sh
+++ scripts/installgo_mac.sh
@@ -2,9 +2,15 @@
 
 set -eux
 
-GO_ARCH="darwin-amd64"
+ARCH=$(uname -m)
 GO_VERSION="1.17.2"
-GO_VERSION_SHA="7914497a302a132a465d33f5ee044ce05568bacdb390ab805cb75a3435a23f94" # from https://golang.org/dl
+if [ "$ARCH" = 'arm64' ]; then
+    GO_ARCH="darwin-arm64"
+    GO_VERSION_SHA="ce8771bd3edfb5b28104084b56bbb532eeb47fbb7769c3e664c6223712c30904" # from https://golang.org/dl
+elif [ "$ARCH" = 'x86_64' ]; then
+    GO_ARCH="darwin-amd64"
+    GO_VERSION_SHA="7914497a302a132a465d33f5ee044ce05568bacdb390ab805cb75a3435a23f94" # from https://golang.org/dl
+fi
 
 # This path is cachable. (Saving in /usr/local/ would cause issues restoring the cache.)
 path="/usr/local/Cellar"
