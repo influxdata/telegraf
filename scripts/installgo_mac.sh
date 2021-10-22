e63ad3089ddeae94523aca7c0dce65fa502059a1
Alan Pope <alan@popey.com>
2021-10-22 12:53:52 +0100

    Update installgo_mac.sh

--- scripts/installgo_mac.sh
+++ scripts/installgo_mac.sh
@@ -14,6 +14,7 @@ fi
 
 # This path is cachable. (Saving in /usr/local/ would cause issues restoring the cache.)
 path="/usr/local/Cellar"
+sudo mkdir -p ${path}
 
 # Download Go and verify Go tarball. (Note: we aren't using brew because
 # it is slow to update and we can't pull specific minor versions.)
@@ -27,8 +28,9 @@ setup_go () {
 
     sudo rm -rf ${path}/go
     sudo tar -C $path -xzf go${GO_VERSION}.${GO_ARCH}.tar.gz
-    ln -sf ${path}/go/bin/go /usr/local/bin/go
-    ln -sf ${path}/go/bin/gofmt /usr/local/bin/gofmt
+    sudo mkdir -p /usr/local/bin
+    sudo ln -sf ${path}/go/bin/go /usr/local/bin/go
+    sudo ln -sf ${path}/go/bin/gofmt /usr/local/bin/gofmt
 }
 
 if command -v go >/dev/null 2>&1; then
