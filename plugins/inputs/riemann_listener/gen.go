4ecc9d4f3992e294b0567ab21575fff2f351e6ca
DStrand1 <dstrandboge@influxdata.com>
2021-10-28 14:02:18 -0500

    build: move to new protobuf library

---
+++ plugins/inputs/riemann_listener/gen.go
@@ -0,0 +1,8 @@
+package riemann_listener
+
+// To run this command, make sure that protoc-gen-go is installed
+// > go install google.golang.org/protobuf/cmd/protoc-gen-go
+//
+// Generated file was last generated with:
+// - protoc-gen-go: v1.27.1
+//go:generate protoc --go_out=proto/ proto/riemann.proto
