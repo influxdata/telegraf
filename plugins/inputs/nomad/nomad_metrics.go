46f434dfd250771691290323ff337e16a3d32466
efbar <17676301+efbar@users.noreply.github.com>
2021-11-17 18:26:33 +0100

    better json parsing

--- plugins/inputs/nomad/nomad_metrics.go
+++ plugins/inputs/nomad/nomad_metrics.go
@@ -5,49 +5,49 @@ import (
 )
 
 type MetricsSummary struct {
-	Timestamp string
-	Gauges    []GaugeValue
-	Points    []PointValue
-	Counters  []SampledValue
-	Samples   []SampledValue
+	Timestamp string         `json:"timestamp"`
+	Gauges    []GaugeValue   `json:"gauges"`
+	Points    []PointValue   `json:"points"`
+	Counters  []SampledValue `json:"counters"`
+	Samples   []SampledValue `json:"samples"`
 }
 
 type GaugeValue struct {
-	Name  string
-	Hash  string `json:"-"`
-	Value float32
+	Name  string  `json:"name"`
+	Hash  string  `json:"-"`
+	Value float32 `json:"value"`
 
 	Labels        []Label           `json:"-"`
 	DisplayLabels map[string]string `json:"Labels"`
 }
 
 type PointValue struct {
-	Name   string
-	Points []float32
+	Name   string    `json:"name"`
+	Points []float32 `json:"points"`
 }
 
 type SampledValue struct {
-	Name string
+	Name string `json:"name"`
 	Hash string `json:"-"`
 	*AggregateSample
-	Mean   float64
-	Stddev float64
+	Mean   float64 `json:"mean"`
+	Stddev float64 `json:"stddev"`
 
 	Labels        []Label           `json:"-"`
 	DisplayLabels map[string]string `json:"Labels"`
 }
 
 type AggregateSample struct {
-	Count       int
-	Rate        float64
-	Sum         float64
-	SumSq       float64 `json:"-"`
-	Min         float64
-	Max         float64
+	Count       int       `json:"count"`
+	Rate        float64   `json:"rate"`
+	Sum         float64   `json:"sum"`
+	SumSq       float64   `json:"-"`
+	Min         float64   `json:"min"`
+	Max         float64   `json:"max"`
 	LastUpdated time.Time `json:"-"`
 }
 
 type Label struct {
-	Name  string
-	Value string
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
