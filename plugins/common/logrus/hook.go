3627961add0118777c14d5e49140f75946c9c287
Paweł Żak <pawel.zak.pawel@gmail.com>
2021-11-30 22:46:46 +0100

    fix: Linter fixes for plugins/common/[a-z]* (#10189)
    Co-authored-by: Pawel Zak <Pawel Zak>

--- plugins/common/logrus/hook.go
+++ plugins/common/logrus/hook.go
@@ -14,7 +14,7 @@ var once sync.Once
 type LogHook struct {
 }
 
-// Install a logging hook into the logrus standard logger, diverting all logs
+// InstallHook installs a logging hook into the logrus standard logger, diverting all logs
 // through the Telegraf logger at debug level.  This is useful for libraries
 // that directly log to the logrus system without providing an override method.
 func InstallHook() {
