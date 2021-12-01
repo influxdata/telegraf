b29111d505bd77214695d6a74d5dda042ccac474
Sven Rebhan <sven.rebhan@honda-ri.de>
2021-12-01 18:03:50 +0100

    Fix backward-compatible initialization with the old way for CSV.

--- plugins/parsers/csv/parser.go
+++ plugins/parsers/csv/parser.go
@@ -325,3 +325,23 @@ func init() {
 			return &Parser{MetricName: defaultMetricName}
 		})
 }
+
+func (p *Parser) InitFromConfig(config *parsers.Config) error {
+	p.HeaderRowCount = config.CSVHeaderRowCount
+	p.SkipRows = config.CSVSkipRows
+	p.SkipColumns = config.CSVSkipColumns
+	p.Delimiter = config.CSVDelimiter
+	p.Comment = config.CSVComment
+	p.TrimSpace = config.CSVTrimSpace
+	p.ColumnNames = config.CSVColumnNames
+	p.ColumnTypes = config.CSVColumnTypes
+	p.TagColumns = config.CSVTagColumns
+	p.MeasurementColumn = config.CSVMeasurementColumn
+	p.TimestampColumn = config.CSVTimestampColumn
+	p.TimestampFormat = config.CSVTimestampFormat
+	p.Timezone = config.CSVTimezone
+	p.DefaultTags = config.DefaultTags
+	p.SkipValues = config.CSVSkipValues
+
+	return p.Init()
+}
