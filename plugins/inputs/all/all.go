dbf55535c753f2b15dadd6e2ac613a45eb71d87d
Francesco Bartolini <17676301+efbar@users.noreply.github.com>
2021-12-08 17:34:11 +0100

    feat: add nomad input plugin (#10106)

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -128,6 +128,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_sts"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_upstream_check"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nginx_vts"
+	_ "github.com/influxdata/telegraf/plugins/inputs/nomad"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nsd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nsq"
 	_ "github.com/influxdata/telegraf/plugins/inputs/nsq_consumer"
