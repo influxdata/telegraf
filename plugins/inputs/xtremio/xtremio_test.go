c30af6bfcc729e8dec7e123f335225148df7da25
Chad Thielen <cthiel42>
2021-12-08 21:43:55 -0600

    update test cases to store data in new file and use correct type for change in input

--- plugins/inputs/xtremio/xtremio_test.go
+++ plugins/inputs/xtremio/xtremio_test.go
@@ -118,9 +118,9 @@ func TestFixedValue(t *testing.T) {
 					map[string]interface{}{
 						"bbus_power":                        244,
 						"bbus_average_daily_temp":           23,
-						"bbus_enabled":                      1,
-						"bbus_ups_need_battery_replacement": 0,
-						"bbus_ups_low_battery_no_input":     0,
+						"bbus_enabled":                      true,
+						"bbus_ups_need_battery_replacement": false,
+						"bbus_ups_low_battery_no_input":     false,
 					},
 					time.Unix(0, 0),
 				),
@@ -181,54 +181,3 @@ func TestAuthenticationFailed(t *testing.T) {
 		})
 	}
 }
-
-var authenticateResponse = `
-{
-	"loginProviderName": "tmos",
-	"token": {
-		"token": "FUL2V33SRR2JBF4NKKABCDEFGH",
-		"name": "FUL2V33SRR2JBF4NKKABCDEFGH"
-	}
-}
-`
-
-var sampleBBUResponseOne = `
-{
-    "content": {
-        "is-low-battery-has-input": "false",
-        "serial-number": "A123B45678",
-        "guid": "987654321abcdef",
-        "brick-name": "X1",
-        "ups-battery-charge-in-percent": 100,
-        "power": 244,
-        "avg-daily-temp": 23,
-        "fw-version": "01.02.0034",
-        "sys-name": "ABCXIO001",
-		"power-feed": "PWR-A",
-        "ups-load-in-percent": 21,
-        "name": "X1-BBU",
-		"enabled-state": "enabled",
-        "is-low-battery-no-input": "false",
-        "ups-need-battery-replacement": "false",
-        "model-name": "Eaton Model Name",
-    }
-}
-`
-
-var sampleGetBBUsResponse = `
-{
-    "bbus": [
-        {
-            "href": "https://127.0.0.1/api/json/v3/types/bbus/987654321abcdef", 
-            "name": "X1-BBU", 
-            "sys-name": "ABCXIO001"
-        }
-    ], 
-    "links": [
-        {
-            "href": "https://127.0.0.1/api/json/v3/types/bbus/", 
-            "rel": "self"
-        }
-    ]
-}
-`
