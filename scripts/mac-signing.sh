64fd9b818a1e063b4753ce75e1aa437c2d6946b8
sspaink <sspaink@influxdata.com>
2021-12-16 18:04:04 -0600

    testing

--- scripts/mac-signing.sh
+++ scripts/mac-signing.sh
@@ -1,67 +1,63 @@
 #!/bin/bash
 
 function cleanup () {
+  echo "Cleaning up any existing Telegraf or Telegraf.app"
   rm -rf Telegraf
   rm -rf Telegraf.app
-  rm -rf "$extractedFolder"
 }
 
 # Acquire the necessary certificates.
+# MacCertificate, MacCertificatePassword, AppleSigningAuthorityCertificate are environment variables, to follow convention they should have been all caps.
 # shellcheck disable=SC2154
 base64 -D -o MacCertificate.p12 <<< "$MacCertificate"
+# shellcheck disable=SC2154
 sudo security import MacCertificate.p12 -k /Library/Keychains/System.keychain -P "$MacCertificatePassword" -A
+# shellcheck disable=SC2154
 base64 -D -o AppleSigningAuthorityCertificate.cer <<< "$AppleSigningAuthorityCertificate"
 sudo security import AppleSigningAuthorityCertificate.cer -k '/Library/Keychains/System.keychain' -A
 
 cd dist || exit
 # amdFile=$(find . -name "*darwin_amd64.tar*")
-armFile=$(find . -name "*darwin_arm64.tar*")
+armFile=$(find . -name "$HOME/project/dist/*darwin_arm64.tar*")
 macFiles=("${armFile}")
 
 
 for tarFile in "${macFiles[@]}";
 do
-  echo "Processing $tarFile"
-  # Extract the built mac binary and sign it.
-  extractedFolder="$(tar -tzf "$tarFile" | head -1 | cut -f1 -d"/")"
   cleanup
-  tar -xzf "$tarFile"
-  echo "$extractedFolder"
-  baseName=$(basename "$tarFile" .tar.gz)
-  echo "$baseName"
-  cd "$extractedFolder" || exit
-  cd usr/bin || exit
-  codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime telegraf
+  echo "Processing $tarFile"
+
+  # Create the .app bundle directory structure
+  mkdir -p Telegraf.app/Contents
+  mkdir -p Telegraf.app/Contents/MacOS
+  mkdir -p Telegraf.app/Contents/Resources
+
+  DeveloperID="Developer ID Application: InfluxData Inc. (M7DN9H35QT)"
+
+  # Sign telegraf binary and the telegraf_entry_mac script
+  tar -xzf "$tarFile" --strip-components=2 -C Telegraf.app/Contents/Resources telegraf-*
+  codesign -s "$DeveloperID" --timestamp --options=runtime Telegraf.app/Contents/Resources/usr/bin/telegraf
   codesign -v telegraf
 
-  # Reset back out to the main directory.
-  cd ~/project/dist || exit
-
-  # Sign the 'telegraf entry' script, which is required to open Telegraf upon opening the .app bundle.
-  codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime ../scripts/telegraf_entry_mac
+  cp ~/project/dist/scripts/telegraf_entry_mac Telegraf/Contents/MacOS
+  codesign -s "$DeveloperID" --timestamp --options=runtime Telegraf.app/Contents/MacOS/telegraf_entry_mac
   codesign -v ../scripts/telegraf_entry_mac
 
-  # Create the .app bundle.
-  mkdir Telegraf
-  cd Telegraf || exit
-  mkdir Contents
-  cd Contents || exit
-  mkdir MacOS
-  mkdir Resources
-  cd ../..
-  cp ../info.plist Telegraf/Contents
-  cp -R "$extractedFolder"/ Telegraf/Contents/Resources
-  cp ../scripts/telegraf_entry_mac Telegraf/Contents/MacOS
-  cp ../assets/icon.icns Telegraf/Contents/Resources
-  chmod +x Telegraf/Contents/MacOS/telegraf_entry_mac
-  mv Telegraf Telegraf.app
+  cp ~/project/dist/assets/info.plist Telegraf.app/Contents
+  cp  ~/project/dist/assets/icon.icns Telegraf.app/Contents/Resources
+
+  chmod +x Telegraf.app/Contents/MacOS/telegraf_entry_mac
 
   # Sign the entire .app bundle, and wrap it in a DMG.
-  codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime --deep --force Telegraf.app
+  codesign -s "$DeveloperID" --timestamp --options=runtime --deep --force Telegraf.app
+  baseName=$(basename "$tarFile" .tar.gz)
+  echo "$baseName"
   hdiutil create -size 500m -volname Telegraf -srcfolder Telegraf.app "$baseName".dmg
-  codesign -s "Developer ID Application: InfluxData Inc. (M7DN9H35QT)" --timestamp --options=runtime "$baseName".dmg
+  codesign -s "$DeveloperID" --timestamp --options=runtime "$baseName".dmg
 
   # Send the DMG to be notarized.
+  # AppleUsername and ApplePassword are environment variables, to follow convention they should have been all caps.
+  # shellcheck disable=SC2154
   uuid=$(xcrun altool --notarize-app --primary-bundle-id "com.influxdata.telegraf" --username "$AppleUsername" --password "$ApplePassword" --file "$baseName".dmg | awk '/RequestUUID/ { print $NF; }')
   echo "$uuid"
   if [[ $uuid == "" ]]; then
