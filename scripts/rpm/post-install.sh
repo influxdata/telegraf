174583e2ff01a89c51ca24cec2c9b7400399588e
Josh Powers <powersj@fastmail.com>
2022-02-02 08:32:20 -0700

    fix: use systemd detection in RPM install scripts
    Instead of having an ever growing list of distros and releases, this
    collapses the logic to switch based on whether systemd is detected or
    not.
    
    This also removes the sysv logic as all supported Fedora and RHEL
    distros are only systemd at this point. This does leave the check for
    systemd in place to ensure installs in containers work without errors.
    
    Fixes: #4507

--- scripts/rpm/post-install.sh
+++ scripts/rpm/post-install.sh
@@ -5,25 +5,6 @@ LOG_DIR=/var/log/telegraf
 SCRIPT_DIR=/usr/lib/telegraf/scripts
 LOGROTATE_DIR=/etc/logrotate.d
 
-function install_init {
-    cp -f $SCRIPT_DIR/init.sh /etc/init.d/telegraf
-    chmod +x /etc/init.d/telegraf
-}
-
-function install_systemd {
-    cp -f $SCRIPT_DIR/telegraf.service $1
-    systemctl enable telegraf || true
-    systemctl daemon-reload || true
-}
-
-function install_update_rcd {
-    update-rc.d telegraf defaults
-}
-
-function install_chkconfig {
-    chkconfig --add telegraf
-}
-
 # Remove legacy symlink, if it exists
 if [[ -L /etc/init.d/telegraf ]]; then
     rm -f /etc/init.d/telegraf
@@ -48,41 +29,14 @@ if [[ ! -f /etc/telegraf/telegraf.conf ]] && [[ -f /etc/telegraf/telegraf.conf.s
    cp /etc/telegraf/telegraf.conf.sample /etc/telegraf/telegraf.conf
 fi
 
+# Set up log directories
 test -d $LOG_DIR || mkdir -p $LOG_DIR
 chown -R -L telegraf:telegraf $LOG_DIR
 chmod 755 $LOG_DIR
 
-# Distribution-specific logic
-if [[ -f /etc/redhat-release ]] || [[ -f /etc/SuSE-release ]]; then
-    # RHEL-variant logic
-    if [[ "$(readlink /proc/1/exe)" == */systemd ]]; then
-        install_systemd /usr/lib/systemd/system/telegraf.service
-    else
-        # Assuming SysVinit
-        install_init
-        # Run update-rc.d or fallback to chkconfig if not available
-        if which update-rc.d &>/dev/null; then
-            install_update_rcd
-        else
-            install_chkconfig
-        fi
-    fi
-elif [[ -f /etc/os-release ]]; then
-    source /etc/os-release
-    if [[ "$NAME" = "Amazon Linux" ]]; then
-        # Amazon Linux 2+ logic
-        install_systemd /usr/lib/systemd/system/telegraf.service
-    elif [[ "$NAME" = "Amazon Linux AMI" ]]; then
-        # Amazon Linux logic
-        install_init
-        # Run update-rc.d or fallback to chkconfig if not available
-        if which update-rc.d &>/dev/null; then
-            install_update_rcd
-        else
-            install_chkconfig
-        fi
-    elif [[ "$NAME" = "Solus" ]]; then
-        # Solus logic
-        install_systemd /usr/lib/systemd/system/telegraf.service
-    fi
+# Set up systemd service
+if [[ -d /run/systemd/system ]]; then
+    cp -f $SCRIPT_DIR/telegraf.service /usr/lib/systemd/system/telegraf.service
+    systemctl enable telegraf || true
+    systemctl daemon-reload || true
 fi
