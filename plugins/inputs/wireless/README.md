2e888c5f94f7aee20d30caafe3507b3a7194559f
David G. Simmons <santafen@mac.com>
2022-03-03 11:53:39 -0500

    Auto stash before checking out "HEAD"

--- plugins/inputs/wireless/README.md
+++ plugins/inputs/wireless/README.md
@@ -29,6 +29,31 @@ The wireless plugin gathers metrics about wireless link quality by reading the `
     - misc (int64, packets, counter) - dropped for un-specified reason
     - missed_beacon (int64, packets, counter) - missed beacon packets
 
+
+### macOS metrics
+
+- metric
+  - tags:
+    - state (running or not)
+    - op_mode (operating mode: station, ad_hoc or ap)
+    - 802.11_auth (open or hidden)
+    - link_auth (authorization scheme)
+
+  - fields:
+    - BSSID (string, mac address, _only_ reported if run as `root`)
+    - SSID (string, The SSID of the network)
+    - agrCtlRSSI (int64, dBm, metric) - The current aggregate RSSI of the link
+    - agrExtRSSI (int64, dBm, metric) - The current aggregate external RSSI of the link
+    - agrCtlNoise (int64, dBm, metric) - The current aggregate noise of the link
+    - agrExtNoise (int64, dBm, metric) - The current aggregate external noise of the link
+    - lastTxRate (int64, Mbps, metric) - The last transmit rate
+    - maxRate (int64, Mbps, metric) - The maximum transmit rate
+    - lastAssocStatus (int64, metric) - The last association status
+    - MCS (int64, MCS, metric) - The last MCS
+    - guardInterval (int64, guard, metric) - The guard interval
+    - NSS (int64, NSS, metric) - The number of spatial streams
+    - channel (channel information)
+
 ## Example Output
 
 This section shows example output in Line Protocol format.
---
+++ plugins/inputs/wireless/wireless_mac.go
@@ -0,0 +1,58 @@
+//go:build darwin
+// +build darwin
+
+package wireless
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
+)
+
+// default executable path & flags
+const (
+	OSXCMD = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+	FLAG   = "-I"
+)
+
+func (w *Wireless) Gather(acc telegraf.Accumulator) error {
+	cmd := exec.Command(OSXCMD, FLAG)
+	wireless, err := internal.StdOutputTimeout(cmd, 2*time.Second)
+	if err != nil {
+		return err
+	}
+	metrics, tags := w.loadMacWirelessTable(wireless)
+	acc.AddFields("wireless", metrics, tags)
+	return nil
+}
+
+func (w *Wireless) loadMacWirelessTable(table []byte) (map[string]interface{}, map[string]string) {
+	lines := strings.Split(string(table), "\n")
+	tags := make(map[string]string)
+	fields := make(map[string]interface{})
+	for _, line := range lines {
+		fm := strings.SplitN(strings.TrimSpace(line), ":", 2)
+		if len(fm) < 2 || fm[1] == "" {
+			continue
+		}
+		name := strings.Replace(strings.Trim(strings.TrimSpace(fm[0]), ":"), " ", "_", -1)
+		v := strings.TrimSpace(fm[1])
+		switch name {
+		case "channel", "BSSID", "SSID":
+			fields[name] = strings.Replace(v, " ", "_", -1)
+		default:
+			if val, err := strconv.Atoi(v); err == nil {
+				// it's a number
+				fields[name] = int64(val)
+			} else {
+				// it's a string
+				tags[name] = strings.Replace(v, " ", "_", -1)
+			}
+		}
+	}
+	return fields, tags
+}
