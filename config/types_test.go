0cb490702ca0dd94d543981864e627c055435eee
Josh Powers <powersj@fastmail.com>
2022-03-10 16:15:05 -0700

    add test case for units only

--- config/types_test.go
+++ config/types_test.go
@@ -52,8 +52,10 @@ func TestDuration(t *testing.T) {
 	require.NoError(t, d.UnmarshalTOML([]byte(`1.5`)))
 	require.Equal(t, time.Second, time.Duration(d))
 
-	require.Error(t, d.UnmarshalTOML([]byte(`"1"`))) // string missing unit
-	require.Error(t, d.UnmarshalTOML([]byte(`'2'`))) // string missing unit
+	require.Error(t, d.UnmarshalTOML([]byte(`"1"`)))  // string missing unit
+	require.Error(t, d.UnmarshalTOML([]byte(`'2'`)))  // string missing unit
+	require.Error(t, d.UnmarshalTOML([]byte(`'ns'`))) // string missing time
+	require.Error(t, d.UnmarshalTOML([]byte(`'us'`))) // string missing time
 }
 
 func TestSize(t *testing.T) {
