dd2119a44b58811a6fc2805ebbded7a9b30edc89
Shrikanth Narayanan <shrikana@amazon.com>
2022-03-16 19:00:20 -0700

    Code clean up and formatting

--- plugins/outputs/cloudwatch_logs/cloudwatch_logs.go
+++ plugins/outputs/cloudwatch_logs/cloudwatch_logs.go
@@ -196,29 +196,27 @@ func (c *CloudWatchLogs) Connect() error {
 		return awsErr
 	}
 
-	if c.CredentialConfig.EndpointURL != "" && c.CredentialConfig.Region != "" {
-		customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-			return aws.Endpoint{
-				PartitionID:   "aws",
-				URL:           c.CredentialConfig.EndpointURL,
-				SigningRegion: c.CredentialConfig.Region,
-			}, nil
-		})
+    cfg, err := config.LoadDefaultConfig(context.TODO())
+    if err != nil {
+            return err
+    }
+    if c.CredentialConfig.EndpointURL != "" && c.CredentialConfig.Region != "" {
+            customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+                    return aws.Endpoint{
+                            PartitionID:   "aws",
+                            URL:           c.CredentialConfig.EndpointURL,
+                            SigningRegion: c.CredentialConfig.Region,
+                    }, nil
+            })
 
-		cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver))
-		if err != nil {
-			return err
-		}
-		cfg.Credentials = awsCreds.Credentials
-		c.svc = cloudwatchlogs.NewFromConfig(cfg)
-	} else {
-		cfg, err := config.LoadDefaultConfig(context.TODO())
-		if err != nil {
-			return err
-		}
-		cfg.Credentials = awsCreds.Credentials
-		c.svc = cloudwatchlogs.NewFromConfig(cfg)
-	}
+            cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver))
+            if err != nil {
+                    return err
+            }
+    }
+
+    cfg.Credentials = awsCreds.Credentials
+    c.svc = cloudwatchlogs.NewFromConfig(cfg)
 
 	//Find log group with name 'c.LogGroup'
 	if c.lg == nil { //In case connection is not retried, first time
