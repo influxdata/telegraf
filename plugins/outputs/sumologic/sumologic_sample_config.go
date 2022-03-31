ded0ede885cbff8c748ed32bd83ce1d115247d66
Sebastian Spaink <sspaink@influxdata.com>
2022-03-31 14:16:09 -0500

    move generate header

--- plugins/outputs/sumologic/sumologic_sample_config.go
+++ plugins/outputs/sumologic/sumologic_sample_config.go
@@ -1,3 +1,5 @@
+//go:generate go run ../../../tools/generate_plugindata/main.go
+//go:generate go run ../../../tools/generate_plugindata/main.go --clean
 // DON'T EDIT; This file is used as a template by tools/generate_plugindata
 package sumologic
 
