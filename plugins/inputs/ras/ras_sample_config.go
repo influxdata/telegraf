95426518556fd2d865743524418d58026dc9d4f0
Sebastian Spaink <sspaink@influxdata.com>
2022-04-04 09:37:05 -0500

    add build tags

--- plugins/inputs/ras/ras_sample_config.go
+++ plugins/inputs/ras/ras_sample_config.go
@@ -1,3 +1,7 @@
+//go:build linux && (386 || amd64 || arm || arm64)
+// +build linux
+// +build 386 amd64 arm arm64
+
 //go:generate go run ../../../tools/generate_plugindata/main.go
 //go:generate go run ../../../tools/generate_plugindata/main.go --clean
 // DON'T EDIT; This file is used as a template by tools/generate_plugindata
