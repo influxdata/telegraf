63ee5123b70db27302ec8345dc212c8e01aef63e
Sebastian Spaink <sspaink@influxdata.com>
2022-04-04 09:46:40 -0500

    add build tags

--- plugins/inputs/intel_pmu/intel_pmu_sample_config.go
+++ plugins/inputs/intel_pmu/intel_pmu_sample_config.go
@@ -1,3 +1,6 @@
+//go:build linux && amd64
+// +build linux,amd64
+
 //go:generate go run ../../../tools/generate_plugindata/main.go
 //go:generate go run ../../../tools/generate_plugindata/main.go --clean
 // DON'T EDIT; This file is used as a template by tools/generate_plugindata
