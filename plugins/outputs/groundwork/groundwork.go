a3f0df87041ef797b4b89add9384a041d1d85d56
Sebastian Spaink <sspaink@influxdata.com>
2022-04-07 16:23:39 -0500

    delete sampleconfig

--- plugins/outputs/groundwork/groundwork.go
+++ plugins/outputs/groundwork/groundwork.go
@@ -17,30 +17,6 @@ import (
 	"github.com/influxdata/telegraf/plugins/outputs"
 )
 
-const sampleConfig = `
-  ## URL of your groundwork instance.
-  url = "https://groundwork.example.com"
-
-  ## Agent uuid for GroundWork API Server.
-  agent_id = ""
-
-  ## Username and password to access GroundWork API.
-  username = ""
-  password = ""
-
-  ## Default display name for the host with services(metrics).
-  # default_host = "telegraf"
-
-  ## Default service state.
-  # default_service_state = "SERVICE_OK"
-
-  ## The name of the tag that contains the hostname.
-  # resource_tag = "host"
-
-  ## The name of the tag that contains the host group name.
-  # group_tag = "group"
-`
-
 type metricMeta struct {
 	group    string
 	resource string
