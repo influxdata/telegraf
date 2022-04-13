b379fc27146cfe3415f8a313b95d66ad06555362
Sebastian Spaink <sspaink@influxdata.com>
2022-04-13 15:56:58 -0500

    update mac SHA

--- scripts/installgo_mac.sh
+++ scripts/installgo_mac.sh
@@ -6,10 +6,10 @@ ARCH=$(uname -m)
 GO_VERSION="1.18.1"
 if [ "$ARCH" = 'arm64' ]; then
     GO_ARCH="darwin-arm64"
-    GO_VERSION_SHA="9cab6123af9ffade905525d79fc9ee76651e716c85f1f215872b5f2976782480" # from https://golang.org/dl
+    GO_VERSION_SHA="6d5641a06edba8cd6d425fb0adad06bad80e2afe0fa91b4aa0e5aed1bc78f58e" # from https://golang.org/dl
 elif [ "$ARCH" = 'x86_64' ]; then
     GO_ARCH="darwin-amd64"
-    GO_VERSION_SHA="70bb4a066997535e346c8bfa3e0dfe250d61100b17ccc5676274642447834969" # from https://golang.org/dl
+    GO_VERSION_SHA="3703e9a0db1000f18c0c7b524f3d378aac71219b4715a6a4c5683eb639f41a4d" # from https://golang.org/dl
 fi
 
 # This path is cachable. (Saving in /usr/local/ would cause issues restoring the cache.)
