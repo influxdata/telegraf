e98755948654e38540d8572f912f4dfa2c251450
Joshua Powers <powersj@fastmail.com>
2022-04-19 15:07:23 -0600

    fix: correctly parse various numeric forms (#10923)
    (cherry picked from commit bcc7c57f187f5912e09b2ede2a344d2f253e43bd)

--- plugins/inputs/smart/smart.go
+++ plugins/inputs/smart/smart.go
@@ -882,6 +882,7 @@ func (m *Smart) gatherDisk(acc telegraf.Accumulator, device string, wg *sync.Wai
 					}
 
 					if err := parse(fields, deviceFields, matches[2]); err != nil {
+						acc.AddError(fmt.Errorf("error parsing %s: '%s': %s", attr.Name, matches[2], err.Error()))
 						continue
 					}
 					// if the field is classified as an attribute, only add it
@@ -1029,8 +1030,20 @@ func parseInt(str string) int64 {
 }
 
 func parseCommaSeparatedInt(fields, _ map[string]interface{}, str string) error {
-	str = strings.Join(strings.Fields(str), "")
-	i, err := strconv.ParseInt(strings.Replace(str, ",", "", -1), 10, 64)
+	// remove any non-utf8 values
+	// '1\xa0292' --> 1292
+	value := strings.ToValidUTF8(strings.Join(strings.Fields(str), ""), "")
+
+	// remove any non-alphanumeric values
+	// '16,626,888' --> 16626888
+	// '16 829 004' --> 16829004
+	numRegex, err := regexp.Compile(`[^0-9\-]+`)
+	if err != nil {
+		return fmt.Errorf("failed to compile numeric regex")
+	}
+	value = numRegex.ReplaceAllString(value, "")
+
+	i, err := strconv.ParseInt(value, 10, 64)
 	if err != nil {
 		return err
 	}
@@ -1045,7 +1058,8 @@ func parsePercentageInt(fields, deviceFields map[string]interface{}, str string)
 }
 
 func parseDataUnits(fields, deviceFields map[string]interface{}, str string) error {
-	units := strings.Fields(str)[0]
+	// Remove everything after '['
+	units := strings.Split(str, "[")[0]
 	return parseCommaSeparatedInt(fields, deviceFields, units)
 }
 
