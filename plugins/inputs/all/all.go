da3292065ea9481645589e628f225aeab6dca17b
gkatzioura <gkatzioura@gmail.com>
2022-04-25 07:29:26 +0100

    Added format changes

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -58,7 +58,6 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/filestat"
 	_ "github.com/influxdata/telegraf/plugins/inputs/fireboard"
 	_ "github.com/influxdata/telegraf/plugins/inputs/fluentd"
-	_ "github.com/influxdata/telegraf/plugins/inputs/google_cloud_storage"
 	_ "github.com/influxdata/telegraf/plugins/inputs/github"
 	_ "github.com/influxdata/telegraf/plugins/inputs/gnmi"
 	_ "github.com/influxdata/telegraf/plugins/inputs/google_cloud_storage"
