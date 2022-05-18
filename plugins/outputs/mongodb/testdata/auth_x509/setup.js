b9551854d8e7a184e858fe23a663f068da56a69a
Josh Powers <powersj@fastmail.com>
2022-05-18 15:10:10 -0600

    update tests to testcontainers + testutil certs

--- plugins/outputs/mongodb/testdata/auth_x509/setup.js
+++ plugins/outputs/mongodb/testdata/auth_x509/setup.js
@@ -2,4 +2,4 @@ const conn = new Mongo();
 const db = conn.getDB("admin");
 // createUser normally requires a password unless $external is used
 // the CN value was found via: openssl x509 -in client.pem -noout -subject -nameopt RFC2253 | sed 's/subject=//g'
-db.getSiblingDB("$external").runCommand({ createUser: "CN=localdomain", roles: [{ role: "root", db: "admin" }] });
+db.getSiblingDB("$external").runCommand({ createUser: "CN=localhost", roles: [{ role: "root", db: "admin" }] });
