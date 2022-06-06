acea120f534769393eee719723074bd401804fb2
Josh Powers <powersj@fastmail.com>
2022-06-06 08:40:34 -0600

    fix: update modernc.org/sqlite from v1.10.8 to v1.17.3
    Enable windows support with the sqlite driver library.
    
    fixes: #11259

--- plugins/inputs/sql/drivers_sqlite.go
+++ plugins/inputs/sql/drivers_sqlite.go
@@ -1,7 +1,8 @@
-//go:build linux && freebsd && darwin && (!mips || !mips64)
+//go:build linux && freebsd && darwin && windows && (!mips || !mips64)
 // +build linux
 // +build freebsd
 // +build darwin
+// +build windows
 // +build !mips !mips64
 
 package sql
