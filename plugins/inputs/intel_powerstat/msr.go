6d829c199d8e7c3bbfeff09671351a121a4c1d14
bkotlowski <bartlomiej.kotlowski@intel.com>
2022-06-06 17:23:48 +0200

    feat(intel_powerstat): add uncore frequency metrics (#11254)

--- plugins/inputs/intel_powerstat/msr.go
+++ plugins/inputs/intel_powerstat/msr.go
@@ -20,6 +20,7 @@ const (
 	systemCPUPath                      = "/sys/devices/system/cpu/"
 	cpuCurrentFreqPartialPath          = "/sys/devices/system/cpu/cpu%s/cpufreq/scaling_cur_freq"
 	msrPartialPath                     = "/dev/cpu/%s/msr"
+	uncoreFreqPath                     = "/sys/devices/system/cpu/intel_uncore_frequency/package_%s_die_%s/%s%s_freq_khz"
 	c3StateResidencyLocation           = 0x3FC
 	c6StateResidencyLocation           = 0x3FD
 	c7StateResidencyLocation           = 0x3FE
@@ -32,14 +33,17 @@ const (
 	turboRatioLimit1Location           = 0x1AE
 	turboRatioLimit2Location           = 0x1AF
 	atomCoreTurboRatiosLocation        = 0x66C
+	uncorePerfStatusLocation           = 0x621
 )
 
 // msrService is responsible for interactions with MSR.
 type msrService interface {
 	getCPUCoresData() map[string]*msrData
 	retrieveCPUFrequencyForCore(core string) (float64, error)
+	retrieveUncoreFrequency(socketID string, typeFreq string, kind string, die string) (float64, error)
 	openAndReadMsr(core string) error
 	readSingleMsr(core string, msr string) (uint64, error)
+	isMsrLoaded() bool
 }
 
 type msrServiceImpl struct {
@@ -53,6 +57,15 @@ func (m *msrServiceImpl) getCPUCoresData() map[string]*msrData {
 	return m.cpuCoresData
 }
 
+func (m *msrServiceImpl) isMsrLoaded() bool {
+	for cpuID := range m.getCPUCoresData() {
+		err := m.openAndReadMsr(cpuID)
+		if err == nil {
+			return true
+		}
+	}
+	return false
+}
 func (m *msrServiceImpl) retrieveCPUFrequencyForCore(core string) (float64, error) {
 	cpuFreqPath := fmt.Sprintf(cpuCurrentFreqPartialPath, core)
 	err := checkFile(cpuFreqPath)
@@ -67,6 +80,49 @@ func (m *msrServiceImpl) retrieveCPUFrequencyForCore(core string) (float64, erro
 
 	cpuFreq, _, err := m.fs.readFileToFloat64(cpuFreqFile)
 	return convertKiloHertzToMegaHertz(cpuFreq), err
+}
+
+func (m *msrServiceImpl) retrieveUncoreFrequency(socketID string, typeFreq string, kind string, die string) (float64, error) {
+	uncoreFreqPath, err := createUncoreFreqPath(socketID, typeFreq, kind, die)
+	if err != nil {
+		return 0, fmt.Errorf("unable to create uncore freq read path for socketID %s, and frequency type %s err: %v", socketID, typeFreq, err)
+	}
+	err = checkFile(uncoreFreqPath)
+	if err != nil {
+		return 0, err
+	}
+	uncoreFreqFile, err := os.Open(uncoreFreqPath)
+	if err != nil {
+		return 0, fmt.Errorf("error opening uncore frequncy file on %s, err: %v", uncoreFreqPath, err)
+	}
+	defer uncoreFreqFile.Close()
+
+	uncoreFreq, _, err := m.fs.readFileToFloat64(uncoreFreqFile)
+	return convertKiloHertzToMegaHertz(uncoreFreq), err
+}
+
+func createUncoreFreqPath(socketID string, typeFreq string, kind string, die string) (string, error) {
+	if socketID >= "0" && socketID <= "9" {
+		socketID = fmt.Sprintf("0%s", socketID)
+	}
+	if die >= "0" && die <= "9" {
+		die = fmt.Sprintf("0%s", die)
+	}
+	var prefix string
+
+	switch typeFreq {
+	case "initial":
+		prefix = "initial_"
+	case "current":
+		prefix = ""
+	default:
+		return "", fmt.Errorf("unknown frequency type %s, only 'initial' and 'current' are supported", typeFreq)
+	}
+
+	if kind != "min" && kind != "max" {
+		return "", fmt.Errorf("unknown frequency type %s, only 'min' and 'max' are supported", kind)
+	}
+	return fmt.Sprintf(uncoreFreqPath, socketID, die, prefix, kind), nil
 }
 
 func (m *msrServiceImpl) openAndReadMsr(core string) error {
@@ -110,6 +166,8 @@ func (m *msrServiceImpl) readSingleMsr(core string, msr string) (uint64, error) 
 		msrAddress = turboRatioLimit2Location
 	case "MSR_ATOM_CORE_TURBO_RATIOS":
 		msrAddress = atomCoreTurboRatiosLocation
+	case "MSR_UNCORE_PERF_STATUS":
+		msrAddress = uncorePerfStatusLocation
 	default:
 		return 0, fmt.Errorf("incorect name of MSR %s", msr)
 	}
@@ -250,6 +308,5 @@ func newMsrServiceWithFs(logger telegraf.Logger, fs fileService) *msrServiceImpl
 	msrService.msrOffsets = []int64{c3StateResidencyLocation, c6StateResidencyLocation, c7StateResidencyLocation,
 		maximumFrequencyClockCountLocation, actualFrequencyClockCountLocation, timestampCounterLocation,
 		throttleTemperatureLocation, temperatureLocation}
-
 	return msrService
 }
