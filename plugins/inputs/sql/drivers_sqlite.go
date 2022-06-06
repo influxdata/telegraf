25bd1d2bc1ad2b60e335b510758f0655be308f66
Josh Powers <powersj@fastmail.com>
2022-06-06 12:52:06 -0600

    only skip mips[64]

--- plugins/inputs/sql/drivers_sqlite.go
+++ plugins/inputs/sql/drivers_sqlite.go
@@ -1,8 +1,4 @@
-//go:build linux && freebsd && darwin && windows && (!mips || !mips64)
-// +build linux
-// +build freebsd
-// +build darwin
-// +build windows
+//go:build !mips || !mips64
 // +build !mips !mips64
 
 package sql
