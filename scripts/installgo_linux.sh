912b6e23da9e121a30a54ee16d16f33133179a56
Josh Powers <powersj@fastmail.com>
2022-06-10 10:46:32 -0600

    test: add install go for linux, use in integraiton tests
    In order to ensure all our testing is using the same version of go
    across all tests, add an install go for linux.

---
+++ scripts/installgo_linux.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -eux
+
+ARCH=$(uname -m)
+GO_VERSION="1.18.3"
+GO_ARCH="linux-amd64"
+# from https://golang.org/dl
+GO_VERSION_SHA="956f8507b302ab0bb747613695cdae10af99bbd39a90cae522b7c0302cc27245"
+
+# Download Go and verify Go tarball
+setup_go () {
+    echo "installing go"
+    curl -L https://golang.org/dl/go${GO_VERSION}.${GO_ARCH}.tar.gz --output go${GO_VERSION}.${GO_ARCH}.tar.gz
+    if ! echo "${GO_VERSION_SHA}  go${GO_VERSION}.${GO_ARCH}.tar.gz" | shasum --algorithm 256 --check -; then
+        echo "Checksum failed" >&2
+        exit 1
+    fi
+
+    sudo tar -C /usr/local -xzf go${GO_VERSION}.${GO_ARCH}.tar.gz
+
+    echo $PATH
+    which go
+    go version
+}
+
+if command -v go >/dev/null 2>&1; then
+    echo "Go is already installed"
+    v=$(go version | { read -r _ _ v _; echo "${v#go}"; })
+    echo "$v is installed, required version is ${GO_VERSION}"
+    if [ "$v" != ${GO_VERSION} ]; then
+        setup_go
+    fi
+else
+    setup_go
+fi
