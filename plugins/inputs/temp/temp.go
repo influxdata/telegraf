4d0d9f94715916c153b842e4a06463dfddd61fef
Sven Rebhan <srebhan@influxdata.com>
2022-06-14 16:52:27 +0200

    Revert to previous state and only change Linux.

--- plugins/inputs/temp/temp.go
+++ plugins/inputs/temp/temp.go
@@ -3,11 +3,8 @@ package temp
 
 import (
 	_ "embed"
-	"fmt"
-	"strings"
 
 	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/internal/choice"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/influxdata/telegraf/plugins/inputs/system"
 )
@@ -17,61 +14,11 @@ import (
 var sampleConfig string
 
 type Temperature struct {
-	Scheme string `toml:"output_scheme"`
-
 	ps system.PS
 }
 
 func (*Temperature) SampleConfig() string {
 	return sampleConfig
-}
-
-func (t *Temperature) Init() error {
-	if !choice.Contains(t.Scheme, []string{"", "measurement", "field"}) {
-		return fmt.Errorf("invalid output_scheme %q", t.Scheme)
-	}
-	return nil
-}
-
-func (t *Temperature) Gather(acc telegraf.Accumulator) error {
-	temps, err := t.ps.Temperature()
-	if err != nil {
-		if strings.Contains(err.Error(), "not implemented yet") {
-			return fmt.Errorf("plugin is not supported on this platform: %v", err)
-		}
-		return fmt.Errorf("error getting temperatures info: %s", err)
-	}
-	for _, temp := range temps {
-		switch t.Scheme {
-		case "", "measurement":
-			acc.AddFields(
-				"temp",
-				map[string]interface{}{"temp": temp.Critical},
-				map[string]string{"sensor": temp.SensorKey + "_crit"},
-			)
-			acc.AddFields(
-				"temp",
-				map[string]interface{}{"temp": temp.High},
-				map[string]string{"sensor": temp.SensorKey + "_max"},
-			)
-			acc.AddFields(
-				"temp",
-				map[string]interface{}{"temp": temp.Temperature},
-				map[string]string{"sensor": temp.SensorKey + "_input"},
-			)
-		case "field":
-			acc.AddFields(
-				"temp",
-				map[string]interface{}{
-					"crit": temp.Critical,
-					"high": temp.High,
-					"temp": temp.Temperature,
-				},
-				map[string]string{"sensor": temp.SensorKey},
-			)
-		}
-	}
-	return nil
 }
 
 func init() {
--- plugins/inputs/temp/temp_test.go
+++ plugins/inputs/temp/temp_test.go
@@ -3,6 +3,7 @@ package temp
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 	"time"
 
@@ -41,179 +42,109 @@ func TestTemperature(t *testing.T) {
 	acc.AssertContainsTaggedFields(t, "temp", expectedFields, expectedTags)
 }
 
-func TestTemperatureOutputInvalid(t *testing.T) {
-	plugin := &Temperature{Scheme: "garbage", ps: system.NewSystemPS()}
-	require.Error(t, plugin.Init(), "alal")
-}
-
-func TestTemperatureOutputMeasurement(t *testing.T) {
-	tests := []struct {
-		name     string
-		path     string
-		expected []telegraf.Metric
-	}{
-		{
-			name: "general",
-			path: filepath.Join("testdata", "general"),
-			expected: []telegraf.Metric{
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_composite_crit"},
-					map[string]interface{}{"temp": 84.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_composite_max"},
-					map[string]interface{}{"temp": 81.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_composite_input"},
-					map[string]interface{}{"temp": 35.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_1_crit"},
-					map[string]interface{}{"temp": 0.0},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_1_max"},
-					map[string]interface{}{"temp": 65261.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_1_input"},
-					map[string]interface{}{"temp": 35.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_2_crit"},
-					map[string]interface{}{"temp": 0.0},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_2_max"},
-					map[string]interface{}{"temp": 65261.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_2_input"},
-					map[string]interface{}{"temp": 38.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tctl_crit"},
-					map[string]interface{}{"temp": 0.0},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tctl_max"},
-					map[string]interface{}{"temp": 0.0},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tctl_input"},
-					map[string]interface{}{"temp": 33.25},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tccd1_crit"},
-					map[string]interface{}{"temp": 0.0},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tccd1_max"},
-					map[string]interface{}{"temp": 0.0},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tccd1_input"},
-					map[string]interface{}{"temp": 33.25},
-					time.Unix(0, 0),
-				),
-			},
-		},
+func TestTemperatureLinux(t *testing.T) {
+	if runtime.GOOS != "linux" {
+		t.Skip("skipping test on non-Linux OS")
 	}
 
-	for _, tt := range tests {
-		os.Setenv("HOST_SYS", filepath.Join(tt.path, "sys"))
-
-		plugin := &Temperature{Scheme: "measurement", ps: system.NewSystemPS()}
-		require.NoError(t, plugin.Init())
-
-		var acc testutil.Accumulator
-		require.NoError(t, plugin.Gather(&acc))
-		testutil.RequireMetricsEqual(t, tt.expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
+	expected := []telegraf.Metric{
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_composite_crit"},
+			map[string]interface{}{"temp": 84.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_composite_max"},
+			map[string]interface{}{"temp": 81.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_composite_input"},
+			map[string]interface{}{"temp": 35.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_sensor_1_crit"},
+			map[string]interface{}{"temp": 0.0},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_sensor_1_max"},
+			map[string]interface{}{"temp": 65261.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_sensor_1_input"},
+			map[string]interface{}{"temp": 35.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_sensor_2_crit"},
+			map[string]interface{}{"temp": 0.0},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_sensor_2_max"},
+			map[string]interface{}{"temp": 65261.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "nvme_sensor_2_input"},
+			map[string]interface{}{"temp": 38.85},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "k10temp_tctl_crit"},
+			map[string]interface{}{"temp": 0.0},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "k10temp_tctl_max"},
+			map[string]interface{}{"temp": 0.0},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "k10temp_tctl_input"},
+			map[string]interface{}{"temp": 33.25},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "k10temp_tccd1_crit"},
+			map[string]interface{}{"temp": 0.0},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "k10temp_tccd1_max"},
+			map[string]interface{}{"temp": 0.0},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"temp",
+			map[string]string{"sensor": "k10temp_tccd1_input"},
+			map[string]interface{}{"temp": 33.25},
+			time.Unix(0, 0),
+		),
 	}
-}
 
-func TestTemperatureOutputField(t *testing.T) {
-	tests := []struct {
-		name     string
-		path     string
-		expected []telegraf.Metric
-	}{
-		{
-			name: "general",
-			path: filepath.Join("testdata", "general"),
-			expected: []telegraf.Metric{
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_composite"},
-					map[string]interface{}{"crit": 84.85, "high": 81.85, "temp": 35.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_1"},
-					map[string]interface{}{"crit": 0.0, "high": 65261.85, "temp": 35.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "nvme_sensor_2"},
-					map[string]interface{}{"crit": 0.0, "high": 65261.85, "temp": 38.85},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tctl"},
-					map[string]interface{}{"crit": 0.0, "high": 0.0, "temp": 33.25},
-					time.Unix(0, 0),
-				),
-				metric.New(
-					"temp",
-					map[string]string{"sensor": "k10temp_tccd1"},
-					map[string]interface{}{"crit": 0.0, "high": 0.0, "temp": 33.25},
-					time.Unix(0, 0),
-				),
-			},
-		},
-	}
+	os.Setenv("HOST_SYS", filepath.Join("testdata", "general", "sys"))
 
-	for _, tt := range tests {
-		os.Setenv("HOST_SYS", filepath.Join(tt.path, "sys"))
+	plugin := &Temperature{ps: system.NewSystemPS()}
 
-		plugin := &Temperature{Scheme: "field", ps: system.NewSystemPS()}
-		require.NoError(t, plugin.Init())
-
-		var acc testutil.Accumulator
-		require.NoError(t, plugin.Gather(&acc))
-		testutil.RequireMetricsEqual(t, tt.expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
-	}
+	var acc testutil.Accumulator
+	require.NoError(t, plugin.Gather(&acc))
+	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
 }
