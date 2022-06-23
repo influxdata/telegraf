f4825628e34f672a2561879b9b1cd436a38ffa5d
Josh Powers <powersj@fastmail.com>
2022-06-23 11:11:31 -0600

    test: add nightly package testing to amd64/linux
    This adds nightly testing of the generated amd64/linux RPM and DEB
    packages. This uses LXD in a VM on CircleCI to launch a number of
    different distros. The official InfluxData repo is added per the
    official instructions, then the new RPM/DEB is installed. This
    method verifies that the upgrade works as expected and the service
    continues to run.
    
    The package_lxd_test can be used to also locally when doing development
    or changes to the DEB and RPM scripts. LXD is required for it to
    correctly works. This is done as LXD let's us use system containers to
    verify the systemd connection with the services versus docker, which
    only lets us sort of test that the install works, but not the end-to-end
    service.
    
    fixes: #10467

---
+++ tools/package_lxd_test/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
+	"github.com/urfave/cli/v2"
+)
+
+var imagesRPM = []string{
+	"fedora/35",
+	"fedora/36",
+	"centos/7",
+	"centos/9-Stream",
+	"amazonlinux/current",
+	//"opensuse/15.3", 			// shadow-utils dependency bug see #3833
+	//"opensuse/tumbleweed", 	// shadow-utils dependency bug see #3833
+}
+
+var imagesDEB = []string{
+	"debian/buster",
+	"debian/bullseye",
+	"debian/bookworm",
+	"ubuntu/bionic",
+	"ubuntu/focal",
+	"ubuntu/jammy",
+}
+
+func main() {
+	packageFile := ""
+	image := ""
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "package",
+				Usage:       ".deb or .rpm file for upgrade testing",
+				Destination: &packageFile,
+			},
+			&cli.StringFlag{
+				Name:        "image",
+				Usage:       "optional, run with specific image",
+				Destination: &image,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if image != "" && packageFile != "" {
+				fmt.Printf("test package '%s' on image '%s'\n", packageFile, image)
+				return launchTests(packageFile, []string{image})
+			} else if packageFile != "" {
+				fmt.Printf("test package '%s' on all applicable images\n", packageFile)
+
+				extension := filepath.Ext(packageFile)
+				switch extension {
+				case ".rpm":
+					return launchTests(packageFile, imagesRPM)
+				case ".deb":
+					return launchTests(packageFile, imagesDEB)
+				default:
+					return fmt.Errorf("%s has unknown package type: %s", packageFile, extension)
+				}
+			}
+
+			return fmt.Errorf("please provide at least a package to test")
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func launchTests(packageFile string, images []string) error {
+	for _, image := range images {
+		fmt.Printf("starting test with %s\n", image)
+
+		uuidWithHyphen := uuid.New()
+		name := fmt.Sprintf("telegraf-test-%s", uuidWithHyphen.String()[0:8])
+
+		err := runTest(image, name, packageFile)
+		if err != nil {
+			fmt.Printf("*** FAIL: %s\n", image)
+			return err
+		}
+
+		fmt.Printf("*** PASS: %s\n\n", image)
+	}
+
+	fmt.Println("*** ALL TESTS PASS ***")
+	return nil
+}
+
+func runTest(image string, name string, packageFile string) error {
+	c := Container{Name: name}
+	if err := c.Create(image); err != nil {
+		return err
+	}
+	defer c.Delete()
+
+	if err := c.Install("telegraf"); err != nil {
+		return err
+	}
+
+	if err := c.CheckStatus("telegraf"); err != nil {
+		return err
+	}
+
+	if err := c.UploadAndInstall(packageFile); err != nil {
+		return err
+	}
+
+	return c.CheckStatus("telegraf")
+}
