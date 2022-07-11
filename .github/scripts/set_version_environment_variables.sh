f0d1c252d72d06ce44fad284432d5b3cca2b2c44
Greg Schrock <gschrock@juniper.net>
2022-07-11 11:42:41 -0400

    PR (12): Account for patch in version tag
    https://github.com/128technology/telegraf/pull/12
    - 5bd9aa0b0: Add a script that more intelligently parses the version tag

---
+++ .github/scripts/set_version_environment_variables.sh
@@ -0,0 +1,17 @@
+echo "VERSION_TAG=$VERSION_TAG"
+
+VERSION_REGEX='^128tech-v([0-9]+\.[0-9]+\.[0-9]+)(-([0-9]+))?$'
+[[ $VERSION_TAG =~ $VERSION_REGEX ]]
+
+if [ -z $BASH_REMATCH ]; then
+    echo "The tagged version does not match the required expression: $VERSION_EXPRESION"
+    exit 1
+fi
+
+echo "::set-env name=VERSION::${BASH_REMATCH[1]}"
+
+if [ ! -z ${BASH_REMATCH[3]} ]; then
+    echo "::set-env name=VERSION_PATCH::${BASH_REMATCH[3]}"
+else
+    echo "::set-env name=VERSION_PATCH::1"
+fi
