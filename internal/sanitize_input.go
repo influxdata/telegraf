83befa041d02eaa0e1c73b2a50f3bf14c36833dc
Sebastian Spaink <sspaink@influxdata.com>
2022-07-12 10:39:21 -0500

    fix: newline to space

--- internal/sanitize_input.go
+++ internal/sanitize_input.go
@@ -3,6 +3,6 @@ package internal
 import "strings"
 
 func SanitizeInput(input string) string {
-	escaped := strings.Replace(input, "\n", "", -1)
+	escaped := strings.Replace(input, "\n", " ", -1)
 	return strings.Replace(escaped, "\r", "", -1)
 }
