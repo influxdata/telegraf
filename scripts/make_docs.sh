0eed50c645872afd28461a7fe4a05a1494441945
Sebastian Spaink <sspaink@influxdata.com>
2022-07-12 11:22:59 -0500

    chore: Update error message

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -5,6 +5,6 @@ make docs
 if [ "$(git status --porcelain | wc -l)" -eq "0" ]; then
   echo "🟢 Git repo is clean."
 else
-  echo "🔴 Git repo dirty. Quit."
+  echo "🔴 Git repo dirty. Please run \"make docs\" and push the updated README. Failing CI."
   exit 1
 fi
