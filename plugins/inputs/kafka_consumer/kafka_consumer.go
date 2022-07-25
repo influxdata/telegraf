ad4852f7854e9d4a62e97371e7a45427af92cb6d
Albertas Bužinskas <albertas.buzinskas1@gmail.com>
2022-07-25 16:27:28 +0300

    Update kafka_consumer.go

--- plugins/inputs/kafka_consumer/kafka_consumer.go
+++ plugins/inputs/kafka_consumer/kafka_consumer.go
@@ -129,7 +129,7 @@ func (k *KafkaConsumer) Init() error {
 	cfg.Consumer.MaxProcessingTime = time.Duration(k.MaxProcessingTime)
 
 	if k.ConsumerFetchDefault != 0 {
-		cfg.Consumer.Fetch.Default = config.Size(k.ConsumerFetchDefault)
+		cfg.Consumer.Fetch.Default = int32(k.ConsumerFetchDefault)
 	}
 
 	k.config = cfg
