e9ffd0cb730593b8ad8de29c0b66a15050fb7aa2
Sven Rebhan <srebhan@influxdata.com>
2022-08-12 12:14:06 +0200

    fix: Sync sql output and input build constraints to handle loong64 in go1.19.

--- plugins/inputs/sql/drivers_sqlite.go
+++ plugins/inputs/sql/drivers_sqlite.go
@@ -1,11 +1,8 @@
-//go:build !arm && !mips && !mipsle && !mips64 && !mips64le && !ppc64 && !(freebsd && arm64)
-// +build !arm
-// +build !mips
-// +build !mipsle
-// +build !mips64
-// +build !mips64le
-// +build !ppc64
-// +build !freebsd !arm64
+//go:build linux && freebsd && darwin && (!mips || !mips64)
+// +build linux
+// +build freebsd
+// +build darwin
+// +build !mips !mips64
 
 package sql
 
