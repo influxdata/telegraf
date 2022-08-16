e7d2a7bd043bfea00514a496007205900b25607e
Sven Rebhan <srebhan@influxdata.com>
2022-08-16 16:11:57 +0200

    Only include tail on non-Solaris platforms.

--- plugins/inputs/all/tail.go
+++ plugins/inputs/all/tail.go
@@ -1,4 +1,4 @@
-//go:build !custom || inputs || inputs.tail
+//go:build (!custom || inputs || inputs.tail) && !solaris
 
 package all
 
--- plugins/inputs/tail/tail_solaris.go
+++
@@ -1,6 +0,0 @@
-// Skipping plugin on Solaris due to fsnotify support
-//
-//go:build solaris
-// +build solaris
-
-package tail
