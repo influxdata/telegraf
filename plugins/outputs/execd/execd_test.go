eb31b59833587d25d34174e20e83774a2ab7373c
Lucas Segawa <lucas.suzuki.113@gmail.com>
2022-09-09 23:00:47 +0900

    fix(outputs.execd): Fixing the execd behavior to not throw error when partially unserializable metrics are written. (#11767)

--- plugins/outputs/execd/execd_test.go
+++ plugins/outputs/execd/execd_test.go
@@ -70,6 +70,80 @@ func TestExternalOutputWorks(t *testing.T) {
 	wg.Wait()
 }
 
+func TestPartiallyUnserializableThrowError(t *testing.T) {
+	influxSerializer, err := serializers.NewInfluxSerializer()
+	require.NoError(t, err)
+
+	exe, err := os.Executable()
+	require.NoError(t, err)
+
+	e := &Execd{
+		Command:                  []string{exe, "-testoutput"},
+		Environment:              []string{"PLUGINS_OUTPUTS_EXECD_MODE=application", "METRIC_NAME=cpu"},
+		RestartDelay:             config.Duration(5 * time.Second),
+		IgnoreSerializationError: false,
+		serializer:               influxSerializer,
+		Log:                      testutil.Logger{},
+	}
+
+	require.NoError(t, e.Init())
+
+	m1 := metric.New(
+		"cpu",
+		map[string]string{"name": "cpu1"},
+		map[string]interface{}{"idle": 50, "sys": 30},
+		now,
+	)
+
+	m2 := metric.New(
+		"cpu",
+		map[string]string{"name": "cpu2"},
+		map[string]interface{}{},
+		now,
+	)
+
+	require.NoError(t, e.Connect())
+	require.Error(t, e.Write([]telegraf.Metric{m1, m2}))
+	require.NoError(t, e.Close())
+}
+
+func TestPartiallyUnserializableCanBeSkipped(t *testing.T) {
+	influxSerializer, err := serializers.NewInfluxSerializer()
+	require.NoError(t, err)
+
+	exe, err := os.Executable()
+	require.NoError(t, err)
+
+	e := &Execd{
+		Command:                  []string{exe, "-testoutput"},
+		Environment:              []string{"PLUGINS_OUTPUTS_EXECD_MODE=application", "METRIC_NAME=cpu"},
+		RestartDelay:             config.Duration(5 * time.Second),
+		IgnoreSerializationError: true,
+		serializer:               influxSerializer,
+		Log:                      testutil.Logger{},
+	}
+
+	require.NoError(t, e.Init())
+
+	m1 := metric.New(
+		"cpu",
+		map[string]string{"name": "cpu1"},
+		map[string]interface{}{"idle": 50, "sys": 30},
+		now,
+	)
+
+	m2 := metric.New(
+		"cpu",
+		map[string]string{"name": "cpu2"},
+		map[string]interface{}{},
+		now,
+	)
+
+	require.NoError(t, e.Connect())
+	require.NoError(t, e.Write([]telegraf.Metric{m1, m2}))
+	require.NoError(t, e.Close())
+}
+
 var testoutput = flag.Bool("testoutput", false,
 	"if true, act like line input program instead of test")
 
