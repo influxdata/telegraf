6606c5ed33ac26652251b7afa2c1e6a28074ec0f
Jacob Marble <jacobmarble@influxdata.com>
2022-10-05 13:03:17 -0700

    chore: update OpenTelemetry plugins to latest release (#11943)

--- plugins/inputs/opentelemetry/opentelemetry_test.go
+++ plugins/inputs/opentelemetry/opentelemetry_test.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
-	"go.opentelemetry.io/otel/metric/global"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
 
@@ -20,60 +18,55 @@ import (
 )
 
 func TestOpenTelemetry(t *testing.T) {
+	// create mock OpenTelemetry client
+
 	mockListener := bufconn.Listen(1024 * 1024)
+	t.Cleanup(func() { _ = mockListener.Close() })
 	plugin := inputs.Inputs["opentelemetry"]().(*OpenTelemetry)
 	plugin.listener = mockListener
 	accumulator := new(testutil.Accumulator)
 
-	err := plugin.Start(accumulator)
-	require.NoError(t, err)
+	require.NoError(t, plugin.Start(accumulator))
 	t.Cleanup(plugin.Stop)
 
-	metricExporter, err := otlpmetricgrpc.New(context.Background(),
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	t.Cleanup(cancel)
+
+	metricExporter, err := otlpmetricgrpc.New(ctx,
 		otlpmetricgrpc.WithInsecure(),
 		otlpmetricgrpc.WithDialOption(
 			grpc.WithBlock(),
-			grpc.WithContextDialer(func(_ context.Context, _ string) (net.Conn, error) {
-				return mockListener.Dial()
+			grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+				return mockListener.DialContext(ctx)
 			})),
 	)
 	require.NoError(t, err)
-	t.Cleanup(func() { _ = metricExporter.Shutdown(context.Background()) })
+	t.Cleanup(func() { _ = metricExporter.Shutdown(ctx) })
 
-	pusher := controller.New(
-		processor.NewFactory(
-			simple.NewWithHistogramDistribution(),
-			metricExporter,
-		),
-		controller.WithExporter(metricExporter),
-	)
+	reader := metric.NewManualReader()
+	mp := metric.NewMeterProvider(metric.WithReader(reader))
 
-	err = pusher.Start(context.Background())
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = pusher.Stop(context.Background()) })
+	// set a metric value
 
-	global.SetMeterProvider(pusher)
-
-	// write metrics
-	meter := global.MeterProvider().Meter("library-name")
+	meter := mp.Meter("library-name")
 	counter, err := meter.SyncInt64().Counter("measurement-counter")
-	require.NoError(t, err)
-	counter.Add(context.Background(), 7)
+	counter.Add(ctx, 7)
 
-	err = pusher.Stop(context.Background())
+	// write metrics through the telegraf OpenTelemetry input plugin
+
+	rm, err := reader.Collect(ctx)
 	require.NoError(t, err)
+	require.NoError(t, metricExporter.Export(ctx, rm))
 
 	// Shutdown
 
+	require.NoError(t, reader.Shutdown(ctx))
+	require.NoError(t, metricExporter.Shutdown(ctx))
 	plugin.Stop()
-
-	err = metricExporter.Shutdown(context.Background())
-	require.NoError(t, err)
 
 	// Check
 
 	require.Empty(t, accumulator.Errors)
-
 	require.Len(t, accumulator.Metrics, 1)
 	got := accumulator.Metrics[0]
 	require.Equal(t, "measurement-counter", got.Measurement)
