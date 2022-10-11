4a3b6dae9cf0eb2f37b1de327894d43be2da42ff
Pawel Zak <Pawel Zak>
2022-10-11 15:03:24 -0700

    chore: change bufio.NewReader to bufio.NewScanner

--- internal/internal.go
+++ internal/internal.go
@@ -70,18 +70,7 @@ func ProductToken() string {
 }
 
 // ReadLines reads contents from a file and splits them by new lines.
-// A convenience wrapper to ReadLinesOffsetN(filename, 0, -1).
 func ReadLines(filename string) ([]string, error) {
-	return ReadLinesOffsetN(filename, 0, -1)
-}
-
-// ReadLinesOffsetN reads contents from file and splits them by new line.
-// The offset tells at which line number to start.
-// The count determines the number of lines to read (starting from offset):
-//
-//	n >= 0: at most n lines
-//	n < 0: whole file
-func ReadLinesOffsetN(filename string, offset uint, n int) ([]string, error) {
 	f, err := os.Open(filename)
 	if err != nil {
 		return []string{""}, err
@@ -89,20 +78,9 @@ func ReadLinesOffsetN(filename string, offset uint, n int) ([]string, error) {
 	defer f.Close()
 
 	var ret []string
-
-	r := bufio.NewReader(f)
-	for i := 0; i < n+int(offset) || n < 0; i++ {
-		line, err := r.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return []string{""}, err
-		}
-		if i < int(offset) {
-			continue
-		}
-		ret = append(ret, strings.Trim(line, "\n"))
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		ret = append(ret, scanner.Text())
 	}
 
 	return ret, nil
