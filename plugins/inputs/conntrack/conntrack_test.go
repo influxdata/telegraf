54fa4fec1a7717ef0c12f63a37f8c59f7c63cb3e
Tomas Barton <barton.tomas@gmail.com>
2022-10-12 21:12:16 +0200

    feat(inputs.conntrack): Parse conntrack stats (#8958)

--- plugins/inputs/conntrack/conntrack_test.go
+++ plugins/inputs/conntrack/conntrack_test.go
@@ -9,8 +9,11 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/influxdata/telegraf/testutil"
+	"github.com/shirou/gopsutil/v3/net"
 	"github.com/stretchr/testify/require"
+
+	"github.com/influxdata/telegraf/plugins/inputs/system"
+	"github.com/influxdata/telegraf/testutil"
 )
 
 func restoreDflts(savedFiles, savedDirs []string) {
@@ -33,11 +36,13 @@ func TestNoFilesFound(t *testing.T) {
 
 func TestDefaultsUsed(t *testing.T) {
 	defer restoreDflts(dfltFiles, dfltDirs)
-	tmpdir := t.TempDir()
+	tmpdir, err := os.MkdirTemp("", "tmp1")
+	require.NoError(t, err)
+	defer os.Remove(tmpdir)
 
 	tmpFile, err := os.CreateTemp(tmpdir, "ip_conntrack_count")
 	require.NoError(t, err)
-	t.Cleanup(func() { require.NoError(t, tmpFile.Close()) })
+	defer os.Remove(tmpFile.Name())
 
 	dfltDirs = []string{tmpdir}
 	fname := path.Base(tmpFile.Name())
@@ -55,15 +60,16 @@ func TestDefaultsUsed(t *testing.T) {
 
 func TestConfigsUsed(t *testing.T) {
 	defer restoreDflts(dfltFiles, dfltDirs)
-	tmpdir := t.TempDir()
+	tmpdir, err := os.MkdirTemp("", "tmp1")
+	require.NoError(t, err)
+	defer os.Remove(tmpdir)
 
 	cntFile, err := os.CreateTemp(tmpdir, "nf_conntrack_count")
 	require.NoError(t, err)
-	t.Cleanup(func() { require.NoError(t, cntFile.Close()) })
-
 	maxFile, err := os.CreateTemp(tmpdir, "nf_conntrack_max")
 	require.NoError(t, err)
-	t.Cleanup(func() { require.NoError(t, maxFile.Close()) })
+	defer os.Remove(cntFile.Name())
+	defer os.Remove(maxFile.Name())
 
 	dfltDirs = []string{tmpdir}
 	cntFname := path.Base(cntFile.Name())
@@ -88,4 +94,156 @@ func TestConfigsUsed(t *testing.T) {
 			fix(cntFname): float64(count),
 			fix(maxFname): float64(max),
 		})
+}
+
+func TestCollectStats(t *testing.T) {
+	var mps system.MockPS
+	defer mps.AssertExpectations(t)
+	var acc testutil.Accumulator
+
+	sts := net.ConntrackStat{
+		Entries:       1234,
+		Searched:      10,
+		Found:         1,
+		New:           5,
+		Invalid:       43,
+		Ignore:        13,
+		Delete:        3,
+		DeleteList:    5,
+		Insert:        9,
+		InsertFailed:  20,
+		Drop:          49,
+		EarlyDrop:     7,
+		IcmpError:     21,
+		ExpectNew:     12,
+		ExpectCreate:  44,
+		ExpectDelete:  53,
+		SearchRestart: 31,
+	}
+
+	mps.On("NetConntrack", false).Return([]net.ConntrackStat{sts}, nil)
+	cs := &Conntrack{
+		ps: &mps,
+	}
+	cs.Collect = []string{"all"}
+
+	err := cs.Gather(&acc)
+	require.NoError(t, err)
+
+	expectedTags := map[string]string{
+		"cpu": "all",
+	}
+
+	expectedFields := map[string]interface{}{
+		"entries":        uint32(1234),
+		"searched":       uint32(10),
+		"found":          uint32(1),
+		"new":            uint32(5),
+		"invalid":        uint32(43),
+		"ignore":         uint32(13),
+		"delete":         uint32(3),
+		"delete_list":    uint32(5),
+		"insert":         uint32(9),
+		"insert_failed":  uint32(20),
+		"drop":           uint32(49),
+		"early_drop":     uint32(7),
+		"icmp_error":     uint32(21),
+		"expect_new":     uint32(12),
+		"expect_create":  uint32(44),
+		"expect_delete":  uint32(53),
+		"search_restart": uint32(31),
+	}
+
+	acc.AssertContainsFields(t, inputName, expectedFields)
+	acc.AssertContainsTaggedFields(t, inputName, expectedFields, expectedTags)
+
+	require.Equal(t, 19, acc.NFields())
+}
+
+func TestCollectStatsPerCpu(t *testing.T) {
+	var mps system.MockPS
+	defer mps.AssertExpectations(t)
+	var acc testutil.Accumulator
+
+	sts := []net.ConntrackStat{
+		{
+			Entries:       59,
+			Searched:      10,
+			Found:         1,
+			New:           5,
+			Invalid:       43,
+			Ignore:        13,
+			Delete:        3,
+			DeleteList:    5,
+			Insert:        9,
+			InsertFailed:  20,
+			Drop:          49,
+			EarlyDrop:     7,
+			IcmpError:     21,
+			ExpectNew:     12,
+			ExpectCreate:  44,
+			ExpectDelete:  53,
+			SearchRestart: 31,
+		},
+		{
+			Entries:       79,
+			Searched:      10,
+			Found:         1,
+			New:           5,
+			Invalid:       43,
+			Ignore:        13,
+			Delete:        3,
+			DeleteList:    5,
+			Insert:        9,
+			InsertFailed:  10,
+			Drop:          49,
+			EarlyDrop:     7,
+			IcmpError:     21,
+			ExpectNew:     12,
+			ExpectCreate:  44,
+			ExpectDelete:  53,
+			SearchRestart: 31,
+		},
+	}
+
+	mps.On("NetConntrack", true).Return(sts, nil)
+
+	cs := &Conntrack{
+		ps: &mps,
+	}
+	cs.Collect = []string{"all", "percpu"}
+
+	err := cs.Gather(&acc)
+	require.NoError(t, err)
+
+	//cpu0
+	expectedFields := map[string]interface{}{
+		"entries":        uint32(59),
+		"searched":       uint32(10),
+		"found":          uint32(1),
+		"new":            uint32(5),
+		"invalid":        uint32(43),
+		"ignore":         uint32(13),
+		"delete":         uint32(3),
+		"delete_list":    uint32(5),
+		"insert":         uint32(9),
+		"insert_failed":  uint32(20),
+		"drop":           uint32(49),
+		"early_drop":     uint32(7),
+		"icmp_error":     uint32(21),
+		"expect_new":     uint32(12),
+		"expect_create":  uint32(44),
+		"expect_delete":  uint32(53),
+		"search_restart": uint32(31),
+	}
+
+	acc.AssertContainsFields(t, inputName, expectedFields)
+	acc.AssertContainsTaggedFields(t, inputName, expectedFields,
+		map[string]string{
+			"cpu": "cpu0",
+		})
+
+	//TODO: check cpu1 fields
+
+	require.Equal(t, 36, acc.NFields())
 }
