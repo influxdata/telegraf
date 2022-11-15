51a0fd5e345b46dd6c13f5c187c2e3f4c851f3d5
Sven Rebhan <srebhan@influxdata.com>
2022-11-15 13:29:29 +0100

    Add script to check plugin document embedding.

---
+++ scripts/check-plugin-doc-embedding.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+plugin="${1}"
+
+if [ ! -d "${plugin}" ]; then
+    echo "ERR: ${plugin} is not a directory"
+    exit 1
+fi
+
+pluginname=$(basename "${plugin}")
+if [[ ":all:" =~ .*:${pluginname}:.* ]]; then
+    echo "INF: ${plugin} ignored"
+    exit 0
+fi
+
+# Check for the sample.conf file
+if [ ! -f "${plugin}/sample.conf" ] && [ "${pluginname}" != "modbus" ]; then
+    echo "ERR: ${plugin} does not contain a sample.conf file"
+    exit 1
+fi
+
+# Check for the generator
+generator="//go:generate ../../../tools/readme_config_includer/generator"
+found=false
+for filename in $(ls "${plugin}/"*.go); do
+    if [[ "${filename}" == *_test.go ]]; then
+        continue
+    fi
+    if ! grep -q "SampleConfig(" "${filename}"; then
+        continue
+    fi
+
+    if grep -q "^${generator}\$" "${filename}"; then
+        found=true
+        break
+    fi
+done
+
+if ! ${found}; then
+    echo "ERR: ${plugin} is missing generator statement!"
+    exit 1
+fi
+
+# Check for the embedding
+embedding="//go:embed sample.*\.conf"
+found=false
+for filename in $(ls "${plugin}/"*.go); do
+    if [[ "${filename}" == *_test.go ]]; then
+        continue
+    fi
+    if ! grep -q "SampleConfig(" "${filename}"; then
+        continue
+    fi
+
+    if grep -q "^${embedding}\$" "${filename}"; then
+        found=true
+        break
+    fi
+done
+
+if ! ${found}; then
+    echo "ERR: ${plugin} is missing embedding statement!"
+    exit 1
+fi
