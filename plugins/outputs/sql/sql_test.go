bd91b2ced2dde5899cadc8477d333e4ffe08f1aa
Joshua Powers <powersj@fastmail.com>
2022-11-18 09:38:40 -0700

    test(outputs.sql): print out what we get, bump time required (#12262)

--- plugins/outputs/sql/sql_test.go
+++ plugins/outputs/sql/sql_test.go
@@ -235,8 +235,9 @@ func TestMysqlIntegration(t *testing.T) {
 			bytes, err := io.ReadAll(out)
 			require.NoError(t, err)
 
+			fmt.Println(string(bytes))
 			return strings.Contains(string(bytes), string(expected))
-		}, 5*time.Second, 500*time.Millisecond)
+		}, 10*time.Second, 500*time.Millisecond, tc.expectedFile)
 	}
 }
 
