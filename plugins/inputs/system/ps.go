e84df8983a0ea530b8b99a5e02daf26872201c79
Paweł Żak <pawel.zak.pawel@gmail.com>
2022-11-21 21:11:07 +0100

    chore: Fix linter findings for errname (#12253)

--- plugins/inputs/system/ps.go
+++ plugins/inputs/system/ps.go
@@ -5,14 +5,14 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/internal"
-
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/shirou/gopsutil/v3/net"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
 )
 
 type PS interface {
@@ -198,7 +198,7 @@ func (s *SystemPS) NetConntrack(perCPU bool) ([]net.ConntrackStat, error) {
 
 func (s *SystemPS) DiskIO(names []string) (map[string]disk.IOCountersStat, error) {
 	m, err := disk.IOCounters(names...)
-	if err == internal.ErrorNotImplemented {
+	if err == internal.ErrNotImplemented {
 		return nil, nil
 	}
 
