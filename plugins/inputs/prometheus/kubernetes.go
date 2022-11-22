c3562ae8b27cb9b382ec86d41e183156f3f18f3d
Maxim Ivanov <hi+github@yamlcoder.me>
2022-11-22 17:04:52 +0000

    feat(inputs.prometheus): Allow explicit scrape configuration without annotations (#11962)

--- plugins/inputs/prometheus/kubernetes.go
+++ plugins/inputs/prometheus/kubernetes.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"os/user"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -96,6 +97,25 @@ func (p *Prometheus) startK8s(ctx context.Context) error {
 	return nil
 }
 
+func shouldScrapePod(pod *corev1.Pod, p *Prometheus) bool {
+	isCandidate := podReady(pod.Status.ContainerStatuses) &&
+		podHasMatchingNamespace(pod, p) &&
+		podHasMatchingLabelSelector(pod, p.podLabelSelector) &&
+		podHasMatchingFieldSelector(pod, p.podFieldSelector)
+
+	var shouldScrape bool
+	switch p.MonitorKubernetesPodsMethod {
+	case MonitorMethodAnnotations: // must have 'true' annotation to be scraped
+		shouldScrape = pod.Annotations != nil && pod.Annotations["prometheus.io/scrape"] == "true"
+	case MonitorMethodSettings: // will be scraped regardless of annotation
+		shouldScrape = true
+	case MonitorMethodSettingsAndAnnotations: // will be scraped unless opts out with 'false' annotation
+		shouldScrape = pod.Annotations == nil || pod.Annotations["prometheus.io/scrape"] != "false"
+	}
+
+	return isCandidate && shouldScrape
+}
+
 // An edge case exists if a pod goes offline at the same time a new pod is created
 // (without the scrape annotations). K8s may re-assign the old pod ip to the non-scrape
 // pod, causing errors in the logs. This is only true if the pod going offline is not
@@ -126,11 +146,7 @@ func (p *Prometheus) watchPod(ctx context.Context, clientset *kubernetes.Clients
 
 			pod, _ := clientset.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
 
-			if pod.Annotations["prometheus.io/scrape"] == "true" &&
-				podReady(pod.Status.ContainerStatuses) &&
-				podHasMatchingNamespace(pod, p) &&
-				podHasMatchingLabelSelector(pod, p.podLabelSelector) &&
-				podHasMatchingFieldSelector(pod, p.podFieldSelector) {
+			if shouldScrapePod(pod, p) {
 				registerPod(pod, p)
 			}
 		},
@@ -147,11 +163,7 @@ func (p *Prometheus) watchPod(ctx context.Context, clientset *kubernetes.Clients
 
 			newPod, _ := clientset.CoreV1().Pods(newNamespace).Get(ctx, newName, metav1.GetOptions{})
 
-			if newPod.Annotations["prometheus.io/scrape"] == "true" &&
-				podReady(newPod.Status.ContainerStatuses) &&
-				podHasMatchingNamespace(newPod, p) &&
-				podHasMatchingLabelSelector(newPod, p.podLabelSelector) &&
-				podHasMatchingFieldSelector(newPod, p.podFieldSelector) {
+			if shouldScrapePod(newPod, p) {
 				if newPod.GetDeletionTimestamp() == nil {
 					registerPod(newPod, p)
 				}
@@ -169,11 +181,7 @@ func (p *Prometheus) watchPod(ctx context.Context, clientset *kubernetes.Clients
 
 			oldPod, _ := clientset.CoreV1().Pods(oldNamespace).Get(ctx, oldName, metav1.GetOptions{})
 
-			if oldPod.Annotations["prometheus.io/scrape"] == "true" &&
-				podReady(oldPod.Status.ContainerStatuses) &&
-				podHasMatchingNamespace(oldPod, p) &&
-				podHasMatchingLabelSelector(oldPod, p.podLabelSelector) &&
-				podHasMatchingFieldSelector(oldPod, p.podFieldSelector) {
+			if shouldScrapePod(oldPod, p) {
 				if oldPod.GetDeletionTimestamp() != nil {
 					unregisterPod(oldPod, p)
 				}
@@ -192,11 +200,7 @@ func (p *Prometheus) watchPod(ctx context.Context, clientset *kubernetes.Clients
 
 			pod, _ := clientset.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
 
-			if pod.Annotations["prometheus.io/scrape"] == "true" &&
-				podReady(pod.Status.ContainerStatuses) &&
-				podHasMatchingNamespace(pod, p) &&
-				podHasMatchingLabelSelector(pod, p.podLabelSelector) &&
-				podHasMatchingFieldSelector(pod, p.podFieldSelector) {
+			if shouldScrapePod(pod, p) {
 				if pod.GetDeletionTimestamp() != nil {
 					unregisterPod(pod, p)
 				}
@@ -276,12 +280,7 @@ func updateCadvisorPodList(p *Prometheus, req *http.Request) error {
 	// Register pod only if it has an annotation to scrape, if it is ready,
 	// and if namespace and selectors are specified and match
 	for _, pod := range pods {
-		if necessaryPodFieldsArePresent(pod) &&
-			pod.Annotations["prometheus.io/scrape"] == "true" &&
-			podReady(pod.Status.ContainerStatuses) &&
-			podHasMatchingNamespace(pod, p) &&
-			podHasMatchingLabelSelector(pod, p.podLabelSelector) &&
-			podHasMatchingFieldSelector(pod, p.podFieldSelector) {
+		if necessaryPodFieldsArePresent(pod) && shouldScrapePod(pod, p) {
 			registerPod(pod, p)
 		}
 	}
@@ -355,7 +354,7 @@ func registerPod(pod *corev1.Pod, p *Prometheus) {
 	if p.kubernetesPods == nil {
 		p.kubernetesPods = map[string]URLAndAddress{}
 	}
-	targetURL, err := getScrapeURL(pod)
+	targetURL, err := getScrapeURL(pod, p)
 	if err != nil {
 		p.Log.Errorf("could not parse URL: %s", err)
 		return
@@ -397,7 +396,7 @@ func registerPod(pod *corev1.Pod, p *Prometheus) {
 	}
 }
 
-func getScrapeURL(pod *corev1.Pod) (*url.URL, error) {
+func getScrapeURL(pod *corev1.Pod, p *Prometheus) (*url.URL, error) {
 	ip := pod.Status.PodIP
 	if ip == "" {
 		// return as if scrape was disabled, we will be notified again once the pod
@@ -405,16 +404,36 @@ func getScrapeURL(pod *corev1.Pod) (*url.URL, error) {
 		return nil, nil
 	}
 
-	scheme := pod.Annotations["prometheus.io/scheme"]
-	pathAndQuery := pod.Annotations["prometheus.io/path"]
-	port := pod.Annotations["prometheus.io/port"]
+	var scheme, pathAndQuery, port string
+
+	if p.MonitorKubernetesPodsMethod == MonitorMethodSettings ||
+		p.MonitorKubernetesPodsMethod == MonitorMethodSettingsAndAnnotations {
+		scheme = p.MonitorKubernetesPodsScheme
+		pathAndQuery = p.MonitorKubernetesPodsPath
+		port = strconv.Itoa(p.MonitorKubernetesPodsPort)
+	}
+
+	if p.MonitorKubernetesPodsMethod == MonitorMethodAnnotations ||
+		p.MonitorKubernetesPodsMethod == MonitorMethodSettingsAndAnnotations {
+		if ann := pod.Annotations["prometheus.io/scheme"]; ann != "" {
+			scheme = ann
+		}
+		if ann := pod.Annotations["prometheus.io/path"]; ann != "" {
+			pathAndQuery = ann
+		}
+		if ann := pod.Annotations["prometheus.io/port"]; ann != "" {
+			port = ann
+		}
+	}
 
 	if scheme == "" {
 		scheme = "http"
 	}
+
 	if port == "" {
 		port = "9102"
 	}
+
 	if pathAndQuery == "" {
 		pathAndQuery = "/metrics"
 	}
@@ -431,7 +450,7 @@ func getScrapeURL(pod *corev1.Pod) (*url.URL, error) {
 }
 
 func unregisterPod(pod *corev1.Pod, p *Prometheus) {
-	targetURL, err := getScrapeURL(pod)
+	targetURL, err := getScrapeURL(pod, p)
 	if err != nil {
 		p.Log.Errorf("failed to parse url: %s", err)
 		return
--- plugins/inputs/prometheus/prometheus.go
+++ plugins/inputs/prometheus/prometheus.go
@@ -31,6 +31,15 @@ var sampleConfig string
 
 const acceptHeader = `application/vnd.google.protobuf;proto=io.prometheus.client.MetricFamily;encoding=delimited;q=0.7,text/plain;version=0.0.4;q=0.3`
 
+type MonitorMethod string
+
+const (
+	MonitorMethodNone                   MonitorMethod = ""
+	MonitorMethodAnnotations            MonitorMethod = "annotations"
+	MonitorMethodSettings               MonitorMethod = "settings"
+	MonitorMethodSettingsAndAnnotations MonitorMethod = "settings+annotations"
+)
+
 type Prometheus struct {
 	// An array of urls to scrape metrics from.
 	URLs []string `toml:"urls"`
@@ -92,6 +101,11 @@ type Prometheus struct {
 	podFieldSelector  fields.Selector
 	isNodeScrapeScope bool
 
+	MonitorKubernetesPodsMethod MonitorMethod `toml:"monitor_kubernetes_pods_method"`
+	MonitorKubernetesPodsScheme string        `toml:"monitor_kubernetes_pods_scheme"`
+	MonitorKubernetesPodsPath   string        `toml:"monitor_kubernetes_pods_path"`
+	MonitorKubernetesPodsPort   int           `toml:"monitor_kubernetes_pods_port"`
+
 	// Only for monitor_kubernetes_pods=true
 	CacheRefreshInterval int `toml:"cache_refresh_interval"`
 
@@ -138,6 +152,10 @@ func (p *Prometheus) Init() error {
 
 		p.Log.Infof("Using pod scrape scope at node level to get pod list using cAdvisor.")
 		p.Log.Infof("Using the label selector: %v and field selector: %v", p.podLabelSelector, p.podFieldSelector)
+	}
+
+	if p.MonitorKubernetesPodsMethod == MonitorMethodNone {
+		p.MonitorKubernetesPodsMethod = MonitorMethodAnnotations
 	}
 
 	ctx := context.Background()
