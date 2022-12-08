04a7ec4f978abe2e4a51dbb658f02d256bc38f51
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2022-12-08 21:13:30 +0100

    Update internal/host_endianess_le.go
    Co-authored-by: Joshua Powers <powersj@fastmail.com>

--- internal/host_endianess_le.go
+++ internal/host_endianess_le.go
@@ -1,4 +1,4 @@
-//go:build !(armbe || arm64be || mips || mips64 || mips64p32 || ppc || ppc64 || s390 || s390x || sparc || sparc64)
+//go:build 386 || amd64 || amd64p32 || arm || arm64 || loong64 || mipsle || mips64le || mips64p32le || ppc64le || riscv || riscv64 || wasm
 
 package internal
 
