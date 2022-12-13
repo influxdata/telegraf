3ace2260d2328441a67d311ace220ad55f9fad2b
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2022-12-13 11:59:04 +0100

    fix: Added own SPID filter to sqlserverqueries.go
    This filter has been set on all other source files (azureSqlDB and azureSqlMI) but not on the on-prem version

--- plugins/inputs/sqlserver/sqlserverqueries.go
+++ plugins/inputs/sqlserver/sqlserverqueries.go
@@ -1133,6 +1133,7 @@ WHERE
 			[is_user_process] = 1
 			OR [status] COLLATE Latin1_General_BIN NOT IN (''background'', ''sleeping'')
 		)
+		AND [session_id] <> @@SPID  --Exclude current SPID
 	)
 OPTION(MAXDOP 1)'
 
