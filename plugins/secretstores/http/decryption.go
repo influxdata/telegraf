cbed4c3d466ef7cedee899d9850e8ed99a376332
Sven Rebhan <srebhan@influxdata.com>
2023-01-09 10:02:21 +0100

    Merge trimming into decryption.

--- plugins/secretstores/http/decryption.go
+++ plugins/secretstores/http/decryption.go
@@ -32,3 +32,10 @@ func (c *DecryptionConfig) CreateDecrypter() (Decrypter, error) {
 	}
 	return nil, fmt.Errorf("unknown cipher %q", c.Cipher)
 }
+
+func PKCS7Trimming(in []byte) []byte {
+	// 'count' number of bytes where padded to the end of the clear-text
+	// each containing the value of 'count'
+	count := int(in[len(in)-1])
+	return in[:len(in)-count]
+}
--- plugins/secretstores/http/padding.go
+++
@@ -1,8 +0,0 @@
-package http
-
-func PKCS7Trimming(in []byte) []byte {
-	// 'count' number of bytes where padded to the end of the clear-text
-	// each containing the value of 'count'
-	count := int(in[len(in)-1])
-	return in[:len(in)-count]
-}
