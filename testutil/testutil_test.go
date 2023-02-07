1260b4523be6c0ae2f5ef707d69c60aaee08b3b6
Paweł Żak <pawel.zak.pawel@gmail.com>
2023-02-07 17:12:42 +0100

    chore: Fix linter findings for tenv (#12622)
    Co-authored-by: Pawel Zak <Pawel Zak>

--- testutil/testutil_test.go
+++ testutil/testutil_test.go
@@ -17,18 +17,14 @@ func TestDockerHost(t *testing.T) {
 		t.Fatalf("Host should be localhost when DOCKER_HOST is not set. Current value [%s]", host)
 	}
 
-	err = os.Setenv("DOCKER_HOST", "1.1.1.1")
-	require.NoError(t, err)
-
+	t.Setenv("DOCKER_HOST", "1.1.1.1")
 	host = GetLocalHost()
 
 	if host != "1.1.1.1" {
 		t.Fatalf("Host should take DOCKER_HOST value when set. Current value is [%s] and DOCKER_HOST is [%s]", host, os.Getenv("DOCKER_HOST"))
 	}
 
-	err = os.Setenv("DOCKER_HOST", "tcp://1.1.1.1:8080")
-	require.NoError(t, err)
-
+	t.Setenv("DOCKER_HOST", "tcp://1.1.1.1:8080")
 	host = GetLocalHost()
 
 	if host != "1.1.1.1" {
