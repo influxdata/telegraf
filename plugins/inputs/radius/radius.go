a29a8a96252a0e98ea64b41d76cd935e84893b79
Hr0bar <opialm@gmail.com>
2023-02-25 15:00:21 +0100

    Changes according to PR comments

--- plugins/inputs/radius/radius.go
+++ plugins/inputs/radius/radius.go
@@ -1,8 +1,10 @@
+//go:generate ../../../tools/readme_config_includer/generator
 package radius
 
 import (
 	"context"
 	_ "embed"
+	"fmt"
 	"net"
 	"sync"
 	"time"
@@ -16,40 +18,39 @@ import (
 )
 
 type Radius struct {
-	Servers         []string
-	Username        string
-	Password        string
-	Secret          string
-	ResponseTimeout config.Duration
-	Log             telegraf.Logger
+	Servers         []string        `toml:"servers"`
+	Username        config.Secret   `toml:"username"`
+	Password        config.Secret   `toml:"password"`
+	Secret          config.Secret   `toml:"secret"`
+	ResponseTimeout config.Duration `toml:"response_timeout"`
+	Log             telegraf.Logger `toml:"-"`
 }
 
 //go:embed sample.conf
 var sampleConfig string
 
-func (n *Radius) SampleConfig() string {
+func (r *Radius) SampleConfig() string {
 	return sampleConfig
 }
 
-func (n *Radius) Description() string {
-	return "Test Radius authentication"
+func (r *Radius) Init() error {
+	if len(r.Servers) == 0 {
+		r.Servers = []string{"127.0.0.1:1812"}
+	}
+	if r.ResponseTimeout < config.Duration(time.Second) {
+		r.ResponseTimeout = config.Duration(time.Second * 5)
+	}
+	return nil
 }
 
-func (n *Radius) Gather(acc telegraf.Accumulator) error {
+func (r *Radius) Gather(acc telegraf.Accumulator) error {
 	var wg sync.WaitGroup
 
-	if len(n.Servers) == 0 {
-		n.Servers = []string{"127.0.0.1:1812"}
-	}
-	if n.ResponseTimeout < config.Duration(time.Second) {
-		n.ResponseTimeout = config.Duration(time.Second * 5)
-	}
-
-	for _, server := range n.Servers {
+	for _, server := range r.Servers {
 		wg.Add(1)
 		go func(server string) {
 			defer wg.Done()
-			acc.AddError(n.pollServer(server, acc))
+			acc.AddError(r.pollServer(server, acc))
 		}(server)
 	}
 
@@ -57,37 +58,50 @@ func (n *Radius) Gather(acc telegraf.Accumulator) error {
 	return nil
 }
 
-func (n *Radius) pollServer(server string, acc telegraf.Accumulator) error {
+func (r *Radius) pollServer(server string, acc telegraf.Accumulator) error {
 	// Create the fields for this metric
 	host, port, err := net.SplitHostPort(server)
-	tags := map[string]string{"server": host, "port": port}
+	tags := map[string]string{"source": host, "port": port}
 	fields := make(map[string]interface{})
 
 	// Create the radius Client
-	var client = &radius.Client{
+	client := &radius.Client{
 		Retry: 0,
 	}
 
+	secret, err := r.Secret.Get()
+	if err != nil {
+		return fmt.Errorf("getting secret failed: %v", err)
+	}
+	username, err := r.Username.Get()
+	if err != nil {
+		return fmt.Errorf("getting username failed: %v", err)
+	}
+	password, err := r.Password.Get()
+	if err != nil {
+		return fmt.Errorf("getting password failed: %v", err)
+	}
+
 	// Create the radius packet with PAP authentication
-	packet := radius.New(radius.CodeAccessRequest, []byte(n.Secret))
-	rfc2865.UserName_SetString(packet, n.Username)
-	rfc2865.UserPassword_SetString(packet, n.Password)
+	packet := radius.New(radius.CodeAccessRequest, secret)
+	rfc2865.UserName_Set(packet, username)
+	rfc2865.UserPassword_Set(packet, password)
 
 	// Do the radius request
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(n.ResponseTimeout))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(r.ResponseTimeout))
 	defer cancel()
 	startTime := time.Now()
 	response, err := client.Exchange(ctx, packet, server)
 	duration := time.Since(startTime)
 
 	if err != nil {
-		n.Log.Warnf("error on new request to %s : %s", server, err)
-		fields["responsetime"] = time.Duration(n.ResponseTimeout).Seconds()
-		fields["responsetime_ms"] = time.Duration(n.ResponseTimeout).Milliseconds()
+		r.Log.Warnf("error on new request to %s : %s", server, err)
+		fields["responsetime"] = time.Duration(r.ResponseTimeout).Seconds()
+		fields["responsetime_ms"] = time.Duration(r.ResponseTimeout).Milliseconds()
 	} else if response.Code != radius.CodeAccessAccept {
-		n.Log.Warnf("Got radius return code: %d", response.Code)
-		fields["responsetime"] = time.Duration(n.ResponseTimeout).Seconds()
-		fields["responsetime_ms"] = time.Duration(n.ResponseTimeout).Milliseconds()
+		r.Log.Warnf("Got radius return code: %d", response.Code)
+		fields["responsetime"] = time.Duration(r.ResponseTimeout).Seconds()
+		fields["responsetime_ms"] = time.Duration(r.ResponseTimeout).Milliseconds()
 	} else {
 		fields["responsetime"] = duration.Seconds()
 		fields["responsetime_ms"] = duration.Milliseconds()
