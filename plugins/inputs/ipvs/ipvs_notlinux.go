4747e7ba10bfbc651c8ef4f1d8b2bc144361331b
Joshua Powers <powersj@fastmail.com>
2023-03-07 01:38:05 -0700

    chore: Unify sample configs across OSes (#12789)

--- plugins/inputs/ipvs/ipvs_notlinux.go
+++ plugins/inputs/ipvs/ipvs_notlinux.go
@@ -1,3 +1,30 @@
 //go:build !linux
 
 package ipvs
+
+import (
+	_ "embed"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+//go:embed sample.conf
+var sampleConfig string
+
+type Ipvs struct {
+	Log telegraf.Logger `toml:"-"`
+}
+
+func (i *Ipvs) Init() error {
+	i.Log.Warn("current platform is not supported")
+	return nil
+}
+func (*Ipvs) SampleConfig() string                { return sampleConfig }
+func (*Ipvs) Gather(_ telegraf.Accumulator) error { return nil }
+
+func init() {
+	inputs.Add("ipvs", func() telegraf.Input {
+		return &Ipvs{}
+	})
+}
--- plugins/inputs/mdstat/mdstat_notlinux.go
+++ plugins/inputs/mdstat/mdstat_notlinux.go
@@ -1,3 +1,30 @@
 //go:build !linux
 
 package mdstat
+
+import (
+	_ "embed"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+//go:embed sample.conf
+var sampleConfig string
+
+type Mdstat struct {
+	Log telegraf.Logger `toml:"-"`
+}
+
+func (m *Mdstat) Init() error {
+	m.Log.Warn("current platform is not supported")
+	return nil
+}
+func (*Mdstat) SampleConfig() string                { return sampleConfig }
+func (*Mdstat) Gather(_ telegraf.Accumulator) error { return nil }
+
+func init() {
+	inputs.Add("mdstat", func() telegraf.Input {
+		return &Mdstat{}
+	})
+}
