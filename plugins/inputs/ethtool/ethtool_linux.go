736f028c03806a8263361f0b0067d4a636c5624a
Josh Powers <powersj@fastmail.com>
2023-03-14 13:27:19 -0600

    ignore operation not supported error

--- plugins/inputs/ethtool/ethtool_linux.go
+++ plugins/inputs/ethtool/ethtool_linux.go
@@ -133,11 +133,10 @@ func (e *Ethtool) gatherEthtoolStats(iface NamespacedInterface, acc telegraf.Acc
 	}
 
 	cmdget, err := e.command.Get(iface)
-	if err != nil {
+	if err != nil && err.Error() != "operation not supported" {
 		acc.AddError(fmt.Errorf("%q get: %w", iface.Name, err))
 		return
 	}
-
 	for k, v := range cmdget {
 		fields[e.normalizeKey(k)] = v
 	}
