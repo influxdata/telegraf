160d800b734fbaf58e9ae14f325aeaefd27c05c6
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2023-03-31 08:34:42 +0200

    Move message to be printed in the error case only

--- cmd/telegraf/telegraf.go
+++ cmd/telegraf/telegraf.go
@@ -316,12 +316,12 @@ func (t *Telegraf) runAgent(ctx context.Context, c *config.Config, reloadConfig 
 	}
 
 	// Compute the amount of locked memory needed for the secrets
-	log.Printf("I! Managed secrets: %d", c.NumberSecrets)
 	required := 2 * c.NumberSecrets * uint64(os.Getpagesize())
 	available := getLockedMemoryLimit()
 	if required > available {
 		required /= 1024
 		available /= 1024
+		log.Printf("I! Found %d secrets...", c.NumberSecrets)
 		msg := fmt.Sprintf("Insufficient lockable memory %dkb when %dkb is required.", available, required)
 		msg += " Please increase the limit for Telegraf in your Operating System!"
 		log.Printf("W! " + color.RedString(msg))
