e34de678825862d2a8bacd4ffb4f90a5b83d60cb
Pawel Zak <Pawel Zak>
2023-04-06 18:53:03 +0200

    chore: Enable G103 rule for gosec

--- plugins/outputs/application_insights/application_insights.go
+++ plugins/outputs/application_insights/application_insights.go
@@ -40,11 +40,6 @@ type ApplicationInsights struct {
 	diagMsgListener   appinsights.DiagnosticsMessageListener
 }
 
-var (
-	is32Bit        bool
-	is32BitChecked bool
-)
-
 func (*ApplicationInsights) SampleConfig() string {
 	return sampleConfig
 }
@@ -281,15 +276,10 @@ func toFloat64(value interface{}) (float64, error) {
 }
 
 func toInt(value interface{}) (int, error) {
-	if !is32BitChecked {
-		is32BitChecked = true
-		is32Bit = strconv.IntSize == 32
-	}
-
 	// Out of all Golang numerical types Telegraf only uses int64, unit64 and float64 for fields
 	switch v := value.(type) {
 	case uint64:
-		if is32Bit {
+		if strconv.IntSize == 32 {
 			if v > math.MaxInt32 {
 				return 0, fmt.Errorf("value [%d] out of range of 32-bit integers", v)
 			}
@@ -302,7 +292,7 @@ func toInt(value interface{}) (int, error) {
 		return int(v), nil
 
 	case int64:
-		if is32Bit {
+		if strconv.IntSize == 32 {
 			if v > math.MaxInt32 || v < math.MinInt32 {
 				return 0, fmt.Errorf("value [%d] out of range of 32-bit integers", v)
 			}
