5abf66d03f32f64e26540a1d7f0bb9345f812513
PaddyKe <34421580+PaddyKe@users.noreply.github.com>
2023-04-09 17:10:56 +0000

    chore: added logging

--- plugins/processors/scaler/scaler.go
+++ plugins/processors/scaler/scaler.go
@@ -27,27 +27,37 @@ type Scaling struct {
 type Scaler struct {
 	Scalings   []Scaling       `toml:"scaling"`
 	Log        telegraf.Logger `toml:"-"`
-	scalingMap map[filter.Filter]Scaling
+	scalingMap map[filter.Filter]*Scaling
 }
 
 func (s *Scaler) Init() error {
+	s.scalingMap = make(map[filter.Filter]*Scaling)
 
-	s.scalingMap = make(map[filter.Filter]Scaling)
-
-	for _, element := range s.Scalings {
+	// convert filter list to filter map for better performance
+	for i, element := range s.Scalings {
 		filter, err := filter.Compile(element.Fields)
+
 		if err != nil {
+			s.Log.Errorf("Could not compile filter: %v\n", err)
 			return nil
 		}
-		s.scalingMap[filter] = element
+
+		if element.InMax != element.InMin {
+			s.scalingMap[filter] = &s.Scalings[i]
+		} else {
+			s.Log.Error("Found scaling with equal input_minimum and input_maximum. Skipping it.")
+		}
 	}
+
 	return nil
 }
 
+// scale a float according to the input and output range
 func Scale(value float64, in_min float64, in_max float64, out_min float64, out_max float64) float64 {
 	return (value-in_min)*(out_max-out_min)/(in_max-in_min) + out_min
 }
 
+// convert a numeric value to float
 func toFloat(v interface{}) (float64, bool) {
 	switch value := v.(type) {
 	case int64:
@@ -60,8 +70,10 @@ func toFloat(v interface{}) (float64, bool) {
 	return 0.0, false
 }
 
+// handle the scaling process
 func (s *Scaler) ScaleValues(metric telegraf.Metric) {
 	if s.Scalings == nil || s.scalingMap == nil || len(s.scalingMap) == 0 {
+		s.Log.Errorf("No valid scalings defined. Skipping scaling")
 		return
 	}
 
@@ -78,10 +90,11 @@ func (s *Scaler) ScaleValues(metric telegraf.Metric) {
 
 				if !ok {
 					metric.RemoveField(key)
-					s.Log.Errorf("error converting to float [%T]: %v", value, value)
+					s.Log.Errorf("error converting to float [%T]: %v\n", value, value)
 					continue
 				}
 
+				// replace filed with the new value (the name remains the same)
 				metric.RemoveField(key)
 				res := Scale(v, scaling.InMin, scaling.InMax, scaling.OutMin, scaling.OutMax)
 				metric.AddField(key, res)
