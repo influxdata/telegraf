3b7ee53b814ff988e22bad6c30ec5303b3b75c7e
Thomas Guenneguez <tguenneguez.ext@orange.com>
2023-04-27 22:37:49 +0200

    Add test new fields

--- plugins/inputs/filecount/filecount_test.go
+++ plugins/inputs/filecount/filecount_test.go
@@ -38,6 +38,8 @@ func TestNoFiltersOnChildDir(t *testing.T) {
 	require.NoError(t, acc.GatherError(fc.Gather))
 	require.True(t, acc.HasPoint("filecount", tags, "count", int64(len(matches))))
 	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(600)))
+	require.True(t, acc.HasInt64Field("filecount", "oldest_file_timestamp"))
+	require.True(t, acc.HasInt64Field("filecount", "newest_file_timestamp"))
 }
 
 func TestNoRecursiveButSuperMeta(t *testing.T) {
@@ -52,6 +54,8 @@ func TestNoRecursiveButSuperMeta(t *testing.T) {
 
 	require.True(t, acc.HasPoint("filecount", tags, "count", int64(len(matches))))
 	require.True(t, acc.HasPoint("filecount", tags, "size_bytes", int64(200)))
+	require.True(t, acc.HasInt64Field("filecount", "oldest_file_timestamp"))
+	require.True(t, acc.HasInt64Field("filecount", "newest_file_timestamp"))
 }
 
 func TestNameFilter(t *testing.T) {
