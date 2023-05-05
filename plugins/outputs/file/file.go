a421ae080c0fef6ce86d948962167b4e435a6d24
Zeyad Kenawi <zeyad.kenawi@starship.co>
2023-05-05 22:26:06 +0300

    feat: add compression to the file output plugin

--- plugins/outputs/file/file.go
+++ plugins/outputs/file/file.go
@@ -13,10 +13,15 @@ import (
 	"github.com/influxdata/telegraf/internal/rotate"
 	"github.com/influxdata/telegraf/plugins/outputs"
 	"github.com/influxdata/telegraf/plugins/serializers"
+	"github.com/klauspost/compress/zstd"
 )
 
 //go:embed sample.conf
 var sampleConfig string
+
+var ValidCompressionAlgorithmLevels = map[string][]int{
+	"zstd": {1, 3, 7, 11},
+}
 
 type File struct {
 	Files               []string        `toml:"files"`
@@ -24,11 +29,40 @@ type File struct {
 	RotationMaxSize     config.Size     `toml:"rotation_max_size"`
 	RotationMaxArchives int             `toml:"rotation_max_archives"`
 	UseBatchFormat      bool            `toml:"use_batch_format"`
+	Compression         Compression     `toml:"compression"`
 	Log                 telegraf.Logger `toml:"-"`
 
 	writer     io.Writer
 	closers    []io.Closer
 	serializer serializers.Serializer
+}
+
+type Compression struct {
+	Enabled   bool   `toml:"enabled"`
+	Algorithm string `toml:"algorithm"`
+	Level     int    `toml:"level"`
+}
+
+func ValidateCompressionAlgorithm(algorithm string) error {
+	for validAlgorithm := range ValidCompressionAlgorithmLevels {
+		if algorithm == validAlgorithm {
+			return nil
+		}
+	}
+	return fmt.Errorf("unknown or unsupported algorithm provided: %s", algorithm)
+}
+
+func ValidateCompressionLevel(algorithm string, level int) error {
+	for _, validAlgorithmLevel := range ValidCompressionAlgorithmLevels[algorithm] {
+		if level == validAlgorithmLevel {
+			return nil
+		}
+	}
+	return fmt.Errorf("unsupported compression level provided: %d. only %v are supported", level, ValidCompressionAlgorithmLevels[algorithm])
+}
+
+func CompressZstd(encoder *zstd.Encoder, src []byte) []byte {
+	return encoder.EncodeAll(src, make([]byte, 0, len(src)))
 }
 
 func (*File) SampleConfig() string {
@@ -37,6 +71,20 @@ func (*File) SampleConfig() string {
 
 func (f *File) SetSerializer(serializer serializers.Serializer) {
 	f.serializer = serializer
+}
+
+func (f *File) Init() error {
+  if f.Compression.Enabled {
+	  err := ValidateCompressionAlgorithm(f.Compression.Algorithm)
+	  if err != nil {
+	  	return err
+	  }
+	  err = ValidateCompressionLevel(f.Compression.Algorithm, f.Compression.Level)
+	  if err != nil {
+	  	return err
+	  }
+  }
+	return nil
 }
 
 func (f *File) Connect() error {
@@ -77,9 +125,20 @@ func (f *File) Close() error {
 
 func (f *File) Write(metrics []telegraf.Metric) error {
 	var writeErr error
+	var encoder interface{}
 
+	if f.Compression.Enabled {
+		if f.Compression.Algorithm == "zstd" {
+			encoder, _ = zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.EncoderLevelFromZstd(f.Compression.Level)))
+		}
+	}
 	if f.UseBatchFormat {
 		octets, err := f.serializer.SerializeBatch(metrics)
+		if f.Compression.Enabled {
+			if f.Compression.Algorithm == "zstd" {
+				octets = CompressZstd(encoder.(*zstd.Encoder), octets)
+			}
+		}
 		if err != nil {
 			f.Log.Errorf("Could not serialize metric: %v", err)
 		}
@@ -91,6 +150,11 @@ func (f *File) Write(metrics []telegraf.Metric) error {
 	} else {
 		for _, metric := range metrics {
 			b, err := f.serializer.Serialize(metric)
+			if f.Compression.Enabled {
+				if f.Compression.Algorithm == "zstd" {
+					b = CompressZstd(encoder.(*zstd.Encoder), b)
+				}
+			}
 			if err != nil {
 				f.Log.Debugf("Could not serialize metric: %v", err)
 			}
