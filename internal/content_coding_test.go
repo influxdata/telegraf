88f60ee6e67b40a1e1a12bfee89509b19276c87c
Zeyad Kenawi <zeyad.kenawi@starship.co>
2023-05-23 01:41:31 +0300

    test: add benchmark tests to internal encoding

--- internal/content_coding_test.go
+++ internal/content_coding_test.go
@@ -156,3 +156,168 @@ func TestStreamGzipDecode(t *testing.T) {
 
 	require.Equal(t, []byte("howdy"), b[:n])
 }
+
+func BenchmarkZstdEncodeDecode(b *testing.B) {
+	enc, err := NewZstdEncoder(1)
+	require.NoError(b, err)
+	dec, err := NewZstdDecoder()
+	require.NoError(b, err)
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		actual, err := dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "howdy", string(actual))
+	}
+}
+
+func BenchmarkZstdReuse(b *testing.B) {
+	enc, err := NewZstdEncoder(1)
+	require.NoError(b, err)
+	dec, err := NewZstdDecoder()
+	require.NoError(b, err)
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		actual, err := dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "howdy", string(actual))
+
+		payload, err = enc.Encode([]byte("doody"))
+		require.NoError(b, err)
+
+		actual, err = dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "doody", string(actual))
+	}
+}
+
+func BenchmarkGzipEncodeDecode(b *testing.B) {
+	enc, err := NewGzipEncoder(-1)
+	require.NoError(b, err)
+	dec := NewGzipDecoder()
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		actual, err := dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "howdy", string(actual))
+	}
+}
+
+func BenchmarkGzipReuse(b *testing.B) {
+	enc, err := NewGzipEncoder(-1)
+	require.NoError(b, err)
+	dec := NewGzipDecoder()
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		actual, err := dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "howdy", string(actual))
+
+		payload, err = enc.Encode([]byte("doody"))
+		require.NoError(b, err)
+
+		actual, err = dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "doody", string(actual))
+	}
+}
+
+func BenchmarkZlibEncodeDecode(b *testing.B) {
+	enc, err := NewZlibEncoder(-1)
+	require.NoError(b, err)
+	dec := NewZlibDecoder()
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		actual, err := dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "howdy", string(actual))
+	}
+}
+
+func BenchmarkZlibEncodeDecodeWithTooLargeMessage(b *testing.B) {
+	enc, err := NewZlibEncoder(-1)
+	require.NoError(b, err)
+	dec := NewZlibDecoder()
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		_, err = dec.Decode(payload, 3)
+		require.ErrorContains(b, err, "size of decoded data exceeds allowed size 3")
+	}
+}
+
+func BenchmarkIdentityEncodeDecode(b *testing.B) {
+	enc := NewIdentityEncoder()
+	dec := NewIdentityDecoder()
+
+	for n := 0; n < b.N; n++ {
+		payload, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		actual, err := dec.Decode(payload, maxDecompressionSize)
+		require.NoError(b, err)
+
+		require.Equal(b, "howdy", string(actual))
+	}
+}
+
+func BenchmarkStreamIdentityDecode(b *testing.B) {
+	var r bytes.Buffer
+	n, err := r.Write([]byte("howdy"))
+	require.NoError(b, err)
+	require.Equal(b, 5, n)
+
+	dec, err := NewStreamContentDecoder("identity", &r)
+	require.NoError(b, err)
+
+	for n := 0; n < b.N; n++ {
+		data, err := io.ReadAll(dec)
+		require.NoError(b, err)
+
+		require.Equal(b, []byte("howdy"), data)
+	}
+}
+
+func BenchmarkStreamGzipDecode(b *testing.B) {
+	enc, err := NewGzipEncoder(-1)
+	require.NoError(b, err)
+	for n := 0; n < b.N; n++ {
+		written, err := enc.Encode([]byte("howdy"))
+		require.NoError(b, err)
+
+		w := bytes.NewBuffer(written)
+
+		dec, err := NewStreamContentDecoder("gzip", w)
+		require.NoError(b, err)
+
+		a := make([]byte, 10)
+		n, err := dec.Read(a)
+		require.NoError(b, err)
+		require.Equal(b, 5, n)
+
+		require.Equal(b, []byte("howdy"), a[:n])
+	}
+}
