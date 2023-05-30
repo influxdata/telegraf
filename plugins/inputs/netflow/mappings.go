476b77657f173eb7c4bf368ca0aea6294e057f71
Sven Rebhan <srebhan@influxdata.com>
2023-05-30 12:41:00 +0200

    Remove unused function

--- plugins/inputs/netflow/mappings.go
+++ plugins/inputs/netflow/mappings.go
@@ -4,7 +4,6 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 var funcMapping = map[string]decoderFunc{
@@ -39,24 +38,6 @@ func loadMapping(filename string) (map[string]fieldMapping, error) {
 			return nil, fmt.Errorf("unknown data-type %q for id %q", dtype, id)
 		}
 		mappings[id] = fieldMapping{name, fun}
-	}
-
-	return mappings, nil
-}
-
-func loadNumericMapping(filename string) (map[uint16]fieldMapping, error) {
-	raw, err := loadMapping(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	mappings := make(map[uint16]fieldMapping, len(raw))
-	for key, fm := range raw {
-		id, err := strconv.ParseUint(key, 10, 16)
-		if err != nil {
-			return nil, fmt.Errorf("parsing ID %q failed: %w", key, err)
-		}
-		mappings[uint16(id)] = fm
 	}
 
 	return mappings, nil
