6409c27d6690d467186bf69b44f8ca8715ef7a77
Joshua Powers <powersj@fastmail.com>
2023-05-31 14:27:39 -0600

    Update plugins/parsers/xpath/parser.go

--- plugins/parsers/xpath/parser.go
+++ plugins/parsers/xpath/parser.go
@@ -100,7 +100,7 @@ func (p *Parser) Init() error {
 			p.Configs = append(p.Configs, p.ConfigsJSON...)
 			models.PrintOptionDeprecationNotice(telegraf.Warn, "parsers.xpath", "xpath_json", telegraf.DeprecationInfo{
 				Since:     "1.23.1",
-				RemovalIn: "1.30.0",
+				RemovalIn: "2.0.0",
 				Notice:    "use 'xpath' instead",
 			})
 		}
