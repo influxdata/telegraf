0b61b5a0e0021b56a2159db2e6b320b0799eb646
Anton Brekhov <tony.brekhov@gmail.com>
2023-06-01 23:00:53 +0300

    feat!: add new plugin for nebius cloud monitoring

---
+++ plugins/outputs/nebius_cloud_monitoring/README.md
@@ -0,0 +1,37 @@
+# Nebius Cloud Monitoring Output Plugin
+
+This plugin will send custom metrics to [ Cloud
+Monitoring](https://nebius.com/il/services/monitoring).
+
+## Global configuration options <!-- @/docs/includes/plugin_config.md -->
+
+In addition to the plugin-specific configuration settings, plugins support
+additional global and plugin configuration settings. These settings are used to
+modify metrics, tags, and field or create aliases and configure ordering, etc.
+See the [CONFIGURATION.md][CONFIGURATION.md] for more details.
+
+[CONFIGURATION.md]: ../../../docs/CONFIGURATION.md#plugins
+
+## Configuration
+
+```toml @sample.conf
+# Send aggregated metrics to Nebius.Cloud Monitoring
+[[outputs.nebius_cloud_monitoring]]
+  ## Timeout for HTTP writes.
+  # timeout = "20s"
+
+  ## Nebius.Cloud monitoring API endpoint. Normally should not be changed
+  # endpoint_url = "https://monitoring.api.il.nebius.cloud/monitoring/v2/data/write"
+
+  ## All user metrics should be sent with "custom" service specified. Normally should not be changed
+  # service = "custom"
+```
+
+### Authentication
+
+This plugin currently support only Compute metadata based authentication.
+
+When plugin is working inside a Compute instance it will take IAM token and
+Folder ID from instance metadata.
+
+Other authentication methods will be added later.
---
+++ plugins/outputs/nebius_cloud_monitoring/nebius_cloud_monitoring.go
@@ -0,0 +1,255 @@
+//go:generate ../../../tools/readme_config_includer/generator
+package nebius_cloud_monitoring
+
+import (
+	"bytes"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/config"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/plugins/outputs"
+	"github.com/influxdata/telegraf/selfstat"
+)
+
+//go:embed sample.conf
+var sampleConfig string
+
+// NebiusCloudMonitoring allows publishing of metrics to the Nebius Cloud Monitoring custom metrics
+// service
+type NebiusCloudMonitoring struct {
+	Timeout     config.Duration `toml:"timeout"`
+	EndpointURL string          `toml:"endpoint_url"`
+	Service     string          `toml:"service"`
+
+	Log telegraf.Logger
+
+	MetadataTokenURL       string
+	MetadataFolderURL      string
+	FolderID               string
+	IAMToken               string
+	IamTokenExpirationTime time.Time
+
+	client *http.Client
+
+	timeFunc func() time.Time
+
+	MetricOutsideWindow selfstat.Stat
+}
+
+type nebiusCloudMonitoringMessage struct {
+	TS      string                        `json:"ts,omitempty"`
+	Labels  map[string]string             `json:"labels,omitempty"`
+	Metrics []nebiusCloudMonitoringMetric `json:"metrics"`
+}
+
+type nebiusCloudMonitoringMetric struct {
+	Name       string            `json:"name"`
+	Labels     map[string]string `json:"labels"`
+	MetricType string            `json:"type,omitempty"` // DGAUGE|IGAUGE|COUNTER|RATE. Default: DGAUGE
+	TS         string            `json:"ts,omitempty"`
+	Value      float64           `json:"value"`
+}
+
+type MetadataIamToken struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int64  `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
+
+const (
+	defaultRequestTimeout = time.Second * 20
+	defaultEndpointURL    = "https://monitoring.api.il.nebius.cloud/monitoring/v2/data/write"
+	//nolint:gosec // G101: Potential hardcoded credentials - false positive
+	defaultMetadataTokenURL  = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
+	defaultMetadataFolderURL = "http://169.254.169.254/computeMetadata/v1/nebius/folder-id"
+)
+
+func (*NebiusCloudMonitoring) SampleConfig() string {
+	return sampleConfig
+}
+
+// Connect initializes the plugin and validates connectivity
+func (a *NebiusCloudMonitoring) Connect() error {
+	if a.Timeout <= 0 {
+		a.Timeout = config.Duration(defaultRequestTimeout)
+	}
+	if a.EndpointURL == "" {
+		a.EndpointURL = defaultEndpointURL
+	}
+	if a.Service == "" {
+		a.Service = "custom"
+	}
+	if a.MetadataTokenURL == "" {
+		a.MetadataTokenURL = defaultMetadataTokenURL
+	}
+	if a.MetadataFolderURL == "" {
+		a.MetadataFolderURL = defaultMetadataFolderURL
+	}
+
+	a.client = &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+		},
+		Timeout: time.Duration(a.Timeout),
+	}
+
+	var err error
+	a.FolderID, err = a.getFolderIDFromMetadata()
+	if err != nil {
+		return err
+	}
+
+	a.Log.Infof("Writing to Nebius.Cloud Monitoring URL: %s", a.EndpointURL)
+
+	tags := map[string]string{}
+	a.MetricOutsideWindow = selfstat.Register("nebius_cloud_monitoring", "metric_outside_window", tags)
+
+	return nil
+}
+
+// Close shuts down an any active connections
+func (a *NebiusCloudMonitoring) Close() error {
+	a.client = nil
+	return nil
+}
+
+// Write writes metrics to the remote endpoint
+func (a *NebiusCloudMonitoring) Write(metrics []telegraf.Metric) error {
+	var nebiusCloudMonitoringMetrics []nebiusCloudMonitoringMetric
+	for _, m := range metrics {
+		for _, field := range m.FieldList() {
+			value, err := internal.ToFloat64(field.Value)
+			if err != nil {
+				a.Log.Errorf("skipping value: %w", err.Error())
+				continue
+			}
+
+			nebiusCloudMonitoringMetrics = append(
+				nebiusCloudMonitoringMetrics,
+				nebiusCloudMonitoringMetric{
+					Name:   fmt.Sprintf("%s_%s", m.Name(), field.Key),
+					Labels: m.Tags(),
+					TS:     fmt.Sprint(m.Time().Format(time.RFC3339)),
+					Value:  value,
+				},
+			)
+		}
+	}
+
+	var body []byte
+	jsonBytes, err := json.Marshal(
+		nebiusCloudMonitoringMessage{
+			Metrics: nebiusCloudMonitoringMetrics,
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+	body = append(jsonBytes, '\n')
+	return a.send(body)
+}
+
+func getResponseFromMetadata(c *http.Client, metadataURL string) ([]byte, error) {
+	req, err := http.NewRequest("GET", metadataURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("Metadata-Flavor", "Google")
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode >= 300 || resp.StatusCode < 200 {
+		return nil, fmt.Errorf("unable to fetch instance metadata: [%s] %d",
+			metadataURL, resp.StatusCode)
+	}
+	return body, nil
+}
+
+func (a *NebiusCloudMonitoring) getFolderIDFromMetadata() (string, error) {
+	a.Log.Infof("getting folder ID in %s", a.MetadataFolderURL)
+	body, err := getResponseFromMetadata(a.client, a.MetadataFolderURL)
+	if err != nil {
+		return "", err
+	}
+	folderID := string(body)
+	if folderID == "" {
+		return "", fmt.Errorf("unable to fetch folder id from URL %s: %w", a.MetadataFolderURL, err)
+	}
+	return folderID, nil
+}
+
+func (a *NebiusCloudMonitoring) getIAMTokenFromMetadata() (string, int, error) {
+	a.Log.Debugf("getting new IAM token in %s", a.MetadataTokenURL)
+	body, err := getResponseFromMetadata(a.client, a.MetadataTokenURL)
+	if err != nil {
+		return "", 0, err
+	}
+	var metadata MetadataIamToken
+	if err := json.Unmarshal(body, &metadata); err != nil {
+		return "", 0, err
+	}
+	if metadata.AccessToken == "" || metadata.ExpiresIn == 0 {
+		return "", 0, fmt.Errorf("unable to fetch authentication credentials %s: %w", a.MetadataTokenURL, err)
+	}
+	return metadata.AccessToken, int(metadata.ExpiresIn), nil
+}
+
+func (a *NebiusCloudMonitoring) send(body []byte) error {
+	req, err := http.NewRequest("POST", a.EndpointURL, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Add("folderId", a.FolderID)
+	q.Add("service", a.Service)
+	req.URL.RawQuery = q.Encode()
+
+	req.Header.Set("Content-Type", "application/json")
+	isTokenExpired := !a.IamTokenExpirationTime.After(time.Now())
+	if a.IAMToken == "" || isTokenExpired {
+		token, expiresIn, err := a.getIAMTokenFromMetadata()
+		if err != nil {
+			return err
+		}
+		a.IamTokenExpirationTime = time.Now().Add(time.Duration(expiresIn) * time.Second)
+		a.IAMToken = token
+	}
+	req.Header.Set("Authorization", "Bearer "+a.IAMToken)
+
+	a.Log.Debugf("sending metrics to %s", req.URL.String())
+	a.Log.Debugf("body: %s", body)
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil || resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf("failed to write batch: [%v] %s", resp.StatusCode, resp.Status)
+	}
+
+	return nil
+}
+
+func init() {
+	outputs.Add("nebius_cloud_monitoring", func() telegraf.Output {
+		return &NebiusCloudMonitoring{
+			timeFunc: time.Now,
+		}
+	})
+}
