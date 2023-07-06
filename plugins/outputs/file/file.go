7aa3d79631e5ddada292381f91916f8936c2bf0c
Zeyad Kenawi <zeyad.kenawi@starship.co>
2023-07-06 18:07:56 +0300

    feat(outputs.file): Add compression (#13245)

--- plugins/outputs/file/file.go
+++ plugins/outputs/file/file.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
+	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/internal/rotate"
 	"github.com/influxdata/telegraf/plugins/outputs"
 	"github.com/influxdata/telegraf/plugins/serializers"
@@ -19,13 +20,16 @@ import (
 var sampleConfig string
 
 type File struct {
-	Files               []string        `toml:"files"`
-	RotationInterval    config.Duration `toml:"rotation_interval"`
-	RotationMaxSize     config.Size     `toml:"rotation_max_size"`
-	RotationMaxArchives int             `toml:"rotation_max_archives"`
-	UseBatchFormat      bool            `toml:"use_batch_format"`
-	Log                 telegraf.Logger `toml:"-"`
+	Files                []string        `toml:"files"`
+	RotationInterval     config.Duration `toml:"rotation_interval"`
+	RotationMaxSize      config.Size     `toml:"rotation_max_size"`
+	RotationMaxArchives  int             `toml:"rotation_max_archives"`
+	UseBatchFormat       bool            `toml:"use_batch_format"`
+	CompressionAlgorithm string          `toml:"compression_algorithm"`
+	CompressionLevel     int             `toml:"compression_level"`
+	Log                  telegraf.Logger `toml:"-"`
 
+	encoder    internal.ContentEncoder
 	writer     io.Writer
 	closers    []io.Closer
 	serializer serializers.Serializer
@@ -39,12 +43,27 @@ func (f *File) SetSerializer(serializer serializers.Serializer) {
 	f.serializer = serializer
 }
 
-func (f *File) Connect() error {
-	writers := []io.Writer{}
-
+func (f *File) Init() error {
+	var err error
 	if len(f.Files) == 0 {
 		f.Files = []string{"stdout"}
 	}
+
+	var options []internal.EncodingOption
+	if f.CompressionAlgorithm == "" {
+		f.CompressionAlgorithm = "identity"
+	}
+
+	if f.CompressionLevel >= 0 {
+		options = append(options, internal.WithCompressionLevel(f.CompressionLevel))
+	}
+	f.encoder, err = internal.NewContentEncoder(f.CompressionAlgorithm, options...)
+
+	return err
+}
+
+func (f *File) Connect() error {
+	var writers []io.Writer
 
 	for _, file := range f.Files {
 		if file == "stdout" {
@@ -84,6 +103,11 @@ func (f *File) Write(metrics []telegraf.Metric) error {
 			f.Log.Errorf("Could not serialize metric: %v", err)
 		}
 
+		octets, err = f.encoder.Encode(octets)
+		if err != nil {
+			f.Log.Errorf("Could not compress metrics: %v", err)
+		}
+
 		_, err = f.writer.Write(octets)
 		if err != nil {
 			f.Log.Errorf("Error writing to file: %v", err)
@@ -93,6 +117,11 @@ func (f *File) Write(metrics []telegraf.Metric) error {
 			b, err := f.serializer.Serialize(metric)
 			if err != nil {
 				f.Log.Debugf("Could not serialize metric: %v", err)
+			}
+
+			b, err = f.encoder.Encode(b)
+			if err != nil {
+				f.Log.Errorf("Could not compress metrics: %v", err)
 			}
 
 			_, err = f.writer.Write(b)
@@ -107,6 +136,8 @@ func (f *File) Write(metrics []telegraf.Metric) error {
 
 func init() {
 	outputs.Add("file", func() telegraf.Output {
-		return &File{}
+		return &File{
+			CompressionLevel: -1,
+		}
 	})
 }
