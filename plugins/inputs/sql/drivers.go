d09888f2ed73a2692eafd3746f91c1baa0b459f0
Joshua Powers <powersj@fastmail.com>
2023-07-07 11:01:55 -0600

    fix: replace github.com/denisenkom/go-mssqldb with github.com/microsoft/go-mssqldb (#13524)
    (cherry picked from commit c050c010bca0a1fda8b5d428c6495c276173480c)

--- plugins/inputs/sql/drivers.go
+++ plugins/inputs/sql/drivers.go
@@ -4,7 +4,7 @@ import (
 	// Blank imports to register the drivers
 	_ "github.com/ClickHouse/clickhouse-go"
 	_ "github.com/apache/arrow/go/v13/arrow/flight/flightsql/driver"
-	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/microsoft/go-mssqldb"
 )
