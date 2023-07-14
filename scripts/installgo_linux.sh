de8a9c514c398640fdc1b9c265000c38655c82aa
Joshua Powers <powersj@fastmail.com>
2023-07-14 08:11:22 -0600

    chore: Update to go1.20.6 (#13618)

--- scripts/installgo_linux.sh
+++ scripts/installgo_linux.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-GO_VERSION="1.20.5"
+GO_VERSION="1.20.6"
 GO_ARCH="linux-amd64"
 # from https://golang.org/dl
-GO_VERSION_SHA="d7ec48cde0d3d2be2c69203bc3e0a44de8660b9c09a6e85c4732a3f7dc442612"
+GO_VERSION_SHA="b945ae2bb5db01a0fb4786afde64e6fbab50b67f6fa0eb6cfa4924f16a7ff1eb"
 
 # Download Go and verify Go tarball
 setup_go () {
--- scripts/installgo_windows.sh
+++ scripts/installgo_windows.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-GO_VERSION="1.20.5"
+GO_VERSION="1.20.6"
 
 setup_go () {
     choco upgrade golang --allow-downgrade --version=${GO_VERSION}
