813250fe666d140686c6bdf41e8e0febd015e658
Chase Sterling <chase.sterling@gmail.com>
2023-07-24 09:47:05 -0400

    Make test cases self contained.

--- plugins/serializers/template/template_test.go
+++ plugins/serializers/template/template_test.go
@@ -10,86 +10,126 @@ import (
 	"github.com/influxdata/telegraf/metric"
 )
 
-var basicMetric = metric.New(
-	"cpu",
-	map[string]string{
-		"host": "localhost",
-		"cpu":  "CPU0",
-	},
-	map[string]interface{}{
-		"x": 42.0,
-		"y": 43.0,
-	},
-	time.Unix(100, 0),
-)
-
-var tests = []struct {
-	name      string
-	input     telegraf.Metric
-	template  string
-	output    []byte
-	errReason string
-}{
-	{
-		name:     "name",
-		input:    basicMetric,
-		template: "{{ .Name }}",
-		output:   []byte("cpu"),
-	},
-	{
-		name:     "time",
-		input:    basicMetric,
-		template: "{{ .Time.Unix }}",
-		output:   []byte("100"),
-	},
-	{
-		name:     "specific field",
-		input:    basicMetric,
-		template: `{{ .Field "x" }}`,
-		output:   []byte("42"),
-	},
-	{
-		name:     "specific tag",
-		input:    basicMetric,
-		template: `{{ .Tag "cpu" }}`,
-		output:   []byte("CPU0"),
-	},
-	{
-		name:     "all fields",
-		input:    basicMetric,
-		template: `{{ range $k, $v := .Fields }}{{$k}}={{$v}},{{end}}`,
-		output:   []byte("x=42,y=43,"),
-	},
-	{
-		name:     "all tags",
-		input:    basicMetric,
-		template: `{{ range $k, $v := .Tags }}{{$k}}={{$v}},{{end}}`,
-		output:   []byte("cpu=CPU0,host=localhost,"),
-	},
-	{
-		name:     "string",
-		input:    basicMetric,
-		template: "{{ .String }}",
-		output:   []byte("cpu map[cpu:CPU0 host:localhost] map[x:42 y:43] 100000000000"),
-	},
-	{
-		name: "complex",
-		input: metric.New(
-			"cpu",
-			map[string]string{
-				"tag1": "tag",
-			},
-			map[string]interface{}{
-				"value": 42.0,
-			},
-			time.Unix(0, 0),
-		),
-		template: `{{ .Name }} {{ range $k, $v := .Fields}}{{$k}}={{$v}}{{end}} {{ .Tag "tag1" }} {{.Time.UnixNano}} literal`,
-		output:   []byte("cpu value=42 tag 0 literal"),
-	},
-}
-
 func TestSerializer(t *testing.T) {
+	var tests = []struct {
+		name      string
+		input     telegraf.Metric
+		template  string
+		output    []byte
+		errReason string
+	}{
+		{
+			name: "name",
+			input: metric.New(
+				"cpu",
+				map[string]string{},
+				map[string]interface{}{},
+				time.Unix(100, 0),
+			),
+			template: "{{ .Name }}",
+			output:   []byte("cpu"),
+		},
+		{
+			name: "time",
+			input: metric.New(
+				"cpu",
+				map[string]string{},
+				map[string]interface{}{},
+				time.Unix(100, 0),
+			),
+			template: "{{ .Time.Unix }}",
+			output:   []byte("100"),
+		},
+		{
+			name: "specific field",
+			input: metric.New(
+				"cpu",
+				map[string]string{},
+				map[string]interface{}{
+					"x": 42.0,
+					"y": 43.0,
+				},
+				time.Unix(100, 0),
+			),
+			template: `{{ .Field "x" }}`,
+			output:   []byte("42"),
+		},
+		{
+			name: "specific tag",
+			input: metric.New(
+				"cpu",
+				map[string]string{
+					"host": "localhost",
+					"cpu":  "CPU0",
+				},
+				map[string]interface{}{},
+				time.Unix(100, 0),
+			),
+			template: `{{ .Tag "cpu" }}`,
+			output:   []byte("CPU0"),
+		},
+		{
+			name: "all fields",
+			input: metric.New(
+				"cpu",
+				map[string]string{},
+				map[string]interface{}{
+					"x": 42.0,
+					"y": 43.0,
+				},
+				time.Unix(100, 0),
+			),
+			template: `{{ range $k, $v := .Fields }}{{$k}}={{$v}},{{end}}`,
+			output:   []byte("x=42,y=43,"),
+		},
+		{
+			name: "all tags",
+			input: metric.New(
+				"cpu",
+				map[string]string{
+					"host": "localhost",
+					"cpu":  "CPU0",
+				},
+				map[string]interface{}{},
+				time.Unix(100, 0),
+			),
+			template: `{{ range $k, $v := .Tags }}{{$k}}={{$v}},{{end}}`,
+			output:   []byte("cpu=CPU0,host=localhost,"),
+		},
+		{
+			name: "string",
+			input: metric.New(
+				"cpu",
+				map[string]string{
+					"host": "localhost",
+					"cpu":  "CPU0",
+				},
+				map[string]interface{}{
+					"x": 42.0,
+					"y": 43.0,
+				},
+				time.Unix(100, 0),
+			),
+			template: "{{ .String }}",
+			output:   []byte("cpu map[cpu:CPU0 host:localhost] map[x:42 y:43] 100000000000"),
+		},
+		{
+			name: "complex",
+			input: metric.New(
+				"cpu",
+				map[string]string{
+					"tag1": "tag",
+				},
+				map[string]interface{}{
+					"value": 42.0,
+				},
+				time.Unix(0, 0),
+			),
+			template: `{{ .Name }} {{ range $k, $v := .Fields}}{{$k}}={{$v}}{{end}} {{ .Tag "tag1" }} {{.Time.UnixNano}} literal`,
+			output:   []byte("cpu value=42 tag 0 literal"),
+		},
+	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			serializer := &Serializer{
