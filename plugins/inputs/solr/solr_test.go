0e817e96d6c3c75dda0235651e51c241e0130281
Sven Rebhan <srebhan@influxdata.com>
2023-07-28 14:21:24 +0200

    Migrate last test to test-cases

--- plugins/inputs/solr/solr_test.go
+++ plugins/inputs/solr/solr_test.go
@@ -1,7 +1,6 @@
 package solr
 
 import (
-	"fmt"
 	"io/fs"
 	"net/http"
 	"net/http/httptest"
@@ -98,42 +97,6 @@ func TestCases(t *testing.T) {
 	}
 }
 
-func TestGatherStats(t *testing.T) {
-	ts := createMockServer(t)
-	solr := &Solr{
-		Servers:     []string{ts.URL},
-		HTTPTimeout: config.Duration(time.Second * 5),
-	}
-	require.NoError(t, solr.Init())
-
-	var acc testutil.Accumulator
-	require.NoError(t, solr.Gather(&acc))
-
-	acc.AssertContainsTaggedFields(t, "solr_admin",
-		solrAdminMainCoreStatusExpected,
-		map[string]string{"core": "main"})
-
-	acc.AssertContainsTaggedFields(t, "solr_admin",
-		solrAdminCore1StatusExpected,
-		map[string]string{"core": "core1"})
-
-	acc.AssertContainsTaggedFields(t, "solr_core",
-		solrCoreExpected,
-		map[string]string{"core": "main", "handler": "searcher"})
-
-	acc.AssertContainsTaggedFields(t, "solr_queryhandler",
-		solrQueryHandlerExpected,
-		map[string]string{"core": "main", "handler": "org.apache.solr.handler.component.SearchHandler"})
-
-	acc.AssertContainsTaggedFields(t, "solr_updatehandler",
-		solrUpdateHandlerExpected,
-		map[string]string{"core": "main", "handler": "updateHandler"})
-
-	acc.AssertContainsTaggedFields(t, "solr_cache",
-		solrCacheExpected,
-		map[string]string{"core": "main", "handler": "filterCache"})
-}
-
 func loadPages(path string) (map[string][]byte, error) {
 	abspath, err := filepath.Abs(path)
 	if err != nil {
@@ -164,33 +127,4 @@ func loadPages(path string) (map[string][]byte, error) {
 	})
 
 	return responses, err
-}
-
-func createMockServer(t *testing.T) *httptest.Server {
-	statusResponse := readJSONAsString(t, "testdata/status_response.json")
-	mBeansMainResponse := readJSONAsString(t, "testdata/m_beans_main_response.json")
-	mBeansCore1Response := readJSONAsString(t, "testdata/m_beans_core1_response.json")
-
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.URL.Path, "/solr/admin/cores") {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, statusResponse)
-		} else if strings.Contains(r.URL.Path, "solr/main/admin") {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, mBeansMainResponse)
-		} else if strings.Contains(r.URL.Path, "solr/core1/admin") {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, mBeansCore1Response)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, "nope")
-		}
-	}))
-}
-
-func readJSONAsString(t *testing.T, jsonFilePath string) string {
-	data, err := os.ReadFile(jsonFilePath)
-	require.NoErrorf(t, err, "could not read from JSON file %s", jsonFilePath)
-
-	return string(data)
 }
