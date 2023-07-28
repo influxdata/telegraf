952863b9a64343f0afd8c30f4f345e9bdecc4a82
Sven Rebhan <srebhan@influxdata.com>
2023-07-28 14:03:42 +0200

    Migrate v3.5 test to test-case

--- plugins/inputs/solr/README.md
+++ plugins/inputs/solr/README.md
@@ -27,10 +27,10 @@ See the [CONFIGURATION.md][CONFIGURATION.md] for more details.
 [[inputs.solr]]
   ## specify a list of one or more Solr servers
   servers = ["http://localhost:8983"]
-  ##
+
   ## specify a list of one or more Solr cores (default - all)
   # cores = ["main"]
-  ##
+  
   ## Optional HTTP Basic Auth Credentials
   # username = "username"
   # password = "pa$$word"
--- plugins/inputs/solr/solr_test.go
+++ plugins/inputs/solr/solr_test.go
@@ -2,6 +2,7 @@ package solr
 
 import (
 	"fmt"
+	"io/fs"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -13,7 +14,9 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 
+	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
+	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/influxdata/telegraf/plugins/parsers/influx"
 	"github.com/influxdata/telegraf/testutil"
 )
@@ -25,6 +28,11 @@ func TestCases(t *testing.T) {
 
 	// Make sure tests contains data
 	require.NotEmpty(t, folders)
+
+	// Register the plugin
+	inputs.Add("gnmi", func() telegraf.Input {
+		return &Solr{HTTPTimeout: config.Duration(5 * time.Second)}
+	})
 
 	options := []cmp.Option{
 		testutil.IgnoreTime(),
@@ -40,6 +48,7 @@ func TestCases(t *testing.T) {
 		fname := f.Name()
 		t.Run(fname, func(t *testing.T) {
 			testdataPath := filepath.Join("testcases", fname)
+			configFilename := filepath.Join(testdataPath, "telegraf.conf")
 			expectedFilename := filepath.Join(testdataPath, "expected.out")
 
 			// Load the expected output
@@ -48,17 +57,35 @@ func TestCases(t *testing.T) {
 			expected, err := testutil.ParseMetricsFromFile(expectedFilename, parser)
 			require.NoError(t, err)
 
-			// Create a HTTP server that delivers all files in the test-directory
-			handler, err := createRequestHandler(testdataPath)
+			// Load the pages for the test case
+			pages, err := loadPages(testdataPath)
 			require.NoError(t, err)
-			server := httptest.NewServer(handler)
+
+			// Create a HTTP server that delivers all files in the test-directory
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if !strings.HasPrefix(r.URL.Path, "/solr/") {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
+				path := strings.TrimPrefix(r.URL.Path, "/solr/")
+				page, found := pages[path]
+				if !found {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
+				w.Write(page)
+			}))
 			require.NotNil(t, server)
 			defer server.Close()
 
-			plugin := &Solr{
-				Servers:     []string{server.URL},
-				HTTPTimeout: config.Duration(time.Second * 5),
-			}
+			// Configure the plugin
+			cfg := config.NewConfig()
+			require.NoError(t, cfg.LoadConfig(configFilename))
+			require.Len(t, cfg.Inputs, 1)
+
+			// Setup the plugin
+			plugin := cfg.Inputs[0].Input.(*Solr)
+			plugin.Servers = []string{server.URL}
 			require.NoError(t, plugin.Init())
 
 			// Gather data and compare results
@@ -107,41 +134,6 @@ func TestGatherStats(t *testing.T) {
 		map[string]string{"core": "main", "handler": "filterCache"})
 }
 
-func TestSolr3GatherStats(t *testing.T) {
-	ts := createMockSolr3Server(t)
-	solr := &Solr{
-		Servers:     []string{ts.URL},
-		HTTPTimeout: config.Duration(time.Second * 5),
-	}
-	require.NoError(t, solr.Init())
-
-	var acc testutil.Accumulator
-	require.NoError(t, solr.Gather(&acc))
-
-	acc.AssertContainsTaggedFields(t, "solr_admin",
-		solrAdminMainCoreStatusExpected,
-		map[string]string{"core": "main"})
-
-	acc.AssertContainsTaggedFields(t, "solr_admin",
-		solrAdminCore1StatusExpected,
-		map[string]string{"core": "core1"})
-
-	acc.AssertContainsTaggedFields(t, "solr_core",
-		solr3CoreExpected,
-		map[string]string{"core": "main", "handler": "searcher"})
-
-	acc.AssertContainsTaggedFields(t, "solr_queryhandler",
-		solr3QueryHandlerExpected,
-		map[string]string{"core": "main", "handler": "org.apache.solr.handler.component.SearchHandler"})
-
-	acc.AssertContainsTaggedFields(t, "solr_updatehandler",
-		solr3UpdateHandlerExpected,
-		map[string]string{"core": "main", "handler": "updateHandler"})
-
-	acc.AssertContainsTaggedFields(t, "solr_cache",
-		solr3CacheExpected,
-		map[string]string{"core": "main", "handler": "filterCache"})
-}
 func TestNoCoreDataHandling(t *testing.T) {
 	ts := createMockNoCoreDataServer(t)
 	solr := &Solr{
@@ -167,33 +159,36 @@ func TestNoCoreDataHandling(t *testing.T) {
 	acc.AssertDoesNotContainMeasurement(t, "solr_handler")
 }
 
-func createRequestHandler(path string) (http.HandlerFunc, error) {
+func loadPages(path string) (map[string][]byte, error) {
 	abspath, err := filepath.Abs(path)
 	if err != nil {
 		return nil, err
 	}
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		if !strings.HasPrefix(r.URL.Path, "/solr/") {
-			w.WriteHeader(http.StatusNotFound)
-		}
-
-		// Construct the path
-		file := strings.TrimPrefix(r.URL.Path, "/solr/")
-		file = filepath.Join(abspath, filepath.FromSlash(file))
-		file, err = filepath.Abs(file)
+	responses := make(map[string][]byte)
+	err = filepath.Walk(abspath, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			return err
 		}
-		file += ".json"
-
-		// Prevent directory traversal
-		if !strings.HasPrefix(file, abspath) {
-			w.WriteHeader(http.StatusBadRequest)
+		if info.IsDir() || !strings.HasSuffix(path, ".json") {
+			// Ignore directories and files not matching the expectations
+			return nil
 		}
+		relpath, err := filepath.Rel(abspath, path)
+		if err != nil {
+			return err
+		}
+		relpath = strings.TrimSuffix(relpath, ".json")
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		responses[relpath] = data
 
-		http.ServeFile(w, r, file)
-	}, nil
+		return nil
+	})
+
+	return responses, err
 }
 
 func createMockServer(t *testing.T) *httptest.Server {
@@ -232,27 +227,6 @@ func createMockNoCoreDataServer(t *testing.T) *httptest.Server {
 		} else if strings.Contains(r.URL.Path, "solr/core1/admin") {
 			w.WriteHeader(http.StatusOK)
 			fmt.Fprintln(w, nodata)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, "nope")
-		}
-	}))
-}
-
-func createMockSolr3Server(t *testing.T) *httptest.Server {
-	data := readJSONAsString(t, "testdata/m_beans_solr3_main_response.json")
-	statusResponse := readJSONAsString(t, "testdata/status_response.json")
-
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.URL.Path, "/solr/admin/cores") {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, statusResponse)
-		} else if strings.Contains(r.URL.Path, "solr/main/admin") {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, data)
-		} else if strings.Contains(r.URL.Path, "solr/core1/admin") {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, data)
 		} else {
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprintln(w, "nope")
