7d4190d414f421d0c066db153b861d1cede9889b
Josh Powers <powersj@fastmail.com>
2023-08-22 06:13:51 -0600

    fix(inputs.cisco_telemetry_mdt): Check for slice length
    fixes: #13789

--- plugins/inputs/cisco_telemetry_mdt/cisco_telemetry_mdt.go
+++ plugins/inputs/cisco_telemetry_mdt/cisco_telemetry_mdt.go
@@ -416,6 +416,10 @@ func (c *CiscoTelemetryMDT) handleTelemetry(data []byte) {
 		if content != nil {
 			// Parse values
 			for _, subfield := range content.Fields {
+				if len(subfield.Fields) == 0 {
+					continue
+				}
+
 				prefix := ""
 				switch subfield.Name {
 				case "operation-metric":
