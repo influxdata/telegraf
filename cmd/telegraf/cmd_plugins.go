62a122e5fcb3892e1b58bd785f954d4a3cc70008
Josh Powers <powersj@fastmail.com>
2023-08-29 07:10:53 -0600

    generics ftw

--- cmd/telegraf/cmd_plugins.go
+++ cmd/telegraf/cmd_plugins.go
@@ -15,104 +15,13 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func getInputNames() []byte {
-	inputNames := make([]string, 0, len(inputs.Inputs))
-	for i := range inputs.Inputs {
-		inputNames = append(inputNames, fmt.Sprintf("inputs.%s\n", i))
+func pluginNames[M ~map[string]V, V any](m M, prefix string) []byte {
+	names := make([]string, 0, len(m))
+	for k := range m {
+		names = append(names, fmt.Sprintf("%s.%s\n", prefix, k))
 	}
-	sort.Strings(inputNames)
-
-	return []byte(strings.Join(inputNames, ""))
-}
-
-func getOutputNames() []byte {
-	outputNames := make([]string, 0, len(outputs.Outputs))
-	for i := range outputs.Outputs {
-		outputNames = append(outputNames, fmt.Sprintf("outputs.%s\n", i))
-	}
-	sort.Strings(outputNames)
-
-	return []byte(strings.Join(outputNames, ""))
-}
-
-func getProcessorNames() []byte {
-	processorNames := make([]string, 0, len(processors.Processors))
-	for i := range processors.Processors {
-		processorNames = append(processorNames, fmt.Sprintf("processors.%s\n", i))
-	}
-	sort.Strings(processorNames)
-
-	return []byte(strings.Join(processorNames, ""))
-}
-
-func getAggregatorNames() []byte {
-	aggregatorNames := make([]string, 0, len(aggregators.Aggregators))
-	for i := range aggregators.Aggregators {
-		aggregatorNames = append(aggregatorNames, fmt.Sprintf("aggregators.%s\n", i))
-	}
-	sort.Strings(aggregatorNames)
-
-	return []byte(strings.Join(aggregatorNames, ""))
-}
-
-func getSecretstoreNames() []byte {
-	stores := make([]string, 0, len(secretstores.SecretStores))
-	for i := range secretstores.SecretStores {
-		stores = append(stores, fmt.Sprintf("secretstores.%s\n", i))
-	}
-	sort.Strings(stores)
-
-	return []byte(strings.Join(stores, ""))
-}
-
-func getDeprecatedInputNames() []byte {
-	inputNames := make([]string, 0, len(inputs.Deprecations))
-	for i := range inputs.Deprecations {
-		inputNames = append(inputNames, fmt.Sprintf("inputs.%s\n", i))
-	}
-	sort.Strings(inputNames)
-
-	return []byte(strings.Join(inputNames, ""))
-}
-
-func getDeprecatedOutputNames() []byte {
-	outputNames := make([]string, 0, len(outputs.Deprecations))
-	for i := range outputs.Deprecations {
-		outputNames = append(outputNames, fmt.Sprintf("outputs.%s\n", i))
-	}
-	sort.Strings(outputNames)
-
-	return []byte(strings.Join(outputNames, ""))
-}
-
-func getDeprecatedProcessorNames() []byte {
-	processorNames := make([]string, 0, len(processors.Deprecations))
-	for i := range processors.Deprecations {
-		processorNames = append(processorNames, fmt.Sprintf("processors.%s\n", i))
-	}
-	sort.Strings(processorNames)
-
-	return []byte(strings.Join(processorNames, ""))
-}
-
-func getDeprecatedAggregatorNames() []byte {
-	aggregatorNames := make([]string, 0, len(aggregators.Deprecations))
-	for i := range aggregators.Deprecations {
-		aggregatorNames = append(aggregatorNames, fmt.Sprintf("aggregators.%s\n", i))
-	}
-	sort.Strings(aggregatorNames)
-
-	return []byte(strings.Join(aggregatorNames, ""))
-}
-
-func getDeprecatedSecretstoreNames() []byte {
-	stores := make([]string, 0, len(secretstores.Deprecations))
-	for i := range secretstores.Deprecations {
-		stores = append(stores, fmt.Sprintf("secretstores.%s\n", i))
-	}
-	sort.Strings(stores)
-
-	return []byte(strings.Join(stores, ""))
+	sort.Strings(names)
+	return []byte(strings.Join(names, ""))
 }
 
 func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
@@ -128,17 +37,17 @@ func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
 			},
 			Action: func(cCtx *cli.Context) error {
 				if cCtx.Bool("deprecated") {
-					outputBuffer.Write(getDeprecatedInputNames())
-					outputBuffer.Write(getDeprecatedOutputNames())
-					outputBuffer.Write(getDeprecatedProcessorNames())
-					outputBuffer.Write(getDeprecatedAggregatorNames())
-					outputBuffer.Write(getDeprecatedSecretstoreNames())
+					outputBuffer.Write(pluginNames(inputs.Deprecations, "inputs"))
+					outputBuffer.Write(pluginNames(outputs.Deprecations, "outputs"))
+					outputBuffer.Write(pluginNames(processors.Deprecations, "processors"))
+					outputBuffer.Write(pluginNames(aggregators.Deprecations, "aggregators"))
+					outputBuffer.Write(pluginNames(secretstores.Deprecations, "secretstores"))
 				} else {
-					outputBuffer.Write(getInputNames())
-					outputBuffer.Write(getOutputNames())
-					outputBuffer.Write(getProcessorNames())
-					outputBuffer.Write(getAggregatorNames())
-					outputBuffer.Write(getSecretstoreNames())
+					outputBuffer.Write(pluginNames(inputs.Inputs, "inputs"))
+					outputBuffer.Write(pluginNames(outputs.Outputs, "outputs"))
+					outputBuffer.Write(pluginNames(processors.Processors, "processors"))
+					outputBuffer.Write(pluginNames(aggregators.Aggregators, "aggregators"))
+					outputBuffer.Write(pluginNames(secretstores.SecretStores, "secretstores"))
 				}
 
 				return nil
@@ -155,9 +64,9 @@ func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
 					},
 					Action: func(cCtx *cli.Context) error {
 						if cCtx.Bool("deprecated") {
-							outputBuffer.Write(getDeprecatedInputNames())
+							outputBuffer.Write(pluginNames(inputs.Deprecations, "inputs"))
 						} else {
-							outputBuffer.Write(getInputNames())
+							outputBuffer.Write(pluginNames(inputs.Inputs, "inputs"))
 						}
 						return nil
 					},
@@ -173,9 +82,9 @@ func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
 					},
 					Action: func(cCtx *cli.Context) error {
 						if cCtx.Bool("deprecated") {
-							outputBuffer.Write(getDeprecatedOutputNames())
+							outputBuffer.Write(pluginNames(outputs.Deprecations, "outputs"))
 						} else {
-							outputBuffer.Write(getOutputNames())
+							outputBuffer.Write(pluginNames(outputs.Outputs, "outputs"))
 						}
 						return nil
 					},
@@ -191,9 +100,9 @@ func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
 					},
 					Action: func(cCtx *cli.Context) error {
 						if cCtx.Bool("deprecated") {
-							outputBuffer.Write(getDeprecatedProcessorNames())
+							outputBuffer.Write(pluginNames(processors.Deprecations, "processors"))
 						} else {
-							outputBuffer.Write(getProcessorNames())
+							outputBuffer.Write(pluginNames(processors.Processors, "processors"))
 						}
 						return nil
 					},
@@ -209,9 +118,9 @@ func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
 					},
 					Action: func(cCtx *cli.Context) error {
 						if cCtx.Bool("deprecated") {
-							outputBuffer.Write(getDeprecatedAggregatorNames())
+							outputBuffer.Write(pluginNames(aggregators.Deprecations, "aggregators"))
 						} else {
-							outputBuffer.Write(getAggregatorNames())
+							outputBuffer.Write(pluginNames(aggregators.Aggregators, "aggregators"))
 						}
 						return nil
 					},
@@ -227,9 +136,9 @@ func getPluginCommands(outputBuffer io.Writer) []*cli.Command {
 					},
 					Action: func(cCtx *cli.Context) error {
 						if cCtx.Bool("deprecated") {
-							outputBuffer.Write(getDeprecatedSecretstoreNames())
+							outputBuffer.Write(pluginNames(secretstores.Deprecations, "secretstores"))
 						} else {
-							outputBuffer.Write(getSecretstoreNames())
+							outputBuffer.Write(pluginNames(secretstores.SecretStores, "secretstores"))
 						}
 						return nil
 					},
