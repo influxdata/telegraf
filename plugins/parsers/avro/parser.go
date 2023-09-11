855b25d38341f18cea3df46267531d5770d2ffa0
Adam Thornton <athornton@users.noreply.github.com>
2023-09-11 07:05:40 -0700

    fix(parsers.avro): Do not force addition of timestamp as a field (#13856)

--- plugins/parsers/avro/parser.go
+++ plugins/parsers/avro/parser.go
@@ -172,24 +172,6 @@ func (p *Parser) createMetric(data map[string]interface{}, schema string) (teleg
 		// If you have specified your fields in the config, you
 		// get what you asked for.
 		fieldList = p.Fields
-
-		// Except...if you specify the timestamp field, and it's
-		// not listed in your fields, you'll get it anyway.
-		// This will randomize your field ordering, which isn't
-		// ideal.  If you care, list the timestamp field.
-		if p.Timestamp != "" {
-			// quick list-to-set-to-list implementation
-			fieldSet := make(map[string]bool)
-			for k := range fieldList {
-				fieldSet[fieldList[k]] = true
-			}
-			fieldSet[p.Timestamp] = true
-			var newList []string
-			for s := range fieldSet {
-				newList = append(newList, s)
-			}
-			fieldList = newList
-		}
 	} else {
 		for k := range data {
 			// Otherwise, that which is not a tag is a field
@@ -255,7 +237,7 @@ func (p *Parser) createMetric(data map[string]interface{}, schema string) (teleg
 	}
 	var timestamp time.Time
 	if p.Timestamp != "" {
-		rawTime := fmt.Sprintf("%v", fields[p.Timestamp])
+		rawTime := fmt.Sprintf("%v", data[p.Timestamp])
 		var err error
 		timestamp, err = internal.ParseTimestamp(p.TimestampFormat, rawTime, nil)
 		if err != nil {
