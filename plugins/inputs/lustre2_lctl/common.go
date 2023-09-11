29a23ce13656ede79b4c5ed36353421daa0b1197
muqali <ephe@mail.ustc.edu.cn>
2023-09-11 19:33:46 +0800

    reformat(lustre2_lctl): refactor

--- plugins/inputs/lustre2_lctl/common.go
+++ plugins/inputs/lustre2_lctl/common.go
@@ -14,6 +14,7 @@ import (
 )
 
 var (
+	//nolint: unparam
 	execCommand           = exec.Command
 	volumesPattern        = regexp.MustCompile(`(\w*).(.+)`)
 	recoveryStatusPattern = regexp.MustCompile(`status:(.+)`)
