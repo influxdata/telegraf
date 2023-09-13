892dafa6636b89d05f322c526177486c56b25011
Ephemeraler <673764589@qq.com>
2023-09-13 14:52:53 +0800

    fix(lustre2_lctl): fix fakeExecCommand in lustre2_lctl_test

--- plugins/inputs/lustre2_lctl/lustre2_lctl_test.go
+++ plugins/inputs/lustre2_lctl/lustre2_lctl_test.go
@@ -17,49 +17,39 @@ var (
 	testdataDir = getTestdataDir()
 )
 
-func TestHelperMDCActive(t *testing.T) {
-	data, err := os.ReadFile(filepath.Join(testdataDir, "mdc_active"))
-	require.NoError(t, err)
-
+func TestHelperProcess(t *testing.T) {
 	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
 		return
 	}
 
-	fmt.Fprint(os.Stdout, string(data))
+	args := os.Args
+	c := strings.Join(args[3:], " ")
 
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSCActive(t *testing.T) {
-	data, err := os.ReadFile(filepath.Join(testdataDir, "osc_active"))
-	require.NoError(t, err)
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, string(data))
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperMDTVolumes(_ *testing.T) {
-	data := `mdt.MDT0000`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperRecoveryStatus(_ *testing.T) {
-	data := `status: COMPLETE
+	switch c {
+	case "lctl get_param -n health_check":
+		fmt.Fprint(os.Stdout, `healthy`)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	// Client
+	case "lctl get_param mdc.*.active":
+		data, err := os.ReadFile(filepath.Join(testdataDir, "mdc_active"))
+		require.NoError(t, err)
+		fmt.Fprint(os.Stdout, string(data))
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param osc.*.active":
+		data, err := os.ReadFile(filepath.Join(testdataDir, "osc_active"))
+		require.NoError(t, err)
+		fmt.Fprint(os.Stdout, string(data))
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	// MDT
+	case "lctl get_param -N mdt.*":
+		fmt.Fprint(os.Stdout, `mdt.MDT0000`)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n mdt.MDT0000.recovery_status":
+		data := `status: COMPLETE
 	recovery_start: 1692176311
 	recovery_duration: 86
 	completed_clients: 2738/2738
@@ -67,222 +57,82 @@ func TestHelperRecoveryStatus(_ *testing.T) {
 	last_transno: 52878551595
 	VBR: DISABLED
 	IR: DISABLED`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperMDTJobStats(t *testing.T) {
-	data, err := os.ReadFile(filepath.Join(testdataDir, "mdt_jobstats"))
-	require.NoError(t, err)
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, string(data))
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperMDTStats(t *testing.T) {
-	data, err := os.ReadFile(filepath.Join(testdataDir, "mdt_stats"))
-	require.NoError(t, err)
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, string(data))
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSTKbytesfree(_ *testing.T) {
-	data := `26942292504`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSTKbytestotal(_ *testing.T) {
-	data := `46488188776`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSTKbytesavail(_ *testing.T) {
-	data := `24598218684`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSTVolumes(_ *testing.T) {
-	data := `obdfilter.OST0000`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSTStats(_ *testing.T) {
-	data := `snapshot_time             1693990463.128002841 secs.nsecs
+		fmt.Fprint(os.Stdout, data)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n mdt.MDT0000.job_stats":
+		data, err := os.ReadFile(filepath.Join(testdataDir, "mdt_jobstats"))
+		require.NoError(t, err)
+		fmt.Fprint(os.Stdout, string(data))
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n mdt.MDT0000.md_stats":
+		data, err := os.ReadFile(filepath.Join(testdataDir, "mdt_stats"))
+		require.NoError(t, err)
+		fmt.Fprint(os.Stdout, string(data))
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	// OST
+	case "lctl get_param -N obdfilter.*":
+		fmt.Fprint(os.Stdout, `obdfilter.OST0000`)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n obdfilter.OST0000.recovery_status":
+		data := `status: COMPLETE
+	recovery_start: 1692176311
+	recovery_duration: 86
+	completed_clients: 2738/2738
+	replayed_requests: 5203
+	last_transno: 52878551595
+	VBR: DISABLED
+	IR: DISABLED`
+		fmt.Fprint(os.Stdout, data)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n obdfilter.OST0000.job_stats":
+		data, err := os.ReadFile(filepath.Join(testdataDir, "ost_jobstats"))
+		require.NoError(t, err)
+		fmt.Fprint(os.Stdout, string(data))
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n obdfilter.OST0000.stats":
+		data := `snapshot_time             1693990463.128002841 secs.nsecs
 	read_bytes                1487077410 samples [bytes] 4096 4194304 606617630789632
 	setattr                   21402423 samples [reqs]`
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
+		fmt.Fprint(os.Stdout, data)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n obdfilter.OST0000.kbytesfree":
+		data := `26942292504`
+		fmt.Fprint(os.Stdout, data)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n obdfilter.OST0000.kbytesavail":
+		data := `24598218684`
+		fmt.Fprint(os.Stdout, data)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	case "lctl get_param -n obdfilter.OST0000.kbytestotal":
+		data := `46488188776`
+		fmt.Fprint(os.Stdout, data)
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(0)
+	default:
+		fmt.Fprint(os.Stdout, "invalid argument")
+		//nolint:revive // os.Exit called intentionally
+		os.Exit(1)
 	}
 
-	fmt.Fprint(os.Stdout, data)
-
-	//nolint:revive // os.Exit called intentionally
-	os.Exit(0)
-}
-
-func TestHelperOSTJobstats(t *testing.T) {
-	data, err := os.ReadFile(filepath.Join(testdataDir, "ost_jobstats"))
-	require.NoError(t, err)
-
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stdout, string(data))
-
-	//nolint:revive // os.Exit called intentionally
+	//nolint:revive // error code is important for this "test"
 	os.Exit(0)
 }
 
 func fakeExecCommand(command string, args ...string) *exec.Cmd {
-	tmp := make([]string, 0)
-	tmp = append(tmp, command)
-	tmp = append(tmp, args...)
-	tmpc := strings.Join(tmp, " ")
-	switch tmpc {
-	case "lctl get_param -n health_check":
-		cs := []string{"-test.run=TestHelperHealthCheck", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	// Client
-	case "lctl get_param mdc.*.active":
-		cs := []string{"-test.run=TestHelperMDCActive", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param osc.*.active":
-		cs := []string{"-test.run=TestHelperOSCActive", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	// MDT
-	case "lctl get_param -N mdt.*":
-		cs := []string{"-test.run=TestHelperMDTVolumes", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n mdt.MDT0000.recovery_status":
-		cs := []string{"-test.run=TestHelperRecoveryStatus", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n mdt.MDT0000.job_stats":
-		cs := []string{"-test.run=TestHelperMDTJobStats", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n mdt.MDT0000.md_stats":
-		cs := []string{"-test.run=TestHelperMDTStats", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	// OST
-	case "lctl get_param -N obdfilter.*":
-		cs := []string{"-test.run=TestHelperOSTVolumes", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n obdfilter.OST0000.recovery_status":
-		cs := []string{"-test.run=TestHelperRecoveryStatus", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n obdfilter.OST0000.job_stats":
-		cs := []string{"-test.run=TestHelperOSTJobstats", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n obdfilter.OST0000.stats":
-		cs := []string{"-test.run=TestHelperOSTStats", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n obdfilter.OST0000.kbytesfree":
-		cs := []string{"-test.run=TestHelperOSTKbytesfree", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n obdfilter.OST0000.kbytesavail":
-		cs := []string{"-test.run=TestHelperOSTKbytesavail", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	case "lctl get_param -n obdfilter.OST0000.kbytestotal":
-		cs := []string{"-test.run=TestHelperOSTKbytestotal", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	}
-
-	return nil
+	cs := []string{"-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...)
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
 }
 
 func getTestdataDir() string {
@@ -294,3 +144,4 @@ func getTestdataDir() string {
 
 	return filepath.Join(dir, "testdata")
 }
+
