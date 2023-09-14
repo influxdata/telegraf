b95bcbce4ec11dc6ef715835f8fc3c405fccbacb
Ephemeraler <673764589@qq.com>
2023-09-14 10:40:42 +0800

    fix(lustre2_lctl): fix GOCOVERDIR

--- plugins/inputs/lustre2_lctl/lustre2_lctl_test.go
+++ plugins/inputs/lustre2_lctl/lustre2_lctl_test.go
@@ -131,7 +131,7 @@ func fakeExecCommand(command string, args ...string) *exec.Cmd {
 	cs := []string{"-test.run=TestHelperProcess", "--", command}
 	cs = append(cs, args...)
 	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1", "GOCOVERDIR=./"}
 	return cmd
 }
 
