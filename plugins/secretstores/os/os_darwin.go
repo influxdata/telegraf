6801b8befb13c20eee2fab3e6269be2a1eae2d57
Sven Rebhan <srebhan@influxdata.com>
2023-09-20 16:03:28 +0200

    Fix darwin OS secret store

--- plugins/secretstores/os/os_darwin.go
+++ plugins/secretstores/os/os_darwin.go
@@ -6,21 +6,18 @@ import (
 	"fmt"
 
 	"github.com/99designs/keyring"
-
-	"github.com/influxdata/telegraf/config"
 )
 
 func (o *OS) createKeyringConfig() (keyring.Config, error) {
-	passwd, err := o.Password.Get()
-	if err != nil {
-		return keyring.Config{}, fmt.Errorf("getting password failed: %w", err)
-	}
-	defer config.ReleaseSecret(passwd)
-
 	// Create the prompt-function in case we need it
 	promptFunc := keyring.TerminalPrompt
-	if len(passwd) != 0 {
-		promptFunc = keyring.FixedStringPrompt(string(passwd))
+	if !o.Password.Empty() {
+		passwd, err := o.Password.Get()
+		if err != nil {
+			return keyring.Config{}, fmt.Errorf("getting password failed: %w", err)
+		}
+		promptFunc = keyring.FixedStringPrompt(passwd.StringCopy())
+		passwd.Destroy()
 	}
 
 	return keyring.Config{
