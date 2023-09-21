192f0efcb15bcf60232f8e846209cb498d1af5ae
Josh Powers <powersj@fastmail.com>
2023-09-21 08:06:51 -0600

    fix(inputs.postgresql_extensible): Restore outputaddress behavior
    fixes: #13965

--- plugins/inputs/postgresql_extensible/postgresql_extensible.go
+++ plugins/inputs/postgresql_extensible/postgresql_extensible.go
@@ -210,20 +210,20 @@ func (p *Postgresql) accRow(measName string, row scanner, acc telegraf.Accumulat
 				return err
 			}
 		default:
-			database, err := p.GetConnectDatabase(tagAddress)
-			if err != nil {
-				return err
-			}
-			if _, err := dbname.WriteString(database); err != nil {
+			if tagAddress != "" {
+				if _, err := dbname.WriteString(tagAddress); err != nil {
+					return err
+				}
+			} else if _, err := dbname.WriteString("postgres"); err != nil {
 				return err
 			}
 		}
 	} else {
-		database, err := p.GetConnectDatabase(tagAddress)
-		if err != nil {
-			return err
-		}
-		if _, err := dbname.WriteString(database); err != nil {
+		if tagAddress != "" {
+			if _, err := dbname.WriteString(tagAddress); err != nil {
+				return err
+			}
+		} else if _, err := dbname.WriteString("postgres"); err != nil {
 			return err
 		}
 	}
