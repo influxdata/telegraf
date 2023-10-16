006245dfcd56a48de64d60ded080133a772892d9
Sven Rebhan <srebhan@influxdata.com>
2023-10-16 22:36:53 +0200

    feat(migrations): Add migration for outputs.riemann_legacy

---
+++ migrations/common/output_options.go
@@ -0,0 +1,19 @@
+package common
+
+type OutputOptions struct {
+	// General options
+	Alias string `toml:"alias,omitempty"`
+
+	// Filter options
+	NamePass       []string            `toml:"namepass,omitempty"`
+	NameDrop       []string            `toml:"namedrop,omitempty"`
+	FieldPassOld   []string            `toml:"pass,omitempty"`
+	FieldPass      []string            `toml:"fieldpass,omitempty"`
+	FieldDropOld   []string            `toml:"drop,omitempty"`
+	FieldDrop      []string            `toml:"fielddrop,omitempty"`
+	TagPassFilters map[string][]string `toml:"tagpass,omitempty"`
+	TagDropFilters map[string][]string `toml:"tagdrop,omitempty"`
+	TagExclude     []string            `toml:"tagexclude,omitempty"`
+	TagInclude     []string            `toml:"taginclude,omitempty"`
+	MetricPass     string              `toml:"metricpass,omitempty"`
+}
