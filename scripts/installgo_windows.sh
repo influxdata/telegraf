0cc6eafb514eebc2f45f2a41ba67883536df91d4
Pawel Zak <pawel.zak.pawel@gmail.com>
2023-10-22 20:57:28 +0200

    Check debug output when binaries are not cached and find all go.exe in system

--- scripts/installgo_windows.sh
+++ scripts/installgo_windows.sh
@@ -11,7 +11,7 @@ setup_go () {
 
 echo "PATH before: $PATH"
 echo "go location before: $(command -v go)"
-sudo find / -name "go.exe"
+find / -name "go.exe"
 
 if command -v go >/dev/null 2>&1; then
     echo "Go is already installed"
@@ -29,4 +29,4 @@ fi
 echo "PATH after: $PATH"
 echo "go location after: $(command -v go)"
 go version
-sudo find / -name "go.exe"
+find / -name "go.exe"
