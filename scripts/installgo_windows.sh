33750da149e998372cbd78db98cffd52267298c0
Pawel Zak <pawel.zak.pawel@gmail.com>
2023-10-22 20:07:44 +0200

    Print debug info

--- scripts/installgo_windows.sh
+++ scripts/installgo_windows.sh
@@ -2,21 +2,29 @@
 
 set -eux
 
-GO_VERSION="1.21.2"
+GO_VERSION="1.21.3"
 
 setup_go () {
-    choco upgrade golang --allow-downgrade --version=${GO_VERSION} --verbose --installargs INSTALLDIR="C:\Go"
+    choco upgrade golang --allow-downgrade --version=${GO_VERSION} --verbose
     git config --system core.longpaths true
 }
 
-if command -v C:\Go\go >/dev/null 2>&1; then
+echo "PATH before: $PATH"
+echo "go location before: `command -v go`"
+
+if command -v go >/dev/null 2>&1; then
     echo "Go is already installed"
-    v=$(C:\Go\go version | { read -r _ _ v _; echo "${v#go}"; })
+    v=$(go version | { read -r _ _ v _; echo "${v#go}"; })
     echo "$v is installed, required version is ${GO_VERSION}"
     if [ "$v" != ${GO_VERSION} ]; then
         setup_go
-        C:\Go\go version
+        go version
     fi
 else
+    echo "Setup go"
     setup_go
 fi
+
+echo "PATH after: $PATH"
+echo "go location after: `command -v go`"
+go version
