3c0704faed2aad98af32598e2c5c77c220078590
Pawel Zak <pawel.zak.pawel@gmail.com>
2023-10-23 02:06:13 +0200

    Check what happens if there is the same Go version in cache

--- scripts/installgo_windows.sh
+++ scripts/installgo_windows.sh
@@ -9,36 +9,16 @@ setup_go () {
     git config --system core.longpaths true
 }
 
-#export PATH="/c/Go/bin:$PATH"
-#refreshenv
-
-#echo "PATH before: $PATH"
-#echo "go location before: $(command -v go)"
-#
-#'/c/Users/circleci/go/bin/go' version
-#'/c/Program Files/Go/bin/go' version
-#'/c/Go/bin/go' version
-
-command -v go
-go version
-
 if command -v go >/dev/null 2>&1; then
     echo "Go is already installed"
     v=$(go version | { read -r _ _ v _; echo "${v#go}"; })
     echo "$v is installed, required version is ${GO_VERSION}"
     if [ "$v" != ${GO_VERSION} ]; then
         setup_go
-        command -v go
-        go version
     fi
 else
     setup_go
-    command -v go
-    go version
 fi
 
-#echo "PATH after: $PATH"
-
-#'/c/Users/circleci/go/bin/go' version
-#'/c/Program Files/Go/bin/go' version
-#'/c/Go/bin/go' version
+command -v go
+go version
