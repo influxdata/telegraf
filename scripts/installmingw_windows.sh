6906c123053f64786708bb26efe0053a9f8dbe6e
Pawel Zak <pawel.zak.pawel@gmail.com>
2023-10-23 14:37:48 +0200

    Check mingw

---
+++ scripts/installmingw_windows.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -ux
+
+MINGW_VERSION="12.2.0.03042023"
+GCC_VERSION="12.2.0"
+
+setup_mingw () {
+    choco upgrade mingw --allow-downgrade --force --version=${MINGW_VERSION} --verbose --debug
+}
+
+echo "PATH: $PATH"
+command -v gcc
+gcc -dumpversion
+
+if command -v gcc >/dev/null 2>&1; then
+    echo "MinGW is already installed"
+    v=$(gcc -dumpversion)
+    echo "$v is installed, required version is ${GCC_VERSION}"
+    if [ "$v" != ${GCC_VERSION} ]; then
+        setup_mingw
+    fi
+else
+    setup_mingw
+fi
+
+echo "PATH: $PATH"
+command -v gcc
+gcc -dumpversion
