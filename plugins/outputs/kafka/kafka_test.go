2c218af53cb752f45e574b2cb5f6359a65446d24
Josh Powers <powersj@fastmail.com>
2023-10-31 07:52:54 -0600

    test(outputs.kafka): Use private network for testing
    This ensures the kafka output integration test will use a private
    network when running tests. This ensures that the default port, 9092, is
    available for testing by the kafka container.

--- plugins/outputs/kafka/kafka_test.go
+++ plugins/outputs/kafka/kafka_test.go
@@ -1,12 +1,14 @@
 package kafka
 
 import (
+	"context"
 	"testing"
 	"time"
 
 	"github.com/Shopify/sarama"
 	"github.com/docker/go-connections/nat"
 	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 
 	"github.com/influxdata/telegraf"
@@ -25,10 +27,25 @@ func TestConnectAndWriteIntegration(t *testing.T) {
 		t.Skip("Skipping integration test in short mode")
 	}
 
+	ctx := context.Background()
+	networkName := "telegraf-test-output-kafka-network"
+	network, err := testcontainers.GenericNetwork(ctx, testcontainers.GenericNetworkRequest{
+		NetworkRequest: testcontainers.NetworkRequest{
+			Name:           networkName,
+			Attachable:     true,
+			CheckDuplicate: true,
+		},
+	})
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, network.Remove(ctx), "terminating network failed")
+	}()
+
 	// Start the container as broker AND controller
 	container := testutil.Container{
 		Image:        "bitnami/kafka",
 		Hostname:     "localhost", // required to be able to resolve the name
+		Networks:     []string{networkName},
 		ExposedPorts: []string{"9092:9092", "9093:9093"},
 		Env: map[string]string{
 			"KAFKA_CFG_NODE_ID":                        "0",
