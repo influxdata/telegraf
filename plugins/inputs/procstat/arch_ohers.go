db55f885bd845f4e0a95654f3f6a02c111bd5fac
Sven Rebhan <srebhan@influxdata.com>
2023-11-17 12:23:51 +0100

    Consolidate remaining arch-specific implementations

--- plugins/inputs/procstat/arch_ohers.go
+++ plugins/inputs/procstat/arch_ohers.go
@@ -2,8 +2,18 @@
 
 package procstat
 
-import "github.com/shirou/gopsutil/v3/process"
+import (
+	"errors"
+
+	"github.com/shirou/gopsutil/v3/process"
+)
 
 func processName(p *process.Process) (string, error) {
 	return p.Exe()
 }
+
+func queryPidWithWinServiceName(_ string) (uint32, error) {
+	return 0, errors.New("os not supporting win_service option")
+}
+
+func collectMemmap(Process, string, map[string]any) {}
