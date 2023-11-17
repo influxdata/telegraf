945c88f8fd09a3d54a31238ed5c75db1b5014edb
Sven Rebhan <srebhan@influxdata.com>
2023-11-17 12:37:43 +0100

    Simplified pgrep finder

--- plugins/inputs/procstat/pgrep.go
+++ plugins/inputs/procstat/pgrep.go
@@ -40,69 +40,38 @@ func (pg *Pgrep) PidFile(path string) ([]PID, error) {
 
 func (pg *Pgrep) Pattern(pattern string) ([]PID, error) {
 	args := []string{pattern}
-	return find(pg.path, args)
+	return pg.find(args)
 }
 
 func (pg *Pgrep) UID(user string) ([]PID, error) {
 	args := []string{"-u", user}
-	return find(pg.path, args)
+	return pg.find(args)
 }
 
 func (pg *Pgrep) FullPattern(pattern string) ([]PID, error) {
 	args := []string{"-f", pattern}
-	return find(pg.path, args)
+	return pg.find(args)
 }
 
 func (pg *Pgrep) ChildPattern(pattern string) ([]PID, error) {
 	args := []string{"-P", pattern}
-	out, err := run(pg.path, args)
-	if err != nil {
-		return nil, err
-	}
+	return pg.find(args)
+}
 
-	pids := []PID{}
-	pid, err := strconv.ParseInt(pattern, 10, 32)
+func (pg *Pgrep) find(args []string) ([]PID, error) {
+	// Execute pgrep with the given arguments
+	buf, err := exec.Command(pg.path, args...).Output()
 	if err != nil {
-		return nil, err
-	}
-	pids = append(pids, PID(pid))
-
-	fields := strings.Fields(out)
-	for _, field := range fields {
-		pid, err := strconv.ParseInt(field, 10, 32)
-		if err != nil {
-			return pids, err
+		// Exit code 1 means "no processes found" so we should not return
+		// an error in this case.
+		if status, _ := internal.ExitStatus(err); status == 1 {
+			return nil, nil
 		}
-		pids = append(pids, PID(pid))
+		return nil, fmt.Errorf("error running %q: %w", pg.path, err)
 	}
+	out := string(buf)
 
-	return pids, nil
-}
-
-func find(path string, args []string) ([]PID, error) {
-	out, err := run(path, args)
-	if err != nil {
-		return nil, err
-	}
-
-	return parseOutput(out)
-}
-
-func run(path string, args []string) (string, error) {
-	out, err := exec.Command(path, args...).Output()
-
-	//if exit code 1, ie no processes found, do not return error
-	if i, _ := internal.ExitStatus(err); i == 1 {
-		return "", nil
-	}
-
-	if err != nil {
-		return "", fmt.Errorf("error running %q: %w", path, err)
-	}
-	return string(out), err
-}
-
-func parseOutput(out string) ([]PID, error) {
+	// Parse the command output to extract the PIDs
 	pids := []PID{}
 	fields := strings.Fields(out)
 	for _, field := range fields {
