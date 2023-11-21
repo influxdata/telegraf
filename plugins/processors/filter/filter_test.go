fba1318bcfb338fec0675294cb66a20173fd17cf
Sven Rebhan <srebhan@influxdata.com>
2023-11-21 15:43:05 +0100

    Update unit-tests

--- plugins/processors/filter/filter_test.go
+++ plugins/processors/filter/filter_test.go
@@ -5,68 +5,499 @@ import (
 	"time"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
 	"github.com/influxdata/telegraf/testutil"
 	"github.com/stretchr/testify/require"
 )
 
+var testmetrics = []telegraf.Metric{
+	metric.New(
+		"packing",
+		map[string]string{
+			"source":   "machine A",
+			"location": "main building",
+			"status":   "OK",
+		},
+		map[string]interface{}{
+			"operating_hours": 37,
+			"temperature":     23.1,
+		},
+		time.Unix(0, 0),
+	),
+	metric.New(
+		"foundry",
+		map[string]string{
+			"source":   "machine B",
+			"location": "factory X",
+			"status":   "OK",
+		},
+		map[string]interface{}{
+			"operating_hours": 1337,
+			"temperature":     19.9,
+			"pieces":          96878,
+		},
+		time.Unix(0, 0),
+	),
+	metric.New(
+		"welding",
+		map[string]string{
+			"source":   "machine C",
+			"location": "factory X",
+			"status":   "failure",
+		},
+		map[string]interface{}{
+			"operating_hours": 1009,
+			"temperature":     67.3,
+			"message":         "temperature alert",
+		},
+		time.Unix(0, 0),
+	),
+	metric.New(
+		"welding",
+		map[string]string{
+			"source":   "machine D",
+			"location": "factory Y",
+			"status":   "OK",
+		},
+		map[string]interface{}{
+			"operating_hours": 825,
+			"temperature":     31.2,
+		},
+		time.Unix(0, 0),
+	),
+}
+
+func TestNoRules(t *testing.T) {
+	plugin := &Filter{}
+	require.ErrorContains(t, plugin.Init(), "no rule(s) given")
+}
+
+func TestInvalidDefaultAction(t *testing.T) {
+	plugin := &Filter{
+		Rules:         []rule{{Name: []string{"foo"}}},
+		DefaultAction: "foo",
+	}
+	require.ErrorContains(t, plugin.Init(), "invalid default action")
+}
+
 func TestNoMetric(t *testing.T) {
-	processor := Noop{}
+	plugin := &Filter{
+		Rules: []rule{{Name: []string{"*"}}},
+	}
+	require.NoError(t, plugin.Init())
 
-	m := []telegraf.Metric{}
-	actual := processor.Apply(m...)
-	require.Empty(t, actual)
-	testutil.RequireMetricsEqual(t, m, actual)
+	input := []telegraf.Metric{}
+	require.Empty(t, plugin.Apply(input...))
 }
 
-func TestSingleMetric(t *testing.T) {
-	processor := Noop{}
-
-	m := []telegraf.Metric{
-		testutil.MustMetric(
-			"test",
-			map[string]string{
-				"tag": "tag_value",
-			},
-			map[string]interface{}{
-				"value": 42,
-			},
-			time.Now(),
-			telegraf.Gauge,
-		),
+func TestDropAll(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{{Name: []string{"*"}, Action: "drop"}},
 	}
-	actual := processor.Apply(m...)
-	require.Len(t, actual, 1)
-	testutil.RequireMetricsEqual(t, m, actual)
+	require.NoError(t, plugin.Init())
+	require.Empty(t, plugin.Apply(testmetrics...))
 }
 
-func TestMultipleMetrics(t *testing.T) {
-	processor := Noop{}
+func TestDropDefault(t *testing.T) {
+	plugin := &Filter{
+		Rules:         []rule{{Name: []string{"foo"}, Action: "pass"}},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+	require.Empty(t, plugin.Apply(testmetrics...))
+}
 
-	m := []telegraf.Metric{
-		testutil.MustMetric(
-			"test",
+func TestPassAll(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{{Name: []string{"*"}}},
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := testmetrics
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestPassDefault(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{{Name: []string{"foo"}, Action: "drop"}},
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := testmetrics
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestNamePass(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Name:   []string{"welding"},
+				Action: "pass",
+			},
+		},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"welding",
 			map[string]string{
-				"tag": "tag_value",
+				"source":   "machine C",
+				"location": "factory X",
+				"status":   "failure",
 			},
 			map[string]interface{}{
-				"value": 42,
+				"operating_hours": 1009,
+				"temperature":     67.3,
+				"message":         "temperature alert",
 			},
-			time.Now(),
-			telegraf.Gauge,
+			time.Unix(0, 0),
 		),
-		testutil.MustMetric(
-			"test",
+		metric.New(
+			"welding",
 			map[string]string{
-				"tag": "tag_value",
+				"source":   "machine D",
+				"location": "factory Y",
+				"status":   "OK",
 			},
 			map[string]interface{}{
-				"value": 42,
+				"operating_hours": 825,
+				"temperature":     31.2,
 			},
-			time.Now(),
-			telegraf.Gauge,
+			time.Unix(0, 0),
 		),
 	}
-	actual := processor.Apply(m...)
-	require.Len(t, actual, 2)
-	testutil.RequireMetricsEqual(t, m, actual)
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestNameDrop(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Name:   []string{"welding"},
+				Action: "drop",
+			},
+		},
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"packing",
+			map[string]string{
+				"source":   "machine A",
+				"location": "main building",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 37,
+				"temperature":     23.1,
+			},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"foundry",
+			map[string]string{
+				"source":   "machine B",
+				"location": "factory X",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 1337,
+				"temperature":     19.9,
+				"pieces":          96878,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestTagPass(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Tags:   map[string]string{"status": "OK"},
+				Action: "pass",
+			},
+		},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"packing",
+			map[string]string{
+				"source":   "machine A",
+				"location": "main building",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 37,
+				"temperature":     23.1,
+			},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"foundry",
+			map[string]string{
+				"source":   "machine B",
+				"location": "factory X",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 1337,
+				"temperature":     19.9,
+				"pieces":          96878,
+			},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"welding",
+			map[string]string{
+				"source":   "machine D",
+				"location": "factory Y",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 825,
+				"temperature":     31.2,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestTagDrop(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Tags:   map[string]string{"status": "OK"},
+				Action: "drop",
+			},
+		},
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"welding",
+			map[string]string{
+				"source":   "machine C",
+				"location": "factory X",
+				"status":   "failure",
+			},
+			map[string]interface{}{
+				"operating_hours": 1009,
+				"temperature":     67.3,
+				"message":         "temperature alert",
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestTagMultiple(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Tags: map[string]string{
+					"location": "factory X",
+					"status":   "OK",
+				},
+				Action: "pass",
+			},
+		},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"foundry",
+			map[string]string{
+				"source":   "machine B",
+				"location": "factory X",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 1337,
+				"temperature":     19.9,
+				"pieces":          96878,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestTagDoesNotExist(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Tags: map[string]string{
+					"operator": "peter",
+					"status":   "OK",
+				},
+				Action: "pass",
+			},
+		},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+
+	require.Empty(t, plugin.Apply(testmetrics...))
+}
+
+func TestFieldPass(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Fields: []string{"message", "pieces"},
+				Action: "pass",
+			},
+		},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"foundry",
+			map[string]string{
+				"source":   "machine B",
+				"location": "factory X",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 1337,
+				"temperature":     19.9,
+				"pieces":          96878,
+			},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"welding",
+			map[string]string{
+				"source":   "machine C",
+				"location": "factory X",
+				"status":   "failure",
+			},
+			map[string]interface{}{
+				"operating_hours": 1009,
+				"temperature":     67.3,
+				"message":         "temperature alert",
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestFieldDrop(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Fields: []string{"message", "pieces"},
+				Action: "drop",
+			},
+		},
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"packing",
+			map[string]string{
+				"source":   "machine A",
+				"location": "main building",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 37,
+				"temperature":     23.1,
+			},
+			time.Unix(0, 0),
+		),
+		metric.New(
+			"welding",
+			map[string]string{
+				"source":   "machine D",
+				"location": "factory Y",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 825,
+				"temperature":     31.2,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
+}
+
+func TestRuleOrder(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Name:   []string{"welding"},
+				Action: "drop",
+			},
+			{
+				Name:   []string{"welding"},
+				Action: "pass",
+			},
+		},
+		DefaultAction: "drop",
+	}
+	require.NoError(t, plugin.Init())
+	require.Empty(t, plugin.Apply(testmetrics...))
+}
+
+func TestRuleMultiple(t *testing.T) {
+	plugin := &Filter{
+		Rules: []rule{
+			{
+				Name:   []string{"welding"},
+				Action: "drop",
+			},
+			{
+				Name:   []string{"foundry"},
+				Action: "drop",
+			},
+		},
+		DefaultAction: "pass",
+	}
+	require.NoError(t, plugin.Init())
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"packing",
+			map[string]string{
+				"source":   "machine A",
+				"location": "main building",
+				"status":   "OK",
+			},
+			map[string]interface{}{
+				"operating_hours": 37,
+				"temperature":     23.1,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	actual := plugin.Apply(testmetrics...)
+	testutil.RequireMetricsEqual(t, expected, actual)
 }
