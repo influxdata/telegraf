716c5ddfab7111ef7ee6c7e95ef0bc5ade91b0a6
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2023-11-23 14:12:15 +0100

    Revert embedding FilterOptions

--- migrations/common/input_options.go
+++ migrations/common/input_options.go
@@ -13,5 +13,17 @@ type InputOptions struct {
 	Tags             map[string]string `toml:"tags,omitempty"`
 
 	// Filter options
-	FilterOptions
+	NamePass       []string            `toml:"namepass,omitempty"`
+	NameDrop       []string            `toml:"namedrop,omitempty"`
+	FieldPassOld   []string            `toml:"pass,omitempty"`
+	FieldPass      []string            `toml:"fieldpass,omitempty"`
+	FieldInclude   []string            `toml:"fieldinclude,omitempty"`
+	FieldDropOld   []string            `toml:"drop,omitempty"`
+	FieldDrop      []string            `toml:"fielddrop,omitempty"`
+	FieldExclude   []string            `toml:"fieldexclude,omitempty"`
+	TagPassFilters map[string][]string `toml:"tagpass,omitempty"`
+	TagDropFilters map[string][]string `toml:"tagdrop,omitempty"`
+	TagExclude     []string            `toml:"tagexclude,omitempty"`
+	TagInclude     []string            `toml:"taginclude,omitempty"`
+	MetricPass     string              `toml:"metricpass,omitempty"`
 }
