8445703ec226f51028fc47a28d42bdb3ff7d430e
Christian Allinson <christian.allinson@gmail.com>
2023-12-04 17:16:06 -0800

    Update plugins/inputs/opcua_listener/opcua_listener_test.go
    Co-authored-by: Thomas Casteleyn <thomas.casteleyn@me.com>

--- plugins/inputs/opcua_listener/opcua_listener_test.go
+++ plugins/inputs/opcua_listener/opcua_listener_test.go
@@ -163,8 +163,7 @@ func TestSubscribeClientIntegrationAdditionalFields(t *testing.T) {
 			wait.ForLog("TCP network layer listening on opc.tcp://"),
 		),
 	}
-	err := container.Start()
-	require.NoError(t, err, "failed to start container")
+	require.NoError(t, container.Start(), "failed to start container")
 	defer container.Terminate()
 
 	testopctags := []OPCTags{
