a3f8b4593d61cba323945ce46639a47b92677d87
elchenberg <elchenberg@users.noreply.github.com>
2023-12-22 13:47:33 +0100

    test if parseJSON() logs decoding errors without panicking

--- plugins/inputs/phpfpm/phpfpm_test.go
+++ plugins/inputs/phpfpm/phpfpm_test.go
@@ -6,9 +6,12 @@
 package phpfpm
 
 import (
+	"bytes"
 	"crypto/rand"
+	_ "embed"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"net/http/fcgi"
@@ -77,8 +80,6 @@ func TestPhpFpmGeneratesMetrics_From_Http(t *testing.T) {
 }
 
 func TestPhpFpmGeneratesJSONMetrics_From_Http(t *testing.T) {
-	outputSampleJSON, err := os.ReadFile("testdata/phpfpm.json")
-	require.NoError(t, err)
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/json")
 		w.Header().Set("Content-Length", strconv.Itoa(len(outputSampleJSON)))
@@ -360,7 +361,10 @@ max children reached: 2
 slow requests:        1
 `
 
-func TestPhpFpmLogWithoutPanic(t *testing.T) {
+//go:embed testdata/phpfpm.json
+var outputSampleJSON []byte
+
+func TestPhpFpmParseJSON_Log_Error_Without_Panic_When_When_JSON_Is_Invalid(t *testing.T) {
 	p := &phpfpm{}
 	// AddInput sets the Logger
 	if err := shim.New().AddInput(p); err != nil {
@@ -368,5 +372,20 @@ func TestPhpFpmLogWithoutPanic(t *testing.T) {
 		return
 	}
 
-	p.Log.Debug("test")
+	// capture log output
+	var logOutput bytes.Buffer
+	log.SetOutput(&logOutput)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+
+	// parse valid JSON without panic and without log output
+	validJSON := outputSampleJSON
+	require.NotPanics(t, func() { p.parseJSON(bytes.NewReader(validJSON), &testutil.NopAccumulator{}, "") })
+	require.Equal(t, logOutput.String(), "")
+
+	// parse invalid JSON without panic but with log output
+	invalidJSON := []byte("X")
+	require.NotPanics(t, func() { p.parseJSON(bytes.NewReader(invalidJSON), &testutil.NopAccumulator{}, "") })
+	require.Contains(t, logOutput.String(), "E! Unable to decode JSON response: invalid character 'X' looking for beginning of value")
 }
