5c5652b2cdf3b5beb4e7ebee57ea4a01814e438f
Sven Rebhan <srebhan@influxdata.com>
2024-01-16 13:13:00 +0100

    Cleanup metric v2 code

--- plugins/parsers/prometheus/metric_v2.go
+++ plugins/parsers/prometheus/metric_v2.go
@@ -1,8 +1,8 @@
 package prometheus
 
 import (
-	"fmt"
 	"math"
+	"strconv"
 	"time"
 
 	dto "github.com/prometheus/client_model/go"
@@ -12,127 +12,97 @@ import (
 )
 
 func (p *Parser) extractMetricsV2(metricFamilies map[string]*dto.MetricFamily) []telegraf.Metric {
-	var metrics []telegraf.Metric
-
 	now := time.Now()
 
-	// read metrics
+	// Convert each prometheus metric to a corresponding telegraf metric
+	// with one field each. The process will filter NaNs in values and skip
+	// the corresponding metrics.
+	var metrics []telegraf.Metric
 	for metricName, mf := range metricFamilies {
-		for _, m := range mf.Metric {
-			// reading tags
-			tags := GetTagsFromLabels(m, p.DefaultTags)
-			t := p.getTimestampV2(m, now)
+		mtype := mf.GetType()
+		for _, pm := range mf.Metric {
+			// Extract the timestamp of the metric if it exists and should
+			// not be ignored.
+			t := now
+			if ts := pm.GetTimestampMs(); !p.IgnoreTimestamp && ts > 0 {
+				t = time.UnixMilli(ts)
+			}
 
-			if mf.GetType() == dto.MetricType_SUMMARY {
-				// summary metric
-				telegrafMetrics := makeQuantilesV2(m, tags, metricName, mf.GetType(), t)
-				metrics = append(metrics, telegrafMetrics...)
-			} else if mf.GetType() == dto.MetricType_HISTOGRAM {
-				// histogram metric
-				telegrafMetrics := makeBucketsV2(m, tags, metricName, mf.GetType(), t)
-				metrics = append(metrics, telegrafMetrics...)
-			} else {
-				// standard metric
-				// reading fields
-				fields := getNameAndValueV2(m, metricName)
-				// converting to telegraf metric
-				if len(fields) > 0 {
-					m := metric.New("prometheus", tags, fields, t, ValueType(mf.GetType()))
+			// Convert the labels to tags
+			tags := GetTagsFromLabels(pm, p.DefaultTags)
+
+			// Construct the metrics
+			switch mtype {
+			case dto.MetricType_SUMMARY:
+				summary := pm.GetSummary()
+
+				// Add an overall metric containing the number of samples and and its sum
+				summaryFields := make(map[string]interface{})
+				summaryFields[metricName+"_count"] = float64(summary.GetSampleCount())
+				summaryFields[metricName+"_sum"] = summary.GetSampleSum()
+				metrics = append(metrics, metric.New("prometheus", tags, summaryFields, t, telegraf.Summary))
+
+				// Add one metric per quantile
+				for _, q := range summary.Quantile {
+					quantileTags := tags
+					quantileTags["quantile"] = strconv.FormatFloat(q.GetQuantile(), 'g', -1, 64)
+					quantileFields := map[string]interface{}{
+						metricName: q.GetValue(),
+					}
+					m := metric.New("prometheus", quantileTags, quantileFields, t, telegraf.Summary)
 					metrics = append(metrics, m)
+				}
+			case dto.MetricType_HISTOGRAM:
+				histogram := pm.GetHistogram()
+
+				// Add an overall metric containing the number of samples and and its sum
+				histFields := make(map[string]interface{})
+				histFields[metricName+"_count"] = float64(histogram.GetSampleCount())
+				histFields[metricName+"_sum"] = histogram.GetSampleSum()
+				metrics = append(metrics, metric.New("prometheus", tags, histFields, t, telegraf.Histogram))
+
+				// Add one metric per histogram bucket
+				var infSeen bool
+				for _, b := range histogram.Bucket {
+					bucketTags := tags
+					bucketTags["le"] = strconv.FormatFloat(b.GetUpperBound(), 'g', -1, 64)
+					bucketFields := map[string]interface{}{
+						metricName + "_bucket": float64(b.GetCumulativeCount()),
+					}
+					m := metric.New("prometheus", bucketTags, bucketFields, t, telegraf.Histogram)
+					metrics = append(metrics, m)
+
+					// Record if any of the buckets marks an infinite upper bound
+					infSeen = infSeen || math.IsInf(b.GetUpperBound(), +1)
+				}
+
+				// Infinity bucket is required for proper function of histogram in prometheus
+				if !infSeen {
+					infTags := tags
+					infTags["le"] = "+Inf"
+					infFields := map[string]interface{}{
+						metricName + "_bucket": float64(histogram.GetSampleCount()),
+					}
+					m := metric.New("prometheus", infTags, infFields, t, telegraf.Histogram)
+					metrics = append(metrics, m)
+				}
+			default:
+				v := math.Inf(1)
+				if gauge := pm.GetGauge(); gauge != nil {
+					v = gauge.GetValue()
+				} else if counter := pm.GetCounter(); counter != nil {
+					v = counter.GetValue()
+				} else if untyped := pm.GetUntyped(); untyped != nil {
+					v = untyped.GetValue()
+				}
+				if !math.IsNaN(v) {
+					fields := map[string]interface{}{metricName: v}
+					vtype := ValueType(mtype)
+					metrics = append(metrics, metric.New("prometheus", tags, fields, t, vtype))
 				}
 			}
 		}
 	}
 
 	return metrics
-}
-
-// Get Quantiles for summary metric & Buckets for histogram
-func makeQuantilesV2(m *dto.Metric, tags map[string]string, metricName string, metricType dto.MetricType, t time.Time) []telegraf.Metric {
-	metrics := make([]telegraf.Metric, 0, len(m.GetSummary().Quantile)+1)
-	fields := make(map[string]interface{})
-
-	fields[metricName+"_count"] = float64(m.GetSummary().GetSampleCount())
-	fields[metricName+"_sum"] = m.GetSummary().GetSampleSum()
-	met := metric.New("prometheus", tags, fields, t, ValueType(metricType))
-	metrics = append(metrics, met)
-
-	for _, q := range m.GetSummary().Quantile {
-		newTags := tags
-		fields = make(map[string]interface{})
-
-		newTags["quantile"] = fmt.Sprint(q.GetQuantile())
-		fields[metricName] = q.GetValue()
-
-		quantileMetric := metric.New("prometheus", newTags, fields, t, ValueType(metricType))
-		metrics = append(metrics, quantileMetric)
-	}
-	return metrics
-}
-
-// Get Buckets  from histogram metric
-func makeBucketsV2(m *dto.Metric, tags map[string]string, metricName string, metricType dto.MetricType, t time.Time) []telegraf.Metric {
-	metrics := make([]telegraf.Metric, 0, len(m.GetHistogram().Bucket)+2)
-	fields := make(map[string]interface{})
-
-	fields[metricName+"_count"] = float64(m.GetHistogram().GetSampleCount())
-	fields[metricName+"_sum"] = m.GetHistogram().GetSampleSum()
-
-	met := metric.New("prometheus", tags, fields, t, ValueType(metricType))
-	metrics = append(metrics, met)
-
-	infSeen := false
-	for _, b := range m.GetHistogram().Bucket {
-		newTags := tags
-		fields = make(map[string]interface{})
-		newTags["le"] = fmt.Sprint(b.GetUpperBound())
-		fields[metricName+"_bucket"] = float64(b.GetCumulativeCount())
-
-		histogramMetric := metric.New("prometheus", newTags, fields, t, ValueType(metricType))
-		metrics = append(metrics, histogramMetric)
-		if math.IsInf(b.GetUpperBound(), +1) {
-			infSeen = true
-		}
-	}
-	// Infinity bucket is required for proper function of histogram in prometheus
-	if !infSeen {
-		newTags := tags
-		newTags["le"] = "+Inf"
-
-		fields = make(map[string]interface{})
-		fields[metricName+"_bucket"] = float64(m.GetHistogram().GetSampleCount())
-
-		histogramInfMetric := metric.New("prometheus", newTags, fields, t, ValueType(metricType))
-		metrics = append(metrics, histogramInfMetric)
-	}
-	return metrics
-}
-
-// Get name and value from metric
-func getNameAndValueV2(m *dto.Metric, metricName string) map[string]interface{} {
-	fields := make(map[string]interface{})
-	if m.Gauge != nil {
-		if !math.IsNaN(m.GetGauge().GetValue()) {
-			fields[metricName] = m.GetGauge().GetValue()
-		}
-	} else if m.Counter != nil {
-		if !math.IsNaN(m.GetCounter().GetValue()) {
-			fields[metricName] = m.GetCounter().GetValue()
-		}
-	} else if m.Untyped != nil {
-		if !math.IsNaN(m.GetUntyped().GetValue()) {
-			fields[metricName] = m.GetUntyped().GetValue()
-		}
-	}
-	return fields
-}
-
-func (p *Parser) getTimestampV2(m *dto.Metric, now time.Time) time.Time {
-	var t time.Time
-	if !p.IgnoreTimestamp && m.TimestampMs != nil && *m.TimestampMs > 0 {
-		t = time.Unix(0, m.GetTimestampMs()*1000000)
-	} else {
-		t = now
-	}
-	return t
 }
