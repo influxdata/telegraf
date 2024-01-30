13009608a4e84d99ec4ad76ba67e2f3f8e962700
Sven Rebhan <srebhan@influxdata.com>
2024-01-30 14:11:47 +0100

    Move tests without device to test-cases

--- plugins/inputs/temp/temp_test.go
+++ plugins/inputs/temp/temp_test.go
@@ -8,10 +8,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/metric"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/influxdata/telegraf/plugins/parsers/influx"
 	"github.com/influxdata/telegraf/testutil"
 )
 
@@ -21,204 +25,6 @@ func TestTemperatureInvaldiMetricFormat(t *testing.T) {
 		Log:          &testutil.Logger{},
 	}
 	require.ErrorContains(t, plugin.Init(), "invalid 'metric_format'")
-}
-
-func TestTemperatureMetricV1(t *testing.T) {
-	expected := []telegraf.Metric{
-		// hwmon0 / temp1
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_alarm"},
-			map[string]interface{}{"active": false},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_crit"},
-			map[string]interface{}{"temp": 84.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_input"},
-			map[string]interface{}{"temp": 35.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_max"},
-			map[string]interface{}{"temp": 81.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_min"},
-			map[string]interface{}{"temp": -273.15},
-			time.Unix(0, 0),
-		),
-		// hwmon0 / temp2
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor1_input"},
-			map[string]interface{}{"temp": 35.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor1_max"},
-			map[string]interface{}{"temp": 65261.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor1_min"},
-			map[string]interface{}{"temp": -273.15},
-			time.Unix(0, 0),
-		),
-		// hwmon0 / temp3
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor2_input"},
-			map[string]interface{}{"temp": 38.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor2_max"},
-			map[string]interface{}{"temp": 65261.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor2_min"},
-			map[string]interface{}{"temp": -273.15},
-			time.Unix(0, 0),
-		),
-		// hwmon1 / temp1
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "k10temp_tctl_input"},
-			map[string]interface{}{"temp": 33.25},
-			time.Unix(0, 0),
-		),
-		// hwmon1 / temp3
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "k10temp_tccd1_input"},
-			map[string]interface{}{"temp": 33.25},
-			time.Unix(0, 0),
-		),
-	}
-
-	require.NoError(t, os.Setenv("HOST_SYS", filepath.Join("testcases", "general", "sys")))
-
-	plugin := &Temperature{
-		MetricFormat: "v1",
-		Log:          &testutil.Logger{},
-	}
-	require.NoError(t, plugin.Init())
-
-	var acc testutil.Accumulator
-	require.NoError(t, plugin.Gather(&acc))
-	actual := acc.GetTelegrafMetrics()
-	testutil.RequireMetricsEqual(t, expected, actual, testutil.IgnoreTime(), testutil.SortMetrics())
-}
-
-func TestTemperature(t *testing.T) {
-	expected := []telegraf.Metric{
-		// hwmon0 / temp1
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_alarm"},
-			map[string]interface{}{"active": false},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_crit"},
-			map[string]interface{}{"temp": 84.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_input"},
-			map[string]interface{}{"temp": 35.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_max"},
-			map[string]interface{}{"temp": 81.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_composite_min"},
-			map[string]interface{}{"temp": -273.15},
-			time.Unix(0, 0),
-		),
-		// hwmon0 / temp2
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor_1_input"},
-			map[string]interface{}{"temp": 35.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor_1_max"},
-			map[string]interface{}{"temp": 65261.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor_1_min"},
-			map[string]interface{}{"temp": -273.15},
-			time.Unix(0, 0),
-		),
-		// hwmon0 / temp3
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor_2_input"},
-			map[string]interface{}{"temp": 38.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor_2_max"},
-			map[string]interface{}{"temp": 65261.85},
-			time.Unix(0, 0),
-		),
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "nvme_sensor_2_min"},
-			map[string]interface{}{"temp": -273.15},
-			time.Unix(0, 0),
-		),
-		// hwmon1 / temp1
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "k10temp_tctl_input"},
-			map[string]interface{}{"temp": 33.25},
-			time.Unix(0, 0),
-		),
-		// hwmon1 / temp3
-		metric.New(
-			"temp",
-			map[string]string{"sensor": "k10temp_tccd1_input"},
-			map[string]interface{}{"temp": 33.25},
-			time.Unix(0, 0),
-		),
-	}
-
-	require.NoError(t, os.Setenv("HOST_SYS", filepath.Join("testcases", "general", "sys")))
-	plugin := &Temperature{Log: &testutil.Logger{}}
-	require.NoError(t, plugin.Init())
-
-	var acc testutil.Accumulator
-	require.NoError(t, plugin.Gather(&acc))
-	actual := acc.GetTelegrafMetrics()
-	testutil.RequireMetricsEqual(t, expected, actual, testutil.IgnoreTime(), testutil.SortMetrics())
 }
 
 func TestTemperatureNameCollisions(t *testing.T) {
@@ -469,5 +275,100 @@ func TestTemperatureWithDeviceTag(t *testing.T) {
 	var acc testutil.Accumulator
 	require.NoError(t, plugin.Gather(&acc))
 	actual := acc.GetTelegrafMetrics()
+	testutil.PrintMetrics(actual)
 	testutil.RequireMetricsEqual(t, expected, actual, testutil.IgnoreTime(), testutil.SortMetrics())
+}
+
+func TestCases(t *testing.T) {
+	// Get all directories in testdata
+	folders, err := os.ReadDir("testcases")
+	require.NoError(t, err)
+
+	// Register the plugin
+	inputs.Add("temp", func() telegraf.Input {
+		return &Temperature{}
+	})
+
+	// Prepare the influx parser for expectations
+	parser := &influx.Parser{}
+	require.NoError(t, parser.Init())
+
+	for _, f := range folders {
+		// Only handle folders
+		if !f.IsDir() {
+			continue
+		}
+		// Compare options
+		options := []cmp.Option{
+			testutil.IgnoreTime(),
+			testutil.SortMetrics(),
+		}
+
+		// Test v1
+		t.Run(f.Name()+"_v1", func(t *testing.T) {
+			testcasePath := filepath.Join("testcases", f.Name())
+			configFilename := filepath.Join(testcasePath, "telegraf.conf")
+			expectedFilename := filepath.Join(testcasePath, "expected_v1.out")
+
+			// Read the expected output if any
+			var expected []telegraf.Metric
+			if _, err := os.Stat(expectedFilename); err == nil {
+				var err error
+				expected, err = testutil.ParseMetricsFromFile(expectedFilename, parser)
+				require.NoError(t, err)
+			}
+
+			// Prepare the environment
+			require.NoError(t, os.Setenv("HOST_SYS", filepath.Join(testcasePath, "sys")))
+
+			// Configure the plugin
+			cfg := config.NewConfig()
+			require.NoError(t, cfg.LoadConfig(configFilename))
+			require.Len(t, cfg.Inputs, 1)
+			plugin := cfg.Inputs[0].Input.(*Temperature)
+			plugin.MetricFormat = "v1"
+			require.NoError(t, plugin.Init())
+
+			var acc testutil.Accumulator
+			require.NoError(t, plugin.Gather(&acc))
+
+			// Check the metric nevertheless as we might get some metrics despite errors.
+			actual := acc.GetTelegrafMetrics()
+			testutil.RequireMetricsEqual(t, expected, actual, options...)
+		})
+
+		// Test v2
+		t.Run(f.Name()+"_v2", func(t *testing.T) {
+			testcasePath := filepath.Join("testcases", f.Name())
+			configFilename := filepath.Join(testcasePath, "telegraf.conf")
+			expectedFilename := filepath.Join(testcasePath, "expected_v2.out")
+
+			// Read the expected output if any
+			var expected []telegraf.Metric
+			if _, err := os.Stat(expectedFilename); err == nil {
+				var err error
+				expected, err = testutil.ParseMetricsFromFile(expectedFilename, parser)
+				require.NoError(t, err)
+			}
+
+			// Prepare the environment
+			require.NoError(t, os.Setenv("HOST_SYS", filepath.Join(testcasePath, "sys")))
+
+			// Configure the plugin
+			cfg := config.NewConfig()
+			require.NoError(t, cfg.LoadConfig(configFilename))
+			require.Len(t, cfg.Inputs, 1)
+			plugin := cfg.Inputs[0].Input.(*Temperature)
+			plugin.MetricFormat = "v2"
+			require.NoError(t, plugin.Init())
+
+			var acc testutil.Accumulator
+			require.NoError(t, plugin.Gather(&acc))
+
+			// Check the metric nevertheless as we might get some metrics despite errors.
+			actual := acc.GetTelegrafMetrics()
+			testutil.PrintMetrics(acc.GetTelegrafMetrics())
+			testutil.RequireMetricsEqual(t, expected, actual, options...)
+		})
+	}
 }
