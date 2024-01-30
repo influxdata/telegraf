375caaa1d868ad08db364e1573f99d0c29b7d94b
Joshua Powers <powersj@fastmail.com>
2024-01-30 10:25:15 -0700

    chore: Disable firewall during nightly package tests (#14652)

--- scripts/install_incus.sh
+++ scripts/install_incus.sh
@@ -22,8 +22,26 @@ networks:
 - name: incusbr0
   type: bridge
   config:
-    ipv4.address: auto
+    ipv4.address: 192.168.100.1/24
+    ipv4.nat: true
     ipv6.address: none
+storage_pools:
+- name: default
+  driver: dir
+profiles:
+- name: default
+  devices:
+    eth0:
+      name: eth0
+      network: incusbr0
+      type: nic
+    root:
+      path: /
+      pool: default
+      type: disk
 EOF
+
+# Disable the firewall to prevent issues with the default configuration.
+sudo ufw disable
 
 sudo usermod -a -G incus-admin "$(whoami)"
