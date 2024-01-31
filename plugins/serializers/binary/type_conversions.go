ca83fa2be6da0215165d78882690d14c0fa86a8e
Sven Rebhan <srebhan@influxdata.com>
2024-01-31 13:36:47 +0100

    Use internal type-conversion for float32

--- plugins/serializers/binary/type_conversions.go
+++ plugins/serializers/binary/type_conversions.go
@@ -2,9 +2,7 @@ package binary
 
 import (
 	"encoding/binary"
-	"fmt"
 	"math"
-	"strconv"
 
 	"github.com/influxdata/telegraf/internal"
 )
@@ -100,68 +98,13 @@ func (e *Entry) convertToFloat64(value interface{}, order binary.ByteOrder) ([]b
 }
 
 func (e *Entry) convertToFloat32(value interface{}, order binary.ByteOrder) ([]byte, error) {
-	var v float32
-	switch raw := value.(type) {
-	case string:
-		x, err := strconv.ParseFloat(raw, 32)
-		if err != nil {
-			return nil, err
-		}
-		v = float32(x)
-	case []byte:
-		x, err := strconv.ParseFloat(string(raw), 32)
-		if err != nil {
-			return nil, err
-		}
-		v = float32(x)
-	case fmt.Stringer:
-		x, err := strconv.ParseFloat(raw.String(), 32)
-		if err != nil {
-			return nil, err
-		}
-		v = float32(x)
-	case bool:
-		if raw {
-			v = 1
-		} else {
-			v = 0
-		}
-	case int:
-		v = float32(raw)
-	case int8:
-		v = float32(raw)
-	case int16:
-		v = float32(raw)
-	case int32:
-		v = float32(raw)
-	case int64:
-		v = float32(raw)
-	case uint:
-		v = float32(raw)
-	case uint8:
-		v = float32(raw)
-	case uint16:
-		v = float32(raw)
-	case uint32:
-		v = float32(raw)
-	case uint64:
-		v = float32(raw)
-	case float32:
-		v = raw
-	case float64:
-		if raw < -math.MaxFloat32 || raw > math.MaxFloat32 {
-			return nil, fmt.Errorf("overflow/underflow with %v for field %q", raw, e.ReadFrom)
-		}
-		v = float32(raw)
-	case nil:
-		v = 0
-	default:
-		return nil, fmt.Errorf("type \"%T\" unsupported", value)
+	v, err := internal.ToFloat32(value)
+	if err != nil {
+		return nil, err
 	}
 
 	buf := make([]byte, 4)
 	x := math.Float32bits(v)
 	order.PutUint32(buf, x)
-
 	return buf, nil
 }
