9d85c53fabc098c11b2a5e40d5104c3a6e719f12
Sven Rebhan <srebhan@influxdata.com>
2024-02-13 23:49:01 +0100

    Migrate list-files test

--- plugins/inputs/systemd_units/systemd_units_test.go
+++ plugins/inputs/systemd_units/systemd_units_test.go
@@ -1,84 +1,273 @@
 package systemd_units
 
 import (
-	"bytes"
-	"reflect"
-	"sort"
-	"strings"
+	"context"
+	"errors"
+	"fmt"
 	"testing"
+	"time"
 
+	sdbus "github.com/coreos/go-systemd/v22/dbus"
+	"github.com/godbus/dbus/v5"
+	"github.com/stretchr/testify/require"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/config"
+	"github.com/influxdata/telegraf/filter"
+	"github.com/influxdata/telegraf/metric"
 	"github.com/influxdata/telegraf/testutil"
 )
 
-// Global test definitions and structure.
-// Tests are located within `subcommand_list_test.go` and
-// `subcommand_show_test.go`.
-
-type TestDef struct {
-	Name   string
-	Line   string
-	Lines  []string
-	Tags   map[string]string
-	Fields map[string]interface{}
-	Status int
-	Err    error
+type properties struct {
+	utype      string
+	state      sdbus.UnitStatus
+	ufPreset   string
+	ufState    string
+	properties map[string]interface{}
 }
 
-func runParserTests(t *testing.T, tests []TestDef, dut *subCommandInfo) {
+func TestListFiles(t *testing.T) {
+	tests := []struct {
+		name        string
+		properties  map[string]properties
+		expected    []telegraf.Metric
+		expectedErr string
+	}{
+		{
+			name: "example loaded active running",
+			properties: map[string]properties{
+				"example.service": {
+					utype: "Service",
+					state: sdbus.UnitStatus{
+						Name:        "example.service",
+						LoadState:   "loaded",
+						ActiveState: "active",
+						SubState:    "running",
+					},
+				},
+			},
+			expected: []telegraf.Metric{
+				metric.New(
+					"systemd_units",
+					map[string]string{
+						"name":   "example.service",
+						"load":   "loaded",
+						"active": "active",
+						"sub":    "running",
+					},
+					map[string]interface{}{
+						"load_code":   0,
+						"active_code": 0,
+						"sub_code":    0,
+					},
+					time.Unix(0, 0),
+				),
+			},
+		},
+		{
+			name: "example loaded active exited",
+			properties: map[string]properties{
+				"example.service": {
+					utype: "Service",
+					state: sdbus.UnitStatus{
+						Name:        "example.service",
+						LoadState:   "loaded",
+						ActiveState: "active",
+						SubState:    "exited",
+					},
+				},
+			},
+			expected: []telegraf.Metric{
+				metric.New(
+					"systemd_units",
+					map[string]string{
+						"name":   "example.service",
+						"load":   "loaded",
+						"active": "active",
+						"sub":    "exited",
+					},
+					map[string]interface{}{
+						"load_code":   0,
+						"active_code": 0,
+						"sub_code":    4,
+					},
+					time.Unix(0, 0),
+				),
+			},
+		},
+		{
+			name: "example loaded failed failed",
+			properties: map[string]properties{
+				"example.service": {
+					utype: "Service",
+					state: sdbus.UnitStatus{
+						Name:        "example.service",
+						LoadState:   "loaded",
+						ActiveState: "failed",
+						SubState:    "failed",
+					},
+				},
+			},
+			expected: []telegraf.Metric{
+				metric.New(
+					"systemd_units",
+					map[string]string{
+						"name":   "example.service",
+						"load":   "loaded",
+						"active": "failed",
+						"sub":    "failed",
+					},
+					map[string]interface{}{
+						"load_code":   0,
+						"active_code": 3,
+						"sub_code":    12,
+					},
+					time.Unix(0, 0),
+				),
+			},
+		},
+		{
+			name: "example not-found inactive dead",
+			properties: map[string]properties{
+				"example.service": {
+					utype: "Service",
+					state: sdbus.UnitStatus{
+						Name:        "example.service",
+						LoadState:   "not-found",
+						ActiveState: "inactive",
+						SubState:    "dead",
+					},
+				},
+			},
+			expected: []telegraf.Metric{
+				metric.New(
+					"systemd_units",
+					map[string]string{
+						"name":   "example.service",
+						"load":   "not-found",
+						"active": "inactive",
+						"sub":    "dead",
+					},
+					map[string]interface{}{
+						"load_code":   2,
+						"active_code": 2,
+						"sub_code":    1,
+					},
+					time.Unix(0, 0),
+				),
+			},
+		},
+		{
+			name: "example unknown unknown unknown",
+			properties: map[string]properties{
+				"example.service": {
+					utype: "Service",
+					state: sdbus.UnitStatus{
+						Name:        "example.service",
+						LoadState:   "unknown",
+						ActiveState: "unknown",
+						SubState:    "unknown",
+					},
+				},
+			},
+			expectedErr: "parsing field 'load' failed, value not in map",
+		},
+	}
+
 	for _, tt := range tests {
-		t.Run(tt.Name, func(t *testing.T) {
-			acc := new(testutil.Accumulator)
-
-			var line string
-			if len(tt.Lines) > 0 && len(tt.Line) == 0 {
-				line = strings.Join(tt.Lines, "\n")
-			} else if len(tt.Lines) == 0 && len(tt.Line) > 0 {
-				line = tt.Line
-			} else {
-				t.Error("property Line and Lines set in test definition")
+		t.Run(tt.name, func(t *testing.T) {
+			// Setup plugin. Do NOT call Start() as this would connect to
+			// the real systemd daemon.
+			plugin := &SystemdUnits{
+				Pattern: "examp*",
+				Timeout: config.Duration(time.Second),
 			}
+			require.NoError(t, plugin.Init())
 
-			dut.parseResult(acc, bytes.NewBufferString(line))
-			err := acc.FirstError()
+			// Create a fake client to inject data
+			plugin.client = &fakeClient{
+				units:     tt.properties,
+				connected: true,
+			}
+			defer plugin.Stop()
 
-			if !reflect.DeepEqual(tt.Err, err) {
-				t.Errorf("%s: expected error '%#v' got '%#v'", tt.Name, tt.Err, err)
+			// Run gather
+			var acc testutil.Accumulator
+			err := acc.GatherError(plugin.Gather)
+			if tt.expectedErr != "" {
+				require.ErrorContains(t, err, tt.expectedErr)
+				return
 			}
-			if len(acc.Metrics) > 0 {
-				m := acc.Metrics[0]
-				if !reflect.DeepEqual(m.Measurement, measurement) {
-					t.Errorf("%s: expected measurement '%#v' got '%#v'\n", tt.Name, measurement, m.Measurement)
-				}
-				if !reflect.DeepEqual(m.Tags, tt.Tags) {
-					t.Errorf("%s: expected tags\n%#v got\n%#v\n", tt.Name, tt.Tags, m.Tags)
-				}
-				if !reflect.DeepEqual(m.Fields, tt.Fields) {
-					t.Errorf("%s: expected fields\n%#v got\n%#v\n", tt.Name, tt.Fields, m.Fields)
-				}
-			}
+			require.NoError(t, err)
+
+			// Do the comparison
+			actual := acc.GetTelegrafMetrics()
+			testutil.RequireMetricsEqual(t, tt.expected, actual, testutil.IgnoreTime())
 		})
 	}
 }
 
-func runCommandLineTest(t *testing.T, paramsTemplate []string, dut *subCommandInfo, systemdUnits *SystemdUnits) {
-	params := *dut.getParameters(systemdUnits)
+type fakeClient struct {
+	units     map[string]properties
+	connected bool
+}
 
-	// Because we sort the params and the template array before comparison
-	// we have to compare the positional parameters first.
-	for i, v := range paramsTemplate {
-		if strings.HasPrefix(v, "--") {
-			break
-		}
+func (c *fakeClient) Connected() bool {
+	return !c.connected
+}
 
-		if v != params[i] {
-			t.Errorf("Positional parameter %d is '%s'. Expected '%s'.", i, params[i], v)
+func (c *fakeClient) Close() {
+	c.connected = false
+}
+
+func (c *fakeClient) ListUnitFilesByPatternsContext(_ context.Context, _, pattern []string) ([]sdbus.UnitFile, error) {
+	f := filter.MustCompile(pattern)
+
+	var files []sdbus.UnitFile
+	for name := range c.units {
+		if f.Match(name) {
+			files = append(files, sdbus.UnitFile{Path: "/usr/lib/systemd/system/" + name, Type: "unknown"})
 		}
 	}
-	// Because the maps do not lead to a stable order of the "--property"
-	// arguments sort all the command line arguments and compare them.
-	sort.Strings(params)
-	sort.Strings(paramsTemplate)
-	if !reflect.DeepEqual(params, paramsTemplate) {
-		t.Errorf("Generated list of command line arguments '%#v' do not match expected list command line arguments '%#v'", params, paramsTemplate)
+	return files, nil
+}
+
+func (c *fakeClient) ListUnitsByNamesContext(_ context.Context, units []string) ([]sdbus.UnitStatus, error) {
+	var states []sdbus.UnitStatus
+	for name, u := range c.units {
+		for _, requestedName := range units {
+			if name == requestedName {
+				states = append(states, u.state)
+				break
+			}
+		}
 	}
+
+	return states, nil
+
+}
+func (c *fakeClient) GetUnitTypePropertiesContext(_ context.Context, unit, unitType string) (map[string]interface{}, error) {
+	u, found := c.units[unit]
+	if !found {
+		return nil, nil
+	}
+	if u.utype != unitType {
+		return nil, fmt.Errorf("Unknown interface 'org.freedesktop.systemd1.%s", unitType)
+	}
+	return u.properties, nil
+}
+
+func (c *fakeClient) GetUnitPropertyContext(ctx context.Context, unit, propertyName string) (*sdbus.Property, error) {
+	u, found := c.units[unit]
+	if !found {
+		return nil, nil
+	}
+
+	switch propertyName {
+	case "UnitFileState":
+		return &sdbus.Property{Name: propertyName, Value: dbus.MakeVariant(u.ufState)}, nil
+	case "UnitFilePreset":
+		return &sdbus.Property{Name: propertyName, Value: dbus.MakeVariant(u.ufPreset)}, nil
+	}
+	return nil, errors.New("unknown property")
 }
