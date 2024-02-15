15abb6c98eacb975067fe69c8927df82fa02b4ff
Sven Rebhan <srebhan@influxdata.com>
2024-02-15 15:48:09 +0100

    Add test for URI parsing

--- plugins/inputs/postgresql/postgresql_test.go
+++ plugins/inputs/postgresql/postgresql_test.go
@@ -318,6 +318,74 @@ func TestPostgresqlDatabaseBlacklistTestIntegration(t *testing.T) {
 	require.True(t, foundTemplate1)
 }
 
+func TestURIParsing(t *testing.T) {
+	tests := []struct {
+		name     string
+		uri      string
+		expected string
+	}{
+		{
+			name:     "short",
+			uri:      `postgres://localhost`,
+			expected: "host=localhost",
+		},
+		{
+			name:     "with port",
+			uri:      `postgres://localhost:5432`,
+			expected: "host=localhost port=5432",
+		},
+		{
+			name:     "with database",
+			uri:      `postgres://localhost/mydb`,
+			expected: "dbname=mydb host=localhost",
+		},
+		{
+			name:     "with additional parameters",
+			uri:      `postgres://localhost/mydb?application_name=pgxtest&search_path=myschema&connect_timeout=5`,
+			expected: "application_name=pgxtest connect_timeout=5 dbname=mydb host=localhost search_path=myschema",
+		},
+		{
+			name:     "with database setting in params",
+			uri:      `postgres://localhost:5432/?database=mydb`,
+			expected: "database=mydb host=localhost port=5432",
+		},
+		{
+			name:     "with authentication",
+			uri:      `postgres://jack:secret@localhost:5432/mydb?sslmode=prefer`,
+			expected: "dbname=mydb host=localhost password=secret port=5432 sslmode=prefer user=jack",
+		},
+		{
+			name:     "with spaces",
+			uri:      `postgres://jack%20hunter:secret@localhost:5432/mydb?application_name=pgx%20test&search_path=myschema&connect_timeout=5`,
+			expected: "application_name='pgx test' connect_timeout=5 dbname=mydb host=localhost password=secret port=5432 search_path=myschema user='jack hunter'",
+		},
+		{
+			name:     "with equal signs",
+			uri:      `postgres://jack%20hunter:secret@localhost:5432/mydb?application_name=pgx%3Dtest&search_path=myschema&connect_timeout=5`,
+			expected: "application_name='pgx=test' connect_timeout=5 dbname=mydb host=localhost password=secret port=5432 search_path=myschema user='jack hunter'",
+		},
+		{
+			name:     "multiple hosts",
+			uri:      `postgres://jack:secret@foo:1,bar:2,baz:3/mydb?sslmode=disable`,
+			expected: "dbname=mydb host=foo,bar,baz password=secret port=1,2,3 sslmode=disable user=jack",
+		},
+		{
+			name:     "multiple hosts without ports",
+			uri:      `postgres://jack:secret@foo,bar,baz/mydb?sslmode=disable`,
+			expected: "dbname=mydb host=foo,bar,baz password=secret sslmode=disable user=jack",
+		},
+	}
+
+	for _, tt := range tests {
+		// Key value without spaces around equal sign
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := parseURL(tt.uri)
+			require.NoError(t, err)
+			require.Equalf(t, tt.expected, actual, "initial: %s", tt.uri)
+		})
+	}
+}
+
 func TestSanitizeAddressKeyValue(t *testing.T) {
 	keys := []string{"password", "sslcert", "sslkey", "sslmode", "sslrootcert"}
 	tests := []struct {
