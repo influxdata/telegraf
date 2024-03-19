824547e254fb71303e88bb83bada6f6cc1d857ab
Matthias Alphart <farmio@alphart.net>
2024-03-19 03:08:22 +0100

    fix(inputs.knx_listener): Ignore GroupValueRead requests (#15007)
    (cherry picked from commit a0cf90b13c7b7d832f9d3feedd872cb7bba5599b)

--- plugins/inputs/knx_listener/knx_listener.go
+++ plugins/inputs/knx_listener/knx_listener.go
@@ -146,6 +146,12 @@ func (kl *KNXListener) Stop() {
 
 func (kl *KNXListener) listen(acc telegraf.Accumulator) {
 	for msg := range kl.client.Inbound() {
+		if msg.Command == knx.GroupRead {
+			// Ignore GroupValue_Read requests as they would either
+			// - fail to unpack due to invalid data length (DPT != 1) or
+			// - create invalid `false` values as their data always unpacks `0` (DPT 1)
+			continue
+		}
 		// Match GA to DataPointType and measurement name
 		ga := msg.Destination.String()
 		target, ok := kl.gaTargetMap[ga]
