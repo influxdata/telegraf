influxdata/telegraf

commit 4d4c9b3d88e57f7da98f3fbbbd79b3995c85af06

Resolve linter issues

plugins/inputs/lustre2/lustre2.go
@@ -380,13 +380,13 @@ func (*Lustre2) SampleConfig() string {
 }
 
 func (l *Lustre2) GetLustreHealth() error {
-	filename := filepath.Join(l.rootdir, "/sys/fs/lustre/health_check")
+	filename := filepath.Join(l.rootdir, "sys", "fs", "lustre", "health_check")
 	if _, err := os.Stat(filename); err != nil {
 		// try falling back to the old procfs location
 		// it was moved in https://github.com/lustre/lustre-release/commit/5d368bd0b2
-		filename = filepath.Join(l.rootdir, "/proc/fs/lustre/health_check")
+		filename = filepath.Join(l.rootdir, "proc", "fs", "lustre", "health_check")
 		if _, err = os.Stat(filename); err != nil {
-			return nil
+			return nil //nolint: nilerr // we don't want to return an error if the file doesn't exist
 		}
 	}
 	contents, err := os.ReadFile(filename)
@@ -395,7 +395,7 @@ func (l *Lustre2) GetLustreHealth() error {
 	}
 
 	value := strings.TrimSpace(string(contents))
-	var health uint64 = 0
+	var health uint64
 	if value == "healthy" {
 		health = 1
 	}

commit 28845f1b9f66bc668ce6ad4b8d21f68245c6adc8

lustre2: fix bug introduced by 4d4c9b3d88

plugins/inputs/lustre2/lustre2.go
@@ -380,11 +380,11 @@ func (*Lustre2) SampleConfig() string {
 }
 
 func (l *Lustre2) GetLustreHealth() error {
-	filename := filepath.Join(l.rootdir, "sys", "fs", "lustre", "health_check")
+	filename := filepath.Join(l.rootdir, "/sys", "fs", "lustre", "health_check")
 	if _, err := os.Stat(filename); err != nil {
 		// try falling back to the old procfs location
 		// it was moved in https://github.com/lustre/lustre-release/commit/5d368bd0b2
-		filename = filepath.Join(l.rootdir, "proc", "fs", "lustre", "health_check")
+		filename = filepath.Join(l.rootdir, "/proc", "fs", "lustre", "health_check")
 		if _, err = os.Stat(filename); err != nil {
 			return nil //nolint: nilerr // we don't want to return an error if the file doesn't exist
 		}