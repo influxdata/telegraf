f76d2d7d0018cc15ed3e37fe2ff501016ec7bb03
Sven Rebhan <srebhan@influxdata.com>
2024-04-25 11:49:28 +0200

    Move telegraf logger definition to own file

---
+++ logger.go
@@ -0,0 +1,23 @@
+package telegraf
+
+var Debug bool
+
+// Logger defines an plugin-related interface for logging.
+type Logger interface {
+	// Errorf logs an error message, patterned after log.Printf.
+	Errorf(format string, args ...interface{})
+	// Error logs an error message, patterned after log.Print.
+	Error(args ...interface{})
+	// Debugf logs a debug message, patterned after log.Printf.
+	Debugf(format string, args ...interface{})
+	// Debug logs a debug message, patterned after log.Print.
+	Debug(args ...interface{})
+	// Warnf logs a warning message, patterned after log.Printf.
+	Warnf(format string, args ...interface{})
+	// Warn logs a warning message, patterned after log.Print.
+	Warn(args ...interface{})
+	// Infof logs an information message, patterned after log.Printf.
+	Infof(format string, args ...interface{})
+	// Info logs an information message, patterned after log.Print.
+	Info(args ...interface{})
+}
--- plugin.go
+++ plugin.go
@@ -1,7 +1,5 @@
 package telegraf
 
-var Debug bool
-
 // Escalation level for the plugin or option
 type Escalation int
 
@@ -79,24 +77,4 @@ type StatefulPlugin interface {
 	// SetState is called by the Persister once after loading and
 	// initialization (after Init() function).
 	SetState(state interface{}) error
-}
-
-// Logger defines an plugin-related interface for logging.
-type Logger interface {
-	// Errorf logs an error message, patterned after log.Printf.
-	Errorf(format string, args ...interface{})
-	// Error logs an error message, patterned after log.Print.
-	Error(args ...interface{})
-	// Debugf logs a debug message, patterned after log.Printf.
-	Debugf(format string, args ...interface{})
-	// Debug logs a debug message, patterned after log.Print.
-	Debug(args ...interface{})
-	// Warnf logs a warning message, patterned after log.Printf.
-	Warnf(format string, args ...interface{})
-	// Warn logs a warning message, patterned after log.Print.
-	Warn(args ...interface{})
-	// Infof logs an information message, patterned after log.Printf.
-	Infof(format string, args ...interface{})
-	// Info logs an information message, patterned after log.Print.
-	Info(args ...interface{})
 }
