a45260e954f2e7e834a7df033754102b2454bea2
Pascal Zimmermann <pascal.zimmermann01@sap.com>
2024-04-26 13:08:16 +0200

    feat: Add the RDS performance and generic metrics option

--- plugins/inputs/aliyuncms/aliyuncms.go
+++ plugins/inputs/aliyuncms/aliyuncms.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -14,6 +15,7 @@ import (
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials/providers"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/cms"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/rds"
 	"github.com/jmespath/go-jmespath"
 
 	"github.com/influxdata/telegraf"
@@ -26,7 +28,17 @@ import (
 //go:embed sample.conf
 var sampleConfig string
 
+// Key and service constants extracted for readability.
+const (
+	tagInstanceID  = "instanceId"
+	tagBucketName  = "BucketName"
+	tagUserID      = "userId"
+	fieldTimestamp = "timestamp"
+	serviceRDS     = "rds"
+)
+
 type (
+	// AliyunCMS is Aliyun cms config info.
 	AliyunCMS struct {
 		AccessKeyID       string `toml:"access_key_id"`
 		AccessKeySecret   string `toml:"access_key_secret"`
@@ -37,6 +49,7 @@ type (
 		PublicKeyID       string `toml:"public_key_id"`
 		RoleName          string `toml:"role_name"`
 
+		MetricServices    []string        `toml:"metric_services"`
 		Regions           []string        `toml:"regions"`
 		DiscoveryInterval config.Duration `toml:"discovery_interval"`
 		Period            config.Duration `toml:"period"`
@@ -47,7 +60,9 @@ type (
 
 		Log telegraf.Logger `toml:"-"`
 
-		client        aliyuncmsClient
+		cmsClient aliyuncmsClient
+		rdsClient aliyunrdsClient
+
 		windowStart   time.Time
 		windowEnd     time.Time
 		dt            *discoveryTool
@@ -56,7 +71,7 @@ type (
 		measurement   string
 	}
 
-	// metric describes what metrics to get
+	// Metric describes what metrics to get
 	metric struct {
 		ObjectsFilter                 string   `toml:"objects_filter"`
 		MetricNames                   []string `toml:"names"`
@@ -67,14 +82,23 @@ type (
 		dtLock               sync.Mutex                   // Guard for discoveryTags & dimensions
 		discoveryTags        map[string]map[string]string // Internal data structure that can enrich metrics with tags
 		dimensionsUdObj      map[string]string
-		dimensionsUdArr      []map[string]string // Parsed Dimesnsions JSON string (unmarshalled)
-		requestDimensions    []map[string]string // this is the actual dimensions list that would be used in API request
+		dimensionsUdArr      []map[string]string // Parsed Dimensions JSON string (unmarshalled)
+		requestDimensions    []map[string]string // this is the actual dimensions list that would be used in the API request
 		requestDimensionsStr string              // String representation of the above
 
 	}
 
+	// Dimension describes how to get metrics
+	Dimension struct {
+		Value string `toml:"value"`
+	}
+
 	aliyuncmsClient interface {
 		DescribeMetricList(request *cms.DescribeMetricListRequest) (response *cms.DescribeMetricListResponse, err error)
+	}
+
+	aliyunrdsClient interface {
+		DescribeDBInstancePerformance(request *rds.DescribeDBInstancePerformanceRequest) (response *rds.DescribeDBInstancePerformanceResponse, err error)
 	}
 )
 
@@ -107,6 +131,7 @@ func (*AliyunCMS) SampleConfig() string {
 	return sampleConfig
 }
 
+// Init performs checks of plugin inputs and initializes internals
 func (s *AliyunCMS) Init() error {
 	if s.Project == "" {
 		return errors.New("project is not set")
@@ -137,9 +162,17 @@ func (s *AliyunCMS) Init() error {
 	if err != nil {
 		return fmt.Errorf("failed to retrieve credential: %w", err)
 	}
-	s.client, err = cms.NewClientWithOptions("", sdk.NewConfig(), credential)
+	s.cmsClient, err = cms.NewClientWithOptions("", sdk.NewConfig(), credential)
 	if err != nil {
 		return fmt.Errorf("failed to create cms client: %w", err)
+	}
+
+	// check metrics dimensions consistency
+	if s.Project == "acs_rds_dashboard" && slices.Contains(s.MetricServices, "rds") {
+		s.rdsClient, err = rds.NewClientWithOptions("", sdk.NewConfig(), credential)
+		if err != nil {
+			return fmt.Errorf("failed to create rds client: %w", err)
+		}
 	}
 
 	// check metrics dimensions consistency
@@ -172,6 +205,12 @@ func (s *AliyunCMS) Init() error {
 			len(s.Regions), strings.Join(s.Regions, ","))
 	}
 
+	// Check metric services
+	if len(s.MetricServices) == 0 {
+		s.MetricServices = []string{"cms"}
+		s.Log.Info("'metric_services' is not set. Metrics will be queried from the cms service")
+	}
+
 	// Init discovery...
 	if s.dt == nil { // Support for tests
 		s.dt, err = newDiscoveryTool(s.Regions, s.Project, s.Log, credential, int(float32(s.RateLimit)*0.2), time.Duration(s.DiscoveryInterval))
@@ -191,7 +230,7 @@ func (s *AliyunCMS) Init() error {
 
 	s.Log.Infof("%d object(s) discovered...", len(s.discoveryData))
 
-	//  Special setting for acs_oss project since the API differs
+	// Special setting for acs_oss project since the API differs
 	if s.Project == "acs_oss" {
 		s.dimensionKey = "BucketName"
 	}
@@ -199,9 +238,9 @@ func (s *AliyunCMS) Init() error {
 	return nil
 }
 
-// Start plugin discovery loop, metrics are gathered through Gather
+// Start a plugin discovery loop, metrics are gathered through Gather
 func (s *AliyunCMS) Start(telegraf.Accumulator) error {
-	// Start periodic discovery process
+	// Start a periodic discovery process
 	if s.dt != nil {
 		s.dt.start()
 	}
@@ -209,16 +248,17 @@ func (s *AliyunCMS) Start(telegraf.Accumulator) error {
 	return nil
 }
 
+// Gather implements telegraf.Inputs interface
 func (s *AliyunCMS) Gather(acc telegraf.Accumulator) error {
 	s.updateWindow(time.Now())
 
-	// limit concurrency or we can easily exhaust user connection limit
+	// limit concurrency, or we can easily exhaust the user connection limit
 	lmtr := limiter.NewRateLimiter(s.RateLimit, time.Second)
 	defer lmtr.Stop()
 
 	var wg sync.WaitGroup
 	for _, m := range s.Metrics {
-		// Prepare internal structure with data from discovery
+		// Prepare an internal structure with data from discovery
 		s.prepareTagsAndDimensions(m)
 		wg.Add(len(m.MetricNames))
 		for _, metricName := range m.MetricNames {
@@ -230,7 +270,6 @@ func (s *AliyunCMS) Gather(acc telegraf.Accumulator) error {
 		}
 		wg.Wait()
 	}
-
 	return nil
 }
 
@@ -241,6 +280,7 @@ func (s *AliyunCMS) Stop() {
 	}
 }
 
+// Gather given metric and emit error
 func (s *AliyunCMS) updateWindow(relativeTo time.Time) {
 	// https://help.aliyun.com/document_detail/51936.html?spm=a2c4g.11186623.6.701.54025679zh6wiR
 	// The start and end times are executed in the mode of
@@ -260,77 +300,211 @@ func (s *AliyunCMS) updateWindow(relativeTo time.Time) {
 	s.windowEnd = windowEnd
 }
 
+// getDataPoints abstracts the source selection (RDS vs CMS) and pagination token.
+func (s *AliyunCMS) getDataPoints(region, metricName string, m *metric) (points []map[string]interface{}, nextToken string, err error) {
+	if s.rdsClient != nil {
+		points, err = s.fetchRDSPerformanceDatapoints(region, metricName, m)
+		// RDS branch doesn't support pagination via NextToken
+		return points, "", err
+	}
+	return s.fetchCMSDatapoints(region, metricName, m)
+}
+
+// parseTimestamp normalizes various timestamp representations into seconds since epoch.
+func (s *AliyunCMS) parseTimestamp(v interface{}) (int64, bool) { //nolint:revive // Valid case
+	switch t := v.(type) {
+	case int64:
+		return t, true
+	case float64:
+		// CMS timestamps are in ms
+		return int64(t) / 1000, true
+	case int:
+		return int64(t), true
+	case json.Number:
+		if val, err := t.Int64(); err == nil {
+			return val, true
+		}
+	default:
+	}
+	return 0, false
+}
+
+// enrichTagsWithDiscovery applies discovery tags and returns whether the datapoint should be kept.
+func (s *AliyunCMS) enrichTagsWithDiscovery(tags map[string]string, m *metric, id string) bool {
+	if m.discoveryTags == nil {
+		return true
+	}
+	disc, ok := m.discoveryTags[id]
+	if !ok && !m.AllowDataPointWODiscoveryData {
+		s.Log.Warnf("Instance %q is not found in discovery, skipping monitoring datapoint...", id)
+		return false
+	}
+	for k, v := range disc {
+		tags[k] = v
+	}
+	return true
+}
+
 // Gather given metric and emit error
-func (s *AliyunCMS) gatherMetric(acc telegraf.Accumulator, metricName string, metric *metric) error {
+func (s *AliyunCMS) gatherMetric(acc telegraf.Accumulator, metricName string, m *metric) error {
 	for _, region := range s.Regions {
-		req := cms.CreateDescribeMetricListRequest()
-		req.Period = strconv.FormatInt(int64(time.Duration(s.Period).Seconds()), 10)
-		req.MetricName = metricName
-		req.Length = "10000"
-		req.Namespace = s.Project
-		req.EndTime = strconv.FormatInt(s.windowEnd.Unix()*1000, 10)
-		req.StartTime = strconv.FormatInt(s.windowStart.Unix()*1000, 10)
-		req.Dimensions = metric.requestDimensionsStr
-		req.RegionId = region
-
 		for more := true; more; {
-			resp, err := s.client.DescribeMetricList(req)
+			dataPoints, nextToken, err := s.getDataPoints(region, metricName, m)
 			if err != nil {
-				return fmt.Errorf("failed to query metricName list: %w", err)
+				return err
 			}
-			if resp.Code != "200" {
-				s.Log.Errorf("failed to query metricName list: %v", resp.Message)
+			if len(dataPoints) == 0 {
+				if s.rdsClient != nil && slices.Contains(s.MetricServices, serviceRDS) {
+					s.Log.Debug("No rds performance metrics returned from RDS")
+				} else {
+					s.Log.Debug("No metrics returned from CMS")
+				}
 				break
 			}
-
-			var datapoints []map[string]interface{}
-			if err := json.Unmarshal([]byte(resp.Datapoints), &datapoints); err != nil {
-				return fmt.Errorf("failed to decode response datapoints: %w", err)
-			}
-
-			if len(datapoints) == 0 {
-				s.Log.Debugf("No metrics returned from CMS, response msg: %s", resp.Message)
-				break
-			}
-
 		NextDataPoint:
-			for _, datapoint := range datapoints {
-				fields := make(map[string]interface{}, len(datapoint))
-				tags := make(map[string]string, len(datapoint))
-				datapointTime := int64(0)
-				for key, value := range datapoint {
-					switch key {
-					case "instanceId", "BucketName":
-						tags[key] = value.(string)
-						if metric.discoveryTags != nil { // discovery can be not activated
-							// Skipping data point if discovery data not exist
-							_, ok := metric.discoveryTags[value.(string)]
-							if !ok &&
-								!metric.AllowDataPointWODiscoveryData {
-								s.Log.Warnf("Instance %q is not found in discovery, skipping monitoring datapoint...", value.(string))
-								continue NextDataPoint
-							}
+			for _, dp := range dataPoints {
+				fields := make(map[string]interface{}, len(dp))
+				tags := make(map[string]string, len(dp))
+				var ts int64
 
-							for k, v := range metric.discoveryTags[value.(string)] {
-								tags[k] = v
-							}
+				for key, value := range dp {
+					switch key {
+					case tagInstanceID, tagBucketName:
+						strVal, ok := value.(string)
+						if !ok {
+							s.Log.Warnf("Unexpected non-string %q value in datapoint, skipping...", key)
+							continue NextDataPoint
 						}
-					case "userId":
-						tags[key] = value.(string)
-					case "timestamp":
-						datapointTime = int64(value.(float64)) / 1000
+						tags[key] = strVal
+						if keep := s.enrichTagsWithDiscovery(tags, m, strVal); !keep {
+							continue NextDataPoint
+						}
+					case tagUserID:
+						if str, ok := value.(string); ok {
+							tags[key] = str
+						}
+					case fieldTimestamp:
+						parsed, ok := s.parseTimestamp(value)
+						if !ok {
+							s.Log.Warnf("Unexpected timestamp type %T, skipping datapoint", value)
+							continue NextDataPoint
+						}
+						ts = parsed
 					default:
 						fields[formatField(metricName, key)] = value
 					}
 				}
-				acc.AddFields(s.measurement, fields, tags, time.Unix(datapointTime, 0))
+
+				acc.AddFields(s.measurement, fields, tags, time.Unix(ts, 0))
 			}
 
-			req.NextToken = resp.NextToken
-			more = req.NextToken != ""
+			more = nextToken != ""
 		}
 	}
 	return nil
+}
+
+// fetchCMSDatapoints queries CMS for datapoints and returns them along with the pagination token (if any).
+func (s *AliyunCMS) fetchCMSDatapoints(region, metricName string, metric *metric) ([]map[string]interface{}, string, error) {
+	req := cms.CreateDescribeMetricListRequest()
+	req.Period = strconv.FormatInt(int64(time.Duration(s.Period).Seconds()), 10)
+	req.MetricName = metricName
+	req.Length = "10000"
+	req.Namespace = s.Project
+	req.EndTime = strconv.FormatInt(s.windowEnd.Unix()*1000, 10)
+	req.StartTime = strconv.FormatInt(s.windowStart.Unix()*1000, 10)
+	req.Dimensions = metric.requestDimensionsStr
+	req.RegionId = region
+
+	cmsResp, err := s.cmsClient.DescribeMetricList(req)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to query metricName list: %w", err)
+	}
+	if cmsResp.Code != "200" {
+		s.Log.Errorf("failed to query metricName list: %v", cmsResp.Message)
+		return nil, "", nil
+	}
+
+	var dataPoints []map[string]interface{}
+	if err := json.Unmarshal([]byte(cmsResp.Datapoints), &dataPoints); err != nil {
+		return nil, "", fmt.Errorf("failed to decode response datapoints: %w", err)
+	}
+	if len(dataPoints) == 0 {
+		s.Log.Debugf("No metrics returned from CMS, response msg: %s", cmsResp.Message)
+		return nil, cmsResp.NextToken, nil
+	}
+	return dataPoints, cmsResp.NextToken, nil
+}
+
+// fetchRDSPerformanceDatapoints queries RDS performance metrics and normalizes them into CMS-like datapoints.
+func (s *AliyunCMS) fetchRDSPerformanceDatapoints(region, metricName string, metric *metric) ([]map[string]interface{}, error) {
+	var dataPoints []map[string]interface{}
+
+	for _, instanceID := range metric.requestDimensions {
+		req := rds.CreateDescribeDBInstancePerformanceRequest()
+		req.DBInstanceId = instanceID["instanceId"]
+		req.Key = metricName
+		req.StartTime = formatTimeUTC(s.windowStart)
+		req.EndTime = formatTimeUTC(s.windowEnd)
+		req.RegionId = region
+
+		resp, err := s.rdsClient.DescribeDBInstancePerformance(req)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get the database instance performance metrics: %w", err)
+		}
+		if resp.GetHttpStatus() != 200 {
+			s.Log.Errorf("failed to get the database instance performance metrics: %v", resp.BaseResponse.GetHttpContentString())
+			continue
+		}
+
+		for _, performanceKey := range resp.PerformanceKeys.PerformanceKey {
+			keyNames := strings.Split(performanceKey.ValueFormat, "&")
+			for _, performanceValue := range performanceKey.Values.PerformanceValue {
+				parsedTime, err := time.Parse(time.RFC3339, performanceValue.Date)
+				if err != nil {
+					return nil, fmt.Errorf("failed to parse response performance time datapoints: %w", err)
+				}
+
+				values := strings.Split(performanceValue.Value, "&")
+				if len(values) == 1 && len(keyNames) == 1 {
+					// Single value
+					valueAsFloat, err := strconv.ParseFloat(values[0], 32)
+					if err != nil {
+						return nil, fmt.Errorf("failed to convert the performance value string to an float: %w", err)
+					}
+					dataPoints = append(dataPoints, map[string]interface{}{
+						"instanceId":               instanceID["instanceId"],
+						performanceKey.ValueFormat: valueAsFloat,
+						"timestamp":                parsedTime.Unix(),
+					})
+					continue
+				}
+
+				// Multiple values with "&" separated lists
+				for i, v := range values {
+					if i >= len(keyNames) {
+						break
+					}
+					valueAsFloat, err := strconv.ParseFloat(v, 32)
+					if err != nil {
+						return nil, fmt.Errorf("failed to convert the performance value string to an float: %w", err)
+					}
+					dataPoints = append(dataPoints, map[string]interface{}{
+						"instanceId": instanceID["instanceId"],
+						keyNames[i]:  valueAsFloat,
+						"timestamp":  parsedTime.Unix(),
+					})
+				}
+			}
+		}
+	}
+
+	return dataPoints, nil
+}
+
+// formatTimeUTC formats time in the RFC3339 minute-precision expected by RDS API.
+func formatTimeUTC(t time.Time) string {
+	return t.UTC().Format("2006-01-02T15:04Z")
 }
 
 // tag helper
@@ -374,7 +548,7 @@ func (s *AliyunCMS) prepareTagsAndDimensions(metric *metric) {
 		return
 	}
 
-	// Reading all data from buffered channel
+	// Reading all data from a buffered channel
 L:
 	for {
 		select {
@@ -386,7 +560,7 @@ L:
 		}
 	}
 
-	// new data arrives (so process it) or this is the first call
+	// new data arrives (so process it), or this is the first call
 	if newData || len(metric.discoveryTags) == 0 {
 		metric.dtLock.Lock()
 		defer metric.dtLock.Unlock()
@@ -445,7 +619,7 @@ L:
 			}
 		}
 
-		// add dimensions filter from config file
+		// add dimension filter from a config file
 		if len(metric.dimensionsUdArr) != 0 {
 			metric.requestDimensions = append(metric.requestDimensions, metric.dimensionsUdArr...)
 		}
