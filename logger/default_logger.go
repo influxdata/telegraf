1fea642704e2fdbf4a5112971b951c2ecdcce624
Sven Rebhan <srebhan@influxdata.com>
2024-06-10 22:38:13 +0200

    Rework logger

--- logger/default_logger.go
+++ logger/default_logger.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal/rotate"
 	"github.com/influxdata/wlog"
 )
@@ -21,6 +22,14 @@ const (
 )
 
 type defaultLogger struct {
+	Category string
+	Name     string
+	Alias    string
+	LogLevel telegraf.LogLevel
+
+	prefix  string
+	onError []func()
+
 	writer         io.Writer
 	internalWriter io.Writer
 	timezone       *time.Location
@@ -52,49 +61,125 @@ func (t *defaultLogger) Close() error {
 	return closer.Close()
 }
 
-// newTelegrafWriter returns a logging-wrapped writer.
-func newTelegrafWriter(w io.Writer, c Config) (*defaultLogger, error) {
-	timezoneName := c.LogWithTimezone
+// NewLogger creates a new logger instance
+func (t *defaultLogger) New(category, name, alias string) telegraf.Logger {
+	var prefix string
+	if category != "" {
+		prefix = "[" + category
+		if name != "" {
+			prefix += "." + name
+		}
+		if alias != "" {
+			prefix += "::" + alias
+		}
+		prefix += "] "
+	}
+
+	return &defaultLogger{
+		Category:       category,
+		Name:           name,
+		Alias:          alias,
+		LogLevel:       t.LogLevel,
+		prefix:         prefix,
+		writer:         t.writer,
+		internalWriter: t.internalWriter,
+		timezone:       t.timezone,
+	}
+}
+
+// OnErr defines a callback that triggers only when errors are about to be written to the log
+func (t *defaultLogger) RegisterErrorCallback(f func()) {
+	t.onError = append(t.onError, f)
+}
+
+func (t *defaultLogger) Level() telegraf.LogLevel {
+	return t.LogLevel
+}
+
+// Errorf logs an error message, patterned after log.Printf.
+func (t *defaultLogger) Errorf(format string, args ...interface{}) {
+	log.Printf("E! "+t.prefix+format, args...)
+	for _, f := range t.onError {
+		f()
+	}
+}
+
+// Error logs an error message, patterned after log.Print.
+func (t *defaultLogger) Error(args ...interface{}) {
+	for _, f := range t.onError {
+		f()
+	}
+	log.Print(append([]interface{}{"E! " + t.prefix}, args...)...)
+}
+
+// Debugf logs a debug message, patterned after log.Printf.
+func (t *defaultLogger) Debugf(format string, args ...interface{}) {
+	log.Printf("D! "+t.prefix+" "+format, args...)
+}
+
+// Debug logs a debug message, patterned after log.Print.
+func (t *defaultLogger) Debug(args ...interface{}) {
+	log.Print(append([]interface{}{"D! " + t.prefix}, args...)...)
+}
+
+// Warnf logs a warning message, patterned after log.Printf.
+func (t *defaultLogger) Warnf(format string, args ...interface{}) {
+	log.Printf("W! "+t.prefix+format, args...)
+}
+
+// Warn logs a warning message, patterned after log.Print.
+func (t *defaultLogger) Warn(args ...interface{}) {
+	log.Print(append([]interface{}{"W! " + t.prefix}, args...)...)
+}
+
+// Infof logs an information message, patterned after log.Printf.
+func (t *defaultLogger) Infof(format string, args ...interface{}) {
+	log.Printf("I! "+t.prefix+format, args...)
+}
+
+// Info logs an information message, patterned after log.Print.
+func (t *defaultLogger) Info(args ...interface{}) {
+	log.Print(append([]interface{}{"I! " + t.prefix}, args...)...)
+}
+
+func createDefaultLogger(cfg *Config) (logger, error) {
+	// Setup the writer target
+	var writer io.Writer = os.Stderr
+	if cfg.LogTarget == "file" && cfg.Logfile != "" {
+		w, err := rotate.NewFileWriter(
+			cfg.Logfile,
+			cfg.RotationInterval,
+			cfg.RotationMaxSize,
+			cfg.RotationMaxArchives,
+		)
+		if err != nil {
+			return nil, err
+		}
+		writer = w
+	}
+
+	// Get configured timezone
+	timezoneName := cfg.LogWithTimezone
 	if strings.EqualFold(timezoneName, "local") {
 		timezoneName = "Local"
 	}
-
 	tz, err := time.LoadLocation(timezoneName)
 	if err != nil {
 		return nil, errors.New("error while setting logging timezone: " + err.Error())
 	}
 
-	return &defaultLogger{
-		writer:         wlog.NewWriter(w),
-		internalWriter: w,
+	// Setup the logger
+	l := &defaultLogger{
+		writer:         wlog.NewWriter(writer),
+		internalWriter: writer,
 		timezone:       tz,
-	}, nil
-}
-
-func createStderrLogger(cfg Config) (io.WriteCloser, error) {
-	return newTelegrafWriter(os.Stderr, cfg)
-}
-
-func createFileLogger(cfg Config) (io.WriteCloser, error) {
-	if cfg.Logfile == "" {
-		return createStderrLogger(cfg)
 	}
 
-	writer, err := rotate.NewFileWriter(
-		cfg.Logfile,
-		cfg.RotationInterval,
-		cfg.RotationMaxSize,
-		cfg.RotationMaxArchives,
-	)
-	if err != nil {
-		log.Printf("E! Unable to open %s (%s), using stderr", cfg.Logfile, err)
-		return createStderrLogger(cfg)
-	}
-
-	return newTelegrafWriter(writer, cfg)
+	log.SetOutput(l)
+	return l, nil
 }
 
 func init() {
-	registerLogger(LogTargetStderr, createStderrLogger)
-	registerLogger(LogTargetFile, createFileLogger)
+	add("stderr", createDefaultLogger)
+	add("file", createDefaultLogger)
 }
--- logger/logger.go
+++ logger/logger.go
@@ -1,18 +1,49 @@
 package logger
 
 import (
-	"io"
+	"fmt"
 	"log"
+	"time"
 
+	"github.com/influxdata/telegraf"
 	"github.com/influxdata/wlog"
 )
 
+type logger interface {
+	telegraf.Logger
+	New(category, name, alias string) telegraf.Logger
+	Close() error
+}
+
+type Config struct {
+	// will set the log level to DEBUG
+	Debug bool
+	//will set the log level to ERROR
+	Quiet bool
+	//stderr, stdout, file or eventlog (Windows only)
+	LogTarget string
+	// will direct the logging output to a file. Empty string is
+	// interpreted as stderr. If there is an error opening the file the
+	// logger will fall back to stderr
+	Logfile string
+	// will rotate when current file at the specified time interval
+	RotationInterval time.Duration
+	// will rotate when current file size exceeds this parameter.
+	RotationMaxSize int64
+	// maximum rotated files to keep (older ones will be deleted)
+	RotationMaxArchives int
+	// pick a timezone to use when logging. or type 'local' for local time.
+	LogWithTimezone string
+	// Logger instance name
+	InstanceName string
+}
+
 // Keep track what is actually set as a log output, because log package doesn't provide a getter.
 // It allows closing previous writer if re-set and have possibility to test what is actually set
-var actualLogger io.WriteCloser
+var instance logger
 
 // SetupLogging configures the logging output.
-func SetupLogging(cfg Config) error {
+func SetupLogging(cfg *Config) error {
 	log.SetFlags(0)
 	if cfg.Debug {
 		wlog.SetLevel(wlog.DEBUG)
@@ -29,14 +60,13 @@ func SetupLogging(cfg Config) error {
 	}
 
 	// Get the logging factory
-	logCreator, ok := loggerRegistry[cfg.LogTarget]
+	creator, ok := registry[cfg.LogTarget]
 	if !ok {
-		log.Printf("E! Unsupported logtarget: %s, using stderr", cfg.LogTarget)
-		logCreator = createStderrLogger
+		return fmt.Errorf("unsupported logtarget: %s, using stderr", cfg.LogTarget)
 	}
 
 	// Create the root logging instance
-	logWriter, err := logCreator(cfg)
+	l, err := creator(cfg)
 	if err != nil {
 		return err
 	}
@@ -47,16 +77,21 @@ func SetupLogging(cfg Config) error {
 	}
 
 	// Use the new logger and store a reference
-	log.SetOutput(logWriter)
-	actualLogger = logWriter
+	instance = l
 
 	return nil
 }
 
-func CloseLogging() error {
-	if actualLogger == nil {
-		return nil
+func NewLogger(category, name, alias string) telegraf.Logger {
+	if instance == nil {
+		SetupLogging(&Config{})
 	}
+	return instance.New(category, name, alias)
+}
 
-	return actualLogger.Close()
+func CloseLogging() error {
+	if instance != nil {
+		return instance.Close()
+	}
+	return nil
 }
