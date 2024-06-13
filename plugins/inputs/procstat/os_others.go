baf49e2ef60bba8a8ebfcf7ed96e6110ffb4c0b3
Sven Rebhan <srebhan@influxdata.com>
2024-06-13 19:04:53 +0200

    Add rudimentary socket stats for non-Linux OSes

--- plugins/inputs/procstat/os_others.go
+++ plugins/inputs/procstat/os_others.go
@@ -4,12 +4,11 @@ package procstat
 
 import (
 	"errors"
+	"syscall"
 
 	"github.com/shirou/gopsutil/v3/net"
 	"github.com/shirou/gopsutil/v3/process"
 )
-
-const supportsSocketStat = false
 
 func processName(p *process.Process) (string, error) {
 	return p.Exe()
@@ -33,17 +32,72 @@ func collectTotalReadWrite(Process) (r, w uint64, err error) {
 	return 0, 0, errors.ErrUnsupported
 }
 
-func unixConnectionsPid(int32) ([]net.ConnectionStat, error) {
-	return nil, errors.ErrUnsupported
+func statsTCP(conns []net.ConnectionStat, _ uint8) ([]map[string]interface{}, error) {
+	if len(conns) == 0 {
+		return nil, nil
+	}
+
+	// Filter the responses via the inodes belonging to the process
+	fieldslist := make([]map[string]interface{}, 0, len(conns))
+	for _, c := range conns {
+		var proto string
+		switch c.Family {
+		case syscall.AF_INET:
+			proto = "tcp4"
+		case syscall.AF_INET6:
+			proto = "tcp6"
+		default:
+			continue
+		}
+
+		fields := map[string]interface{}{
+			"protocol":  proto,
+			"state":     c.Status,
+			"pid":       c.Pid,
+			"src":       c.Laddr.IP,
+			"src_port":  c.Laddr.Port,
+			"dest":      c.Raddr.IP,
+			"dest_port": c.Raddr.Port,
+		}
+		fieldslist = append(fieldslist, fields)
+	}
+
+	return fieldslist, nil
 }
 
-func statsTCP([]net.ConnectionStat, uint8) ([]map[string]interface{}, error) {
-	return nil, errors.ErrUnsupported
+func statsUDP(conns []net.ConnectionStat, _ uint8) ([]map[string]interface{}, error) {
+	if len(conns) == 0 {
+		return nil, nil
+	}
+
+	// Filter the responses via the inodes belonging to the process
+	fieldslist := make([]map[string]interface{}, 0, len(conns))
+	for _, c := range conns {
+		var proto string
+		switch c.Family {
+		case syscall.AF_INET:
+			proto = "udp4"
+		case syscall.AF_INET6:
+			proto = "udp6"
+		default:
+			continue
+		}
+
+		fields := map[string]interface{}{
+			"protocol":  proto,
+			"state":     c.Status,
+			"pid":       c.Pid,
+			"src":       c.Laddr.IP,
+			"src_port":  c.Laddr.Port,
+			"dest":      c.Raddr.IP,
+			"dest_port": c.Raddr.Port,
+		}
+		fieldslist = append(fieldslist, fields)
+	}
+
+	return fieldslist, nil
 }
 
-func statsUDP([]net.ConnectionStat, uint8) ([]map[string]interface{}, error) {
-	return nil, errors.ErrUnsupported
-}
 func statsUnix([]net.ConnectionStat) ([]map[string]interface{}, error) {
 	return nil, errors.ErrUnsupported
 }
