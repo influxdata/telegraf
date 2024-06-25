2282b1ad3a6d81fc31aa69055b3c1f0cd77631b2
Josh Powers <powersj@fastmail.com>
2024-06-25 07:30:00 -0600

    Remove deprecated usage

--- plugins/inputs/ldap/ldap.go
+++ plugins/inputs/ldap/ldap.go
@@ -133,19 +133,19 @@ func (l *LDAP) connect() (*ldap.Conn, error) {
 	switch l.mode {
 	case "ldap":
 		var err error
-		conn, err = ldap.Dial("tcp", l.Server)
+		conn, err = ldap.DialURL(l.Server)
 		if err != nil {
 			return nil, err
 		}
 	case "ldaps":
 		var err error
-		conn, err = ldap.DialTLS("tcp", l.Server, l.tlsCfg)
+		conn, err = ldap.DialURL(l.Server, ldap.DialWithTLSConfig(l.tlsCfg))
 		if err != nil {
 			return nil, err
 		}
 	case "starttls":
 		var err error
-		conn, err = ldap.Dial("tcp", l.Server)
+		conn, err = ldap.DialURL(l.Server)
 		if err != nil {
 			return nil, err
 		}
