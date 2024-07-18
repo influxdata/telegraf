5b24a8bf4f7cd369660c5e25d56dc9be1276d87a
Sven Rebhan <srebhan@influxdata.com>
2024-07-18 15:45:50 +0200

    feat(inputs.opentelemetry): Add profiles support

--- plugins/inputs/opentelemetry/opentelemetry.go
+++ plugins/inputs/opentelemetry/opentelemetry.go
@@ -8,13 +8,14 @@ import (
 	"sync"
 	"time"
 
-	"github.com/influxdata/influxdb-observability/otel2influx"
 	"go.opentelemetry.io/collector/pdata/plog/plogotlp"
 	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
 	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
+	pprofileotlp "go.opentelemetry.io/proto/otlp/collector/profiles/v1experimental"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
+	"github.com/influxdata/influxdb-observability/otel2influx"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/plugins/common/tls"
@@ -28,11 +29,11 @@ type OpenTelemetry struct {
 	ServiceAddress      string          `toml:"service_address"`
 	SpanDimensions      []string        `toml:"span_dimensions"`
 	LogRecordDimensions []string        `toml:"log_record_dimensions"`
+	ProfileDimensions   []string        `toml:"profile_dimensions"`
 	MetricsSchema       string          `toml:"metrics_schema"`
 	MaxMsgSize          config.Size     `toml:"max_msg_size"`
 	Timeout             config.Duration `toml:"timeout"`
 	Log                 telegraf.Logger `toml:"-"`
-
 	tls.ServerConfig
 
 	listener   net.Listener // overridden in tests
@@ -45,11 +46,26 @@ func (*OpenTelemetry) SampleConfig() string {
 	return sampleConfig
 }
 
-func (o *OpenTelemetry) Gather(_ telegraf.Accumulator) error {
+func (*OpenTelemetry) Gather(telegraf.Accumulator) error {
 	return nil
 }
 
-func (o *OpenTelemetry) Start(accumulator telegraf.Accumulator) error {
+func (o *OpenTelemetry) Init() error {
+	if o.ServiceAddress == "" {
+		o.ServiceAddress = "0.0.0.0:4317"
+	}
+	switch o.MetricsSchema {
+	case "": // Set default
+		o.MetricsSchema = "prometheus-v1"
+	case "prometheus-v1", "prometheus-v2": // Valid values
+	default:
+		return fmt.Errorf("invalid metric schema %q", o.MetricsSchema)
+	}
+
+	return nil
+}
+
+func (o *OpenTelemetry) Start(acc telegraf.Accumulator) error {
 	var grpcOptions []grpc.ServerOption
 	if tlsConfig, err := o.ServerConfig.TLSConfig(); err != nil {
 		return err
@@ -64,7 +80,7 @@ func (o *OpenTelemetry) Start(accumulator telegraf.Accumulator) error {
 	}
 
 	logger := &otelLogger{o.Log}
-	influxWriter := &writeToAccumulator{accumulator}
+	influxWriter := &writeToAccumulator{acc}
 	o.grpcServer = grpc.NewServer(grpcOptions...)
 
 	traceSvc, err := newTraceService(logger, influxWriter, o.SpanDimensions)
@@ -72,30 +88,36 @@ func (o *OpenTelemetry) Start(accumulator telegraf.Accumulator) error {
 		return err
 	}
 	ptraceotlp.RegisterGRPCServer(o.grpcServer, traceSvc)
+
 	metricsSvc, err := newMetricsService(logger, influxWriter, o.MetricsSchema)
 	if err != nil {
 		return err
 	}
 	pmetricotlp.RegisterGRPCServer(o.grpcServer, metricsSvc)
+
 	logsSvc, err := newLogsService(logger, influxWriter, o.LogRecordDimensions)
 	if err != nil {
 		return err
 	}
 	plogotlp.RegisterGRPCServer(o.grpcServer, logsSvc)
 
-	if o.listener == nil {
-		o.listener, err = net.Listen("tcp", o.ServiceAddress)
-		if err != nil {
-			return err
-		}
+	profileSvc, err := newProfileService(acc, o.Log, o.ProfileDimensions)
+	if err != nil {
+		return err
+	}
+	pprofileotlp.RegisterProfilesServiceServer(o.grpcServer, profileSvc)
+
+	o.listener, err = net.Listen("tcp", o.ServiceAddress)
+	if err != nil {
+		return err
 	}
 
 	o.wg.Add(1)
 	go func() {
+		defer o.wg.Done()
 		if err := o.grpcServer.Serve(o.listener); err != nil {
-			accumulator.AddError(fmt.Errorf("failed to stop OpenTelemetry gRPC service: %w", err))
+			acc.AddError(fmt.Errorf("failed to stop OpenTelemetry gRPC service: %w", err))
 		}
-		o.wg.Done()
 	}()
 
 	return nil
@@ -105,6 +127,7 @@ func (o *OpenTelemetry) Stop() {
 	if o.grpcServer != nil {
 		o.grpcServer.Stop()
 	}
+	o.listener = nil
 
 	o.wg.Wait()
 }
@@ -112,10 +135,9 @@ func (o *OpenTelemetry) Stop() {
 func init() {
 	inputs.Add("opentelemetry", func() telegraf.Input {
 		return &OpenTelemetry{
-			ServiceAddress:      "0.0.0.0:4317",
 			SpanDimensions:      otel2influx.DefaultOtelTracesToLineProtocolConfig().SpanDimensions,
 			LogRecordDimensions: otel2influx.DefaultOtelLogsToLineProtocolConfig().LogRecordDimensions,
-			MetricsSchema:       "prometheus-v1",
+			ProfileDimensions:   []string{"host.name"},
 			Timeout:             config.Duration(5 * time.Second),
 		}
 	})
--- plugins/inputs/opentelemetry/opentelemetry_test.go
+++ plugins/inputs/opentelemetry/opentelemetry_test.go
@@ -3,31 +3,41 @@ package opentelemetry
 import (
 	"context"
 	"net"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 	"testing"
 	"time"
 
-	"go.opentelemetry.io/otel/sdk/metric/metricdata"
-
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	otlplogs "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	otlpmetrics "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
+	otlpprofiles "go.opentelemetry.io/proto/otlp/collector/profiles/v1experimental"
+	otlptrace "go.opentelemetry.io/proto/otlp/collector/trace/v1"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
 
+	"github.com/influxdata/influxdb-observability/otel2influx"
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/config"
 	tmetric "github.com/influxdata/telegraf/metric"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/influxdata/telegraf/plugins/parsers/influx"
 	"github.com/influxdata/telegraf/testutil"
 )
 
 func TestOpenTelemetry(t *testing.T) {
-	// Setup the plugin with a direct mockup connection
-	listener := bufconn.Listen(1024 * 1024)
-	defer listener.Close()
-
+	// Setup and start the plugin
 	plugin := &OpenTelemetry{
 		MetricsSchema: "prometheus-v1",
-		listener:      listener,
 	}
+	require.NoError(t, plugin.Init())
 
 	var acc testutil.Accumulator
 	require.NoError(t, plugin.Start(&acc))
@@ -35,21 +45,22 @@ func TestOpenTelemetry(t *testing.T) {
 
 	// Setup the OpenTelemetry exporter
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	t.Cleanup(cancel)
+	defer cancel()
 
 	exporter, err := otlpmetricgrpc.New(ctx,
 		otlpmetricgrpc.WithInsecure(),
 		otlpmetricgrpc.WithDialOption(
-			grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
-				return listener.DialContext(ctx)
+			grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+				return net.Dial("tcp", plugin.listener.Addr().String())
 			})),
 	)
 	require.NoError(t, err)
-	defer exporter.Shutdown(ctx)
+	defer exporter.Shutdown(ctx) //nolint:errcheck // We cannot do anything if the shutdown fails
 
 	// Setup the metric to send
 	reader := metric.NewManualReader()
-	defer reader.Shutdown(ctx)
+	defer reader.Shutdown(ctx) //nolint:errcheck // We cannot do anything if the shutdown fails
+
 	provider := metric.NewMeterProvider(metric.WithReader(reader))
 	meter := provider.Meter("library-name")
 	counter, err := meter.Int64Counter("measurement-counter")
@@ -88,4 +99,149 @@ func TestOpenTelemetry(t *testing.T) {
 	}
 	actual := acc.GetTelegrafMetrics()
 	testutil.RequireMetricsEqual(t, expected, actual, testutil.IgnoreTime(), testutil.IgnoreFields("start_time_unix_nano"))
+}
+
+func TestCases(t *testing.T) {
+	// Get all directories in testdata
+	folders, err := os.ReadDir("testcases")
+	require.NoError(t, err)
+
+	// Register the plugin
+	inputs.Add("opentelemetry", func() telegraf.Input {
+		return &OpenTelemetry{
+			ServiceAddress:      "127.0.0.1:0",
+			SpanDimensions:      otel2influx.DefaultOtelTracesToLineProtocolConfig().SpanDimensions,
+			LogRecordDimensions: otel2influx.DefaultOtelLogsToLineProtocolConfig().LogRecordDimensions,
+			ProfileDimensions:   []string{"host.name"},
+			Timeout:             config.Duration(5 * time.Second),
+		}
+	})
+
+	// Prepare the influx parser for expectations
+	parser := &influx.Parser{}
+	require.NoError(t, parser.Init())
+
+	for _, f := range folders {
+		// Only handle folders
+		if !f.IsDir() {
+			continue
+		}
+		testcasePath := filepath.Join("testcases", f.Name())
+		configFilename := filepath.Join(testcasePath, "telegraf.conf")
+		inputFiles := filepath.Join(testcasePath, "*.json")
+		expectedFilename := filepath.Join(testcasePath, "expected.out")
+		expectedErrorFilename := filepath.Join(testcasePath, "expected.err")
+
+		// Compare options
+		options := []cmp.Option{
+			testutil.IgnoreTime(),
+			testutil.SortMetrics(),
+			testutil.IgnoreFields("start_time_unix_nano"),
+		}
+
+		t.Run(f.Name(), func(t *testing.T) {
+			// Read the input data
+			inputs := make(map[string][][]byte)
+			matches, err := filepath.Glob(inputFiles)
+			require.NoError(t, err)
+			require.NotEmpty(t, matches)
+			sort.Strings(matches)
+			for _, fn := range matches {
+				buf, err := os.ReadFile(fn)
+				require.NoError(t, err)
+
+				key := strings.TrimSuffix(filepath.Base(fn), ".json")
+				key, _, _ = strings.Cut(key, "_")
+				inputs[key] = append(inputs[key], buf)
+			}
+
+			// Read the expected output if any
+			var expected []telegraf.Metric
+			if _, err := os.Stat(expectedFilename); err == nil {
+				var err error
+				expected, err = testutil.ParseMetricsFromFile(expectedFilename, parser)
+				require.NoError(t, err)
+			}
+
+			// Read the expected output if any
+			var expectedErrors []string
+			if _, err := os.Stat(expectedErrorFilename); err == nil {
+				var err error
+				expectedErrors, err = testutil.ParseLinesFromFile(expectedErrorFilename)
+				require.NoError(t, err)
+				require.NotEmpty(t, expectedErrors)
+			}
+
+			// Configure the plugin
+			cfg := config.NewConfig()
+			require.NoError(t, cfg.LoadConfig(configFilename))
+			require.Len(t, cfg.Inputs, 1)
+
+			// Setup and start the plugin
+			plugin := cfg.Inputs[0].Input.(*OpenTelemetry)
+			require.NoError(t, plugin.Init())
+
+			var acc testutil.Accumulator
+			require.NoError(t, plugin.Start(&acc))
+			defer plugin.Stop()
+
+			// Send all data to the plugin
+			addr := plugin.listener.Addr().String()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			grpcClient, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+			require.NoError(t, err)
+			defer grpcClient.Close()
+			for msgtype, messages := range inputs {
+				switch msgtype {
+				case "logs":
+					client := otlplogs.NewLogsServiceClient(grpcClient)
+					for _, buf := range messages {
+						var msg otlplogs.ExportLogsServiceRequest
+						require.NoError(t, protojson.Unmarshal(buf, &msg))
+						_, err := client.Export(ctx, &msg)
+						require.NoError(t, err)
+					}
+				case "metrics":
+					client := otlpmetrics.NewMetricsServiceClient(grpcClient)
+					for _, buf := range messages {
+						var msg otlpmetrics.ExportMetricsServiceRequest
+						require.NoError(t, protojson.Unmarshal(buf, &msg))
+						_, err := client.Export(ctx, &msg)
+						require.NoError(t, err)
+					}
+				case "profiles":
+					client := otlpprofiles.NewProfilesServiceClient(grpcClient)
+					for _, buf := range messages {
+						var msg otlpprofiles.ExportProfilesServiceRequest
+						require.NoError(t, protojson.Unmarshal(buf, &msg))
+						_, err := client.Export(ctx, &msg)
+						require.NoError(t, err)
+					}
+				case "traces":
+					client := otlptrace.NewTraceServiceClient(grpcClient)
+					for _, buf := range messages {
+						var msg otlptrace.ExportTraceServiceRequest
+						require.NoError(t, protojson.Unmarshal(buf, &msg))
+						_, err := client.Export(ctx, &msg)
+						require.NoError(t, err)
+					}
+				}
+			}
+
+			// Close the plugin to make sure all data is flushed
+			require.NoError(t, grpcClient.Close())
+			plugin.Stop()
+
+			// Check the metrics
+			require.Eventually(t, func() bool {
+				return acc.NMetrics() >= uint64(len(expected))
+			}, 3*time.Second, 100*time.Millisecond)
+			require.Empty(t, acc.Errors)
+
+			actual := acc.GetTelegrafMetrics()
+			testutil.RequireMetricsEqual(t, expected, actual, options...)
+		})
+	}
 }
