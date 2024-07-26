34c4a4d737f56cefdd399b7353223daf7dd12e97
Dane Strandboge <136023093+DStrand1@users.noreply.github.com>
2024-07-26 13:09:52 -0500

    fix output config fields not being populated

--- config/config.go
+++ config/config.go
@@ -1513,8 +1513,10 @@ func (c *Config) buildOutput(name string, tbl *ast.Table) (*models.OutputConfig,
 		return nil, err
 	}
 	oc := &models.OutputConfig{
-		Name:   name,
-		Filter: filter,
+		Name:            name,
+		Filter:          filter,
+		BufferStrategy:  c.Agent.BufferStrategy,
+		BufferDirectory: c.Agent.BufferDirectory,
 	}
 
 	// TODO: support FieldPass/FieldDrop on outputs
@@ -1529,8 +1531,6 @@ func (c *Config) buildOutput(name string, tbl *ast.Table) (*models.OutputConfig,
 	c.getFieldString(tbl, "name_suffix", &oc.NameSuffix)
 	c.getFieldString(tbl, "name_prefix", &oc.NamePrefix)
 	c.getFieldString(tbl, "startup_error_behavior", &oc.StartupErrorBehavior)
-	c.getFieldString(tbl, "buffer_strategy", &oc.BufferStrategy)
-	c.getFieldString(tbl, "buffer_directory", &oc.BufferDirectory)
 
 	if c.hasErrs() {
 		return nil, c.firstErr()
