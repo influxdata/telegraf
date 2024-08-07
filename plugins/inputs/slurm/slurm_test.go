9b6f7643970819a124c3d038bf70f52e15a41ecb
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-08-07 12:29:00 +0200

    feat(inputs.slurm): Enhance URL tests

--- plugins/inputs/slurm/slurm_test.go
+++ plugins/inputs/slurm/slurm_test.go
@@ -12,14 +12,16 @@ import (
 )
 
 func TestURLs(t *testing.T) {
-	for _, url := range []string{"http://example.com:6820", "https://example.com:6820"} {
+	for _, url := range []string{"http://example.com:6820", "https://example.com:6820", "http://example.com"} {
 		plugin := Slurm{
 			URL: url,
 		}
 		require.NoError(t, plugin.Init())
 	}
 
-	for _, url := range []string{"httpp://example.com:6820", "httpss://example.com:6820"} {
+	for _, url := range []string{
+		"httpp://example.com:6820", "httpss://example.com:6820", "", "http://:6820", "http://",
+	} {
 		plugin := Slurm{
 			URL: url,
 		}
