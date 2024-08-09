72ca42b743a88ad5cb658c5147a9107f51e17389
Joshua Powers <powersj@fastmail.com>
2024-08-09 08:43:58 -0600

    chore(deps): Bump Go from v1.22.5 to v1.22.6 (#15720)

--- scripts/installgo_mac.sh
+++ scripts/installgo_mac.sh
@@ -3,9 +3,9 @@
 set -eux
 
 ARCH=$(uname -m)
-GO_VERSION="1.22.5"
-GO_VERSION_SHA_arm64="4cd1bcb05be03cecb77bccd765785d5ff69d79adf4dd49790471d00c06b41133" # from https://golang.org/dl
-GO_VERSION_SHA_amd64="95d9933cdcf45f211243c42c7705c37353cccd99f27eb4d8e2d1bf2f4165cb50" # from https://golang.org/dl
+GO_VERSION="1.22.6"
+GO_VERSION_SHA_arm64="ebac39fd44fc22feed1bb519af431c84c55776e39b30f4fd62930da9c0cfd1e3" # from https://golang.org/dl
+GO_VERSION_SHA_amd64="9c3c0124b01b5365f73a1489649f78f971ecf84844ad9ca58fde133096ddb61b" # from https://golang.org/dl
 
 if [ "$ARCH" = 'arm64' ]; then
     GO_ARCH="darwin-arm64"
