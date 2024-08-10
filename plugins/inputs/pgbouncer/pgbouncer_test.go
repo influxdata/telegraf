101ab3fdcdf7b758c2e04460e3a9654a5eeeae99
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2024-08-10 12:04:29 +0200

    feat: Update the PGBouncer version inside the tests

--- plugins/inputs/pgbouncer/pgbouncer_test.go
+++ plugins/inputs/pgbouncer/pgbouncer_test.go
@@ -33,7 +33,7 @@ func TestPgBouncerGeneratesMetricsIntegration(t *testing.T) {
 	defer backend.Terminate()
 
 	container := testutil.Container{
-		Image:        "z9pascal/pgbouncer-container:1.18.0-latest",
+		Image:        "z9pascal/pgbouncer-container:1.23.1-latest",
 		ExposedPorts: []string{pgBouncerServicePort},
 		Env: map[string]string{
 			"PG_ENV_POSTGRESQL_USER": "pgbouncer",
@@ -123,7 +123,7 @@ func TestPgBouncerGeneratesMetricsIntegrationShowCommands(t *testing.T) {
 	defer backend.Terminate()
 
 	container := testutil.Container{
-		Image:        "z9pascal/pgbouncer-container:1.18.0-latest",
+		Image:        "z9pascal/pgbouncer-container:1.23.1-latest",
 		ExposedPorts: []string{pgBouncerServicePort},
 		Env: map[string]string{
 			"PG_ENV_POSTGRESQL_USER": "pgbouncer",
