5000d895daa38133a09918d73c019b7a936dd993
Pawel Zak <pawel.zak.pawel@gmail.com>
2024-11-23 15:02:14 +0100

    chore: Fix linter findings for `revive:exported` in `plugins/inputs/o*`

--- plugins/inputs/opcua/opcua.go
+++ plugins/inputs/opcua/opcua.go
@@ -16,26 +16,24 @@ import (
 var sampleConfig string
 
 type OpcUA struct {
-	ReadClientConfig
+	readClientConfig
 	Log telegraf.Logger `toml:"-"`
 
-	client *ReadClient
+	client *readClient
 }
 
 func (*OpcUA) SampleConfig() string {
 	return sampleConfig
 }
 
-// Init Initialise all required objects
 func (o *OpcUA) Init() (err error) {
-	o.client, err = o.ReadClientConfig.CreateReadClient(o.Log)
+	o.client, err = o.readClientConfig.createReadClient(o.Log)
 	return err
 }
 
-// Gather defines what data the plugin will gather.
 func (o *OpcUA) Gather(acc telegraf.Accumulator) error {
 	// Will (re)connect if the client is disconnected
-	metrics, err := o.client.CurrentValues()
+	metrics, err := o.client.currentValues()
 	if err != nil {
 		return err
 	}
@@ -51,7 +49,7 @@ func (o *OpcUA) Gather(acc telegraf.Accumulator) error {
 func init() {
 	inputs.Add("opcua", func() telegraf.Input {
 		return &OpcUA{
-			ReadClientConfig: ReadClientConfig{
+			readClientConfig: readClientConfig{
 				InputClientConfig: input.InputClientConfig{
 					OpcUAClientConfig: opcua.OpcUAClientConfig{
 						Endpoint:       "opc.tcp://localhost:4840",
