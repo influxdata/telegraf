4d8de2c19dce6955917b24b50fa71ecfea3c7969
Chris Dalke <chris.dalke@havocai.com>
2024-11-22 21:53:06 -0500

    Working tests

--- plugins/inputs/mavlink/mavlink_test.go
+++ plugins/inputs/mavlink/mavlink_test.go
@@ -2,56 +2,58 @@ package mavlink
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // Test that a serial port URL can be parsed.
 func TestParseSerialFcuUrl(t *testing.T) {
-	testConfig = Mavlink{
+	testConfig := Mavlink{
 		FcuUrl: "serial://dev/ttyACM0:115200",
 	}
 
-	conf, error := ParseMavlinkEndpointConfig(&testConfig)
+	_, error := ParseMavlinkEndpointConfig(&testConfig)
 	require.Equal(t, error, nil)
 }
 
 // Test that a UDP client URL can be parsed.
 func TestParseUDPClientFcuUrl(t *testing.T) {
-	testConfig = Mavlink{
+	testConfig := Mavlink{
 		FcuUrl: "udp://192.168.1.12:14550",
 	}
 
-	conf, error := ParseMavlinkEndpointConfig(&testConfig)
+	_, error := ParseMavlinkEndpointConfig(&testConfig)
 	require.Equal(t, error, nil)
 }
 
 // Test that a UDP server URL can be parsed.
 func TestParseUDPServerFcuUrl(t *testing.T) {
-	testConfig = Mavlink{
+	testConfig := Mavlink{
 		FcuUrl: "udp://:14540",
 	}
 
-	conf, error := ParseMavlinkEndpointConfig(&testConfig)
+	_, error := ParseMavlinkEndpointConfig(&testConfig)
 	require.Equal(t, error, nil)
 }
 
 // Test that a TCP client URL can be parsed.
 func TestParseTCPClientFcuUrl(t *testing.T) {
-	testConfig = Mavlink{
+	testConfig := Mavlink{
 		FcuUrl: "tcp://192.168.1.12:14550",
 	}
 
-	conf, error := ParseMavlinkEndpointConfig(&testConfig)
+	_, error := ParseMavlinkEndpointConfig(&testConfig)
 	require.Equal(t, error, nil)
 }
 
 // Test that an invalid URL is caught.
 func TestParseInvalidFcuUrl(t *testing.T) {
-	testConfig = Mavlink{
+	testConfig := Mavlink{
 		FcuUrl: "ftp://not-a-valid-fcu-url",
 	}
 
-	conf, error := ParseMavlinkEndpointConfig(&testConfig)
-	require.Equal(t, error, "")
+	_, error := ParseMavlinkEndpointConfig(&testConfig)
+	require.Equal(t, error.Error(), "Mavlink setup error: invalid fcu_url!")
 }
 
 func TestStringContains(t *testing.T) {
@@ -64,7 +66,8 @@ func TestStringContains(t *testing.T) {
 
 func TestConvertToSnakeCase(t *testing.T) {
 	require.Equal(t, "", ConvertToSnakeCase(""))
-	require.Equal(t, "CamelCase", ConvertToSnakeCase("camel_case"))
-	require.Equal(t, "CamelCamelCase", ConvertToSnakeCase("camel_camel_case"))
+	require.Equal(t, "camel_case", ConvertToSnakeCase("CamelCase"))
+	require.Equal(t, "camel_camel_case", ConvertToSnakeCase("CamelCamelCase"))
 	require.Equal(t, "snake_case", ConvertToSnakeCase("snake_case"))
+	require.Equal(t, "snake_case", ConvertToSnakeCase("SNAKE_CASE"))
 }
