48a315b57e22b8fbb547dc590061ef96848afbf4
frmoschner <freddymoschner@yahoo.de>
2024-12-11 16:24:33 +0100

    added plugin to registry

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -1,1 +1,5 @@
 package all
+
+import (
+    _ "github.com/influxdata/telegraf/plugins/inputs/opcua_event_subscription"
+)
---
+++ plugins/inputs/all/opcua_event_subscription.go
@@ -0,0 +1,3 @@
+package all
+
+import _ "github.com/influxdata/telegraf/plugins/inputs/opcua_event_subscription"
