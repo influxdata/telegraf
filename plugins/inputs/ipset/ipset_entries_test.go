ad92f7035f914d8cd3d4160243e9a63ec884b799
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2024-12-17 21:09:55 +0100

    Update plugins/inputs/ipset/ipset_entries_test.go

--- plugins/inputs/ipset/ipset_entries_test.go
+++ plugins/inputs/ipset/ipset_entries_test.go
@@ -44,7 +44,7 @@ func TestIpsetEntries(t *testing.T) {
 }
 
 func TestIpsetEntriesCidr(t *testing.T) {
-	acc := new(testutil.Accumulator)
+	var acc testutil.Accumulator
 
 	lines := []string{
 		"create mylist0 hash:net family inet hashsize 16384 maxelem 131072 timeout 300 bucketsize 12 initval 0x4effa9ad",
