69e6c0b97621720a2c958fcc8a2b95cb3806baba
Neelay Upadhyaya <neelay.upadhyaya@nutanix.com>
2025-01-16 12:16:24 +0530

    basic changes

---
+++ config/labels.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/validation"
+)
+
+var (
+	// LabelFlags holds the labels for the running instance of telegraf
+	// These are user labels passed via flags
+	LabelFlags []string
+
+	label labels.Labels
+)
+
+func SetupLabels() error {
+	lbls := labels.Set{}
+	fmt.Println("inside")
+
+	for _, label := range LabelFlags {
+		entry := strings.SplitN(label, ":", 2)
+		if len(entry) < 2 {
+			return fmt.Errorf("invalid label format %v", entry)
+		}
+		if errs := validation.IsDNS1123Label(entry[0]); len(errs) > 0 {
+			return fmt.Errorf("invalid label key %s, %s", entry[0], strings.Join(errs, " "))
+		}
+		if errs := validation.IsValidLabelValue(entry[1]); len(errs) > 0 {
+			return fmt.Errorf("invalid label value %s, %s", entry[1], strings.Join(errs, " "))
+		}
+		lbls[entry[0]] = entry[1]
+	}
+	label = lbls
+	fmt.Println(label)
+	return nil
+}
