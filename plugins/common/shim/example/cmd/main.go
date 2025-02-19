a8ce9b83117f81c85f281e44395678ee6e4936ff
Paulo Dias <paulodias.gm@gmail.com>
2025-02-19 10:54:17 +0000

    fix: fix example declaration
    Signed-off-by: Paulo Dias <paulodias.gm@gmail.com>

--- plugins/common/shim/example/cmd/main.go
+++ plugins/common/shim/example/cmd/main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"time"
 
+	// TODO: import your plugins
+	_ "github.com/hashicorp/go-uuid" // Example external package for showing where you can import your plugins
+
 	"github.com/influxdata/telegraf/plugins/common/shim"
 )
 
