42814c2b2146ddf7f946f6b0ac1b70030a746367
frmoschner <freddymoschner@yahoo.de>
2025-02-19 17:15:40 +0100

    small adjusts

--- plugins/inputs/opcua_listener/opcua_listener_test.go
+++ plugins/inputs/opcua_listener/opcua_listener_test.go
@@ -1014,3 +1014,70 @@ func TestSubscribeClientConfigEventStereamingInputInvalidNodeID(t *testing.T) {
 	_, err := ua.ParseNodeID("ns=3s=Pressure")
 	require.ErrorContains(t, err, "opcua: invalid node id: ns=3s=Pressure")
 }
+
+func TestSubscribeClientConfigEventStereamingInputMissingFields(t *testing.T) {
+	eventType, err := ua.ParseNodeID("ns=2;i=1001")
+	require.NoError(t, err)
+
+	pressureNode, err := ua.ParseNodeID("ns=3;s=Pressure")
+	require.NoError(t, err)
+
+	subscribeConfig := subscribeClientConfig{
+		InputClientConfig: input.InputClientConfig{
+			OpcUAClientConfig: opcua.OpcUAClientConfig{
+				Endpoint:       "opc.tcp://opcua.demo-this.com:62544/Quickstarts/AlarmConditionServer",
+				SecurityPolicy: "None",
+				SecurityMode:   "None",
+				AuthMethod:     "Anonymous",
+				ConnectTimeout: config.Duration(10 * time.Second),
+				RequestTimeout: config.Duration(1 * time.Second),
+				Workarounds:    opcua.OpcUAWorkarounds{},
+			},
+			MetricName: "testing",
+			EventStreamingInput: &input.OpcUAEventStreamingInput{
+				Interval:    config.Duration(5 * time.Second),
+				EventType:   input.NodeIDWrapper{ID: eventType},
+				NodeIDs:     []input.NodeIDWrapper{{ID: pressureNode}},
+				SourceNames: []string{"SensorXYZ"},
+			},
+		},
+		SubscriptionInterval: 0,
+	}
+
+	_, err = subscribeConfig.createSubscribeClient(testutil.Logger{})
+	require.ErrorContains(t, err, "at least one Field must be specified")
+}
+
+func TestSubscribeClientConfigEventStereamingInputInvalidFields(t *testing.T) {
+	eventType, err := ua.ParseNodeID("ns=2;i=1001")
+	require.NoError(t, err)
+
+	pressureNode, err := ua.ParseNodeID("ns=3;s=Pressure")
+	require.NoError(t, err)
+
+	subscribeConfig := subscribeClientConfig{
+		InputClientConfig: input.InputClientConfig{
+			OpcUAClientConfig: opcua.OpcUAClientConfig{
+				Endpoint:       "opc.tcp://opcua.demo-this.com:62544/Quickstarts/AlarmConditionServer",
+				SecurityPolicy: "None",
+				SecurityMode:   "None",
+				AuthMethod:     "Anonymous",
+				ConnectTimeout: config.Duration(10 * time.Second),
+				RequestTimeout: config.Duration(1 * time.Second),
+				Workarounds:    opcua.OpcUAWorkarounds{},
+			},
+			MetricName: "testing",
+			EventStreamingInput: &input.OpcUAEventStreamingInput{
+				Interval:    config.Duration(5 * time.Second),
+				EventType:   input.NodeIDWrapper{ID: eventType},
+				NodeIDs:     []input.NodeIDWrapper{{ID: pressureNode}},
+				SourceNames: []string{"SensorXYZ"},
+				Fields:      []string{"", "Field"},
+			},
+		},
+		SubscriptionInterval: 0,
+	}
+
+	_, err = subscribeConfig.createSubscribeClient(testutil.Logger{})
+	require.ErrorContains(t, err, "empty field name in fields stanza")
+}
