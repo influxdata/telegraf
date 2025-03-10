8974563967bc7db8c10ac99422d972590b5462e6
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2025-03-10 16:56:51 +0100

    fix(inputs.huebridge): Cleanup and fix linter issues (#16606)

--- plugins/inputs/huebridge/huebridge.go
+++ plugins/inputs/huebridge/huebridge.go
@@ -3,6 +3,9 @@ package huebridge
 
 import (
 	_ "embed"
+	"errors"
+	"fmt"
+	"net/url"
 	"sync"
 	"time"
 
@@ -16,9 +19,9 @@ import (
 var sampleConfig string
 
 type RemoteClientConfig struct {
-	RemoteClientId     string `toml:"remote_client_id"`
+	RemoteClientID     string `toml:"remote_client_id"`
 	RemoteClientSecret string `toml:"remote_client_secret"`
-	RemoteCallbackUrl  string `toml:"remote_callback_url"`
+	RemoteCallbackURL  string `toml:"remote_callback_url"`
 	RemoteTokenDir     string `toml:"remote_token_dir"`
 }
 
@@ -38,15 +41,45 @@ func (*HueBridge) SampleConfig() string {
 }
 
 func (h *HueBridge) Init() error {
-	h.bridges = make([]*bridge, 0, len(h.BridgeUrls))
-	for _, bridgeUrl := range h.BridgeUrls {
-		bridge, err := newBridge(bridgeUrl, h.RoomAssignments, &h.RemoteClientConfig, &h.ClientConfig, h.Timeout, h.Log)
-		if err != nil {
-			h.Log.Warnf("Failed to instantiate bridge for URL %s: %s", bridgeUrl, err)
-			continue
-		}
-		h.bridges = append(h.bridges, bridge)
+	tlsCfg, err := h.ClientConfig.TLSConfig()
+	if err != nil {
+		return fmt.Errorf("creating TLS configuration failed: %w", err)
 	}
+
+	h.bridges = make([]*bridge, 0, len(h.BridgeUrls))
+	for _, b := range h.BridgeUrls {
+		u, err := url.Parse(b)
+		if err != nil {
+			return fmt.Errorf("failed to parse bridge URL %s: %w", b, err)
+		}
+
+		switch u.Scheme {
+		case "address", "cloud", "mdns":
+			// Do nothing, those are valid
+		case "remote":
+			// Remote scheme also requires a configured rcc
+			if h.RemoteClientID == "" || h.RemoteClientSecret == "" || h.RemoteTokenDir == "" {
+				return errors.New("missing remote application credentials and/or token director not configured")
+			}
+		default:
+			return fmt.Errorf("unrecognized scheme %s in URL %s", u.Scheme, b)
+		}
+
+		// All schemes require a password in the URL
+		if _, set := u.User.Password(); !set {
+			return fmt.Errorf("missing password in URL %s", u)
+		}
+
+		h.bridges = append(h.bridges, &bridge{
+			url:                   u,
+			configRoomAssignments: h.RoomAssignments,
+			remoteCfg:             &h.RemoteClientConfig,
+			tlsCfg:                tlsCfg,
+			timeout:               time.Duration(h.Timeout),
+			log:                   h.Log,
+		})
+	}
+
 	return nil
 }
 
