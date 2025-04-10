656b7f42667ab27ccd990e81c7ab4d76ea721977
Paweł Żak <pawel.zak.pawel@gmail.com>
2025-04-10 19:41:41 +0200

    chore: Fix linter findings for `revive:exported` in `plugins/inputs/system` (#16721)
    (cherry picked from commit 59234e6aa63b8c276edee11f8474f9eb1d5a9a13)

--- plugins/inputs/swap/swap_test.go
+++ plugins/inputs/swap/swap_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/shirou/gopsutil/v4/mem"
 	"github.com/stretchr/testify/require"
 
-	"github.com/influxdata/telegraf/plugins/inputs/system"
+	"github.com/influxdata/telegraf/plugins/common/psutil"
 	"github.com/influxdata/telegraf/testutil"
 )
 
 func TestSwapStats(t *testing.T) {
-	var mps system.MockPS
+	var mps psutil.MockPS
 	var err error
 	defer mps.AssertExpectations(t)
 	var acc testutil.Accumulator
