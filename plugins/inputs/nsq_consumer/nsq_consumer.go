fc6f75f1779813d48d062b87f9a6ec2c8fe8efb7
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2025-06-12 23:10:19 +0200

    feat(migrations): Add option migration for inputs.nsq_consumer (#17166)

--- plugins/inputs/nsq_consumer/nsq_consumer.go
+++ plugins/inputs/nsq_consumer/nsq_consumer.go
@@ -21,7 +21,6 @@ const (
 )
 
 type NSQConsumer struct {
-	Server                 string          `toml:"server" deprecated:"1.5.0;1.35.0;use 'nsqd' instead"`
 	Nsqd                   []string        `toml:"nsqd"`
 	Nsqlookupd             []string        `toml:"nsqlookupd"`
 	Topic                  string          `toml:"topic"`
@@ -59,11 +58,6 @@ func (*NSQConsumer) SampleConfig() string {
 }
 
 func (n *NSQConsumer) Init() error {
-	// For backward compatibility
-	if n.Server != "" {
-		n.Nsqd = append(n.Nsqd, n.Server)
-	}
-
 	// Check if we have anything to connect to
 	if len(n.Nsqlookupd) == 0 && len(n.Nsqd) == 0 {
 		return errors.New("either 'nsqd' or 'nsqlookupd' needs to be specified")
