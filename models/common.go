c054f8ce4991eccce25f35725a1482b02a858a0d
Sven Rebhan <srebhan@influxdata.com>
2025-07-16 20:00:13 +0200

    feat(selfstat): Implement collection of plugin-internal statistics

--- models/common.go
+++ models/common.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/selfstat"
 )
 
 // logName returns the log-friendly name/type.
@@ -26,13 +27,35 @@ func SetLoggerOnPlugin(i interface{}, logger telegraf.Logger) {
 		return
 	}
 
-	switch field.Type().String() {
-	case "telegraf.Logger":
-		if field.CanSet() {
-			field.Set(reflect.ValueOf(logger))
-		}
-	default:
-		logger.Debugf("Plugin %q defines a 'Log' field on its struct of an unexpected type %q. Expected telegraf.Logger",
-			valI.Type().Name(), field.Type().String())
+	if field.Type().String() != "telegraf.Logger" || !field.CanSet() {
+		logger.Debugf(
+			"Plugin %q defines a 'Log' field on its struct of an unexpected type %q. Expected telegraf.Logger",
+			valI.Type().Name(), field.Type().String(),
+		)
+		return
 	}
+
+	field.Set(reflect.ValueOf(logger))
+}
+
+func SetStatisticsOnPlugin(plugin interface{}, logger telegraf.Logger, tags map[string]string) {
+	// Find the statistics collector
+	instance := reflect.Indirect(reflect.ValueOf(plugin))
+	field := instance.FieldByName("Statistics")
+	if !field.IsValid() {
+		return
+	}
+
+	// Validate the type and make sure we can actually set the struct field
+	if field.Type().String() != "*selfstat.Collector" || !field.CanSet() {
+		logger.Debugf(
+			"Plugin %q defines a 'Statistics' field on its struct of an unexpected type %q. Expected *selfstat.Collector",
+			instance.Type().Name(), field.Type().String(),
+		)
+		return
+	}
+
+	// Create a new collector and set it
+	collector := selfstat.NewCollector(tags)
+	field.Set(reflect.ValueOf(collector))
 }
