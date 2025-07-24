8519eda217cd29f5d56a4a37d9c5aa106e46b6ae
Neelay Upadhyaya <neelay.upadhyaya@nutanix.com>
2025-07-24 11:37:49 +0530

    add nil check for mytomb as well

--- cmd/telegraf/telegraf.go
+++ cmd/telegraf/telegraf.go
@@ -327,7 +327,9 @@ func (t *Telegraf) watchLocalConfig(ctx context.Context, signals chan os.Signal,
 			}
 
 		case <-mytomb.Dying():
-			reloadTimer.Stop()
+			if reloadTimer != nil {
+				reloadTimer.Stop()
+			}
 			log.Printf("I! Config watcher %q ended\n", fConfig)
 			return
 		}
