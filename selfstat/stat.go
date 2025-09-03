f7a9a77e66f93d9c9eca1f7df2804fa221f7e6d8
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2025-09-03 09:09:47 +0200

    Update selfstat/stat.go
    Co-authored-by: skartikey <1942366+skartikey@users.noreply.github.com>

--- selfstat/stat.go
+++ selfstat/stat.go
@@ -41,6 +41,7 @@ func (s *stat) Tags() map[string]string {
 	return m
 }
 
+// Unregister removes this stat from the registry only
 func (s *stat) Unregister() {
 	registry.remove(s.measurement, s.field, s.tags)
 }
