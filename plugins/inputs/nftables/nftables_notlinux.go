841b7e4c7643fc82efa822c6554f9f587c01aefb
Anthony Johnson <ajohnson@aurora.tech>
2025-09-23 23:37:44 +0000

    fix `make check` errors

--- plugins/inputs/nftables/nftables_notlinux.go
+++ plugins/inputs/nftables/nftables_notlinux.go
@@ -4,30 +4,30 @@
 package nftables
 
 import (
-        _ "embed"
+	_ "embed"
 
-        "github.com/influxdata/telegraf"
-        "github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
 //go:embed sample.conf
 var sampleConfig string
 
 type Nftables struct {
-        Log telegraf.Logger `toml:"-"`
+	Log telegraf.Logger `toml:"-"`
 }
 
 func (*Nftables) SampleConfig() string { return sampleConfig }
 
 func (i *Nftables) Init() error {
-        i.Log.Warn("Current platform is not supported")
-        return nil
+	i.Log.Warn("Current platform is not supported")
+	return nil
 }
 
 func (*Nftables) Gather(_ telegraf.Accumulator) error { return nil }
 
 func init() {
-        inputs.Add("nftables", func() telegraf.Input {
-                return &Nftables{}
-        })
+	inputs.Add("nftables", func() telegraf.Input {
+		return &Nftables{}
+	})
 }
