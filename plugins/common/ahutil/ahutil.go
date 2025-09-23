6f754c45c056ae47a399ee3ce72dfa23ef4f82c9
Chayan Biswas <128743402+chayan-04@users.noreply.github.com>
2025-09-23 23:42:12 +0530

    HOS-23669: Adding ssid and bssid to ai-rrm (#79)
    Adding ssid and bssid info to ai-rrm. Also
    changing metric name from NbrStats to
    rfNbrStats

--- plugins/common/ahutil/ahutil.go
+++ plugins/common/ahutil/ahutil.go
@@ -4,6 +4,7 @@ import (
 	"golang.org/x/sys/unix"
 	"syscall"
 	"os"
+	"fmt"
 	"strings"
 	"bufio"
 	"strconv"
@@ -439,3 +440,6 @@ func GetTxPower(ifname string) int8 {
 	return -1
 }
 
+func MacToString(mac [6]byte) string {
+	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5])
+}
