f67b136fe338fa21631cb0c42403546bdbd9ece7
Maya Strandboge <mstrandboge@influxdata.com>
2025-10-15 11:38:21 -0600

    change for both scripts

--- scripts/check-file-changes.sh
+++ scripts/check-file-changes.sh
@@ -8,5 +8,5 @@ if [[ ${CIRCLE_PULL_REQUEST##*/} != "" ]]; then # Only skip if their is an assoc
     # Ask git for all the differences between this branch and master
     # Then use grep to look for changes in the .circleci/ directory, anything named *.go or *.mod or *.sum or *.sh or Makefile
     # If no match is found, then circleci step halt will stop the CI job but mark it successful
-    git diff master --name-only --no-color | egrep -e "^(\.circleci\/.*)$|^(.*\.(go|mod|sum|sh))$|^Makefile$" || circleci step halt;
+    git diff --name-only "$(git merge-base HEAD origin/master)" | grep -E -e "^(\.circleci\/.*)$|^(.*\.(go|mod|sum|sh))$|^Makefile$" || circleci step halt;
 fi
