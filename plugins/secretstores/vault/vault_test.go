e0c19ae42e560dc90b1eeb84bff94cdf89968bed
Maya Strandboge <mstrandboge@influxdata.com>
2025-10-17 11:16:22 -0600

    refactor and clean up tests

--- plugins/secretstores/vault/vault_test.go
+++ plugins/secretstores/vault/vault_test.go
@@ -14,22 +14,19 @@ import (
 	"github.com/testcontainers/testcontainers-go/modules/vault"
 )
 
-func TestIntegrationKVv1(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration test in short mode")
-	}
-
-	ctx := context.Background()
-	mountPath := "my-mount-path"
-	secretPath := "my-secret-path"
-	secretName := "secret-some-name"
-	secretValue := "secret-some-value"
+func createContainer(t *testing.T, initCommands []string) (*vault.VaultContainer, func()) {
+	// Create container with a default AppRole setup
+	initCommands = append([]string{
+		"auth enable approle",
+		"policy write my-policy /tmp/policy.hcl",
+		"write auth/approle/role/my-role policies=my-policy",
+	}, initCommands...)
 
 	policyPath, err := filepath.Abs("testdata/policy.hcl")
 	require.NoError(t, err)
 
 	container, err := vault.Run(
-		ctx,
+		context.Background(),
 		"hashicorp/vault:1.20.4",
 		testcontainers.WithFiles(testcontainers.ContainerFile{
 			HostFilePath:      policyPath,
@@ -37,102 +34,18 @@ func TestIntegrationKVv1(t *testing.T) {
 			FileMode:          0644,
 		}),
 		vault.WithToken("SomeToken"),
-		vault.WithInitCommand(
-			"auth enable approle",
-			"policy write my-policy /tmp/policy.hcl",
-			"write auth/approle/role/my-role policies=my-policy",
-			fmt.Sprintf("secrets enable -path=%s kv-v1", mountPath),
-			fmt.Sprintf("kv put -mount=%s %s %s=%s", mountPath, secretPath, secretName, secretValue),
-		),
+		vault.WithInitCommand(initCommands...),
 	)
 
 	require.NoError(t, err)
-	defer container.Terminate(ctx) //nolint:errcheck // no check needed
 
-	state, err := container.State(ctx)
+	state, err := container.State(context.Background())
 	require.NoError(t, err)
 	require.True(t, state.Running)
 
-	addr, err := container.HttpHostAddress(ctx)
-	require.NoError(t, err)
-
-	plugin := &Vault{
-		ID:         "test_integration_kv_v1",
-		Address:    addr,
-		MountPath:  mountPath,
-		SecretPath: secretPath,
-		UseKVv1:    true,
-		AppRole: &appRole{
-			RoleID:   getRoleID(t, container),
-			SecretID: getSecretID(t, container),
-		},
+	return container, func() {
+		_ = container.Terminate(context.Background())
 	}
-
-	require.NoError(t, plugin.Init())
-
-	secret, err := plugin.Get(secretName)
-	require.NoError(t, err)
-	require.Equal(t, []byte(secretValue), secret)
-}
-
-func TestIntegrationKVv2(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration test in short mode")
-	}
-
-	ctx := context.Background()
-	mountPath := "my-mount-path"
-	secretPath := "my-secret-path"
-	secretName := "secret-some-name"
-	secretValue := "secret-some-value"
-
-	policyPath, err := filepath.Abs("testdata/policy.hcl")
-	require.NoError(t, err)
-
-	container, err := vault.Run(
-		ctx,
-		"hashicorp/vault:1.20.4",
-		testcontainers.WithFiles(testcontainers.ContainerFile{
-			HostFilePath:      policyPath,
-			ContainerFilePath: "/tmp/policy.hcl",
-			FileMode:          0644,
-		}),
-		vault.WithToken("SomeToken"),
-		vault.WithInitCommand(
-			"auth enable approle",
-			"policy write my-policy /tmp/policy.hcl",
-			"write auth/approle/role/my-role policies=my-policy",
-			fmt.Sprintf("secrets enable -path=%s kv-v2", mountPath),
-			fmt.Sprintf("kv put -mount=%s %s %s=%s", mountPath, secretPath, secretName, secretValue),
-		),
-	)
-
-	require.NoError(t, err)
-	defer container.Terminate(ctx) //nolint:errcheck // no check needed
-
-	state, err := container.State(ctx)
-	require.NoError(t, err)
-	require.True(t, state.Running)
-
-	addr, err := container.HttpHostAddress(ctx)
-	require.NoError(t, err)
-
-	plugin := &Vault{
-		ID:         "test_integration_kv_v2",
-		Address:    addr,
-		MountPath:  mountPath,
-		SecretPath: secretPath,
-		AppRole: &appRole{
-			RoleID:   getRoleID(t, container),
-			SecretID: getSecretID(t, container),
-		},
-	}
-
-	require.NoError(t, plugin.Init())
-
-	secret, err := plugin.Get(secretName)
-	require.NoError(t, err)
-	require.Equal(t, []byte(secretValue), secret)
 }
 
 func getRoleID(t *testing.T, container *vault.VaultContainer) string {
@@ -181,4 +94,79 @@ type SecretIDResponse struct {
 	Data struct {
 		SecretID string `json:"secret_id"`
 	} `json:"data"`
+}
+
+func TestIntegrationKVv1(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	mountPath := "my-mount-path"
+	secretPath := "my-secret-path"
+	secretName := "secret-some-name"
+	secretValue := "secret-some-value"
+
+	container, closer := createContainer(t, []string{
+		fmt.Sprintf("secrets enable -path=%s kv-v1", mountPath),
+		fmt.Sprintf("kv put -mount=%s %s %s=%s", mountPath, secretPath, secretName, secretValue),
+	})
+	defer closer()
+
+	addr, err := container.HttpHostAddress(context.Background())
+	require.NoError(t, err)
+
+	plugin := &Vault{
+		ID:         "test_integration_kv_v1",
+		Address:    addr,
+		MountPath:  mountPath,
+		SecretPath: secretPath,
+		UseKVv1:    true,
+		AppRole: &appRole{
+			RoleID:   getRoleID(t, container),
+			SecretID: getSecretID(t, container),
+		},
+	}
+
+	require.NoError(t, plugin.Init())
+
+	secret, err := plugin.Get(secretName)
+	require.NoError(t, err)
+	require.Equal(t, []byte(secretValue), secret)
+}
+
+func TestIntegrationKVv2(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	mountPath := "my-mount-path"
+	secretPath := "my-secret-path"
+	secretName := "secret-some-name"
+	secretValue := "secret-some-value"
+
+	container, closer := createContainer(t, []string{
+		fmt.Sprintf("secrets enable -path=%s kv-v2", mountPath),
+		fmt.Sprintf("kv put -mount=%s %s %s=%s", mountPath, secretPath, secretName, secretValue),
+	})
+	defer closer()
+
+	addr, err := container.HttpHostAddress(context.Background())
+	require.NoError(t, err)
+
+	plugin := &Vault{
+		ID:         "test_integration_kv_v2",
+		Address:    addr,
+		MountPath:  mountPath,
+		SecretPath: secretPath,
+		AppRole: &appRole{
+			RoleID:   getRoleID(t, container),
+			SecretID: getSecretID(t, container),
+		},
+	}
+
+	require.NoError(t, plugin.Init())
+
+	secret, err := plugin.Get(secretName)
+	require.NoError(t, err)
+	require.Equal(t, []byte(secretValue), secret)
 }
