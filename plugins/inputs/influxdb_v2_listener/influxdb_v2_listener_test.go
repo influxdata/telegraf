44204801bf29be39b1e76d817e82950e6a299f4e
Sven Rebhan <srebhan@influxdata.com>
2025-11-03 12:04:22 +0100

    test(inputs.influxdb_v2_listener): Fix flaky tests

--- plugins/inputs/influxdb_v2_listener/influxdb_v2_listener_test.go
+++ plugins/inputs/influxdb_v2_listener/influxdb_v2_listener_test.go
@@ -14,10 +14,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gofrs/uuid/v5"
 	"github.com/stretchr/testify/require"
 
 	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/selfstat"
 	"github.com/influxdata/telegraf/testutil"
 )
 
@@ -543,6 +545,15 @@ func TestHealth(t *testing.T) {
 	require.NoError(t, listener.Start(acc))
 	defer listener.Stop()
 
+	// Make sure we do have a unique stats for this instance to avoid side effects
+	id, err := uuid.NewV4()
+	require.NoError(t, err)
+	listener.healthsServed = selfstat.Register("influxdb_v2_listener", "healths_served", map[string]string{
+		"address": listener.ServiceAddress,
+		"id":      id.String(),
+	})
+	defer listener.healthsServed.Unregister()
+
 	// post ping to listener
 	resp, err := http.Get(createURL(listener, "http", "/api/v2/health", ""))
 	require.NoError(t, err)
@@ -591,6 +602,15 @@ func TestPing(t *testing.T) {
 	require.NoError(t, listener.Start(acc))
 	defer listener.Stop()
 
+	// Make sure we do have a unique stats for this instance to avoid side effects
+	id, err := uuid.NewV4()
+	require.NoError(t, err)
+	listener.pingsServed = selfstat.Register("influxdb_v2_listener", "pings_served", map[string]string{
+		"address": listener.ServiceAddress,
+		"id":      id.String(),
+	})
+	defer listener.pingsServed.Unregister()
+
 	resp, err := http.Get(createURL(listener, "http", "/ping", ""))
 	require.NoError(t, err)
 	require.EqualValues(t, 204, resp.StatusCode)
@@ -612,6 +632,15 @@ func TestReady(t *testing.T) {
 	require.NoError(t, listener.Init())
 	require.NoError(t, listener.Start(acc))
 	defer listener.Stop()
+
+	// Make sure we do have a unique stats for this instance to avoid side effects
+	id, err := uuid.NewV4()
+	require.NoError(t, err)
+	listener.readysServed = selfstat.Register("influxdb_v2_listener", "readys_served", map[string]string{
+		"address": listener.ServiceAddress,
+		"id":      id.String(),
+	})
+	defer listener.readysServed.Unregister()
 
 	// post ping to listener
 	resp, err := http.Get(createURL(listener, "http", "/api/v2/ready", ""))
@@ -722,6 +751,9 @@ func TestRateLimitedConnectionAcceptsNewRequestOnDelivery(t *testing.T) {
 	for _, m := range ms {
 		m.Accept()
 	}
+	require.Eventually(t, func() bool {
+		return listener.totalUndeliveredMetrics.Load() == 0
+	}, 3*time.Second, 100*time.Millisecond)
 
 	resp, err = http.Post(postURL, "", bytes.NewBufferString(msg)) // #nosec G107 -- url has to be dynamic due to dynamic port number
 	require.NoError(t, err)
