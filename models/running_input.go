0245d696d92a23f5c1bea9b3aeb88923dee95da7
wanzh <wanzh@nscc-tj.cn>
2026-03-12 15:41:13 +0800

    fix(agent): allow per-input collection_jitter to override with 0s

--- models/running_input.go
+++ models/running_input.go
@@ -92,6 +92,7 @@ type InputConfig struct {
 	ID                   string
 	Interval             time.Duration
 	CollectionJitter     time.Duration
+	CollectionJitterSet  bool
 	CollectionOffset     time.Duration
 	Precision            time.Duration
 	TimeSource           string
