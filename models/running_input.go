4b2a0bdf3bdd61ec25f206b9f4033a04ae9ba92a
Sven Rebhan <srebhan@influxdata.com>
2026-03-18 15:25:05 +0100

    fix(models): Add inputs startup-errors to gather statistics

--- models/running_input.go
+++ models/running_input.go
@@ -76,7 +76,7 @@ func NewRunningInput(input telegraf.Input, config *InputConfig) *RunningInput {
 			tags,
 		),
 		StartupErrors: selfstat.Register(
-			"write",
+			"gather",
 			"startup_errors",
 			tags,
 		),
