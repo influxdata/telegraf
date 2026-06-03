f27c89bcd6c3ed25f71dcf6b35d6d4404b185bc2
Sullivan Motley <sullivanmotley@MacBookAir.lan>
2026-06-03 19:04:32 -0400

    added oracle implementation for sql plugin

---
+++ plugins/outputs/sql/testdata/oracle/updatetable/expected_metric_one.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "expected_metric_one" (
+    "timestamp" TIMESTAMP,
+    "tag_one" VARCHAR2(4000),
+    "tag_two" VARCHAR2(4000),
+    "int64_one" NUMBER(38),
+    "int64_two" NUMBER(38),
+    "bool_one" BOOLEAN,
+    "bool_two" BOOLEAN,
+    "uint64_one" NUMBER(38),
+    "float64_one" NUMBER,
+    "tag_add_after_create" VARCHAR2(4000),
+    "bool_add_after_create" BOOLEAN
+);
+INSERT INTO "expected_metric_one" VALUES (TO_TIMESTAMP('2021-05-17 22:04:45', 'YYYY-MM-DD HH24:MI:SS'),'tag1','tag2',1234,2345,1,0,1000000000,3.1415,null,null);
+INSERT INTO "expected_metric_one" VALUES (TO_TIMESTAMP('2021-05-17 22:04:45', 'YYYY-MM-DD HH24:MI:SS'),null,null,null,null,null,null,null,null,'tag2',1);
