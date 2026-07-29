2453f0bd289c7943798a20b89cb845f0081611ab
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2026-07-29 17:31:21 +0200

    chore(inputs.cisco_telemetry_mdt): Cleanup parsing (#19323)

--- plugins/inputs/cisco_telemetry_mdt/parser.go
+++ plugins/inputs/cisco_telemetry_mdt/parser.go
@@ -110,7 +110,7 @@ func newParser(includeDelete bool, aliases, dmes map[string]string, embeddedTags
 	return p
 }
 
-func (p *parser) parse(
+func (s *parser) parse(
 	grouper *metric.SeriesGrouper,
 	content *telemetry.TelemetryField,
 	encodingPath string,
@@ -120,15 +120,15 @@ func (p *parser) parse(
 ) []error {
 	// Do alias lookup, to shorten measurement names
 	measurement := encodingPath
-	if alias, ok := p.aliases[encodingPath]; ok {
+	if alias, ok := s.aliases[encodingPath]; ok {
 		measurement = alias
 	} else {
-		p.Lock()
-		if !p.warned[encodingPath] {
-			p.log.Debugf("No measurement alias for encoding path: %s", encodingPath)
-			p.warned[encodingPath] = true
+		s.Lock()
+		if !s.warned[encodingPath] {
+			s.log.Debugf("No measurement alias for encoding path: %s", encodingPath)
+			s.warned[encodingPath] = true
 		}
-		p.Unlock()
+		s.Unlock()
 	}
 
 	// Determine what OS we are on and if the message encodes events
@@ -140,12 +140,12 @@ func (p *parser) parse(
 	state := &state{
 		path:        encodingPath,
 		measurement: measurement,
-		tagPrefix:   strings.ReplaceAll(encodingPath, "-", "_"),
+		tagPrefix:   strings.ReplaceAll(encodingPath, "-", "_") + "/",
 		isNXOS:      isNXOS,
 		isEvent:     isEvent,
-		extraTags:   p.extraTags,
-		propMap:     p.propMap,
-		nxpathMap:   p.nxpathMap[encodingPath],
+		extraTags:   s.extraTags,
+		propMap:     s.propMap,
+		nxpathMap:   s.nxpathMap[encodingPath],
 		grouper:     grouper,
 	}
 
@@ -171,7 +171,7 @@ func (p *parser) parse(
 	}
 
 	// Add a delete field if configured
-	if p.includeDelete {
+	if s.includeDelete {
 		grouper.Add(measurement, tags, timestamp, "delete", isDeleted)
 	}
 
@@ -207,7 +207,8 @@ func (s *state) parseField(field *telemetry.TelemetryField, prefix string, tags 
 	name := strings.ReplaceAll(field.Name, "-", "_")
 
 	// Exit early on fields to ignore
-	if (name == "modTs" || name == "createTs") && decode(field) == "never" {
+	value := decode(field)
+	if (name == "modTs" || name == "createTs") && value == "never" {
 		return nil
 	}
 
@@ -219,7 +220,7 @@ func (s *state) parseField(field *telemetry.TelemetryField, prefix string, tags 
 	}
 
 	// Decode scalar fields if present and exit
-	if value := decode(field); value != nil {
+	if value != nil {
 		if s.isNXOS {
 			// NXOS specific values take precedence if existing
 			if val := nxosValueXform(field, s.propMap, s.nxpathMap); val != nil {
@@ -232,132 +233,73 @@ func (s *state) parseField(field *telemetry.TelemetryField, prefix string, tags 
 	}
 
 	// Get extra-tags defined by the user in embedded_tags
-	extraTags := s.extraTags[s.tagPrefix+"/"+name]
-	if len(extraTags) > 0 {
-		for _, subfield := range field.Fields {
-			if _, isExtraTag := extraTags[subfield.Name]; isExtraTag {
-				tags[name+"/"+strings.ReplaceAll(subfield.Name, "-", "_")] = decodeTag(subfield)
-			}
+	extraTags := s.extraTags[s.tagPrefix+name]
+	fields := make([]*telemetry.TelemetryField, 0, len(field.Fields))
+	for _, subfield := range field.Fields {
+		if _, isExtraTag := extraTags[subfield.Name]; isExtraTag {
+			tags[name+"/"+strings.ReplaceAll(subfield.Name, "-", "_")] = decodeTag(subfield)
+		} else {
+			fields = append(fields, subfield)
 		}
+	}
+
+	// No more fields to process
+	if len(fields) == 0 {
+		return nil
 	}
 
 	// Extract special field elements
+	switch {
+	case s.isNXOS && field.Name == "attributes":
+		return s.parseDME(fields[0].Fields, prefix, tags, timestamp)
+	case s.isNXOS && field.Name == "children" && !s.isEvent:
+		// Parse the subfields according to their class
+		//nolint:prealloc // We expect errors to be empty by default
+		var errs []error
+		for _, sub := range fields {
+			errs = append(errs, s.parseClassAttributeField(sub, tags, timestamp)...)
+		}
+		return errs
+	case s.isNXOS && field.Name == "children" && s.isEvent:
+		return s.parseEvents(field.Fields, prefix, tags, timestamp)
+	case s.isNXOS && strings.HasPrefix(field.Name, "ROW_"):
+		return s.parseNXRows(fields, prefix, tags, timestamp)
+	}
+
+	// No special fields, continue with regular telemetry decoding of the tree
+	//nolint:prealloc // We expect errors to be empty by default
 	var errs []error
-	var nxAttributes, nxChildren, nxRows *telemetry.TelemetryField
-	for _, subfield := range field.Fields {
-		if s.isNXOS && subfield.Name == "attributes" && len(subfield.Fields) > 0 {
-			nxAttributes = subfield.Fields[0]
-		} else if s.isNXOS && subfield.Name == "children" && len(subfield.Fields) > 0 {
-			if !s.isEvent {
-				nxChildren = subfield
-			} else {
-				sub := subfield.Fields
-				if len(sub) > 0 && sub[0] != nil && len(sub[0].Fields) >= 2 {
-					if sub[0].Fields[0].Name == "subscriptionId" {
-						nxAttributes = sub[0].Fields[1].Fields[0].Fields[0].Fields[0].Fields[0].Fields[0]
-					} else if sub[0].Fields[1].Name == "subscriptionId" {
-						nxAttributes = sub[0].Fields[0].Fields[0].Fields[0].Fields[0].Fields[0].Fields[0]
-					}
-				}
-			}
-
-			// If we did not see any attributes yet walk the sub-fields and parse
-			// according to the class received
-			if nxAttributes == nil {
-				// call function walking over walking list.
-				for _, sub := range subfield.Fields {
-					errs = append(errs, s.parseClassAttributeField(sub, tags, timestamp)...)
-				}
-			}
-		} else if s.isNXOS && strings.HasPrefix(subfield.Name, "ROW_") {
-			// TODO: Verify if it is safe to override the nxRows variable. Can there be multiple ROW_abc entires?
-			nxRows = subfield
-		} else if _, isExtraTag := extraTags[subfield.Name]; !isExtraTag {
-			// Continue with regular telemetry decoding of the tree
-			errs = append(errs, s.parseField(subfield, name, tags, timestamp)...)
-		}
+	for _, sub := range fields {
+		errs = append(errs, s.parseField(sub, name, tags, timestamp)...)
 	}
-
-	if nxAttributes == nil && nxRows == nil {
-		return nil
-	}
-	if nxRows != nil {
-		// NXAPI structure: https://developer.cisco.com/docs/cisco-nexus-9000-series-nx-api-cli-reference-release-9-2x/
-		for _, row := range nxRows.Fields {
-			for i, subfield := range row.Fields {
-				if i == 0 { // First subfield contains the index, promote it from value to tag
-					tags[prefix] = decodeTag(subfield)
-					// We can have subfield so recursively handle it.
-					if len(row.Fields) == 1 {
-						tags["row_number"] = strconv.FormatInt(int64(i), 10)
-						errs = append(errs, s.parseField(subfield, "", tags, timestamp)...)
-					}
-				} else {
-					errs = append(errs, s.parseField(subfield, "", tags, timestamp)...)
-				}
-				// Nxapi we can't identify keys always from prefix
-				tags["row_number"] = strconv.FormatInt(int64(i), 10)
-			}
-			delete(tags, prefix)
-		}
-		return nil
-	}
-
-	// DME structure: https://developer.cisco.com/site/nxapi-dme-model-reference-api/
-	var rn string
-	var dn bool
-	for _, subfield := range nxAttributes.Fields {
-		switch subfield.Name {
-		case "rn":
-			rn = decodeTag(subfield)
-		case "dn":
-			dn = true
-		}
-	}
-
-	if len(rn) > 0 {
-		tags[prefix] = rn
-	} else if !dn { // Check for distinguished name being present
-		return []error{errors.New("failed while decoding NX-OS: missing 'dn' field")}
-	}
-
-	for _, subfield := range nxAttributes.Fields {
-		if subfield.Name != "rn" {
-			errs = append(errs, s.parseField(subfield, "", tags, timestamp)...)
-		}
-	}
-
-	if nxChildren != nil {
-		// This is a nested structure, children will inherit relative name keys of parent
-		for _, subfield := range nxChildren.Fields {
-			errs = append(errs, s.parseField(subfield, prefix, tags, timestamp)...)
-		}
-	}
-	delete(tags, prefix)
-
 	return errs
 }
 
 func (s *state) parseClassAttributeField(field *telemetry.TelemetryField, tags map[string]string, timestamp time.Time) []error {
 	if s.path == "rib" {
 		// handle native data path rib
-		s.parseRib(field, tags, timestamp)
+		s.parseRib(field.Fields, tags, timestamp)
 		return nil
 	}
 	if s.path == "microburst" {
-		s.parseMicroburst(field, tags, timestamp)
+		s.parseMicroburst(field.Fields, tags, timestamp)
 		return nil
 	}
 
-	// DME structure: https://developer.cisco.com/site/nxapi-dme-model-reference-api/
-	isDme := strings.Contains(s.path, "sys/")
-	if field == nil || !isDme || len(field.Fields) == 0 || len(field.Fields[0].Fields) == 0 || len(field.Fields[0].Fields[0].Fields) == 0 {
+	// Check if we have a DME structure
+	// https://developer.cisco.com/site/nxapi-dme-model-reference-api/
+	if !strings.Contains(s.path, "sys/") {
 		return nil
 	}
 
-	if field.Fields[0] != nil && field.Fields[0].Fields != nil && field.Fields[0].Fields[0] != nil && field.Fields[0].Fields[0].Fields[0].Name != "attributes" {
+	if field == nil || len(field.Fields) == 0 || len(field.Fields[0].Fields) == 0 || len(field.Fields[0].Fields[0].Fields) == 0 {
 		return nil
 	}
+
+	if field.Fields[0].Fields[0].Fields[0].Name != "attributes" {
+		return nil
+	}
+
 	nxAttributes := field.Fields[0].Fields[0].Fields[0].Fields[0]
 
 	// Find dn tag among list of attributes
@@ -380,8 +322,91 @@ func (s *state) parseClassAttributeField(field *telemetry.TelemetryField, tags m
 	return errs
 }
 
-func (s *state) parseRib(field *telemetry.TelemetryField, tags map[string]string, timestamp time.Time) {
-	for _, subfield := range field.Fields {
+// DME structure: https://developer.cisco.com/site/nxapi-dme-model-reference-api/
+func (s *state) parseDME(fields []*telemetry.TelemetryField, prefix string, tags map[string]string, timestamp time.Time) []error {
+	var errs []error
+
+	var rn string
+	var dn bool
+	for _, subfield := range fields {
+		switch subfield.Name {
+		case "rn":
+			rn = decodeTag(subfield)
+		case "dn":
+			dn = true
+		}
+	}
+
+	if len(rn) > 0 {
+		tags[prefix] = rn
+	} else if !dn { // Check for distinguished name being present
+		return []error{errors.New("failed while decoding NX-OS: missing 'dn' field")}
+	}
+
+	for _, subfield := range fields {
+		if subfield.Name != "rn" {
+			errs = append(errs, s.parseField(subfield, "", tags, timestamp)...)
+		}
+	}
+	delete(tags, prefix)
+
+	return errs
+}
+
+func (s *state) parseEvents(events []*telemetry.TelemetryField, prefix string, tags map[string]string, timestamp time.Time) []error {
+	var attrs *telemetry.TelemetryField
+	if events[0] != nil && len(events[0].Fields) >= 2 {
+		if events[0].Fields[0].Name == "subscriptionId" {
+			attrs = events[0].Fields[1].Fields[0].Fields[0].Fields[0].Fields[0].Fields[0]
+		} else if events[0].Fields[1].Name == "subscriptionId" {
+			attrs = events[0].Fields[0].Fields[0].Fields[0].Fields[0].Fields[0].Fields[0]
+		}
+	}
+
+	// Parse the attribute subfields according to their class
+	if attrs == nil {
+		//nolint:prealloc // We expect errors to be empty by default
+		var errs []error
+		for _, sub := range events {
+			errs = append(errs, s.parseClassAttributeField(sub, tags, timestamp)...)
+		}
+		return errs
+	}
+
+	return s.parseDME(attrs.Fields, prefix, tags, timestamp)
+}
+
+// NXAPI structure: https://developer.cisco.com/docs/cisco-nexus-9000-series-nx-api-cli-reference-release-9-2x/
+func (s *state) parseNXRows(rows []*telemetry.TelemetryField, prefix string, tags map[string]string, timestamp time.Time) []error {
+	var errs []error
+
+	for _, row := range rows {
+		if len(row.Fields) == 0 {
+			continue
+		}
+
+		tags[prefix] = decodeTag(row.Fields[0])
+		for i, field := range row.Fields {
+			if i == 0 { // First subfield contains the index, promote it from value to tag
+				// We can have subfield so recursively handle it.
+				if len(row.Fields) == 1 {
+					tags["row_number"] = strconv.FormatInt(int64(i), 10)
+					errs = append(errs, s.parseField(field, "", tags, timestamp)...)
+				}
+			} else {
+				errs = append(errs, s.parseField(field, "", tags, timestamp)...)
+			}
+			// Nxapi we can't identify keys always from prefix
+			tags["row_number"] = strconv.FormatInt(int64(i), 10)
+		}
+		delete(tags, prefix)
+	}
+
+	return errs
+}
+
+func (s *state) parseRib(fields []*telemetry.TelemetryField, tags map[string]string, timestamp time.Time) {
+	for _, subfield := range fields {
 		// For Every table fill the keys which are vrfName, address and masklen
 		switch subfield.Name {
 		case "vrfName", "address", "maskLen":
@@ -410,19 +435,13 @@ func (s *state) parseRib(field *telemetry.TelemetryField, tags map[string]string
 	}
 }
 
-func (s *state) parseMicroburst(field *telemetry.TelemetryField, tags map[string]string, timestamp time.Time) {
-	var nxMicro *telemetry.TelemetryField
-	var nxMicro1 *telemetry.TelemetryField
-	if len(field.Fields) > 3 {
-		nxMicro = field.Fields[2]
-		if len(nxMicro.Fields) > 0 {
-			nxMicro1 = nxMicro.Fields[0]
-			if len(nxMicro1.Fields) >= 3 {
-				nxMicro = nxMicro1.Fields[3]
-			}
-		}
+func (s *state) parseMicroburst(fields []*telemetry.TelemetryField, tags map[string]string, timestamp time.Time) {
+	if len(fields) < 3 || len(fields[2].Fields) == 0 || len(fields[2].Fields[0].Fields) < 4 {
+		return
 	}
-	for _, subfield := range nxMicro.Fields {
+
+	root := fields[2].Fields[0].Fields[3]
+	for _, subfield := range root.Fields {
 		if subfield.Name == "interfaceName" {
 			tags[subfield.Name] = decodeTag(subfield)
 		}
