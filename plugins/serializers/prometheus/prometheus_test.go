858a0adb5234fcbf2a2b553596d526b2056c5443
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2026-08-21 17:54:11 +0200

    test(serializers): Move benchmark test-data to test-only location (#19504)

--- plugins/serializers/prometheus/prometheus_test.go
+++ plugins/serializers/prometheus/prometheus_test.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
-	"github.com/influxdata/telegraf/plugins/serializers"
+	"github.com/influxdata/telegraf/testutil/serializers"
 )
 
 func TestSerialize(t *testing.T) {
@@ -766,7 +766,7 @@ rpc_duration_seconds_count 2693
 func BenchmarkSerialize(b *testing.B) {
 	s := &Serializer{}
 	require.NoError(b, s.Init())
-	metrics := serializers.BenchmarkMetrics(b)
+	metrics := serializers.BenchmarkMetrics()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_, err := s.Serialize(metrics[i%len(metrics)])
@@ -777,7 +777,7 @@ func BenchmarkSerialize(b *testing.B) {
 func BenchmarkSerializeBatch(b *testing.B) {
 	s := &Serializer{}
 	require.NoError(b, s.Init())
-	m := serializers.BenchmarkMetrics(b)
+	m := serializers.BenchmarkMetrics()
 	metrics := m[:]
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
