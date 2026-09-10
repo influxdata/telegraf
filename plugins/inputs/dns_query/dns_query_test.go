a1d258a51780d1af0b81f042e176bfaf6ca94798
Sven Rebhan <36194019+srebhan@users.noreply.github.com>
2026-09-10 17:49:14 +0200

    chore(inputs.dns_query): Cleanup code (#19669)
    (cherry picked from commit 8593ad12750ccb7a503666273a378148253e0e23)

--- plugins/inputs/dns_query/dns_query_test.go
+++ plugins/inputs/dns_query/dns_query_test.go
@@ -13,198 +13,46 @@ import (
 	"github.com/influxdata/telegraf/testutil"
 )
 
-var (
-	servers = []string{"8.8.8.8"}
-	domains = []string{"google.com"}
-)
-
-func TestGathering(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping network-dependent test in short mode.")
-	}
-
-	dnsConfig := DNSQuery{
-		Servers: servers,
-		Domains: domains,
-		Timeout: config.Duration(2 * time.Second),
-	}
-
-	var acc testutil.Accumulator
-	require.NoError(t, dnsConfig.Init())
-	require.NoError(t, acc.GatherError(dnsConfig.Gather))
-	m, ok := acc.Get("dns_query")
-	require.True(t, ok)
-	queryTime, ok := m.Fields["query_time_ms"].(float64)
-	require.True(t, ok)
-	require.NotEqual(t, float64(0), queryTime)
-}
-
-func TestGatherInvalid(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping network-dependent test in short mode.")
-	}
-
-	dnsConfig := DNSQuery{
-		Servers: servers,
-		Domains: []string{"qwerty123.example.com"},
-		Timeout: config.Duration(1 * time.Second),
-	}
-
-	var acc testutil.Accumulator
-	require.NoError(t, dnsConfig.Init())
-	require.NoError(t, dnsConfig.Gather(&acc))
-	require.Empty(t, acc.Errors)
-}
-
-func TestGatheringMxRecord(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping network-dependent test in short mode.")
-	}
-
-	dnsConfig := DNSQuery{
-		Servers:    servers,
-		Domains:    domains,
-		RecordType: "MX",
-		Timeout:    config.Duration(2 * time.Second),
-	}
-	var acc testutil.Accumulator
-
-	require.NoError(t, dnsConfig.Init())
-	require.NoError(t, acc.GatherError(dnsConfig.Gather))
-	m, ok := acc.Get("dns_query")
-	require.True(t, ok)
-	queryTime, ok := m.Fields["query_time_ms"].(float64)
-	require.True(t, ok)
-	require.NotEqual(t, float64(0), queryTime)
-	preference, ok := m.Fields["preference"].(uint16)
-	require.True(t, ok)
-	require.NotEqual(t, 0, preference)
-}
-
-func TestGatheringRootDomain(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping network-dependent test in short mode.")
-	}
-
-	dnsConfig := DNSQuery{
-		Servers:    servers,
-		Domains:    []string{"."},
-		RecordType: "MX",
-		Timeout:    config.Duration(2 * time.Second),
-	}
-	require.NoError(t, dnsConfig.Init())
-
-	var acc testutil.Accumulator
-	require.NoError(t, acc.GatherError(dnsConfig.Gather))
-
-	m, ok := acc.Get("dns_query")
-	require.True(t, ok)
-	queryTime, ok := m.Fields["query_time_ms"].(float64)
-	require.True(t, ok)
-
-	expected := []telegraf.Metric{
-		metric.New(
-			"dns_query",
-			map[string]string{
-				"server":      "8.8.8.8",
-				"domain":      ".",
-				"record_type": "MX",
-				"rcode":       "NOERROR",
-				"result":      "success",
+func TestInit(t *testing.T) {
+	tests := []struct {
+		name     string
+		plugin   *DNSQuery
+		expected *DNSQuery
+	}{
+		{
+			name: "domain",
+			plugin: &DNSQuery{
+				Domains: []string{"google.com"},
 			},
-			map[string]interface{}{
-				"rcode_value":   0,
-				"result_code":   uint64(0),
-				"query_time_ms": queryTime,
+			expected: &DNSQuery{
+				Network:    "udp",
+				RecordType: "NS",
+				Domains:    []string{"google.com"},
+				Port:       53,
 			},
-			time.Unix(0, 0),
-		),
-	}
-	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
-}
-
-func TestMetricContainsServerAndDomainAndRecordTypeTags(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping network-dependent test in short mode.")
-	}
-
-	dnsConfig := DNSQuery{
-		Servers: servers,
-		Domains: domains,
-		Timeout: config.Duration(2 * time.Second),
-	}
-	require.NoError(t, dnsConfig.Init())
-
-	var acc testutil.Accumulator
-	require.NoError(t, acc.GatherError(dnsConfig.Gather))
-
-	m, ok := acc.Get("dns_query")
-	require.True(t, ok)
-	queryTime, ok := m.Fields["query_time_ms"].(float64)
-	require.True(t, ok)
-	expected := []telegraf.Metric{
-		metric.New(
-			"dns_query",
-			map[string]string{
-				"server":      "8.8.8.8",
-				"domain":      "google.com",
-				"record_type": "NS",
-				"rcode":       "NOERROR",
-				"result":      "success",
+		},
+		{
+			name: "timeout",
+			plugin: &DNSQuery{
+				Timeout: config.Duration(2 * time.Second),
 			},
-			map[string]interface{}{
-				"rcode_value":   0,
-				"result_code":   uint64(0),
-				"query_time_ms": queryTime,
+			expected: &DNSQuery{
+				Network:    "udp",
+				RecordType: "NS",
+				Domains:    []string{"."},
+				Port:       53,
+				Timeout:    config.Duration(2 * time.Second),
 			},
-			time.Unix(0, 0),
-		),
-	}
-	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
-}
-
-func TestGatheringTimeout(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping network-dependent test in short mode.")
+		},
 	}
 
-	dnsConfig := DNSQuery{
-		Servers: servers,
-		Domains: domains,
-		Timeout: config.Duration(1 * time.Second),
-		Port:    60054,
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			plugin := tt.plugin
+			require.NoError(t, plugin.Init())
+			require.EqualExportedValues(t, tt.expected, plugin)
+		})
 	}
-	require.NoError(t, dnsConfig.Init())
-
-	var acc testutil.Accumulator
-	channel := make(chan error, 1)
-	go func() {
-		channel <- acc.GatherError(dnsConfig.Gather)
-	}()
-	select {
-	case err := <-channel:
-		require.NoError(t, err)
-	case <-time.After(time.Second * 2):
-		require.Fail(t, "DNS query did not timeout")
-	}
-}
-
-func TestSettingDefaultValues(t *testing.T) {
-	dnsConfig := DNSQuery{
-		Timeout: config.Duration(2 * time.Second),
-	}
-	require.NoError(t, dnsConfig.Init())
-	require.Equal(t, []string{"."}, dnsConfig.Domains, "Default domain not equal \".\"")
-	require.Equal(t, "NS", dnsConfig.RecordType, "Default record type not equal 'NS'")
-	require.Equal(t, 53, dnsConfig.Port, "Default port number not equal 53")
-	require.Equal(t, config.Duration(2*time.Second), dnsConfig.Timeout, "Default timeout not equal 2s")
-
-	dnsConfig = DNSQuery{
-		Domains: []string{"."},
-		Timeout: config.Duration(2 * time.Second),
-	}
-	require.NoError(t, dnsConfig.Init())
-	require.Equal(t, "NS", dnsConfig.RecordType, "Default record type not equal 'NS'")
 }
 
 func TestRecordTypeParser(t *testing.T) {
@@ -260,25 +108,193 @@ func TestRecordTypeParser(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.record, func(t *testing.T) {
-			plugin := DNSQuery{
+			plugin := &DNSQuery{
 				Timeout:    config.Duration(2 * time.Second),
 				Domains:    []string{"example.com"},
 				RecordType: tt.record,
 			}
 			require.NoError(t, plugin.Init())
-			recordType, err := plugin.parseRecordType()
-			require.NoError(t, err)
-			require.Equal(t, tt.expected, recordType)
+			require.Equal(t, tt.expected, plugin.record)
 		})
 	}
 }
 
 func TestRecordTypeParserError(t *testing.T) {
-	plugin := DNSQuery{
+	plugin := &DNSQuery{
 		Timeout:    config.Duration(2 * time.Second),
+		Domains:    []string{"example.com"},
 		RecordType: "nil",
 	}
+	require.ErrorContains(t, plugin.Init(), "record type \"nil\" not recognized")
+}
 
-	_, err := plugin.parseRecordType()
-	require.Error(t, err)
+func TestGathering(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping network-dependent test in short mode.")
+	}
+
+	dnsConfig := &DNSQuery{
+		Servers: []string{"8.8.8.8"},
+		Domains: []string{"google.com"},
+		Timeout: config.Duration(2 * time.Second),
+	}
+
+	var acc testutil.Accumulator
+	require.NoError(t, dnsConfig.Init())
+	require.NoError(t, acc.GatherError(dnsConfig.Gather))
+	m, ok := acc.Get("dns_query")
+	require.True(t, ok)
+	queryTime, ok := m.Fields["query_time_ms"].(float64)
+	require.True(t, ok)
+	require.NotEqual(t, float64(0), queryTime)
+}
+
+func TestGatherInvalid(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping network-dependent test in short mode.")
+	}
+
+	dnsConfig := &DNSQuery{
+		Servers: []string{"8.8.8.8"},
+		Domains: []string{"qwerty123.example.com"},
+		Timeout: config.Duration(1 * time.Second),
+	}
+
+	var acc testutil.Accumulator
+	require.NoError(t, dnsConfig.Init())
+	require.NoError(t, dnsConfig.Gather(&acc))
+	require.Empty(t, acc.Errors)
+}
+
+func TestGatheringMxRecord(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping network-dependent test in short mode.")
+	}
+
+	dnsConfig := &DNSQuery{
+		Servers:    []string{"8.8.8.8"},
+		Domains:    []string{"google.com"},
+		RecordType: "MX",
+		Timeout:    config.Duration(2 * time.Second),
+	}
+	var acc testutil.Accumulator
+
+	require.NoError(t, dnsConfig.Init())
+	require.NoError(t, acc.GatherError(dnsConfig.Gather))
+	m, ok := acc.Get("dns_query")
+	require.True(t, ok)
+	queryTime, ok := m.Fields["query_time_ms"].(float64)
+	require.True(t, ok)
+	require.NotEqual(t, float64(0), queryTime)
+	preference, ok := m.Fields["preference"].(uint16)
+	require.True(t, ok)
+	require.NotEqual(t, 0, preference)
+}
+
+func TestGatheringRootDomain(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping network-dependent test in short mode.")
+	}
+
+	dnsConfig := &DNSQuery{
+		Servers:    []string{"8.8.8.8"},
+		Domains:    []string{"."},
+		RecordType: "MX",
+		Timeout:    config.Duration(2 * time.Second),
+	}
+	require.NoError(t, dnsConfig.Init())
+
+	var acc testutil.Accumulator
+	require.NoError(t, acc.GatherError(dnsConfig.Gather))
+
+	m, ok := acc.Get("dns_query")
+	require.True(t, ok)
+	queryTime, ok := m.Fields["query_time_ms"].(float64)
+	require.True(t, ok)
+
+	expected := []telegraf.Metric{
+		metric.New(
+			"dns_query",
+			map[string]string{
+				"server":      "8.8.8.8",
+				"domain":      ".",
+				"record_type": "MX",
+				"rcode":       "NOERROR",
+				"result":      "success",
+			},
+			map[string]interface{}{
+				"rcode_value":   0,
+				"result_code":   uint64(0),
+				"query_time_ms": queryTime,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
+}
+
+func TestMetricContainsServerAndDomainAndRecordTypeTags(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping network-dependent test in short mode.")
+	}
+
+	dnsConfig := &DNSQuery{
+		Servers: []string{"8.8.8.8"},
+		Domains: []string{"google.com"},
+		Timeout: config.Duration(2 * time.Second),
+	}
+	require.NoError(t, dnsConfig.Init())
+
+	var acc testutil.Accumulator
+	require.NoError(t, acc.GatherError(dnsConfig.Gather))
+
+	m, ok := acc.Get("dns_query")
+	require.True(t, ok)
+	queryTime, ok := m.Fields["query_time_ms"].(float64)
+	require.True(t, ok)
+	expected := []telegraf.Metric{
+		metric.New(
+			"dns_query",
+			map[string]string{
+				"server":      "8.8.8.8",
+				"domain":      "google.com",
+				"record_type": "NS",
+				"rcode":       "NOERROR",
+				"result":      "success",
+			},
+			map[string]interface{}{
+				"rcode_value":   0,
+				"result_code":   uint64(0),
+				"query_time_ms": queryTime,
+			},
+			time.Unix(0, 0),
+		),
+	}
+	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics(), testutil.IgnoreTime())
+}
+
+func TestGatheringTimeout(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping network-dependent test in short mode.")
+	}
+
+	dnsConfig := &DNSQuery{
+		Servers: []string{"8.8.8.8"},
+		Domains: []string{"google.com"},
+		Timeout: config.Duration(1 * time.Second),
+		Port:    60054,
+	}
+	require.NoError(t, dnsConfig.Init())
+
+	var acc testutil.Accumulator
+	channel := make(chan error, 1)
+	go func() {
+		channel <- acc.GatherError(dnsConfig.Gather)
+	}()
+	select {
+	case err := <-channel:
+		require.NoError(t, err)
+	case <-time.After(time.Second * 2):
+		require.Fail(t, "DNS query did not timeout")
+	}
 }
