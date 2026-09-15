504e58af59ed207cf2dbf962a96be81ac0da7ef9
skartikey <1942366+skartikey@users.noreply.github.com>
2026-09-15 15:53:01 +0100

    chore(inputs.vault): Merge identical counter and summary structs

--- plugins/inputs/vault/vault_metrics.go
+++ plugins/inputs/vault/vault_metrics.go
@@ -1,10 +1,10 @@
 package vault
 
 type sysMetrics struct {
-	Timestamp string    `json:"timestamp"`
-	Gauges    []gauge   `json:"Gauges"`
-	Counters  []counter `json:"Counters"`
-	Summaries []summary `json:"Samples"`
+	Timestamp string         `json:"timestamp"`
+	Gauges    []gauge        `json:"Gauges"`
+	Counters  []sampledValue `json:"Counters"`
+	Summaries []sampledValue `json:"Samples"`
 }
 
 type baseInfo struct {
@@ -17,18 +17,7 @@ type gauge struct {
 	Value float64 `json:"Value"`
 }
 
-type counter struct {
-	baseInfo
-	Count  int     `json:"Count"`
-	Rate   float64 `json:"Rate"`
-	Sum    float64 `json:"Sum"`
-	Min    float64 `json:"Min"`
-	Max    float64 `json:"Max"`
-	Mean   float64 `json:"Mean"`
-	Stddev float64 `json:"Stddev"`
-}
-
-type summary struct {
+type sampledValue struct {
 	baseInfo
 	Count  int     `json:"Count"`
 	Rate   float64 `json:"Rate"`
