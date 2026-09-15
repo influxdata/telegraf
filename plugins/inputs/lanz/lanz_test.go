816213c6966041ba856bed95632df143c51a95a4
SungJin1212 <tjdwls1201@gmail.com>
2026-09-15 17:50:13 +0900

    chore: Use new(expr) instead of pointer helper functions
    Signed-off-by: SungJin1212 <tjdwls1201@gmail.com>

--- plugins/inputs/lanz/lanz_test.go
+++ plugins/inputs/lanz/lanz_test.go
@@ -14,7 +14,7 @@ import (
 var testProtoBufCongestionRecord1 = &pb.LanzRecord{
 	CongestionRecord: &pb.CongestionRecord{
 		Timestamp:          proto.Uint64(100000000000000),
-		IntfName:           proto.String("eth1"),
+		IntfName:           new("eth1"),
 		SwitchId:           proto.Uint32(1),
 		PortId:             proto.Uint32(1),
 		QueueSize:          proto.Uint32(1),
@@ -23,13 +23,13 @@ var testProtoBufCongestionRecord1 = &pb.LanzRecord{
 		TimeOfMaxQLen:      proto.Uint64(100000000000000),
 		TxLatency:          proto.Uint32(100),
 		QDropCount:         proto.Uint32(1),
-		FabricPeerIntfName: proto.String("FabricPeerIntfName1"),
+		FabricPeerIntfName: new("FabricPeerIntfName1"),
 	},
 }
 var testProtoBufCongestionRecord2 = &pb.LanzRecord{
 	CongestionRecord: &pb.CongestionRecord{
 		Timestamp:          proto.Uint64(200000000000000),
-		IntfName:           proto.String("eth2"),
+		IntfName:           new("eth2"),
 		SwitchId:           proto.Uint32(2),
 		PortId:             proto.Uint32(2),
 		QueueSize:          proto.Uint32(2),
@@ -38,7 +38,7 @@ var testProtoBufCongestionRecord2 = &pb.LanzRecord{
 		TimeOfMaxQLen:      proto.Uint64(200000000000000),
 		TxLatency:          proto.Uint32(200),
 		QDropCount:         proto.Uint32(2),
-		FabricPeerIntfName: proto.String("FabricPeerIntfName2"),
+		FabricPeerIntfName: new("FabricPeerIntfName2"),
 	},
 }
 
